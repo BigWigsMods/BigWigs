86b96f925086612fba754a617a8623eb04103d98
handdol <handdol@users.noreply.wowace.com>
2006-05-08 23:04:44 +0000

    BigWigs  - Merging error is corrected

--- BWL/Vaelastrasz.lua
+++ BWL/Vaelastrasz.lua
@@ -3,17 +3,31 @@ BigWigsVaelastrasz = AceAddon:new({
 	cmd           = AceChatCmd:new({}, {}),
 
 	zonename = "BWL",
-	enabletrigger = GetLocale() == "deDE" and "Vaelastrasz der Verdorbene" or "Vaelastrasz the Corrupt",
+	enabletrigger = GetLocale() == "koKR" and "타락한 밸라스트라즈"
+		or GetLocale() == "deDE" and "Vaelastrasz der Verdorbene" 
+		or "Vaelastrasz the Corrupt",
 
-	loc = GetLocale() == "deDE" and 
-	{
+	loc = GetLocale() == "koKR" and {
+		bossname = "타락한 밸라스트라즈",
+		disabletrigger = "밸라스트라즈|1이;가; 죽었습니다.",
+
+		trigger1 = "(.*)불타는 아드레날린에 걸렸습니다.",
+
+		whopattern = "(.+)|1이;가; ", 
+		you = "",		
+
+		warn1 = "당신은 불타는 아드레날린에 걸렸습니다!",
+		warn2 = "님이 불타는 아드레날린에 걸렸습니다!",
+		bosskill = "타락의 벨라스트라즈를 물리쳤습니다!",		
+	} or GetLocale() == "deDE" and {
 		bossname = "Vaelastrasz der Verdorbene",
 		disabletrigger = "Vaelastrasz der Verdorbene stirbt.",
 
-		trigger1 = "^([^%s]+) ([^%s]+) von Brennendes Adrenalin betroffen",
+		trigger1 = "^(.*)von Brennendes Adrenalin betroffen",
 
-		you = "Ihr",
+		you = "Ihr seid ",
 		are = "seid",
+		whopattern = "([^%s]+) ([^%s]+) ",
 
 		warn1 = "Du brennst!",
 		warn2 = " brennt!",
@@ -24,10 +38,11 @@ BigWigsVaelastrasz = AceAddon:new({
 		bossname = "Vaelastrasz the Corrupt",
 		disabletrigger = "Vaelastrasz the Corrupt dies.",
 
-		trigger1 = "^([^%s]+) ([^%s]+) afflicted by Burning Adrenaline",
+		trigger1 = "^(.*)afflicted by Burning Adrenaline",
 
-		you = "You",
+		you = "You are ",
 		are = "are",
+		whopattern = "([^%s]+) ([^%s]+) ", 
 
 		warn1 = "You are burning!",
 		warn2 = " is burning!",
@@ -60,6 +75,7 @@ function BigWigsVaelastrasz:CHAT_MSG_COMBAT_HOSTILE_DEATH()
 	end
 end
 
+--[[
 function BigWigsVaelastrasz:Event()
 	local _,_, EPlayer, EType = string.find(arg1, self.loc.trigger1)
 	if (EPlayer and EType) then
@@ -68,6 +84,19 @@ function BigWigsVaelastrasz:Event()
 			self:TriggerEvent("BIGWIGS_MESSAGE", self.loc.warn1, "Red", true)
 		else
 			self:TriggerEvent("BIGWIGS_MESSAGE", EPlayer .. self.loc.warn2, "Yellow")
+		end
+	end
+end]] 
+
+function BigWigsVaelastrasz:Event()
+	local _,_, EPlayer = string.find(arg1, self.loc.trigger1)
+	if (EPlayer) then
+		if (EPlayer == self.loc.you) then
+			self:TriggerEvent("BIGWIGS_MESSAGE", self.loc.warn1, "Red", true)
+			self:TriggerEvent("BIGWIGS_MESSAGE", self.loc.warn1, "Red", true)
+		else
+			local _,_, EWho = string.find(EPlayer, self.loc.whopattern)
+			self:TriggerEvent("BIGWIGS_MESSAGE", EWho .. self.loc.warn2, "Yellow")
 		end
 	end
 end
