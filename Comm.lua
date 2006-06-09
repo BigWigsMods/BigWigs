14e34b75e142a86812cc8075bf028bc45dc2f615
tekkub <git@tekkub.net>
2006-06-09 19:58:38 +0000

    BigWigs - Added throttle code to the Comm module.  To register a message for throttling fire off a "BIGWIGS_SYNC_THROTTLE" event with args msg, time.  Time is in seconds.  If time is nil the throttle is erased.  This will supress repeat events from firing for x seconds after the first one does.

--- Comm.lua
+++ Comm.lua
@@ -1,4 +1,6 @@
 
+local throt, times = {}, {}
+
 
 BigWigsComm = AceAddon:new({
 	name          = "BigWigsComm",
@@ -10,6 +12,7 @@ function BigWigsComm:Enable()
 	if oRA_Core then oRA_Core:AddCheck("BIGWIGS_SYNC_RECV","BIGWIGSSYNC") end
 	self:RegisterEvent("BIGWIGS_SYNC_RECV")
 	self:RegisterEvent("BIGWIGS_SYNC_SEND")
+	self:RegisterEvent("BIGWIGS_SYNC_THROTTLE")
 end
 
 
@@ -29,14 +32,25 @@ function BigWigsComm:BIGWIGS_SYNC_RECV(a1, a2, a3, a4, a5, a6, a7, a8 )
 
 	if not msg then return end
 
-	local _, _,sync,rest  = string.find(msg, "^(%S+)%s*(.*)$")
+	local _, _, sync, rest = string.find(msg, "^(%S+)%s*(.*)$")
 
-	self:TriggerEvent("BIGWIGS_SYNC_"..sync, rest )
+	if not throt[msg] or not times[msg] or (times[msg] + throt[msg]) <= GetTime() then
+		self:TriggerEvent("BIGWIGS_SYNC_"..sync, rest, nick)
+		times[msg] = GetTime()
+	end
 end
 
 
 function BigWigsComm:BIGWIGS_SYNC_SEND(msg)
 	if oRA_Core then oRA_Core:Send("BIGWIGSSYNC " .. msg) end
+end
+
+
+function BigWigsComm:BIGWIGS_SYNC_THROTTLE(msg, time)
+	assert(msg, "No message passed")
+	assert(time, "No time passed")
+
+	throt[msg] = time
 end
 
 
