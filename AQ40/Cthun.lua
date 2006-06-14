5a0976c78be7a6f8a0b6976826f0626c72d0da46
Wouter Commandeur <wouter@ctlaltdel.nl>
2006-06-14 09:52:32 +0000

    Bigwigs: * Added phase 2 tentacle timers for CThun * This is based off the start Kenubisan made with them, I cleaned it up some.

--- AQ40/Cthun.lua
+++ AQ40/Cthun.lua
@@ -31,6 +31,8 @@
 
 			eyebeam		= "Eye Beam",
 			cthun		= "C'Thun",
+
+			vomitwarning	= "Digestive Acid Stacked to 5 - Get Out!",
 	} or {
 			bossname 	= "Eye of C'Thun",
 			cthun		= "C'Thun",
@@ -57,6 +59,12 @@
 
 			eyebeam		= "Eye Beam",
 	},
+
+	timeP1Tentacle	= 44,
+	timeP1Glare	= 44,
+	
+	timeP2Start     = 42,
+	timeP2Tentacle  = 29,
 })
 
 function BigWigsCThun:Initialize()
@@ -78,12 +86,28 @@ function BigWigsCThun:CHAT_MSG_MONSTER_EMOTE()
 	if( arg1 == self.loc.weakendtrigger ) then
 		self:TriggerEvent("BIGWIGS_MESSAGE", self.loc.weakend, "red")
 		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barWeakend, 45, 3, "Red", "Interface\\Icons\\INV_ValentinesCandy")
+
+		-- cancel tentacle timer
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_CANCEL", self.loc.tentacle1)
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_CANCEL", self.loc.tentacle2)
+		self:TriggerEvent("BIGWIGS_BAR_CANCEL", self.loc.barTentacle)
+
+		-- Reschedule Tentacle Spawn Timers 
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle1, self.timeP2Tentacle+48, "Red")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle2, self.timeP2Tentacle+48-5, "Orange")
+		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barTentacle, self.timeP2Tentacle+48, 1, "Green", "Interface\\Icons\\Spell_Nature_CallStorm")
 	end
 end
 
 function BigWigsCThun:CHAT_MSG_COMBAT_HOSTILE_DEATH()
 		if(arg1 == self.loc.phase1) then
 			self.phase2 = 1
+
+			-- schedule first rape into phase 2
+			self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle1, self.timeP2Start, "Red")
+			self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle2, self.timeP2Start-5, "Orange")
+			self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barTentacle, self.loc.timeP2Start, 1, "Green", "Interface\\Icons\\Spell_Nature_CallStorm")
+
 		elseif( arg1 == self.loc.disabletrigger) then
 			self:Disable()
 		end
@@ -116,14 +140,14 @@ function BigWigsCThun:CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE()
 	if arg1 and string.find(arg1, self.loc.eyebeam) then
 		self:TriggerEvent("BIGWIGS_MESSAGE", self.loc.combat, "Yellow")
 		
-		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barTentacle, 44, 1, "Green", "Interface\\Icons\\Spell_Nature_CallStorm")
-		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barGlare, 44, 2, "Red", "Interface\\Icons\\Spell_Shadow_ShadowBolt")
+		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barTentacle, self.timeP1Glare, 1, "Green", "Interface\\Icons\\Spell_Nature_CallStorm")
+		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barGlare, self.timeP1Tentacle, 2, "Red", "Interface\\Icons\\Spell_Shadow_ShadowBolt")
 		
-		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.glare2, 39, "Orange")
-		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle2, 39, "Orange")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.glare2, self.timeP1Glare - 5, "Orange")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle2, self.timeP1Tentacle - 5, "Orange")
 		
-		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.glare1, 44, "Red")
-		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle1, 44, "Red")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.glare1, self.timeP1Glare, "Red")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle1, self.timeP1Tentacle, "Red")
 		
 		self:UnregisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE")
 		self:UnregisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE")
@@ -133,19 +157,28 @@ end
 function BigWigsCThun:BIGWIGS_MESSAGE(text)
 	if (text == self.loc.glare1 and not self.phase2 ) then
 		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.glare1, 87, "Red")
-		
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.glare2, 82, "Orange")
 		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barGlare, 87, 2, "Red", "Interface\\Icons\\Spell_Shadow_ShadowBolt")
 	elseif (text == self.loc.tentacle1 and not self.phase2 ) then
-		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle1, 45, "Red")
-		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle2, 40, "Orange")
-	
-		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barTentacle, 45, 1, "Green", "Interface\\Icons\\Spell_Nature_CallStorm")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle1, self.timeP1Tentacle, "Red")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle2, self.timeP1Tentacle-5, "Orange")
+		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barTentacle, self.timeP1Tentacle, 1, "Green", "Interface\\Icons\\Spell_Nature_CallStorm")
+	elseif (text == self.loc.tentacle1 and  self.phase2) then
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle1, self.timeP2Tentacle, "Red")
+		self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_START", self.loc.tentacle2, self.timeP2Tentacle-5, "Orange")
+		self:TriggerEvent("BIGWIGS_BAR_START", self.loc.barTentacle, self.timeP2Tentacle, 1, "Green", "Interface\\Icons\\Spell_Nature_CallStorm")
 	end
 end
 
 function BigWigsCThun:Disable()
 	self.disabled = true
 	self.phase2 = nil
+	self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_CANCEL", self.loc.tentacle1)
+	self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_CANCEL", self.loc.tentacle2)
+	self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_CANCEL", self.loc.glare1)
+	self:TriggerEvent("BIGWIGS_DELAYEDMESSAGE_CANCEL", self.loc.glare2)
+	self:TriggerEvent("BIGWIGS_BAR_CANCEL", self.loc.barGlare)
+	self:TriggerEvent("BIGWIGS_BAR_CANCEL", self.loc.barTentacle)
 	self:UnregisterAllEvents()
 end
 
