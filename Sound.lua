8be8821f34a5ab9ab0fe552e20b3ccf4656a611e
Wouter Commandeur <wouter@ctlaltdel.nl>
2006-06-19 10:57:20 +0000

    BigWigs: * Moved all mp3's into the Sounds/ folder. And adjusted Sound.lua to look for them there   looks much cleaner now.

--- Sound.lua
+++ Sound.lua
@@ -12,12 +12,12 @@ local cmdopt = {
 	},
 }
 local sounds = {
-	Long = "Interface\\AddOns\\BigWigs\\Long.mp3",
-	Info = "Interface\\AddOns\\BigWigs\\Info.mp3",
-	Msg = "Interface\\AddOns\\BigWigs\\Msg.mp3",
-	Alert = "Interface\\AddOns\\BigWigs\\Alert.mp3",
-	Alarm = "Interface\\AddOns\\BigWigs\\Alarm.mp3",
-	Victory = "Interface\\AddOns\\BigWigs\\VictoryShort.mp3",
+	Long = "Interface\\AddOns\\BigWigs\\Sounds\\Long.mp3",
+	Info = "Interface\\AddOns\\BigWigs\\Sounds\\Info.mp3",
+	Msg = "Interface\\AddOns\\BigWigs\\Sounds\\Msg.mp3",
+	Alert = "Interface\\AddOns\\BigWigs\\Sounds\\Alert.mp3",
+	Alarm = "Interface\\AddOns\\BigWigs\\Sounds\\Alarm.mp3",
+	Victory = "Interface\\AddOns\\BigWigs\\Sounds\\VictoryShort.mp3",
 }
 
 
