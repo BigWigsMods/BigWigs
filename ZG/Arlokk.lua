18d95cae405bc9d82cec63fe98319ac3029dc5aa
tekkub <git@tekkub.net>
2006-06-23 00:36:13 +0000

    BigWigs - Added BabbleLib Boss module, module authors please switch to using this for your localized boss names - Added Arlokk ZG module, this uses thi Babble boss library if you need an example.

---
+++ ZG/Arlokk.lua
@@ -0,0 +1,68 @@
+﻿
+local bboss = BabbleLib:GetInstance("Boss 1.2")
+
+
+BigWigsMandokir = AceAddon:new({
+	name          = "BigWigsMandokir",
+	cmd           = AceChatCmd:new({}, {}),
+
+	zonename = "ZG",
+	enabletrigger = bboss("High Priestess Arlokk"),
+
+	loc = {
+		disabletrigger = "High Priestess Arlokk dies.",
+
+		trigger1 = "Feast on ([^%s]+), my pretties!$",
+
+		warn1 = "You are marked!",
+		warn2 = "%s is marked!",
+		bosskill = "High Priestess Arlokk has been defeated!",
+	},
+})
+
+
+function BigWigsMandokir:Initialize()
+	self.loc.bossname = bboss("High Priestess Arlokk")
+	self.disabled = true
+	BigWigs:RegisterModule(self)
+end
+
+
+function BigWigsMandokir:Enable()
+	self.disabled = nil
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH")
+end
+
+
+function BigWigsMandokir:Disable()
+	self.disabled = true
+	self:UnregisterAllEvents()
+end
+
+
+function BigWigsMandokir:CHAT_MSG_COMBAT_HOSTILE_DEATH()
+	if arg1 == self.loc.disabletrigger then
+		self:TriggerEvent("BIGWIGS_MESSAGE", self.loc.bosskill, "Green", nil, "Victory")
+		self:Disable()
+	end
+end
+
+
+function BigWigsMandokir:CHAT_MSG_MONSTER_YELL()
+	local _,_, n = string.find(arg1, self.loc.trigger1)
+	if n then
+		if n == UnitName("player") then
+			self:TriggerEvent("BIGWIGS_MESSAGE", self.loc.warn1, "Red", true, "Alarm")
+		else
+			self:TriggerEvent("BIGWIGS_MESSAGE", string.format(self.loc.warn2, n), "Yellow")
+			self:TriggerEvent("BIGWIGS_SENDTELL", n, self.loc.warn1)
+		end
+	end
+end
+
+
+--------------------------------
+--			Load this bitch!			--
+--------------------------------
+BigWigsMandokir:RegisterForLoad()
