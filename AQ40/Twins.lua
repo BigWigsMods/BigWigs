6d61b9ee958012973d066894567a4cc93ad17235
Wouter Commandeur <wouter@ctlaltdel.nl>
2006-08-03 11:25:02 +0000

    BigWigs: * Twins, small change to Scan function

--- AQ40/Twins.lua
+++ AQ40/Twins.lua
@@ -152,14 +152,14 @@ function BigWigsTwins:PLAYER_REGEN_ENABLED()
 end
 
 function BigWigsTwins:Scan()
-	if UnitName("target") == (veklor or veknilash) and UnitAffectingCombat("target") then
+	if (UnitName("target") == (veklor or veknilash) and UnitAffectingCombat("target")) then
 		return true
-	elseif UnitName("playertarget") == (veklor or veknilash) and UnitAffectingCombat("playertarget") then
+	elseif (UnitName("playertarget") == (veklor or veknilash) and UnitAffectingCombat("playertarget")) then
 		return true
 	else
 		local i
 		for i = 1, GetNumRaidMembers(), 1 do
-			if UnitName("Raid"..i.."target") == (veklor or veknilash) and UnitAffectingCombat("Raid"..i.."target") then
+			if (UnitName("Raid"..i.."target") == (veklor or veknilash) and UnitAffectingCombat("Raid"..i.."target")) then
 				return true
 			end
 		end
