08df0853bb7ef09e2adb15769571240ae042de60
tekkub <git@tekkub.net>
2006-08-08 23:54:36 +0000

    BigWigs - Tweaked sync-enabling, again, expect big painful errors as I can't test shit (why can't you people speak common?)

--- Core.lua
+++ Core.lua
@@ -261,12 +261,12 @@ function BigWigs:RegisterModule(name,module)
 end
 
 
-function BigWigs:EnableModule(module)
+function BigWigs:EnableModule(module, nosync)
 	local m = self:GetModule(module)
 	if m and m:IsBossModule() and not self:IsModuleActive(module) then
 		self:ToggleModuleActive(module, true)
 		self:TriggerEvent("BigWigs_Message", string.format(L"%s mod enabled", m:ToString() or "??"), "Cyan", true)
-		self:TriggerEvent("BigWigs_SendSync", "EnableModule " .. (BB:HasReverseTranslation(module) == true and BB:GetReverseTranslation(module) or module))
+		if not nosync then self:TriggerEvent("BigWigs_SendSync", "EnableModule " .. BB:GetReverseTranslation(module)) end
 	end
 end
 
@@ -279,8 +279,9 @@ end
 
 function BigWigs:BigWigs_RecvSync(sync, module)
 	if sync == "EnableModule" and module then
-		local m = self:GetModule(BB:HasTranslation(module) == true and BB:GetTranslation(module) or module)
-		if m.zonename == GetRealZoneText() then self:EnableModule(BB:HasTranslation(module) == true and BB:GetTranslation(module) or module) end
+		local name = BB(module)
+		local m = self:GetModule(name)
+		if m.zonename == GetRealZoneText() then self:EnableModule(name, true) end
 	end
 end
 
