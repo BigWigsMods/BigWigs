31a3465af4d4e3e2e58399564ef7503163d15df2
Wouter Commandeur <wouter@ctlaltdel.nl>
2006-08-09 16:42:21 +0000

    BigWigs (Fankriss): Sync back for worms but in an improved form.

--- AQ40/Fankriss.lua
+++ AQ40/Fankriss.lua
@@ -16,8 +16,8 @@ L:RegisterTranslations("enUS", function() return {
 	worm_desc = "Warn for Incoming Worms",
 
 	wormtrigger = "Fankriss the Unyielding casts Summon Worm.",
-	wormwarn = "Incoming Worm!",
-	wormbar = "Sandworm Enrage",
+	wormwarn = "Incoming Worm! (%d)",
+	wormbar = "Sandworm Enrage (%d)",
 } end )
 
 L:RegisterTranslations("zhCN", function() return {
@@ -25,12 +25,12 @@ L:RegisterTranslations("zhCN", function() return {
 	worm_desc = "召唤虫子出现时发出警报",
 	
 	wormtrigger = "顽强的范克瑞斯施放了召唤虫子。",
-	wormwarn = "虫子出现 - 赶快杀掉！",
+	wormwarn = "虫子出现 - 赶快杀掉！ (%d)",
 } end )
 
 L:RegisterTranslations("koKR", function() return {
 	wormtrigger = "불굴의 판크리스|1이;가; 벌레 소환|1을;를; 시전합니다.",
-	wormwarn = "벌레 소환 - 제거!",
+	wormwarn = "벌레 소환 - 제거! (%d)",
 } end )
 
 ----------------------------------
@@ -48,8 +48,12 @@ BigWigsFankriss.revision = tonumber(string.sub("$Revision$", 12, -3))
 ------------------------------
 
 function BigWigsFankriss:OnEnable()
+	self.worms = 0
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "FankrissWormSpawn", .1)
 end
 
 ------------------------------
@@ -57,8 +61,23 @@ end
 ------------------------------
 
 function BigWigsFankriss:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF(msg)
-	if arg1 == L"wormtrigger" and self.db.profile.worm then
-		self:TriggerEvent("BigWigs_Message", L"wormwarn", "Orange")
-		self:TriggerEvent("BigWigs_StartBar", self, L"wormbar", 20, "Interface\\Icons\\Spell_Shadow_UnholyFrenzy", "Yellow", "Orange", "Red")
+	if arg1 == L"wormtrigger" then
+		self:TriggerEvent("BigWigs_SendSync", "FankrissWormSpawn", self.worms + 1 )
+	end
+end
+
+function BigWigsFankriss:BigWigs_RecvSync(sync, rest)
+	if sync ~= "FankrissWormSpawn" then return end
+	if not rest then return end
+	rest = tonumber(rest)
+	if rest == (self.worms + 1) then
+		-- we accept this worm
+		-- Yes, this could go completely wrong when you don't reset your module and the whole raid does after a wipe
+		-- or you reset your module and the rest doesn't. Anyway. it'll work a lot better than anything else.
+		self.worms = self.worms + 1
+		if self.db.profile.worm then
+			self:TriggerEvent("BigWigs_Message", string.format( L"wormwarn", self.worms ), "Orange")
+			self:TriggerEvent("BigWigs_StartBar", self, string.format( L"wormbar", self.worms ), 20, "Interface\\Icons\\Spell_Shadow_UnholyFrenzy", "Yellow", "Orange", "Red")
+		end	
 	end
 end
