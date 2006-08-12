1d548a8d46d8e2c694ce132e0ab5e249e700c05d
hk2717 <hk2717@users.noreply.wowace.com>
2006-08-12 07:18:21 +0000

    BigWigs: A lot of update to zhCN localizaition.

--- BossBlock.lua
+++ BossBlock.lua
@@ -1,4 +1,8 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
 
+local L = AceLibrary("AceLocale-2.0"):new("BigWigsBossBlock")
 
 local map = {[true] = "|cffff0000".."Supressed".."|r", [false] = "|cff00ff00".."Shown".."|r"}
 local raidchans = {
@@ -20,6 +24,67 @@ local blockregexs = {
 	"%*+ .+ %*+$",
 }
 
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	["BossBlock"] = true,
+	["Supress bossmod chat from other players."] = true,
+	
+	["Supress Raid Chat"] = true,
+	["Supress messages in the raid channel."] = true,
+	
+	["Supress RaidWarn Chat"] = true,
+	["Supress RaidWarn messages in the chat frames."] = true,
+	
+	["Supress RaidWarn"] = true,
+	["Supress RaidWarn popup messages."] = true,
+	
+	["Supress RaidSay"] = true,
+	["Supress CTRA RaidSay popup messages."] = true,
+	
+	["Supress Tells"] = true,
+	["Supress Tell messages."] = true,
+	
+	["Debugging"] = true,
+	["Show debug messages."] = true,
+	
+	["Supressing Chatframe"] = true,
+	["Supressing RaidWarningFrame"] = true,
+	["Supressing CT_RAMessageFrame"] = true,
+} end)
+
+L:RegisterTranslations("zhCN", function() return {
+	["BossBlock"] = "信息阻挡",
+	["Supress bossmod chat from other players."] = "阻挡其他玩家的首领插件发送的信息。",
+	
+	["Supress Raid Chat"] = "阻挡团队频道",
+	["Supress messages in the raid channel."] = "阻挡团队频道中的信息",
+	
+	["Supress RaidWarn Chat"] = "阻挡团队警告聊天",
+	["Supress RaidWarn messages in the chat frames."] = "阻挡聊天窗体中的团队警告信息",
+	
+	["Supress RaidWarn"] = "阻挡团队警告",
+	["Supress RaidWarn popup messages."] = "阻挡团队警告中的信息",
+	
+	["Supress RaidSay"] = "阻挡RS",
+	["Supress CTRA RaidSay popup messages."] = "阻挡团队助手(CTRA)的RS信息",
+	
+	["Supress Tells"] = "阻挡密语",
+	["Supress Tell messages."] = "阻挡密语中的信息",
+	
+	["Debugging"] = "除错",
+	["Show debug messages."] = "显示除错信息",
+	
+	["Supressing Chatframe"] = "正在阻挡Chatframe",
+	["Supressing RaidWarningFrame"] = "正在阻挡RaidWarningFrame",
+	["Supressing CT_RAMessageFrame"] = "正在阻挡CT_RAMessageFrame",
+} end)
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
 
 BigWigsBossBlock = BigWigs:NewModule("BossBlock", "AceHook-2.0")
 BigWigsBossBlock.defaultDB = {
@@ -32,14 +97,14 @@ BigWigsBossBlock.defaultDB = {
 BigWigsBossBlock.consoleCmd = "block"
 BigWigsBossBlock.consoleOptions = {
 	type = "group",
-	name = "BossBlock",
-	desc = "Supress bossmod chat from other players.",
+	name = L"BossBlock",
+	desc = L"Supress bossmod chat from other players.",
 	args   = {
 		["chat"] = {
 			type = "toggle",
 			order = 1,
-			name = "Supress Raid Chat",
-			desc = "Supress messages in the raid channel.",
+			name = L"Supress Raid Chat",
+			desc = L"Supress messages in the raid channel.",
 			get = function() return BigWigsBossBlock.db.profile.hideraidchat end,
 			set = function(v) BigWigsBossBlock.db.profile.hideraidchat = v end,
 			map = map,
@@ -47,8 +112,8 @@ BigWigsBossBlock.consoleOptions = {
 		["rwchat"] = {
 			type = "toggle",
 			order = 1,
-			name = "Supress RaidWarn Chat",
-			desc = "Supress RaidWarn messages in the chat frames.",
+			name = L"Supress RaidWarn Chat",
+			desc = L"Supress RaidWarn messages in the chat frames.",
 			get = function() return BigWigsBossBlock.db.profile.hideraidwarnchat end,
 			set = function(v) BigWigsBossBlock.db.profile.hideraidwarnchat = v end,
 			map = map,
@@ -56,8 +121,8 @@ BigWigsBossBlock.consoleOptions = {
 		["rw"] = {
 			type = "toggle",
 			order = 1,
-			name = "Supress RaidWarn",
-			desc = "Supress RaidWarn popup messages.",
+			name = L"Supress RaidWarn",
+			desc = L"Supress RaidWarn popup messages.",
 			get = function() return BigWigsBossBlock.db.profile.hideraidwarn end,
 			set = function(v) BigWigsBossBlock.db.profile.hideraidwarn = v end,
 			map = map,
@@ -65,8 +130,8 @@ BigWigsBossBlock.consoleOptions = {
 		["rs"] = {
 			type = "toggle",
 			order = 1,
-			name = "Supress RaidSay",
-			desc = "Supress CTRA RaidSay popup messages.",
+			name = L"Supress RaidSay",
+			desc = L"Supress CTRA RaidSay popup messages.",
 			get = function() return BigWigsBossBlock.db.profile.hideraidsay end,
 			set = function(v) BigWigsBossBlock.db.profile.hideraidsay = v end,
 			map = map,
@@ -75,8 +140,8 @@ BigWigsBossBlock.consoleOptions = {
 		["tell"] = {
 			type = "toggle",
 			order = 1,
-			name = "Supress Tells",
-			desc = "Supress Tell messages.",
+			name = L"Supress Tells",
+			desc = L"Supress Tell messages.",
 			get = function() return BigWigsBossBlock.db.profile.hidetells end,
 			set = function(v) BigWigsBossBlock.db.profile.hidetells = v end,
 			map = map,
@@ -88,8 +153,8 @@ BigWigsBossBlock.consoleOptions = {
 		},
 		["debug"] = {
 			type = "toggle",
-			name = "Debugging",
-			desc = "Show debug messages.",
+			name = L"Debugging",
+			desc = L"Show debug messages.",
 			order = 100,
 			get = function() return BigWigsBossBlock:IsDebugging() end,
 			set = function(v) BigWigsBossBlock:SetDebugging(v) end,
@@ -98,6 +163,9 @@ BigWigsBossBlock.consoleOptions = {
 	},
 }
 
+------------------------------
+--      Event Handlers      --
+------------------------------
 
 function BigWigsBossBlock:OnEnable()
 	self:Hook("ChatFrame_OnEvent")
@@ -108,7 +176,7 @@ end
 
 function BigWigsBossBlock:ChatFrame_OnEvent(event)
 	if self:IsChannelSupressed(event) and self:IsSpam(arg1) then
-		self:Debug("Supressing Chatframe", event, arg1)
+		self:Debug(L"Supressing Chatframe", event, arg1)
 		return
 	end
 	self.hooks.ChatFrame_OnEvent.orig(event)
@@ -117,7 +185,7 @@ end
 
 function BigWigsBossBlock:RaidWarningFrame_OnEvent(event, message)
 	if self.db.profile.hideraidwarn and self:IsSpam(message) then
-		self:Debug("Supressing RaidWarningFrame", message)
+		self:Debug(L"Supressing RaidWarningFrame", message)
 		return
 	end
 	self.hooks.RaidWarningFrame_OnEvent.orig(event, message)
@@ -126,7 +194,7 @@ end
 
 function BigWigsBossBlock:CTRA_AddMessage(obj, text, red, green, blue, alpha, holdTime)
 	if self.db.profile.hideraidsay and self:IsSpam(text) then
-		self:Debug("Supressing CT_RAMessageFrame", text)
+		self:Debug(L"Supressing CT_RAMessageFrame", text)
 		return
 	end
 	self.hooks[obj].AddMessage.orig(obj, text, red, green, blue, alpha, holdTime)
