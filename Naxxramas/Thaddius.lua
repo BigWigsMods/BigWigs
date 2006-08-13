3238f787cf098e05b0e54634aa59fed75456b118
Vidar Haarr <vhaarr@gmail.com>
2006-08-13 14:10:56 +0000

    BigWigs: Thaddius: Don't warn 2 times about phase 1.

--- Naxxramas/Thaddius.lua
+++ Naxxramas/Thaddius.lua
@@ -148,10 +148,9 @@ BigWigsThaddius.revision = tonumber(string.sub("$Revision$", 12, -3))
 --      Initialization      --
 ------------------------------
 
--- TODO: Add sync'ing for Stalagg's Power Surge.
-
 function BigWigsThaddius:OnEnable()
 	self.enrageStarted = nil
+	self.engaged = nil
 	self.addsdead = 0
 	self.teslawarn = nil
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
@@ -194,8 +193,9 @@ end
 function BigWigsThaddius:CHAT_MSG_MONSTER_YELL( msg )
 	if msg == L"pstrigger" then
 		self:TriggerEvent("BigWigs_SendSync", "ThaddiusPolarity")
-	elseif msg == L"starttrigger" or msg == L"starttrigger1" then
+	elseif msg == L"starttrigger" or msg == L"starttrigger1" and not self.engaged then
 		if self.db.profile.phase then self:TriggerEvent("BigWigs_Message", L"startwarn", "Red") end
+		self.engaged = true
 	elseif msg == L"starttrigger2" or msg == L"starttrigger3" or msg == L"starttrigger4" then
 		if self.db.profile.phase then self:TriggerEvent("BigWigs_Message", L"startwarn2", "Red") end
 		if self.db.profile.enrage then
