cbaafdaf3eda0f2b34e6651d04a2e697bd832a7f
Wouter Commandeur <wouter@ctlaltdel.nl>
2006-08-17 08:27:52 +0000

    BigWigs: * Now takes into account the '#' CTRA message seperator. * Closes: FS#182

--- Comm.lua
+++ Comm.lua
@@ -40,16 +40,16 @@ function BigWigsComm:CHAT_MSG_CHANNEL(msg, sender, sschan, arg4, arg5, arg6, arg
 	cleanmsg = string.gsub(cleanmsg, "§", "S")
 	if strsub(cleanmsg, strlen(cleanmsg)-7) == " ...hic!" then cleanmsg = strsub(cleanmsg, 1, strlen(cleanmsg)-8) end
 
-	local _, _, sync, rest = string.find(cleanmsg, "^BigWigsSync (%S+)%s*(.*)$")
-	if not sync then return end
-
-
-	if not throt[sync] or not times[sync] or (times[sync] + throt[sync]) <= GetTime() then
-		self:TriggerEvent("BigWigs_RecvSync", sync, rest, sender)
-		times[sync] = GetTime()
+	local msgarr = self:SplitMessage(cleanmsg, "#")
+	
+ 	for _, c in msgarr do
+		local _, _, sync, rest = string.find(c, "^BigWigsSync (%S+)%s*(.*)$")
+		if sync and not throt[sync] or not times[sync] or (times[sync] + throt[sync]) <= GetTime() then
+			self:TriggerEvent("BigWigs_RecvSync", sync, rest, sender)
+			times[sync] = GetTime()
+		end
 	end
 end
-
 
 function BigWigsComm:BigWigs_SendSync(msg)
 	if oRA_Core then oRA_Core:Send("BigWigsSync " .. msg)
@@ -65,3 +65,15 @@ function BigWigsComm:BigWigs_ThrottleSync(msg, time)
 end
 
 
+function BigWigsComm:SplitMessage( msg, char )
+	local arr = { }
+	while (string.find(msg, char) ) do
+		local iStart, iEnd = string.find(msg, char)
+		table.insert(arr, strsub(msg, 1, iStart-1))
+		msg = strsub(msg, iEnd+1, strlen(msg))
+	end
+	if ( strlen(msg) > 0 ) then
+		table.insert(arr, msg)
+	end
+	return arr
+end
