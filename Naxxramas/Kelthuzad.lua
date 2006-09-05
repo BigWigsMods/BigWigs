83d3c7699f5e22f28074b61c98eb9629168687df
Wouter Commandeur <wouter@ctlaltdel.nl>
2006-09-05 14:48:50 +0000

    BigWigs (Kelthuzad): * Start warning, 5 minute phase 1 bar * Phase 2 incoming 20 second bar * Shadow Fissure warnings

--- Naxxramas/Kelthuzad.lua
+++ Naxxramas/Kelthuzad.lua
@@ -20,11 +20,24 @@ L:RegisterTranslations("enUS", function() return {
 	mc_name = "Mind Control",
 	mc_desc = "Warn for mind control.",
 	
-	mc_trigger = "Your soul is bound to me, now!",
+	fissure_cmd = "fissure",
+	fissure_name = "Shadow Fissure",
+	fissure_desc = "Warn for Shadow Fissure",
+
+	mc_trigger1 = "Your soul is bound to me, now!",
+	mc_trigger2 = "There will be no escape!",
 	mc_warning = "Mind Control!",
 	
-    	phase2_trigger = "Minions, servants, soldiers of the cold dark! Obey the call of Kel'Thuzad!",
-	phase2_warning = "Phase 2, Kel'Thuzad active!",
+    	start_trigger = "Minions, servants, soldiers of the cold dark! Obey the call of Kel'Thuzad!",
+	start_warning = "Kel'Thuzad encounter started! ~5min till he is active!",
+	start_bar = "Phase 2",
+
+    	phase2_trigger = "Pray for mercy!",
+	phase2_warning = "Phase 2, Kel'Thuzad incoming!",
+	phase2_bar = "Kel'Thuzad Active!",
+
+	fissure_trigger = "Kel'Thuzad casts Shadow Fissure.",
+	fissure_warning = "Shadow Fissure!",
 } end )
 
 ----------------------------------
@@ -63,6 +76,7 @@ you enter room, Kel'Thuzad says bla blah blah
 
 function BigWigsKelThuzad:OnEnable()
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 end
 
@@ -71,10 +85,26 @@ end
 ------------------------------
 
 function BigWigsKelThuzad:CHAT_MSG_MONSTER_YELL(msg)
-	if msg == L["phase2_trigger"] then
-		if self.db.profile.phase2 then self:TriggerEvent("BigWigs_Message", L["phase2_warning"], "Red") end
-	elseif msg == L["mc_trigger"] then
+	if msg == L["start_trigger"] then
+		if self.db.profile.phase then 
+			self:TriggerEvent("BigWigs_Message", L["start_warning"], "Yellow")
+			self:TriggerEvent("BigWigs_StartBar", self, L["start_bar"], 300 )
+		end
+	elseif msg == L["phase2_trigger"] then
+		if self.db.profile.phase then
+			self:TriggerEvent("BigWigs_StopBar", self, L["start_bar"] )
+			self:TriggerEvent("BigWigs_Message", L["phase2_warning"], "Red")
+			self:TriggerEvent("BigWigs_StartBar", self, L["phase2_bar"], 20 )
+		end
+	elseif msg == L["mc_trigger1"] or msg == L["mc_trigger2"] then
 		if self.db.profile.mc then self:TriggerEvent("BigWigs_Message", L["mc_warning"], "Orange") end
 	end
 end
 
+function BigWigsKelThuzad:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE( msg )
+	if msg == L["fissure_trigger"] then
+		if self.db.profile.fissure then
+			self:TriggerEvent("BigWigs_Message", L["fissure_warning"], "Red")
+		end
+	end
+end
