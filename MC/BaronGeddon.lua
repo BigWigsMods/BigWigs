06bcc6b8ff4b30cedf0eb6a0a94ac66f9cc6c791
Vidar Haarr <vhaarr@gmail.com>
2006-10-07 14:18:00 +0000

    BigWigs: Geddon:  * Add warnings and timers for Inferno and Last Service.  * Add bar for when the bomb explodes on target

--- MC/BaronGeddon.lua
+++ MC/BaronGeddon.lua
@@ -10,37 +10,58 @@ local L = AceLibrary("AceLocale-2.0"):new("BigWigs"..boss)
 ----------------------------
 
 L:RegisterTranslations("enUS", function() return {
-	trigger1 = "^([^%s]+) ([^%s]+) afflicted by Living Bomb",
+	bomb_trigger = "^([^%s]+) ([^%s]+) afflicted by Living Bomb",
+	inferno_trigger = "Baron Geddon gains Inferno.",
+	service_trigger = "%s performs one last service for Ragnaros.",
 
 	you = "You",
 	are = "are",
 
-	warn1 = "You are the bomb!",
-	warn2 = " is the bomb!",
+	bomb_message_you = "You are the bomb!",
+	bomb_message_other = "%s is the bomb!",
+
+	bombtimer_bar = "%s: Living Bomb",
+	inferno_bar = "Inferno",
+	service_bar = "Last Service",
+
+	service_message = "Last Service, Geddon exploding in 5sec!",
+	inferno_message = "Inferno for 10sec!",
 
 	cmd = "Baron",
-	
+
+	service_cmd = "service",
+	service_name = "Last service",
+	service_desc = "Timer bar for Geddon's last service.",
+
+	inferno_cmd = "inferno",
+	inferno_name = "Inferno",
+	inferno_desc = "Timer bar for Geddons Inferno.",
+
+	bombtimer_cmd = "bombtimer",
+	bombtimer_name = "Bar for when the bomb goes off",
+	bombtimer_desc = "Shows a 10 second bar for when the bomb goes off at the target.",
+
 	youbomb_cmd = "youbomb",
 	youbomb_name = "You are the bomb alert",
 	youbomb_desc = "Warn when you are the bomb",
-	
+
 	elsebomb_cmd = "elsebomb",
 	elsebomb_name = "Someone else is the bomb alert",
 	elsebomb_desc = "Warn when others are the bomb",
-	
+
 	icon_cmd = "icon",
 	icon_name = "Raid Icon on bomb",
 	icon_desc = "Put a Raid Icon on the person who's the bomb. (Requires promoted or higher)",
 } end)
 
 L:RegisterTranslations("zhCN", function() return {
-	trigger1 = "^(.+)受(.+)了活化炸弹",
+	bomb_trigger = "^(.+)受(.+)了活化炸弹",
 
 	you = "你",
 	are = "到",
 
-	warn1 = "你是炸弹人！向着夕阳奔跑吧！",
-	warn2 = "是炸弹人！向着夕阳奔跑吧！",
+	bomb_message_you = "你是炸弹人！向着夕阳奔跑吧！",
+	--bomb_message_other = "是炸弹人！向着夕阳奔跑吧！",
 	
 	youbomb_name = "玩家炸弹警报",
 	youbomb_desc = "你成为炸弹时发出警报",
@@ -53,13 +74,13 @@ L:RegisterTranslations("zhCN", function() return {
 } end)
 
 L:RegisterTranslations("koKR", function() return {
-	trigger1 = "^([^|;%s]*)(.*)살아있는 폭탄에 걸렸습니다%.$",
+	bomb_trigger = "^([^|;%s]*)(.*)살아있는 폭탄에 걸렸습니다%.$",
 
 	you = "",
 	are = "",
 
-	warn1 = "당신은 폭탄입니다!",
-	warn2 = "님이 폭탄입니다!",
+	bomb_message_you = "당신은 폭탄입니다!",
+	--bomb_message_other = "님이 폭탄입니다!",
 	
 	youbomb_name = "자신의 폭탄 경고",
 	youbomb_desc = "자신이 폭탄 일때 경고",
@@ -72,13 +93,13 @@ L:RegisterTranslations("koKR", function() return {
 } end)
 
 L:RegisterTranslations("deDE", function() return {
-	trigger1 = "^([^%s]+) ([^%s]+) von Lebende Bombe betroffen",
+	bomb_trigger = "^([^%s]+) ([^%s]+) von Lebende Bombe betroffen",
 
 	you = "Ihr",
 	are = "seid",
 
-	warn1 = "Du bist die Bombe!",
-	warn2 = " ist die Bombe!",
+	bomb_message_you = "Du bist die Bombe!",
+	bomb_message_other = "%s ist die Bombe!",
 
 	youbomb_name = "Du bist die Bombe",
 	youbomb_desc = "Warnung, wenn Du die Bombe bist.",
@@ -91,13 +112,13 @@ L:RegisterTranslations("deDE", function() return {
 } end)
 
 L:RegisterTranslations("frFR", function() return {
-	trigger1 = "^([^%s]+) ([^%s]+) les effets de Bombe vivante.",
+	bomb_trigger = "^([^%s]+) ([^%s]+) les effets de Bombe vivante.",
 
 	you = "Vous",
 	are = "subissez",
 
-	warn1 = "Tu es la bombe !",
-	warn2 = " est la bombe !",
+	bomb_message_you = "Tu es la bombe !",
+	bomb_message_other = "%s est la bombe !",
 } end)
 
 ----------------------------------
@@ -107,7 +128,7 @@ L:RegisterTranslations("frFR", function() return {
 BigWigsBaronGeddon = BigWigs:NewModule(boss)
 BigWigsBaronGeddon.zonename = AceLibrary("Babble-Zone-2.0")("Molten Core")
 BigWigsBaronGeddon.enabletrigger = boss
-BigWigsBaronGeddon.toggleoptions = {"youbomb", "elsebomb", "icon", "bosskill"}
+BigWigsBaronGeddon.toggleoptions = {"inferno", "service", -1, "bombtimer", "youbomb", "elsebomb", "icon", "bosskill"}
 BigWigsBaronGeddon.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 ------------------------------
@@ -118,10 +139,13 @@ function BigWigsBaronGeddon:OnEnable()
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_BUFFS")
+	self:RegisterEvent("CHAT_MSG_MONSTER_EMOTE")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 
 	self:RegisterEvent("BigWigs_RecvSync")
-	self:TriggerEvent( "BigWigs_ThrottleSync", "GeddonBomb", 1)
+	self:TriggerEvent("BigWigs_ThrottleSync", "GeddonBomb", 1)
+	self:TriggerEvent("BigWigs_ThrottleSync", "GeddonInferno", 5)
 end
 
 ------------------------------
@@ -129,8 +153,8 @@ end
 ------------------------------
 
 function BigWigsBaronGeddon:Event(msg)
-	local _, _, EPlayer, EType = string.find(msg, L["trigger1"])
-	if (EPlayer and EType) then
+	local _, _, EPlayer, EType = string.find(msg, L["bomb_trigger"])
+	if EPlayer and EType then
 		if EPlayer == L["you"] and EType == L["are"] then
 			EPlayer = UnitName("player")
 		end
@@ -138,20 +162,41 @@ function BigWigsBaronGeddon:Event(msg)
 	end
 end
 
-function BigWigsBaronGeddon:BigWigs_RecvSync(sync, rest, nick)
-	if sync ~= "GeddonBomb" or not rest then return end
-	local player = rest
-	
-	if player == UnitName("player") and self.db.profile.youbomb then
-		self:TriggerEvent("BigWigs_Message", L["warn1"], "Personal", true)
-		self:TriggerEvent("BigWigs_Message", player .. L["warn2"], "Attention", nil, nil, true )
-	elseif (self.db.profile.elsebomb) then
-		self:TriggerEvent("BigWigs_Message", player .. L["warn2"], "Attention")
-		self:TriggerEvent("BigWigs_SendTell", player, L["warn1"])
+function BigWigsBaronGeddon:CHAT_MSG_SPELL_PERIODIC_CREATURE_BUFFS(msg)
+	if msg == L["inferno_trigger"] then
+		self:TriggerEvent("BigWigs_SendSync", "GeddonInferno")
 	end
+end
 
-	if self.db.profile.icon then
-		self:TriggerEvent("BigWigs_SetRaidIcon", player )
+function BigWigsBaronGeddon:CHAT_MSG_MONSTER_EMOTE(msg)
+	if msg == L["service_trigger"] and self.db.profile.service then
+		self:TriggerEvent("BigWigs_StartBar", self, L["service_bar"], 5, "Interface\\Icons\\Spell_Shadow_MindBomb", "Red")
+		self:TriggerEvent("BigWigs_Message", L["service_message"], "Important")
+	end
+end
+
+function BigWigsBaronGeddon:BigWigs_RecvSync(sync, rest, nick)
+	if sync == "GeddonBomb" and rest then
+		local player = rest
+		
+		if player == UnitName("player") and self.db.profile.youbomb then
+			self:TriggerEvent("BigWigs_Message", L["bomb_message_you"], "Personal", true)
+			self:TriggerEvent("BigWigs_Message", string.format(L["bomb_message_other"], player), "Attention", nil, nil, true )
+		elseif self.db.profile.elsebomb then
+			self:TriggerEvent("BigWigs_Message", string.format(L["bomb_message_other"], player), "Attention")
+			self:TriggerEvent("BigWigs_SendTell", player, L["bomb_message_you"])
+		end
+
+		if self.db.profile.bombtimer then
+			self:TriggerEvent("BigWigs_StartBar", self, string.format(L["bombtimer_bar"], player), 10, "Interface\\Icons\\Spell_Shadow_MindBomb", "Red")
+		end
+
+		if self.db.profile.icon then
+			self:TriggerEvent("BigWigs_SetRaidIcon", player)
+		end
+	elseif sync == "GeddonInferno" and self.db.profile.inferno then
+		self:TriggerEvent("BigWigs_StartBar", self, L["inferno_bar"], 10, "Interface\\Icons\\Spell_Fire_SealOfFire", "Orange")
+		self:TriggerEvent("BigWigs_Message", L["inferno_message"], "Important")
 	end
 end
 
