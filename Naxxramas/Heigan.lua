7a5d111e3667a5813c2cbef1375da33b19a68e2c
Vidar Haarr <vhaarr@gmail.com>
2006-10-13 03:24:45 +0000

    BigWigs: Heigan: String cleanups; added Eye Stalk and Rotting Maggot to the wipe mobs list, so we don't disable the mod if you have any of those targetted.

--- Naxxramas/Heigan.lua
+++ Naxxramas/Heigan.lua
@@ -24,20 +24,28 @@ L:RegisterTranslations("enUS", function() return {
 	starttrigger = "You are mine now.",
 	starttrigger2 = "You... are next.",
 	starttrigger3 = "I see you...",
-	teleporttrigger = "The end is upon you.",
-	dietrigger = "%s takes his last breath.",
+	teleport_trigger = "The end is upon you.",
+	die_trigger = "%s takes his last breath.",
+
 	-- [[ Warnings ]]--
-	startwarn = "Heigan the Unclean engaged! 90 seconds till teleport",
-	warn1 = "Teleport in 1 minute",
-	warn2 = "Teleport in 30 seconds",
-	warn3 = "Teleport in 10 seconds",
-	backwarn = "He's back on the floor! 90 seconds till next teleport",
-	teleportwarn2 = "Inc to floor in 30 seconds",
-	teleportwarn3 = "Inc to floor in 10 seconds",
-	teleportwarn1 = "Teleport! %d sec till back in room!",
+	engage_message = "Heigan the Unclean engaged! 90 sec to teleport!",
+
+	teleport_1min_message = "Teleport in 1 min",
+	teleport_30sec_message = "Teleport in 30 sec",
+	teleport_10sec_message = "Teleport in 10 sec!",
+	on_platform_message = "Teleport! On platform for %d sec!",
+
+	to_floor_30sec_message = "Back in 30 sec",
+	to_floor_10sec_message = "Back in 10 sec!",
+	on_floor_message = "Back on the floor! 90 sec to next teleport!",
+
 	-- [[ Bars ]]--
-	teleportbar = "Teleport!",
-	backbar = "Back on floor!",
+	teleport_bar = "Teleport!",
+	back_bar = "Back on the floor!",
+
+	-- [[ Dream Room Mobs ]] --
+	["Eye Stalk"] = true,
+	["Rotting Maggot"] = true,
 } end )
 
 -- Korean Translation by gezta --
@@ -53,20 +61,20 @@ L:RegisterTranslations("koKR", function() return {
 	starttrigger = "이제 넌 내 것이다.",
 	starttrigger2 = "다음은... 너다.",
 	starttrigger3 = "네가 보인다...",
-	teleporttrigger = "여기가 너희 무덤이 되리라.",
-	dietrigger = "%s|%1이;가; 마지막 숨결을 얻었습니다.", -- "%s takes his last breath.", -- check
+	teleport_trigger = "여기가 너희 무덤이 되리라.",
+	die_trigger = "%s|%1이;가; 마지막 숨결을 얻었습니다.", -- "%s takes his last breath.", -- check
 	-- [[ Warnings ]]--
-	startwarn = "부정의 헤이건, 단상으로 순간 이동까지 90초",
-	warn1 = "순간이동 1 분전 !!!",
-	warn2 = "순간이동 30초전 !!!",
-	warn3 = "순간이동 10초전 !!!",
-	backwarn = "헤이건 단상 아래!! 90초 후에 순간이동 !!!",
-	teleportwarn2 = "내려오기까지 30초 전 !!!",
-	teleportwarn3 = "내려오기까지 10초 전 !!!",
-	teleportwarn1 = "순간이동! %d초 후 내려옵니다. !!!",
+	engage_message = "부정의 헤이건, 단상으로 순간 이동까지 90초",
+	teleport_1min_message = "순간이동 1 분전 !!!",
+	teleport_30sec_message = "순간이동 30초전 !!!",
+	teleport_10sec_message = "순간이동 10초전 !!!",
+	on_floor_message = "헤이건 단상 아래!! 90초 후에 순간이동 !!!",
+	to_floor_30sec_message = "내려오기까지 30초 전 !!!",
+	to_floor_10sec_message = "내려오기까지 10초 전 !!!",
+	on_platform_message = "순간이동! %d초 후 내려옵니다. !!!",
 	-- [[ Bars ]]--
-	teleportbar = "순간이동!",
-	backbar = "단상으로 이동!",
+	teleport_bar = "순간이동!",
+	back_bar = "단상으로 이동!",
 } end )
 
 L:RegisterTranslations("deDE", function() return {
@@ -76,19 +84,19 @@ L:RegisterTranslations("deDE", function() return {
 	starttrigger = "Ihr geh\195\182rt mir...",
 	starttrigger2 = "Ihr seid.... als n\195\164chstes dran.",
 	starttrigger3 = "Ihr entgeht mir nicht...",
-	teleporttrigger = "Euer Ende naht.",
+	teleport_trigger = "Euer Ende naht.",
 
-	startwarn = "Heigan der Unsaubere angegriffen! 90 Sekunden bis Teleport!",
-	warn1 = "Teleport in 1 Minute",
-	warn2 = "Teleport in 30 Sekunden",
-	warn3 = "Teleport in 10 Sekunden",
-	backwarn = "Zur\195\188ck im Raum! N\195\164chster Teleport in 90 Sekunden!",
-	teleportwarn2 = "Zur\195\188ck im Raum in 30 Sekunden",
-	teleportwarn3 = "Zur\195\188ck im Raum in 10 Sekunden",
-	teleportwarn1 = "Teleport! Zur\195\188ck im Raum in %d Sekunden!",
+	engage_message = "Heigan der Unsaubere angegriffen! 90 Sekunden bis Teleport!",
+	teleport_1min_message = "Teleport in 1 Minute",
+	teleport_30sec_message = "Teleport in 30 Sekunden",
+	teleport_10sec_message = "Teleport in 10 Sekunden",
+	on_floor_message = "Zur\195\188ck im Raum! N\195\164chster Teleport in 90 Sekunden!",
+	to_floor_30sec_message = "Zur\195\188ck im Raum in 30 Sekunden",
+	to_floor_10sec_message = "Zur\195\188ck im Raum in 10 Sekunden",
+	on_platform_message = "Teleport! Zur\195\188ck im Raum in %d Sekunden!",
 
-	teleportbar = "Teleport!",
-	backbar = "R\195\188ckteleport!",
+	teleport_bar = "Teleport!",
+	back_bar = "R\195\188ckteleport!",
 } end )
 
 L:RegisterTranslations("zhCN", function() return {
@@ -102,20 +110,20 @@ L:RegisterTranslations("zhCN", function() return {
 	starttrigger = "你是我的了。",
 	starttrigger2 = "你……就是下一个。",
 	starttrigger3 = "我看到你了……",
-	teleporttrigger = "你的生命正走向终结。",
-	dietrigger = "%s takes his last breath.",
+	teleport_trigger = "你的生命正走向终结。",
+	die_trigger = "%s takes his last breath.",
 	-- [[ Warnings ]]--
-	startwarn = "希尔盖已激活 - 90秒后传送",
-	warn1 = "1分钟后传送",
-	warn2 = "30秒后传送",
-	warn3 = "10秒后传送",
-	backwarn = "希尔盖出现 - 90秒后再次传送",
-	teleportwarn2 = "30秒后希尔盖出现",
-	teleportwarn3 = "10秒后希尔盖出现",
-	teleportwarn1 = "传送发动！ - %d秒后希尔盖出现！",
+	engage_message = "希尔盖已激活 - 90秒后传送",
+	teleport_1min_message = "1分钟后传送",
+	teleport_30sec_message = "30秒后传送",
+	teleport_10sec_message = "10秒后传送",
+	on_floor_message = "希尔盖出现 - 90秒后再次传送",
+	to_floor_30sec_message = "30秒后希尔盖出现",
+	to_floor_10sec_message = "10秒后希尔盖出现",
+	on_platform_message = "传送发动！ - %d秒后希尔盖出现！",
 	-- [[ Bars ]]--
-	teleportbar = "传送！",
-	backbar = "出现！",
+	teleport_bar = "传送！",
+	back_bar = "出现！",
 } end )
 
 L:RegisterTranslations("frFR", function() return {
@@ -123,9 +131,8 @@ L:RegisterTranslations("frFR", function() return {
 	starttrigger = "Vous \195\170tes \195\160 moi, maintenant.",
 	starttrigger2 = "Tu es%.%.%. le suivant.",
 	starttrigger3 = "Je vous vois%.%.%.",
-	teleporttrigger = "The end is upon you.",-- to translate need french /chatlog
+	teleport_trigger = "The end is upon you.",-- to translate need french /chatlog
 } end )
-
 
 ----------------------------------
 --      Module Declaration      --
@@ -134,6 +141,7 @@ L:RegisterTranslations("frFR", function() return {
 BigWigsHeigan = BigWigs:NewModule(boss)
 BigWigsHeigan.zonename = AceLibrary("Babble-Zone-2.0")("Naxxramas")
 BigWigsHeigan.enabletrigger = boss
+BigWigsHeigan.wipemobs = { L["Eye Stalk"], L["Rotting Maggot"] }
 BigWigsHeigan.toggleoptions = {"engage", "teleport", "bosskill"}
 BigWigsHeigan.revision = tonumber(string.sub("$Revision$", 12, -3))
 
@@ -154,7 +162,7 @@ function BigWigsHeigan:OnEnable()
 end
 
 function BigWigsHeigan:CHAT_MSG_MONSTER_EMOTE( msg )
-	if msg == L["dietrigger"] then
+	if msg == L["die_trigger"] then
 		if self.db.profile.bosskill then self:TriggerEvent("BigWigs_Message", string.format(AceLibrary("AceLocale-2.0"):new("BigWigs")["%s has been defeated"], boss), "Bosskill", nil, "Victory") end
 		self.core:ToggleModuleActive(self, false)
 	end
@@ -163,15 +171,15 @@ end
 function BigWigsHeigan:CHAT_MSG_MONSTER_YELL( msg )
 	if msg == L["starttrigger"] or msg == L["starttrigger2"] or msg == L["starttrigger3"] then
 		if self.db.profile.engage then
-			self:TriggerEvent("BigWigs_Message", L["startwarn"], "Important")
+			self:TriggerEvent("BigWigs_Message", L["engage_message"], "Important")
 		end
 		if self.db.profile.teleport then
-			self:TriggerEvent("BigWigs_StartBar", self, L["teleportbar"], self.toPlatformTime, "Interface\\Icons\\Spell_Arcane_Blink")
-			self:ScheduleEvent("bwheiganwarn1", "BigWigs_Message", self.toPlatformTime-60, L["warn1"], "Attention")
-			self:ScheduleEvent("bwheiganwarn2", "BigWigs_Message", self.toPlatformTime-30, L["warn2"], "Urgent")
-			self:ScheduleEvent("bwheiganwarn3", "BigWigs_Message", self.toPlatformTime-10, L["warn3"], "Important")
+			self:TriggerEvent("BigWigs_StartBar", self, L["teleport_bar"], self.toPlatformTime, "Interface\\Icons\\Spell_Arcane_Blink")
+			self:ScheduleEvent("bwheiganwarn1", "BigWigs_Message", self.toPlatformTime-60, L["teleport_1min_message"], "Attention")
+			self:ScheduleEvent("bwheiganwarn2", "BigWigs_Message", self.toPlatformTime-30, L["teleport_30sec_message"], "Urgent")
+			self:ScheduleEvent("bwheiganwarn3", "BigWigs_Message", self.toPlatformTime-10, L["teleport_10sec_message"], "Important")
 		end
-	elseif string.find(msg, L["teleporttrigger"]) then
+	elseif string.find(msg, L["teleport_trigger"]) then
 		self:TriggerEvent("BigWigs_SendSync", "HeiganTeleport")
 	end
 end
@@ -179,22 +187,22 @@ end
 function BigWigsHeigan:BigWigs_RecvSync( sync )
 	if sync ~= "HeiganTeleport" then return end
 
-	self:ScheduleEvent( self.BackToRoom, self.toRoomTime, self )	
+	self:ScheduleEvent( self.BackToRoom, self.toRoomTime, self )
 
 	if self.db.profile.teleport then
-		self:TriggerEvent("BigWigs_Message", string.format(L["teleportwarn1"], self.toRoomTime), "Attention")
-		self:ScheduleEvent("bwheiganwarn2","BigWigs_Message", self.toRoomTime-30, L["teleportwarn2"], "Urgent")
-		self:ScheduleEvent("bwheiganwarn3","BigWigs_Message", self.toRoomTime-10, L["teleportwarn3"], "Important")
-		self:TriggerEvent("BigWigs_StartBar", self, L["backbar"], self.toRoomTime, "Interface\\Icons\\Spell_Magic_LesserInvisibilty")
+		self:TriggerEvent("BigWigs_Message", string.format(L["on_platform_message"], self.toRoomTime), "Attention")
+		self:ScheduleEvent("bwheiganwarn2","BigWigs_Message", self.toRoomTime-30, L["to_floor_30sec_message"], "Urgent")
+		self:ScheduleEvent("bwheiganwarn3","BigWigs_Message", self.toRoomTime-10, L["to_floor_10sec_message"], "Important")
+		self:TriggerEvent("BigWigs_StartBar", self, L["back_bar"], self.toRoomTime, "Interface\\Icons\\Spell_Magic_LesserInvisibilty")
 	end
 end
 
 function BigWigsHeigan:BackToRoom()
 	if self.db.profile.teleport then
-		self:TriggerEvent("BigWigs_Message", L["backwarn"], "Attention")
-		self:ScheduleEvent("bwheiganwarn2","BigWigs_Message", self.toPlatformTime-30, L["warn2"], "Urgent")
-		self:ScheduleEvent("bwheiganwarn3","BigWigs_Message", self.toPlatformTime-10, L["warn3"], "Important")
-		self:TriggerEvent("BigWigs_StartBar", self, L["teleportbar"], self.toPlatformTime, "Interface\\Icons\\Spell_Arcane_Blink")
+		self:TriggerEvent("BigWigs_Message", L["on_floor_message"], "Attention")
+		self:ScheduleEvent("bwheiganwarn2","BigWigs_Message", self.toPlatformTime-30, L["teleport_30sec_message"], "Urgent")
+		self:ScheduleEvent("bwheiganwarn3","BigWigs_Message", self.toPlatformTime-10, L["teleport_10sec_message"], "Important")
+		self:TriggerEvent("BigWigs_StartBar", self, L["teleport_bar"], self.toPlatformTime, "Interface\\Icons\\Spell_Arcane_Blink")
 	end
 end
 
