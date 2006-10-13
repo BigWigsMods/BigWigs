598e4d12a4358c4bd831ac6dd4216b674af6922b
Vidar Haarr <vhaarr@gmail.com>
2006-10-13 14:02:09 +0000

    BigWigs: Loatheb: Doom and Spore bars and messages now have counters.

--- Naxxramas/Loatheb.lua
+++ Naxxramas/Loatheb.lua
@@ -26,15 +26,14 @@ L:RegisterTranslations("enUS", function() return {
 	curse_name = "Remove Curse Alert",
 	curse_desc = "Warn when curses are removed from Loatheb",
 
-	doombar = "Inevitable Doom",
-	doomwarn = "Inevitable Doom! %s sec to next!",
-	doomwarn5sec = "Inevitable Doom in 5 sec!",		
+	doombar = "Inevitable Doom %d",
+	doomwarn = "Inevitable Doom %d! %d sec to next!",
+	doomwarn5sec = "Inevitable Doom %d in 5 sec!",
 	doomtrigger = "afflicted by Inevitable Doom.",
 
-	sporewarn = "Spore Spawned",
-	sporebar = "Summon Spore",
+	sporewarn = "Spore %d Spawned",
+	sporebar = "Summon Spore %d",
 	sporespawntrigger = "Loatheb casts Summon Spore.",
-	sporedietrigger = "Spore dies.",
 
 	removecursewarn = "Curses removed on Loatheb",
 	removecursebar = "Remove Curse",
@@ -61,15 +60,14 @@ L:RegisterTranslations("koKR", function() return {
 	curse_name = "저주 해제 경고",
 	curse_desc = "로데브 저주 해제에 대한 경고",
 
-	doombar = "피할 수 없는 파멸",
-	doomwarn = "피할 수 없는 파멸! 다은은 %s초 후.",
-	doomwarn5sec = "피할 수 없는 파멸 5초 전",		
+	--doombar = "피할 수 없는 파멸",
+	--doomwarn = "피할 수 없는 파멸! 다은은 %s초 후.",
+	--doomwarn5sec = "피할 수 없는 파멸 5초 전",
 	doomtrigger = "피할 수 없는 파멸에 걸렸습니다.",
 	
 	sporewarn = "포자소환!",
 	sporebar = "포자 소환!",
 	sporespawntrigger = "로데브|1이;가; 포자 소환|1을;를; 시전합니다.",
-	sporedietrigger = "포자|1이;가; 죽었습니다.",
 
 	removecursewarn = "로데브 저주 헤제 시전!",
 	removecursebar = "저주 해제",
@@ -95,15 +93,14 @@ L:RegisterTranslations("deDE", function() return {
 	curse_name = "Fluch-Aufhebungs Warnung",
 	curse_desc = "Warnung wenn Fl\195\188che bei Loatheb aufgehoben wurden",
 
-	doombar = "Unausweichliches Schicksal",
-	doomwarn = "Unausweichliches Schicksal! %s Sekunden bis zum n\195\164chsten.",
-	doomwarn5sec = "Unausweichliches Schicksal in 5 Sekunden",
+	doombar = "Unausweichliches Schicksal %d",
+	doomwarn = "Unausweichliches Schicksal %d! %d Sekunden bis zum n\195\164chsten.",
+	doomwarn5sec = "Unausweichliches Schicksal %d in 5 Sekunden",
 	doomtrigger = "von Unausweichliches Schicksal betroffen.",
 
 	sporewarn = "Spore aufgetaucht",
 	sporebar = "Spore beschw\195\182ren",
 	sporespawntrigger = "Loatheb wirkt Spore beschw\195\182ren.",
-	sporedietrigger = "Spore stirbt.",
 
 	removecursewarn = "Fl\195\188che bei Loatheb aufgehoben",
 	removecursebar = "Fluch aufheben",
@@ -129,15 +126,14 @@ L:RegisterTranslations("zhCN", function() return {
 	curse_name = "诅咒驱散警报",
 	curse_desc = "洛欧塞布驱散了一个诅咒效果时发出警报",
 
-	doombar = "必然的厄运",
-	doomwarn = "必然的厄运 - %s秒后再次发动",
-	doomwarn5sec = "5秒后发动必然的厄运！",		
+	--doombar = "必然的厄运",
+	--doomwarn = "必然的厄运 - %s秒后再次发动",
+	--doomwarn5sec = "5秒后发动必然的厄运！",
 	doomtrigger = "受到了必然的厄运效果的影响",
 
 	sporewarn = "孢子出现",
 	sporebar = "召唤孢子",
 	sporespawntrigger = "洛欧塞布施放了召唤孢子。",
-	sporedietrigger = "孢子死亡了。",
 
 	removecursewarn = "洛欧塞布驱散了一个诅咒效果",
 	removecursebar = "驱散诅咒",
@@ -169,8 +165,10 @@ BigWigsLoatheb.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 function BigWigsLoatheb:OnEnable()
 	self.doomTime = 30
+	self.sporeCount = 1
+	self.doomCount = 1
 	started = nil
-	
+
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
@@ -180,8 +178,11 @@ function BigWigsLoatheb:OnEnable()
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF")
 
 	self:RegisterEvent("BigWigs_RecvSync")
-	self:TriggerEvent("BigWigs_ThrottleSync", "LoathebDoom", 10)
-	self:TriggerEvent("BigWigs_ThrottleSync", "LoathebSporeSpawn", 5)
+
+	-- 2: Doom and SporeSpawn versioned up because of the sync including the
+	-- doom/spore count now, so we don't hold back the counter.
+	self:TriggerEvent("BigWigs_ThrottleSync", "LoathebDoom2", 10)
+	self:TriggerEvent("BigWigs_ThrottleSync", "LoathebSporeSpawn2", 5)
 	self:TriggerEvent("BigWigs_ThrottleSync", "LoathebRemoveCurse", 10)
 end
 
@@ -202,19 +203,35 @@ function BigWigsLoatheb:BigWigs_RecvSync(sync, rest, nick)
 			self:ScheduleEvent("bwloathebdoomtimerreduce", function () BigWigsLoatheb.doomTime = 15 end, 300)
 
 			self:TriggerEvent("BigWigs_Message", L["startwarn"], "Red")
-			self:TriggerEvent("BigWigs_StartBar", self, L["doombar"], 120, "Interface\\Icons\\Spell_Shadow_NightOfTheDead")
-			self:ScheduleEvent("bwloathebdoom", "BigWigs_Message", 115, L["doomwarn5sec"], "Urgent")
+			self:TriggerEvent("BigWigs_StartBar", self, string.format(L["doombar"], self.doomCount), 120, "Interface\\Icons\\Spell_Shadow_NightOfTheDead")
+			self:ScheduleEvent("bwloathebdoom", "BigWigs_Message", 115, string.format(L["doomwarn5sec"], self.doomCount), "Urgent")
 		end
-	elseif sync == "LoathebDoom" then
-		if self.db.profile.doom then
-			self:TriggerEvent("BigWigs_Message", string.format(L["doomwarn"], self.doomTime), "Important")
-			self:TriggerEvent("BigWigs_StartBar", self, L["doombar"], self.doomTime, "Interface\\Icons\\Spell_Shadow_NightOfTheDead")
-			self:ScheduleEvent("bwloathebdoom", "BigWigs_Message", self.doomTime - 5, L["doomwarn5sec"], "Urgent")
+	elseif sync == "LoathebDoom2" and rest then
+		rest = tonumber(rest)
+		if not rest then return end
+
+		if rest == (self.doomCount + 1) then
+			if self.db.profile.doom then
+				self:TriggerEvent("BigWigs_Message", string.format(L["doomwarn"], self.doomCount, self.doomTime), "Important")
+			end
+			self.doomCount = self.doomCount + 1
+			if self.db.profile.doom then
+				self:TriggerEvent("BigWigs_StartBar", self, string.format(L["doombar"], self.doomCount), self.doomTime, "Interface\\Icons\\Spell_Shadow_NightOfTheDead")
+				self:ScheduleEvent("bwloathebdoom", "BigWigs_Message", self.doomTime - 5, string.format(L["doomwarn5sec"], self.doomCount), "Urgent")
+			end
 		end
-	elseif sync == "LoathebSporeSpawn" then
-		if self.db.profile.spore then
-			self:TriggerEvent("BigWigs_Message", L["sporewarn"], "Important")
-			self:TriggerEvent("BigWigs_StartBar", self, L["sporebar"], 12, "Interface\\Icons\\Ability_TheBlackArrow")
+	elseif sync == "LoathebSporeSpawn2" and rest then
+		rest = tonumber(rest)
+		if not rest then return end
+
+		if rest == (self.sporeCount + 1) then
+			if self.db.profile.spore then
+				self:TriggerEvent("BigWigs_Message", string.format(L["sporewarn"], self.sporeCount), "Important")
+			end
+			self.sporeCount = self.sporeCount + 1
+			if self.db.profile.spore then
+				self:TriggerEvent("BigWigs_StartBar", self, string.format(L["sporebar"], self.sporeCount), 12, "Interface\\Icons\\Ability_TheBlackArrow")
+			end
 		end
 	elseif sync == "LoathebRemoveCurse" then
 		if self.db.profile.curse then
@@ -225,13 +242,15 @@ function BigWigsLoatheb:BigWigs_RecvSync(sync, rest, nick)
 end
 
 function BigWigsLoatheb:Event( msg )
-	if string.find(msg, L["doomtrigger"]) then self:TriggerEvent("BigWigs_SendSync", "LoathebDoom") end
+	if string.find(msg, L["doomtrigger"]) then
+		self:TriggerEvent("BigWigs_SendSync", "LoathebDoom2 "..tostring(self.doomCount + 1))
+	end
 end
 
 function BigWigsLoatheb:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF( msg )
-	if msg == L["sporespawntrigger"] then 
-		self:TriggerEvent("BigWigs_SendSync", "LoathebSporeSpawn")
-	elseif msg == L["removecursetrigger"] then 
+	if msg == L["sporespawntrigger"] then
+		self:TriggerEvent("BigWigs_SendSync", "LoathebSporeSpawn2 "..tostring(self.sporeCount + 1))
+	elseif msg == L["removecursetrigger"] then
 		self:TriggerEvent("BigWigs_SendSync", "LoathebRemoveCurse")
 	end
 end
