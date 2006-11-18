e3a20e3e54292ff58dd9a12b54bb98b6a2bb381a
Wouter Commandeur <wouter@ctlaltdel.nl>
2006-11-18 17:59:59 +0000

    BigWigs: bigwigslod.bat added. Execute after a checkout for easy LoD enabling.

---
+++ bigwigslod.bat
@@ -0,0 +1,20 @@
+@echo off
+move AQ20\AQ20.toc AQ20\BigWigs_AQ20.toc
+move AQ40\AQ40.toc AQ40\BigWigs_AQ40.toc
+move BWL\BWL.toc BWL\BigWigs_BWL.toc
+move MC\MC.toc MC\BigWigs_MC.toc
+move Naxxramas\Naxxramas.toc Naxxramas\BigWigs_Naxxramas.toc
+move ZG\ZG.toc ZG\BigWigs_ZG.toc
+move Other\Other.toc Other\BigWigs_Other.toc
+move Extras\Extras.toc Extras\BigWigs_Extras.toc
+move Plugins\Plugins.toc Plugins\BigWigs_Plugins.toc
+
+move AQ20 ..\BigWigs_AQ20
+move AQ40 ..\BigWigs_AQ40
+move BWL ..\BigWigs_BWL
+move MC ..\BigWigs_MC
+move Naxxramas ..\BigWigs_Naxxramas
+move ZG ..\BigWigs_ZG
+move Other ..\BigWigs_Other
+move Extras ..\BigWigs_Extras
+move Plugins ..\BigWigs_Plugins
