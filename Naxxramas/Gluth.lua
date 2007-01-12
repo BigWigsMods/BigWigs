25aede65b80c9847abbbbfda1ebf609886970616
Vidar Haarr <vhaarr@gmail.com>
2007-01-12 22:41:22 +0000

    BigWigs: Gluth: Add enrage timer and don't sync decimate, it hits everyone anyway + some cleanup.

--- Naxxramas/Gluth.lua
+++ Naxxramas/Gluth.lua
@@ -6,6 +6,7 @@ local boss = AceLibrary("Babble-Boss-2.2")["Gluth"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
 local started = nil
+local prior = nil
 
 ----------------------------
 --      Localization      --
@@ -26,19 +27,26 @@ L:RegisterTranslations("enUS", function() return {
 	decimate_name = "Decimate Alert",
 	decimate_desc = "Warn for Decimate",
 
-	trigger1 = "%s goes into a frenzy!",
-	trigger2 = "by Terrifying Roar.",
+	enrage_cmd = "enrage",
+	enrage_name = "Enrage",
+	enrage_desc = "Timer for when he becomes enraged.",
 
-	warn1 = "Frenzy Alert!",
-	warn2 = "5 second until AoE Fear!",
-	warn3 = "AoE Fear alert - 20 seconds till next!",
+	frenzy_trigger = "%s goes into a frenzy!",
+	fear_trigger = "by Terrifying Roar.",
+
+	frenzy_message = "Frenzy Alert!",
+	fear_warning = "5 second until AoE Fear!",
+	fear_message = "AoE Fear alert - 20 seconds till next!",
 
 	startwarn = "Gluth Engaged! ~105 seconds till Zombies!",
 	decimatesoonwarn = "Decimate Soon!",
 	decimatewarn = "Decimate!",
 	decimatetrigger = "Decimate",
 
-	bar1text = "AoE Fear",
+	enrage_warning = "Enrage in %d sec!",
+	enrage_bar = "Enrage",
+
+	fear_bar = "AoE Fear",
 	decimatebartext = "Decimate Zombies",
 } end )
 
@@ -52,19 +60,19 @@ L:RegisterTranslations("koKR", function() return {
 	decimate_name = "척살 경보",
 	decimate_desc = "척살에 대한 경고",
 
-	trigger1 = "%s|1이;가; 광란의 상태에 빠집니다!", -- CHECK
-	trigger2 = "공포의 포효에",
+	frenzy_trigger = "%s|1이;가; 광란의 상태에 빠집니다!", -- CHECK
+	fear_trigger = "공포의 포효에",
 
-	warn1 = "광폭화 경고!",
-	warn2 = "광역 공포 5초 전!",
-	warn3 = "광역 공포 경고 - 20초 후",
+	frenzy_message = "광폭화 경고!",
+	fear_warning = "광역 공포 5초 전!",
+	fear_message = "광역 공포 경고 - 20초 후",
 
 	startwarn = "글루스 전투 시작! 약 105초 후 좀비!",
 	decimatesoonwarn = "곧 척살!",
 	decimatewarn = "척살!",
 	decimatetrigger = "척살",
 
-	bar1text = "광역 공포",
+	fear_bar = "광역 공포",
 	decimatebartext = "척살 좀비",
 } end )
 
@@ -78,19 +86,19 @@ L:RegisterTranslations("deDE", function() return {
 	decimate_name = "Dezimieren",
 	decimate_desc = "Warnung vor Dezimieren.",
 
-	trigger1 = "%s ger\195\164t in Raserei!",
-	trigger2 = "von Erschreckendes Gebr\195\188ll betroffen.",
+	frenzy_trigger = "%s ger\195\164t in Raserei!",
+	fear_trigger = "von Erschreckendes Gebr\195\188ll betroffen.",
 
-	warn1 = "Raserei!",
-	warn2 = "AoE Furcht in 5 Sekunden!",
-	warn3 = "AoE Furcht! N\195\164chste in 20 Sekunden!",
+	frenzy_message = "Raserei!",
+	fear_warning = "AoE Furcht in 5 Sekunden!",
+	fear_message = "AoE Furcht! N\195\164chste in 20 Sekunden!",
 
 	startwarn = "Gluth angegriffen! Zombies in ~105 Sekunden!",
 	decimatesoonwarn = "Dezimieren kurz bevor",
 	decimatewarn = "Dezimieren!",
 	decimatetrigger = "Dezimieren",
 
-	bar1text = "Furcht",
+	fear_bar = "Furcht",
 	decimatebartext = "Dezimieren",
 } end )
 
@@ -104,19 +112,19 @@ L:RegisterTranslations("zhCN", function() return {
 	decimate_name = "残杀警报",
 	decimate_desc = "残杀警报",
 
-	trigger1 = "%s变得狂怒无比！",
-	trigger2 = "恐惧怒吼",
+	frenzy_trigger = "%s变得狂怒无比！",
+	fear_trigger = "恐惧怒吼",
 
-	warn1 = "狂暴警报 - 猎人立刻使用宁神射击！",
-	warn2 = "5秒后发动群体恐惧！",
-	warn3 = "群体恐惧 - 20秒后再次发动",
+	frenzy_message = "狂暴警报 - 猎人立刻使用宁神射击！",
+	fear_warning = "5秒后发动群体恐惧！",
+	fear_message = "群体恐惧 - 20秒后再次发动",
 
 	startwarn = "格拉斯已激活，~105秒后僵尸出现！",
 	decimatesoonwarn = "残杀来临！",
 	decimatewarn = "残杀 - AoE僵尸！",
 	decimatetrigger = "残杀",
 
-	bar1text = "群体恐惧",
+	fear_bar = "群体恐惧",
 	decimatebartext = "残杀",
 } end )
 
@@ -130,25 +138,25 @@ L:RegisterTranslations("zhTW", function() return {
 	decimate_name = "屠殺警報",
 	decimate_desc = "當古魯斯屠殺時發出警報",
 
-	trigger1 = "變得狂暴起來！",
-	trigger2 = "恐嚇咆哮",
+	frenzy_trigger = "變得狂暴起來！",
+	fear_trigger = "恐嚇咆哮",
 
-	warn1 = "狂暴警報 - 獵人立刻使用寧神射擊！",
-	warn2 = "5 秒後發動群體恐懼！",
-	warn3 = "群體恐懼 - 20 秒後再次發動",
+	frenzy_message = "狂暴警報 - 獵人立刻使用寧神射擊！",
+	fear_warning = "5 秒後發動群體恐懼！",
+	fear_message = "群體恐懼 - 20 秒後再次發動",
 
 	startwarn = "古魯斯已進入戰鬥 - 105 秒後僵屍出現！",
 	decimatesoonwarn = "屠殺來臨！",
 	decimatewarn = "屠殺 - AE僵屍！",
 	decimatetrigger = "屠殺",
 
-	bar1text = "群體恐懼",
+	fear_bar = "群體恐懼",
 	decimatebartext = "屠殺",
 } end )
 
 L:RegisterTranslations("frFR", function() return {
-	trigger1 = "%s gagne Fr\195\169n\195\169sie.",
-	trigger2 = "de Rugissement terrifiant.",
+	frenzy_trigger = "%s gagne Fr\195\169n\195\169sie.",
+	fear_trigger = "de Rugissement terrifiant.",
 	decimatetrigger = "D\195\169cimer",
 } end )
 
@@ -160,19 +168,15 @@ L:RegisterTranslations("frFR", function() return {
 BigWigsGluth = BigWigs:NewModule(boss)
 BigWigsGluth.zonename = AceLibrary("Babble-Zone-2.2")["Naxxramas"]
 BigWigsGluth.enabletrigger = boss
-BigWigsGluth.toggleoptions = {"frenzy", "fear", "decimate", "bosskill"}
+BigWigsGluth.toggleoptions = {"enrage", "frenzy", "fear", "decimate", "bosskill"}
 BigWigsGluth.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 ------------------------------
 --      Initialization      --
 ------------------------------
 
--- XXX Need to add a timer bar for berserker rage.
--- XXX It happens some time after the 3rd decimate, but it's probably on a
--- XXX fixed timer, so just make it a bar like the Twins@AQ40 enrage timer.
-
 function BigWigsGluth:OnEnable()
-	self.prior = nil
+	prior = nil
 	started = nil
 
 	self:RegisterEvent("BigWigs_Message")
@@ -192,36 +196,33 @@ function BigWigsGluth:OnEnable()
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Decimate")
 
 	self:RegisterEvent("BigWigs_RecvSync")
-	self:TriggerEvent("BigWigs_ThrottleSync", "GluthDecimate", 30)
 end
 
 function BigWigsGluth:Frenzy( msg )
-	if self.db.profile.frenzy and msg == L["trigger1"] then
-		self:TriggerEvent("BigWigs_Message", L["warn1"], "Important")
+	if self.db.profile.frenzy and msg == L["frenzy_trigger"] then
+		self:TriggerEvent("BigWigs_Message", L["frenzy_message"], "Important")
 	end
 end
 
 function BigWigsGluth:Fear( msg )
-	if self.db.profile.fear and not self.prior and msg:find(L["trigger2"]) then
-		self:TriggerEvent("BigWigs_Message", L["warn3"], "Important")
-		self:TriggerEvent("BigWigs_StartBar", self, L["bar1text"], 20, "Interface\\Icons\\Spell_Shadow_PsychicScream")
-		self:ScheduleEvent("BigWigs_Message", 15, L["warn2"], "Urgent")
-		self.prior = true
+	if self.db.profile.fear and not prior and msg:find(L["fear_trigger"]) then
+		self:TriggerEvent("BigWigs_Message", L["fear_message"], "Important")
+		self:TriggerEvent("BigWigs_StartBar", self, L["fear_bar"], 20, "Interface\\Icons\\Spell_Shadow_PsychicScream")
+		self:ScheduleEvent("BigWigs_Message", 15, L["fear_warning"], "Urgent")
+		prior = true
 	end
 end
 
 function BigWigsGluth:Decimate( msg )
-	if msg:find(L["decimatetrigger"]) then
-		self:TriggerEvent("BigWigs_SendSync", "GluthDecimate")
+	if msg:find(L["decimatetrigger"]) and self.db.profile.decimate then
+		self:TriggerEvent("BigWigs_Message", L["decimatewarn"], "Important")
+		self:TriggerEvent("BigWigs_StartBar", self, L["decimatebartext"], 105, "Interface\\Icons\\INV_Shield_01")
+		self:ScheduleEvent("BigWigs_Message", 100, L["decimatesoonwarn"], "Urgent")
 	end
 end
 
 function BigWigsGluth:BigWigs_RecvSync( sync, rest, nick )
-	if sync == "GluthDecimate" and self.db.profile.decimate then
-		self:TriggerEvent("BigWigs_Message", L["decimatewarn"], "Important")
-		self:TriggerEvent("BigWigs_StartBar", self, L["decimatebartext"], 105, "Interface\\Icons\\INV_Shield_01")
-		self:ScheduleEvent("BigWigs_Message", 100, L["decimatesoonwarn"], "Urgent")
-	elseif self:ValidateEngageSync(sync, rest) and not started then
+	if self:ValidateEngageSync(sync, rest) and not started then
 		started = true
 		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then self:UnregisterEvent("PLAYER_REGEN_DISABLED") end
 		if self.db.profile.decimate then
@@ -229,9 +230,17 @@ function BigWigsGluth:BigWigs_RecvSync( sync, rest, nick )
 			self:TriggerEvent("BigWigs_StartBar", self, L["decimatebartext"], 105, "Interface\\Icons\\INV_Shield_01")
 			self:ScheduleEvent("BigWigs_Message", 100, L["decimatesoonwarn"], "Urgent")
 		end
+		if self.db.profile.enrage then
+			self:ScheduleEvent("BigWigs_Message", 240, string.format(L["enrage_warning"], 120), "Attention")
+			self:ScheduleEvent("BigWigs_Message", 300, string.format(L["enrage_warning"], 60), "Attention")
+			self:ScheduleEvent("BigWigs_Message", 330, string.format(L["enrage_warning"], 30), "Urgent")
+			self:ScheduleEvent("BigWigs_Message", 350, string.format(L["enrage_warning"], 10), "Important")
+			self:TriggerEvent("BigWigs_StartBar", self, L["enrage_bar"], 360, "Interface\\Icons\\Spell_Shadow_UnholyFrenzy")
+		end
 	end
 end
 
 function BigWigsGluth:BigWigs_Message(text)
-	if text == L["warn2"] then self.prior = nil end
+	if text == L["fear_warning"] then prior = nil end
 end
+
