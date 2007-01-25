cc3173e7eed0de31ede8cbed5c86f37fbc6c00eb
funkydude <funkydude@users.noreply.github.com>
2007-01-25 19:46:22 +0000

    BigWigs: Other: prepin' Gruul's Lair

---
+++ Other/Gruul.lua
@@ -0,0 +1,32 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["Gruul"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Gruul",
+} end)
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+BigWigsGruul = BigWigs:NewModule(boss)
+BigWigsGruul.zonename = AceLibrary("Babble-Zone-2.2")["Gruul's Lair"]
+BigWigsGruul.enabletrigger = boss
+BigWigsGruul.toggleoptions = {"bosskill"}
+BigWigsGruul.revision = tonumber(string.sub("$Revision$", 12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function BigWigsGruul:OnEnable()
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+end
