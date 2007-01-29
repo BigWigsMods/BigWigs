1ad37177b7a48fec357c3124c9e047a2d699bb90
funkydude <funkydude@users.noreply.github.com>
2007-01-29 08:10:29 +0000

    BigWigs: Other: updated Kazzak mod a little

--- Other/Kazzak.lua
+++ Other/Kazzak.lua
@@ -11,6 +11,13 @@ local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Kazzak",
+
+	engage_cmd = "engage",
+	engage_name = "Engage Alert",
+	engage_desc = "Warn when Kazzak is pulled",
+
+	engage_trigger = "For the Legion! For Kil'Jaeden!",
+	engage_message = "Kazzak Engaged - Have a fun Death!",
 } end)
 
 ----------------------------------
@@ -20,7 +27,7 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsKazzak = BigWigs:NewModule(boss)
 BigWigsKazzak.zonename = AceLibrary("Babble-Zone-2.2")["Hellfire Peninsula"]
 BigWigsKazzak.enabletrigger = boss
-BigWigsKazzak.toggleoptions = {"bosskill"}
+BigWigsKazzak.toggleoptions = {"engage", "bosskill"}
 BigWigsKazzak.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 ------------------------------
@@ -29,4 +36,15 @@ BigWigsKazzak.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 function BigWigsKazzak:OnEnable()
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function BigWigsKazzak:CHAT_MSG_MONSTER_YELL(msg)
+	if self.db.profile.engage and msg == L["engage_trigger"] then
+		self:TriggerEvent("BigWigs_Message", L["engage_message"], "Attention")
+	end
 end
