06de27662ca45cdea3618913f7cd1faae7752aaa
Vidar Haarr <vhaarr@gmail.com>
2007-01-31 15:02:51 +0000

    BigWigs: Karazhan: Fix the Transcriptor download link.

--- Karazhan/KarazhanBeta.lua
+++ Karazhan/KarazhanBeta.lua
@@ -16,7 +16,7 @@ function KarazhanBeta:ZONE_CHANGED_NEW_AREA()
 	if not IsAddOnLoaded("Transcriptor") then
 		self:TriggerEvent("BigWigs_Message", "Karazhan Beta Bossmods Loaded", "Important")
 		self:Print("The BigWigs boss modules for Karazhan are not complete yet, by far, and we need your help.")
-		self:Print("Please download and install Transcriptor from http://wowace.com/files/index.php?path=Transcriptor/")
+		self:Print("Please download and install Transcriptor from http://files.wowace.com/Transcriptor/")
 		self:Print("It records what happens during an encounter, and you can then post the log on our forums.")
 		self:Print("Thanks!")
 	else
