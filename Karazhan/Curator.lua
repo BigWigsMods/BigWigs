87ea8dc369f6c7ad0bc8ac9fc53a1c1c67a01c3c
funkydude <funkydude@users.noreply.github.com>
2007-02-03 01:30:27 +0000

    BigWigs: Karazhan/Curator: - try warn for evoc

--- Karazhan/Curator.lua
+++ Karazhan/Curator.lua
@@ -25,11 +25,20 @@ L:RegisterTranslations("enUS", function() return {
 	weaken_name = "Weaken",
 	weaken_desc = "Alert when The Curator is weakened.",
 
+	weaktime_cmd = "weaktime",
+	weaktime_name = "Weaken Countdown",
+	weaktime_desc = "Countdown warning and bar untill next weaken.",
+
 	weaken_trigger = "The Curator is afflicted by Evocation.",
-	weaken_message = "Evocation - Weakened for ~20sec!",
+	weaken_message = "Evocation - Weakened for 20sec!",
 	weaken_bar = "Evocation",
-	weaken_fade_trigger = "Evocation fades from The Curator.",
 	weaken_fade_message = "Evocation Finished - Weakened Gone!",
+	weaken_fade_warning = "Evocation over in 5sec!",
+
+	weaktime_message1 = "Evocation in ~10 seconds",
+	weaktime_message2 = "Evocation in ~30 seconds",
+	weaktime_message3 = "Evocation in ~70 seconds",
+	weaktime_bar = "Next Evocation",
 
 	berserk_trigger = "The Menagerie is for guests only.",
 	berserk_message = "Curator engaged, 12min to berserk!",
@@ -47,7 +56,7 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsCurator = BigWigs:NewModule(boss)
 BigWigsCurator.zonename = AceLibrary("Babble-Zone-2.2")["Karazhan"]
 BigWigsCurator.enabletrigger = boss
-BigWigsCurator.toggleoptions = {"weaken", "berserk", "enrage", "bosskill"}
+BigWigsCurator.toggleoptions = {"weaken", "weaktime", "berserk", "enrage", "bosskill"}
 BigWigsCurator.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 ------------------------------
@@ -57,13 +66,12 @@ BigWigsCurator.revision = tonumber(string.sub("$Revision$", 12, -3))
 function BigWigsCurator:OnEnable()
 	enrageannounced = nil
 
-	self:RegisterEvent("CHAT_MSG_SPELL_AURA_GONE_OTHER")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_DAMAGE")
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 
 	self:RegisterEvent("UNIT_HEALTH")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
-	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 end
 
 ------------------------------
@@ -72,8 +80,10 @@ end
 
 function BigWigsCurator:CHAT_MSG_MONSTER_YELL(msg)
 	if msg == L["berserk_trigger"] and self.db.profile.berserk then
-			self:TriggerEvent("BigWigs_Message", L["berserk_message"], "Attention")
-			self:TriggerEvent("BigWigs_StartBar", self, L["berserk_bar"], 720, "Interface\\Icons\\INV_Shield_01")
+		self:TriggerEvent("BigWigs_Message", L["berserk_message"], "Attention")
+		self:TriggerEvent("BigWigs_StartBar", self, L["berserk_bar"], 720, "Interface\\Icons\\INV_Shield_01")
+	elseif msg == L["berserk_trigger"] and self.db.profile.weaktime then
+		self:Evoc()
 	end
 end
 
@@ -81,12 +91,18 @@ function BigWigsCurator:CHAT_MSG_SPELL_PERIODIC_CREATURE_DAMAGE(msg)
 	if msg == L["weaken_trigger"] and self.db.profile.weaken then
 		self:TriggerEvent("BigWigs_Message", L["weaken_message"], "Important", nil, "Alarm")
 		self:TriggerEvent("BigWigs_StartBar", self, L["weaken_bar"], 20, "Interface\\Icons\\Spell_Nature_Purge")
+		self:ScheduleEvent("BigWigs_Message", 15, L["weaken_fade_warning"], "Urgent")
+		self:ScheduleEvent("BigWigs_Message", 20, L["weaken_fade_message"], "Important", nil, "Alarm")
+		self:Evoc()
 	end
 end
 
-function BigWigsCurator:CHAT_MSG_SPELL_AURA_GONE_OTHER(msg)
-	if msg == L["weaken_fade_trigger"] and self.db.profile.weaken then
-		self:TriggerEvent("BigWigs_Message", L["weaken_fade_message"], "Urgent")
+function BigWigsCurator:Evoc()
+	if self.db.profile.weaktime then
+		self:TriggerEvent("BigWigs_StartBar", self, L["weaktime_bar"], 109, "Interface\\Icons\\Spell_Nature_Purge")
+		self:ScheduleEvent("BigWigs_Message", 39, L["weaktime_message3"], "Positive")
+		self:ScheduleEvent("BigWigs_Message", 79, L["weaktime_message2"], "Attention")
+		self:ScheduleEvent("BigWigs_Message", 99, L["weaktime_message1"], "Urgent")
 	end
 end
 
