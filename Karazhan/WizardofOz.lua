6fed4f4b7175dc309274b483a5a2090cb5b893f2
saroz <saroz@users.noreply.wowace.com>
2007-02-06 17:01:45 +0000

    BigWigs: Wizard of Oz: Add option to display when Roar, Tinhead and Strawman becomes hostile. Show a timer for when Tito gets summoned.

--- Karazhan/WizardofOz.lua
+++ Karazhan/WizardofOz.lua
@@ -19,9 +19,20 @@ L:RegisterTranslations("enUS", function() return {
 	summon_cmd = "summon",
 	summon_name = "Summon Tito",
 	summon_desc = "Alert when the Dorothee begins to summon Tito",
+	
+	spawns_cmd = "spawns",
+	spawns_name = "Sapwn timers",
+	spawns_desc = "Shows timer for when Roar, Tunehead and Strawman becomes hostile",
+	
+	spawns_roar = "Roar attacks!",
+	spawns_strawman = "Strawman attacks!",
+	spawns_tinhead = "Tinhead attacks!",
+	spawns_tito = "Tito summoned!",
 
 	summon_trigger = "Don't let them hurt us Tito! Oh, you won't, will you?",
 	summon_message = "Summoning Tito!",
+	
+	engage_trigger = "Oh Tito, we simply must find a way home!",
 } end)
 
 ----------------------------------
@@ -31,7 +42,7 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsWizardofOz = BigWigs:NewModule(boss)
 BigWigsWizardofOz.zonename = AceLibrary("Babble-Zone-2.2")["Karazhan"]
 BigWigsWizardofOz.enabletrigger = {roar, tinhead, strawman, dorothee}
-BigWigsWizardofOz.toggleoptions = {"summon", "bosskill"}
+BigWigsWizardofOz.toggleoptions = {"summon", "spawns", "bosskill"}
 BigWigsWizardofOz.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 ------------------------------
@@ -51,5 +62,13 @@ end
 function BigWigsWizardofOz:CHAT_MSG_MONSTER_YELL(msg)
 	if msg:find(L["summon_trigger"]) and self.db.profile.summon then
 		self:TriggerEvent("BigWigs_Message", L["summon_message"], "Attention")
+	end
+
+	if msg:find(L["engage_trigger"]) and self.db.profile.spawns then
+		self:TriggerEvent("BigWigs_StartBar", self, L["spawns_roar"], 15, "Interface\\Icons\\INV_Staff_08")
+		self:TriggerEvent("BigWigs_StartBar", self, L["spawns_strawman"], 25, "Interface\\Icons\\Ability_Druid_ChallangingRoar")
+		self:TriggerEvent("BigWigs_StartBar", self, L["spawns_tinhead"], 35, "Interface\\Icons\\INV_Chest_Plate06")
+		-- Summon is performed at 47 seconds, 1 second cast
+		self:TriggerEvent("BigWigs_StartBar", self, L["spawns_tito"], 48, "Interface\\Icons\\Ability_Hunter_Pet_Wolf")
 	end
 end
