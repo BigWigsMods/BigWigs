573821930a2bf86ccd66cc231c543436961f34d2
Vidar Haarr <vhaarr@gmail.com>
2007-02-06 14:43:41 +0000

    BigWigs: Fix the bat+sh files and the TOC for Other -> Azeroth.

--- bigwigslod.bat
+++ bigwigslod.bat
@@ -3,15 +3,17 @@
 cd ..
 rmdir /s /q BigWigs_AQ20
 rmdir /s /q BigWigs_AQ40
+rmdir /s /q BigWigs_Azeroth
 rmdir /s /q BigWigs_BWL
 rmdir /s /q BigWigs_MC
 rmdir /s /q BigWigs_ZG
 rmdir /s /q BigWigs_Naxxramas
 rmdir /s /q BigWigs_Karazhan
-rmdir /s /q BigWigs_Other
 rmdir /s /q BigWigs_Outland
 rmdir /s /q BigWigs_Extras
 rmdir /s /q BigWigs_Plugins
+
+rmdir /s /q BigWigs_Other
 
 cd BigWigs
 
@@ -22,7 +24,7 @@ move MC\MC.toc MC\BigWigs_MC.toc
 move Naxxramas\Naxxramas.toc Naxxramas\BigWigs_Naxxramas.toc
 move Karazhan\Karazhan.toc Karazhan\BigWigs_Karazhan.toc
 move ZG\ZG.toc ZG\BigWigs_ZG.toc
-move Other\Other.toc Other\BigWigs_Other.toc
+move Azeroth\Azeroth.toc Azeroth\BigWigs_Azeroth.toc
 move Outland\Outland.toc Outland\BigWigs_Outland.toc
 move Extras\Extras.toc Extras\BigWigs_Extras.toc
 move Plugins\Plugins.toc Plugins\BigWigs_Plugins.toc
@@ -34,7 +36,7 @@ move MC ..\BigWigs_MC
 move Naxxramas ..\BigWigs_Naxxramas
 move Karazhan ..\BigWigs_Karazhan
 move ZG ..\BigWigs_ZG
-move Other ..\BigWigs_Other
+move Azeroth ..\BigWigs_Azeroth
 move Outland ..\BigWigs_Outland
 move Extras ..\BigWigs_Extras
 move Plugins ..\BigWigs_Plugins
