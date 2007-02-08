6fd042d8392f95421b041c0a1575fc1dce4ff922
funkydude <funkydude@users.noreply.github.com>
2007-02-08 23:31:24 +0000

    BigWigs: Karazhan/Hyakiss: - fixed bar timer and name display

--- Karazhan/Hyakiss.lua
+++ Karazhan/Hyakiss.lua
@@ -4,7 +4,6 @@
 
 local boss = AceLibrary("Babble-Boss-2.2")["Hyakiss the Lurker"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
-
 local times
 
 ----------------------------
@@ -18,7 +17,7 @@ L:RegisterTranslations("enUS", function() return {
 	web_name = "Web",
 	web_desc = "Alert when a player gets webbed.",
 
-	web_trigger = "(.*) (.*) afflicted by Hyakiss' Web.",
+	web_trigger = "^([^%s]+) ([^%s]+) afflicted by Hyakiss' Web.",
 	web_message = "%s has been webbed.",
 	web_bar = "Web: %s",
 
@@ -49,14 +48,13 @@ function BigWigsHyakiss:OnEnable()
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 
 	self:RegisterEvent("BigWigs_RecvSync")
-	self:TriggerEvent("BigWigs_ThrottleSync", "HyakissWeb", 5)
+	self:TriggerEvent("BigWigs_ThrottleSync", "HyakissWeb", 3)
 end
 
 ------------------------------
 --      Event Handlers      --
 ------------------------------
 
--- Event bucket until we know what's really going on.
 function BigWigsHyakiss:Web(msg)
 	if msg:find(L["web_trigger"]) then
 		local webPlayer, webType = select(3, msg:find(L["web_trigger"]))
@@ -78,7 +76,7 @@ function BigWigsHyakiss:BigWigs_RecvSync(sync, rest, nick)
 		if not times[rest] or (times[rest] and (times[rest] + 5) < t) then
 			if self.db.profile.web then
 				self:TriggerEvent("BigWigs_Message", string.format(L["web_message"], rest), "Urgent")
-				self:TriggerEvent("BigWigs_StartBar", self, L["web_bar"], 720, "Interface\\Icons\\Spell_Nature_Web")
+				self:TriggerEvent("BigWigs_StartBar", self, string.format(L["web_bar"], rest), 8, "Interface\\Icons\\Spell_Nature_Web")
 			end
 			times[rest] = t
 		end
