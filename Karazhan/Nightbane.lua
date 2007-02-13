3173131431c6dd3b57013acc911682eb38c5f05b
Wouter Commandeur <wouter@ctlaltdel.nl>
2007-02-13 14:31:07 +0000

    BigWigs: add belowing roar bar and warnings to Nightbane

--- Karazhan/Nightbane.lua
+++ Karazhan/Nightbane.lua
@@ -19,8 +19,15 @@ every 25% he going into the air. one person will get "rain of bones" debuff and 
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Nightbane",
 
+	fear_cmd = "fear",
+	fear_name = "Fear Alert",
+	fear_desc = "Warn for Bellowing Roar",
 
-	fear_trigger = "afflicted by Bellowing Roar",
+	fear_you = "You",
+	fear_trigger = "^([^%s]+) ([^%s]+) afflicted by Bellowing Roar",
+	fear_warn = "AoE Fear - ~30 seconds until next!",
+	fear5sec_warn = "~5 sec until AoE Fear",
+	fear_bar = "AoE Fear", 
 	
 } end )
 
@@ -31,7 +38,7 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsNightbane = BigWigs:NewModule(boss)
 BigWigsNightbane.zonename = AceLibrary("Babble-Zone-2.2")["Karazhan"]
 BigWigsNightbane.enabletrigger = boss
-BigWigsNightbane.toggleoptions = {"bosskill"}
+BigWigsNightbane.toggleoptions = {"fear", "bosskill"}
 BigWigsNightbane.revision = tonumber(string.sub("$Revision$", 12, -3))
 
 ------------------------------
@@ -39,9 +46,17 @@ BigWigsNightbane.revision = tonumber(string.sub("$Revision$", 12, -3))
 ------------------------------
 
 function BigWigsNightbane:OnEnable()
+	prior = nil
+	self:Registerevent("BigWigs_RecvSync")
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "CheckRoar")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "CheckRoar")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "CheckRoar")
+
+	self:TriggerEvent("BigWigs_ThrottleSync", "NightbaneFear", 5)
 end
 
 ------------------------------
@@ -54,5 +69,20 @@ function BigWigsNightbane:BigWigs_RecvSync( sync, rest, nick )
 		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
 			self:UnregisterEvent("PLAYER_REGEN_DISABLED")
 		end
+	elseif sync == "NightbaneFear" and self.db.profile.fear then
+		self:Bar( L["fear_bar"], 30, "Spell_Shadow_PsychicScream" )
+		self:Message( L["fear_warn"], "Important" )
+		self:DelayedMessage( 25, L["fear5sec_warn"], "Urgent" )
 	end
+end
+
+function BigWigsNightbane:CheckRoar( msg )
+	if not prior and msg:find( L["fear_trigger"] ) then
+		self:Sync( "NightbaneFear" )
+		prior = true
+	end
+end
+
+function BigWigsNightbane:BigWigs_Message( text )
+	if text == L["fear5sec_warn"] then prior = nil end
 end
