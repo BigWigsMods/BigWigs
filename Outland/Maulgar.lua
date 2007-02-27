1669019f6f68e3bcc23757eaa75005e0ef9134c4
next96 <next96@users.noreply.wowace.com>
2007-02-27 14:43:39 +0000

    BigWigs: - Add locale koKR

--- Outland/Maulgar.lua
+++ Outland/Maulgar.lua
@@ -1,4 +1,4 @@
-﻿------------------------------
+------------------------------
 --      Are you local?    --
 ------------------------------
 
@@ -149,6 +149,50 @@ L:RegisterTranslations("deDE", function() return {
 	whirlwind_nextbar = "~Naechster Wirbelwind",
 	whirlwind_warning1 = "Raufgar angegriffen - Wirbelwind in ~50sek!",
 	whirlwind_warning2 = "Wirbelwind bald!",
+} end)
+
+L:RegisterTranslations("koKR", function() return {
+	heal_name = "치유",
+	heal_desc = "블라인드아이 치유 시전 경고",
+
+	shield_name = "Shield",
+	shield_desc = "Warn when Blindeye the Seer gains Greater Power Word: Shield",
+
+	spellshield_name = "주문 보호막",
+	spellshield_desc = "크로쉬 주문 보호막 시전 경고",
+
+	summon_name = "사나운 지옥사냥개 소환",
+	summon_desc = "울름 지옥사냥개 소환 경고",
+
+	whirlwind_name = "소용돌이",
+	whirlwind_desc = "마울가르 소용돌이 경고",
+
+	flurry_name = "광폭화",
+	flurry_desc = "마울가르 광폭화 경고",
+
+	heal_trigger = "현자 블라인드아이|1이;가; 치유 시전을 시작합니다.",
+	heal_message = "현자 블라인드아이 치유 시전중!",
+	heal_bar = "치유",
+
+	shield_trigger = "gains Greater Power Word: Shield",
+	shield_message = "Shield on Blindeye!",
+
+	spellshield_trigger = "크로쉬 파이어핸드|1이;가; 주문 보호막 효과를 얻었습니다.",
+	spellshield_message = "크로쉬 주문 보호막!",
+ 
+	summon_trigger = "소환사 올름|1이;가; 사나운 지옥사냥개 소환 시전을 시작합니다.",
+	summon_message = "지옥사냥개 소환!",
+
+	flurry_trigger = "You will not defeat the hand of Gruul!",
+	flurry_message = "50% - 광폭화!",
+	flurry_warning = "곧 광폭화!",
+
+	whirlwind_trigger = "왕중왕 마울가르|1이;가; 소용돌이 효과를 얻었습니다.",
+	whirlwind_message = " 15초간 마울가르 휠윈드!",
+	whirlwind_bar = "소용돌이",
+	whirlwind_nextbar = "다음 소용돌이",
+	whirlwind_warning1 = "마울가르 소용돌이 시전까지 ~50초!",
+	whirlwind_warning2 = "곧 소용돌이!",
 } end)
 
 ----------------------------------
