00e365b22204b773df3c33d55a1f5d1bedf7eb91
funkydude <funkydude@users.noreply.github.com>
2007-03-07 05:42:02 +0000

    BigWigs: SC: finish prep

---
+++ SC/Vashj.lua
@@ -0,0 +1,50 @@
+﻿------------------------------
+--      Are you local?    --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["Lady Vashj"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local started
+
+----------------------------
+--      Localization     --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Vashj",
+} end )
+
+----------------------------------
+--    Module Declaration   --
+----------------------------------
+
+local mod = BigWigs:NewModule(boss)
+mod.zonename = AceLibrary("Babble-Zone-2.2")["Coilfang Reservoir"]
+mod.enabletrigger = boss
+mod.toggleoptions = {"bosskill"}
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function mod:OnEnable()
+	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+	self:RegisterEvent("BigWigs_RecvSync")
+	started = nil
+end
+
+------------------------------
+--    Event Handlers     --
+------------------------------
+
+function mod:BigWigs_RecvSync( sync, rest, nick )
+	if self:ValidateEngageSync(sync, rest) and not started then
+		started = true
+		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
+			self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+		end
+	end
+end
