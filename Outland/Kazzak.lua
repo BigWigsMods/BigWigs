32eb1425b51e2d8cfdeb69c514b47d3b59adc6a4
funkydude <funkydude@users.noreply.github.com>
2007-03-13 00:19:15 +0000

    BigWigs: Outland/Kazzak: - fix enrage, now repeats, clarify 50-60sec - checkforwipe

--- Outland/Kazzak.lua
+++ Outland/Kazzak.lua
@@ -17,11 +17,12 @@ L:RegisterTranslations("enUS", function() return {
 
 	enrage_trigger1 = "For the Legion! For Kil'Jaeden!",
 	enrage_trigger2 = "%s becomes enraged!",
-	enrage_warning1 = "%s Engaged - Enrage in 1min",
-	enrage_warning2 = "Enrage in 5 sec",
-	enrage_message = "Enrage for 10sec!",
-	enrage_finished = "Enrage over",
+	enrage_warning1 = "%s Engaged - Enrage in 50-60sec",
+	enrage_warning2 = "Enrage soon!",
+	enrage_message = "Enraged for 10sec!",
+	enrage_finished = "Enrage over - Next in 50-60sec",
 	enrage_bar = "Enrage",
+	enraged_bar = "<Enraged>",
 } end)
 
 L:RegisterTranslations("frFR", function() return {
@@ -31,10 +32,11 @@ L:RegisterTranslations("frFR", function() return {
 	enrage_trigger1 = "Pour la L\195\169gion\194\160! Pour Kil'Jaeden\194\160!",
 	enrage_trigger2 = "%s devient fou furieux\194\160!",
 	enrage_warning1 = "%s Engag\195\169 - Enrag\195\169 dans 1min",
-	enrage_warning2 = "Enrag\195\169 dans 5 sec",
+	--enrage_warning2 = "Enrag\195\169 dans 5 sec", --enUS changed
 	enrage_message = "Enrag\195\169 pendant 10sec!",
-	enrage_finished = "Enrag\195\169 fini",
+	--enrage_finished = "Enrag\195\169 fini", --enUS changed
 	enrage_bar = "Enrag\195\169",
+	--enraged_bar = "<Enraged>",
 } end)
 
 L:RegisterTranslations("koKR", function() return {
@@ -44,10 +46,11 @@ L:RegisterTranslations("koKR", function() return {
 	enrage_trigger1 = "불타는 군단과 킬제덴을 위하여!",
 	enrage_trigger2 = "%s|1이;가; 분노에 휩싸입니다!",
 	enrage_warning1 = "%s 전투 개시 - 1분 후 격노",
-	enrage_warning2 = "격노 5초 전",
+	--enrage_warning2 = "격노 5초 전", --enUS changed
 	enrage_message = "10초간 격노!",
-	enrage_finished = "격노 종료",
+	--enrage_finished = "격노 종료", --enUS changed
 	enrage_bar = "격노",
+	--enraged_bar = "<Enraged>",
 } end)
 
 ----------------------------------
@@ -69,6 +72,7 @@ function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	self:RegisterEvent("CHAT_MSG_MONSTER_EMOTE")
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 end
 
 ------------------------------
@@ -78,7 +82,7 @@ end
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if self.db.profile.enrage and msg == L["enrage_trigger1"] then
 		self:Message(L["enrage_warning1"]:format(boss), "Attention")
-		self:DelayedMessage(55, L["enrage_warning2"], "Urgent")
+		self:DelayedMessage(49, L["enrage_warning2"], "Urgent")
 		self:Bar(L["enrage_bar"], 60, "Spell_Shadow_UnholyFrenzy")
 	end
 end
@@ -87,6 +91,8 @@ function mod:CHAT_MSG_MONSTER_EMOTE(msg)
 	if self.db.profile.enrage and msg == L["enrage_trigger2"] then
 		self:Message(L["enrage_message"], "Important")
 		self:DelayedMessage(10, L["enrage_finished"], "Positive")
-		self:Bar(L["enrage_bar"], 10, "Spell_Shadow_UnholyFrenzy")
+		self:Bar(L["enraged_bar"], 10, "Spell_Shadow_UnholyFrenzy")
+		self:DelayedMessage(49, L["enrage_warning2"], "Urgent")
+		self:Bar(L["enrage_bar"], 60, "Spell_Shadow_UnholyFrenzy")
 	end
 end
