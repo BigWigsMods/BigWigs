401fd82a043e2681a2b0832545629cb446294bc8
funkydude <funkydude@users.noreply.github.com>
2007-03-15 14:33:44 +0000

    BigWigs: Karazhan/RomuloJulianne: - fix koKR

--- Karazhan/RomuloJulianne.lua
+++ Karazhan/RomuloJulianne.lua
@@ -139,7 +139,7 @@ L:RegisterTranslations("koKR", function() return {
 	poison_message = "중독: %s",
 
 	heal_trigger = "영원한 사랑 시전을 시작합니다.",
-	heal_message = "%s 치유 시전!",
+	heal_message = "%s 힐 시전 중!",
 
 	buff1_trigger = "사랑의 용기 효과를 얻었습니다.", -- check
 	buff1_message = "%s 사랑의 용기 효과 얻음!",
