19967651aa62775516c848d289abb5ffb38ea6a9
Vidar Haarr <vhaarr@gmail.com>
2007-03-27 15:25:59 +0000

    BigWigs: SC: Add some more abilities and timers for Karathress and Morogrim.

--- SC/Karathress.lua
+++ SC/Karathress.lua
@@ -13,26 +13,26 @@ L:RegisterTranslations("enUS", function() return {
 	cmd = "Karathress",
 
 	enrage = "Enrage",
-	enrage_desc = "Enrage Timers",
+	enrage_desc = "Enrage timer.",
 
 	totem = "Spitfire Totem",
-	totem_desc = "Warn for Spitfire Totem's and who cast them",
+	totem_desc = "Warn for Spitfire Totems and who cast them.",
 
-	--heal = "Heal",
-	--heal_desc = "Warn when Caribdis casts a heal",
+	heal = "Heal",
+	heal_desc = "Warn when Caribdis casts a heal.",
 
 	enrage_trigger = "Guards, attention!",
 	enrage_message = "%s engaged, Enrage in 10min",
 	enrage_min = "Enrage in %dmin",
-	enrage_sec = "Enrage in %dsec",
+	enrage_sec = "Enrage in %dsec!",
 
 	totem_trigger1 = "Guard Tidalvess casts Spitfire Totem",
 	totem_trigger2 = "Lord Karathress casts Spitfire Totem",
 	totem_message1 = "Tidalvess: Spitfire Totem",
 	totem_message2 = "Karathress: Spitfire Totem",
 
-	--heal_trigger = "", -need trigger and event name
-	--heal_message = "Caribdis Healing!",
+	heal_trigger = "Caribdis begins to cast Healing Wave",
+	heal_message = "Caribdis casting heal!",
 
 	["Fathom-Guard Sharkkis"] = true, --hunter
 	["Fathom-Guard Tidalvess"] = true, --shaman
@@ -48,7 +48,7 @@ mod.zonename = AceLibrary("Babble-Zone-2.2")["Coilfang Reservoir"]
 mod.otherMenu = "Serpentshrine Cavern"
 mod.enabletrigger = boss
 mod.wipemobs = {L["Fathom-Guard Sharkkis"], L["Fathom-Guard Tidalvess"], L["Fathom-Guard Caribdis"]}
-mod.toggleoptions = {"enrage", "totem", "bosskill"}
+mod.toggleoptions = {"heal", "enrage", "totem", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -58,6 +58,7 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE")
 
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
@@ -81,6 +82,12 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		self:DelayedMessage(590, L["enrage_sec"]:format(10), "Urgent")
 		self:DelayedMessage(600, L["enrage"], "Attention", nil, "Alarm")
 		self:Bar(L["enrage"], 600, "Spell_Shadow_UnholyFrenzy")
+	end
+end
+
+function mod:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE(msg)
+	if msg:find(L["heal_trigger"]) and self.db.profile.heal then
+		self:Message(L["heal_message"], "Important", nil, "Long")
 	end
 end
 
--- SC/Morogrim.lua
+++ SC/Morogrim.lua
@@ -5,7 +5,9 @@
 local boss = AceLibrary("Babble-Boss-2.2")["Morogrim Tidewalker"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 local L2 = AceLibrary("AceLocale-2.2"):new("BigWigsCommonWords")
-local times
+
+local inGrave = {}
+local started = nil
 
 ----------------------------
 --      Localization     --
@@ -14,29 +16,40 @@ local times
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Morogrim",
 
+	enrage = "Enrage",
+	enrage_desc = "Warn for the enrage after 10min.",
+
 	grave = "Watery Grave",
-	grave_desc = "Alert who has watery grave",
+	grave_desc = "Alert who has watery grave.",
 
 	gravealert = "Incoming Watery Grave",
-	gravealert_desc = "Warn for incoming Watery Graves",
+	gravealert_desc = "Warn for incoming Watery Graves.",
 
 	murloc = "Incoming Murlocs",
-	murloc_desc = "Warn for incoming murlocs",
+	murloc_desc = "Warn for incoming murlocs.",
 
 	grobules = "Incoming Grobules",
-	grobules_desc = "Warn for incoming Watery Grobules",
+	grobules_desc = "Warn for incoming Watery Grobules.",
 
 	grave_trigger = "^([^%s]+) ([^%s]+) afflicted by Watery Grave",
 	grave_message = "Watery Grave: %s",
 
 	gravealert_trigger = "sends his enemies",
-	gravealert_message = "Incoming Watery Graves!",
+	gravealert_message = "Watery Graves soon!",
+	grave_bar = "Watery Graves",
 
+	murloc_bar = "Murlocs incoming",
 	murloc_trigger = "Murlocs",
 	murloc_message = "Incoming Murlocs!",
+	murlocs_soon_message = "Murlocs soon!",
 
 	grobules_trigger = "watery grobules",
 	grobules_message = "Incoming Grobules!",
+
+	enrage_message = "%s engaged, enrage in 10min!",
+	enrage_min = "Enrage in %d min",
+	enrage_sec = "Enrage in %d sec!",
+	enrage_bar = "Enrage",
 } end )
 
 ----------------------------------
@@ -47,7 +60,7 @@ local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Coilfang Reservoir"]
 mod.otherMenu = "Serpentshrine Cavern"
 mod.enabletrigger = boss
-mod.toggleoptions = {"grave", "gravealert", "murloc", "grobules", "bosskill"}
+mod.toggleoptions = {"enrage", "grave", "gravealert", "murloc", "grobules", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -55,16 +68,20 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 ------------------------------
 
 function mod:OnEnable()
+	for k in pairs(inGrave) do inGrave[k] = nil end
+	started = nil
+
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event")
 
-	self:RegisterEvent("BigWigs_RecvSync")
-	self:TriggerEvent("BigWigs_ThrottleSync", "MoroGrave", 0)
-	times = {}
+	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 
 	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
+	self:RegisterEvent("BigWigs_RecvSync")
 end
 
 ------------------------------
@@ -74,8 +91,11 @@ end
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	if self.db.profile.gravealert and msg:find(L["gravealert_trigger"]) then
 		self:Message(L["gravealert_message"], "Attention", nil, "Alarm")
+		self:Bar(L["grave_bar"], 30, "Spell_Frost_ArcticWinds")
 	elseif self.db.profile.murloc and msg:find(L["murloc_trigger"]) then
 		self:Message(L["murloc_message"], "Positive")
+		self:Bar(L["murloc_bar"], 60, "INV_Misc_Head_Murloc_01")
+		self:DelayedMessage(55, L["murlocs_soon_message"], "Attention")
 	elseif self.db.profile.grobules and msg:find(L["grobules_trigger"]) then
 		self:Message(L["grobules_message"], "Important", nil, "Alert")
 	end
@@ -87,20 +107,42 @@ function mod:Event(msg)
 		if gplayer == L2["you"] and gtype == L2["are"] then
 			gplayer = UnitName("player")
 		end
-		local t = GetTime()
-		if not times[gplayer] or (times[gplayer] and (times[gplayer] + 5) < t) then
-			self:Sync("MoroGrave "..gplayer)
+		inGrave[#inGrave + 1] = gplayer
+		if #inGrave > 3 then
+			self:GraveWarn()
+		else
+			self:ScheduleEvent("Grave", self.GraveWarn, 0.4)
 		end
 	end
 end
 
+function mod:GraveWarn()
+	if self.db.profile.grave and #inGrave > 0 then
+		self:Message(L["grave_message"]:format(table.concat(inGrave, ",")), "Important", nil, "Alert")
+	end
+	for k in pairs(inGrave) do inGrave[k] = nil end
+end
+
 function mod:BigWigs_RecvSync(sync, rest, nick)
-	if sync == "MoroGrave" and rest and self.db.profile.grave then
-		local t = GetTime()
-		if not times[rest] or (times[rest] and (times[rest] + 5) < t) then
-			self:Message(L["grave_message"]:format(rest), "Urgent")
-			self:Bar(L["grave_message"]:format(rest), 5, "Spell_Shadow_DemonBreath")
-			times[rest] = t
+	if self:ValidateEngageSync(sync, rest) and not started then
+		started = true
+		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
+			self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+		end
+		if self.db.profile.enrage then
+			self:Message(L["enrage_message"]:format(boss), "Important")
+			self:DelayedMessage(300, L["enrage_min"]:format(5), "Positive")
+			self:DelayedMessage(420, L["enrage_min"]:format(3), "Positive")
+			self:DelayedMessage(540, L["enrage_min"]:format(1), "Positive")
+			self:DelayedMessage(570, L["enrage_sec"]:format(30), "Positive")
+			self:DelayedMessage(590, L["enrage_sec"]:format(10), "Urgent")
+			self:DelayedMessage(600, L["enrage"], "Attention", nil, "Alarm")
+			self:Bar(L["enrage_bar"], 600, "Spell_Shadow_UnholyFrenzy")
+		end
+		if self.db.profile.murloc then
+			self:Bar(L["murloc_bar"], 60, "INV_Misc_Head_Murloc_01")
+			self:DelayedMessage(55, L["murlocs_soon_message"], "Attention")
 		end
 	end
 end
+
