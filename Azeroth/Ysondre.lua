655873675273ad1d6b252056ce981f0e2cc3eb17
funkydude <funkydude@users.noreply.github.com>
2007-04-05 16:19:03 +0000

    BigWigs: Azeroth/Ysondre: - warn for druids - scan for resists of noxious breath - sync

--- Azeroth/Ysondre.lua
+++ Azeroth/Ysondre.lua
@@ -16,13 +16,20 @@ L:RegisterTranslations("enUS", function() return {
 	engage = "Engage Alert",
 	engage_desc = "Warn when Ysondre is engaged",
 
+	druids = "Druids Alert",
+	druids_desc = "Warn for incoming druids",
+
 	noxious = "Noxious breath alert",
 	noxious_desc = "Warn for noxious breath",
 
 	engage_message = "Ysondre Engaged! - Noxious Breath in ~10seconds",
 	engage_trigger = "The strands of LIFE have been severed! The Dreamers must be avenged!",
 
-	noxious_trigger = "afflicted by Noxious Breath",
+	druids_trigger = "Come forth, ye Dreamers - and claim your vengeance!",
+	druids_message = "Incoming Druids!",
+
+	noxious_hit = "afflicted by Noxious Breath",
+	noxious_resist = "Noxious Breath was resisted",
 	noxious_warn = "5 seconds until Noxious Breath!",
 	noxious_message = "Noxious Breath - 30 seconds till next!",
 	noxious_bar = "Noxious Breath",
@@ -54,8 +61,8 @@ local mod = BigWigs:NewModule(boss)
 mod.zonename = {BZ["Ashenvale"], BZ["Duskwood"], BZ["The Hinterlands"], BZ["Feralas"]}
 mod.otherMenu = "Azeroth"
 mod.enabletrigger = boss
-mod.toggleoptions = {"engage", -1, "noxious", "bosskill"}
-mod.revision = tonumber(string.sub("$Revision$", 12, -3))
+mod.toggleoptions = {"engage", -1, "druids", "noxious", "bosskill"}
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
 --      Initialization      --
@@ -64,30 +71,45 @@ mod.revision = tonumber(string.sub("$Revision$", 12, -3))
 function mod:OnEnable()
 	self:RegisterEvent("BigWigs_Message")
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event")
-	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE", "Event")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Event")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "Event")
+
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "YsoNox", 10)
 end
 
-function mod:Event( msg )
-	if (not self.prior and msg:find(L["noxious_trigger"])) then
-		self.prior = true
-		if self.db.profile.noxious then 
-			self:Message( L["noxious_message"], "Important")
-			self:DelayedMessage( 25, L["noxious_warn"], "Important")
-			self:Bar(L["noxious_bar"], 30, "Spell_Shadow_LifeDrain02")
-		end
+function mod:Event(msg)
+	if msg:find(L["noxious_hit"]) or msg:find(L["noxious_resist"]) then
+		self:Sync("YsoNox")
 	end
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if self.db.profile.engage and msg == L["engage_trigger"] then
-		self:Message(L["engage_message"], "Important")
+		self:Message(L["engage_message"], "Attention")
 		self:Bar( L["noxious_bar"], 10, "Spell_Shadow_LifeDrain02")
+	elseif self.db.profile.druids and msg == L["druids_trigger"] then
+		self:Message(L["druids_message"], "Positive")
 	end
 end
 
 function mod:BigWigs_Message(text)
-	if text == L["noxious_warn"] then self.prior = nil end
+	if text == L["noxious_warn"] then
+		self.prior = nil
+	end
+end
+
+function mod:BigWigs_RecvSync(sync)
+	if sync == "YsoNox" and not self.prior and self.db.profile.noxious then
+		self:Message(L["noxious_message"], "Important")
+		self:DelayedMessage(25, L["noxious_warn"], "Urgent")
+		self:Bar(L["noxious_bar"], 30, "Spell_Shadow_LifeDrain02")
+		self.prior = true
+	end
 end
