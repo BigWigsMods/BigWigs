9dfb0cbb11a2fc9a971a9cd1166ae83ed2d514fd
Vidar Haarr <vhaarr@gmail.com>
2007-04-18 00:23:35 +0000

    BigWigs: Comm: Fix synchronization for 5-mans.

--- Plugins/Comm.lua
+++ Plugins/Comm.lua
@@ -41,7 +41,7 @@ end
 ------------------------------
 
 function plugin:CHAT_MSG_ADDON(prefix, message, type, sender)
-	if prefix ~= "BigWigs" or type ~= "RAID" then return end
+	if prefix ~= "BigWigs" then return end
 
 	local sync, rest = select(3, message:find("(%S+)%s*(.*)$"))
 	if not sync then return end
