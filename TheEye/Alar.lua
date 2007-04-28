c0d536bc69cf99e342d6e403a3cc9f4cd1c743e5
funkydude <funkydude@users.noreply.github.com>
2007-04-28 06:10:35 +0000

    BigWigs: TheEye/Alar: sync rebirth, remove wing buffet code

--- TheEye/Alar.lua
+++ TheEye/Alar.lua
@@ -1,13 +1,12 @@
 ﻿------------------------------
---      Are you local?    --
+--      Are you local?      --
 ------------------------------
 
 local boss = AceLibrary("Babble-Boss-2.2")["Al'ar"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
---local buffet
 
 ----------------------------
---      Localization     --
+--      Localization      --
 ----------------------------
 
 L:RegisterTranslations("enUS", function() return {
@@ -16,27 +15,19 @@ L:RegisterTranslations("enUS", function() return {
 	rebirth = "Rebirth",
 	rebirth_desc = "Alert when Al'ar casts rebirth",
 
-	--buffet = "Flame Buffet",
-	--buffet_desc = "Approximate Flame Buffet Timers",
-
 	rebirth_trigger = "Al'ar begins to cast Rebirth.",
 	rebirth_message = "Rebirth!",
-
-	--buffet_trigger = "afflicted by Flame Buffet",
-	--buffet_message = "Flame Buffet! Next in ~40sec",
-	--buffet_warning = "Flame Buffet Soon",
-	--buffet_bar = "Flame Buffet",
 } end )
 
 ----------------------------------
---    Module Declaration   --
+--      Module Declaration      --
 ----------------------------------
 
 local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Tempest Keep"]
 mod.otherMenu = "The Eye"
 mod.enabletrigger = boss
-mod.toggleoptions = {"rebirth", "bosskill"} --buffet
+mod.toggleoptions = {"rebirth", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -45,39 +36,25 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE")
-
-	--self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event")
-	--self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event")
-	--self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event")
-
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
-	--self:RegisterEvent("BigWigs_Message")
-	--buffet = nil
+
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "AlArRebirth", 5)
 end
 
 ------------------------------
---    Event Handlers     --
+--      Event Handlers      --
 ------------------------------
 
 function mod:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE(msg)
-	if self.db.profile.rebirth and msg == L["rebirth_trigger"] then
+	if msg == L["rebirth_trigger"] then
+		self:Sync("AlArRebirth")
+	end
+end
+
+function mod:BigWigs_RecvSync(sync)
+	if sync == "AlArRebirth" and self.db.profile.rebirth then
 		self:Message(L["rebirth_message"], "Urgent", nil, "Alarm")
 		self:Bar(L["rebirth_message"], 2, "Spell_Fire_Burnout")
 	end
 end
---[[
-function mod:Event(msg)
-	if not buffet and self.db.profile.buffet and msg:find(L["buffet_trigger"]) then
-		self:Message(L["buffet_message"], "Attention")
-		self:DelayedMessage(36, L["buffet_warning"], "Positive")
-		self:Bar(L["buffet_bar"], 40, "Spell_Fire_Fireball")
-		buffet = true
-	end
-end
-
-function mod:BigWigs_Message(text)
-	if text == L["buffet_warning"] then
-		buffet = nil
-	end
-end
-]]
