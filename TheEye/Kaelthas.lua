877520f22e6ca171e8886dfbc0ca6389a4fe855c
Vidar Haarr <vhaarr@gmail.com>
2007-04-30 18:53:44 +0000

    BigWigs: Added skeletal module for Kael'Thas - no warnings or timers, only bosskill.

---
+++ TheEye/Kaelthas.lua
@@ -0,0 +1,48 @@
+------------------------------
+--      Are you local?      --
+------------------------------
+
+local BB = AceLibrary("Babble-Boss-2.2")
+
+local boss = BB["Kael'thas Sunstrider"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local L2 = AceLibrary("AceLocale-2.2"):new("BigWigsCommonWords")
+
+local capernian = BB["Grand Astromancer Capernian"]
+local sanguinar = BB["Lord Sanguinar"]
+local telonicus = BB["Master Engineer Telonicus"]
+local thaladred = BB["Thaladred the Darkener"]
+
+BB = nil
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Kael'thas",
+} end )
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+local mod = BigWigs:NewModule(boss)
+mod.zonename = AceLibrary("Babble-Zone-2.2")["Tempest Keep"]
+mod.otherMenu = "The Eye"
+mod.enabletrigger = { boss, capernian, sanguinar, telonicus, thaladred }
+mod.toggleoptions = { "bosskill" }
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function mod:OnEnable()
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
