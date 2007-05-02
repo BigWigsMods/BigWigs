2a9645a50f7f40233bd01a70959a490254583bb5
Vidar Haarr <vhaarr@gmail.com>
2007-05-02 14:50:38 +0000

    BigWigs: Clean out the old Azeroth instances.

--- bigwigslod.bat
+++ bigwigslod.bat
@@ -1,14 +1,7 @@
 @echo off
 
 cd ..
-rmdir /s /q BigWigs_AQ20
-rmdir /s /q BigWigs_AQ40
-rmdir /s /q BigWigs_Azeroth
-rmdir /s /q BigWigs_BWL
-rmdir /s /q BigWigs_MC
-rmdir /s /q BigWigs_ZG
 rmdir /s /q BigWigs_SC
-rmdir /s /q BigWigs_Naxxramas
 rmdir /s /q BigWigs_Karazhan
 rmdir /s /q BigWigs_Outland
 rmdir /s /q BigWigs_Extras
@@ -19,15 +12,8 @@ rmdir /s /q BigWigs_Other
 
 cd BigWigs
 
-move AQ20 ..\BigWigs_AQ20
-move AQ40 ..\BigWigs_AQ40
-move BWL ..\BigWigs_BWL
-move MC ..\BigWigs_MC
-move Naxxramas ..\BigWigs_Naxxramas
 move Karazhan ..\BigWigs_Karazhan
-move ZG ..\BigWigs_ZG
 move SC ..\BigWigs_SC
-move Azeroth ..\BigWigs_Azeroth
 move Outland ..\BigWigs_Outland
 move Extras ..\BigWigs_Extras
 move Plugins ..\BigWigs_Plugins
--- bigwigslod.sh
+++ bigwigslod.sh
@@ -1,14 +1,7 @@
 #/bin/bash
 
 cd ..
-rm -rf BigWigs_AQ20
-rm -rf BigWigs_AQ40
-rm -rf BigWigs_Azeroth
-rm -rf BigWigs_BWL
-rm -rf BigWigs_MC
-rm -rf BigWigs_ZG
 rm -rf BigWigs_SC
-rm -rf BigWigs_Naxxramas
 rm -rf BigWigs_Karazhan
 rm -rf BigWigs_Outland
 rm -rf BigWigs_Extras
@@ -19,15 +12,8 @@ rm -rf BigWigs_Other
 
 cd BigWigs
 
-mv AQ20 ../BigWigs_AQ20
-mv AQ40 ../BigWigs_AQ40
-mv BWL ../BigWigs_BWL
-mv MC ../BigWigs_MC
-mv Naxxramas ../BigWigs_Naxxramas
 mv Karazhan ../BigWigs_Karazhan
-mv ZG ../BigWigs_ZG
 mv SC ../BigWigs_SC
-mv Azeroth ../BigWigs_Azeroth
 mv Outland ../BigWigs_Outland
 mv Extras ../BigWigs_Extras
 mv Plugins ../BigWigs_Plugins
