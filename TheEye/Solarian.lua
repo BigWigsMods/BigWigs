b572e8f1e32e86205e57404cb5e0175d657418ae
funkydude <funkydude@users.noreply.github.com>
2007-05-15 00:11:44 +0000

    BigWigs: TheEye/Solarian: - add warning for when wrath is casted - do a target scan to provide an early warning of who wrath is casted on

--- TheEye/Solarian.lua
+++ TheEye/Solarian.lua
@@ -20,6 +20,9 @@ L:RegisterTranslations("enUS", function() return {
 	phase = "Phase",
 	phase_desc = "Warn for phase changes",
 
+	wrath = "Wrath Cast",
+	wrath_desc = "Warn when Wrath is being cast, and the current target",
+
 	wrathyou = "Wrath Debuff on You",
 	wrathyou_desc = "Warn when you have Wrath of the Astromancer",
 
@@ -45,6 +48,8 @@ L:RegisterTranslations("enUS", function() return {
 	wrath_trigger = "^([^%s]+) ([^%s]+) afflicted by Wrath of the Astromancer",
 	wrath_other = "Wrath: %s",
 	wrath_you = "Wrath on YOU!",
+	wrath_alert = "Casting Wrath! - Target: %s",
+	wrath_alert_trigger = "begins to cast Wrath of the Astromancer.$",
 
 	agent_warning = "Split! - Agents in 6 sec",
 	agent_bar = "Agents",
@@ -145,7 +150,7 @@ mod.zonename = AceLibrary("Babble-Zone-2.2")["Tempest Keep"]
 mod.otherMenu = "The Eye"
 mod.enabletrigger = boss
 mod.wipemobs = {L["Solarium Priest"], L["Solarium Agent"]}
-mod.toggleoptions = {"phase", "split", -1, "wrathyou", "wrathother", "icon", "proximity", "bosskill"}
+mod.toggleoptions = {"phase", "split", -1, "wrath", "wrathyou", "wrathother", "icon", "proximity", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 mod.proximityCheck = function( unit ) return CheckInteractDistance( unit, 4 ) end
 
@@ -158,6 +163,7 @@ function mod:OnEnable()
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE")
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "debuff")
@@ -167,6 +173,7 @@ function mod:OnEnable()
 	self:RegisterEvent("UNIT_HEALTH")
 	self:RegisterEvent("BigWigs_RecvSync")
 	self:TriggerEvent("BigWigs_ThrottleSync", "SolaWrath", 1)
+	self:TriggerEvent("BigWigs_ThrottleSync", "SolaWCast", 5)
 	self:TriggerEvent("BigWigs_ThrottleSync", "SolaSplit", 6)
 
 	p1 = nil
@@ -201,6 +208,9 @@ function mod:BigWigs_RecvSync(sync, rest, nick)
 		if self.db.profile.icon then
 			self:Icon(rest)
 		end
+	elseif sync == "SolaWCast" and self.db.profile.wrath then
+		self:Bar(L["wrath"], 3, "Spell_Arcane_Arcane02")
+		self:ScheduleEvent("BWSolaToTScan", self.WrathCheck, 1, self) --target scan once, after 1 second
 	elseif sync == "SolaSplit" and self.db.profile.split then
 		--split is around 90 seconds after the previous
 		self:Bar(L["split_bar"], 90, "Spell_Shadow_SealOfKings")
@@ -220,6 +230,12 @@ function mod:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF(msg)
 	if msg:find(L["split_trigger"]) and (GetTime() - split > 1) then
 		split = GetTime()
 		self:Sync("SolaSplit")
+	end
+end
+
+function mod:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE(msg)
+	if msg:find(L["wrath_alert_trigger"]) then
+		self:Sync("SolaWCast")
 	end
 end
 
@@ -257,4 +273,20 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		self:CancelScheduledEvent("split1")
 		self:TriggerEvent("BigWigs_StopBar", self, L["split_bar"])
 	end
+end
+
+--Wrath is usually around a 3 second cast, and she takes a target, so we scan her target to try give some early advantage :)
+function mod:WrathCheck()
+	local target
+	if UnitName("target") == boss then
+		target = UnitName("targettarget")
+	else
+		for i = 1, GetNumRaidMembers() do
+			if UnitName("raid"..i.."target") == boss then
+				target = UnitName("raid"..i.."targettarget")
+				break
+			end
+		end
+	end
+	self:Message(L["wrath_alert"]:format(target), "Attention")
 end
