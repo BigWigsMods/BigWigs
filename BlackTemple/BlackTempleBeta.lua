1f6fdd82c0f3caa9dd05c3336407805680a1eabc
funkydude <funkydude@users.noreply.github.com>
2007-05-15 01:27:35 +0000

    BigWigs: - TheEye, Remove Beta, toc change - SC, toc change - BlackTemple & Hyjal beta, ask for BigWigs_Debugger

--- BlackTemple/BlackTempleBeta.lua
+++ BlackTemple/BlackTempleBeta.lua
@@ -13,12 +13,12 @@ end
 function BlackTempleBeta:ZONE_CHANGED_NEW_AREA()
 	if GetRealZoneText() ~= BZ["Black Temple"] or warnedThisSession then return end
 	warnedThisSession = true
-	if not IsAddOnLoaded("Transcriptor") then
+	if not IsAddOnLoaded("BigWigs_Debugger") then
 		self:Print("The BigWigs boss modules for The Black Temple are not complete yet, by far, and we need your help.")
-		self:Print("Please download and install Transcriptor from http://files.wowace.com/Transcriptor/")
+		self:Print("Please download and install BigWigs_Debugger from http://files.wowace.com/BigWigs_Debugger/")
 		self:Print("It records what happens during an encounter, and you can then post the log on our forums.")
 		self:Print("Thanks!")
 	else
-		self:Print("Thanks for running Transcriptor! Please remember to click the icon so it glows red.")
+		self:Print("Thanks for running BigWigs_Debugger! Please remember to click the icon so it glows red.")
 	end
 end
