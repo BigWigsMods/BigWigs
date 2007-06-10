cbb825e2f2146c4bb8396e3131808db98e30a4be
funkydude <funkydude@users.noreply.github.com>
2007-06-10 01:41:34 +0000

    BigWigs: BlackTemple: add Teron Gorefiend mod

---
+++ BlackTemple/Teron.lua
@@ -0,0 +1,82 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["Teron Gorefiend"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local L2 = AceLibrary("AceLocale-2.2"):new("BigWigsCommonWords")
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Teron",
+
+	start_trigger = "Vengeance is mine!",
+
+	shadow = "Shadow of Death",
+	shadow_desc = "Tells you who has Shadow of Death.",
+	shadow_trigger = "^([^%s]+) ([^%s]+) afflicted by Shadow of Death.$",
+	shadow_other = "Shadow of Death: %s!",
+	shadow_you = "Shadow of Death on YOU!",
+
+	icon = "Raid Icon",
+	icon_desc = "Place a Raid Icon on players with Shadow of Death.",
+} end )
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+local mod = BigWigs:NewModule(boss)
+mod.zonename = AceLibrary("Babble-Zone-2.2")["Black Temple"]
+mod.enabletrigger = boss
+mod.toggleoptions = {"shadow", "icon", "bosskill"}
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function mod:OnEnable()
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "death")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "death")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "death")
+
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "TeronShadow", 3)
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function mod:death(msg)
+	local splayer, stype = select(3, msg:find(L["shadow_trigger"]))
+	if splayer and stype then
+		if splayer == L2["you"] and stype == L2["are"] then
+			splayer = UnitName("player")
+		end
+		self:Sync("TeronShadow " .. splayer)
+	end
+end
+
+function mod:BigWigs_RecvSync(sync, rest, nick)
+	if sync == "TeronShadow" and rest and self.db.profile.shadow then
+		local other = L["shadow_other"]:format(rest)
+		if rest == UnitName("player") then
+			self:Message(L["shadow_you"], "Personal", true, "Long")
+			self:Message(other, "Attention", nil, nil, true)
+			self:Bar(other, 55, "Spell_Arcane_PrismaticCloak")
+		else
+			self:Message(other, "Attention")
+			self:Bar(other, 55, "Spell_Arcane_PrismaticCloak")
+		end
+		if self.db.profile.icon then
+			self:Icon(rest)
+		end
+	end
+end
