312f28598591789643f7be9075cf339b43e49605
funkydude <funkydude@users.noreply.github.com>
2007-06-20 04:50:09 +0000

    BigWigs: BlackTemple: add Illidan mod

---
+++ BlackTemple/Illidan.lua
@@ -0,0 +1,80 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["Illidan Stormrage"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local L2 = AceLibrary("AceLocale-2.2"):new("BigWigsCommonWords")
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Illidan",
+
+	parasite = "Parasitic Shadowfiend",
+	parasite_desc = "Warn who has Parasitic Shadowfiend.",
+	parasite_trigger = "^([^%s]+) ([^%s]+) afflicted by Parasitic Shadowfiend.$",
+	parasite_you = "You have a Parasite!",
+	parasite_other = "%s has a Parasite!",
+
+	icon = "Raid Icon",
+	icon_desc = "Place a Raid Icon on the player with Parasitic Shadowfiend.",
+} end )
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+local mod = BigWigs:NewModule(boss)
+mod.zonename = AceLibrary("Babble-Zone-2.2")["Black Temple"]
+mod.enabletrigger = boss
+mod.toggleoptions = {"parasite", "bosskill"}
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function mod:OnEnable()
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "IliPara", 2)
+
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "parasite")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "parasite")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "parasite")
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function mod:BigWigs_RecvSync(sync, rest, nick)
+	if sync == "IliPara" and rest and self.db.profile.parasite then 
+		local other = L["parasite_other"]:format(rest)
+		if rest == UnitName("player") then
+			self:Message(L["parasite_you"], "Personal", true, "Long")
+			self:Message(other, "Attention", nil, nil, true)
+			self:Bar(other, 10, "Spell_Shadow_SoulLeech_3")
+		else
+			self:Message(other, "Attention")
+			self:Bar(other, 10, "Spell_Shadow_SoulLeech_3")
+		end
+		if self.db.profile.icon then
+			self:Icon(rest)
+		end
+	end
+end
+
+function mod:parasite(msg)
+	local pplayer, ptype = select(3, msg:find(L["parasite_trigger"]))
+	if pplayer and ptype then
+		if pplayer == L2["you"] and ptype == L2["are"] then
+			pplayer = UnitName("player")
+		end
+		self:Sync("IliPara "..pplayer)
+	end
+end
