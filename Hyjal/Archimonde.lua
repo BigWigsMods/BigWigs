6d2d1e34c1440d3c13e5bdb058553ffc7592b6d3
funkydude <funkydude@users.noreply.github.com>
2007-06-21 22:50:46 +0000

    BigWigs: Hyjal/Archimonde: add fear alert

--- Hyjal/Archimonde.lua
+++ Hyjal/Archimonde.lua
@@ -23,6 +23,12 @@ L:RegisterTranslations("enUS", function() return {
 
 	icon = "Raid Icon",
 	icon_desc = "Place a Raid Icon on the player with Grip of the Legion.",
+
+	fear = "Fear",
+	fear_desc = "Fear Timers.",
+	fear_message = "Fear! - Next in ~30sec",
+	fear_bar = "~Fear Cooldown",
+	fear_warning = "Fear Soon!",
 } end )
 
 ----------------------------------
@@ -32,7 +38,7 @@ L:RegisterTranslations("enUS", function() return {
 local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Hyjal Summit"]
 mod.enabletrigger = boss
-mod.toggleoptions = {"grip", "icon", "bosskill"}
+mod.toggleoptions = {"grip", "icon", "fear", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -43,10 +49,13 @@ function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 	self:RegisterEvent("BigWigs_RecvSync")
 	self:TriggerEvent("BigWigs_ThrottleSync", "ArchGrip", 2)
+	self:TriggerEvent("BigWigs_ThrottleSync", "ArchFear", 5)
 
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "grip")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "grip")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "grip")
+	self:RegisterEvent("UNIT_SPELLCAST_START")
+
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "GripEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "GripEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "GripEvent")
 end
 
 ------------------------------
@@ -67,15 +76,25 @@ function mod:BigWigs_RecvSync(sync, rest, nick)
 		if self.db.profile.icon then
 			self:Icon(rest)
 		end
+	elseif sync == "ArchFear" and self.db.profile.fear then
+		self:Bar(L["fear_bar"], 30, "Spell_Shadow_DeathScream")
+		self:Message(L["fear_message"], "Important")
+		self:DelayedMessage(25, L["fear_warning"], "Urgent")
 	end
 end
 
-function mod:grip(msg)
+function mod:GripEvent(msg)
 	local gplayer, gtype = select(3, msg:find(L["grip_trigger"]))
 	if gplayer and gtype then
 		if gplayer == L2["you"] and gtype == L2["are"] then
 			gplayer = UnitName("player")
 		end
 		self:Sync("ArchGrip "..gplayer)
+	end
+end
+
+function mod:UNIT_SPELLCAST_START(msg)
+	if UnitName(msg) == boss and (UnitCastingInfo(msg)) == L["fear"] then
+		self:Sync("ArchFear")
 	end
 end
