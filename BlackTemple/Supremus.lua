f85479706a2c1958e8275851211ae359e4206d76
funkydude <funkydude@users.noreply.github.com>
2007-07-09 21:03:03 +0000

    BigWigs: BlackTemple/Supremus: bugfix target scan

--- BlackTemple/Supremus.lua
+++ BlackTemple/Supremus.lua
@@ -19,28 +19,27 @@ L:RegisterTranslations("enUS", function() return {
 
 	phase = "Phases",
 	phase_desc = "Warn about the different phases.",
-
-	punch = "Molten Punch",
-	punch_desc = "Alert when he does Molten Punch, and display a countdown bar.",
-
-	target = "Target",
-	target_desc = "Warn who he targets during the kite phase, and put a raid icon on them.",
-
 	normal_phase_message = "Tank'n'spank!",
 	normal_phase_trigger = "Supremus punches the ground in anger!",
-
-	kite_phase_message = "Supremus loose!",
+	kite_phase_message = "%s loose!",
 	kite_phase_trigger = "The ground begins to crack open!",
-
 	next_phase_bar = "Next phase",
 	next_phase_message = "Phase change in 10sec!",
 
+	punch = "Molten Punch",
+	punch_desc = "Alert when he does Molten Punch, and display a countdown bar.",
+	punch_message = "Molten Punch!",
+	punch_bar = "~Possible Punch!",
+	punch_trigger = "Supremus casts Molten Punch.",
+
+	target = "Target",
+	target_desc = "Warn who he targets during the kite phase, and put a raid icon on them.",
 	target_trigger = "Supremus acquires a new target!",
 	target_message = "%s being chased!",
 	target_message_nounit = "New target!",
 
-	punch_message = "Punch!",
-	punch_trigger = "Supremus casts Molten Punch.",
+	icon = "Raid Target Icon",
+	icon_desc = "Place a Raid Target Icon on the player being chased(requires promoted or higher).",
 } end )
 
 L:RegisterTranslations("deDE", function() return {
@@ -56,7 +55,7 @@ L:RegisterTranslations("deDE", function() return {
 	normal_phase_message = "Tank'n'spank!",
 	normal_phase_trigger = "Supremus schlägt wütend auf den Boden!",
 
-	kite_phase_message = "Kitephase!",
+	kite_phase_message = "%s Kitephase!",
 	kite_phase_trigger = "Der Boden beginnt aufzubrechen!",
 
 	next_phase_bar = "Nächste Phase",
@@ -83,7 +82,7 @@ L:RegisterTranslations("koKR", function() return {
 	normal_phase_message = "탱킹'n'딜링!",
 	normal_phase_trigger =  "궁극의 심연이 분노하여 땅을 내리찍습니다!",
 
-	kite_phase_message = "궁극의 심연 풀려남!",
+	kite_phase_message = "%s 풀려남!",
 	kite_phase_trigger = "땅이 갈라져서 열리기 시작합니다!",
 
 	next_phase_bar = "다음 형상",
@@ -104,7 +103,7 @@ L:RegisterTranslations("koKR", function() return {
 local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Black Temple"]
 mod.enabletrigger = boss
-mod.toggleoptions = { "punch", "target", "phase", "enrage", "bosskill" }
+mod.toggleoptions = { "punch", "target", "icon", "phase", "enrage", "bosskill" }
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -135,18 +134,25 @@ function mod:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF(msg)
 	end
 end
 
-local function findTarget()
+function mod:TargetCheck()
+	local target
 	if UnitName("target") == boss then
-		return UnitName("targettarget")
+		target = UnitName("targettarget")
 	elseif UnitName("focus") == boss then
-		return UnitName("focustarget")
+		target = UnitName("focustarget")
 	else
 		local num = GetNumRaidMembers()
 		for i = 1, num do
-			local unit = ("raid%starget"):format(i)
-			if UnitExists(unit) and UnitName(unit) == boss then
-				return UnitName(unit .. "target")
+			if UnitName("raid"..i.."target") == boss then
+				target = UnitName("raid"..i.."targettarget")
+				break
 			end
+		end
+	end
+	if target then
+		self:Message(L["target_message"]:format(target), "Attention")
+		if self.db.profile.icon then
+			self:Icon(target)
 		end
 	end
 end
@@ -157,17 +163,11 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 		self:Bar(L["next_phase_bar"], 60, "INV_Helmet_08")
 		self:DelayedMessage(50, L["next_phase_message"], "Attention")
 	elseif msg == L["kite_phase_trigger"] and self.db.profile.phase then
-		self:Message(L["kite_phase_message"], "Positive")
+		self:Message(L["kite_phase_message"]:format(boss), "Positive")
 		self:Bar(L["next_phase_bar"], 60, "Spell_Fire_MoltenBlood")
 		self:DelayedMessage(50, L["next_phase_message"], "Attention")
 	elseif msg == L["target_trigger"] and self.db.profile.target then
-		local name = findTarget()
-		if name then
-			self:Message(L["target_message"]:format(name), "Important")
-		else
-			self:Message(L["target_message_nounit"], "Important")
-		end
-		self:Icon(name)
+		self:ScheduleEvent("BWToTScan", self.TargetCheck, 0.5, self)
 	end
 end
 
@@ -175,7 +175,7 @@ function mod:BigWigs_RecvSync(sync, rest, nick)
 	if sync == "SupPunch" then
 		if not self.db.profile.punch then return end
 		self:Message(L["punch_message"], "Attention")
-		self:Bar(L["punch_message"], 10, "Spell_Frost_FreezingBreath")
+		self:Bar(L["punch_bar"], 10, "Spell_Frost_FreezingBreath")
 	elseif self:ValidateEngageSync(sync, rest) and not started then
 		started = true
 		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
