91c55ddb39e51dfefe4c7aeaf84055228e8ceb23
funkydude <funkydude@users.noreply.github.com>
2007-07-11 18:20:40 +0000

    BigWigs: BlackTemple/Illidan: add eyeblast and barrage warnings

--- BlackTemple/Illidan.lua
+++ BlackTemple/Illidan.lua
@@ -5,6 +5,7 @@
 local boss = AceLibrary("Babble-Boss-2.2")["Illidan Stormrage"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 local L2 = AceLibrary("AceLocale-2.2"):new("BigWigsCommonWords")
+local pName = nil
 
 ----------------------------
 --      Localization      --
@@ -21,6 +22,16 @@ L:RegisterTranslations("enUS", function() return {
 
 	icon = "Raid Icon",
 	icon_desc = "Place a Raid Icon on the player with Parasitic Shadowfiend.",
+
+	barrage = "Dark Barrage",
+	barrage_desc = "Warn who has Dark Barrage.",
+	barrage_trigger = "^([^%s]+) ([^%s]+) afflicted by Dark Barrage%.$",
+	barrage_message = "%s is being Barraged!",
+
+	eyeblast = "Eye Blast",
+	eyeblast_desc = "Warn when Eye Blast is cast.",
+	eyeblast_trigger = "Stare into the eyes of the Betrayer!",
+	eyeblast_message = "Eye Blast!",
 } end )
 
 ----------------------------------
@@ -30,7 +41,7 @@ L:RegisterTranslations("enUS", function() return {
 local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Black Temple"]
 mod.enabletrigger = boss
-mod.toggleoptions = {"parasite", "bosskill"}
+mod.toggleoptions = {"parasite", "eyeblast", "barrage", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -42,10 +53,14 @@ function mod:OnEnable()
 
 	self:RegisterEvent("BigWigs_RecvSync")
 	self:TriggerEvent("BigWigs_ThrottleSync", "IliPara", 2)
+	self:TriggerEvent("BigWigs_ThrottleSync", "IliBara", 2)
 
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "parasite")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "parasite")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "parasite")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "AfflictEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "AfflictEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "AfflictEvent")
+
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	pName = UnitName("player")
 end
 
 ------------------------------
@@ -66,15 +81,31 @@ function mod:BigWigs_RecvSync(sync, rest, nick)
 		if self.db.profile.icon then
 			self:Icon(rest)
 		end
+	elseif sync == "IliBara" and rest and self.db.profile.barrage then
+		self:Message(L["barrage_message"]:format(rest), "Important", nil, "Alert")
 	end
 end
 
-function mod:parasite(msg)
+function mod:AfflictEvent(msg)
 	local pplayer, ptype = select(3, msg:find(L["parasite_trigger"]))
 	if pplayer and ptype then
 		if pplayer == L2["you"] and ptype == L2["are"] then
-			pplayer = UnitName("player")
+			pplayer = pName
 		end
 		self:Sync("IliPara "..pplayer)
+	end
+
+	local bplayer, btype = select(3, msg:find(L["barrage_trigger"]))
+	if bplayer and btype then
+		if bplayer == L2["you"] and btype == L2["are"] then
+			bplayer = pName
+		end
+		self:Sync("IliBara "..bplayer)
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L["eyeblast_trigger"] then
+		self:Message(L["eyeblast_message"], "Important", nil, "Alert")
 	end
 end
