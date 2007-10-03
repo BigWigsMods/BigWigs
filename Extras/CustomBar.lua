1e78c4db9c8a1e016beaff69a92cdb667c328116
Vidar Haarr <vhaarr@gmail.com>
2007-10-03 15:36:53 +0000

    BigWigs: CustomBar: Support more time formats, including "M:S" and "Mm".

--- Extras/CustomBar.lua
+++ Extras/CustomBar.lua
@@ -17,9 +17,10 @@ L:RegisterTranslations("enUS", function() return {
 	["Start a custom bar, either local or global."] = true,
 	["Local"] = true,
 	["Global"] = true,
-	["<seconds> <bar text>"] = true,
+	["<time> <bar text>"] = true,
 	["Starts a custom bar with the given parameters."] = true,
 	["%s: Timer [%s] finished."] = true,
+	["Invalid time (|cffff0000%q|r) or missing bar text in a custom bar started by |cffd9d919%s|r. <time> can be either a number in seconds, a M:S pair, or Mm. For example 5, 1:20 or 2m."] = true,
 } end)
 
 L:RegisterTranslations("koKR", function() return {
@@ -27,7 +28,7 @@ L:RegisterTranslations("koKR", function() return {
 	["Start a custom bar, either local or global."] = "글로벌 혹은 로컬 사용자 바를 시작합니다.",
 	["Local"] = "로컬",
 	["Global"] = "글로벌",
-	["<seconds> <bar text>"] = "<초> <바 텍스트>",
+	["<time> <bar text>"] = "<초> <바 텍스트>",
 	["Starts a custom bar with the given parameters."] = "입력한 매개변수로 사용자 바를 시작합니다.",
 	["%s: Timer [%s] finished."] = "%s: [%s] 타이머가 종료되었습니다.",
 } end)
@@ -42,7 +43,7 @@ L:RegisterTranslations("zhCN", function() return {
 	["Start a custom bar, either local or global."] = "启动自定义时间条,本地或全局",
 	["Local"] = "本地",
 	["Global"] = "全局",
-	["<seconds> <bar text>"] = "<秒> <计时条名字>",
+	["<time> <bar text>"] = "<秒> <计时条名字>",
 	["Starts a custom bar with the given parameters."] = "启动自定义时间条",
 	["%s: Timer [%s] finished."] = "%s: 计时器 [%s] 到时间.",
 } end)
@@ -55,7 +56,7 @@ L:RegisterTranslations("zhTW", function() return {
 	["Start a custom bar, either local or global."] = "開始一個自定時間條，區域或者全域",
 	["Local"] = "區域",
 	["Global"] = "全域",
-	["<seconds> <bar text>"] = "<秒> <列文字>",
+	["<time> <bar text>"] = "<秒> <列文字>",
 	["Starts a custom bar with the given parameters."] = "開始一個包含參數的自定時間條",
 	["%s: Timer [%s] finished."] = "%s: 計時器 [%s] 終了。",
 } end)
@@ -68,7 +69,7 @@ L:RegisterTranslations("deDE", function() return {
 	["Start a custom bar, either local or global."] = "Einen individuellen Anzeigebalken starten (entweder lokal oder global).",
 	["Local"] = "Lokal",
 	["Global"] = "Global",
-	["<seconds> <bar text>"] = "<Sekunden> <Balkentext>",
+	["<time> <bar text>"] = "<Sekunden> <Balkentext>",
 	["Starts a custom bar with the given parameters."] = "Einen individuellen Anzeigebalken mit den gegebenen Parametern starten. \n<Sekunden> <Balkentext>",
 	["%s: Timer [%s] finished."] = "%s: Timer [%s] beendet.",
 } end)
@@ -78,7 +79,7 @@ L:RegisterTranslations("frFR", function() return {
 	["Start a custom bar, either local or global."] = "Démarre une barre personnalisée, soit locale, soit globale.",
 	["Local"] = "Locale",
 	["Global"] = "Globale",
-	["<seconds> <bar text>"] = "<secondes> <texte de la barre>",
+	["<time> <bar text>"] = "<secondes> <texte de la barre>",
 	["Starts a custom bar with the given parameters."] = "Démarre une barre personnalisée avec les paramètres indiqués.",
 	["%s: Timer [%s] finished."] = "%s: Délai [%s] terminé.",
 } end)
@@ -105,7 +106,7 @@ mod.consoleOptions = {
 			desc = L["Starts a custom bar with the given parameters."],
 			get = false,
 			set = function(v) mod:TriggerEvent("BigWigs_SendSync", "BWCustomBar "..v) end,
-			usage = L["<seconds> <bar text>"],
+			usage = L["<time> <bar text>"],
 			disabled = function() return (not IsRaidLeader() and not IsRaidOfficer()) and UnitInRaid("player") end,
 			order = 1,
 		},
@@ -115,7 +116,7 @@ mod.consoleOptions = {
 			desc = L["Starts a custom bar with the given parameters."],
 			get = false,
 			set = function(v) mod:StartBar(v, nil, true) end,
-			usage = L["<seconds> <bar text>"],
+			usage = L["<time> <bar text>"],
 			order = 2,
 		},
 	},
@@ -163,11 +164,28 @@ end
 --      Utility             --
 ------------------------------
 
+local function parseTime(input)
+	if type(input) == "nil" then return end
+	if tonumber(input) then return tonumber(input) end
+	if type(input) == "string" then
+		input = input:trim()
+		if input:find(":") then
+			local m, s = select(3, input:find("^(%d+):(%d+)$"))
+			if not tonumber(m) or not tonumber(s) then return end
+			return (tonumber(m) * 60) + tonumber(s)
+		elseif input:find("^%d+m$") then
+			return tonumber(select(3, input:find("^(%d+)m$"))) * 60
+		end
+	end
+end
+
 function mod:StartBar(bar, nick, localOnly)
-	local seconds, barText = select(3, bar:find("(%d+) (.*)"))
-	if not seconds or not barText then return end
-	seconds = tonumber(seconds)
-	if seconds == nil then return end
+	local time, barText = select(3, bar:find("(%S+) (.*)"))
+	local seconds = parseTime(time)
+	if type(seconds) ~= "number" or type(barText) ~= "string" then
+		BigWigs:Print(L["Invalid time (|cffff0000%q|r) or missing bar text in a custom bar started by |cffd9d919%s|r. <time> can be either a number in seconds, a M:S pair, or Mm. For example 5, 1:20 or 2m."]:format(tostring(time), nick or UnitName("player")))
+		return
+	end
 
 	if not nick then nick = L["Local"] end
 	if seconds == 0 then
@@ -181,7 +199,7 @@ end
 
 -- For easy use in macros.
 function BWCB(seconds, message)
-	if message then seconds = fmt("%d %s", seconds, message) end
+	if message then seconds = fmt("%s %s", seconds, message) end
 	local t = GetTime()
 	if not times[seconds] or (times[seconds] and (times[seconds] + 2) < t) then
 		times[seconds] = t
@@ -190,7 +208,7 @@ function BWCB(seconds, message)
 end
 
 function BWLCB(seconds, message)
-	if message then seconds = fmt("%d %s", seconds, message) end
+	if message then seconds = fmt("%s %s", seconds, message) end
 	mod:StartBar(seconds, nil, true)
 end
 
