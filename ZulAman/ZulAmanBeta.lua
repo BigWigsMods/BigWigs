cc5f5c64c6f19772e3cb20f9220b4f2c5c19fd5f
funkydude <funkydude@users.noreply.github.com>
2007-10-12 19:27:16 +0000

    BigWigs: ZulAmanBeta: add log requests

---
+++ ZulAman/ZulAmanBeta.lua
@@ -0,0 +1,24 @@
+﻿ZulAmanBeta = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceEvent-2.0")
+
+local BZ = AceLibrary("Babble-Zone-2.2")
+local warnedThisSession = nil
+
+function ZulAman:OnEnable()
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	if GetRealZoneText() == BZ["Zul'Aman"] then
+		self:ZONE_CHANGED_NEW_AREA()
+	end
+end
+
+function ZulAman:ZONE_CHANGED_NEW_AREA()
+	if GetRealZoneText() ~= BZ["Zul'Aman"] or warnedThisSession then return end
+	warnedThisSession = true
+	if not IsAddOnLoaded("Transcriptor") then
+		self:Print("The BigWigs boss modules for Zul'Aman are not complete yet, by far, and we need your help.")
+		self:Print("Please download and install Transcriptor from http://files.wowace.com/Transcriptor/")
+		self:Print("It records what happens during an encounter, and you can then post the log on our forums.")
+		self:Print("Thanks!")
+	else
+		self:Print("Thanks for running Transcriptor! Please remember to click the icon so it glows red.")
+	end
+end
