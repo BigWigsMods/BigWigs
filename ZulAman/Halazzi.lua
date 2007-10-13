d6cf8c1b0d511c75c5c6cb5e00e3a6fa122551c8
funkydude <funkydude@users.noreply.github.com>
2007-10-13 18:38:16 +0000

    BigWigs: ZulAman: Add Halazzi module

---
+++ ZulAman/Halazzi.lua
@@ -0,0 +1,67 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["Halazzi"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Halazzi",
+
+	engage_trigger = "Get on ya knees and bow.... to da fang and claw!",
+
+	totem = "Totem",
+	totem_desc = "Warn when Halazzi casts a Lightning Totem.",
+	totem_trigger = "Halazzi  begins to cast Lightning Totem.",
+	totem_message = "Incoming Lightning Totem!",
+
+	phase = "Phases",
+	phase_desc = "Warn for phase changes",
+	phase_spirit = "I fight wit' untamed spirit....",
+	phase_normal = "Spirit, come back to me!",
+	normal_message = "Normal Phase!",
+	spirit_message = "Spirit Phase!",
+} end )
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+local mod = BigWigs:NewModule(boss)
+mod.zonename = AceLibrary("Babble-Zone-2.2")["Zul'Aman"]
+mod.enabletrigger = boss
+mod.toggleoptions = {"totem", "phase", "bosskill"}
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function mod:OnEnable()
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF")
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function mod:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF(msg)
+	if msg == L["totem_trigger"] then
+		self:Message(L["totem_message"], "Attention")
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L["phase_spirit"] then
+		self:Message(L["spirit_message"], "Attention")
+	elseif msg == L["phase_normal"] then
+		self:Message(L["normal_message"], "Attention")
+	end
+end
