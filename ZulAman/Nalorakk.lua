2b587a771ea4da9d7088087134ed948c64119bcb
funkydude <funkydude@users.noreply.github.com>
2007-10-13 20:27:01 +0000

    BigWigs: ZulAman: add partial Nalorakk module

---
+++ ZulAman/Nalorakk.lua
@@ -0,0 +1,57 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = AceLibrary("Babble-Boss-2.2")["Nalorakk"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Nalorakk",
+
+	engage_trigger = "You be dead soon enough!",
+
+	phase = "Phases",
+	phase_desc = "Warn for phase changes",
+	phase_bear = "",
+	phase_normal = "",
+	normal_message = "Normal Phase!",
+	bear_message = "Bear Phase!",
+} end )
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+local mod = BigWigs:NewModule(boss)
+mod.zonename = AceLibrary("Babble-Zone-2.2")["Zul'Aman"]
+mod.enabletrigger = boss
+mod.toggleoptions = {"phase", "bosskill"}
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function mod:OnEnable()
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+
+	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if not self.db.profile.phase then return end
+
+	if msg == L["phase_bear"] then
+		self:Message(L["bear_message"], "Attention")
+	elseif msg == L["phase_normal"] then
+		self:Message(L["normal_message"], "Attention")
+	end
+end
