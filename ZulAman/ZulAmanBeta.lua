f25072ccdb703fe25a602db980ff459699c2fa3f
funkydude <funkydude@users.noreply.github.com>
2007-10-13 16:31:35 +0000

    BigWigs: ZulAman/ZulAmanBeta: fix nub error

--- ZulAman/ZulAmanBeta.lua
+++ ZulAman/ZulAmanBeta.lua
@@ -3,14 +3,14 @@
 local BZ = AceLibrary("Babble-Zone-2.2")
 local warnedThisSession = nil
 
-function ZulAman:OnEnable()
+function ZulAmanBeta:OnEnable()
 	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 	if GetRealZoneText() == BZ["Zul'Aman"] then
 		self:ZONE_CHANGED_NEW_AREA()
 	end
 end
 
-function ZulAman:ZONE_CHANGED_NEW_AREA()
+function ZulAmanBeta:ZONE_CHANGED_NEW_AREA()
 	if GetRealZoneText() ~= BZ["Zul'Aman"] or warnedThisSession then return end
 	warnedThisSession = true
 	if not IsAddOnLoaded("Transcriptor") then
