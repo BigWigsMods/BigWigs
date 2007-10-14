23c22049cc4d22679c3f8d54d80bcd5dc3eaf9e6
funkydude <funkydude@users.noreply.github.com>
2007-10-14 00:36:09 +0000

    BigWigs: ZulAman/Akil'zon: - add icon placement - add storm cooldown

--- ZulAman/Akil'zon.lua
+++ ZulAman/Akil'zon.lua
@@ -13,11 +13,16 @@ L:RegisterTranslations("enUS", function() return {
 	cmd = "Akil'zon",
 
 	engage_trigger = "I be da predator! You da prey...",
+	engage_message = "%s Engaged - Storm in ~55sec!",
 
 	elec = "Electrical Storm",
 	elec_desc = "Warn who has Electrical Storm.",
 	elec_trigger = "An electrical storm appears!",
+	elec_bar = "~Storm Cooldown",
 	elec_message = "Storm on %s!",
+
+	icon = "Raid Icon",
+	icon_desc = "Place a Raid Target Icon on the player with Electrical Storm. (requires promoted or higher)",
 } end )
 
 ----------------------------------
@@ -27,7 +32,7 @@ L:RegisterTranslations("enUS", function() return {
 local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Zul'Aman"]
 mod.enabletrigger = boss
-mod.toggleoptions = {"elec", "bosskill"}
+mod.toggleoptions = {"elec", "icon", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -36,6 +41,7 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 end
@@ -49,5 +55,18 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg, b, player) --test
 		local show = L["elec_message"]:format(player)
 		self:Message(show, "Attention")
 		self:Bar(show, 8, "Spell_Nature_EyeOfTheStorm")
+		self:Bar(L["elec_bar"], 55, "Spell_Lightning_LightningBolt01")
+		if self.db.profile.icon then
+			self:Icon(player)
+		end
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if not self.db.profile.elec then return end
+
+	if msg == L["engage_trigger"] then
+		self:Message(L["engage_message"]:format(boss), "Positive")
+		self:Bar(L["elec_bar"], 55, "Spell_Lightning_LightningBolt01")
 	end
 end
