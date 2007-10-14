4fcba8765a2a06b3664951b34f035486d1b7f501
funkydude <funkydude@users.noreply.github.com>
2007-10-14 20:53:12 +0000

    BigWigs: ZulAman/ZulAmanBeta: Change zone in message as the mods are somewhat more stable now

--- ZulAman/ZulAmanBeta.lua
+++ ZulAman/ZulAmanBeta.lua
@@ -13,12 +13,5 @@ end
 function ZulAmanBeta:ZONE_CHANGED_NEW_AREA()
 	if GetRealZoneText() ~= BZ["Zul'Aman"] or warnedThisSession then return end
 	warnedThisSession = true
-	if not IsAddOnLoaded("Transcriptor") then
-		self:Print("The BigWigs boss modules for Zul'Aman are in Beta! Timers may be wrong and bugs may appear!")
-		self:Print("You can download and install Transcriptor from http://files.wowace.com/Transcriptor/")
-		self:Print("It records what happens during an encounter, and you can then post the log on our forums.")
-		self:Print("Thanks!")
-	else
-		self:Print("Thanks for running Transcriptor! Please remember to click the icon so it glows red.")
-	end
+	self:Print("The BigWigs boss modules for Zul'Aman are in Beta! Timers may be wrong and bugs may appear!")
 end
