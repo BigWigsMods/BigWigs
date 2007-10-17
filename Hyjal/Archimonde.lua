7ded980fa575e54e19e9a18b4e807ad6e59be118
chuanhsing <chuanhsing@users.noreply.wowace.com>
2007-10-17 14:01:56 +0000

    BigWigs: update zhTW

--- Hyjal/Archimonde.lua
+++ Hyjal/Archimonde.lua
@@ -176,7 +176,7 @@ L:RegisterTranslations("zhTW", function() return {
 
 	grip = "軍團之握",
 	grip_desc = "受到軍團之握時發送警告",
-	grip_trigger = "^^(.+)受到(.*)軍團之握",
+	grip_trigger = "^(.+)受到(.*)軍團之握",
 	grip_you = "軍團之握在你身上!",
 	grip_other = "軍團之握在 %s 身上!",
 
