5af5666fe8b1c9e9da1704d58b48ab3a98a8be42
funkydude <funkydude@users.noreply.github.com>
2007-11-07 20:18:58 +0000

    BigWigs: BlackTemple/Najentus: Remove health scan code, see BigWigs_NajAssist

--- BlackTemple/Najentus.lua
+++ BlackTemple/Najentus.lua
@@ -7,10 +7,6 @@ local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 local L2 = AceLibrary("AceLocale-2.2"):new("BigWigsCommonWords")
 
 local db = nil
-local UnitExists = UnitExists
-local UnitIsDeadOrGhost = UnitIsDeadOrGhost
-local UnitHealth = UnitHealth
-local GetNumRaidMembers = GetNumRaidMembers
 local CheckInteractDistance = CheckInteractDistance
 local fmt = string.format
 
@@ -32,11 +28,6 @@ L:RegisterTranslations("enUS", function() return {
 	spinesay_desc = "Print in say when you have a Spine, can help nearby members with speech bubbles on.",
 	spinesay_message = "Spine on me!",
 
-	health = "Health Scan",
-	health_desc = "Scan raid members health during the tidal shield phase and warn if all the raid is over 8500HP.",
-	shield_fade_trigger = "Tidal Shield fades from High Warlord Naj'entus.",
-	shield_blow_message = "Raid health > 8500!",
-
 	shield = "Tidal Shield",
 	shield_desc = "Timers for when Naj'entus will gain tidal shield.",
 	shield_trigger = "High Warlord Naj'entus is afflicted by Tidal Shield.",
@@ -44,6 +35,7 @@ L:RegisterTranslations("enUS", function() return {
 	shield_warn = "Tidal Shield!",
 	shield_soon_warn = "Tidal Shield in ~10sec!",
 	shield_fade = "Shield Faded!",
+	shield_fade_trigger = "Tidal Shield fades from High Warlord Naj'entus.",
 
 	icon = "Icon",
 	icon_desc = "Put an icon on players with Impaling Spine.",
@@ -61,11 +53,6 @@ L:RegisterTranslations("deDE", function() return {
 	--spinesay_desc = "Print in say when you have a Spine, can help nearby members with speech bubbles on.",
 	--spinesay_message = "Spine on me!",
 
-	--health = "Health Scan",
-	--health_desc = "Scan raid members health during the tidal shield phase and warn if all the raid is over 8500HP.",
-	--shield_fade_trigger = "Tidal Shield fades from High Warlord Naj'entus.",
-	--shield_blow_message = "Raid health > 8500!",
-
 	shield = "Gezeitenschild",
 	shield_desc = "Timer f\195\188r Gezeigenschild von Naj'entus.",
 	shield_trigger = "Oberster Kriegsf\195\188rst Naj'entus ist von Gezeitenschild betroffen.",
@@ -73,6 +60,7 @@ L:RegisterTranslations("deDE", function() return {
 	shield_warn = "Gezeitenschild!",
 	shield_soon_warn = "Gezeitenschild in ~10sec!",
 	--shield_fade = "Shield Faded!",
+	--shield_fade_trigger = "Tidal Shield fades from High Warlord Naj'entus.",
 
 	icon = "Icon",
 	icon_desc = "Plaziert ein Icon auf Spielern mit Aufspießendem Stachel.",
@@ -90,11 +78,6 @@ L:RegisterTranslations("koKR", function() return {
 	spinesay_desc = "꿰뚫는 돌기에 걸렸을 때, 주변 아군에게 돌기에 걸렸음을 일반 대화로 알립니다.",
 	spinesay_message = "저 돌기! 살려주세요!!",
 
-	health = "체력 검사",
-	health_desc = "해일의 보호막 단계 중에 공대원 체력을 살펴보고 만약 모든 공대원이 8500HP 이상이라면 경고합니다.",
-	shield_fade_trigger = "대장군 나젠투스의 몸에서 해일의 보호막 효과가 사라졌습니다.",
-	shield_blow_message = "공격대 체력 > 8500!",
-
 	shield = "해일의 보호막",
 	shield_desc = "대장군 나젠투스가 해일의 보호막을 얻을 떄에 대한 타이머 입니다.",
 	shield_trigger = "대장군 나젠투스|1이;가; 해일의 보호막에 걸렸습니다.",
@@ -102,6 +85,7 @@ L:RegisterTranslations("koKR", function() return {
 	shield_warn = "해일의 보호막!",
 	shield_soon_warn = "약 10초 이내 해일의 보호막!",
 	--shield_fade = "Shield Faded!",
+	shield_fade_trigger = "대장군 나젠투스의 몸에서 해일의 보호막 효과가 사라졌습니다.",
 
 	icon = "전술 표시",
 	icon_desc = "꿰뚫는 돌기에 걸린 플레이어에게 전술 표시를 지정합니다.",
@@ -119,11 +103,6 @@ L:RegisterTranslations("frFR", function() return {
 	spinesay_desc = "Fais dire à votre personnage qu'il a une épine quand c'est le cas, afin d'aider les membres proches.",
 	spinesay_message = "Epine sur moi !",
 
-	health = "Analyse de la vie",
-	health_desc = "Analyse la vie des membres du raid pendant la phase Bouclier de flots et préviens si tout le raid a plus de 8500 PV.",
-	shield_fade_trigger = "Bouclier de flots sur Grand seigneur de guerre Naj'entus vient de se dissiper.",
-	shield_blow_message = "Vie du raid > 8500 !",
-
 	shield = "Bouclier de flots",
 	shield_desc = "Délais concernant le Bouclier de flots de Naj'entus.",
 	shield_trigger = "Grand seigneur de guerre Naj'entus subit les effets .* Bouclier de flots.",
@@ -131,6 +110,7 @@ L:RegisterTranslations("frFR", function() return {
 	shield_warn = "Bouclier de flots !",
 	shield_soon_warn = "Bouclier de flots dans ~10 sec. !",
 	--shield_fade = "Shield Faded!",
+	shield_fade_trigger = "Bouclier de flots sur Grand seigneur de guerre Naj'entus vient de se dissiper.",
 
 	icon = "Icône",
 	icon_desc = "Place une icône de raid sur le dernier joueur affecté par l'Epine de perforation (nécessite d'être promu ou mieux).",
@@ -148,11 +128,6 @@ L:RegisterTranslations("zhCN", function() return {
 	spinesay_desc = "当你中了穿刺会自动喊话,能帮助周围队员避让.",
 	spinesay_message = "我中了穿刺!",
 
-	health = "生命值监视",
-	health_desc = "在海潮之盾阶段监视团队生命值并发出警告, 如果全部团队血量超过8500.",
-	shield_fade_trigger = "海潮之盾效果从高阶督军纳因图斯身上消失。$",--没 "了"
-	shield_blow_message = "团队生命值 >大于< 8500!",
-
 	shield = "海潮之盾",
 	shield_desc = "当纳因图斯获得海潮之盾后计时.",
 	shield_trigger = "高阶督军纳因图斯受到了海潮之盾效果的影响。$",
@@ -160,6 +135,7 @@ L:RegisterTranslations("zhCN", function() return {
 	shield_warn = "海潮之盾!",
 	shield_soon_warn = "海潮之盾 ~10秒 后发动!",
 	--shield_fade = "Shield Faded!",
+	shield_fade_trigger = "海潮之盾效果从高阶督军纳因图斯身上消失。$",--没 "了"
 
 	icon = "团队标记",
 	icon_desc = "给中了穿刺之脊的玩家打上团队标记.",
@@ -177,11 +153,6 @@ L:RegisterTranslations("zhTW", function() return {
 	spinesay_desc = "當你中了尖刺脊椎會時自動喊話，讓周圍隊友幫忙。",
 	spinesay_message = "我中刺了！麻煩拔一下！",
 
-	--health = "Health Scan",
-	--health_desc = "Scan raid members health during the tidal shield phase and warn if all the raid is over 8500HP.",
-	--shield_fade_trigger = "Tidal Shield fades from High Warlord Naj'entus.",
-	--shield_blow_message = "Raid health > 8500!",
-
 	shield = "潮汐之盾",
 	shield_desc = "潮汐之盾計時",
 	shield_trigger = "高階督軍納珍塔斯受到潮汐之盾效果的影響。",
@@ -189,6 +160,7 @@ L:RegisterTranslations("zhTW", function() return {
 	shield_warn = "潮汐之盾!",
 	shield_soon_warn = "潮汐之盾在 ~10秒內施放!",
 	--shield_fade = "Shield Faded!",
+	--shield_fade_trigger = "Tidal Shield fades from High Warlord Naj'entus.",
 
 	icon = "團隊標記",
 	icon_desc = "在受到尖刺脊椎的隊友頭上標記。",
@@ -201,7 +173,7 @@ L:RegisterTranslations("zhTW", function() return {
 local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Black Temple"]
 mod.enabletrigger = boss
-mod.toggleoptions = {"enrage", "shield", "health", -1, "spine", "spinesay", "icon", "proximity", "bosskill"}
+mod.toggleoptions = {"enrage", "shield", -1, "spine", "spinesay", "icon", "proximity", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 mod.proximityCheck = function( unit ) return CheckInteractDistance( unit, 2 ) end
 mod.proximitySilent = true
@@ -280,25 +252,6 @@ function mod:Spine(msg)
 	end
 end
 
-function mod:HealthCheck()
-		local blow = true
-		local num = GetNumRaidMembers()
-		for i = 1, num do
-			local id = fmt("%s%d", "raid", i)
-			if UnitExists(id) and not UnitIsDeadOrGhost(id) and UnitHealth(id) <= 8500 then
-				blow = false
-			break
-		end
-	end
-
-	if blow then
-		self:Message(L["shield_blow_message"], "Important", nil, "Alert")
-		if self:IsEventScheduled("BWNajHealthCheck") then
-			self:CancelScheduledEvent("BWNajHealthCheck")
-		end
-	end
-end
-
 function mod:BigWigs_RecvSync(sync, rest, nick)
 	if sync == "NajSpine" and rest and db.spine then
 		self:Message(fmt(L["spine_message"], rest), "Important", nil, "Alert")
@@ -311,17 +264,7 @@ function mod:BigWigs_RecvSync(sync, rest, nick)
 			self:DelayedMessage(46, L["shield_soon_warn"], "Positive")
 			self:Bar(L["shield_nextbar"], 56, "Spell_Frost_FrostBolt02")
 		end
-		if db.health then
-			self:ScheduleRepeatingEvent("BWNajHealthCheck", self.HealthCheck, 0.5, self)
-		end
-	elseif sync == "NahShieldOff" then
-		if db.shield then
-			self:Message(L["shield_fade"], "Positive")
-		end
-		if db.health then
-			if self:IsEventScheduled("BWNajHealthCheck") then
-				self:CancelScheduledEvent("BWNajHealthCheck")
-			end
-		end
+	elseif sync == "NahShieldOff" and db.shield then
+		self:Message(L["shield_fade"], "Positive")
 	end
 end
