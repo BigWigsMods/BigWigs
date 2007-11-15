95c4fd9766bf4a5a4e62610477eb3cf6617c3f08
funkydude <funkydude@users.noreply.github.com>
2007-11-15 03:51:13 +0000

    BigWigs: ZulAman/Zul'jin: drycoded additions: - Paralyze Warnings - Bleed Warnings - Phase Boss HP% Warnings

--- ZulAman/Zul'jin.lua
+++ ZulAman/Zul'jin.lua
@@ -4,6 +4,7 @@
 
 local boss = AceLibrary("Babble-Boss-2.2")["Zul'jin"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local pName = nil
 
 ----------------------------
 --      Localization      --
@@ -18,13 +19,28 @@ L:RegisterTranslations("enUS", function() return {
 	form = "Form Shift",
 	form_desc = "Warn when Zul'jin changes form.",
 	form_bear_trigger = "Got me some new tricks... like me brudda bear....",
-	form_bear_message = "Phase 2 - Bear Form!",
+	form_bear_message = "80%% Phase 2 - Bear Form!",
 	form_eagle_trigger = "Dere be no hidin' from da eagle!",
-	form_eagle_message = "Phase 3 - Eagle Form!",
+	form_eagle_message = "60%% Phase 3 - Eagle Form!",
 	form_lynx_trigger = "Let me introduce you to me new bruddas: fang and claw!",
-	form_lynx_message = "Phase 4 - Lynx Form!",
+	form_lynx_message = "40%% Phase 4 - Lynx Form!",
 	form_dragonhawk_trigger = "Ya don' have to look to da sky to see da dragonhawk!",
-	form_dragonhawk_message = "Phase 5 - Dragonhawk Form!",
+	form_dragonhawk_message = "20%% Phase 5 - Dragonhawk Form!",
+
+	throw = "Grievous Throw",
+	throw_desc = "Warn who is afflicted by Grievous Throw.",
+	throw_message = "%s has Grievous Throw",
+	throw_trigger = "^(%S+) (%S+) afflicted by Grievous Throw%.$",
+
+	icon = "Raid Icon",
+	icon_desc = "Place a Raid Target Icon on the player afflicted by Grievous Throw. (requires promoted or higher)",
+
+	paralyze = "Paralyze",
+	paralyze_desc = "Warn for Creeping Paralysis and the impending Paralyze after effect.",
+	paralyze_warning = "Creeping Paralysis - Paralyze in 5 sec!",
+	paralyze_message = "Paralyzed!",
+	paralyze_bar = "Inc Paralyze",
+	paralyze_trigger = "afflicted by Creeping Paralysis%.$",
 } end )
 
 L:RegisterTranslations("frFR", function() return {
@@ -41,6 +57,21 @@ L:RegisterTranslations("frFR", function() return {
 	form_lynx_message = "Phase 4 - Forme de lynx !",
 	form_dragonhawk_trigger = "Pas besoin d'lever les yeux au ciel pour voir l'faucon-dragon !",
 	form_dragonhawk_message = "Phase 5 - Forme de faucon-dragon !",
+
+	--throw = "Grievous Throw",
+	--throw_desc = "Warn who is afflicted by Grievous Throw.",
+	--throw_message = "%s has Grievous Throw",
+	--throw_trigger = "^(%S+) (%S+) afflicted by Grievous Throw%.$",
+
+	--icon = "Raid Icon",
+	--icon_desc = "Place a Raid Target Icon on the player afflicted by Grievous Throw. (requires promoted or higher)",
+
+	--paralyze = "Paralyze",
+	--paralyze_desc = "Warn for Creeping Paralysis and the impending Paralyze after effect.",
+	--paralyze_warning = "Creeping Paralysis - Paralyze in 5 sec!",
+	--paralyze_message = "Paralyzed!",
+	--paralyze_bar = "Inc Paralyze",
+	--paralyze_trigger = "afflicted by Creeping Paralysis%.$",
 } end )
 
 L:RegisterTranslations("koKR", function() return {
@@ -57,6 +88,21 @@ L:RegisterTranslations("koKR", function() return {
 	form_lynx_message = "4단계 - 스라소니!",
 	form_dragonhawk_trigger = "용매를 하늘에서만 찾을 필요는 없다!",  -- Check
 	form_dragonhawk_message = "5단계 - 용매!",
+
+	--throw = "Grievous Throw",
+	--throw_desc = "Warn who is afflicted by Grievous Throw.",
+	--throw_message = "%s has Grievous Throw",
+	--throw_trigger = "^(%S+) (%S+) afflicted by Grievous Throw%.$",
+
+	--icon = "Raid Icon",
+	--icon_desc = "Place a Raid Target Icon on the player afflicted by Grievous Throw. (requires promoted or higher)",
+
+	--paralyze = "Paralyze",
+	--paralyze_desc = "Warn for Creeping Paralysis and the impending Paralyze after effect.",
+	--paralyze_warning = "Creeping Paralysis - Paralyze in 5 sec!",
+	--paralyze_message = "Paralyzed!",
+	--paralyze_bar = "Inc Paralyze",
+	--paralyze_trigger = "afflicted by Creeping Paralysis%.$",
 } end )
 
 L:RegisterTranslations("zhCN", function() return {
@@ -73,6 +119,21 @@ L:RegisterTranslations("zhCN", function() return {
 	form_lynx_message = "第四阶段 - 山猫形态!",
 	form_dragonhawk_trigger = "Ya don' have to look to da sky to see da dragonhawk!",
 	form_dragonhawk_message = "第五阶段 - 龙鹰形态!",
+
+	--throw = "Grievous Throw",
+	--throw_desc = "Warn who is afflicted by Grievous Throw.",
+	--throw_message = "%s has Grievous Throw",
+	--throw_trigger = "^(%S+) (%S+) afflicted by Grievous Throw%.$",
+
+	--icon = "Raid Icon",
+	--icon_desc = "Place a Raid Target Icon on the player afflicted by Grievous Throw. (requires promoted or higher)",
+
+	--paralyze = "Paralyze",
+	--paralyze_desc = "Warn for Creeping Paralysis and the impending Paralyze after effect.",
+	--paralyze_warning = "Creeping Paralysis - Paralyze in 5 sec!",
+	--paralyze_message = "Paralyzed!",
+	--paralyze_bar = "Inc Paralyze",
+	--paralyze_trigger = "afflicted by Creeping Paralysis%.$",
 } end )
 
 ----------------------------------
@@ -82,7 +143,7 @@ L:RegisterTranslations("zhCN", function() return {
 local mod = BigWigs:NewModule(boss)
 mod.zonename = AceLibrary("Babble-Zone-2.2")["Zul'Aman"]
 mod.enabletrigger = boss
-mod.toggleoptions = {"form", "bosskill"}
+mod.toggleoptions = {"form", "paralyze", -1, "throw", "icon", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -92,7 +153,17 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "ZulBleed", 2)
+	self:TriggerEvent("BigWigs_ThrottleSync", "ZulPara", 5)
+
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Bleed")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Bleed")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Bleed")
+
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+
+	pName = UnitName("player")
 end
 
 ------------------------------
@@ -112,5 +183,34 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 		self:Message(L["form_dragonhawk_message"], "Attention")
 	elseif msg == L["engage_trigger"] then
 		self:Message(L["engage_message"], "Attention")
+	end
+end
+
+function mod:Bleed(msg)
+	if msg:find(L["paralyze_trigger"]) then
+		self:Sync("ZulPara")
+		return
+	end
+
+	local gplayer, gtype = select(3, msg:find(L["throw_trigger"]))
+	if gplayer and gtype then
+		if gplayer == L2["you"] and gtype == L2["are"] then
+			gplayer = pName
+		end
+		self:Sync("ZulBleed", gplayer)
+		return
+	end
+end
+
+function mod:BigWigs_RecvSync(sync, rest, nick)
+	if sync == "ZulBleed" and rest and self.db.profile.throw then
+		self:Message(L["throw_message"]:format(rest), "Attention")
+		if self.db.profile.icon then
+			self:Icon(rest)
+		end
+	elseif sync == "ZulPara" and self.db.profile.paralyze then
+		self:Message(L["paralyze_warning"], "Urgent")
+		self:DelayedMessage(5, L["paralyze_message"], "Positive")
+		self:Bar(L["paralyze_bar"], 5, "Spell_Nature_TimeStop")
 	end
 end
