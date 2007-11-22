233e1ffa069e98ef6db9d8237ad72899530c3e57
scorpio0920 <scorpio0920@users.noreply.wowace.com>
2007-11-22 19:00:32 +0000

    BigWigs: Update zhTW translation.

--- TheEye/Reaver.lua
+++ TheEye/Reaver.lua
@@ -176,7 +176,7 @@ L:RegisterTranslations("zhCN", function() return {
 } end )
 
 L:RegisterTranslations("zhTW", function() return {
-	engage_trigger = "警告!你已經被標記為撲殺的對象。",
+	engage_trigger = "警告!你已經被標記為消滅的對象。",
 
 	orbyou = "秘法寶珠瞄準你",
 	orbyou_desc = "當秘法寶珠目標為你時警告",
