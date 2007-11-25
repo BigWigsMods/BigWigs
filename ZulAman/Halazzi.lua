583ebd979867b559bdd10a93357f9fcfcb387033
7destiny <7destiny@users.noreply.wowace.com>
2007-11-25 17:38:30 +0000

    BigWigs: ZulAman/Halazzi: koKR update.

--- ZulAman/Halazzi.lua
+++ ZulAman/Halazzi.lua
@@ -72,7 +72,7 @@ L:RegisterTranslations("koKR", function() return {
 
 	frenzy = "광기",
 	frenzy_desc = "광기 경고.",
-	frenzy_trigger = "%s가 죽일 듯한 기세로 격분합니다!",
+	frenzy_trigger = "%s|1이;가; 죽일 듯한 기세로 격분합니다!",
 	frenzy_message = "광기!",
 
 	flame = "화염 충격",
