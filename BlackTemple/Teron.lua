e800651fbcfc99b23694a21e0d9ab399e3611793
funkydude <funkydude@users.noreply.github.com>
2007-12-08 06:23:32 +0000

    BigWigs: BlackTemple/Teron: Tweak SoD message

--- BlackTemple/Teron.lua
+++ BlackTemple/Teron.lua
@@ -21,7 +21,7 @@ L:RegisterTranslations("enUS", function() return {
 	shadow = "Shadow of Death",
 	shadow_desc = "Tells you who has Shadow of Death.",
 	shadow_trigger = "^(%S+) (%S+) afflicted by Shadow of Death%.$",
-	shadow_other = "Shadow of Death: %s!",
+	shadow_other = "Shadow: %s!",
 	shadow_you = "Shadow of Death on YOU!",
 
 	ghost = "Ghost",
