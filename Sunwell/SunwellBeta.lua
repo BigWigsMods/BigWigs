32a1bb596a1cda93050291f0a60e3a4277c36a84
funkydude <funkydude@users.noreply.github.com>
2008-02-13 00:22:13 +0000

    BigWigs: Sunwell: add sunwellbeta

---
+++ Sunwell/SunwellBeta.lua
@@ -0,0 +1,19 @@
+﻿local SunwellBeta = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceEvent-2.0")
+
+local BZ = AceLibrary("Babble-Zone-2.2")
+local warnedThisSession = nil
+
+function SunwellBeta:OnEnable()
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	if GetRealZoneText() == BZ["Sunwell Plateau"] then
+		self:ZONE_CHANGED_NEW_AREA()
+	end
+end
+
+function SunwellBeta:ZONE_CHANGED_NEW_AREA()
+	if GetRealZoneText() ~= BZ["Sunwell Plateau"] or warnedThisSession then return end
+	warnedThisSession = true
+	self:Print("The BigWigs boss modules for Sunwell are in Beta! Timers may be wrong and bugs may appear!")
+	self:Print("Please refer to the forum page at http://www.wowace.com/forums/index.php?topic=11354")
+	self:Print("for info on how you can help us!")
+end
