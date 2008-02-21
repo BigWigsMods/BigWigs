e058b347048cb91a4046914bd5f9d2bc8cd76eda
Wouter Commandeur <wouter@ctlaltdel.nl>
2008-02-21 09:49:52 +0000

    BigWigs: 2.4 compat kazzak

--- Outland/Kazzak.lua
+++ Outland/Kazzak.lua
@@ -148,6 +148,10 @@ function mod:OnEnable()
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	self:RegisterEvent("CHAT_MSG_MONSTER_EMOTE")
 
+	self:AddCombatListener("UNIT_DIED", "GenericBossDeath")
+	self:AddCombatListener("SPELL_AURA_APPLIED", "Mark", 32960)
+	self:AddCombatListener("SPELL_AURA_APPLIED", "Twisted", 21064, 21063) --figure out which one
+	
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event")
@@ -177,6 +181,16 @@ function mod:CHAT_MSG_MONSTER_EMOTE(msg)
 		self:DelayedMessage(49, L["enrage_warning2"], "Urgent")
 		self:Bar(L["enrage_bar"], 60, "Spell_Shadow_UnholyFrenzy")
 	end
+end
+
+function mod:Mark(player)
+	if player and player == pName and self.db.profile.mark then
+		self:Message(L["mark_message"], "Personal", true, "Alarm")
+	end
+end
+
+function mod:Twisted(player)
+	if player then self:Sync("Twisted", player) end
 end
 
 function mod:Event(msg)
