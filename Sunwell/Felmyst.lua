1dedfdfcb5167ee2406c46dc90f1e268947d5268
funkydude <funkydude@users.noreply.github.com>
2008-02-23 20:21:16 +0000

    BigWigs: Sunwell/Felmyst: Add in module, shows takeoff, yeah well it's something.

--- Sunwell/Felmyst.lua
+++ Sunwell/Felmyst.lua
@@ -41,18 +41,12 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 function mod:OnEnable()
 	started = nil
 
-	self:AddCombatListener("SPELL_MISSED", "BurnResist", 45141)
-	self:AddCombatListener("SPELL_CAST_START", "Meteor", 45150)
-	self:AddCombatListener("SPELL_AURA_APPLIED", "Burn", 46394)
-	self:AddCombatListener("SPELL_AURA_REMOVED", "BurnRemove", 46394)
-	self:AddCombatListener("SPELL_DAMAGE", "Stomp", 45185)
 	self:AddCombatListener("UNIT_DIED", "GenericBossDeath")
 
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 
 	self:RegisterEvent("BigWigs_RecvSync")
-	self:Throttle(300, "BrutallusBurn", "BrutallusBurnJump") --remove after brut is disabled on ptr
 
 	db = self.db.profile
 end
@@ -61,57 +55,12 @@ end
 --      Event Handlers      --
 ------------------------------
 
-function mod:Burn(player, spellID)
-	if db.burn then
-		local other = L["burn_other"]:format(player)
-		if player == pName then
-			self:Message(L["burn_you"], "Personal", true, "Alert", nil, spellID)
-			self:Message(other, "Attention", nil, nil, true)
-		else
-			self:Message(other, "Attention", nil, nil, nil, spellID)
-		end
-		self:Bar(other, 60, spellID)
-	end
-end
-
-function mod:Meteor()
-	if db.meteor then
-		self:Bar(L["meteor_bar"], 12, 45150)
-	end
-end
-
-function mod:BurnRemove(player)
-	if db.burn then
-		self:TriggerEvent("BigWigs_StopBar", self, L["burn_other"]:format(player))
-	end
-end
-
-function mod:BurnResist(player)
-	if db.burnresist then
-		self:Message(L["burn_resist"]:format(player), "Positive", nil, nil, nil, 45141)
-	end
-end
-
-function mod:Stomp(player, spellID)
-	if db.stomp then
-		self:Message(L["stomp_message"]:format(player), "Urgent", nil, nil, nil, spellID)
-		self:DelayedMessage(25, L["stomp_warning"], "Attention")
-		self:Bar(L["stomp_bar"], 30, spellID)
-	end
-end
-
 function mod:BigWigs_RecvSync(sync, rest, nick)
 	if self:ValidateEngageSync(sync, rest) and not started then
 		started = true
 		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
 			self:UnregisterEvent("PLAYER_REGEN_DISABLED")
 		end
-		if db.burn then
-			self:Bar(L["burn_bar"], 20, 45141)
-			self:DelayedMessage(15, L["burn_message"], "Attention")
-		end
-		if db.enrage then
-			self:Enrage(360)
-		end
+		self:Bar("~Possible takeoff", 20, 31550)
 	end
 end
