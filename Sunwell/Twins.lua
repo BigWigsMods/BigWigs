be0ad091ca056141277ba50faa1db6d86d01e16a
funkydude <funkydude@users.noreply.github.com>
2008-03-02 20:25:49 +0000

    BigWigs: Sunwell/Twins: Warn the players that get conflag/nova since the event seems more stable in the latest ptr now.

--- Sunwell/Twins.lua
+++ Sunwell/Twins.lua
@@ -24,9 +24,11 @@ L:RegisterTranslations("enUS", function() return {
 
 	nova = "Shadow Nova",
 	nova_desc = "Warn for Shadow Nova being cast.",
+	nova_message = "Shadow Nova on %s",
 
 	conflag = "Conflagration",
 	conflag_desc = "Warn for Conflagration being cast.",
+	conflag_message = "Conflag on %s",
 
 	pyro = "Pyrogenics",
 	pyro_desc = "Warn who gains and removes Pyrogenics.",
@@ -49,14 +51,12 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 ------------------------------
 
 function mod:OnEnable()
-	self:AddCombatListener("SPELL_CAST_SUCCESS", "Nova", 45332)
-	self:AddCombatListener("SPELL_CAST_SUCCESS", "Conflag", 45333)
 	self:AddCombatListener("SPELL_AURA_APPLIED", "PyroGain", 45230)
 	self:AddCombatListener("SPELL_AURA_STOLEN", "PyroRemove", 45230)
 	self:AddCombatListener("SPELL_AURA_REMOVED", "PyroRemove", 45230)
 
-
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
 
 	--self:AddCombatListener("UNIT_DIED", "GenericBossDeath")
 
@@ -71,15 +71,11 @@ end
 --      Event Handlers      --
 ------------------------------
 
-function mod:Nova()
-	if db.nova then
-		self:Message(L["nova"], "Urgent", nil, nil, nil, 45329)
-	end
-end
-
-function mod:Conflag()
-	if db.conflag then
-		self:Message(L["conflag"], "Attention", nil, nil, nil, 45333)
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, unit, _, _, player)
+	if unit == lady and player and db.nova then
+		self:Message(L["nova_message"]:format(player), "Urgent", nil, nil, nil, 45329)
+	elseif unit == lock and player and db.conflag then
+		self:Message(L["conflag_message"]:format(player), "Attention", nil, nil, nil, 45333)
 	end
 end
 
