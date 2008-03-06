44a5c58c36ed1fdc79ae77efacd1cf62a28407d3
funkydude <funkydude@users.noreply.github.com>
2008-03-06 23:12:34 +0000

    BigWigs: Sunwell/M'uru: add template

---
+++ Sunwell/Muru.lua
@@ -0,0 +1,59 @@
+﻿------------------------------
+--      Are you local?      --
+------------------------------
+
+local boss = BB["M'uru"]
+local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+
+local db = nil
+local started = nil
+local pName = UnitName("player")
+
+----------------------------
+--      Localization      --
+----------------------------
+
+L:RegisterTranslations("enUS", function() return {
+	cmd = "Muru",
+
+	engage_trigger = "",
+} end )
+
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+local mod = BigWigs:NewModule(boss)
+mod.zonename = BZ["Sunwell Plateau"]
+mod.enabletrigger = boss
+mod.toggleoptions = {"bosskill"}
+mod.revision = tonumber(("$Revision$"):sub(12, -3))
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function mod:OnEnable()
+	self:AddCombatListener("UNIT_DIED", "GenericBossDeath")
+
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+	self:RegisterEvent("BigWigs_RecvSync")
+
+	db = self.db.profile
+	started = nil
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function mod:BigWigs_RecvSync(sync, rest, nick)
+	if self:ValidateEngageSync(sync, rest) and not started then
+		started = true
+		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
+			self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+		end
+	end
+end
+
