7a4d52bd7edf7279e5293ccd3a4406a5fec66e5c
funkydude <funkydude@users.noreply.github.com>
2008-03-08 02:33:37 +0000

    BigWigs: Sunwell/Muru: add stuff

--- Sunwell/Muru.lua
+++ Sunwell/Muru.lua
@@ -8,6 +8,7 @@ local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 local db = nil
 local started = nil
 local pName = UnitName("player")
+local inDark = {}
 
 ----------------------------
 --      Localization      --
@@ -16,7 +17,21 @@ local pName = UnitName("player")
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Muru",
 
-	engage_trigger = "",
+	darkness = "Darkness",
+	darkness_desc = "Warn who has Darkness.",
+	darkness_message = "Darkness: %s",
+	darkness_next = "Next Darkness",
+	darkness_soon = "Darkness in 5sec!",
+
+	void = "Void Sentinel",
+	void_desc = "Warn when the Void Sentinel spawns.",
+	void_next = "Next Void Sentinel",
+	void_soon = "Sentinel in 5 sec!",
+
+	humanoid = "Humanoid Adds",
+	humanoid_desc = "Warn when the Humanoid Adds spawn.",
+	humanoid_next = "Next Humanoids",
+	humanoid_soon = "Humanoids in 5sec!",
 } end )
 
 ----------------------------------
@@ -26,7 +41,7 @@ L:RegisterTranslations("enUS", function() return {
 local mod = BigWigs:NewModule(boss)
 mod.zonename = BZ["Sunwell Plateau"]
 mod.enabletrigger = boss
-mod.toggleoptions = {"bosskill"}
+mod.toggleoptions = {"darkness", "void", "humanoid", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
 
 ------------------------------
@@ -34,6 +49,7 @@ mod.revision = tonumber(("$Revision$"):sub(12, -3))
 ------------------------------
 
 function mod:OnEnable()
+	self:AddCombatListener("SPELL_AURA_APPLIED", "Darkness", 45996)
 	self:AddCombatListener("UNIT_DIED", "GenericBossDeath")
 
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
@@ -48,11 +64,50 @@ end
 --      Event Handlers      --
 ------------------------------
 
+function mod:Darkness(player, spellID)
+	if not db.darkness then return end
+
+	if player == boss then
+		self:Bar(L["darkness_message"], 20, spellID)
+		self:IfMessage(L["darkness_message"]:format(player), "Urgent", spellID)
+		self:Bar(L["darkness_next"], 45, spellID)
+		self:DelayedMessage(40, L["darkness_soon"], "Attention")
+	else
+		inDark[player] = true
+		self:ScheduleEvent("BWMuruDark", self.DarkWarn, 0.4, self)
+	end
+end
+
+function mod:DarkWarn()
+	local msg = nil
+	for k in pairs(inDark) do
+		if not msg then
+			msg = k
+		else
+			msg = msg .. ", " .. k
+		end
+	end
+	self:IfMessage(L["darkness_message"]:format(msg), "Urgent", 45996)
+end
+
 function mod:BigWigs_RecvSync(sync, rest, nick)
 	if self:ValidateEngageSync(sync, rest) and not started then
 		started = true
 		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
 			self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+		end
+		for k in pairs(inDark) do inDark[k] = nil end
+		if db.darkness then
+			self:Bar(L["darkness_next"], 45, 45996)
+			self:DelayedMessage(40, L["darkness_soon"], "Attention")
+		end
+		if db.void then
+			self:Bar(L["void_next"], 35, 46087)
+			self:DelayedMessage(30, L["void_soon"], "Positive")
+		end
+		if db.humanoid then
+			self:Bar(L["humanoid_next"], 70, 46087)
+			self:DelayedMessage(65, L["void_soon"], "Positive")
 		end
 	end
 end
