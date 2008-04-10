0c1573f0a4119b183e192e17dfe35501fa706098
funkydude <funkydude@users.noreply.github.com>
2008-04-10 23:02:56 +0000

    BigWigs: Plugins/TargetMonitor; Send the UnitGUID in the even trigger as an extra for future use

--- Plugins/TargetMonitor.lua
+++ Plugins/TargetMonitor.lua
@@ -80,7 +80,8 @@ end
 
 function plugin:TargetCheck(unit)
 	local n = UnitName(unit)
+	local guid = UnitGUID(unit)
 	if not n or not enablemobs[n] or UnitIsCorpse(unit) or UnitIsDead(unit) or UnitPlayerControlled(unit) then return end
-	self:TriggerEvent("BigWigs_TargetSeen", n, unit)
+	self:TriggerEvent("BigWigs_TargetSeen", n, unit, guid)
 end
 
