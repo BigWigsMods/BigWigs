3adc658b392506301f7a42c104da8c835afe0fa9
funkydude <funkydude@users.noreply.github.com>
2008-05-24 02:18:15 +0000

    BigWigs: Sunwell/KilJaeden: Merge in [UNTESTED] patches by Dartanius & Subkiller20

--- Sunwell/KilJaeden.lua
+++ Sunwell/KilJaeden.lua
@@ -4,10 +4,13 @@
 
 local boss = BB["Kil'jaeden"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
+local CheckInteractDistance = CheckInteractDistance
 
 local db = nil
 local started = nil
 local deaths = 0
+local pName = UnitName("player")
+local bloomed = {}
 
 ----------------------------
 --      Localization      --
@@ -16,7 +19,7 @@ local deaths = 0
 L:RegisterTranslations("enUS", function() return {
 	cmd = "KilJaeden",
 
-	bomb = "Darkness of a Thousand Souls",
+	bomb = "Darkness",
 	bomb_desc = "Warn when Darkness of a Thousand Souls is being cast.",
 	bomb_cast = "Incoming Big Bomb!",
 	bomb_bar = "Explosion!",
@@ -29,8 +32,36 @@ L:RegisterTranslations("enUS", function() return {
 	orb_shooting = "Orb Alive - Shooting People!",
 
 	bloom = "Fire Bloom",
-	bloom_desc = "Warn for incoming Fire Bloom.",
-	bloom_message = "Inc Bloom!",
+	bloom_desc = "Tells you who has been hit by Fire Bloom.",
+	bloom_other = "Fire Bloom on %s!",
+	bloom_bar = "Fire Blooms",
+	bloom_message = "Next Fire Bloom in 5 seconds!",
+
+	bloomsay = "Fire Bloom Say",
+	bloomsay_desc = "Place a msg in say notifying that you have Fire Bloom",
+	bloom_say = "Fire Bloom on ME!",
+
+	bloomwhisper = "Fire Bloom Whisper",
+	bloomwhisper_desc = "Whisper players with Fire Bloom.",
+
+	icons = "Bloom Icons",
+	icons_desc = "Place random Raid Icons on players with Fire Bloom (requires promoted or higher)",
+
+	shadow = "Shadow Spike",
+	shadow_desc = "Raid warn of casting of Shadow Spike.",
+	shadow_message = "Shadow Spikes Inc For 28sec! WATCH OUT!",
+	shadow_bar = "Shadow Spikes Expire",
+	shadow_warning = "Shadow Spikes Done in 5 seconds!",
+	shadow_debuff_bar = "Reduced Healing on %s",
+
+	flame = "Flame Dart",
+	flame_desc = "Show Flame Dart timer bar.",
+	flame_bar = "Next Flame Dart",
+	flame_message = "Next Flame Dart in 5 seconds!",
+
+	sinister = "Sinister Reflections",
+	sinister_desc = "Warns on Sinister Reflection spawns.",
+	sinister_message = "Sinister Reflections Up!",
 
 	shield_up = "Shield is UP!",
 
@@ -92,18 +123,22 @@ local deceiver = L["Hand of the Deceiver"]
 local mod = BigWigs:NewModule(boss)
 mod.zonename = BZ["Sunwell Plateau"]
 mod.enabletrigger = {deceiver, boss}
-mod.toggleoptions = {"bomb", "orb", "bloom", "bosskill"}
+mod.toggleoptions = {"bomb", "orb", "flame", -1, "bloom", "bloomwhisper","bloomsay", "icons", -1, "sinister", "shadow", "bosskill"}
 mod.revision = tonumber(("$Revision$"):sub(12, -3))
+mod.proximityCheck = function( unit ) return CheckInteractDistance( unit, 3 ) end
+mod.proximitySilent = true
 
 ------------------------------
 --      Initialization      --
 ------------------------------
 
 function mod:OnEnable()
+	self:AddCombatListener("SPELL_CAST_SUCCESS", "Sinister", 45892)
 	self:AddCombatListener("SPELL_CAST_SUCCESS", "Shield", 45848)
 	self:AddCombatListener("SPELL_DAMAGE", "Orb", 45680)
 	self:AddCombatListener("SPELL_MISSED", "Orb", 45680)
-	self:AddCombatListener("SPELL_CAST_START", "Bloom", 45641)
+	self:AddCombatListener("SPELL_AURA_APPLIED", "Bloom", 45641)
+	self:AddCombatListener("SPELL_CAST_START", "Shadow", 45885)
 	self:AddCombatListener("UNIT_DIED", "Deaths")
 
 	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
@@ -116,11 +151,22 @@ function mod:OnEnable()
 	db = self.db.profile
 	started = nil
 	deaths = 0
+	for i = 1, #bloomed do bloomed[i] = nil end
 end
 
 ------------------------------
 --      Event Handlers      --
 ------------------------------
+
+function mod:Sinister()
+	if db.sinister then
+		self:IfMessage(L["sinister_message"], "Attention", 45892)
+	end
+	if db.flame then
+		self:Bar(L["flame_bar"], 60, 45737)
+		self:DelayedMessage(55, L["flame_message"], "Attention")
+	end
+end
 
 function mod:Shield()
 	self:IfMessage(L["shield_up"], "Urgent", 45848)
@@ -137,19 +183,13 @@ function mod:Orb()
 	end
 end
 
-function mod:Bloom()
-	--some kind of  timer on this thing that doesn't suck?
-	if db.bloom then
-		self:IfMessage(L["bloom_message"], "Important", 45641)
-	end
-end
-
 function mod:Deaths(unit)
 	if unit == deceiver then
 		deaths = deaths + 1
 		self:IfMessage(L["deceiver_dies"]:format(deaths), "Positive")
 		if deaths == 3 then
 			self:Bar(boss, 10, "Spell_Shadow_Charm")
+			self:TriggerEvent("BigWigs_ShowProximity", self)
 		end
 	elseif unit == boss then
 		self:GenericBossDeath(unit)
@@ -172,6 +212,45 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 	end
 end
 
+function mod:Shadow()
+	if db.shadow then
+		self:Bar(L["shadow_bar"], 28, 45885)
+		self:DelayedMessage(0.1, L["shadow_message"], "Attention")
+		self:DelayedMessage(23, L["shadow_warning"], "Attention")
+	end
+end
+
+function mod:Bloom(player)
+	if db.bloom then
+		tinsert(bloomed, player)
+		self:Whisper(player, L["bloom_you"], "bloomwhisper")
+		self:ScheduleEvent("BWBloomWarn", self.BloomWarn, 0.4, self)
+		if player == pName and db.bloomsay then
+			SendChatMessage(L["bloom_say"], "SAY")
+		end
+	end
+end
+
+function mod:BloomWarn()
+	local msg = nil
+	table.sort(bloomed)
+
+	for i,v in ipairs(bloomed) do
+		if not msg then
+			msg = v
+		else
+			msg = msg .. ", " .. v
+		end
+		if db.icons then
+			SetRaidTarget(v, i)
+		end
+	end
+
+	self:IfMessage(L["bloom_other"]:format(msg), "Important", 45641, "Alert")
+	self:Bar(L["bloom_bar"], 20, 45641)
+	for i = 1, #bloomed do bloomed[i] = nil end
+end
+
 function mod:BigWigs_RecvSync(sync, rest, nick)
 	if self:ValidateEngageSync(sync, rest) and not started then
 		started = true
@@ -180,3 +259,4 @@ function mod:BigWigs_RecvSync(sync, rest, nick)
 		end
 	end
 end
+
