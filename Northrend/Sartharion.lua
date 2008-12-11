fdf9f2bdf5d4a2a554238c630aa5753481b0aa43
7destiny <7destiny@users.noreply.wowace.com>
2008-12-11 23:34:59 +0000

    BigWigs: Northrend/Sartharion: koKR Update

--- Northrend/Sartharion.lua
+++ Northrend/Sartharion.lua
@@ -65,7 +65,7 @@ L:RegisterTranslations("koKR", function() return {
 	tsunami_warning = "약 5초 후 용암 파도!",
 	tsunami_message = "용암 파도!",
 	tsunami_cooldown = "용암 파도 대기시간",
-	tsunami_trigger = "%s을 둘러싼 용암이 끓어오릅니다!",
+	tsunami_trigger = "%s를 둘러싼 용암이 끓어오릅니다!",
 
 	breath = "화염 숨결",
 	breath_desc = "화염 숨결 시전을 알립니다.",
