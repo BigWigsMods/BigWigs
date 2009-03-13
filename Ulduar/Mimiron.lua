76f9c14d9b2022c6828454c227a72e25dc3089eb
Hendrik Leppkes <h.leppkes@gmail.com>
2009-03-13 22:11:49 +0000

    BigWigs: Add a few more details for Mimiron P1

--- Ulduar/Mimiron.lua
+++ Ulduar/Mimiron.lua
@@ -28,9 +28,12 @@ local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Mimiron",
 	
+	starttrigger = "We haven't much time, friends! You're going to help me test out my latest and greatest creation. Now, before you change your minds, remember that you kind of owe it to me after the mess you made with the XT-002.",
+	
 	plasma = "Plasma Blast",
 	plasma_desc = "Warns when Plasma Blast is casting.",
 	plasma_warning = "Casting Plasma Blast!",
+	plasma_soon = "Plasma Blast soon!",
 	
 	shock = "Shock Blast",
 	shock_desc = "Warns when Shock Blast is casting.",
@@ -59,6 +62,8 @@ function mod:OnEnable()
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	--self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
 	
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	
 	self:RegisterEvent("BigWigs_RecvSync")
 
 	db = self.db.profile
@@ -74,6 +79,8 @@ function mod:Plasma(_, spellID)
 	if db.plasma then
 		self:IfMessage(L["plasma_warning"], "Important", spellID)
 		self:Bar(L["plasma_warning"], 3, spellID)
+		self:Bar(L["plasma"], 30, spellID)
+		self:DelayedMessage(27, L["plasma_soon"], "Attention")
 	end
 end
 
@@ -81,6 +88,13 @@ function mod:Shock(_, spellID)
 	if db.shock then
 		self:IfMessage(L["shock_warning"], "Important", spellID)
 		self:Bar(L["shock"], 5, spellID)
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg:match(L["starttrigger"]) and db.plasma then
+		self:Bar(L["plasma"], 20, spellID)
+		self:DelayedMessage(17, L["plasma_soon"], "Attention")
 	end
 end
 
