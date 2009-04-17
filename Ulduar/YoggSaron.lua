a6878926b567e6ab5e13a9721157535b9175264e
7destiny <7destiny@users.noreply.wowace.com>
2009-04-17 12:28:21 +0000

    BigWigs: Ulduar/Yogg-Saron: Add simple warn

--- Ulduar/YoggSaron.lua
+++ Ulduar/YoggSaron.lua
@@ -2,13 +2,14 @@
 --      Module Declaration      --
 ----------------------------------
 
-local boss = BB["Yogg-Saron"]	--need the add name translated, maybe add to BabbleBoss.
+local sara = BB["Sara"]	--need the add name translated, maybe add to BabbleBoss.
+local boss = BB["Yogg-Saron"]
 local mod = BigWigs:New(boss, "$Revision$")
 if not mod then return end
 mod.zonename = BZ["Ulduar"]
-mod.enabletrigger = boss
-mod.guid = 16777215
-mod.toggleoptions = {"bosskill"}
+mod.enabletrigger = {"sara", "boss"}
+mod.guid = 33288
+mod.toggleoptions = {"phase", "portal", "weakened", "bosskill"}
 
 ------------------------------
 --      Are you local?      --
@@ -24,6 +25,26 @@ local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
 L:RegisterTranslations("enUS", function() return {
 	cmd = "YoggSaron",
+	
+	phase = "Phase",
+	phase_desc = "Warn for phase changes.",
+	engage_warning = "Phase 1",
+	engage_trigger = "^The time to",
+	phase2_warning = "Phase 2",
+	phase2_trigger = "^I am the lucid dream",
+	phase2_warning = "Phase 3",
+	phase3_trigger = "^Look upon the true face",
+		
+	portal = "Portal",
+	portal_desc = "Warn for Portals.",
+	portal_trigger = "Portals open into Yogg-Saron's mind!",
+	portal_message = "Portals open!",
+	
+	weakened = "Weakened",
+	weakened_desc = "Warn for Weakened State.",
+	weakened_message = "%s is weakened!",
+	weakened_trigger = "The Illusion shatters and a path to the central chamber opens!",
+	
 
 	log = "|cffff0000"..boss.."|r: This boss needs data, please consider turning on your /combatlog or transcriptor and submit the logs.",
 } end )
@@ -50,6 +71,10 @@ L:RegisterTranslations("deDE", function() return {
 function mod:OnEnable()
 	self:AddCombatListener("UNIT_DIED", "BossDeath")
 	
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	
 	db = self.db.profile
 
 	BigWigs:Print(L["log"])
@@ -59,3 +84,30 @@ end
 --      Event Handlers      --
 ------------------------------
 
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
+	if msg == L["portal_trigger"] and db.portal then
+		self:IfMessage(L["portal_message"], "Attention")
+	elseif msg == L["weakened_trigger"] and db.weakened then
+		self:IfMessage(L["weakened_message"]:format(boss), "Attention")
+		self:Bar(L["weakened"], 45, 50661)	--50661, looks like a weakened :)
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg:find(L["engage_trigger"]) then
+		phase = 1
+		if db.phase then
+			self:Message(L["engage_warning"], "Important", nil, "Alarm")
+		end
+	elseif msg:find(L["phase2_trigger"]) then
+		phase = 2
+		if db.phase then
+			self:IfMessage(L["phase2_warning"], "Important", nil, "Alarm")
+		end
+	elseif msg:find(L["phase3_trigger"]) then
+		phase = 3
+		if db.phase then
+			self:IfMessage(L["phase3_warning"], "Important", nil, "Alarm")
+		end
+	end
+end
