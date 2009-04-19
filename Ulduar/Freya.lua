c8498215687c361928f728b2a9db1a9d0a9970b5
funkydude <funkydude@users.noreply.github.com>
2009-04-19 23:08:48 +0000

    Ulduar: Bandage API updates

--- Ulduar/Freya.lua
+++ Ulduar/Freya.lua
@@ -9,30 +9,7 @@ mod.zonename = BZ["Ulduar"]
 mod.enabletrigger = boss
 mod.guid = 32906
 mod.toggleoptions = {"phase", -1, "wave", "attuned", "fury", "sunbeam", -1, "icon", "berserk", "bosskill"}
-local bandages = {
-	[34722] = true, -- Heavy Frostweave Bandage
-	[34721] = true, -- Frostweave Bandage
-	[21991] = true, -- Heavy Netherweave Bandage
-	[21990] = true, -- Netherweave Bandage
-	[14530] = true, -- Heavy Runecloth Bandage
-	[14529] = true, -- Runecloth Bandage
-	[8545] = true, -- Heavy Mageweave Bandage
-	[8544] = true, -- Mageweave Bandage
-	[6451] = true, -- Heavy Silk Bandage
-	[6450] = true, -- Silk Bandage
-	[3531] = true, -- Heavy Wool Bandage
-	[3530] = true, -- Wool Bandage
-	[2581] = true, -- Heavy Linen Bandage
-	[1251] = true, -- Linen Bandage
-}
-mod.proximityCheck = function( unit )
-	for k, v in pairs( bandages ) do
-		if IsItemInRange( k, unit) == 1 then
-			return true
-		end
-	end
-	return false
-end
+mod.proximityCheck = "bandage"
 
 ------------------------------
 --      Are you local?      --
