dae1b137cc4ec4f0605497443c4afaadab39fe52
Caleb <calebzor@gmail.com>
2009-07-23 10:49:48 +0000

    removed: pact added: shield/vortex cooldown, touch warning, enrage timer tuned: vortex and shield warning

--- Coliseum/ValkyrTwins.lua
+++ Coliseum/ValkyrTwins.lua
@@ -7,18 +7,20 @@ local edyis = BB["Edyis Darkbane"]
 local fjola = BB["Fjola Lightbane"]
 local mod = BigWigs:New(boss, "$Revision$")
 if not mod then return end
-mod.zonename = BZ["Trial of the Crusader"]	--need the add name translated, maybe add to BabbleZone.
+mod.zonename = BZ["Trial of the Crusader"]
 mod.otherMenu = "The Argent Coliseum"
 mod.enabletrigger = { edyis, fjola }
 mod.guid = 34496
 --34496 Darkbane
 --34497 Lightbane
-mod.toggleoptions = {"vortex", "pact", "shield", "bosskill"}
+mod.toggleoptions = {"vortex", "shield", "berserk", "touch", "bosskill"}
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
+local db = nil
+local pName = UnitName("player")
 local essenceLight = GetSpellInfo(67223)
 local essenceDark = GetSpellInfo(67176)
 
@@ -32,14 +34,10 @@ L:RegisterTranslations("enUS", function() return {
 
 	engage_trigger1 = "In the name of our dark master. For the Lich King. You. Will. Die.",	
 	
+	vortex_or_shield_cd = "Vortex/Shield Cooldown",
+	
 	vortex = "Vortex",
 	vortex_desc = "Warn when the twins start casting Vortexes.",
-	
-	pact = "Twin's Pact",
-	pact_desc = "Warn when the twins heal eachother.",
-	
-	pact_dark_message = "Darkbane healing!",
-	pact_light_message = "Lightbane healing!",
 	
 	shield = "Shield of Darkness/Light",
 	shield_desc = "Warn for Shield of Darkness/Light.",
@@ -49,18 +47,20 @@ L:RegisterTranslations("enUS", function() return {
 	--vortex_same_message = "Vortex", -- If the player has the same affliction, we don't need to say anything really
 	vortex_light_message = "Light vortex!",
 	vortex_dark_message = "Dark vortex!",
+	
+	touch = "Touch of Darkness/Light",
+	touch_desc = "Warn for Touch of Darkness/Light",
+	touch_of_light_on_you = "Touch of Light on You!",
+	touch_of_light_on_other = "Touch of Light on %s",
+	touch_of_dark_on_you = "Touch of Darkness on You!",
+	touch_of_dark_on_other = "Touch of Darkness on %s",
+	
 } end)
 L:RegisterTranslations("koKR", function() return {
 	engage_trigger1 = "In the name of our dark master. For the Lich King. You. Will. Die.",	--check
 	
 	vortex = "소용돌이",
 	vortex_desc = "쌍둥이의 소용돌이 시전을 알립니다.",
-	
-	pact = "쌍둥이의 서약",
-	pact_desc = "쌍둥이가 서로 치유시 알립니다.",
-	
-	pact_dark_message = "파멸의 암흑 치유!",	--check
-	pact_light_message = "파멸의 빛 치유!",	--check
 	
 	shield = "어둠/빛의 방패",
 	shield_desc = "어둠/빛의 방패를 알립니다.",
@@ -87,64 +87,93 @@ L:RegisterTranslations("ruRU", function() return {
 --
 
 function mod:OnEnable()
-	self:AddCombatListener("SPELL_CAST_START", "DarkPact", 67303)
-	self:AddCombatListener("SPELL_CAST_START", "LightPact", 67306)
-	self:AddCombatListener("SPELL_CAST_START", "LightVortex", 67206)
-	self:AddCombatListener("SPELL_CAST_START", "DarkVortex", 67182)
-	self:AddCombatListener("SPELL_AURA_APPLIED", "Shield", 67257, 67260)
+	self:AddCombatListener("SPELL_CAST_START", "LightVortex", 67206, 67208)
+	self:AddCombatListener("SPELL_CAST_START", "DarkVortex", 67182, 67184)
+	self:AddCombatListener("SPELL_AURA_APPLIED", "DarkShield", 67257, 67258)
+	self:AddCombatListener("SPELL_AURA_APPLIED", "LightShield", 67260, 67261) 
+	self:AddCombatListener("SPELL_AURA_APPLIED", "DarkTouch", 67283) 
+	self:AddCombatListener("SPELL_AURA_APPLIED", "LightTouch", 67298) 
 	self:AddCombatListener("UNIT_DIED", "BossDeath")
+	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	db = self.db.profile
 	--self:RegisterEvent("")
 end
---[[
-7/9 21:32:04.937  SPELL_CAST_SUCCESS,0x0100000000069322,"Zkygge",0x40514,0xF1300086C1001F13,"Fjola Lightbane",0x110a48,56222,"Dark Command",0x1
-7/9 21:32:50.765  SPELL_CAST_START,0xF1300086C0001F14,"Eydis Darkbane",0x8000a48,0x0000000000000000,nil,0x80000000,67303,"Twin's Pact",0x20
-7/9 21:34:20.312  SPELL_CAST_START,0xF1300086C1001F13,"Fjola Lightbane",0x100a48,0x0000000000000000,nil,0x80000000,67306,"Twin's Pact",0x40
-7/9 21:36:35.562  SPELL_CAST_START,0xF1300086C0001F14,"Eydis Darkbane",0x8010a48,0x0000000000000000,nil,0x80000000,67303,"Twin's Pact",0x20
-7/9 21:40:49.671  SPELL_CAST_START,0xF1300086C10032E2,"Fjola Lightbane",0x110a48,0x0000000000000000,nil,0x80000000,67306,"Twin's Pact",0x40
-7/9 21:43:04.859  SPELL_CAST_START,0xF1300086C00032E3,"Eydis Darkbane",0xa48,0x0000000000000000,nil,0x80000000,67303,"Twin's Pact",0x20
-2 minute CD on heal? Perhaps 1min from engage. Need transcript logs to verify.
-]]
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:Shield(_, spellID)
-	if db.shield then
-		if spellID == 67257 then
-			self:IfMessage(L["shield_darkness"], "Attention", spellID)
-		elseif spellID == 67260 then
-			self:IfMessage(L["shield_light"], "Attention", spellID)
-		end
+function mod:DarkTouch(_, spellID)
+	if db.touch then
+		if player == pName then
+			self:LocalMessage(L["touch_of_dark_on_you"], "Personal", spellID, "Alarm")
+			self:WideMessage(L["touch_of_dark_on_other"]:format(player))
+		else
+			self:TargetMessage(L["touch_of_dark_on_other"], player, "Attention", spellID)
+			self:Whisper(player, L["touch_of_dark_on_you"])
+		end		
 	end
 end
 
-function mod:DarkPact(_, spellId)
-	if not db.pact then return end
-	self:IfMessage(L["pact_dark_message"], "Important", spellId)
+function mod:LightTouch(_, spellID)
+	if db.touch then
+		if player == pName then
+			self:LocalMessage(L["touch_of_light_on_you"], "Personal", spellID, "Alarm")
+			self:WideMessage(L["touch_of_light_on_other"]:format(player))
+		else
+			self:TargetMessage(L["touch_of_light_on_other"], player, "Attention", spellID)
+			self:Whisper(player, L["touch_of_light_on_you"])
+		end		
+	end
 end
-function mod:LightPact(_, spellId)
-	if not db.pact then return end
-	self:IfMessage(L["pact_light_message"], "Important", spellId)
+
+function mod:DarkShield(_, spellID)
+	if db.shield then
+		self:Bar(L["vortex_or_shield_cd"], 45, 39089)
+		self:IfMessage(L["shield_darkness"], "Attention", spellID)
+		local d = UnitDebuff("player", essenceDark)
+		if not d then return end
+		self:LocalMessage(L["shield_darkness"], "Important", spellID, "Alert")
+	end
+end
+
+function mod:LightShield(_, spellID)
+	if db.shield then
+		self:Bar(L["vortex_or_shield_cd"], 45, 39089)
+		self:IfMessage(L["shield_light"], "Attention", spellID)
+		local d = UnitDebuff("player", essenceLight)
+		if not d then return end
+		self:LocalMessage(L["shield_light"], "Important", spellID, "Alert")
+	end
 end
 
 function mod:LightVortex(_, spellId)
-	local d = UnitDebuff("player", essenceDark)
-	if not d then return end
-	self:IfMessage(L["vortex_light_message"], "Personal", spellId)
-end
-
-function mod:DarkVortex(_, spellId)
-	local d = UnitDebuff("player", essenceLight)
-	if not d then return end
-	self:IfMessage(L["vortex_dark_message"], "Personal", spellId)
-end
-
-function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if msg:find(L["engage_trigger1"]) then
-
+	if vortex then
+		self:Bar(L["vortex_or_shield_cd"], 45, 39089)
+		local d = UnitDebuff("player", essenceDark)
+		if not d then return end
+		self:IfMessage(L["vortex_light_message"], "Personal", spellId, "Alarm")
 	end
 end
 
+function mod:DarkVortex(_, spellId)
+	if vortex then
+		self:Bar(L["vortex_or_shield_cd"], 45, 39089)
+		local d = UnitDebuff("player", essenceLight)
+		if not d then return end
+		self:IfMessage(L["vortex_dark_message"], "Personal", spellId, "Alarm")
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg, sender)
+	if msg == L["engage_trigger1"] and sender == edyis then
+		if db.shield or db.vortex then
+			self:Bar(L["vortex_or_shield_cd"], 45, 39089)
+		end
+		if db.berserk then
+			self:Enrage(360, true)
+		end
+	end
+end
