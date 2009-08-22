9c04e6e9f79a4cebb279aec7ff5fcbac1caed8d9
Vidar Haarr <vhaarr@gmail.com>
2009-08-22 23:24:07 +0000

    Convert Thorim and XT002 to spellId options.

--- Ulduar/Thorim.lua
+++ Ulduar/Thorim.lua
@@ -9,7 +9,7 @@ local behemoth = BB["Jormungar Behemoth"]
 mod.zonename = BZ["Ulduar"]
 mod.enabletrigger = {behemoth, boss}
 mod.guid = 32865	--Sif(33196)
-mod.toggleoptions = {"phase", "p2berserk", "hardmode", -1, "hammer", "impale", "shock", "barrier", "detonation", "charge", "strike", -1, "icon", "proximity", "bosskill"}
+mod.toggleoptions = {"phase", "p2berserk", "hardmode", -1, 62042, 62331, 62017, 62338, 62526, 62279, 62130, -1, "icon", "proximity", "bosskill"}
 mod.proximityCheck = function(unit) return CheckInteractDistance(unit, 3) end
 mod.proximitySilent = true
 mod.consoleCmd = "Thorim"
@@ -42,41 +42,21 @@ L:RegisterTranslations("enUS", function() return {
 	p2berserk = "Phase 2 - Berserk",
 	p2berserk_desc = "Warn when the boss goes Berserk in Phase 2.",
 
-	hardmode = "Hard Mode Timer",
-	hardmode_desc = "Show Timer for Hard Mode.",
-	hardmode_warning = "Hard Mode Expires",
+	hardmode = "Hard mode",
+	hardmode_desc = "Show timer for hard mode.",
+	hardmode_warning = "Hard mode expires",
 
-	hammer = "Stormhammer",
-	hammer_desc = "Warns for Stormhammer.",
 	hammer_message = "Hammer on %s",
-	hammer_bar = "Stormhammer",
-
-	impale = "Impale",
-	impale_desc = "Warn who is afflicted by Impale.",
 	impale_message = "Impale on %s",
-
-	shock = "Lightning Shock",
-	shock_desc = "Warn for Charge Orb and Lightning Shock.",
 	shock_message = "You're getting shocked!",
-	shock_warning = "Charge Orb!",
-	shock_bar = "Charge Orb",
-
-	barrier = "Runic Barrier",
-	barrier_desc = "Warn when Runic Colossus gains Runic Barrier.",
 	barrier_message = "Barrier up!",
 
-	detonation = "Rune Detonation",
-	detonation_desc = "Tells you who has been hit by Rune Detonation.",
 	detonation_message = "Bomb on %s!",
-	detonation_yell = "I'm a bomb!",
+	detonation_say = "I'm a bomb!",
 
-	charge = "Lightning Charge",
-	charge_desc = "Count and warn for Thorim's Lightning Charge.",
 	charge_message = "Charged x%d!",
 	charge_bar = "Charge %d",
 
-	strike = "Unbalancing Strike",
-	strike_desc = "Warn when a player has Unbalancing Strike.",
 	strike_message= "Unbalancing Strike: %s",
 	strike_bar = "Unbalancing Strike CD",
 
@@ -102,37 +82,17 @@ L:RegisterTranslations("koKR", function() return {
 	hardmode_desc = "도전 모드의 시간을 표시합니다.",
 	hardmode_warning = "도전 모드 종료",
 
-	hammer = "폭풍망치",
-	hammer_desc = "폭풍망치를 알립니다.",
 	hammer_message = "폭풍망치: %s",
-	hammer_bar = "다음 폭풍망치",
-
-	impale = "꿰뚫기",
-	impale_desc = "꿰뚫기에 걸린 플레이어를 알립니다.",
 	impale_message = "꿰뚫기: %s",
-
-	shock = "번개 충격",
-	shock_desc = "번개구 충전과 번개 충격을 알립니다.",
 	shock_message = "당신은 번개 충격! 이동!",
-	shock_warning = "번개구 충전!",
-	shock_bar = "번개구 충전",
-
-	barrier = "룬문자 방벽",
-	barrier_desc = "룬문자 거인의 룬문자 방벽 획득을 알립니다.",
 	barrier_message = "거인 - 룬문자 방벽!",
 
-	detonation = "룬 폭발",
-	detonation_desc = "룬 폭발에 걸린 플레이어를 알립니다.",
 	detonation_message = "룬 폭발: %s",
-	detonation_yell = "저 푹탄이에요! 피하세요!",
+	detonation_say = "저 푹탄이에요! 피하세요!",
 
-	charge = "번개 충전",
-	charge_desc = "토림의 번개 충전과 횟수를 알립니다.",
 	charge_message = "충전 (%d)!",
 	charge_bar = "충전 (%d)",
 
-	strike = "혼란의 일격",
-	strike_desc = "혼란의 일격에 걸린 플레이어를 알립니다.",
 	strike_message= "혼란의 일격: %s",
 	strike_bar = "혼란의 일격 대기시간",
 
@@ -158,37 +118,17 @@ L:RegisterTranslations("frFR", function() return {
 	hardmode_desc = "Affiche une barre de 3 minutes pour le mode difficile (délai avant que Sif ne disparaisse).",
 	hardmode_warning = "Délai du mode difficile dépassé",
 
-	hammer = "Marteau-tempête",
-	hammer_desc = "Affiche une barre indiquant le prochain Marteau-tempête.",
 	hammer_message = "Marteau-tempête : %s",
-	hammer_bar = "Marteau-tempête",
-
-	impale = "Empaler",
-	impale_desc = "Prévient quand un joueur subit les effets d'un Empaler.",
 	impale_message = "Empaler : %s",
-
-	shock = "Horion de foudre",
-	shock_desc = "Prévient de l'arrivée des Horions de foudre et des Charger l'orbe.",
 	shock_message = "Horion de foudre sur VOUS !",
-	shock_warning = "Charger l'orbe !",
-	shock_bar = "Charger l'orbe",
-
-	barrier = "Barrière runique",
-	barrier_desc = "Prévient quand le Colosse runique gagne une Barrière runique.",
 	barrier_message = "Barrière runique actif !",
 
-	detonation = "Détonation runique",
-	detonation_desc = "Prévient quand un joueur subit les effets d'une Détonation runique.",
 	detonation_message = "Détonation : %s",
-	detonation_yell = "Je suis une bombe !",
+	detonation_say = "Je suis une bombe !",
 
-	charge = "Charge de foudre",
-	charge_desc = "Compte et prévient de l'arrivée des Charges de foudre de Thorim.",
 	charge_message = "Charge de foudre x%d !",
 	charge_bar = "Charge %d",
 
-	strike = "Frappe déséquilibrante",
-	strike_desc = "Prévient quand un joueur subit les effets d'une Frappe déséquilibrante.",
 	strike_message = "Frappe : %s",
 	strike_bar = "Recharge Frappe",
 
@@ -214,37 +154,17 @@ L:RegisterTranslations("deDE", function() return {
 	hardmode_desc = "Timer für den Hard Mode.",
 	hardmode_warning = "Hard Mode beendet!",
 
-	hammer = "Sturmhammer",
-	hammer_desc = "Warnung und Timer für Sturmhammer.",
 	hammer_message = "Sturmhammer: %s!",
-	hammer_bar = "Sturmhammer",
-
-	impale = "Durchbohren",
-	impale_desc = "Warnt, wer von Durchbohren betroffen ist.",
 	impale_message = "Durchbohren: %s!",
-
-	shock = "Blitzschock",
-	shock_desc = "Warnung und Timer für Kugel aufladen and Blitzschock.",
 	shock_message = "DU wirst geschockt!",
-	shock_warning = "Kugel aufladen!",
-	shock_bar = "Kugel aufladen",
-
-	barrier = "Runenbarriere",
-	barrier_desc = "Warnt, wenn der Runenverzierter Koloss von Runenbarriere betroffen ist.",
 	barrier_message = "Runenbarriere oben!",
 
-	detonation = "Runendetonation",
-	detonation_desc = "Warnt, wer von Runendetonation betroffen ist.",
 	detonation_message = "Bombe: %s!",
-	detonation_yell = "Ich bin die Bombe!",
+	detonation_say = "Ich bin die Bombe!",
 
-	charge = "Blitzladung",
-	charge_desc = "Warnung und Zähler für Thorims Blitzladung.",
 	charge_message = "Blitzladung x%d!",
 	charge_bar = "Blitzladung %d",
 
-	strike = "Schlag des Ungleichgewichts",
-	strike_desc = "Warnt, wenn ein Spieler von Schlag des Ungleichgewichts betroffen ist.",
 	strike_message= "Schlag: %s!",
 	strike_bar = "~Schlag",
 
@@ -270,37 +190,17 @@ L:RegisterTranslations("zhCN", function() return {
 	hardmode_desc = "显示困难模式计时器。",
 	hardmode_warning = "困难模式结束！",
 
-	hammer = "风暴之锤",
-	hammer_desc = "当风暴之锤时发出警报。",
 	hammer_message = "风暴之锤：>%s<！",
-	hammer_bar = "<风暴之锤>",
-
-	impale = "穿刺",
-	impale_desc = "当玩家中了穿刺时发出警报。",
 	impale_message = "穿刺：>%s<！",
-
-	shock = "闪电震击",
-	shock_desc = "当宝珠充电和闪电震击时发出警报。",
 	shock_message = ">你< 闪电震击！移动！",
-	shock_warning = "宝珠充电！",
-	shock_bar = "<下一宝珠充电>",
-
-	barrier = "符文屏障",
-	barrier_desc = "当符文巨像获得符文屏障时发出警报。",
 	barrier_message = "符文巨像 - 符文屏障！",
 
-	detonation = "符文爆裂",
-	detonation_desc = "当玩家中了符文爆裂时发出警报。",
 	detonation_message = "符文爆裂：>%s<！",
-	detonation_yell = "我是炸弹！",
+	detonation_say = "我是炸弹！",
 
-	charge = "闪电充能",
-	charge_desc = "当托里姆施放闪电充能时记数警报。",
 	charge_message = "闪电充能：>%d<！",
 	charge_bar = "<闪电充能：%d>",
 
-	strike = "重压打击",
-	strike_desc = "当玩家中了重压打击时发出警报。",
 	strike_message= "重压打击：>%s<！",
 	strike_bar = "<重压打击 冷却>",
 
@@ -326,37 +226,17 @@ L:RegisterTranslations("zhTW", function() return {
 	hardmode_desc = "顯示困難模式計時器。",
 	hardmode_warning = "困難模式結束！",
 
-	hammer = "風暴之錘",
-	hammer_desc = "當風暴之錘時發出警報。",
 	hammer_message = "風暴之錘：>%s<！",
-	hammer_bar = "<風暴之錘>",
-
-	impale = "刺穿",
-	impale_desc = "當玩家中了刺穿時發出警報。",
 	impale_message = "刺穿：>%s<！",
-
-	shock = "閃電震擊",
-	shock_desc = "當寶珠充能和閃電震擊時發出警報。",
 	shock_message = ">你< 閃電震擊！移動！",
-	shock_warning = "寶珠充能！",
-	shock_bar = "<下一寶珠充能>",
-
-	barrier = "符刻屏障",
-	barrier_desc = "當符刻巨像獲得符刻屏障時發出警報。",
 	barrier_message = "符文巨像 - 符刻屏障！",
 
-	detonation = "引爆符文",
-	detonation_desc = "當玩家中了引爆符文時發出警報。",
 	detonation_message = "引爆符文：>%s<！",
-	detonation_yell = "我是炸彈！",
+	detonation_say = "我是炸彈！",
 
-	charge = "閃電能量",
-	charge_desc = "當索林姆施放閃電能量時記數警報。",
 	charge_message = "閃電能量：>%d<！",
 	charge_bar = "<閃電能量：%d>",
 
-	strike = "失衡打擊",
-	strike_desc = "當玩家中了失衡打擊時發出警報。",
 	strike_message= "失衡打擊：>%s<！",
 	strike_bar = "<失衡打擊 冷卻>",
 
@@ -382,37 +262,17 @@ L:RegisterTranslations("ruRU", function() return {
 	hardmode_desc = "Отображения таймера для сложного режима.",
 	hardmode_warning = "Завершение сложного режима",
 
-	hammer = "Молот бури",
-	hammer_desc = "Сообщает о применении Молота бури.",
 	hammer_message = "Молот брошен в |3-3(%s)",
-	hammer_bar = "Следующий Молот",
-
-	impale = "Прокалывание",
-	impale_desc = "Сообщает кто поражен Прокалыванием.",
 	impale_message = "%s проколот! Лечите!", 
-
-	shock = "Поражение громом",
-	shock_desc = "Сообщать о Заряженных сферах и Поражении громом.",
 	shock_message = "На вас Поражение громом! Шевелитесь!",
-	shock_warning = "Заряженная сфера!",
-	shock_bar = "Следующая сфера",
-
-	barrier = "Руническая преграда",
-	barrier_desc = "Сообщать когда Рунический колосс подвергается воздействию Рунической преграды.",
 	barrier_message = "Колосс под Рунической преградой!",
 
-	detonation = "Взрыв руны",
-	detonation_desc = "Сообщает кто попал под воздействие Взрыва рун.",
 	detonation_message = "Бомба у: |3-1(%s)",
-	detonation_yell = "Я БОМБА!",
+	detonation_say = "Я БОМБА!",
 
-	charge = "Разряд молнии",
-	charge_desc = "Подсчитывать и сообщать о разрядах молнии Торима.",
 	charge_message = "Разряд: x%d",
 	charge_bar = "Разряд %d",
 
-	strike = "Дисбалансирующий удар",
-	strike_desc = "Сообщает когда игрок получает Дисбалансирующий удар.",
 	strike_message= "Удар по: |3-2(%s)",
 
 	end_trigger = "Придержите мечи! Я сдаюсь.",
@@ -453,47 +313,35 @@ end
 --      Event Handlers      --
 ------------------------------
 
-function mod:Barrier(_, spellId)
-	if db.barrier then
-		self:IfMessage(L["barrier_message"], "Urgent", spellId, "Alarm")
-		self:Bar(L["barrier"], 20, spellId)
-	end
+function mod:Barrier(_, spellId, _, _, spellName)
+	self:IfMessage(L["barrier_message"], "Urgent", spellId, "Alarm")
+	self:Bar(spellName, 20, spellId)
 end
 
 function mod:Charge(_, spellId)
-	if db.charge then
-		self:IfMessage(L["charge_message"]:format(chargeCount), "Attention", spellId)
-		chargeCount = chargeCount + 1
-		self:Bar(L["charge_bar"]:format(chargeCount), 15, spellId)
-	end
+	self:IfMessage(L["charge_message"]:format(chargeCount), "Attention", spellId)
+	chargeCount = chargeCount + 1
+	self:Bar(L["charge_bar"]:format(chargeCount), 15, spellId)
 end
 
-function mod:Hammer(player, spellId)
-	if db.hammer then
-		self:TargetMessage(L["hammer_message"], player, "Urgent", spellId)
-		self:Bar(L["hammer_bar"], 16, spellId)
-		self:Icon(player, "icon")
-	end
+function mod:Hammer(player, spellId, _, _, spellName)
+	self:TargetMessage(L["hammer_message"], player, "Urgent", spellId)
+	self:Bar(spellName, 16, spellId)
+	self:Icon(player, "icon")
 end
 
 function mod:Strike(player, spellId)
-	if db.strike then
-		self:TargetMessage(L["strike_message"], player, "Attention", spellId)
-		self:Bar(L["strike_message"]:format(player), 15, spellId)
-	end
+	self:TargetMessage(L["strike_message"], player, "Attention", spellId)
+	self:Bar(L["strike_message"]:format(player), 15, spellId)
 end
 
 function mod:StrikeCooldown(player, spellId)
-	if db.strike then
-		self:Bar(L["strike_bar"], 25, spellId)
-	end
+	self:Bar(L["strike_bar"], 25, spellId)
 end
 
-function mod:Orb(_, spellId)
-	if db.shock then
-		self:IfMessage(L["shock_warning"], "Urgent", spellId)
-		self:Bar(L["shock_bar"], 15, spellId)
-	end
+function mod:Orb(_, spellId, _, _, spellName)
+	self:IfMessage(spellName, "Urgent", spellId)
+	self:Bar(spellName, 15, spellId)
 end
 
 local last = 0
@@ -501,29 +349,25 @@ function mod:Shock(player, spellId)
 	local time = GetTime()
 	if (time - last) > 5 then
 		last = time
-		if player == pName and db.shock then
+		if player == pName then
 			self:LocalMessage(L["shock_message"], "Personal", spellId, "Info")
 		end
 	end
 end
 
 function mod:Impale(player, spellId)
-	if db.impale then
-		self:TargetMessage(L["impale_message"], player, "Important", spellId)
-	end
+	self:TargetMessage(L["impale_message"], player, "Important", spellId)
 end
 
 function mod:Detonation(player, spellId)
-	if db.detonation then
-		if player == pName then
-			self:WideMessage(L["detonation_message"]:format(player))
-			SendChatMessage(L["detonation_yell"], "SAY")
-		else
-			self:TargetMessage(L["detonation_message"], player, "Important", spellId)
-		end
-		self:Bar(L["detonation_message"]:format(player), 4, spellId)
-		self:Icon(player, "icon")
+	if player == pName then
+		self:WideMessage(L["detonation_message"]:format(player))
+		SendChatMessage(L["detonation_say"], "SAY")
+	else
+		self:TargetMessage(L["detonation_message"], player, "Important", spellId)
 	end
+	self:Bar(L["detonation_message"]:format(player), 4, spellId)
+	self:Icon(player, "icon")
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
@@ -539,12 +383,8 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 			self:DelayedMessage(173, L["hardmode_warning"], "Attention")
 		end
 	elseif msg == L["phase3_trigger"] then
-		local berserkBar = GetSpellInfo(43)
-		self:TriggerEvent("BigWigs_StopBar", self, berserkBar)
-		self:TriggerEvent("BigWigs_StopBar", self, L["shock_bar"])
-		self:TriggerEvent("BigWigs_StopBar", self, L["hammer_bar"])
-		self:TriggerEvent("BigWigs_StopBar", self, L["hardmode"])
 		self:CancelAllScheduledEvents()
+		self:TriggerEvent("BigWigs_StopBars", self)
 		if db.phase then
 			self:IfMessage(L["phase3_message"]:format(boss), "Attention")
 		end
--- Ulduar/XT-002.lua
+++ Ulduar/XT-002.lua
@@ -8,7 +8,7 @@ if not mod then return end
 mod.zonename = BZ["Ulduar"]
 mod.enabletrigger = boss
 mod.guid = 33293
-mod.toggleoptions = {"heartbreak", "gravitybombicon", "exposed", -1, "gravitybomb", "lightbomb", "proximity", "berserk", "tantrum", "bosskill"}
+mod.toggleoptions = {64193, "gravitybombicon", 63849, -1, 63024, 63018, 62776, "proximity", "berserk", "bosskill"}
 mod.proximityCheck = "bandage"
 mod.consoleCmd = "XT"
 
@@ -31,31 +31,18 @@ local exposed3 = nil
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 
 L:RegisterTranslations("enUS", function() return {
-	exposed = "Exposed Heart",
-	exposed_desc = "Warn when XT-002 gains Exposed Heart.",
 	exposed_warning = "Exposed soon",
 	exposed_message = "Heart exposed!",
 
-	gravitybomb = "Gravity Bomb",
-	gravitybomb_desc = "Tells you who has been hit by Gravity Bomb.",
 	gravitybomb_you = "Gravity on YOU!",
 	gravitybomb_other = "Gravity on %s!",
 
 	gravitybombicon = "Gravity Bomb Icon",
 	gravitybombicon_desc = "Place a Blue Square icon on the player effected by Gravity Bomb. (requires promoted or higher)",
 
-	lightbomb = "Light Bomb",
-	lightbomb_desc = "Tells you who has been hit by Light Bomb.",
 	lightbomb_you = "Light on YOU!",
 	lightbomb_other = "Light on %s!",
 
-	heartbreak = "Heartbreak",
-	heartbreak_desc = "Warn when XT-002 gains Heartbreak",
-	heartbreak_message = "Heartbreak!",
-
-	tantrum = "Tympanic Tantrum",
-	tantrum_desc = "Warn when XT-002 casts Tympanic Tantrum in Hard Mode",
-	tantrum_message = "Tympanic Tantrum!",
 	tantrum_bar = "~Tantrum Cooldown",
 
 	icon = "Raid Icon",
@@ -63,31 +50,18 @@ L:RegisterTranslations("enUS", function() return {
 } end )
 
 L:RegisterTranslations("koKR", function() return {
-	exposed = "심장 노출",
-	exposed_desc = "XT-002의 심장 노출 획득을 알립니다.",
 	exposed_warning = "잠시 후 심장 노출!",
 	exposed_message = "심장 노출 - 로봇들 추가!",
 
-	gravitybomb = "중력 폭탄",
-	gravitybomb_desc = "중력 폭탄에 걸린 플레이어를 알립니다.",
 	gravitybomb_you = "당신은 중력 폭탄!",
 	gravitybomb_other = "중력 폭탄: %s!",
 
 	gravitybombicon = "중력 폭탄 아이콘",
 	gravitybombicon_desc = "중력 폭탄에 걸린 플레이어를 네모 전술로 지정합니다. (승급자 이상 권한 필요)",
 
-	lightbomb = "빛의 폭탄",
-	lightbomb_desc = "빛의 폭탄에 걸린 플레이어를 알립니다.",
 	lightbomb_you = "당신은 빛의 폭탄!",
 	lightbomb_other = "빛의 폭탄: %s!",
 
-	heartbreak = "부서진 심장",
-	heartbreak_desc = "XT-002의 부서진 심장 획득을 알립니다.",
-	heartbreak_message = "심장 파괴됨!",
-
-	tantrum = "격분의 땅울림",
-	tantrum_desc = "XT-002의 도전 모드시에 격분의 땅울림 시전을 알립니다.",
-	tantrum_message = "격분의 땅울림!",
 	tantrum_bar = "~땅울림 대기시간",
 
 	icon = "전술 표시",
@@ -95,31 +69,18 @@ L:RegisterTranslations("koKR", function() return {
 } end )
 
 L:RegisterTranslations("frFR", function() return {
-	exposed = "Cœur exposé",
-	exposed_desc = "Prévient quand le cœur du XT-002 est exposé.",
 	exposed_warning = "Cœur exposé imminent",
 	exposed_message = "Cœur exposé !",
 
-	gravitybomb = "Bombe à gravité",
-	gravitybomb_desc = "Prévient quand un joueur subit les effets d'une Bombe à gravité.",
 	gravitybomb_you = "Bombe à gravité sur VOUS !",
 	gravitybomb_other = "Gravité : %s",
 
 	gravitybombicon = "Bombe à gravité - Icône",
 	gravitybombicon_desc = "Place une icône de raid bleue sur le dernier joueur affecté par une Bombe à gravité (nécessite d'être assistant ou mieux).",
 
-	lightbomb = "Lumière incendiaire",
-	lightbomb_desc = "Prévient quand un joueur subit les effets d'une Lumière incendiaire.",
 	lightbomb_you = "Lumière incendiaire sur VOUS !",
 	lightbomb_other = "Lumière : %s",
 
-	heartbreak = "Bris du cœur",
-	heartbreak_desc = "Prévient quand le cœur du XT-002 est brisé.",
-	heartbreak_message = "Bris du cœur !",
-
-	tantrum = "Colère assourdissante",
-	tantrum_desc = "Prévient quand le XT-002 incante une Colère assourdissante en mode difficile.",
-	tantrum_message = "Colère assourdissante!",
 	tantrum_bar = "~Recharge Colère",
 
 	icon = "Icône",
@@ -127,31 +88,18 @@ L:RegisterTranslations("frFR", function() return {
 } end )
 
 L:RegisterTranslations("deDE", function() return {
-	exposed = "Freigelegtes Herz",
-	exposed_desc = "Warnt, wenn XT-002 ein Freigelegtes Herz hat.",
 	exposed_warning = "Freigelegtes Herz bald!",
 	exposed_message = "Herz freigelegt!",
 
-	gravitybomb = "Gravitationsbombe",
-	gravitybomb_desc = "Warnt, wer von Gravitationsbombe getroffen wurde.",
 	gravitybomb_you = "Gravitationsbombe auf DIR!",
 	gravitybomb_other = "Gravitationsbombe: %s!",
 	
 	gravitybombicon = "Gravitationsbombe: Symbol",
 	gravitybombicon_desc = "Platziert ein blaues Quadrat auf Spielern, die von Gravitationsbombe getroffen werden (benötigt Assistent oder höher).",
 
-	lightbomb = "Lichtbombe",
-	lightbomb_desc = "Warnt, wer von Lichtbombe getroffen wurde.",
 	lightbomb_you = "Lichtbombe auf DIR!",
 	lightbomb_other = "Lichtbombe: %s!",
 
-	heartbreak = "Gebrochenes Herz",
-	heartbreak_desc = "Warnt, wenn der XT-002 Gebrochenes Herz bekommt.",
-	heartbreak_message = "Gebrochenes Herz!",
-
-	tantrum = "Betäubender Koller",
-	tantrum_desc = "Warnung und Timer für Betäubender Koller im Hard Mode.",
-	tantrum_message = "Betäubender Koller!",
 	tantrum_bar = "~Betäubender Koller",
 
 	icon = "Schlachtzugs-Symbol",
@@ -159,31 +107,18 @@ L:RegisterTranslations("deDE", function() return {
 } end )
 
 L:RegisterTranslations("zhCN", function() return {
-	exposed = "暴露心脏",
-	exposed_desc = "当 XT-002拆解者获得暴露心脏时发出警报。",
 	exposed_warning = "即将 暴露心脏！",
 	exposed_message = "暴露心脏！",
 
-	gravitybomb = "重力炸弹",
-	gravitybomb_desc = "当玩家中了重力炸弹时发出警报。",
 	gravitybomb_you = ">你< 重力炸弹！",
 	gravitybomb_other = "重力炸弹：>%s<！",
 
 	gravitybombicon = "重力炸弹标记",
 	gravitybombicon_desc = "为中了重力炸弹的玩家打上蓝色方框标记。（需要权限）",
 
-	lightbomb = "灼热之光",
-	lightbomb_desc = "当玩家中了灼热之光时发出警报。",
 	lightbomb_you = ">你< 灼热之光！",
 	lightbomb_other = "灼热之光：>%s<！",
 
-	heartbreak = "心碎",
-	heartbreak_desc = "当 XT-002拆解者获得心碎时发出警报。",
-	heartbreak_message = "心碎！",
-
-	tantrum = "发脾气",
-	tantrum_desc = "当困难模式 XT-002拆解者施放发脾气时发出警报。",
-	tantrum_message = "发脾气！",
 	tantrum_bar = "<发脾气 冷却>",
 
 	icon = "团队标记",
@@ -191,31 +126,18 @@ L:RegisterTranslations("zhCN", function() return {
 } end )
 
 L:RegisterTranslations("zhTW", function() return {
-	exposed = "機心外露",
-	exposed_desc = "當 XT-002拆解者獲得機心外露時發出警報。",
 	exposed_warning = "即將 機心外露！",
 	exposed_message = "機心外露！",
 
-	gravitybomb = "重力彈",
-	gravitybomb_desc = "當玩家中了重力彈時發出警報。",
 	gravitybomb_you = ">你< 重力彈！",
 	gravitybomb_other = "重力彈：>%s<！",
 
 	gravitybombicon = "重力彈標記",
 	gravitybombicon_desc = "為中了重力彈的玩家打上藍色方框標記。（需要權限）",
 
-	lightbomb = "灼熱之光",
-	lightbomb_desc = "當玩家中了灼熱之光時發出警報。",
 	lightbomb_you = ">你< 灼熱之光！",
 	lightbomb_other = "灼熱之光：>%s<！",
 
-	heartbreak = "心碎",
-	heartbreak_desc = "當 XT-002拆解者獲得心碎時發出警報。",
-	heartbreak_message = "心碎！",
-
-	tantrum = "躁怒",
-	tantrum_desc = "當困難模式 XT-002拆解者施放躁怒時發出警報。",
-	tantrum_message = "躁怒！",
 	tantrum_bar = "<躁怒 冷卻>",
 
 	icon = "團隊標記",
@@ -223,31 +145,18 @@ L:RegisterTranslations("zhTW", function() return {
 } end )
 
 L:RegisterTranslations("ruRU", function() return {
-	exposed = "Обнаженное сердце",
-	exposed_desc = "Сообщать, когда XT-002 обнажает сердце.",
 	exposed_warning = "Скоро сердце станет уязвимо!",
 	exposed_message = "Сердце уязвимо!",
 
-	gravitybomb = "Гравитационная бомба",
-	gravitybomb_desc = "Сообщать об игроках с гравитационной бомбой.",
 	gravitybomb_you = "Бомба на ВАС!",
 	gravitybomb_other = "Бомба на |3-5(%s)!",
 	
 	gravitybombicon = "Иконка гравитационной бомбы",
 	gravitybombicon_desc = "Помечать рейдовой иконкой (синим квадратом) игрока с бомбой (необходимо обладать промоутом).",
 
-	lightbomb = "Светлый взрыв",
-	lightbomb_desc = "Сообщать об игроках со светлым взрывом.",
 	lightbomb_you = "Взрыв на ВАС!",
 	lightbomb_other = "Взрыв на |3-5(%s)!",
 
-	heartbreak = "Разрыв сердца",
-	heartbreak_desc = "Сообщать, когда XT-002 получает разрыв сердца",
-	heartbreak_message = "Разрыв сердца!",
-	
-	tantrum = "Раскаты ярости",
-	tantrum_desc = "Сообщать, когда XT-002 в сложном режиме, применяет Раскаты ярости",
-	tantrum_message = "Раскаты ярости!",
 	tantrum_bar = "~Раскаты ярости",
 
 	icon = "Помечать иконкой",
@@ -278,59 +187,51 @@ end
 --      Event Handlers      --
 ------------------------------
 
-function mod:Exposed(_, spellId)
-	if db.exposed then
-		self:IfMessage(L["exposed_message"], "Attention", spellId)
-		self:Bar(L["exposed"], 30, spellId)
-	end
+function mod:Exposed(_, spellId, _, _, spellName)
+	self:IfMessage(L["exposed_message"], "Attention", spellId)
+	self:Bar(spellName, 30, spellId)
 end
 
-function mod:Heartbreak(_, spellId)
+function mod:Heartbreak(_, spellId, _, _, spellName)
 	phase = 2
-	if db.heartbreak then
-		self:IfMessage(L["heartbreak_message"], "Important", spellId)
-	end
+	self:IfMessage(spellName, "Important", spellId)
 end
 
-function mod:Tantrum(_, spellId)
-	if phase == 2 and db.tantrum then
-		self:IfMessage(L["tantrum_message"], "Attention", spellId)
+function mod:Tantrum(_, spellId, _, _, spellName)
+	if phase == 2 then
+		self:IfMessage(spellName, "Attention", spellId)
 		self:Bar(L["tantrum_bar"], 65, spellId)
 	end
 end
 
 function mod:GravityBomb(player, spellId)
-	if db.gravitybomb then
-		if player == pName then
-			self:LocalMessage(L["gravitybomb_you"], "Personal", spellId, "Alert")
-			self:WideMessage(L["gravitybomb_other"]:format(player))
-			self:TriggerEvent("BigWigs_ShowProximity", self)
-		else
-			self:TargetMessage(L["gravitybomb_other"], player, "Attention", spellId)
-			self:Whisper(player, L["gravitybomb_you"])
-		end
-		self:Bar(L["gravitybomb_other"]:format(player), 9, spellId)
-		if db.gravitybombicon then
-			SetRaidTarget(player, 6)
-		else
-			self:Icon(player, "icon")
-		end
+	if player == pName then
+		self:LocalMessage(L["gravitybomb_you"], "Personal", spellId, "Alert")
+		self:WideMessage(L["gravitybomb_other"]:format(player))
+		self:TriggerEvent("BigWigs_ShowProximity", self)
+	else
+		self:TargetMessage(L["gravitybomb_other"], player, "Attention", spellId)
+		self:Whisper(player, L["gravitybomb_you"])
+	end
+	self:Bar(L["gravitybomb_other"]:format(player), 9, spellId)
+	if db.gravitybombicon then
+		SetRaidTarget(player, 6)
+	else
+		self:Icon(player, "icon")
 	end
 end
 
 function mod:LightBomb(player, spellId)
-	if db.lightbomb then
-		if player == pName then
-			self:LocalMessage(L["lightbomb_you"], "Personal", spellId, "Alert")
-			self:WideMessage(L["lightbomb_other"]:format(player))
-			self:TriggerEvent("BigWigs_ShowProximity", self)
-		else
-			self:TargetMessage(L["lightbomb_other"], player, "Attention", spellId)
-			self:Whisper(player, L["lightbomb_you"])
-		end
-		self:Bar(L["lightbomb_other"]:format(player), 9, spellId)
-		self:Icon(player, "icon")
+	if player == pName then
+		self:LocalMessage(L["lightbomb_you"], "Personal", spellId, "Alert")
+		self:WideMessage(L["lightbomb_other"]:format(player))
+		self:TriggerEvent("BigWigs_ShowProximity", self)
+	else
+		self:TargetMessage(L["lightbomb_other"], player, "Attention", spellId)
+		self:Whisper(player, L["lightbomb_you"])
 	end
+	self:Bar(L["lightbomb_other"]:format(player), 9, spellId)
+	self:Icon(player, "icon")
 end
 
 function mod:BombRemoved(player)
@@ -340,7 +241,7 @@ function mod:BombRemoved(player)
 end
 
 function mod:UNIT_HEALTH(msg)
-	if phase == 1 and UnitName(msg) == boss and db.exposed then
+	if phase == 1 and UnitName(msg) == boss and self:GetOption(63849) then
 		local health = UnitHealth(msg)
 		if not exposed1 and health > 86 and health <= 88 then
 			exposed1 = true
