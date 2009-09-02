712537d16710f8235c7042fadc955d92604b082a
Adavak <adavak@adavak.com>
2009-09-02 02:42:42 +0000

    BigWigs: - Configuration interface localization completion. - zhCN/zhTW update.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -19,6 +19,17 @@ L["Minimap icon"] = true
 L["Toggle show/hide of the minimap icon."] = true
 L["Advanced"] = true
 L["You shouldn't really need to touch these options, but if you want to tweak them then you're welcome to do so!"] = true
+L["Welcome to Big Wigs, where the boss encounters roam. Please fasten your seatbelt, eat peanuts and enjoy the ride. It will not eat your children, but it will assist you in preparing that new boss encounter as a 7-course dinner for your raid group.\n\n|cffff0000Note that some (!) of these options do not work at the moment. Please don't file bug reports for things concerning the Big Wigs interface right now, come talk to us on IRC instead.|r\n"] = true
+L["Configure ..."] = true
+L["Closes the interface options window and lets you configure displays for things like bars and messages."] = true
+L["Whisper warnings |cffff0000(!)|r"] = true
+L["Toggles whether you will send a whisper notification to fellow players about certain boss encounter abilities that affect them personally. Think 'bomb'-type effects and such."] = true
+L["Raid icons |cffff0000(!)|r"] = true
+L["Some boss modules use raid icons to mark players in your group that are of special interest to your raid. Things like 'bomb'-type effects and mind control are examples of this. If you turn this off, you won't mark anyone. Note that you need to be promoted to assistant or be the raid leader in order to set these raid icons."] = true
+L["Sound |cffff0000(!)|r"] = true
+L["Some boss messages come with warning sounds of different kinds. Some people find it easier to just listen for these sounds after they've learned which sound goes with which message, instead of reading the actual message on screen."] = true
+L["\n\n\n|cffccccccMooses don't appreciate being prodded with long pointy sticks.\nContact us on #wowace. [Ammo] and Rabbit can service all your needs.|r"] = true
+L["Customize ..."] = true
 
 L["Toggles whether or not the boss module should warn about %s."] = true
 L.bosskill = "Boss death"
