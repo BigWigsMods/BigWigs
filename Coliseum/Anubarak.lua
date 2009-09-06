1b923329c651b5393f60cf815bbf315954bac870
Caleb <calebzor@gmail.com>
2009-09-06 10:06:45 +0000

    added: Nerubian Burrower spawn timer, Penetrating Cold, some heroic spellIds

--- Coliseum/Anubarak.lua
+++ Coliseum/Anubarak.lua
@@ -8,13 +8,15 @@ mod.bossName = boss
 mod.zoneName = "Trial of the Crusader"
 mod.enabletrigger = 34564
 mod.guid = 34564
-mod.toggleOptions = {66118, 67574, "icon", "burrow", "berserk", "bosskill"}
+mod.toggleOptions = {66118, 67574, "icon", "burrow", 68510, "berserk", "bosskill"}
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
 local db
+local pName = UnitName("player")
+local phase2 = nil
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -31,10 +33,14 @@ if L then
 	L.burrow_desc = "Show a timer for Anub'Arak's Burrow ability"
 	L.burrow_cooldown = "Next Burrow"
 	L.burrow_soon = "Burrow soon"
+	
+	L.nerubian_burrower = "Nerubian Burrower"
+	
+	L.pcold_bar = "~Next Penetrating Cold"
 
 	L.icon = "Place icon"
 	L.icon_desc = "Place a raid target icon on the person targetted by Anub'arak during his burrow phase. (requires promoted or higher)"
-
+	
 	L.chase = "Pursue"
 end
 L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Anub'arak")
@@ -46,20 +52,34 @@ mod.locale = L
 
 function mod:OnBossEnable()
 	db = self.db.profile
-	self:AddCombatListener("SPELL_CAST_START", "Swarm", 66118)
+	self:AddCombatListener("SPELL_CAST_START", "Swarm", 66118, 68646, 68647)
+	self:AddCombatListener("SPELL_CAST_SUCCESS", "ColdCooldown", 68510, 68509)
+	self:AddCombatListener("SPELL_AURA_APPLIED", "ColdDebuff", 68510, 68509)
 	self:AddCombatListener("SPELL_AURA_APPLIED", "Pursue", 67574)
 	self:AddCombatListener("UNIT_DIED", "BossDeath")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
+	phase2 = nil
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:ColdDebuff(player, spellId, _, _, spellName)
+	if player ~= pName and not phase2 then return end
+	self:LocalMessage(spellName, "Personal", spellId, "Alarm")
+end
+
+function mod:ColdCooldown(_, spellId)
+	if not phase2 then return end
+	self:Bar(L["pcold_bar"], 15, spellId)		
+end
+
 function mod:Swarm(player, spellId, _, _, spellName)
 	self:IfMessage(spellName, "Important", spellId)
+	phase2 = true
 end
 
 function mod:Pursue(player, spellId)
@@ -68,11 +88,17 @@ function mod:Pursue(player, spellId)
 	self:PrimaryIcon(player, "icon")
 end
 
+local function nextwave()
+	mod:Bar(L["nerubian_burrower"], 45, 66333)
+end
+
 function mod:CHAT_MSG_MONSTER_YELL(event, msg)
 	if msg:find(L["engage_trigger"]) then
 		if db.burrow then
 			self:IfMessage(L["engage_message"], "Attention", 65919)
 			self:Bar(L["burrow_cooldown"], 80, 65919)
+			self:Bar(L["nerubian_burrower"], 10, 66333)
+			self:ScheduleEvent("BWnextwave", nextwave, 10)	
 		end
 		if db.berserk then
 			self:Berserk(570)
@@ -84,6 +110,8 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(event, msg)
 	if db.burrow and msg:find(L["unburrow_trigger"]) then
 		self:Bar(L["burrow_cooldown"], 80, 65919)
 		self:DelayedMessage(70, L["burrow_soon"], "Attention")
+		self:Bar(L["nerubian_burrower"], 10, 66333)
+		self:ScheduleEvent("BWnextwave", nextwave, 10)
 	end
 	if db.burrow and msg:find(L["burrow_trigger"]) then
 		self:Bar(L["burrow"], 65, 65919)
