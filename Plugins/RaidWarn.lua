3abe5f4b259f0d5e90db3c24e5b70f97b6b55018
funkydude <funkydude@users.noreply.github.com>
2009-09-11 14:29:14 +0000

    Re-add the ability to use RAID_WARNING, this only works for raid leaders in a raid, but for all members in a party. No options yet.

--- Plugins/RaidWarn.lua
+++ Plugins/RaidWarn.lua
@@ -10,6 +10,7 @@ if not plugin then return end
 ------------------------------
 
 local sentWhispers = {}
+local output = "*** %s ***"
 local L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Plugins")
 
 --------------------------------------------------------------------------------
@@ -18,6 +19,8 @@ local L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Plugins")
 
 plugin.defaultDB = {
 	whisper = true,
+	broadcast = false,
+	useraidchannel = false,
 }
 
 ------------------------------
@@ -32,7 +35,28 @@ end
 
 function plugin:OnPluginEnable()
 	self:RegisterMessage("BigWigs_SendTell")
+	self:RegisterMessage("BigWigs_Message")
 	sentWhispers = wipe(sentWhispers)
+end
+
+function plugin:BigWigs_Message(msg, color, noraidsay)
+	if not msg or noraidsay or not self.db.profile.broadcast then return end
+
+	local inRaid = UnitInRaid("player")
+	-- In a 5-man group, everyone can use the raid warning channel.
+	if inRaid and not IsRaidLeader() then
+		return
+	elseif GetNumPartyMembers() == 0 and not inRaid then
+		return
+	end
+
+	local clean = msg:gsub("(|c%x%x%x%x%x%x%x%x)", ""):gsub("(|r)", "")
+	local o = output:format(clean)
+	if self.db.profile.useraidchannel then
+		SendChatMessage(o, inRaid and "RAID" or "PARTY")
+	else
+		SendChatMessage(o, "RAID_WARNING")
+	end
 end
 
 function plugin:BigWigs_SendTell(event, player, msg)
