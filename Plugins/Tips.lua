326a2e3d719d3108b852c2214ef06edc9d969353
Vidar Haarr <vhaarr@gmail.com>
2009-10-05 00:32:05 +0000

    Add in a tip about middle clicking bars.

--- Plugins/Tips.lua
+++ Plugins/Tips.lua
@@ -141,6 +141,7 @@ local tips = {
 	"Rabbit##/bwlcb 20m Oven warm, put in pizza!#2",
 	"Rabbit##You can open the proximity monitor manually with /proximity <range>. Can be useful on new fights or if you use a special strategy for a encounter.#2",
 	"Rabbit##If your raid leader is spamming you with tips, you can make them output to the chat frame or turn them off completely. Just visit the 'Customize ...' section under the Big Wigs interface options!#2",
+	"Rabbit##Sometimes you'll want to remove a bar from the screen while in combat, so you can focus more on some other ability. Clicking your middle mouse button over a bar will stop it -- but next time the ability is used, it will reappear.#2",
 }
 
 -------------------------------------------------------------------------------
