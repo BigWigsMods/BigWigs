b08cc2f8f76483d724236c21a2dbe028c80965a6
Vidar Haarr <vhaarr@gmail.com>
2009-10-07 00:50:12 +0000

    Add shadow strike timers from Darthn.

--- Coliseum/Anubarak.lua
+++ Coliseum/Anubarak.lua
@@ -4,21 +4,23 @@
 
 local mod = BigWigs:NewBoss("Anub'arak", "Trial of the Crusader")
 if not mod then return end
-mod.toggleOptions = {66012, "burrow", {67574, "WHISPER", "ICON", "FLASHSHAKE"}, {68510, "FLASHSHAKE"}, 66118, "berserk", "bosskill"}
---[[mod.optionHeaders = {
+mod.toggleOptions = {66012, "burrow", {67574, "WHISPER", "ICON", "FLASHSHAKE"}, {68510, "FLASHSHAKE"}, 66118, 66134, "berserk", "bosskill"}
+mod.optionHeaders = {
 	[66012] = "normal",
-	--[the shadow strike thing from the adds] = "hard",
+	[66134] = "hard",
 	berserk = "general",
-}]]
+}
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
+local ssName = GetSpellInfo(66134)
 local difficulty = nil
 local pName = UnitName("player")
 local phase2 = nil
 local burrowMessage = nil
+local strikeMessage = nil
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -37,6 +39,8 @@ if L then
 	L.burrow_soon = "Burrow soon"
 
 	L.nerubian_burrower = "Nerubian Burrower"
+
+	L.shadow_soon = "Shadow Strike in ~5sec!"
 
 	L.freeze_bar = "~Next Freezing Slash"
 	L.pcold_bar = "~Next Penetrating Cold"
@@ -63,6 +67,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "ColdDebuff", 68510, 68509)
 	self:Log("SPELL_AURA_APPLIED", "Pursue", 67574)
 	
+	self:Log("SPELL_CAST_START", "Strike", 66134)
 	self:Log("SPELL_CAST_SUCCESS", "FreezeCooldown", 66012)
 	self:Log("SPELL_MISSED", "FreezeCooldown", 66012)
 	
@@ -81,6 +86,12 @@ function mod:OnEngage()
 	self:Bar("burrow", L["burrow_cooldown"], 80, 65919)
 	self:Bar("burrow", L["nerubian_burrower"], 10, 66333)
 	self:ScheduleEvent("BWnextwave", nextwave, 10)
+	if self:GetOption(66134) and difficulty > 2 then
+		self:Bar(66134, ssName, 30, 66134)
+		strikeMessage = self:DelayedMessage(66134, 25, L["shadow_soon"], "Attention")
+		self:ScheduleEvent("BWnextstrike", nextstrike, 30)
+	end
+	
 	self:Berserk(570, true)
 	phase2 = nil
 end
@@ -88,6 +99,22 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+local function nextstrike()
+	mod:Bar(66134, ssName, 30, 66134)
+	strikeMessage = mod:DelayedMessage(66134, 25, L["shadow_soon"], "Attention")
+	mod:ScheduleEvent("BWnextstrike", nextstrike, 30)
+end
+
+function mod:Strike(player, spellId, _, _, spellName)
+	if difficulty > 2 then
+		self:CancelScheduledEvent("BWnextstrike")
+		self:CancelScheduledEvent(strikeMessage)
+		self:Bar(66134, spellName, 30, spellId)
+		strikeMessage = self:DelayedMessage(66134, 25, L["shadow_soon"], "Attention")
+		self:ScheduleEvent("BWnextstrike", nextstrike, 30)
+	end
+end
 
 function mod:FreezeCooldown(player, spellId)
 	self:Bar(66012, L["freeze_bar"], 20, spellId)
@@ -126,8 +153,15 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(event, msg)
 	if msg:find(L["unburrow_trigger"]) then
 		self:Bar("burrow", L["burrow_cooldown"], 80, 65919)
 		burrowMessage = self:DelayedMessage("burrow", 70, L["burrow_soon"], "Attention")
-		self:Bar("burrow", L["nerubian_burrower"], 6, 66333)
-		self:ScheduleEvent("BWnextwave", nextwave, 6)
+
+		self:Bar("burrow", L["nerubian_burrower"], 5, 66333)
+		self:ScheduleEvent("BWnextwave", nextwave, 5)
+
+		if self:GetOption(66134) and difficulty > 2 then
+			self:Bar(66134, ssName, 35, 66134)
+			strikeMessage = self:DelayedMessage(66134, 30, L["shadow_soon"], "Attention")
+			self:ScheduleEvent("BWnextstrike", nextstrike, 35)
+		end
 	elseif msg:find(L["burrow_trigger"]) then
 		self:Bar("burrow", L["burrow"], 65, 65919)
 	end
