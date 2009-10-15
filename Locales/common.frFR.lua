60b37402a0b7a740849dec58aaa4c01bdadd073a
tborlee <tborlee@users.noreply.github.com>
2009-10-15 18:15:38 +0000

    frFR Update

--- Locales/common.frFR.lua
+++ Locales/common.frFR.lua
@@ -6,6 +6,7 @@ L.other = "%s : %s"
 
 L.phase = "Phase %d"
 L.normal = "Mode normal"
+L.heroic = "Mode héroïque"
 L.hard = "Mode difficile"
 L.general = "Général" -- General settings, i.e. things that apply to both normal and hard mode.
 
