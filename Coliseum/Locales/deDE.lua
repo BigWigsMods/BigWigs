e58e36f436e1cd7dfd09e40c893915a736e36eb9
mojosdojo <mojosdojo@users.noreply.github.com>
2009-10-19 14:40:10 +0000

    Add deDE charge say.

--- Coliseum/Locales/deDE.lua
+++ Coliseum/Locales/deDE.lua
@@ -56,6 +56,7 @@ if L then
 	L.charge = "Wütender Ansturm"
 	L.charge_desc = "Warnt vor Wütender Ansturm auf Spielern."
 	L.charge_trigger = "zornig an"
+	L.charge_say = "Ansturm auf MIR!"
 
 	L.bosses = "Bosse"
 	L.bosses_desc = "Warnt, wann die nachfolgenden Bosse eintreffen."
