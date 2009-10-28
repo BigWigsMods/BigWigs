8a2de81c2c0846934c812350e9b160e66c58c20c
yoshimo <yoshimo@users.noreply.wowace.com>
2009-10-28 13:37:37 +0000

    festergut: fixed silly copy&paste error rotface: added skeleton module

--- Citadel/Rotface.lua
+++ Citadel/Rotface.lua
@@ -1,22 +1,22 @@
---------------------------------------------------------------------------------
+﻿--------------------------------------------------------------------------------
 -- Module declaration
 --
 
-local mod = BigWigs:NewBoss("Festergut", "Icecrown Citadel")
+local mod = BigWigs:NewBoss("Rotface", "Icecrown Citadel")
 if not mod then return end
 
-mod:RegisterEnableMob(36626)
-mod.toggleOptions = {"berserk", "bosskill"}
+mod:RegisterEnableMob(36627)
+mod.toggleOptions = {"bosskill"}
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
-local L = LibStub("AceLocale-3.0"):NewLocale("Big Wigs: Festergut", "enUS", true)
+local L = LibStub("AceLocale-3.0"):NewLocale("Big Wigs: Rotface", "enUS", true)
 if L then
 
 end
-L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Festergut")
+L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Rotface")
 mod.locale = L
 
 --------------------------------------------------------------------------------
@@ -24,13 +24,8 @@ mod.locale = L
 --
 
 function mod:OnBossEnable()
-	self:Death("Win", 36626)
+	self:Death("Win", 36627)
 
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 end
-
-function mod:OnEngage()
-	self:Berserk(300, true)
-end
-
