00f0e538f129387c401059db59dc4b5ea907f7f3
yoshimo <yoshimo@users.noreply.wowace.com>
2009-10-28 17:41:22 +0000

    Added skeleton module for tonights Lady Deathwhisper

---
+++ Citadel/Deathwhisper.lua
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+-- Module declaration
+--
+
+local mod = BigWigs:NewBoss("Lady Deathwhisper", "Icecrown Citadel")
+if not mod then return end
+
+mod:RegisterEnableMob(36855)
+mod.toggleOptions = {"bosskill"}
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Big Wigs: Lady Deathwhisper", "enUS", true)
+if L then
+
+end
+L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Lady Deathwhisper")
+mod.locale = L
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:OnBossEnable()
+	self:Death("Win", 36855)
+
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+end
