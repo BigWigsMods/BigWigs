2510f6bc19780aff9dc8cff7bf210740e6e165dc
yoshimo <yoshimo@users.noreply.wowace.com>
2009-11-14 07:51:09 +0000

    - 6m berserktimer - Pact of the Darkfallen

--- Citadel/Lanathel.lua
+++ Citadel/Lanathel.lua
@@ -6,10 +6,17 @@ if not QueryQuestsCompleted then return end
 local mod = BigWigs:NewBoss("Blood-Queen Lana'thel", "Icecrown Citadel")
 if not mod then return end
 mod:RegisterEnableMob(37955)
-mod.toggleOptions = {"bosskill"}
+mod.toggleOptions = {71340, "berserk", "bosskill"}
 
 --------------------------------------------------------------------------------
 -- Locals
+--
+
+local pName = UnitName("player")
+local pact = mod:NewTargetList()
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Locale
 --
 
 local L = mod:NewLocale("enUS", true)
@@ -23,8 +30,42 @@ L = mod:GetLocale()
 --
 
 function mod:OnBossEnable()
+
+	self:Log("SPELL_AURA_APPLIED", "PactApplied", 71340)
 	self:Death("Win", 37955)
 
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+end
+
+function mod:OnEngage()
+	self:Berserk(360, true)
+end
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+do
+	local handle = nil
+	local warned = nil
+	local id, name = nil, nil
+	local function PactWarn()
+		if not warned then
+			mod:TargetMessage(71340, name, pact, "Urgent", id)
+		else
+			warned = nil
+			wipe(pact)
+		end
+		handle = nil
+	end
+	function mod:PactApplied(player, spellId, _, _, spellName)
+		pact[#pact + 1] = player
+		if handle then self:CancelTimer(handle) end
+		id, name = spellId, spellName
+		handle = self:ScheduleTimer(PactWarn, 0.1)
+		if player == pName then
+			warned = true
+			self:TargetMessage(71340, spellName, player, "Important", spellId, "Info")
+		end
+	end
 end
