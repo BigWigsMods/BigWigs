530315b016ab2ff185fd6f6cf7f1c0ca32323436
mojosdojo <mojosdojo@users.noreply.github.com>
2009-12-14 07:38:31 +0000

    deDE update.

--- Citadel/Locales/deDE.lua
+++ Citadel/Locales/deDE.lua
@@ -5,7 +5,14 @@ end
 
 L = BigWigs:NewBossLocale("Lady Deathwhisper", "deDE")
 if L then
+	L.dnd_message = "Tod und Verfall auf DIR!"
+	L.phase2_message = "Manabarriere weg - Phase 2!"
+	--L.engage_trigger = "What is this disturbance?"
 
+	L.adds = "Adds"
+	L.adds_desc = "Zeigt Timer und Nachrichten für das Auftauchen der Adds."
+	L.adds_bar = "~Adds"
+	L.adds_warning = "Adds in 5 sek!"
 end
 
 L = BigWigs:NewBossLocale("Festergut", "deDE")
@@ -20,7 +27,12 @@ end
 
 L = BigWigs:NewBossLocale("Lord Marrowgar", "deDE")
 if L then
+	L.impale_cd = "~Aufspießen"
+	L.whirlwind_cd = "~Knochensturm"
+	L.ww_start = "Knochensturm startet!"
+	L.ww_end = "Knochensturm endet!"
 
+	L.coldflame_message = "Eisflamme auf DIR!"
 end
 
 L = BigWigs:NewBossLocale("Precious", "deDE")
@@ -40,7 +52,17 @@ end
 
 L = BigWigs:NewBossLocale("Deathbringer Saurfang", "deDE")
 if L then
-
+	L.adds = "Blutbestien"
+	L.adds_desc = "Zeigt Timer und Nachrichten für das Auftauchen der Blutbestien."
+	L.adds_warning = "Blutbestien in 5 sek!"
+	L.adds_message = "Blutbestien!"
+	L.adds_bar = "~Blutbestien"
+	
+	L.rune_bar = "~Rune"
+	
+	L.nova_bar = "~Nova"
+	
+	L.mark = "Mal"
 end
 
 L = BigWigs:NewBossLocale("Sindragosa", "deDE")
