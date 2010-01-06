f9e0b5ecb8cfa969b6724609db1b24470737f6ae
7destiny <7destiny@users.noreply.wowace.com>
2010-01-06 07:27:12 +0000

    - add Unstable Experiment - change Icon for Violation

--- Citadel/Putricide.lua
+++ Citadel/Putricide.lua
@@ -5,7 +5,7 @@
 local mod = BigWigs:NewBoss("Professor Putricide", "Icecrown Citadel")
 if not mod then return end
 mod:RegisterEnableMob(36678)
-mod.toggleOptions = {{70447, "ICON", "WHISPER"}, {72455, "ICON", "WHISPER"}, "bosskill"}
+mod.toggleOptions = {{70447, "ICON", "WHISPER"}, {72455, "ICON", "WHISPER"}, 71966, "bosskill"}
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -31,6 +31,7 @@ L = mod:GetLocale()
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Blight", 72455)
 	self:Log("SPELL_AURA_APPLIED", "Violation", 70447)
+	self:Log("SPELL_CAST_START", "Experiment", 70351, 71966)
 
 	self:Death("Win", 36678)
 
@@ -53,6 +54,9 @@ end
 function mod:Violation(player, spellId)
 	self:TargetMessage(70447, L["violation_message"], player, "Personal", spellId)
 	self:Whisper(70447, player, L["violation_message"])
-	self:PrimaryIcon(70447, player)
+	self:SecondaryIcon(70447, player)
 end
 
+function mod:Experiment(_, spellId, _, _, spellName)
+	self:Message(71966, spellName, "Important", spellId)
+end
