9f452e31e4cd6152db087acd8a163fee4782e612
Starinnia <Starinnia@users.noreply.wowace.com>
2010-01-08 01:54:46 +0000

    Festergut: add Vile Gas spellId

--- Citadel/Festergut.lua
+++ Citadel/Festergut.lua
@@ -45,7 +45,7 @@ L = mod:GetLocale()
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "InhaleCD", 69165)
 	self:Log("SPELL_CAST_START", "Blight", 69195, 71219, 73031, 73032)
-	self:Log("SPELL_AURA_APPLIED", "VileGas", 71218, 72272, 72273, 73019, 73020)
+	self:Log("SPELL_AURA_APPLIED", "VileGas", 71218, 72272, 72273, 73019, 73020, 69240)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "Bloat", 72551, 72219)
 	self:Death("Win", 36626)
 
