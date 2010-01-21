bcbe91b0709382c5f2d6b81e9661ff3a2d27e9e1
funkydude <funkydude@users.noreply.github.com>
2010-01-21 00:11:13 +0000

    Citadel/Blood_Princes: Warn for inferno flames and target

--- Citadel/Blood_Princes.lua
+++ Citadel/Blood_Princes.lua
@@ -5,7 +5,7 @@ local mod = BigWigs:NewBoss("Blood Princes", "Icecrown Citadel")
 if not mod then return end
 --Prince Valanar, Prince Keleseth, Prince Taldaram
 mod:RegisterEnableMob(37970, 37972, 37973)
-mod.toggleOptions = {71079, "bosskill"}
+mod.toggleOptions = {{72040, "FLASHSHAKE"}, 71079, "bosskill"}
 
 --------------------------------------------------------------------------------
 --  Localization
@@ -14,6 +14,9 @@ mod.toggleOptions = {71079, "bosskill"}
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.switch_message = "Vulnerability switch"
+
+	L.infernoflames = "Inferno Flames"
+	L.infernoflames_message = "Inferno Flames following YOU"
 end
 L = mod:GetLocale()
 
@@ -23,6 +26,7 @@ L = mod:GetLocale()
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "Switch", 71079, 71082, 71075)
+	self:RegisterEvent("CHAT_MSG_RAID_BOSS_EMOTE")
 
 	self:Death("Deaths", 37970, 37972, 37973)
 end
@@ -36,10 +40,24 @@ function mod:Switch()
 	self:Bar("Switch", L["switch_message"], 40)
 end
 
-function mod:Deaths()
-	deaths = deaths + 1
-	if deaths == 3 then 
-		self:Win()
+function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, msg, _, _, _, player)
+	if msg:find(L["infernoflames"]) then
+		if UnitIsUnit(player, "player") then
+			self:FlashShake(72040)
+			self:LocalMessage(72040, L["infernoflames_message"], "Personal", 72040, "Alarm")
+		else
+			self:TargetMessage(72040, L["infernoflames"], player, "Urgent", 72040)
+		end
+	end
+end
+
+do
+	local deaths = 0
+	function mod:Deaths()
+		deaths = deaths + 1
+		if deaths == 3 then 
+			self:Win()
+		end
 	end
 end
 
