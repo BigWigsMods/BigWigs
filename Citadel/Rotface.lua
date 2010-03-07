f86bb46da2451fe2cb05d4b586ffb8e88fecf830
funkydude <funkydude@users.noreply.github.com>
2010-03-07 21:11:55 +0000

    Citadel/Rotface: bugfix vile gas

--- Citadel/Rotface.lua
+++ Citadel/Rotface.lua
@@ -42,7 +42,7 @@ L = mod:GetLocale()
 --
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_START", "VileGas", 72272, 72273)
+	self:Log("SPELL_CAST_SUCCESS", "VileGas", 72272, 72273) --Heroic only 10/25
 	self:Log("SPELL_AURA_APPLIED", "Infection", 69674, 71224, 73022, 73023)
 	self:Log("SPELL_AURA_REMOVED", "InfectionRemoved", 69674, 71224)
 	self:Log("SPELL_CAST_START", "SlimeSpray", 69508)
