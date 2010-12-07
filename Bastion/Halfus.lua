29be057a8d21d453853079959c852937e42bd71b
Caleb <calebzor@gmail.com>
2010-12-07 20:15:45 +0000

    update Halfus for live

--- Bastion/Halfus.lua
+++ Bastion/Halfus.lua
@@ -6,7 +6,7 @@ if not GetSpellInfo(90000) then return end
 local mod = BigWigs:NewBoss("Halfus Wyrmbreaker", "The Bastion of Twilight")
 if not mod then return end
 mod:RegisterEnableMob(44600)
-mod.toggleOptions = {86170, "berserk", "bosskill"}
+mod.toggleOptions = {86169, "berserk", "bosskill"}
 mod.optionHeaders = {
 	berserk = "general",
 }
@@ -30,10 +30,12 @@ L = mod:GetLocale()
 --
 
 function mod:OnBossEnable()
-	self:Log("SPELL_CAST_START", "FuriousRoar", 86170)
+	self:Log("SPELL_CAST_START", "FuriousRoar", 86169)
 
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	
+	self:RegisterEvent("UNIT_HEALTH")
 
 	self:Death("Win", 44600)
 end
@@ -47,8 +49,18 @@ end
 -- Event Handlers
 --
 
+function mod:UNIT_HEALTH(event, unit)
+	if unit == "boss1" then
+		local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+		if hp == 50 then
+			self:Message(86169, spellName, "Urgent", spellId)
+			self:Bar(86169, spellName, 25, 86169)
+		end
+	end
+end
+
 function mod:FuriousRoar(_, spellId, _, _, spellName)
-	self:Message(86170, spellName, "Urgent", spellId)
-	self:Bar(86170, spellName, 25, 86170)
+	self:Message(86169, spellName, "Urgent", spellId)
+	self:Bar(86169, spellName, 25, 86169)
 end
 
