abc2ec1d4e54be5a512cf8b4a0b0bec9ecfa5265
jongt23 <jongt23@gmail.com>
2010-12-14 17:26:35 +0000

    - esES Update

---
+++ Throne/Locales/esES.lua
@@ -0,0 +1,15 @@
+local L = BigWigs:NewBossLocale("Al'Akir", "esES")
+if L then
+
+end
+
+local L = BigWigs:NewBossLocale("Conclave of Wind", "esES")
+if L then
+	L.gather_strength = "%s está reuniendo fuerza"
+	
+	L.full_power = "Máximo poder"
+	L.full_power_desc = "Avisa cuando los jefes alcanzan el Máximo poder y empiezan a lanzar las abilidades especiales."
+	L.gather_strength_emote = "%s empieza a reunir fuerza de los señores de viento restantes!"
+	
+	L.wind_chill = "TU tienes %s stacks de Viento escalofriante"
+end
