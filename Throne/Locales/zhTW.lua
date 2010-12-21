ca06e3c2aa2389e61a0b1330cac5362afd7ee14c
zhTW <zhTW@users.noreply.wowace.com>
2010-12-21 02:07:39 +0000

    zhTW update

--- Throne/Locales/zhTW.lua
+++ Throne/Locales/zhTW.lua
@@ -7,8 +7,8 @@ local L = BigWigs:NewBossLocale("Conclave of Wind", "zhTW")
 if L then
 	L.gather_strength = "%正在聚集力量！"
 	
-	L.full_power = "满能量"
-	L.full_power_desc = "当首領獲得滿能量並開始施放特殊技能時發出警報。"
+	L.full_power = "滿能量"
+	L.full_power_desc = "當首領獲得滿能量並開始施放特殊技能時發出警報。"
 	L.gather_strength_emote = "%s begins to gather strength from the remaining Wind Lords!"
 	
 	L.wind_chill = ">你<獲得%s層風寒冷卻！"
