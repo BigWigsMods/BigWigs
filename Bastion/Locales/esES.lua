438919f072ea935d12ace8ec78927efdc7e9bc21
jongt23 <jongt23@gmail.com>
2010-12-31 11:41:40 +0000

    esES update

--- Bastion/Locales/esES.lua
+++ Bastion/Locales/esES.lua
@@ -5,22 +5,22 @@ if L then
 	L.orders_desc = "Warning for Shadow/Flame Orders"
 
 	--normal
-	L.worship_cooldown = "~Worship"
+	L.worship_cooldown = "~Adoración"
 end
 
 L = BigWigs:NewBossLocale("Valiona and Theralion", "esES")
 if L then
-	L.phase_switch = "Phase Switch"
-	L.phase_switch_desc = "Warning for Phase Switches"
+	L.phase_switch = "Cambio de fase"
+	L.phase_switch_desc = "Aviso para cambios de fase"
 
-	L.engulfingmagic_say = "Engulfing Magic on ME!"
-	L.engulfingmagic_cooldown = "~Engulfing Magic"
+	L.engulfingmagic_say = "¡Trago de magia en MI!"
+	L.engulfingmagic_cooldown = "~Trago de magia"
 
-	L.devouringflames_cooldown = "~Devouring Flames"
+	L.devouringflames_cooldown = "~Llamas devoradoras"
 
 	-- L.valiona_trigger = "Theralion, I will engulf the hallway. Cover their escape!"
 
-	L.twilight_shift = "%2$dx Twilight Shift on %1$s"
+	L.twilight_shift = "%2$dx Cambio crepuscular en %1$s"
 end
 
 L = BigWigs:NewBossLocale("Halfus Wyrmbreaker", "esES")
@@ -35,21 +35,21 @@ end
 
 L = BigWigs:NewBossLocale("Ascendant Council", "esES")
 if L then
-	L.static_overload_say = "Static Overload on ME!"
-	L.gravity_core_say = "Gravity Core on ME!"
-	L.health_report = "%s is at %d%% health, switch soon!"
-	L.switch = "Switch"
-	L.switch_desc = "Warning for boss switches"
+	L.static_overload_say = "¡Sobrecarga estática en MI!"
+	L.gravity_core_say = "¡Núcleo de gravedad en MI!"
+	L.health_report = "%s está al %d%% de vida, ¡cambio pronto!"
+	L.switch = "Cambio"
+	L.switch_desc = "Avisa los cambios de jefes"
 
-	L.lightning_rod_say = "Lightning Rod on ME!"
+	L.lightning_rod_say = "¡Vara relámpago en MI!"
 
 	-- L.switch_trigger = "We will handle them!"
 
 	-- L.quake_trigger = "The ground beneath you rumbles ominously...."
 	-- L.thundershock_trigger = "The surrounding air crackles with energy...."
 
-	L.searing_winds_message = "Get Searing Winds!"
-	L.grounded_message = "Get Grounded!"
+	L.searing_winds_message = "¡Se forman Vientos espirales!"
+	L.grounded_message = "¡Toma tierra!"
 
 	-- L.last_phase_trigger = "BEHOLD YOUR DOOM!"
 end
