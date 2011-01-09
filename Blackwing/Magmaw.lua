492ac52593af57c44fc72ace0168f94eda2b8469
funkydude <funkydude@users.noreply.github.com>
2011-01-09 03:43:59 +0000

    Blackwing/Magmaw: Tweak slump description.

--- Blackwing/Magmaw.lua
+++ Blackwing/Magmaw.lua
@@ -33,8 +33,8 @@ if L then
 	L.blazing_message = "Add incoming!"
 	L.blazing_bar = "Next skeleton"
 
-	L.slump = "Slump"
-	L.slump_desc = "Warn for when Magmaw slumps forward and exposes himself."
+	L.slump = "Slump (Rodeo)"
+	L.slump_desc = "Warn for when Magmaw slumps forward and exposes himself, allowing the riding rodeo to start."
 
 	L.slump_bar = "Next rodeo"
 	L.slump_message = "Yeehaw, ride on!"
