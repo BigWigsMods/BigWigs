b391f9ebadb5d831ea8e198f82b05ed11a0caa14
Caleb <calebzor@gmail.com>
2011-01-11 20:06:34 +0000

    warn only once for phase3 add onyxia power warning fix phase2 duration, shadow blaze timer increase proximity range for cinders still assumed

--- Blackwing/Nefarian.lua
+++ Blackwing/Nefarian.lua
@@ -5,7 +5,7 @@
 local mod = BigWigs:NewBoss("Nefarian", "Blackwing Descent")
 if not mod then return end
 mod:RegisterEnableMob(41270, 41376)
-mod.toggleOptions = {{79339, "FLASHSHAKE", "SAY"}, { 80626, "FLASHSHAKE"}, "proximity", "phase", 81272, 94085, "bosskill"}
+mod.toggleOptions = {{79339, "FLASHSHAKE", "SAY"}, { 80626, "FLASHSHAKE"}, "proximity", "phase", 78999, 81272, 94085, "bosskill"}
 local CL = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Common")
 mod.optionHeaders = {
 	[79339] = "heroic",
@@ -18,6 +18,7 @@ mod.optionHeaders = {
 local phase, deadAdds, shadowBlazeTimer = 1, 0, 30
 local cinderTargets = mod:NewTargetList()
 local shadowblaze = GetSpellInfo(94085)
+local phase3warned = false
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -35,7 +36,7 @@ if L then
 	L.crackle_trigger = "The air crackles with electricity!"
 	L.crackle_message = "Electrocute soon!"
 
-	L.shadowblaze_trigger = "Flesh turns to ash!"
+	L.onyxia_power_message = "Explosion soon!"
 
 	L.cinder_say = "Explosive Cinders on ME!"
 
@@ -50,7 +51,6 @@ L = mod:GetLocale()
 function mod:OnBossEnable()
 	self:Yell("PhaseTwo", L["phase_two_trigger"])
 	self:Yell("PhaseThree", L["phase_three_trigger"])
-	self:Yell("ShadowBlaze", L["shadowblaze_trigger"])
 
 	self:Log("SPELL_AURA_APPLIED", "ExplosiveCindersApplied", 79339)
 	self:Log("SPELL_AURA_REMOVED", "ExplosiveCindersRemoved", 79339)
@@ -60,12 +60,15 @@ function mod:OnBossEnable()
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
+	self:RegisterEvent("UNIT_POWER")
+
 	self:Death("Deaths", 41376, 41948)
 end
 
 
 function mod:OnEngage(diff)
-	phase, deadAdds, shadowBlazeTimer = 1, 0, 30
+	phase, deadAdds, shadowBlazeTimer = 1, 0, 35
+	phase3warned = false
 end
 
 --------------------------------------------------------------------------------
@@ -79,15 +82,17 @@ end
 function mod:Deaths(mobId)
 	if mobId == 41948 then
 		deadAdds = deadAdds + 1
-		if self:GetInstanceDifficulty() > 2 then
+		if self:GetInstanceDifficulty() > 2 and not phase3warned then
 			self:SendMessage("BigWigs_StopBar", self, CL["phase"]:format(phase))
 			phase = 3
 			self:Message("phase", CL["phase"]:format(phase), "Attention", 81007)
+			phase3warned = true
 		end
-		if deadAdds == 3 then
+		if deadAdds == 3 and not phase3warned then
 			self:SendMessage("BigWigs_StopBar", self, CL["phase"]:format(phase))
 			phase = 3
 			self:Message("phase", CL["phase"]:format(phase), "Attention", 81007)
+			phase3warned = true
 		end
 	elseif mobId == 41376 then
 		self:Win()
@@ -97,27 +102,32 @@ end
 function mod:PhaseTwo()
 	phase = 2
 	self:Message("phase", CL["phase"]:format(phase), "Attention", 78621)
-	self:Bar("phase", CL["phase"]:format(phase), 180, 78621)
-end
-
-function mod:PhaseThree()
-	self:SendMessage("BigWigs_StopBar", self, CL["phase"]:format(phase))
-	phase = 3
-	self:Message("phase", CL["phase"]:format(phase), "Attention", 78621)
-	self:Bar(94085, shadowblaze, 10, 94085)
+	self:Bar("phase", CL["phase"]:format(phase), 127, 78621)
 end
 
 local function ShadowBlazeNoTrigger()
-	if shadowBlazeTimer > 10 then
-		shadowBlazeTimer = shadowBlazeTimer - 5
+	if mod:GetInstanceDifficulty() > 2 then
+		if shadowBlazeTimer > 5 then
+			shadowBlazeTimer = shadowBlazeTimer - 5
+		end
+	else
+		if shadowBlazeTimer > 10 then
+			shadowBlazeTimer = shadowBlazeTimer - 5
+		end
 	end
 	mod:Bar(94085, shadowblaze, shadowBlazeTimer, 94085)
 	mod:ScheduleTimer(ShadowBlazeNoTrigger, shadowBlazeTimer)
 end
 
-function mod:ShadowBlaze()
-	self:Bar(94085, shadowblaze, shadowBlazeTimer, 94085)
-	self:ScheduleTimer(ShadowBlazeNoTrigger, shadowBlazeTimer)
+function mod:PhaseThree()
+	self:SendMessage("BigWigs_StopBar", self, CL["phase"]:format(phase))
+	if not phase3warned then
+		phase = 3
+		self:Message("phase", CL["phase"]:format(phase), "Attention", 78621)
+		phase3warned = true
+	end
+	self:Bar(94085, shadowblaze, 10, 94085)
+	self:ScheduleTimer(ShadowBlazeNoTrigger, 10)
 end
 
 do
@@ -132,7 +142,7 @@ do
 			self:FlashShake(79339)
 			self:Say(79339, L["cinder_say"])
 			self:Bar(79339, spellName, 8, 79339)
-			self:OpenProximity(6) -- assumed
+			self:OpenProximity(10) -- assumed
 		end
 		if not scheduled then
 			scheduled = true
@@ -144,12 +154,24 @@ end
 function mod:StolenPower(player, spellId, _, _, spellName, stack)
 	if UnitIsUnit(player, "player") and stack == 150 then
 		self:FlashShake(80626)
-		self:LocalMessage(80626, spellName, "Personal", spellId, "Info")	end
+		self:LocalMessage(80626, spellName, "Personal", spellId, "Info")
+	end
 end
 
 function mod:ExplosiveCindersRemoved(player)
 	if UnitIsUnit(player, "player") then
-		self:CloseProximity(6)
+		self:CloseProximity()
 	end
 end
+
+function mod:UNIT_POWER(event, unit, powerType)
+	if UnitIsUnit("boss1", unit) and powerType == "ALTERNATE" then
+		local power = UnitPower(unit, ALTERNATE_POWER_INDEX)
+		if power > 80 then
+			self:Message(78999, L["onyxia_power_message"], "Attention", 78999)
+			self:UnregisterEvent("UNIT_POWER")
+		end
+	end
+end
+
 
