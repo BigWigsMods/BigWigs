53444885b9bc3d6031296e6f17e1df873c37f97f
Vidar Haarr <vhaarr@gmail.com>
2011-01-19 02:24:56 +0000

    Enable icons in the boss options. Even though it could look better, it's fine for now.

--- Options/Options.lua
+++ Options/Options.lua
@@ -468,15 +468,15 @@ local function getOptionDetails(module, bossOption)
 	local bf = module.toggleDefaults[option]
 	if t == "string" then
 		if customBossOptions[option] then
-			return option, customBossOptions[option][1], customBossOptions[option][2], bf
+			return option, customBossOptions[option][1], customBossOptions[option][2]
 		else
 			local L = module:GetLocale()
-			return option, L[option], L[option .. "_desc"], bf
+			return option, L[option], L[option .. "_desc"] -- , L[option .. "_icon"]
 		end
 	elseif t == "number" then
-		local spellName = GetSpellInfo(option)
+		local spellName, _, icon = GetSpellInfo(option)
 		if not spellName then error(("Invalid option %d in module %s."):format(option, module.displayName)) end
-		return spellName, spellName, getSpellDescription(option), bf
+		return spellName, spellName, getSpellDescription(option), icon
 	end
 end
 
@@ -640,7 +640,8 @@ local function buttonClicked(widget)
 end
 
 local function getDefaultToggleOption(scrollFrame, dropdown, module, bossOption)
-	local dbKey, name, desc = getOptionDetails(module, bossOption)
+	local dbKey, name, desc, icon = getOptionDetails(module, bossOption)
+
 	local check = AceGUI:Create("CheckBox")
 	check:SetLabel(colorize[name])
 	check:SetTriState(true)
@@ -651,6 +652,7 @@ local function getDefaultToggleOption(scrollFrame, dropdown, module, bossOption)
 	check:SetDescription(desc)
 	check:SetCallback("OnValueChanged", masterOptionToggled)
 	check:SetValue(getMasterOption(check))
+	if icon then check:SetImage(icon, 0.07, 0.93, 0.07, 0.93) end
 
 	local button = AceGUI:Create("Button")
 	button:SetText(">>")
