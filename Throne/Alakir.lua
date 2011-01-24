a824d4765a894f2a5433cbfa5660ed3ed5c24084
Caleb <calebzor@gmail.com>
2011-01-24 22:15:36 +0000

    add another p2 trigger spellId

--- Throne/Alakir.lua
+++ Throne/Alakir.lua
@@ -55,7 +55,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED_DOSE", "Feedback", 87904)
 	self:Log("SPELL_AURA_APPLIED", "Feedback", 87904)
 	-- Acid Rain is applied at P2 transition
-	self:Log("SPELL_AURA_APPLIED", "Phase2", 88301, 93279)
+	self:Log("SPELL_AURA_APPLIED", "Phase2", 88301, 93279, 93281)
 
 	self:Yell("Phase3", L["phase3_yell"])
 
