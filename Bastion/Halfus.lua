2d77c4cdafbd52ab84da30fc2ed27c394fc25299
Kyle Buller <bullerk@gmail.com>
2011-01-26 06:59:37 +0000

    Bastion/Halfus: announce strike stacking

--- Bastion/Halfus.lua
+++ Bastion/Halfus.lua
@@ -7,13 +7,30 @@ if not mod then return end
 mod:RegisterEnableMob(44600)
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local stackWarn = nil
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:NewLocale("enUS", true)
+if L then
+	L.strikes_message = "%2$dx Malevolent Strikes on %1$s"
+end
+L = mod:GetLocale()
+
+--------------------------------------------------------------------------------
 -- Initialization
 --
 
-function mod:GetOptions() return {86169, "berserk", "bosskill"} end
+function mod:GetOptions() return {83908, 86169, "berserk", "bosskill"} end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "FuriousRoar", 86169, 86170, 86171, 83710)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "MalevolentStrikes", 83908, 86158, 86157, 86159)
 
 	--no CheckBossStatus here as event does not fire, GM confirms known issue
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
@@ -22,7 +39,8 @@ function mod:OnBossEnable()
 	self:Death("Win", 44600)
 end
 
-function mod:OnEngage()
+function mod:OnEngage(diff)
+	stackWarn = diff > 2 and 5 or 10 --8% in heroic, 6% in normal, announce around 50-60% reduced healing
 	self:Berserk(360)
 end
 
@@ -35,3 +53,8 @@ function mod:FuriousRoar(_, spellId, _, _, spellName)
 	self:Bar(86169, spellName, 25, 86169)
 end
 
+function mod:MalevolentStrikes(player, spellId, _, _, spellName, stack)
+	if stack > stackWarn then
+		self:TargetMessage(83908, L["strikes_message"], player, "Urgent", spellId, "Info", stack)
+	end
+end
