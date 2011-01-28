c064a2859ff1955d40bcc5b38b3d41dda148b2b2
7destiny <7destiny@users.noreply.wowace.com>
2011-01-28 11:57:36 +0000

    koKR Update

--- Blackwing/Locales/koKR.lua
+++ Blackwing/Locales/koKR.lua
@@ -77,14 +77,18 @@ if L then
 	L.phase_desc = "단계 변화를 알립니다."
 	L.next_phase = "다음 단계"
 	L.green_phase_bar = "녹색 단계"
-
-	L.red_phase_trigger = "붉은"
+	
+	L.red_phase_trigger = "섞고 흔들어서, 열을 가한다..."
+	L.red_phase_emote_trigger = "붉은"
 	L.red_phase = "|cFFFF0000붉은|r 단계"
-	L.blue_phase_trigger = "푸른"
+	L.blue_phase_trigger = "급격한 온도 변화에 필멸의 육신"
+	L.blue_phase_emote_trigger = "푸른"
 	L.blue_phase = "|cFF809FFE푸른|r 단계"
-	L.green_phase_trigger = "초록"
+	L.green_phase_trigger = "이건 좀 불안정하지만"
+	L.green_phase_emote_trigger = "초록"
 	L.green_phase = "|cFF33FF00초록|r 단계"
-	L.dark_phase_trigger = "암흑"
+	L.dark_phase_trigger = "혼합물이 너무 약하구나, 말로리악!"
+	L.dark_phase_emote_trigger = "암흑"
 	L.dark_phase = "|cFF660099암흑|r 단계"
 end
 
