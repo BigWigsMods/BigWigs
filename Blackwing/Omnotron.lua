fdfe29880c6bc63e68e2384a3606bd80455c40dc
funkydude <funkydude@users.noreply.github.com>
2011-02-18 13:56:08 +0000

    Blackwing/Omnotron: reduce proximity

--- Blackwing/Omnotron.lua
+++ Blackwing/Omnotron.lua
@@ -169,7 +169,7 @@ end
 function mod:LightningConductor(player, spellId, _, _, spellName)
 	if UnitIsUnit(player, "player") then
 		self:FlashShake(79888)
-		self:OpenProximity(15, 79888) --assumed
+		self:OpenProximity(10, 79888) --assumed
 	end
 	self:TargetMessage(79888, spellName, player, "Attention", spellId, "Alarm")
 	self:SecondaryIcon(79888, player)
