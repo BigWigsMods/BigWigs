b36988e3124fd294461a7b2d7f380e5328271b6f
Caleb <calebzor@gmail.com>
2011-03-02 23:35:11 +0000

    improve obnoxious fiend warning a bit (thx to mysticalos for the suggestion)

--- Blackwing/Atramedes.lua
+++ Blackwing/Atramedes.lua
@@ -11,7 +11,8 @@ mod:RegisterEnableMob(41442)
 --
 
 local CL = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Common")
-local searingFlame, obnoxious = GetSpellInfo(77840), GetSpellInfo(92702)
+local searingFlame, obnoxious, pestered = GetSpellInfo(77840), GetSpellInfo(92702), GetSpellInfo(92685)
+local pesteredWarned = nil
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -58,9 +59,7 @@ function mod:OnBossEnable()
 	self:Yell("AirPhase", L["air_phase_trigger"])
 
 	self:Log("SPELL_AURA_APPLIED", "ObnoxiousPhaseShift", 92681)
-	-- I assume whoever it melees it will cast obnoxious on, but that has no destName
-	self:Log("SWING_DAMAGE", "Obnoxious", "*")
-	self:Log("SWING_MISS", "Obnoxious", "*")
+	self:RegisterEvent("UNIT_AURA")
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
@@ -73,6 +72,7 @@ function mod:OnEngage(diff)
 	self:DelayedMessage(77840, 35, L["searing_soon"], "Attention", 77840)
 	self:Bar("air_phase", L["air_phase"], 92, 5740) -- Rain of Fire Icon
 	if diff > 2 then
+		pesteredWarned = nil
 		self:Berserk(600)
 	end
 end
@@ -94,14 +94,16 @@ function mod:ObnoxiousPhaseShift(...)
 	self:Message(92677, L["obnoxious_soon"], "Attention", 92677) -- do we really need this?
 	local dGUID = select(10, ...)
 	FiendCheck(dGUID)
+	pesteredWarned = nil
 end
 
-function mod:Obnoxious(player, _, _, _, _, _, _, _, _, _, sGUID)
-	if tonumber(sGUID:sub(7, 10), 16) ~= 49740 then return end
-	if UnitIsUnit(player, "player") then
-		self:Say(92702, CL["say"]:format(obnoxious))
+function mod:UNIT_AURA(_, unit)
+	if pesteredWarned then return end
+	if unit == "player" and UnitDebuff("player", pestered) then
+		self:Say(92677, CL["say"]:format(obnoxious))
 	end
-	self:TargetMessage(92702, obnoxious, player, "Attention", spellId, "Long")
+	self:TargetMessage(92677, obnoxious, unit, "Attention", 92677, "Long")
+	pesteredWarned = true
 end
 
 function mod:Tracking(player, spellId, _, _, spellName)
