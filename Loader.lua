5d33155264af17ddbcfc4035af9553e719f80792
funkydude <funkydude@users.noreply.github.com>
2011-03-11 02:46:00 +0000

    flip around an if statement

--- Loader.lua
+++ Loader.lua
@@ -346,10 +346,10 @@ do
 		self.elapsed = self.elapsed + elapsed
 		if self.elapsed > 5 then
 			self:Hide()
-			if BIGWIGS_RELEASE_TYPE == ALPHA or BIGWIGS_RELEASE_TYPE == REPO then
-				SendAddonMessage("BWVRA3", BIGWIGS_RELEASE_REVISION, "RAID")
-			else
+			if BIGWIGS_RELEASE_TYPE == RELEASE then
 				SendAddonMessage("BWVR3", BIGWIGS_RELEASE_REVISION, "RAID")
+			else
+				SendAddonMessage("BWVRA3", BIGWIGS_RELEASE_REVISION, "RAID")
 			end
 		end
 	end)
