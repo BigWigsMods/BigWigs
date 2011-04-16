180b498feea6c05ae0b41c4823e12aaa636c0003
zhTW <zhTW@users.noreply.wowace.com>
2011-04-16 16:30:40 +0000

    zhTW Update.

--- Blackwing/Locales/zhTW.lua
+++ Blackwing/Locales/zhTW.lua
@@ -44,7 +44,7 @@ if L then
 	L.slump = "撲倒（騎乘）"
 	L.slump_desc = "當熔喉撲倒並暴露時發出警報。騎乘載具開始。"
 	L.slump_bar = "<下一騎乘>"
-	L.slump_message = "嘢，快騎上它！"
+	L.slump_message = "嘿，快騎上它！"
 	L.slump_trigger = "%s往前撲倒，露出他的鉗子!"
 
 	L.infection_message = ">你< 感染！"
