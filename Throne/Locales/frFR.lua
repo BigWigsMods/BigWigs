f2e8a04537490024d0922aedf00ffca8e3dda862
mojosdojo <mojosdojo@users.noreply.github.com>
2011-05-03 10:40:47 +0000

    Al'Akir frFR phase 3 fix according to forums.

--- Throne/Locales/frFR.lua
+++ Throne/Locales/frFR.lua
@@ -10,7 +10,7 @@ if L then
 
 	L.acid_rain = "Pluie acide (%d)"
 
-	L.phase3_yell = "Assez ! Je ne serais pas contenu plus longtemps !" -- à vérifier
+	L.phase3_yell = "Assez ! Je ne serai pas contenu plus longtemps !"
 
 	L.phase = "Changement de phase"
 	L.phase_desc = "Prévient quand la rencontre entre dans une nouvelle phase."
