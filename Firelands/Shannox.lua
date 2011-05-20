75dc25483cae481723e02961ab6488d55405becf
Caleb <calebzor@gmail.com>
2011-05-20 19:35:01 +0000

    start populating the module

--- Firelands/Shannox.lua
+++ Firelands/Shannox.lua
@@ -1,4 +1,4 @@
---[[if tonumber((select(4, GetBuildInfo()))) < 40200 then return end
+if tonumber((select(4, GetBuildInfo()))) < 40200 then return end
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
@@ -14,7 +14,7 @@ mod:RegisterEnableMob(53691)
 local CL = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Common")
 local L = mod:NewLocale("enUS", true)
 if L then
-
+	L.safe = "%s safe"
 end
 L = mod:GetLocale()
 
@@ -24,6 +24,8 @@ L = mod:GetLocale()
 
 function mod:GetOptions(CL)
 	return {
+		100002,
+		{100129, "ICON"},
 		"bosskill"
 	}, {
 		bosskill = "general"
@@ -31,18 +33,34 @@ function mod:GetOptions(CL)
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_SUCCESS", "FaceRage", 99945)
+	self:Log("SPELL_AURA_REMOVED", "FaceRageRemoved", 99945)
+	self:Log("SPELL_CAST_SUCCESS", "HurlSpear", 100031)
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
-	self:Death("Deaths", 53691)
+	self:Death("Death", 53691)
 end
 
 function mod:OnEngage(diff)
-
+	self:Bar(100002, (GetSpellInfo(100002)), 23, 100002) -- Hurl Spear
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
-]]
+
+function mod:HurlSpear(_, spellId, _, _, spellName)
+	self:Message(100002, spellName, "Attention", 100002)
+	self:Bar(100002, spellName, 41, 100002)
+end
+
+function mod:FaceRage(player, spellId, _, _, spellName)
+	self:TargetMessage(100129, spellName, player, "Important", 100129, "Alert")
+	self:PrimaryIcon(100129, player)
+end
+
+function mod:FaceRageRemoved(player, spellId)
+	self:Message(100129, L["safe"]:format(player), "Important", 100129)
+end
 
