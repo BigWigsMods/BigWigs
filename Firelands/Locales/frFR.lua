e4de0ec3598c1a854df3261c506fa6003c65df92
tborlee <tborlee@users.noreply.github.com>
2011-06-10 11:12:58 +0000

    frFR Update

--- Firelands/Locales/frFR.lua
+++ Firelands/Locales/frFR.lua
@@ -6,19 +6,19 @@ end
 
 L = BigWigs:NewBossLocale("Lord Rhyolith", "frFR")
 if L then
-	L.phase2_message = "Phase Immoler imminente ! Le boss a %dx %s"
+	L.phase2_message = "Phase Immoler imminenteÂ ! Le boss a %dx %s"
 end
 
 L = BigWigs:NewBossLocale("Alysrazor", "frFR")
 if L then
-	L.tornado_trigger = "These skies are MINE!" -- transcription needed
+	L.tornado_trigger = "Ce ciel est Ã  MOIÂ !" -- Ã  vÃ©rifier
 	L.claw_message = "%2$dx Griffes sur %1$s"
-	L.fullpower_message = "%s imminent !"
+	L.fullpower_message = "%s imminentÂ !"
 end
 
 L = BigWigs:NewBossLocale("Shannox", "frFR")
 if L then
-	L.safe = "%s sauvé"
+	L.safe = "%s sauvÃ©"
 end
 
 L = BigWigs:NewBossLocale("Baleroc", "frFR")
@@ -28,16 +28,14 @@ end
 
 L = BigWigs:NewBossLocale("Majordomo Staghelm", "frFR")
 if L then
-
+	L.seed_explosion = "Explosion de la graÃ®ne imminenteÂ !"
 end
 
 L = BigWigs:NewBossLocale("Ragnaros", "frFR")
 if L then
-	L.stage_one = "Phase 1 : Par le Feu soyez purifiés !" -- not yet translated in the EJ
-	L.intermission = "Intermission"
-	L.stage_two = "Phase 2 : Sulfuras sera votre fin !" -- not yet translated in the EJ
-	L.stage_three = "Phase 3 : Barrez-vous de mon royaume !" -- not yet translated in the EJ
-	L.sons_dead = "Fils morts !"
+	L.intermission = "Intervalle"
 	L.sons_left = "Il reste %d Fils"
+	L.engulfing_close = "%s proches"
+	L.engulfing_middle = "%s Ã©loignÃ©es"
+	L.engulfing_far = "%s au milieu"
 end
-
