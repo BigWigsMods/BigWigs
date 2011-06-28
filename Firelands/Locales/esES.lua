37e6abc4689e104b9f0f3f71730e9f6aad6c937f
jongt23 <jongt23@gmail.com>
2011-06-28 13:35:35 +0000

    esES Firelands update

--- Firelands/Locales/esES.lua
+++ Firelands/Locales/esES.lua
@@ -1,36 +1,42 @@
 local L = BigWigs:NewBossLocale("Beth'tilac", "esES")
 if not L then return end
 if L then
-
+	L.kiss_message = "%2$dx Beso en %1$s"
 end
 
 L = BigWigs:NewBossLocale("Lord Rhyolith", "esES")
 if L then
-
+	L.phase2_message = "¡Fase de inmolación inminente! El jefe tiene %dx %s"
 end
 
 L = BigWigs:NewBossLocale("Alysrazor", "esES")
 if L then
-
+	L.tornado_trigger = "¡El cielo es mío!"
+	L.claw_message = "%2$dx Garra en %1$s"
+	L.fullpower_message = "¡%s pronto!"
 end
 
 L = BigWigs:NewBossLocale("Shannox", "esES")
 if L then
-
+	L.safe = "%s a salvo"
 end
 
 L = BigWigs:NewBossLocale("Baleroc", "esES")
 if L then
-
+	L.torment_message = "%2$dx Tormento en %1$s"
 end
 
 L = BigWigs:NewBossLocale("Majordomo Staghelm", "esES")
 if L then
-
+	L.seed_explosion = "¡Semilla explota pronto!"
 end
 
 L = BigWigs:NewBossLocale("Ragnaros", "esES")
 if L then
-
+	L.intermission = "Intermedio"
+	L.sons_left = "%d Hijos restantes"
+	L.engulfing_close = "Cerca %s"
+	L.engulfing_middle = "Lejos %s"
+	L.engulfing_far = "Medio %s"
 end
 
