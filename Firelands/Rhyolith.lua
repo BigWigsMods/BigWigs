089b4c106f70c8f1d97ef7d7d91df2e1b8184971
Vidar Haarr <vhaarr@gmail.com>
2011-07-01 11:37:57 +0000

    Add messages for obsidian stacks and spark/fragment spawns.

--- Firelands/Rhyolith.lua
+++ Firelands/Rhyolith.lua
@@ -11,6 +11,8 @@ mod:RegisterEnableMob(52577, 53087, 52558) -- Left foot, Right Foot, Lord Rhyoli
 --
 
 local moltenArmor = GetSpellInfo(98255)
+local lastFragments = nil
+local bossGuid = nil
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -20,8 +22,12 @@ local CL = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Common")
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.molten_message = "%dx stacks on boss!"
+	L.armor_message = "%d%% armor left"
+	L.armor_gone_message = "Armor go bye-bye!"
 	L.phase2_soon_message = "Phase 2 soon!"
 	L.stomp_message = "Stomp! Stomp! Stomp!"
+	L.big_add_message = "Big add spawned!"
+	L.small_adds_message = "Small adds inc!"
 end
 L = mod:GetLocale()
 
@@ -31,7 +37,7 @@ L = mod:GetLocale()
 
 function mod:GetOptions(CL)
 	return {
-		97282, 98255, 99846,
+		98552, 98136, 97282, 98255, 99846,
 		"bosskill"
 	}, {
 		[98255] = "general"
@@ -41,6 +47,10 @@ end
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED_DOSE", "MoltenArmor", 98255, 101157)
 	self:Log("SPELL_CAST_START", "Stomp", 97282, 100411, 100968, 100969)
+	self:Log("SPELL_SUMMON", "Spark", 98552)
+	self:Log("SPELL_SUMMON", "Fragments", 100392, 98136)
+	self:Log("SPELL_AURA_REMOVED_DOSE", "ObsidianStack", 98632)
+	self:Log("SPELL_AURA_REMOVED", "Obsidian", 98632)
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
@@ -49,11 +59,35 @@ end
 
 function mod:OnEngage(diff)
 	self:RegisterEvent("UNIT_HEALTH_FREQUENT")
+	lastFragments = GetTime()
+	bossGuid = UnitGUID("boss1")
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:Obsidian(_, spellId, _, _, _, _, _, _, _, dGUID)
+	if dGUID ~= bossGuid then return end
+	self:Message(98632, L["armor_gone_message"], "Positive", spellId)
+end
+
+function mod:ObsidianStack(_, spellId, _, _, _, buffStack, _, _, _, dGUID)
+	if dGUID ~= bossGuid then return end
+	if buffStack % 20 ~= 0 then return end -- Only warn every 20
+	self:Message(98632, L["armor_message"]:format(buffStack), "Positive", spellId)
+end
+
+function mod:Spark(_, spellId)
+	self:Message(98552, L["big_add_message"], "Important", spellId, "Alarm")
+end
+
+function mod:Fragments(_, spellId)
+	local t = GetTime()
+	if lastFragments and t < (lastFragments + 5) then return end
+	lastFragments = t
+	self:Message(98136, L["small_adds_message"], "Attention", spellId, "Info")
+end
 
 function mod:Stomp(_, spellId, _, _, spellName)
 	self:Message(97282, L["stomp_message"], "Urgent",  spellId, "Alert")
