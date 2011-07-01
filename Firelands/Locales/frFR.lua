6fda54b30406e8027c745790b07e2b81d0cb6ca4
tborlee <tborlee@users.noreply.github.com>
2011-07-01 13:46:34 +0000

    frFR Update

--- Firelands/Locales/frFR.lua
+++ Firelands/Locales/frFR.lua
@@ -2,28 +2,42 @@ local L = BigWigs:NewBossLocale("Beth'tilac", "frFR")
 if not L then return end
 if L then
 	L.kiss_message = "%2$dx Baisers sur %1$s"
+	L.devastate_message = "Dévastation #%d !"
+	L.devastate_bar = "~Prochaine dévastation"
+	L.drone_bar = "Prochaine ouvrière braisetoile"
+	L.drone_message = "Arrivée d'une ouvrière braisetoile !"
 end
 
 L = BigWigs:NewBossLocale("Lord Rhyolith", "frFR")
 if L then
-	L.phase2_message = "Phase Immoler imminente ! Le boss a %dx %s"
+	L.molten_message = "%dx cumuls sur le boss !"
+	L.armor_message = "%d%% d'armure restants"
+	L.armor_gone_message = "Armure détruite !"
+	L.phase2_soon_message = "Phase 2 imminente !"
+	L.stomp_message = "Piétinement ! Piétinement ! Piétinement !"
+	L.big_add_message = "Étincelle de Rhyolith apparue !"
+	L.small_adds_message = "Arrivée de fragments de Rhyolith !"
 end
 
 L = BigWigs:NewBossLocale("Alysrazor", "frFR")
 if L then
 	L.tornado_trigger = "Ce ciel est à MOI !" -- à vérifier
 	L.claw_message = "%2$dx Griffes sur %1$s"
-	L.fullpower_message = "%s imminent !"
+	L.fullpower_soon_message = "Pleine puissance imminente !"
+	L.halfpower_soon_message = "Phase 4 imminente !"
+	L.encounter_restart = "Pleine puissance ! Et c'est reparti..."
 end
 
 L = BigWigs:NewBossLocale("Shannox", "frFR")
 if L then
 	L.safe = "%s sauvé"
+	L.immolation_trap = "Immolation sur %s !"
 end
 
 L = BigWigs:NewBossLocale("Baleroc", "frFR")
 if L then
 	L.torment_message = "%2$dx Tourments sur %1$s"
+	L.blade = "~Lame"
 end
 
 L = BigWigs:NewBossLocale("Majordomo Staghelm", "frFR")
