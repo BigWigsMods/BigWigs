a599b59b0a29653f29e94ba0290d4d4ca565e1b5
Vidar Haarr <vhaarr@gmail.com>
2011-07-01 09:20:43 +0000

    Redo the power based warnings and set berserk to 15min.

--- Firelands/Alysrazor.lua
+++ Firelands/Alysrazor.lua
@@ -7,7 +7,6 @@ if not mod then return end
 mod:RegisterEnableMob(52530)
 
 local fieryTornado, firestorm = (GetSpellInfo(99816)), (GetSpellInfo(101659))
-local powerWarned = false
 local woundTargets = mod:NewTargetList()
 
 --------------------------------------------------------------------------------
@@ -19,7 +18,9 @@ local L = mod:NewLocale("enUS", true)
 if L then
 	L.tornado_trigger = "These skies are MINE!"
 	L.claw_message = "%2$dx Claw on %1$s"
-	L.fullpower_message = "Full power soon!"
+	L.fullpower_soon_message = "Full power soon!"
+	L.halfpower_soon_message = "Phase 4 soon!"
+	L.encounter_restart = "Full power! Here we go again ..."
 end
 L = mod:GetLocale()
 
@@ -31,7 +32,7 @@ function mod:GetOptions(CL)
 	return {
 		99362, 100723,
 		99816, 99464,
-		99844, {99925, "FLASHSHAKE"},
+		99844, 99925,
 		{100744, "FLASHSHAKE"}, 100761,
 		"berserk", "bosskill"
 	}, {
@@ -44,14 +45,23 @@ function mod:GetOptions(CL)
 end
 
 function mod:OnBossEnable()
+	-- General
+	self:Log("SPELL_AURA_APPLIED", "Molting", 99464, 99465, 100698)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "BlazingClaw", 99844)
+
+	-- Phase 1
+	self:Log("SPELL_AURA_APPLIED", "Wound", 100723, 100722, 100721, 100720, 100719, 100718, 100024, 99308)
+	self:Log("SPELL_AURA_APPLIED", "Tantrum", 99362)
+	
+	-- Phase 2
 	self:Yell("FieryTornado", L["tornado_trigger"])
 
-	self:Log("SPELL_AURA_APPLIED", "Wound", 100723, 100722, 100721, 100720, 100719, 100718, 100024, 99308)
-	self:Log("SPELL_AURA_APPLIED", "Molting", 99464, 99465, 100698)
-	self:Log("SPELL_CAST_START", "Firestorm", 100744)
+	-- Phase 3
+	self:Log("SPELL_AURA_APPLIED", "Burnout", 99432)
+
+	-- Heroic only
 	self:Log("SPELL_CAST_START", "Cataclysm", 100761)
-	self:Log("SPELL_AURA_APPLIED", "Tantrum", 99362)
-	self:Log("SPELL_AURA_APPLIED_DOSE", "BlazingClaw", 99844)
+	self:Log("SPELL_CAST_START", "Firestorm", 100744)
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
@@ -59,7 +69,7 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage(diff)
-	self:Berserk(600) -- assumed
+	self:Berserk(900) -- assumed
 	self:Bar(99816, fieryTornado, 190, 99816)
 	if diff > 2 then
 		self:Bar(99816, fieryTornado, 250, 99816)
@@ -67,8 +77,6 @@ function mod:OnEngage(diff)
 	else
 		self:Bar(99464, (GetSpellInfo(99464)), 60, 99464) -- Molting
 	end
-	self:RegisterEvent("UNIT_POWER")
-	powerWarned = false
 end
 
 --------------------------------------------------------------------------------
@@ -130,17 +138,33 @@ function mod:BlazingClaw(player, spellId, _, _, _, stack)
 	end
 end
 
-function mod:UNIT_POWER()
-	local power = UnitPower("boss1", SPELL_POWER_FOCUS)
-	if power > 80 and not powerWarned then
-		powerWarned = true
-		self:Message(99925, L["fullpower_message"], "Attention", 99925)
-	elseif power < 80 then
-		powerWarned = false
-	elseif power == 100 then
-		self:Message(99925, (GetSpellInfo(99925)), "Important", 99925, "Alert")
-		self:FlashShake(99925)
-		self:Bar(99816, fieryTornado, 165, 99816)
+do
+	local halfWarned = false
+	local fullWarned = false
+
+	-- Alysrazor crashes to the ground
+	function mod:Burnout()
+		halfWarned, fullWarned = false, false
+		self:RegisterEvent("UNIT_POWER")
+	end
+
+	function mod:UNIT_POWER(_, unit)
+		local t = UnitPowerType("boss1") -- I think it's SPELL_POWER_FOCUS
+		local power = UnitPower("boss1", t)
+		if t ~= SPELL_POWER_FOCUS then
+			print("Power type is " .. tostring(t) .. "?!")
+		end
+		if power > 40 and not halfWarned then
+			self:Message(99925, L["halfpower_soon_message"], "Urgent", 99925)
+			halfWarned = true
+		elseif power > 80 and not fullWarned then
+			self:Message(99925, L["fullpower_soon_message"], "Attention", 99925)
+			fullWarned = true
+		elseif power == 100 then
+			self:Message(99925, L["encounter_restart"], "Positive", 99925, "Alert")
+			self:Bar(99816, fieryTornado, 165, 99816)
+			self:UnregisterEvent("UNIT_POWER")
+		end
 	end
 end
 
