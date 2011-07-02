4f496fdf6a3d60644407b271474c6ae77676518b
Vidar Haarr <vhaarr@gmail.com>
2011-07-02 22:13:02 +0000

    Poke the player if he has no feather buffs before phase 2.

--- Firelands/Alysrazor.lua
+++ Firelands/Alysrazor.lua
@@ -21,6 +21,15 @@ if L then
 	L.fullpower_soon_message = "Full power soon!"
 	L.halfpower_soon_message = "Phase 4 soon!"
 	L.encounter_restart = "Full power! Here we go again ..."
+	L.no_stacks_message = "Dunno if you care, but you have no feathers"
+	L.moonkin_message = "Stop pretending and get some real feathers"
+
+	L.worm_emote = "Fiery Lava Worms erupt from the ground!"
+	L.phase2_soon_emote = "Alysrazor begins to fly in a rapid circle!"
+	L.phase2_emote = "99794" -- Fiery Vortex spell ID used in the emote
+	L.phase3_emote = "99432" -- Burns Out spell ID used in the emote
+	L.phase4_emote = "99922" -- Re-Ignites spell ID used in the emote
+	L.restart_emote = "99925" -- Full Power spell ID used in the emote
 end
 L = mod:GetLocale()
 
@@ -30,7 +39,7 @@ L = mod:GetLocale()
 
 function mod:GetOptions(CL)
 	return {
-		99362, 100723,
+		99362, 100723, 97128,
 		99816, 99464,
 		99844, 99925,
 		{100744, "FLASHSHAKE"}, 100761,
@@ -53,6 +62,8 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Wound", 100723, 100722, 100721, 100720, 100719, 100718, 100024, 99308)
 	self:Log("SPELL_AURA_APPLIED", "Tantrum", 99362)
 
+	self:Emote("BuffCheck", L["worm_emote"], L["phase2_soon_emote"])
+
 	-- Phase 2
 	self:Yell("FieryTornado", L["tornado_trigger"])
 
@@ -70,11 +81,11 @@ end
 
 function mod:OnEngage(diff)
 	self:Berserk(900) -- assumed
-	self:Bar(99816, fieryTornado, 190, 99816)
 	if diff > 2 then
 		self:Bar(99816, fieryTornado, 250, 99816)
 		self:Bar(100744, firestorm, 93, 100744)
 	else
+		self:Bar(99816, fieryTornado, 190, 99816)
 		self:Bar(99464, (GetSpellInfo(99464)), 60, 99464) -- Molting
 	end
 end
@@ -82,6 +93,21 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+do
+	local feather = GetSpellInfo(97128)
+	local moonkin = GetSpellInfo(24858)
+	function mod:BuffCheck()
+		local name = UnitBuff("player", feather)
+		if not name then
+			if UnitBuff("player", moonkin) then
+				self:Message(97128, L["moonkin_message"], "Personal", 97128)
+			else
+				self:Message(97128, L["no_stacks_message"], "Personal", 97128)
+			end
+		end
+	end
+end
 
 do
 	local scheduled = nil
@@ -146,6 +172,17 @@ do
 		halfWarned, fullWarned = false, false
 		self:RegisterEvent("UNIT_POWER")
 	end
+
+--[[
+In case we need to rebase this on emotes instead of unit power, here's a few noteworthy events
+"<2329.1> RAID_BOSS_EMOTE#Fiery Lava Worms erupt from the ground!#Plump Lava Worm#0#false", -- [17]
+"<2391.0> RAID_BOSS_EMOTE#Fiery Lava Worms erupt from the ground!#Plump Lava Worm#0#false", -- [18]
+"<2454.5> RAID_BOSS_EMOTE#Alysrazor begins to fly in a rapid circle!  The harsh winds will remove Wings of Flame!#Alysrazor#0#false", -- [19]
+"<2461.0> RAID_BOSS_EMOTE#|TInterface\\Icons\\ability_mage_firestarter.blp:20|t The harsh winds form a |cFFFF0000|Hspell:99794|h[Fiery Vortex]|h|r!#Fiery Vortex#0#false", -- [20]
+"<2485.0> RAID_BOSS_EMOTE#|TInterface\\Icons\\spell_holiday_tow_spicecloud.blp:20|t Alysrazor's fire |cFFFF0000|Hspell:99432|h[Burns Out]|h|r!#Alysrazor#0#false", -- [21]
+"<2515.6> RAID_BOSS_EMOTE#|TInterface\\Icons\\inv_elemental_primal_fire.blp:20|t Alysrazor's firey core |cFFFF0000|Hspell:99922|h[Re-Ignites]|h|r!#Alysrazor#0#false", -- [22]
+"<2542.8> RAID_BOSS_EMOTE#|TInterface\\Icons\\spell_shaman_improvedfirenova.blp:20|t Alysrazor is at |cFFFF0000|Hspell:99925|h[Full Power]|h|r!#Alysrazor#0#false", -- [23]
+]]
 
 	function mod:UNIT_POWER(_, unit)
 		local power = UnitPower("boss1", 0)
