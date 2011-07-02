9ba26c75be6686e8690e2ca4bdef2da97aa22aef
Adavak <adavak@adavak.com>
2011-07-02 15:27:42 +0000

    BigWigs: - zhCN/zhTW update.

--- Firelands/Locales/zhCN.lua
+++ Firelands/Locales/zhCN.lua
@@ -1,29 +1,42 @@
 local L = BigWigs:NewBossLocale("Beth'tilac", "zhCN")
 if not L then return end
 if L then
-
+	L.devastate_message = "Devastation #%d!"
+	L.devastate_bar = "~Next devastation"
+	L.drone_bar = "Next Cinderweb Drone"
+	L.drone_message = "Big drone incoming!"
 end
 
 L = BigWigs:NewBossLocale("Lord Rhyolith", "zhCN")
 if L then
-	L.phase2_message = "Immolation phase soon! Boss has %dx %s"
+	L.molten_message = "%dx stacks on boss!"
+	L.armor_message = "%d%% armor left"
+	L.armor_gone_message = "Armor go bye-bye!"
+	L.phase2_soon_message = "Phase 2 soon!"
+	L.stomp_message = "Stomp! Stomp! Stomp!"
+	L.big_add_message = "Big add spawned!"
+	L.small_adds_message = "Small adds inc!"
 end
 
 L = BigWigs:NewBossLocale("Alysrazor", "zhCN")
 if L then
 	L.tornado_trigger = "These skies are MINE!"
 	L.claw_message = "%2$dx Claw on %1$s"
-	L.fullpower_message = "%s soon!"
+	L.fullpower_soon_message = "Full power soon!"
+	L.halfpower_soon_message = "Phase 4 soon!"
+	L.encounter_restart = "Full power! Here we go again ..."
 end
 
 L = BigWigs:NewBossLocale("Shannox", "zhCN")
 if L then
 	L.safe = "%s safe"
+	L.immolation_trap = "Immolation on %s!"
 end
 
 L = BigWigs:NewBossLocale("Baleroc", "zhCN")
 if L then
 	L.torment_message = "%2$dx torment on %1$s"
+	L.blade = "~Blade"
 end
 
 L = BigWigs:NewBossLocale("Majordomo Staghelm", "zhCN")
@@ -36,7 +49,7 @@ if L then
 	L.intermission = "Intermission"
 	L.sons_left = "%d Sons Left"
 	L.engulfing_close = "Close %s"
-	L.engulfing_middle = "Far %s"
-	L.engulfing_far = "Middle %s"
+	L.engulfing_middle = "Middle %s"
+	L.engulfing_far = "Far %s"
 end
 
--- Firelands/Locales/zhTW.lua
+++ Firelands/Locales/zhTW.lua
@@ -1,42 +1,55 @@
 local L = BigWigs:NewBossLocale("Beth'tilac", "zhTW")
 if not L then return end
 if L then
-
+	L.devastate_message = "悶燒破壞：>%d<！"
+	L.devastate_bar = "<下一悶燒破壞>"
+	L.drone_bar = "<下一燼網雄蛛>"
+	L.drone_message = "即將 燼網雄蛛！"
 end
 
 L = BigWigs:NewBossLocale("Lord Rhyolith", "zhTW")
 if L then
-	L.phase2_message = "Immolation phase soon! Boss has %dx %s"
+	L.molten_message = "首領炎甲術堆疊：>%d<層！"
+	L.armor_message = "%d%% 黑曜石護甲剩餘！"
+	L.armor_gone_message = "黑曜石護甲消失！"
+	L.phase2_soon_message = "第二階段！"
+	L.stomp_message = "烈焰踐踏！"
+	L.big_add_message = "萊爾利斯火花 出現！"
+	L.small_adds_message = "即將 萊爾利斯碎片！"
 end
 
 L = BigWigs:NewBossLocale("Alysrazor", "zhTW")
 if L then
-	L.tornado_trigger = "These skies are MINE!"
-	L.claw_message = "%2$dx Claw on %1$s"
-	L.fullpower_message = "%s soon!"
+	L.tornado_trigger = "天空是屬於我的!"
+	L.claw_message = "熾炎爪擊%2$d層：>%1$s<！"
+	L.fullpower_soon_message = "即將 完全的力量！"
+	L.halfpower_soon_message = "第四階段！"
+	L.encounter_restart = "完全的力量"
 end
 
 L = BigWigs:NewBossLocale("Shannox", "zhTW")
 if L then
-	L.safe = "%s safe"
+	L.safe = ">%s< 安全！"
+	L.immolation_trap = "獻祭陷阱：>%s<！"
 end
 
 L = BigWigs:NewBossLocale("Baleroc", "zhTW")
 if L then
-	L.torment_message = "%2$dx torment on %1$s"
+	L.torment_message = "折磨%2$d層：>%1$s<！"
+	L.blade = "<虐殺之刃>"
 end
 
 L = BigWigs:NewBossLocale("Majordomo Staghelm", "zhTW")
 if L then
-	L.seed_explosion = "Seed explosion soon!"
+	L.seed_explosion = "即將 灼熱種子爆炸！"
 end
 
 L = BigWigs:NewBossLocale("Ragnaros", "zhTW")
 if L then
-	L.intermission = "Intermission"
-	L.sons_left = "%d Sons Left"
-	L.engulfing_close = "Close %s"
-	L.engulfing_middle = "Far %s"
-	L.engulfing_far = "Middle %s"
+	L.intermission = "中場"
+	L.sons_left = ">%d< 烈焰之子剩餘！"
+	L.engulfing_close = "近點：>%s<！"
+	L.engulfing_middle = "中點：>%s<！"
+	L.engulfing_far = "遠點：>%s<！"
 end
 
