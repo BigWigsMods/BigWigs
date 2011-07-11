92bba9f15dcf8c524d45de7e96e01e9bb9278a1d
a9012456 <a9012456@users.noreply.wowace.com>
2011-07-11 09:10:26 +0000

    zhTW update

--- Locales/common.zhTW.lua
+++ Locales/common.zhTW.lua
@@ -3,9 +3,9 @@ local L = LibStub("AceLocale-3.0"):NewLocale("Big Wigs: Common", "zhTW")
 if not L then return end
 
 -- Prototype.lua common words
-L.you = ">你< %s！"
-L.other = "%s：>%s<！"
-L.say = ">我< %s！"
+L.you = "你 %s！"
+L.other = "%s：%s！"
+L.say = "我 %s！"
 
 L.phase = "階段%d"
 L.normal = "普通模式"
@@ -15,7 +15,7 @@ L.general = "通用" -- General settings, i.e. things that apply to both normal 
 
 -- Localizers note:
 -- The default mod:Berserk(600) uses spell ID 26662 to get the Berserk name
-L.custom_start = ">%s<激活 - %s將在%d分後！"
-L.custom_end = ">%s<將%s！"
-L.custom_min = ">%s<分後%d！"
-L.custom_sec = ">%s<秒後%d！"
+L.custom_start = "%s 開戰 - %s將在%d分後！"
+L.custom_end = "%s 將%s！"
+L.custom_min = "%s 分後%d！"
+L.custom_sec = "%s 秒後%d！"
