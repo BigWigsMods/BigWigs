076964722710648940b304ad720cc1415aaf85c8
tborlee <tborlee@users.noreply.github.com>
2011-07-22 13:58:15 +0000

    frFR Update

--- Firelands/Locales/frFR.lua
+++ Firelands/Locales/frFR.lua
@@ -30,6 +30,8 @@ if L then
 	L.no_stacks_message = "Juste au cas où : vous n'avez pas de plumes."
 	L.moonkin_message = "Arrêtez de faire genre et récupérez de vraies plumes."
 
+	L.stage_message = "Stage %d"
+
 	L.worm_emote = "Des vers de lave embrasés surgissent du sol !"
 	L.phase2_soon_emote = "Alysrazor commence à voler en cercles rapides !"
 	L.phase2_emote = "99794" -- Fiery Vortex spell ID used in the emote
@@ -68,10 +70,11 @@ if L then
 	L.seed_explosion = "Explosion des graînes !"
 	L.intermission_bar = "Intervalle"
 	L.intermission_message = "Intervalle !"
-	L.sons_left = "Il reste %d fils"
+	L.sons_left = "%d |4fils restants:fils restant;"
 	L.engulfing_close = "Section proche engloutie !"
 	L.engulfing_middle = "Section centrale engloutie !"
 	L.engulfing_far = "Section éloignée engloutie !"
 	L.hand_bar = "Prochain \"knockback\""
+	L.wound_bar = "Blessure sur %s"
 	L.ragnaros_back_message = "Ragnaros est de retour !"
 end
