7ac70cacc3beafa72bd38047f396370d092c0954
funkydude <funkydude@users.noreply.github.com>
2011-10-21 16:11:57 +0000

    Options/Options.lua: more 4.2 > 4.3 compat

--- Options/Options.lua
+++ Options/Options.lua
@@ -188,8 +188,15 @@ local function translateZoneID(id)
 		name = GetMapNameByID(id)
 	end
 	-- XXX temp until 4.3
-	if id == 820 then name = "End Time - 4.3 PTR" end
-	if id == 824 then name = "Dragon Soul - 4.3 PTR" end
+	if id == 820 then
+		name = "End Time - 4.3 PTR"
+	elseif id == 824 then
+		name = "Dragon Soul - 4.3 PTR"
+	elseif id == 816 then
+		name = "Well of Eternity - 4.3 PTR"
+	elseif if == 819 then
+		name = "Hour of Twilight - 4.3 PTR"
+	end
 	if not name then
 		print(("Big Wigs: Tried to translate %q as a zone ID, but it could not be resolved into a name."):format(tostring(id)))
 	end
