b17d539854be175c531db9310f49cbc68dc3f0e3
tborlee <tborlee@users.noreply.github.com>
2011-11-08 20:01:35 +0000

    ANSI FTL.

--- DragonSoul/Locales/frFR.lua
+++ DragonSoul/Locales/frFR.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Morchok", "frFR")
 if not L then return end
 if L then
 	L.crush = "Ecraser armure"
-	L.crush_desc = "Alerte pour tanks uniquement. Compte les cumuls d'écraser armure et affiche une barre de durée."
+	L.crush_desc = "Alerte pour tanks uniquement. Compte les cumuls d'Ã©craser armure et affiche une barre de durÃ©e."
 	L.crush_icon = 103687
 	L.crush_message = "%2$dx Ecraser sur %1$s"
 
@@ -26,10 +26,10 @@ end
 
 L = BigWigs:NewBossLocale("Yor'sahj the Unsleeping", "frFR")
 if L then
-	L.bolt = "Éclair de Vide"
-	L.bolt_desc = "Alerte pour tanks uniquement. Compte les cumuls d'éclairs de Vide et affiche une barre de durée."
+	L.bolt = "Ã‰clair de Vide"
+	L.bolt_desc = "Alerte pour tanks uniquement. Compte les cumuls d'Ã©clairs de Vide et affiche une barre de durÃ©e."
 	L.bolt_icon = 108383
-	L.bolt_message = "%2$dx Éclairs sur %1$s"
+	L.bolt_message = "%2$dx Ã‰clairs sur %1$s"
 
 	L.blue = "Bleu"
 	L.green = "Vert"
@@ -40,7 +40,7 @@ if L then
 
 	L.blobs = "Globules"
 	L.blobs_bar = "Proch. globules"
-	L.blobs_desc = "Globules se déplacant en direction du boss."
+	L.blobs_desc = "Globules se dÃ©placant en direction du boss."
 	L.blobs_icon = "achievement_doublerainbow"
 end
 
@@ -53,36 +53,36 @@ L = BigWigs:NewBossLocale("Ultraxion", "frFR")
 if L then
 	L.engage_trigger = "I sense a great disturbance in the balance approaching. The chaos of it burns my mind"
 
-	L.warmup = "Échauffement"
-	L.warmup_desc = "Délai indiquant le temps restant avant le début de la rencontre."
+	L.warmup = "Ã‰chauffement"
+	L.warmup_desc = "DÃ©lai indiquant le temps restant avant le dÃ©but de la rencontre."
 end
 
 L = BigWigs:NewBossLocale("Warmaster Blackhorn", "frFR")
 if L then
 	L.harpooning = "Harponnage"
 
-	L.rush = "Volée de lames"
+	L.rush = "VolÃ©e de lames"
 	L.rush_desc = select(2, EJ_GetSectionInfo(4198))
 	L.rush_icon = 100 -- charge icon
 
 	L.sunder = "Fracasser armure"
-	L.sunder_desc = "Alerte pour tanks uniquement. Compte les cumuls de fracasser armure et affiche une barre de durée."
+	L.sunder_desc = "Alerte pour tanks uniquement. Compte les cumuls de fracasser armure et affiche une barre de durÃ©e."
 	L.sunder_icon = 108043
 	L.sunder_message = "%2$dx Fracasser sur %1$s"
 end
 
 L = BigWigs:NewBossLocale("Spine of Deathwing", "frFR")
 if L then
-	L.left_start = "va faire un tonneau à gauche"
-	L.right_start = "va faire un tonneau à droite"
-	L.left = "fait un tonneau à gauche"
-	L.right = "fait un tonneau à droite"
+	L.left_start = "va faire un tonneau Ã  gauche"
+	L.right_start = "va faire un tonneau Ã  droite"
+	L.left = "fait un tonneau Ã  gauche"
+	L.right = "fait un tonneau Ã  droite"
 
 	L.roll, L.roll_desc = EJ_GetSectionInfo(4050)
 	L.roll_icon = "ACHIEVEMENT_BG_RETURNXFLAGS_DEF_WSG"
-	L.roll_soon = "Tonneau imminent !"
+	L.roll_soon = "Tonneau imminentÂ !"
 
-	L.not_hooked = "Vous n'êtes PAS accroché !"
+	L.not_hooked = "Vous n'Ãªtes PAS accrochÃ©Â !"
 end
 
 L = BigWigs:NewBossLocale("Madness of Deathwing", "frFR")
