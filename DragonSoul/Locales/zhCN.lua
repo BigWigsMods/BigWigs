4fa010b570ece94aa40c59c57106ac9e1209e9e7
Adavak <adavak@adavak.com>
2011-11-15 16:02:51 +0000

    - zhCN/zhTW utf8.

--- DragonSoul/Locales/zhCN.lua
+++ DragonSoul/Locales/zhCN.lua
@@ -1,17 +1,41 @@
 local L = BigWigs:NewBossLocale("Morchok", "zhCN")
 if not L then return end
 if L then
+	L.crush = "Crush Armor"
+	L.crush_desc = "Tank alert only. Count the stacks of crush armor and show a duration bar."
+	L.crush_message = "%2$dx Crush on %1$s"
 
+	L.explosion = "Explosion"
 end
 
 L = BigWigs:NewBossLocale("Warlord Zon'ozz", "zhCN")
 if L then
+	L.ball = "Void ball"
+	L.ball_desc = "Void ball that bounces off of players and the boss."
 
+	L.bounce = "Void ball bounce"
+	L.bounce_desc = "Counter for the void ball bounces."
+
+	L.darkness = "Tentacle disco party!"
+	L.darkness_desc = "This phase starts, when the void ball hits the boss."
 end
 
 L = BigWigs:NewBossLocale("Yor'sahj the Unsleeping", "zhCN")
 if L then
+	L.bolt = "Void Bolt"
+	L.bolt_desc = "Tank alert only. Count the stacks of void bolt and show a duration bar."
+	L.bolt_message = "%2$dx Bolt on %1$s"
 
+	L.blue = "蓝"
+	L.green = "绿"
+	L.purple = "粉"
+	L.yellow = "黄"
+	L.black = "黑"
+	L.red = "红"
+
+	L.blobs = "Blobs"
+	L.blobs_bar = "Next blob spawn"
+	L.blobs_desc = "Blobs moving towards the boss"
 end
 
 L = BigWigs:NewBossLocale("Hagara the Stormbinder", "zhCN")
@@ -21,17 +45,33 @@ end
 
 L = BigWigs:NewBossLocale("Ultraxion", "zhCN")
 if L then
+	L.engage_trigger = "I sense a great disturbance in the balance approaching. The chaos of it burns my mind"
 
+	L.warmup = "Warmup"
+	L.warmup_desc = "Warmup timer"
 end
 
 L = BigWigs:NewBossLocale("Warmaster Blackhorn", "zhCN")
 if L then
+	L.harpooning = "Harpooning"
 
+	L.rush = "Blade Rush"
+
+	L.sunder = "Sunder Armor"
+	L.sunder_desc = "Tank alert only. Count the stacks of sunder armor and show a duration bar."
+	L.sunder_message = "%2$dx Sunder on %1$s"
 end
 
 L = BigWigs:NewBossLocale("Spine of Deathwing", "zhCN")
 if L then
+	L.left_start = "about to roll left"
+	L.right_start = "about to roll right"
+	L.left = "rolls left"
+	L.right = "rolls right"
 
+	L.roll_soon = "Barrel roll soon!"
+
+	L.not_hooked = "You are NOT hooked!"
 end
 
 L = BigWigs:NewBossLocale("Madness of Deathwing", "zhCN")
--- DragonSoul/Locales/zhTW.lua
+++ DragonSoul/Locales/zhTW.lua
@@ -1,17 +1,41 @@
 local L = BigWigs:NewBossLocale("Morchok", "zhTW")
 if not L then return end
 if L then
+	L.crush = "擊碎護甲"
+	L.crush_desc = "只警報坦克。擊碎護甲堆疊計數並顯示持續條。"
+	L.crush_message = "%2$d層擊碎護甲：>%1$s<！"
 
+	L.explosion = "爆炸水晶"
 end
 
 L = BigWigs:NewBossLocale("Warlord Zon'ozz", "zhTW")
 if L then
+	L.ball = "壞滅虛無"
+	L.ball_desc = "壞滅虛無在玩家和首領之間來回彈跳時發出警報。"
 
+	L.bounce = "壞滅虛無彈跳"
+	L.bounce_desc = "壞滅虛無彈跳計數。"
+
+	L.darkness = "觸鬚迪斯可聚會！"
+	L.darkness_desc = "當此階段開始，壞滅虛無撞擊首領。"
 end
 
 L = BigWigs:NewBossLocale("Yor'sahj the Unsleeping", "zhTW")
 if L then
+	L.bolt = "虛無箭"
+	L.bolt_desc = 只警報坦克。虛無箭堆疊計數並顯示持續條。"
+	L.bolt_message = "%2$d層虛無箭：>%1$s<！"
 
+	L.blue = "藍"
+	L.green = "綠"
+	L.purple = "粉"
+	L.yellow = "黃"
+	L.black = "黑"
+	L.red = "紅"
+
+	L.blobs = "血珠"
+	L.blobs_bar = "下一血珠。"
+	L.blobs_desc = "當血珠向首領移動時發出警報。"
 end
 
 L = BigWigs:NewBossLocale("Hagara the Stormbinder", "zhTW")
@@ -21,17 +45,33 @@ end
 
 L = BigWigs:NewBossLocale("Ultraxion", "zhTW")
 if L then
+	L.engage_trigger = "I sense a great disturbance in the balance approaching. The chaos of it burns my mind"
 
+	L.warmup = "暮光之時"
+	L.warmup_desc = "暮光之時计时器。"
 end
 
 L = BigWigs:NewBossLocale("Warmaster Blackhorn", "zhTW")
 if L then
+	L.harpooning = "魚叉"
 
+	L.rush = "刃突"
+
+	L.sunder = "破甲攻擊"
+	L.sunder_desc = "只警報坦克。破甲攻擊堆疊計數並顯示持續條。"
+	L.sunder_message = "%2$d層破甲攻擊：>%1$s<！"
 end
 
 L = BigWigs:NewBossLocale("Spine of Deathwing", "zhTW")
 if L then
+	L.left_start = "即將左側翻滾"
+	L.right_start = "即將右側翻滾"
+	L.left = "左側翻滾"
+	L.right = "右側翻滾"
 
+	L.roll_soon = "即將翻滾！"
+
+	L.not_hooked = ">你< 沒有抓牢！"
 end
 
 L = BigWigs:NewBossLocale("Madness of Deathwing", "zhTW")
