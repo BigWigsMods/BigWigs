96c905256aa4851584809c2d299d67f7c0ad0e46
jongt23 <jongt23@gmail.com>
2011-11-30 12:16:08 +0000

    esES Update (Dragon Soul/Alma de Dragón)

--- DragonSoul/Locales/esES.lua
+++ DragonSoul/Locales/esES.lua
@@ -1,37 +1,88 @@
 local L = BigWigs:NewBossLocale("Morchok", "esES")
 if not L then return end
 if L then
+	L.crush = "Machacar armadura"
+	L.crush_desc = "Alerta solo para tanques. Muestra los stacs de Machacar armadura y una barra con su duración."
+	L.crush_message = "%2$dx Machacar en %1$s"
 
+	L.blood = "Sangre"
+
+	L.explosion = "Explosión"
 end
 
 L = BigWigs:NewBossLocale("Warlord Zon'ozz", "esES")
 if L then
+	L.ball = "Esfera de vacío"
+	L.ball_desc = "Una esfera de vacío que rebota entre jugadores y el jefe."
 
+	L.bounce = "Rebotar Esfera de vacío"
+	L.bounce_desc = "Contador para el rebote de la Esfera de vacío."
+
+	L.darkness = "¡Fiesta de tentáculos!"
+	L.darkness_desc = "Esta fase comienza, cuando la esfera de vacío golpea al jefe."
 end
 
 L = BigWigs:NewBossLocale("Yor'sahj the Unsleeping", "esES")
 if L then
+	L.bolt = "Descarga del Vacío"
+	L.bolt_desc = "Alerta para tanques. Cuenta los stacs de Descarga de Vacío y muestra una barra con su duración."
+	L.bolt_message = "%2$dx Descarga en %1$s"
 
+	L.blue = "Azul"
+	L.green = "Verde"
+	L.purple = "Morado"
+	L.yellow = "Amarillo"
+	L.black = "Negro"
+	L.red = "Rojo"
+
+	L.blobs = "Manchas"
+	L.blobs_bar = "Próxima Mancha aparece"
+	L.blobs_desc = "Las Manchas se mueven hacia el jefe"
 end
 
 L = BigWigs:NewBossLocale("Hagara the Stormbinder", "esES")
 if L then
+	L.lightning_or_frost = "Relámpago o Hielo"
+	L.ice_next = "Fase de Hielo"
+	L.lightning_next = "Fase de Relámpago"
 
+	L.nextphase = "Siguiente Fase"
+	L.nextphase_desc = "Avisos para la siguiente fase"
 end
 
 L = BigWigs:NewBossLocale("Ultraxion", "esES")
 if L then
+	L.engage_trigger = "I am the beginning of the end...the shadow which blots out the sun" -- translate
 
+	L.warmup = "Calentamiento"
+	L.warmup_desc = "Tiempo de calentamiento"
 end
 
 L = BigWigs:NewBossLocale("Warmaster Blackhorn", "esES")
 if L then
+	L.harpooning = "Arponear"
 
+	L.rush = "Carga de hojas"
+
+	L.sunder = "Hender armadura"
+	L.sunder_desc = "Alerta para tanques. Muestra los stacs de Hender armadura y una barra con su duración."
+	L.sunder_message = "%2$dx Hender en %1$s"
+
+	L.sapper_trigger = "A drake swoops down to drop a Twilight Sapper onto the deck!" -- translate
+	L.sapper = "Zapador"
+	L.sapper_desc = "El Zapador intenta dañar la nave"
 end
 
 L = BigWigs:NewBossLocale("Spine of Deathwing", "esES")
 if L then
+	L.left_start = "a punto de rodar a la izquierda"
+	L.right_start = "a punto de rodar a la derecha"
+	L.left = "rueda a la izquierda"
+	L.right = "rueda a la derecha"
 
+	L.roll_soon = "¡Barril rodará pronto!"
+
+	L.not_hooked = "¡NO ESTAS enganchado!"
 end
 
 L = BigWigs:NewBossLocale("Madness of Deathwing", "esES")
