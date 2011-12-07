112fa9751e18bf1ba79914eac04c368fe669ace0
funkydude <funkydude@users.noreply.github.com>
2011-12-07 17:12:45 +0000

    ruRU commonlocale update by wetxius

--- Locales/common.ruRU.lua
+++ Locales/common.ruRU.lua
@@ -3,12 +3,12 @@ local L = LibStub("AceLocale-3.0"):NewLocale("Big Wigs: Common", "ruRU")
 if not L then return end
 
 -- Prototype.lua common words
-L.you = "%s на ВАС"
+L.you = "%s на ТЕБЕ"
 L.underyou = "%s под ТОБОЙ!"
 L.other = "%s: %s"
-L.say = "%s on ME!"
-L.cast = "<Casting %s>"
-L.soon = "%s soon!"
+L.say = "%s на МНЕ!"
+L.cast = "<Каст %s>"
+L.soon = "%s скоро!"
 
 L.phase = "Фаза %d"
 L.normal = "Обычный режим"
@@ -16,8 +16,8 @@ L.heroic = "Героический режим"
 L.hard = "Сложный режим"
 L.general = "Общие способности" -- General settings, i.e. things that apply to both normal and hard mode.
 
-L.custom_start = "%s вступает в бой - %s через %d минут"
-L.custom_start_s = "%s вступает в бой - %s через %d секунд"
-L.custom_end = "%s goes %s!"--need check
-L.custom_min = "%s через %d минут"
-L.custom_sec = "%s через %d секунд!"
+L.custom_start = "%s вступает в бой - %s через %d мин"
+L.custom_start_s = "%s вступает в бой - %s через %d сек"
+L.custom_end = "%s проходит %s!"--need check
+L.custom_min = "%s через %d мин"
+L.custom_sec = "%s через %d сек!"
