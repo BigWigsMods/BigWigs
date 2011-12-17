2fc80405ff6926bd551ba8fb44524e7d38add7ea
funkydude <funkydude@users.noreply.github.com>
2011-12-17 02:06:13 +0000

    DragonSoul/Morchok: separate the stomp/crystal options between Morchok and Kohcrom so they can be enabled separately.

--- DragonSoul/Morchok.lua
+++ DragonSoul/Morchok.lua
@@ -6,8 +6,6 @@ local mod, CL = BigWigs:NewBoss("Morchok", 824, 311)
 if not mod then return end
 mod:RegisterEnableMob(55265)
 
-local stomp = GetSpellInfo(108571)
-local crystal = GetSpellInfo(103640)
 local kohcrom = EJ_GetSectionInfo(4262)
 local fmtStr = "~%s - %s"
 local crystalCount = 0
@@ -19,6 +17,16 @@ local crystalCount = 0
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.engage_trigger = "You seek to halt an avalanche. I will bury you."
+
+	L.stomp_boss, L.stomp_boss_desc = EJ_GetSectionInfo(3879)
+	L.stomp_boss_icon = 108571
+	L.crystal_boss, L.crystal_boss_desc = EJ_GetSectionInfo(3876)
+	L.crystal_boss_icon = 103640
+
+	L.stomp_add, L.stomp_add_desc =  EJ_GetSectionInfo(3879)
+	L.stomp_add_icon = 108571
+	L.crystal_add, L.crystal_add_desc = EJ_GetSectionInfo(3876)
+	L.crystal_add_icon = 103640
 
 	L.crush = "Crush Armor"
 	L.crush_desc = "Tank alert only. Count the stacks of crush armor and show a duration bar."
@@ -38,9 +46,13 @@ L.crush = L.crush.." "..INLINE_TANK_ICON
 
 function mod:GetOptions()
 	return {
-		108571, "crush", 103640, {103851, "FLASHSHAKE"}, 103846, 109017, "berserk", "bosskill",
+		"stomp_boss", "crystal_boss",
+		109017, "stomp_add", "crystal_add",
+		{103851, "FLASHSHAKE"}, "crush", 103846, "berserk", "bosskill",
 	}, {
-		[108571] = "general",
+		stomp_boss = self.displayName,
+		[109017] = kohcrom,
+		[103851] = "general",
 	}
 end
 
@@ -66,8 +78,8 @@ end
 
 function mod:OnEngage(diff)
 	self:Berserk(420) -- confirmed
-	self:Bar(108571, stomp, 11, 108571)
-	self:Bar(103640, crystal, 16, 103640)
+	self:Bar("stomp_boss", L["stomp_boss"], 11, L["stomp_boss_icon"])
+	self:Bar("crystal_boss", L["crystal_boss"], 16, L["crystal_boss_icon"])
 	crystalCount = 0
 end
 
@@ -76,8 +88,9 @@ end
 --
 
 function mod:SummonKohcrom(_, spellId)
-	self:Bar(109017, (fmtStr):format(self.displayName, stomp), 6, spellId)
-	self:Bar(109017, (fmtStr):format(kohcrom, stomp), 12, spellId)
+	self:Bar("stomp_boss", (fmtStr):format(self.displayName, L["stomp_boss"]), 6, L["stomp_boss_icon"])
+	self:Bar("stomp_add", (fmtStr):format(kohcrom, L["stomp_add"]), 12, L["stomp_add_icon"])
+	self:Message(109017, spellName, "Positive", spellId)
 end
 
 -- I know it's ugly to use this, but if we were to start bars at :BlackBlood then we are subject to BlackBlood duration changes
@@ -85,11 +98,11 @@ function mod:Blood(_, unit, _, _, _, spellId)
 	if unit == "boss1" and spellId == 103851 then
 		crystalCount = 0
 		if self:Difficulty() > 2 then
-			self:Bar(108571, (fmtStr):format(self.displayName, stomp), 15, 108571)
-			self:Bar(103640, (fmtStr):format(self.displayName, crystal), 22, 103640)
+			self:Bar("stomp_boss", (fmtStr):format(self.displayName, L["stomp_boss"]), 15, L["stomp_boss_icon"])
+			self:Bar("crystal_boss", (fmtStr):format(self.displayName, L["crystal_boss"]), 22, L["crystal_boss_icon"])
 		else
-			self:Bar(108571, "~"..stomp, 5, 108571)
-			self:Bar(103640, crystal, 29, 103640)
+			self:Bar("stomp_boss", "~"..L["stomp_boss"], 5, L["stomp_boss_icon"])
+			self:Bar("crystal_boss", L["crystal_boss"], 29, L["crystal_boss_icon"])
 		end
 	end
 end
@@ -97,12 +110,12 @@ end
 function mod:Stomp(_, spellId, source, _, spellName)
 	if self:Difficulty() > 2 then
 		if UnitExists("boss2") and source ~= kohcrom then -- Since we trigger kohcrom bar off morchok for more accuracy, we gotta make sure he exists and he isn't caster to avoid bad timers.
-			self:Bar(108571, (fmtStr):format(kohcrom, spellName), (self:Difficulty() == 3) and 6 or 5, 108571) -- 6sec after on 10 man, 5 sec on 25
+			self:Bar("stomp_add", (fmtStr):format(kohcrom, spellName), (self:Difficulty() == 3) and 6 or 5, spellId) -- 6sec after on 10 man, 5 sec on 25
 		else -- It's not kohcrom casting, start morchoks normal bar.
-			self:Bar(108571, (fmtStr):format(source, spellName), 12, spellId)
+			self:Bar("stomp_boss", (fmtStr):format(source, spellName), 12, spellId)
 		end
 	else
-		self:Bar(108571, "~"..spellName, 12, spellId)
+		self:Bar("stomp_boss", "~"..spellName, 12, spellId)
 	end
 end
 
@@ -136,14 +149,14 @@ end
 function mod:ResonatingCrystal(_, spellId, source, _, spellName)
 	if source ~= kohcrom then crystalCount = crystalCount + 1 end -- Only incriment count off morchok casts.
 	if self:Difficulty() > 2 then
-		self:Message(103640, source.." - "..spellName, "Urgent", spellId, "Alarm")
-		self:Bar(103640, source.." - "..(L["explosion"]), 12, spellId)
+		self:Message((source == kohcrom) and "crystal_add" or "crystal_boss", source.." - "..spellName, "Urgent", spellId, "Alarm")
+		self:Bar((source == kohcrom) and "crystal_add" or "crystal_boss", source.." - "..(L["explosion"]), 12, spellId)
 		if UnitExists("boss2") and crystalCount == 2 then -- The CD bar will only start off morchok's 2nd crystal, if kohcrom is already summoned. Explosion bar will be CD for kohcrom's 3rd so redundant to have both.
-			self:Bar(103640, (fmtStr):format(kohcrom, crystal), (self:Difficulty() == 3) and 6 or 5, 103640) -- Same as stomp, 6/5
+			self:Bar("crystal_add", (fmtStr):format(kohcrom, spellName), (self:Difficulty() == 3) and 6 or 5, spellId) -- Same as stomp, 6/5
 		end
 	else
-		self:Message(103640, spellName, "Urgent", spellId, "Alarm")
-		self:Bar(103640, L["explosion"], 12, spellId)
+		self:Message("crystal_boss", spellName, "Urgent", spellId, "Alarm")
+		self:Bar("crystal_boss", L["explosion"], 12, spellId)
 	end
 end
 
