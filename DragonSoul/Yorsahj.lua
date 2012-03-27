d65a2cfcec00a46c1acc5a124ff3b7d6d06e762b
funkydude <funkydude@users.noreply.github.com>
2012-03-27 16:58:56 +0000

    Yorsahj: add a warning for corruption and a bar for the aoe acid pulse.

--- DragonSoul/Yorsahj.lua
+++ DragonSoul/Yorsahj.lua
@@ -30,6 +30,9 @@ if L then
 	L.blobs_bar = "Next blob spawn"
 	L.blobs_desc = "Blobs moving towards the boss"
 	L.blobs_icon = "achievement_doublerainbow"
+
+	L.acid, L.acid_desc = EJ_GetSectionInfo(4320)
+	L.acid_icon = "spell_nature_corrosivebreath"
 end
 L = mod:GetLocale()
 L.bolt = L.bolt.." "..INLINE_TANK_ICON
@@ -54,7 +57,7 @@ local colorCombinations = {
 
 function mod:GetOptions()
 	return {
-		"blobs", "bolt", "proximity", "berserk", "bosskill"
+		"blobs", "bolt", "acid", "ej:4321", "proximity", "berserk", "bosskill"
 	}
 end
 
@@ -62,15 +65,19 @@ function mod:OnBossEnable()
 	self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED", "Blobs")
 	self:Log("SPELL_AURA_APPLIED", "AcidicApplied", 104898)
 	self:Log("SPELL_AURA_REMOVED", "AcidicRemoved", 104898)
+	self:Log("SPELL_CAST_SUCCESS", "DeepCorruption", 105171)
 	self:Log("SPELL_AURA_APPLIED", "Bolt", 108383, 108384, 108385, 104849, 105416, 109549, 109550, 109551)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "Bolt", 108383, 108384, 108385, 104849, 105416, 109549, 109550, 109551)
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+
+	self:Log("SPELL_DAMAGE", "AcidPulse", 108350, 108351, 108352, 105573)
+	self:Log("SPELL_MISSED", "AcidPulse", 108350, 108351, 108352, 105573)
 
 	self:Death("Win", 55312)
 end
 
 function mod:OnEngage()
-	self:Berserk(600) --complete guess
+	self:Berserk(600)
 	self:Bar("blobs", L["blobs_bar"], 21, L["blobs_icon"])
 end
 
@@ -108,6 +115,21 @@ end
 function mod:AcidicRemoved()
 	if not self:LFR() then
 		self:CloseProximity()
+	end
+end
+
+function mod:DeepCorruption(_, spellId)
+	self:LocalMessage("ej:4321", GetSpellInfo(23401), "Personal", spellId, "Alert") -- Corrupted Healing
+end
+
+do
+	local prev = 0
+	function mod:AcidPulse()
+		if GetTime() - prev > 1.5 then
+			prev = GetTime()
+			-- Time when it's actually going to hit you
+			self:Bar("ej:4320", L["acid"], 8, L["acid_icon"])
+		end
 	end
 end
 
