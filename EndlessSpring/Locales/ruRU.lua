cc0b1f6e7f8e281e62e4d967ef3a78222d2fd5f1
Wetxius <wetxius@gmail.com>
2012-09-14 15:00:13 +0000

    ruRU update.

--- EndlessSpring/Locales/ruRU.lua
+++ EndlessSpring/Locales/ruRU.lua
@@ -7,16 +7,24 @@ end
 
 L = BigWigs:NewBossLocale("Tsulong", "ruRU")
 if L then
-
+	L.phases = "Фазы"
+	L.phases_desc = "Предупреждать о смене фаз."
 end
 
 L = BigWigs:NewBossLocale("Lei Shi", "ruRU")
 if L then
+	L.engage_trigger = "Wh-what are you doing here?! G-go away!"	-- Needs review
+	L.hp_to_go = "%d%% to go"	-- Needs review
+	L.end_hide = "Спрятаться"
 
+	L.special = "След. спец. способность"
+	L.special_desc = "Предупредать о следующей особой способности"
 end
 
 L = BigWigs:NewBossLocale("Sha of Fear", "ruRU")
 if L then
+	L.fading_soon = "%s fading soon"	-- Needs review
 
+	L.swing = "Замах"
+	L.swing_desc = "Считает количество замахов перед Взбучкой."
 end
-
--- Mogushan/Locales/ruRU.lua
+++ Mogushan/Locales/ruRU.lua
@@ -2,30 +2,48 @@ if not GetNumGroupMembers then return end
 local L = BigWigs:NewBossLocale("The Stone Guard", "ruRU")
 if not L then return end
 if L then
+	L.petrifications = "Окаменение"
+	L.petrifications_desc = "Предупреждать, когда боссы начинают окаменение."
 
+	L.overload = "Перенасыщение"
+	L.overload_desc = "Предупреждать о всех типах перенасыщения."
 end
 
 L = BigWigs:NewBossLocale("Feng the Accursed", "ruRU")
 if L then
+	L.phases = "Фазы"
+	L.phases_desc = "Предупреждать о смене фаз."
 
+	L.phase_flame_trigger = "Oh exalted one! Through me you shall melt flesh from bone!"	-- Needs review
+	L.phase_lightning_trigger = "Oh great spirit! Grant me the power of the earth!"			-- Needs review
+	L.phase_arcane_trigger =  "Oh sage of the ages! Instill to me your arcane wisdom!"		-- Needs review
+	L.phase_shadow_trigger = "Great soul of champions past! Bear to me your shield!"		-- Needs review
+
+	L.phase_flame = "Фаза огня!"
+	L.phase_lightning = "Фаза молний!"
+	L.phase_arcane = "Фаза тайной магии!"
+	L.phase_shadow = "Фаза тьмы!"
 end
 
 L = BigWigs:NewBossLocale("Gara'jal the Spiritbinder", "ruRU")
 if L then
-
+	L.frenzy = "Бешенство скоро!"
 end
 
 L = BigWigs:NewBossLocale("The Spirit Kings", "ruRU")
 if L then
-
+	L.shield_removed = "Щит убран!"
 end
 
 L = BigWigs:NewBossLocale("Elegon", "ruRU")
 if L then
-
+	L.floor_despawn = "Исчезновение пола"
 end
 
 L = BigWigs:NewBossLocale("Will of the Emperor", "ruRU")
 if L then
-
+	L.rage_trigger = "The Emperor's Rage echoes through the hills."				-- Needs review
+	L.strength_trigger = "The Emperor's Strength appears in the alcoves!"		-- Needs review
+	L.courage_trigger = "The Emperor's Courage appears in the alcoves!"			-- Needs review
+	L.bosses_trigger = "Two titanic constructs appear in the large alcoves!"	-- Needs review
 end
