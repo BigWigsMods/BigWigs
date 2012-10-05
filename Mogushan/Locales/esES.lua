791234f52333fd531d0c5bc84b6454d204a5545c
jongt23 <jongt23@gmail.com>
2012-10-05 00:22:36 +0000

    esES update (Feng)

--- Mogushan/Locales/esES.lua
+++ Mogushan/Locales/esES.lua
@@ -6,7 +6,18 @@ end
 
 L = BigWigs:NewBossLocale("Feng the Accursed", "esES")
 if L then
+	L.phases = "Fases"
+	L.phases_desc = "Aviso para cambios de fase"
 
+	L.phase_flame_trigger = "¡Oh, exaltado! ¡Soy tu herramienta para desgarrar la carne de los huesos!"
+	L.phase_lightning_trigger = "¡Oh, gran espíritu! ¡Otórgame el poder de la tierra!"
+	L.phase_arcane_trigger =  "¡Oh, sabio eterno! ¡Transmíteme tu sapiencia Arcana!"
+	L.phase_shadow_trigger = "Great soul of champions past! Bear to me your shield!"
+
+	L.phase_flame = "¡Fase de llamas!"
+	L.phase_lightning = "¡Fase de rayos!"
+	L.phase_arcane = "¡Fase arcana!"
+	L.phase_shadow = "¡Fase de sombras!"
 end
 
 L = BigWigs:NewBossLocale("Gara'jal the Spiritbinder", "esES")
