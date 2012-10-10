ee633f914fcad10fada4d3904c8d29c07e937763
funkydude <funkydude@users.noreply.github.com>
2012-10-10 03:10:23 +0000

    deDE update

--- Mogushan/Locales/deDE.lua
+++ Mogushan/Locales/deDE.lua
@@ -16,12 +16,12 @@ if L then
 	L.phase_lightning_trigger = "Oh, großer Geist! Gewährt mir die Macht der Erde!"
 	L.phase_flame_trigger = "Oh, Erhabener! Durch mich sollt Ihr das Fleisch von den Knochen schmelzen!"
 	L.phase_arcane_trigger =  "Oh, Weiser der Zeitalter! Vertraut mir Euer arkanes Wissen an!"
-	L.phase_shadow_trigger = "Große Seele vergangener Helden! Reicht mir euer Schild!"		-- ATTENTION! Guessed translation since I did not see the hardmode with the german client. Most probably wrong! Review!
+	L.phase_shadow_trigger = "Große Seele vergangener Helden! Reicht mir euer Schild!" -- ATTENTION! Guessed translation since I did not see the hardmode with the german client. Most probably wrong! Review!
 
 	L.phase_lightning = "Blitzphase!"
 	L.phase_flame = "Feuerphase!"
 	L.phase_arcane = "Arkanphase!"
-	L.phase_shadow = "Schattenphase!"
+	L.phase_shadow = "(Heroisch) Schattenphase!"
 end
 
 L = BigWigs:NewBossLocale("Gara'jal the Spiritbinder", "deDE")
@@ -36,7 +36,9 @@ end
 
 L = BigWigs:NewBossLocale("Elegon", "deDE")
 if L then
+	L.last_phase = "Letzte Phase"
 	L.floor_despawn = "Boden verschwindet"
+	L.overcharged_total_annihilation = "Du hast (%d) %s, werde deinen Debuff los!"
 end
 
 L = BigWigs:NewBossLocale("Will of the Emperor", "deDE")
@@ -45,5 +47,7 @@ if L then
 	L.strength_trigger = "Die Stärke des Kaisers erscheint in den Erkern!"
 	L.courage_trigger = "Der Mut des Kaisers erscheint in den Erkern!"
 	L.bosses_trigger = "In den riesigen Erkern erscheinen zwei Titanenkonstrukte!"
+
+	L.arc_desc = "|cFFFF0000Diese Warnung wird nur für den Boss angezeigt, welchen du anvisierst.|r " .. (select(2, EJ_GetSectionInfo(5673)))
 end
 
