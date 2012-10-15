eb389b94f03da03dcae89facf9f86c8d9a4c4ee9
funkydude <funkydude@users.noreply.github.com>
2012-10-15 23:54:11 +0000

    remove commented out utility functions

--- Loader.lua
+++ Loader.lua
@@ -101,33 +101,6 @@ local enableZones = {} -- contains the zones in which BigWigs will enable
 -- Utility
 --
 
---[[
-function GetMapID(name)
-	for i=1,1500 do
-		local fetchedName = GetMapNameByID(i)
-		if fetchedName and (fetchedName):find(name) then print(fetchedName, i) end
-	end
-end
-function GetBossID(name)
-	for i=1,1500 do
-		local fetchedName = EJ_GetEncounterInfo(i)
-		if fetchedName and (fetchedName):find(name) then print(fetchedName, i) end
-	end
-end
-function GetSectionID(name)
-	for i=1,10000 do
-		local fetchedName = EJ_GetSectionInfo(i)
-		if fetchedName and (fetchedName):find(name) then print(fetchedName, i) end
-	end
-end
-function PrintNewBossIDs() 
-	for i=200,1500 do
-		local n,_,id = EJ_GetEncounterInfo(i)
-		if n then print(n,id) end
-	end
-end
-]]
-
 local printFormat = "|cffffff00%s|r"
 local function sysprint(msg) print(printFormat:format(msg)) end
 
