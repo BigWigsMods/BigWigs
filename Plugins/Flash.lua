e46e45597b6bd9f32c42fe241dbaaef83b060805
funkydude <funkydude@users.noreply.github.com>
2012-10-31 18:02:13 +0000

    Flash: clean this up

--- Plugins/Flash.lua
+++ Plugins/Flash.lua
@@ -9,17 +9,12 @@ if not mod then return end
 -- Locals
 --
 
--- Frames
+local flashFrame = nil
+
+local originalPoints = nil
 local flasher = nil
 local shaker = nil
 
--- Shake properties
-local shaking = nil
-local SHAKE_DURATION = 0.8
-local SHAKE_X = 10
-local SHAKE_Y = 10
-
--- color module
 local colors = nil
 
 -------------------------------------------------------------------------------
@@ -29,40 +24,53 @@ local colors = nil
 function mod:OnPluginEnable()
 	colors = BigWigs:GetPlugin("Colors")
 	self:RegisterMessage("BigWigs_FlashShake")
-end
 
--------------------------------------------------------------------------------
--- Shaking
---
+	if not flashFrame then
+		flashFrame = CreateFrame("Frame", nil, UIParent)
+		flashFrame:SetFrameStrata("BACKGROUND")
+		flashFrame:SetBackdrop({bgFile = "Interface\\Tooltips\\UI-Tooltip-Background"})
+		flashFrame:SetAllPoints(UIParent)
+		flashFrame:SetAlpha(0)
+		flashFrame:Hide()
 
-local originalPoints = nil
-local function startShake()
-	if not shaking then
-		-- store old worldframe positions, we need them all, people have frame modifiers for it etc.
-		if not originalPoints then
-			originalPoints = {}
-			for i = 1, WorldFrame:GetNumPoints() do
-				originalPoints[i] = {WorldFrame:GetPoint(i)}
+		flasher = flashFrame:CreateAnimationGroup()
+		flasher:SetScript("OnLoop", function(frame)
+			frame.count = frame.count + 1
+			if frame.count > 3 then
+				frame:Stop()
+				flashFrame:Hide()
+				frame.count = nil
 			end
-		end
-		shaking = SHAKE_DURATION -- don't think we want to make this a setting.
-		shaker:Show()
-	end
-end
+		end)
+		flasher:SetLooping("BOUNCE")
+		local fade = flasher:CreateAnimation("Alpha")
+		fade:SetDuration(0.2)
+		fade:SetChange(1)
 
-local function shakeOnUpdate(frame, elapsed)
-	shaking = shaking - elapsed
-	local x, y = 0, 0 -- Resets to original position if we're supposed to stop.
-	if shaking <= 0 then -- stop shaking
-		shaking = nil
-		shaker:Hide()
-	else
-		x = math.random(-SHAKE_X,SHAKE_X)
-		y = math.random(-SHAKE_Y,SHAKE_Y)
-	end
-	WorldFrame:ClearAllPoints()
-	for i, v in next, originalPoints do
-		WorldFrame:SetPoint(v[1], v[2], v[3], v[4] + x, v[5] + y)
+		shaker = flashFrame:CreateAnimationGroup()
+		shaker:SetScript("OnLoop", function(frame)
+			local x, y = 0, 0
+			frame.count = frame.count + 1
+			if frame.count > 15 then
+				frame:Stop()
+				flashFrame:Hide()
+				frame.count = nil
+			else
+				x, y = math.random(-10,10), math.random(-10,10)
+			end
+			WorldFrame:ClearAllPoints()
+			for i, v in next, originalPoints do
+				WorldFrame:SetPoint(v[1], v[2], v[3], v[4] + x, v[5] + y)
+			end
+		end)
+		shaker:SetLooping("REPEAT")
+		local timer = shaker:CreateAnimation()
+		timer:SetDuration(0.05)
+
+		originalPoints = {}
+		for i = 1, WorldFrame:GetNumPoints() do
+			originalPoints[i] = {WorldFrame:GetPoint(i)}
+		end
 	end
 end
 
@@ -73,43 +81,14 @@ end
 function mod:BigWigs_FlashShake(event, module, key)
 	if BigWigs.db.profile.flash then
 		local r, g, b = colors:GetColor("flashshake", module, key)
-		if not flasher then --frame creation
-			local flashFrame = CreateFrame("Frame", nil, UIParent)
-			flashFrame:SetFrameStrata("BACKGROUND")
-			flashFrame:SetBackdrop({bgFile = "Interface\\Tooltips\\UI-Tooltip-Background"})
-			flashFrame:SetAllPoints(UIParent)
-			flashFrame:SetBackdropColor(r, g, b, 0.55)
-			flashFrame:SetAlpha(0)
-			flashFrame:Hide()
-
-			flasher = flashFrame:CreateAnimationGroup()
-			flasher:SetScript("OnLoop", function(frame)
-				frame.count = frame.count + 1
-				if frame.count > 3 then
-					frame:Stop()
-					flashFrame:Hide()
-				end
-			end)
-			flasher:SetScript("OnPlay", function(frame)
-				frame.count = 0
-				flashFrame:SetBackdropColor(r, g, b, 0.55)
-				flashFrame:Show()
-			end)
-			flasher:SetLooping("BOUNCE")
-
-			local fade = flasher:CreateAnimation("Alpha")
-			fade:SetDuration(0.2)
-			fade:SetChange(1)
-		end
+		flashFrame:SetBackdropColor(r, g, b, 0.55)
+		flashFrame:Show()
+		flasher.count = 0
 		flasher:Play()
 	end
 	if not WorldFrame:IsProtected() and BigWigs.db.profile.shake then
-		if not shaker then
-			shaker = CreateFrame("Frame", "BWShaker", UIParent)
-			shaker:Hide()
-			shaker:SetScript("OnUpdate", shakeOnUpdate)
-		end
-		startShake()
+		shaker.count = 0
+		shaker:Play()
 	end
 end
 
