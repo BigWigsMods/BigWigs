bb5008a8c2c59b20227b16ee932f1f0bbc287a04
Kyle Buller <bullerk@gmail.com>
2012-11-02 05:56:46 +0000

    HeartOfFear/Garalon: fix up Furious Swipe timer (expires on damage), add Fury message

--- HeartOfFear/Garalon.lua
+++ HeartOfFear/Garalon.lua
@@ -11,7 +11,7 @@ mod:RegisterEnableMob(62164, 63191) -- Verify id
 -- Locals
 --
 
-local legCounter, mendLegTimerRunning, mendLegCD = 4, false, 30
+local legCounter, mendLegTimerRunning = 4, nil
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -20,8 +20,9 @@ local legCounter, mendLegTimerRunning, mendLegCD = 4, false, 30
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.crush_stun = "Crush stun"
-	L.crush_trigger = "Garalon prepares to"
-	L.crush_trigger1 = "Garalon senses"
+	L.crush_trigger1 = "Garalon prepares to"
+	L.crush_trigger2 = "Garalon senses" --Garalon senses the passage of Pheromones and begins to cast [Crush]!
+	L.crush_trigger3 = "Garalon detects" --Garalon detects Baddie under him and begins to cast [Crush]!
 end
 L = mod:GetLocale()
 
@@ -31,7 +32,7 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		122774, {122835, "ICON"}, 123081, 123495, "ej:6294", 122735,
+		122774, 122735, 122754, {122835, "ICON"}, 123081, 123495, "ej:6294",
 		"berserk", "bosskill",
 	}, {
 		[122774] = "general",
@@ -39,7 +40,7 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
-	self:Emote("Crush", L["crush_trigger"], L["crush_trigger1"])
+	self:Emote("Crush", L["crush_trigger1"], L["crush_trigger2"], L["crush_trigger3"])
 
 	self:Log("SPELL_AURA_APPLIED", "PheromonesApplied", 122835)
 	self:Log("SPELL_AURA_REMOVED", "PheromonesRemoved", 122835)
@@ -47,8 +48,8 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "MendLeg", 123495)
 	self:Log("SPELL_CAST_SUCCESS", "BrokenLeg", 122786)
 	self:Log("SPELL_CAST_START", "FuriousSwipe", 122735)
-
-	-- not sure if a CL.underyou type warning should be added for Pheromone Trail, you aren't really supposed to be near it unless you have Pheromones on
+	self:Log("SPELL_AURA_APPLIED", "Fury", 122754)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "Fury", 122754)
 
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
@@ -56,8 +57,9 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage(diff)
-	legCounter, mendLegTimerRunning = 4, false
+	legCounter, mendLegTimerRunning = 4, nil
 	self:Berserk(420)
+	self:Bar(122735, 122735, 11, 122735) --Furious Swipe
 	if self:Heroic() then
 		self:RegisterEvent("UNIT_HEALTH_FREQUENT")
 	end
@@ -67,10 +69,24 @@ end
 -- Event Handlers
 --
 
-
 function mod:Crush()
-	self:Bar(122774, L["crush_stun"], 4, 122774)
-	self:Message(122774, 122774, "Important", 122774, "Alarm") -- Crush
+	self:Message(122774, CL["soon"]:format(self:SpellName(122774)), "Important", 122774, "Alarm") -- Crush
+	--self:Bar(122774, L["crush_stun"], 4, 122774)
+	self:Bar(122774, CL["cast"]:format(self:SpellName(122774)), 4, 122774) --Crush
+
+	self:Bar(122735, 122735, 9, 122735) --Furious Swipe
+end
+
+do
+	local prev = 0
+	function mod:Fury(_, spellId, _, _, spellName, buffStack)
+		local t = GetTime()
+		if t-prev > 1 then
+			prev = t
+			self:Bar(spellId, spellName, 30, 119622) --Rage like icon (swipe and fury have the same)
+			self:Message(spellId, ("%s (%d)"):format(spellName, buffStack or 1), "Urgent", 119622)
+		end
+	end
 end
 
 function mod:PheromonesApplied(player, spellId, _, _, spellName)
@@ -82,7 +98,6 @@ function mod:PheromonesApplied(player, spellId, _, _, spellName)
 end
 
 function mod:PheromonesRemoved(player, spellId, _, _, spellName)
-	self:PrimaryIcon(spellId)
 	if UnitIsUnit("player", player) then
 		-- Local message with important and alarm for when you loose the debuff, others don't care that you lost it
 		self:LocalMessage(spellId, CL["other"]:format(spellName, player), "Important", spellId, "Alarm")
@@ -90,20 +105,19 @@ function mod:PheromonesRemoved(player, spellId, _, _, spellName)
 end
 
 function mod:Pungency(player, spellId, _, _, spellName, buffStack)
-	if (buffStack > 3 and buffStack % 2 == 0 and self:Heroic()) or (buffStack > 7 and buffStack % 2 == 0) then
+	if buffStack > (self:Heroic() and 3 or 7) and buffStack % 2 == 0 then
 		self:TargetMessage(spellId, ("%s (%d)"):format(spellName, buffStack), player, "Attention", spellId)
 	end
 end
-
 
 function mod:MendLeg(_, spellId, _, _, spellName)
 	legCounter = legCounter + 1
 	if legCounter < 4 then -- don't start a timer if it has all 4 legs
 		self:Message(spellId, spellName, "Urgent", spellId)
-		self:Bar(spellId, "~"..spellName, mendLegCD, spellId)
+		self:Bar(spellId, "~"..spellName, 30, spellId)
 	else
 		-- all legs grew back, no need to start a bar, :BrokenLeg will start it
-		mendLegTimerRunning = false
+		mendLegTimerRunning = nil
 	end
 end
 
@@ -111,13 +125,32 @@ function mod:BrokenLeg()
 	legCounter = legCounter - 1
 	-- this is just a way to start the bar after 1st legs death
 	if not mendLegTimerRunning then
-		self:Bar(123495, "~"..self:SpellName(123495), mendLegCD, 123495) -- Mend Leg, need logs of longer attempts to verify if it is CD or not
-		mendLegTimerRunning = true
+		self:Bar(123495, "~"..self:SpellName(123495), 30, 123495)
+		mendLegTimerRunning = nil
 	end
 end
 
-function mod:FuriousSwipe(_, spellId, _, _, spellName)
-	self:Bar(spellId, spellName, 8, spellId)
+do
+	--Furious Swipe's cast time is 2.5ish seconds, with 8s between SPELL_CAST_STARTs
+	local function nextSwipe(spellName)
+		mod:Bar(122735, spellName, 8, 122735)
+	end
+	function mod:FuriousSwipe(_, spellId, _, _, spellName)
+		--delay the bar so it ends when the damage occurs
+		self:ScheduleTimer(nextSwipe, 2.5, spellName)
+	end
+
+	--[[
+	--another idea is we have one bar for the cd and one bar for the cast, but they're both pretty short
+	local function nextSwipe(spellName)
+		self:Bar(122735, spellName, 5.5, 122735)
+	end
+	function mod:FuriousSwipe(_, spellId, _, _, spellName)
+		self:SendMessage("BigWigs_StopBar", self, spellName)
+		self:Bar(122735, CL["cast"]:format(spellName), 2.5, spellId) --use "cast" to help distinguish the two bars
+		self:ScheduleTimer(nextSwipe, 2.5, spellName)
+	end
+	--]]
 end
 
 function mod:UNIT_HEALTH_FREQUENT(_, unitId)
