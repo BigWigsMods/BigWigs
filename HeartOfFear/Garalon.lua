60384ba47c5b159f9af027adf1d069eb734b7c17
tborlee <tborlee@users.noreply.github.com>
2012-11-10 11:53:40 +0000

    HeartOfFear/Garalon: fix typo

--- HeartOfFear/Garalon.lua
+++ HeartOfFear/Garalon.lua
@@ -106,7 +106,7 @@ function mod:PheromonesApplied(player, spellId, _, _, spellName)
 		-- Local message with personal and info for when you gain the debuff, others don't care that you got it
 		self:LocalMessage(spellId, CL["you"]:format(spellName), "Personal", spellId, "Info")
 	elseif self:Healer() then
-		selt:LocalMessage(spellId, spellName, "Attention", spellId, nil, player)
+		self:LocalMessage(spellId, spellName, "Attention", spellId, nil, player)
 	end
 end
 
