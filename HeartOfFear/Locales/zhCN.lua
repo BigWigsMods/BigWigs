746a89a4febcea7af8e0e827ad1006c63d7c68ea
tnt2ray <tnt2ray@gmail.com>
2012-11-14 11:03:09 +0000

    zhCN Update

--- HeartOfFear/Locales/zhCN.lua
+++ HeartOfFear/Locales/zhCN.lua
@@ -1,8 +1,23 @@
 local L = BigWigs:NewBossLocale("Imperial Vizier Zor'lok", "zhCN")
 if not L then return end
 if L then
+	L.engage_yell = "我们不会向黑暗虚空的绝望屈服。如果女皇要我们去死，我们便照做。"
+
+	L.force_message = "AoE Pulse"
+
+	L.attenuation_bar = "Discs... Dance!"
+	L.attenuation_message = "%s Dancing %s"
+	L.echo = "|c001cc986Echo|r"
+	L.zorlok = "|c00ed1ffaZor'lok|r"
+	L.left = "|c00008000<- 左 <-|r"
+	L.right = "|c00FF0000-> 右 ->|r"
+
+	L.platform_emote = "朝他其中一个平台飞去了！" -- Imperial Vizier Zor'lok flies to one of his platforms!
+	L.platform_emote_final = "inhales"-- Imperial Vizier Zor'lok inhales the Pheromones of Zeal!
+	L.platform_message = "换平台"
 
 end
+
 
 L = BigWigs:NewBossLocale("Blade Lord Ta'yak", "zhCN")
 if L then
