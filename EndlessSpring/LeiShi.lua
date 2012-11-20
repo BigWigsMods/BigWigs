93e3f987a3cb93bfd681574bfd8e59ed5e790d5d
Kyle Buller <bullerk@gmail.com>
2012-11-20 09:52:07 +0000

    EndlessSpring/LeiShi: protect soon warning

--- EndlessSpring/LeiShi.lua
+++ EndlessSpring/LeiShi.lua
@@ -12,6 +12,7 @@ mod:RegisterEnableMob(62983)
 --
 
 local hiding = false
+local nextProtectWarning = 85
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -47,16 +48,18 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_REMOVED", "GetAwayRemoved", 123461)
 	self:Log("SPELL_AURA_APPLIED", "Protect", 123250)
 	self:Log("SPELL_CAST_START", "Hide", 123244)
-	self:Yell("Engage", L["engage_trigger"])
 
+	self:Yell("Engage", L["engage_trigger"])
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "EngageCheck") -- use this to detect him coming out of hide
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	self:RegisterEvent("UNIT_HEALTH_FREQUENT")
 
 	self:Death("Win", 62983)
 end
 
 function mod:OnEngage(diff)
 	hiding = false
+	nextProtectWarning = 85
 	self:Bar("special", "~"..L["special"], 33, 123263)
 	self:Berserk(480)
 end
@@ -80,32 +83,40 @@ function mod:Hide(_, spellId, _, _, spellName)
 	self:Message(spellId, spellName, "Attention", spellId)
 end
 
-function mod:GetAwayRemoved()
-	self:UnregisterEvent("UNIT_HEALTH_FREQUENT")
-	self:Bar("special", "~"..L["special"], 30, 123263)
-end
-
 do
 	local getAwayStartHP
 	function mod:GetAwayApplied(_, spellId, _, _, spellName)
 		if UnitHealthMax("boss1") > 0 then
 			getAwayStartHP = UnitHealth("boss1") / UnitHealthMax("boss1") * 100
 			self:Message(spellId, spellName, "Important", spellId, "Alarm")
-			self:RegisterEvent("UNIT_HEALTH_FREQUENT")
 		end
+	end
+	function mod:GetAwayRemoved()
+		getAwayStartHP = nil
+		self:Bar("special", "~"..L["special"], 30, 123263)
 	end
 
 	local prev = 0
 	local lastHpToGo
 	function mod:UNIT_HEALTH_FREQUENT(_, unitId)
 		if unitId == "boss1" then
-			local t = GetTime()
-			if t-prev > 3 then -- warn max once every 3 sec
-				prev = t
-				local hp = UnitHealth(unitId) / UnitHealthMax(unitId) * 100
-				local hpToGo = math.floor(4 - (getAwayStartHP - hp))
-				if lastHpToGo ~= hpToGo and hpToGo > 0 then
-					self:Message(123461, L["hp_to_go"]:format(hpToGo), "Positive", 123461)
+			local hp = UnitHealth(unitId) / UnitHealthMax(unitId) * 100
+			if hp < nextProtectWarning then
+				self:Message(123250, CL["soon"]:format(self:SpellName(123250)), "Positive", 123250) -- Protect
+				nextProtectWarning = hp - 20
+				if nextProtectWarning < 20 then
+					nextProtectWarning = 0
+				end
+			end
+			if getAwayStartHP then
+				local t = GetTime()
+				if t-prev > 3 then -- warn max once every 3 sec
+					prev = t
+					local hpToGo = math.floor(4 - (getAwayStartHP - hp))
+					if lastHpToGo ~= hpToGo and hpToGo > 0 then
+						lastHpToGo = hpToGo
+						self:Message(123461, L["hp_to_go"]:format(hpToGo), "Positive", 123461)
+					end
 				end
 			end
 		end
