c688c1661198891babae033e5882b1eca7e156da
funkydude <funkydude@users.noreply.github.com>
2012-11-29 06:22:36 +0000

    TheSpiritKings: various fixes

--- Mogushan/TheSpiritKings.lua
+++ Mogushan/TheSpiritKings.lua
@@ -98,7 +98,7 @@ function mod:OnEngage()
 		self:Bar(117961, 117961, 40, 117961) -- Impervious Shield
 	end
 	self:Bar(119521, 119521, 10, 119521) -- Annihilate
-	self:Bar(117910, 117910, 26, 117910) -- Flanking Orders
+	self:Bar(117910, 117910, 25, 117910) -- Flanking Orders
 	self:Message("ej:5841", qiang, "Positive", 117920)
 end
 
@@ -106,12 +106,12 @@ end
 -- Event Handlers
 --
 
-local function isBossActiveById(bossId)
+local function isBossActiveById(bossId, bossIdTwo)
 	for i=1, 5 do
 		local unitId = ("boss%d"):format(i)
 		if UnitExists(unitId) then
 			local id = mod:GetCID(UnitGUID(unitId))
-			if id == bossId then
+			if id == bossId or id == bossIdTwo then
 				return true
 			end
 		end
@@ -134,7 +134,7 @@ do
 	function mod:UNIT_POWER_FREQUENT(_, unitId)
 		if not unitId:find("boss", nil, true) then return end
 		local id = self:GetCID(UnitGUID(unitId))
-		if id == 60708 then
+		if id == 60708 or id == 61429 then
 			local power = UnitPower(unitId)
 			if power > 74 and prevPower == 0 then
 				prevPower = 75
@@ -158,8 +158,8 @@ end
 
 function mod:MaddeningShout(_, spellId, _, _, spellName)
 	self:Message(spellId, spellName, "Urgent", spellId, "Alarm")
-	if isBossActiveById(60708) then
-		self:Bar(spellId, "~"..spellName, self:Heroic() and 47 or 48.3, spellId)
+	if isBossActiveById(60708, 61429) then
+		self:Bar(spellId, "~"..spellName, 46.7, spellId)
 	else
 		self:Bar(spellId, "~"..spellName, 76, spellId)
 	end
@@ -167,7 +167,7 @@ end
 
 function mod:ShieldofDarkness(_, spellId, _, _, spellName)
 	self:Message(spellId, spellName, "Important", spellId, "Alert")
-	self:Bar(spellId, "~"..spellName, 42.5, spellId)
+	self:Bar(spellId, spellName, 42, spellId)
 	self:Bar("casting_shields", CL["cast"]:format(spellName), 2, spellId)
 	self:FlashShake(spellId)
 end
@@ -190,12 +190,16 @@ end
 
 function mod:Pillage(_, spellId, _, _, spellName)
 	self:Message(spellId, spellName, "Urgent", spellId, "Alarm")
-	self:Bar(spellId, spellName, 41, spellId)
+	if isBossActiveById(60710, 61427) then
+		self:Bar(spellId, "~"..spellName, 40, spellId)
+	else
+		self:Bar(spellId, spellName, 75.5, spellId)
+	end
 end
 
 function mod:Volley(_, spellId, _, _, spellName)
 	self:Message(spellId, spellName, "Urgent", spellId)
-	self:Bar(spellId, spellName, 37, spellId)
+	self:Bar(spellId, spellName, 41, spellId)
 end
 
 function mod:SleightofHand(_, spellId, _, _, spellName)
@@ -216,8 +220,8 @@ end
 -- qiang
 function mod:FlankingOrders(_, spellId, _, _, spellName)
 	self:Message(spellId, spellName, "Attention", spellId, "Long")
-	if isBossActiveById(60709) then
-		self:Bar(spellId, spellName, self:Heroic() and 46 or 41, spellId)
+	if isBossActiveById(60709, 61423) then
+		self:Bar(spellId, spellName, self:Heroic() and 45.7 or 41, spellId)
 	else
 		self:Bar(spellId, spellName, 75, spellId)
 	end
@@ -225,12 +229,12 @@ end
 
 function mod:Annihilate(_, _, _, _, spellName)
 	self:Message(119521, spellName, "Urgent", 119521, "Alarm")
-	self:Bar(119521, spellName, self:Heroic() and 32 or 39, 119521)
+	self:Bar(119521, spellName, self:Difficulty() == 6 and 32 or 39, 119521)
 end
 
 function mod:ImperviousShield(_, spellId, _, _, spellName)
 	self:Message(spellId, spellName, "Important", spellId, "Alert")
-	self:Bar(spellId, spellName, 42, spellId)
+	self:Bar(spellId, spellName, self:Difficulty() == 5 and 62 or 42, spellId)
 	self:Bar("casting_shields", CL["cast"]:format(spellName), 2, spellId)
 	self:FlashShake(spellId)
 end
@@ -256,12 +260,12 @@ function mod:EngageCheck()
 			elseif (id == 60710 or id == 61427) and not bossActivated[60710] then -- subetai
 				bossActivated[60710] = true
 				if self:Heroic() then
-					self:Bar(118162, 118162, 40, 118162) -- Sleight of Hand
+					self:Bar(118162, 118162, 15, 118162) -- Sleight of Hand
 				end
 				self:OpenProximity(8)
 				self:Bar(118094, 118094, 5, 118094) -- Volley
 				self:Bar(118047, 118047, 26, 118047) -- Pillage
-				self:Bar(118122, 118122, self:Heroic() and 40 or 21, 118122) -- Rain of Arrows
+				self:Bar(118122, 118122, self:Heroic() and 40 or 15, 118122) -- Rain of Arrows
 				self:Message("ej:5846", subetai, "Positive", 118122)
 			elseif (id == 60708 or id == 61429) and not bossActivated[60708] then -- meng
 				bossActivated[60708] = true
@@ -281,23 +285,25 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, unitId, spellName, _, _, spellId)
 				self:StopBar(117961) -- Impervious Shield
 				self:Bar(117910, 117910, 30, 117910) -- Flanking Orders
 			elseif (id == 60701 or id == 61421) then -- zian
-				self:StopBar("~"..self:SpellName(117697)) -- Shield of Darkness
-				if isBossActiveById(60710) then -- don't close if subetai is active
+				self:StopBar(117697) -- Shield of Darkness
+				if not isBossActiveById(60710, 61427) then -- don't close if subetai is active
 					self:CloseProximity()
 				end
 			elseif (id == 60710 or id == 61427) then -- subetai
 				self:StopBar(118162) -- Sleight of Hand
 				self:StopBar(118094) -- Volley
-				self:StopBar(118122) -- Rain of Arrows
+				self:StopBar(self:Heroic() and 118122 or ("~"..self:SpellName(118122))) -- Rain of Arrows
+				self:StopBar("~"..self:SpellName(118047)) -- Pillage
 				self:Bar(118047, 118047, 30, 118047) -- Pillage
-				if isBossActiveById(60701) then -- don't close if zian is active
+				if not isBossActiveById(60701, 61421) then -- don't close if zian is active
 					self:CloseProximity()
 				end
 			elseif (id == 60708 or id == 61429) then -- meng
 				self:Bar(117708, "~"..self:SpellName(117708), 30, 117708) -- Maddening Shout
 			end
 		elseif spellId == 118121 then -- Rain of Arrows for Pinned Down
-			self:Bar(118122, 118122, self:Heroic() and 41 or 50, 118122) -- Rain of Arrows
+			local hc = self:Heroic()
+			self:Bar(118122, hc and 118122 or ("~"..self:SpellName(118122)), hc and 41 or 51, 118122) -- Rain of Arrows, exact on heroic, 50-60 on norm
 		end
 	end
 end
