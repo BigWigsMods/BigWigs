8e8fb44be4c8a9dba17bf7064a7fe3419c37a62e
jongt23 <jongt23@gmail.com>
2012-12-02 22:13:47 +0000

    esES update (convert UTF-8)

--- HeartOfFear/Locales/esES.lua
+++ HeartOfFear/Locales/esES.lua
@@ -6,24 +6,24 @@ if L then
 	L.force_message = "Pulso AoE"
 
 	L.attenuation = EJ_GetSectionInfo(6426) .. " (Discos)"
-	L.attenuation_bar = "°Discos... baila!"
+	L.attenuation_bar = "¬°Discos... baila!"
 	L.attenuation_message = "%s Baile %s"
 	L.echo = "|c001cc986Echo|r"
 	L.zorlok = "|c00ed1ffaZor'lok|r"
 	L.left = "|c00008000<- Izquierda <-|r"
 	L.right = "|c00FF0000-> Derecha ->|r"
 
-	L.platform_emote = "°El visir imperial Zor'lok vuela hacia una de las plataformas!" -- Imperial Vizier Zor'lok flies to one of his platforms!
-	L.platform_emote_final = "°El visir imperial Zor'lok inhala las feromonas de celo!"-- Imperial Vizier Zor'lok inhales the Pheromones of Zeal!
+	L.platform_emote = "¬°El visir imperial Zor'lok vuela hacia una de las plataformas!" -- Imperial Vizier Zor'lok flies to one of his platforms!
+	L.platform_emote_final = "¬°El visir imperial Zor'lok inhala las feromonas de celo!"-- Imperial Vizier Zor'lok inhales the Pheromones of Zeal!
 	L.platform_message = "Cambio de plataforma"
 end
 
 L = BigWigs:NewBossLocale("Blade Lord Ta'yak", "esES") or BigWigs:NewBossLocale("Blade Lord Ta'yak", "esMX")
 if L then
-	L.engage_yell = "°En guardia, invasores! Yo, Ta'yak, SeÒor de las Espadas, serÈ vuestro contrincante."
+	L.engage_yell = "¬°En guardia, invasores! Yo, Ta'yak, Se√±or de las Espadas, ser√© vuestro contrincante."
 
-	L.unseenstrike_inc = "°Golpe inminente!"
-	L.unseenstrike_soon = "°Golpe en ~5-10 seg!"
+	L.unseenstrike_inc = "¬°Golpe inminente!"
+	L.unseenstrike_soon = "¬°Golpe en ~5-10 seg!"
 
 	L.assault_message = "Asalto"
 	L.side_swap = "Cambia de lado"
@@ -31,48 +31,48 @@ end
 
 L = BigWigs:NewBossLocale("Garalon", "esES") or BigWigs:NewBossLocale("Garalon", "esMX")
 if L then
-	L.removed = "°%s eliminado!"
+	L.removed = "¬°%s eliminado!"
 end
 
 L = BigWigs:NewBossLocale("Wind Lord Mel'jarak", "esES") or BigWigs:NewBossLocale("Wind Lord Mel'jarak", "esMX")
 if L then
-	L.spear_removed = "°Tu Lanza empaladora fue eliminada!"
-	L.residue_removed = "°%s eliminado!"
+	L.spear_removed = "¬°Tu Lanza empaladora fue eliminada!"
+	L.residue_removed = "¬°%s eliminado!"
 end
 
 L = BigWigs:NewBossLocale("Amber-Shaper Un'sok", "esES") or BigWigs:NewBossLocale("Amber-Shaper Un'sok", "esMX")
 if L then
-	L.explosion_by_other = "DeflagraciÛn de ·mbar en otros"
-	L.explosion_by_other_desc = "Alerta de CD para DeflagraciÛn de ·mbar lanzado por Monstruosidad de ·mbar o tu foco si lo tienes marcado."
+	L.explosion_by_other = "Deflagraci√≥n de √°mbar en otros"
+	L.explosion_by_other_desc = "Alerta de CD para Deflagraci√≥n de √°mbar lanzado por Monstruosidad de √°mbar o tu foco si lo tienes marcado."
 
-	L.explosion_casting_by_other = "DeflagraciÛn de ·mbar lanzado por otros"
-	L.explosion_casting_by_other_desc = "Avisos para lanzamientos de DeflagraciÛn de ·mbar empezados por Monstruosidad de ·mbar o tu foco si lo tienes marcado. °Es altamente recomendable enfatizar esto!"
+	L.explosion_casting_by_other = "Deflagraci√≥n de √°mbar lanzado por otros"
+	L.explosion_casting_by_other_desc = "Avisos para lanzamientos de Deflagraci√≥n de √°mbar empezados por Monstruosidad de √°mbar o tu foco si lo tienes marcado. ¬°Es altamente recomendable enfatizar esto!"
 
-	L.explosion_by_you = "DeflagraciÛn de ·mbar en ti"
-	L.explosion_by_you_desc = "Alerta de CD para tu DeflagraciÛn de ·mbar."
+	L.explosion_by_you = "Deflagraci√≥n de √°mbar en ti"
+	L.explosion_by_you_desc = "Alerta de CD para tu Deflagraci√≥n de √°mbar."
 
-	L.explosion_casting_by_you = "DeflagraciÛn de ·mbar lanzado por ti"
-	L.explosion_casting_by_you_desc = "Avisos para DeflagraciÛn de ·mbar empezados por ti. °Es altamente recomendable enfatizar esto!"
+	L.explosion_casting_by_you = "Deflagraci√≥n de √°mbar lanzado por ti"
+	L.explosion_casting_by_you_desc = "Avisos para Deflagraci√≥n de √°mbar empezados por ti. ¬°Es altamente recomendable enfatizar esto!"
 
 	L.willpower = "Voluntad"
-	L.willpower_message = "°Voluntad al %d!"
+	L.willpower_message = "¬°Voluntad al %d!"
 
-	L.break_free_message = "°Vida al %d%%!"
+	L.break_free_message = "¬°Vida al %d%%!"
 	L.fling_message = "Getting tossed!"
-	L.parasite = "Par·sito"
+	L.parasite = "Par√°sito"
 
-	L.boss_is_casting = "°EL JEFE est· casteando!"
-	L.you_are_casting = "°EST¡S casteando!"
+	L.boss_is_casting = "¬°EL JEFE est√° casteando!"
+	L.you_are_casting = "¬°EST√ÅS casteando!"
 end
 
 L = BigWigs:NewBossLocale("Grand Empress Shek'zeer", "esES") or BigWigs:NewBossLocale("Grand Empress Shek'zeer", "esMX")
 if L then
-	L.engage_trigger = "°Muerte a todo aquel que desafÌe a mi imperio!"
+	L.engage_trigger = "¬°Muerte a todo aquel que desaf√≠e a mi imperio!"
 	L.phases = "Fases"
 	L.phases_desc = "Aviso para cambios de fase."
 
 	L.eyes = "Ojos de la Emperatriz"
-	L.eyes_desc = "Cuenta los stacks y muestra una barra de duraciÛn para Ojos de la Emperatriz."
+	L.eyes_desc = "Cuenta los stacks y muestra una barra de duraci√≥n para Ojos de la Emperatriz."
 	L.eyes_message = "%2$dx Ojos en %1$s"
 
 	L.fumes_bar = "Tu beneficio de Vapores"
