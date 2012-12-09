4e00c263387bcb0b67fdae0f775aeed4efecc8d7
Kyle Buller <bullerk@gmail.com>
2012-12-09 19:55:56 +0000

    with more ()

--- HeartOfFear/Garalon.lua
+++ HeartOfFear/Garalon.lua
@@ -61,7 +61,7 @@ end
 function mod:OnEngage(diff)
 	legCounter, mendLegTimerRunning = 4, nil
 	crushCounter = 0
-	self:Berserk(self:LFR and 720 or 420)
+	self:Berserk(self:LFR() and 720 or 420)
 	self:Bar(122735, 122735, 11, 122735) --Furious Swipe
 	if self:Heroic() then
 		self:RegisterEvent("UNIT_HEALTH_FREQUENT")
