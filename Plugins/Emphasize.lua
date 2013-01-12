b9bb5a8be430bf506d5a122038ccd27a28f31331
Caleb <calebzor@gmail.com>
2013-01-12 15:54:52 +0000

    move all remaining options to configuration window

--- Plugins/Emphasize.lua
+++ Plugins/Emphasize.lua
@@ -15,75 +15,17 @@ local L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Plugins")
 local temporaryEmphasizes = {}
 local emphasizeFlag = nil
 
-local colorize = nil
-do
-	local r, g, b
-	colorize = setmetatable({}, { __index =
-		function(self, key)
-			if not r then r, g, b = GameFontNormal:GetTextColor() end
-			self[key] = "|cff" .. ("%02x%02x%02x"):format(r * 255, g * 255, b * 255) .. key .. "|r"
-			return self[key]
-		end
-	})
-end
-
 -------------------------------------------------------------------------------
 -- Options
 --
 
 plugin.defaultDB = {
 	upper = true,
-	size = true,
 	countdown = true,
 	font = "Friz Quadrata TT",
 	outline = "THICKOUTLINE",
 	fontSize = 32,
 	fontColor = { r = 1, g = 0, b = 0 },
-}
-
-plugin.subPanelOptions = {
-	key = "Big Wigs: Super Emphasize",
-	name = L["Super Emphasize"],
-	options = {
-		name = L["Super Emphasize"],
-		type = "group",
-		get = function(info) return plugin.db.profile[info[#info]] end,
-		set = function(info, value) plugin.db.profile[info[#info]] = value end,
-		args = {
-			heading = {
-				type = "description",
-				name = L.superEmphasizeDesc,
-				order = 10,
-				width = "full",
-				fontSize = "medium",
-			},
-			upper = {
-				type = "toggle",
-				name = colorize[L["UPPERCASE"]],
-				desc = L["Uppercases all messages related to a super emphasized option."],
-				order = 11,
-				width = "full",
-				descStyle = "inline",
-			},
-			size = {
-				type = "toggle",
-				name = colorize[L["Double size"]],
-				desc = L["Doubles the size of super emphasized bars and messages."],
-				order = 12,
-				width = "full",
-				descStyle = "inline",
-				hidden = true,
-			},
-			countdown = {
-				type = "toggle",
-				name = colorize[L["Countdown"]],
-				desc = L["If a related timer is longer than 5 seconds, a vocal and visual countdown will be added for the last 5 seconds. Imagine someone counting down \"5... 4... 3... 2... 1... COUNTDOWN!\" and big numbers in the middle of your screen."],
-				order = 13,
-				width = "full",
-				descStyle = "inline",
-			},
-		},
-	},
 }
 
 do
@@ -100,13 +42,18 @@ do
 					end
 				end,
 				args = {
+					heading = {
+						type = "description",
+						name = L.superEmphasizeDesc,
+						order = 0.1,
+						width = "full",
+						--fontSize = "medium",
+					},
 					font = {
 						type = "select",
 						name = L["Font"],
 						order = 1,
 						values = media:List("font"),
-						--width = "half",
-						--itemControl = "DDI-Font",
 						get = function()
 							for i, v in next, media:List("font") do
 								if v == plugin.db.profile.font then return i end
@@ -129,7 +76,6 @@ do
 							OUTLINE = L["Thin"],
 							THICKOUTLINE = L["Thick"],
 						},
-						--width = "half",
 						get = function()
 							return plugin.db.profile.outline or "NONE"
 						end,
@@ -169,6 +115,18 @@ do
 						name = L["Monochrome"],
 						desc = L["Toggles the monochrome flag on all messages, removing any smoothing of the font edges."],
 						order = 5,
+					},
+					upper = {
+						type = "toggle",
+						name = L["UPPERCASE"],
+						desc = L["Uppercases all messages related to a super emphasized option."],
+						order = 6,
+					},
+					countdown = {
+						type = "toggle",
+						name = L["Countdown"],
+						desc = L["If a related timer is longer than 5 seconds, a vocal and visual countdown will be added for the last 5 seconds. Imagine someone counting down \"5... 4... 3... 2... 1... COUNTDOWN!\" and big numbers in the middle of your screen."],
+						order = 7,
 					},
 				},
 			}
