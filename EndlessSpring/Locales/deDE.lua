617e87520440d5bd9710a8be298f326bbdfd5c4c
taker21 <taker21@users.noreply.wowace.com>
2013-01-30 01:24:37 +0000

    Populated deDE Localization.

--- EndlessSpring/Locales/deDE.lua
+++ EndlessSpring/Locales/deDE.lua
@@ -2,21 +2,48 @@
 local L = BigWigs:NewBossLocale("Protectors of the Endless", "deDE")
 if not L then return end
 if L then
-
+	L.under = "%s unter %s!"
+	L.heal = "%s Heilung"
 end
 
 L = BigWigs:NewBossLocale("Tsulong", "deDE")
 if L then
+	L.engage_yell = "Ihr gehört hier nicht hin! Die Wasser müssen beschützt werden... Ich werde euch verteiben oder abschlachten!"
+	L.kill_yell = "Ich danke euch Fremde, ihr habt mich befreit."
 
+	L.phases = "Phasen"
+	L.phases_desc = "Warnung für Phasenwechsel"
+	L.sunbeam_spawn = "Neuer Sonnestrahl!"
 end
 
 L = BigWigs:NewBossLocale("Lei Shi", "deDE")
 if L then
+	L.hp_to_go = "Noch %d%%"
 
+	L.special = "Nächste Spezialfähigkeit"
+	L.special_desc = "Warnung für nächste Spezialfähigkeit"
 end
 
 L = BigWigs:NewBossLocale("Sha of Fear", "deDE")
 if L then
+.fading_soon = "%s läuft bald ab"
 
+	L.swing = "Hauen"
+	L.swing_desc = "Zählt den den Schwüngen vorangehenden Thrash."
+
+	L.throw = "Wurf!"
+	L.ball_dropped = "Ball abgeworfen!"
+	L.ball_you = "Du hast den Ball!"
+	L.ball = "Ball"
+
+	L.cooldown_reset = "Deine Abklingzeiten wurden zurückgesetzt!"
+
+	L.ability_cd = "Fähigkeiten-Abklingzeit"
+	L.ability_cd_desc = "Versuche der Reihenfolge der Fähigkeiten nach dem Auftauchen zu erraten"
+	
+	L.huddle_or_spout = "Kauern oder Fontäne"
+	L.huddle_or_strike = "Kauern oder Stoß"
+	L.strike_or_spout = "Stoß oder Fontäne"
+	L.huddle_or_spout_or_strike = "Kauern oder Fontäne der Stoß"
 end
 
