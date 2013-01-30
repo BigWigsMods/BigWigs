b2d54e49623a9cea130fdc37fa2acc926ef58c7b
valdriethien <valdriethien@users.noreply.wowace.com>
2013-01-30 15:27:20 +0000

    Initial deDE Localization for Throne

--- ThroneOfThunder/Locales/deDE.lua
+++ ThroneOfThunder/Locales/deDE.lua
@@ -1,12 +1,21 @@
 local L = BigWigs:NewBossLocale("Jin'rokh the Breaker", "deDE")
 if not L then return end
 if L then
-
+	L.storm_duration = "Gewittersturm Dauer"
+	L.storm_duration_desc = "Eine seperate Leiste für die Dauer von Gewittersturm"
+	L.in_water = "Du stehst im Wasser!"
 end
 
 L = BigWigs:NewBossLocale("Council of Elders", "deDE")
 if L then
+	L.full_power = "Volle Energie"
 
+	L.assault_message = "Angriff"
+
+	L.loa_kills = "Loa kills: %s"
+	L.loa_spirit = "Loa Geist"
+	L.loa_spirits = "Loa Geister"
+	L.loa_spirits_desc = "Warnung für beide Arten von Loa Geister adds"
 end
 
 L = BigWigs:NewBossLocale("Tortos", "deDE")
@@ -40,7 +49,10 @@ if L then
 end
 L = BigWigs:NewBossLocale("Iron Qon", "deDE")
 if L then
+	L.molten_energy = "Geschmolzene Macht"
 
+	L.overload_casting = "Wirkt Geschmolzene Überladung"
+	L.overload_casting_desc = "Warnung für das Wirken von Geschmolzene Überladung"
 end
 L = BigWigs:NewBossLocale("Twin Consorts", "deDE")
 if L then
@@ -48,7 +60,15 @@ if L then
 end
 L = BigWigs:NewBossLocale("Lei Shen", "deDE")
 if L then
-
+	L.conduit_abilities = "Leiter-Fähigkeiten"
+	L.conduit_abilities_desc = "Ungefähre Cooldown-Leisten für die spezifischen Fähigkeiten der Leiter"
+	
+	L.conduit_ability_meassage = "Nächste Leiter-Fähigkeit"
+	L.intermission = "Unterbrechung"
+	L.overchargerd_message = "Betäubender AoE Puls"
+	L.static_shock_message = "Aufteilender AoE Schaden"
+	L.diffusion_add_message = "Wetterleucht-Elementare"
+	L.diffusion_chain_message = "Wetterleucht-Elementare bald - VERTEILEN!"
 end
 L = BigWigs:NewBossLocale("Ra-den", "deDE")
 if L then
