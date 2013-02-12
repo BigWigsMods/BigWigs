ac0e2baca439fcb2d5b5741d6ac2b992d90d8496
funkydude <funkydude@users.noreply.github.com>
2013-02-12 18:13:38 +0000

    Duration: allow the module to print to get some feedback on reliability of engage/wipe callbacks.

--- Plugins/Duration.lua
+++ Plugins/Duration.lua
@@ -19,7 +19,6 @@ local activeDurations = nil
 -- Initialization
 --
 
-local dev = true
 function plugin:OnPluginEnable()
 	if not BigWigsDurationDB then
 		BigWigsDurationDB = {}
@@ -29,11 +28,9 @@ function plugin:OnPluginEnable()
 		activeDurations = {}
 	end
 
-	if not dev then
-		self:RegisterMessage("BigWigs_OnBossEngage")
-		self:RegisterMessage("BigWigs_OnBossWin")
-		self:RegisterMessage("BigWigs_OnBossReboot")
-	end
+	self:RegisterMessage("BigWigs_OnBossEngage")
+	self:RegisterMessage("BigWigs_OnBossWin")
+	self:RegisterMessage("BigWigs_OnBossReboot")
 end
 
 -------------------------------------------------------------------------------
@@ -42,21 +39,20 @@ end
 
 function plugin:BigWigs_OnBossEngage(event, module)
 	if module.encounterId then
-		BigWigs:Print("Boss", module.moduleName, "engaged.")
 		activeDurations[module.encounterId] = GetTime()
 	end
 end
 
 function plugin:BigWigs_OnBossWin(event, module)
 	if module.encounterId and activeDurations[module.encounterId] then
-		BigWigs:Print("Boss", module.moduleName, "defeated after", SecondsToTime(GetTime()-lastTime))
+		print("Big Wigs Experimental: Boss", module.moduleName, "defeated after", SecondsToTime(GetTime()-activeDurations[module.encounterId]))
 		activeDurations[module.encounterId] = nil
 	end
 end
 
 function plugin:BigWigs_OnBossReboot(event, module)
 	if module.encounterId and activeDurations[module.encounterId] then
-		BigWigs:Print("Boss", module.moduleName, "wiped after", SecondsToTime(GetTime()-lastTime))
+		print("Big Wigs Experimental: Boss", module.moduleName, "wiped after", SecondsToTime(GetTime()-activeDurations[module.encounterId]))
 		activeDurations[module.encounterId] = nil
 	end
 end
