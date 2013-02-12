552f60666f8b51eeff21f908f5b0019e415281b0
simoflorence <simoflorence@users.noreply.github.com>
2013-02-12 17:04:04 +0000

    Populating itIT Locale for Throne of Thunders. Still miss some bosses and some triggers needs finetuning, but it's still a first go.

--- ThroneOfThunder/Locales/itIT.lua
+++ ThroneOfThunder/Locales/itIT.lua
@@ -1,37 +1,81 @@
 local L = BigWigs:NewBossLocale("Jin'rokh the Breaker", "itIT")
 if not L then return end
 if L then
+	L.storm_duration = "Durata Tempesta di Fulmini"
+	L.storm_duration_desc = "Una barra di avviso separata per la durata del lancio di Tempesta di Fulmini"
 
+	L.in_water = "Sei nell'acqua!"
 end
 
 L = BigWigs:NewBossLocale("Horridon", "itIT")
 if L then
+	L.orb_message = "Globo del Controllo a terra!"
 
+	L.chain_lightning_warning = "Il tuo focus sta lanciando Catena di Fulmini!"
+	L.chain_lightning_bar = "Focus: Catena di Fulmini"
+
+	L.fireball_warning = "Il tuo focus sta lanciando Palla di Fuoco!"
+	L.fireball_bar = "Focus: Palla di Fuoco"
+
+	L.venom_bolt_volley_desc = "|cFFFF0000ATTENZIONE: Viene mostrato solo il timer del tuo bersaglio 'focus' perché tutti i lanciatori di Raffica hanno tempi di recupero diversi.|r "
+	L.venom_bolt_volley_warning = "Il tuo Focus sta lanciando Raffica Venefica!"
+	L.venom_bolt_volley_bar = "Focus: Raffica Venefica"
+	L.puncture_message = "Perforazione Tripla"
 end
 
 L = BigWigs:NewBossLocale("Council of Elders", "itIT")
 if L then
+	L.full_power = "Pieno Potere"
 
+	L.assault_message = "Assalto"
+
+	L.loa_kills = "Spirito Loa ucciso: %s"
+	L.loa_spirit = "Spirito Loa"
+	L.loa_spirits = "Spiriti Loa"
+	L.loa_spirits_desc = "Avviso per entrambi i tipi di Spiriti Loa"
 end
 
 L = BigWigs:NewBossLocale("Tortos", "itIT")
 if L then
-
+	L.kick = "Calcio"
+	L.kick_desc = "Tieni il conto di quante tartarighe possono essere prese a calci"
+	L.kickable_turtles = "Tartarughe disponibili per essere calciate: %d"
 end
 
 L = BigWigs:NewBossLocale("Megaera", "itIT")
 if L then
-
+	L.breaths = "Soffi"
+	L.breaths_desc = "Avvisi relativi ad ogni tipo di soffio possibile."
+	L.rampage_over = "Furia Terminata!"
 end
 
 L = BigWigs:NewBossLocale("Ji-Kun", "itIT")
 if L then
-
+	L.flight_over = "Termine del Volo"
+	L.young_egg_hatching = "Schiusura Uova Giovani"
+	L.lower_hatch_trigger = "Le uova in uno dei nidi più bassi iniziano a schiudersi!"
+	L.upper_hatch_trigger = "Le uova in uno dei nidi più alti iniziano a schiudersi!"
+	L.upper_nest = "Nido |c00008000Superiore|r"
+	L.lower_nest = "Nido |c00FF0000Inferiore|r"
+	L.food_call_trigger = "Hatchling calls for food!"
+	L.nest = "Nidi"
+	L.nest_desc = "Avvisi rlativi ai nidi. |c00FF0000Deselezionalo per spengere gli avvisi, se non sei designato a gestire i nidi!|r"
 end
 
 L = BigWigs:NewBossLocale("Durumu the Forgotten", "itIT")
 if L then
+	L.rays_spawn = "Apparizione raggi"
+	L.ray_controller = "Controllore del Raggio"
+	L.ray_controller_desc = "Annuncia la direzione del raggio del controllore per il raggio blu e rosso."
 
+	L.red_ray_controller = "Sei il controllore del raggio |c000000FFBlu|r"
+	L.blue_ray_controller = "Sei il controllore del raggio |c00FF0000Rosso|r"
+	L.red_spawn_trigger = "The Infrared Light reveals a Crimson Fog!"
+	L.blue_spawn_trigger = "The Blue Rays reveal an Azure Eye!"
+	L.red_add = "Add |c00FF0000Rosso|r"
+	L.blue_add = "Add |c000000FFBlu|r"
+	L.clockwise = "Senso Orario"
+	L.counter_clockwise = "Senso Antiorario"
 end
 
 L = BigWigs:NewBossLocale("Primordius", "itIT")
@@ -41,11 +85,15 @@ end
 
 L = BigWigs:NewBossLocale("Dark Animus", "itIT")
 if L then
-
+	L.engage_trigger = "Il globo esplode!"
+	L.slam_message = "Colpo"
 end
 L = BigWigs:NewBossLocale("Iron Qon", "itIT")
 if L then
+	L.molten_energy = "Energia Fusa"
 
+	L.overload_casting = "Lancio Sovraccarico Fuso"
+	L.overload_casting_desc = "Avvisa quando viene lanciato Sovraccarico Fuso"
 end
 L = BigWigs:NewBossLocale("Twin Consorts", "itIT")
 if L then
@@ -53,7 +101,14 @@ if L then
 end
 L = BigWigs:NewBossLocale("Lei Shen", "itIT")
 if L then
-
+	L.conduit_abilities = "Abilità dei Condotti"
+	L.conduit_abilities_desc = "Barre di recupero approssimative per le abilità specifiche dei condotti"
+	L.conduit_ability_meassage = "Abilità successiva del condotto"
+	L.intermission = "Intermezzo"
+	L.overchargerd_message = "Pulsazione ad Effetto Sotrdente"
+	L.static_shock_message = "Danno ad Effetto da Suddividere"
+	L.diffusion_add_message = "Add di Diffusione"
+	L.diffusion_chain_message = "Add di Diffusione tra poco - ALLARGARSI!!!"
 end
 L = BigWigs:NewBossLocale("Ra-den", "itIT")
 if L then
