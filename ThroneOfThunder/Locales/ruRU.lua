a56a9687095223f9531480238dc35a27c1bb8f88
Wetxius <wetxius@gmail.com>
2013-02-13 08:35:34 +0000

    ruRU update.

--- ThroneOfThunder/Locales/ruRU.lua
+++ ThroneOfThunder/Locales/ruRU.lua
@@ -1,72 +1,72 @@
 local L = BigWigs:NewBossLocale("Jin'rokh the Breaker", "ruRU")
 if not L then return end
 if L then
-	L.storm_duration = "Длительность Грозы"
-	L.storm_duration_desc = "Отдельная полоса предупреждения для длительности чтения Грозы."
+	L.storm_duration = "Р”Р»РёС‚РµР»СЊРЅРѕСЃС‚СЊ Р“СЂРѕР·С‹"
+	L.storm_duration_desc = "РћС‚РґРµР»СЊРЅР°СЏ РїРѕР»РѕСЃР° РїСЂРµРґСѓРїСЂРµР¶РґРµРЅРёСЏ РґР»СЏ РґР»РёС‚РµР»СЊРЅРѕСЃС‚Рё С‡С‚РµРЅРёСЏ Р“СЂРѕР·С‹."
 
-	L.in_water = "Вы в воде!"
+	L.in_water = "Р’С‹ РІ РІРѕРґРµ!"
 end
 
 L = BigWigs:NewBossLocale("Horridon", "ruRU")
 if L then
-	L.orb_message = "Сфера контроля упала!"
+	L.orb_message = "РЎС„РµСЂР° РєРѕРЅС‚СЂРѕР»СЏ СѓРїР°Р»Р°!"
 
-	L.chain_lightning_warning = "Ваш фокус читает Цепную молнию!"
-	L.chain_lightning_bar = "Фокус: Цепная молния"
+	L.chain_lightning_warning = "Р’Р°С€ С„РѕРєСѓСЃ С‡РёС‚Р°РµС‚ Р¦РµРїРЅСѓСЋ РјРѕР»РЅРёСЋ!"
+	L.chain_lightning_bar = "Р¤РѕРєСѓСЃ: Р¦РµРїРЅР°СЏ РјРѕР»РЅРёСЏ"
 
-	L.fireball_warning = "Ваш фокус читает Огненный шар!"
-	L.fireball_bar = "Фокус: Огненный шар"
+	L.fireball_warning = "Р’Р°С€ С„РѕРєСѓСЃ С‡РёС‚Р°РµС‚ РћРіРЅРµРЅРЅС‹Р№ С€Р°СЂ!"
+	L.fireball_bar = "Р¤РѕРєСѓСЃ: РћРіРЅРµРЅРЅС‹Р№ С€Р°СЂ"
 
-	L.venom_bolt_volley_desc = "|cFFFF0000WARNING: Таймер показывается только для вашего 'фокуса', так как заклинатели имеют разное время восстановление способностей.|r "..select(2, EJ_GetSectionInfo(7112))
-	L.venom_bolt_volley_warning = "Ваш фокус читает Залп!"
-	L.venom_bolt_volley_bar = "Фокус: Залп"
+	L.venom_bolt_volley_desc = "|cFFFF0000WARNING: РўР°Р№РјРµСЂ РїРѕРєР°Р·С‹РІР°РµС‚СЃСЏ С‚РѕР»СЊРєРѕ РґР»СЏ РІР°С€РµРіРѕ 'С„РѕРєСѓСЃР°', С‚Р°Рє РєР°Рє Р·Р°РєР»РёРЅР°С‚РµР»Рё РёРјРµСЋС‚ СЂР°Р·РЅРѕРµ РІСЂРµРјСЏ РІРѕСЃСЃС‚Р°РЅРѕРІР»РµРЅРёРµ СЃРїРѕСЃРѕР±РЅРѕСЃС‚РµР№.|r "..select(2, EJ_GetSectionInfo(7112))
+	L.venom_bolt_volley_warning = "Р’Р°С€ С„РѕРєСѓСЃ С‡РёС‚Р°РµС‚ Р—Р°Р»Рї!"
+	L.venom_bolt_volley_bar = "Р¤РѕРєСѓСЃ: Р—Р°Р»Рї"
 
-	L.puncture_message = "Прокол"
+	L.puncture_message = "РџСЂРѕРєРѕР»"
 end
 
 L = BigWigs:NewBossLocale("Council of Elders", "ruRU")
 if L then
-	L.full_power = "Полная энергия"
+	L.full_power = "РџРѕР»РЅР°СЏ СЌРЅРµСЂРіРёСЏ"
 
-	L.assault_message = "Выпад"
+	L.assault_message = "Р’С‹РїР°Рґ"
 
-	L.loa_kills = "Убито Лоа: %s"
-	L.loa_spirit = "Дух Лоа"
-	L.loa_spirits = "Духи Лоа"
-	L.loa_spirits_desc = "Предупреждать о двух типах духов Лоа."
+	L.loa_kills = "РЈР±РёС‚Рѕ Р›РѕР°: %s"
+	L.loa_spirit = "Р”СѓС… Р›РѕР°"
+	L.loa_spirits = "Р”СѓС…Рё Р›РѕР°"
+	L.loa_spirits_desc = "РџСЂРµРґСѓРїСЂРµР¶РґР°С‚СЊ Рѕ РґРІСѓС… С‚РёРїР°С… РґСѓС…РѕРІ Р›РѕР°."
 end
 
 L = BigWigs:NewBossLocale("Tortos", "ruRU")
 if L then
-	L.kick = "Пинок"
-	L.kick_desc = "Отслеживает, сколько черепах можно пнуть."
-	L.kickable_turtles = "Можно пнуть черепах: %d"
+	L.kick = "РџРёРЅРѕРє"
+	L.kick_desc = "РћС‚СЃР»РµР¶РёРІР°РµС‚, СЃРєРѕР»СЊРєРѕ С‡РµСЂРµРїР°С… РјРѕР¶РЅРѕ РїРЅСѓС‚СЊ."
+	L.kickable_turtles = "РњРѕР¶РЅРѕ РїРЅСѓС‚СЊ С‡РµСЂРµРїР°С…: %d"
 end
 
 L = BigWigs:NewBossLocale("Megaera", "ruRU")
 if L then
-	L.breaths = "Дыхание"
-	L.breaths_desc = "Предупреждения, связанные со всеми типами дыхания."
-	L.rampage_over = "Неистовство окончено!"
+	L.breaths = "Р”С‹С…Р°РЅРёРµ"
+	L.breaths_desc = "РџСЂРµРґСѓРїСЂРµР¶РґРµРЅРёСЏ, СЃРІСЏР·Р°РЅРЅС‹Рµ СЃРѕ РІСЃРµРјРё С‚РёРїР°РјРё РґС‹С…Р°РЅРёСЏ."
+	L.rampage_over = "РќРµРёСЃС‚РѕРІСЃС‚РІРѕ РѕРєРѕРЅС‡РµРЅРѕ!"
 end
 
 L = BigWigs:NewBossLocale("Ji-Kun", "ruRU")
 if L then
-	L.flight_over = "Облет"
+	L.flight_over = "РћР±Р»РµС‚"
 	-- L.young_egg_hatching = "Young egg hatching"
 	-- L.lower_hatch_trigger = "The eggs in one of the lower nests begin to hatch!"
 	-- L.upper_hatch_trigger = "The eggs in one of the upper nests begin to hatch!"
-	L.upper_nest = "|c00008000Верхние|r гнезда"
-	L.lower_nest = "|c00FF0000Нижние|r гнезда"
+	L.upper_nest = "|c00008000Р’РµСЂС…РЅРёРµ|r РіРЅРµР·РґР°"
+	L.lower_nest = "|c00FF0000РќРёР¶РЅРёРµ|r РіРЅРµР·РґР°"
 	-- L.food_call_trigger = "Hatchling calls for food!"
-	L.nest = "Гнезда"
-	L.nest_desc = "Предупреждения, связанные с гнездами. |c00FF0000Снимите галочку, чтобы отключить предупреждения, если вы не назначены на гнезда!|r"
+	L.nest = "Р“РЅРµР·РґР°"
+	L.nest_desc = "РџСЂРµРґСѓРїСЂРµР¶РґРµРЅРёСЏ, СЃРІСЏР·Р°РЅРЅС‹Рµ СЃ РіРЅРµР·РґР°РјРё. |c00FF0000РЎРЅРёРјРёС‚Рµ РіР°Р»РѕС‡РєСѓ, С‡С‚РѕР±С‹ РѕС‚РєР»СЋС‡РёС‚СЊ РїСЂРµРґСѓРїСЂРµР¶РґРµРЅРёСЏ, РµСЃР»Рё РІС‹ РЅРµ РЅР°Р·РЅР°С‡РµРЅС‹ РЅР° РіРЅРµР·РґР°!|r"
 end
 
 L = BigWigs:NewBossLocale("Durumu the Forgotten", "ruRU")
 if L then
-	L.rays_spawn = "Появляются лучи"
-	L.ray_controller = "Оператор луча"
+	L.rays_spawn = "РџРѕСЏРІР»СЏСЋС‚СЃСЏ Р»СѓС‡Рё"
+	L.ray_controller = "РћРїРµСЂР°С‚РѕСЂ Р»СѓС‡Р°"
 	-- L.ray_controller_desc = "Announce the ray direction controllers for the red and blue rays."
 	-- L.red_ray_controller = "You are the |c000000FFBlue|r ray controller"
 	-- L.blue_ray_controller = "You are the |c00FF0000Red|r ray controller"
@@ -74,8 +74,8 @@ if L then
 	-- L.blue_spawn_trigger = "The Blue Rays reveal an Azure Eye!"
 	-- L.red_add = "|c00FF0000Red|r add"
 	-- L.blue_add = "|c000000FFBlue|r add"
-	L.clockwise = "По часовой стрелке"
-	L.counter_clockwise = "Отсчет часовой стрелки"
+	L.clockwise = "РџРѕ С‡Р°СЃРѕРІРѕР№ СЃС‚СЂРµР»РєРµ"
+	L.counter_clockwise = "РћС‚СЃС‡РµС‚ С‡Р°СЃРѕРІРѕР№ СЃС‚СЂРµР»РєРё"
 end
 
 L = BigWigs:NewBossLocale("Primordius", "ruRU")
@@ -86,14 +86,14 @@ end
 L = BigWigs:NewBossLocale("Dark Animus", "ruRU")
 if L then
 	-- L.engage_trigger = "The orb explodes!"
-	L.slam_message = "Удар"
+	L.slam_message = "РЈРґР°СЂ"
 end
 L = BigWigs:NewBossLocale("Iron Qon", "ruRU")
 if L then
-	L.molten_energy = "Огненная эгергия"
+	L.molten_energy = "РћРіРЅРµРЅРЅР°СЏ СЌРіРµСЂРіРёСЏ"
 
-	L.overload_casting = "Чтение Огненной перегрузка"
-	L.overload_casting_desc = "Предупрждение для чтения Огненной перегрузки"
+	L.overload_casting = "Р§С‚РµРЅРёРµ РћРіРЅРµРЅРЅРѕР№ РїРµСЂРµРіСЂСѓР·РєР°"
+	L.overload_casting_desc = "РџСЂРµРґСѓРїСЂР¶РґРµРЅРёРµ РґР»СЏ С‡С‚РµРЅРёСЏ РћРіРЅРµРЅРЅРѕР№ РїРµСЂРµРіСЂСѓР·РєРё"
 end
 L = BigWigs:NewBossLocale("Twin Consorts", "ruRU")
 if L then
@@ -104,9 +104,9 @@ if L then
 	-- L.conduit_abilities = "Conduit Abilities"
 	-- L.conduit_abilities_desc = "Approximate cooldown bars for the conduit specific abilities"
 	-- L.conduit_ability_meassage = "Next conduit ability"
-	L.intermission = "Перерыв"
-	L.overchargerd_message = "Оглушающий AoE импульс"
-	L.static_shock_message = "Делящийся AoE урон"
+	L.intermission = "РџРµСЂРµСЂС‹РІ"
+	L.overchargerd_message = "РћРіР»СѓС€Р°СЋС‰РёР№ AoE РёРјРїСѓР»СЊСЃ"
+	L.static_shock_message = "Р”РµР»СЏС‰РёР№СЃСЏ AoE СѓСЂРѕРЅ"
 	-- L.diffusion_add_message = "Diffusion adds"
 	-- L.diffusion_chain_message = "Diffusion adds soon - SPREAD!"
 end
