9ce668a9ad3e4eb7f023f65fc8e9c8090e50d26b
jongt23 <jongt23@gmail.com>
2013-03-05 18:14:28 +0000

    esES update

--- ThroneOfThunder/Locales/esES.lua
+++ ThroneOfThunder/Locales/esES.lua
@@ -1,12 +1,36 @@
 local L = BigWigs:NewBossLocale("Jin'rokh the Breaker", "esES")
 if not L then return end
 if L then
+	L.storm_duration = "Tormenta de relámpagos duración"
+	L.storm_duration_desc = "Una barra a parte avisa para la duración del casteo de la Tormenta de relámpagos."
 
+	L.in_water = "¡Estás en el agua!"
 end
 
 L = BigWigs:NewBossLocale("Horridon", "esES")
 if L then
+	L.charge_trigger = "sets his eyes" -- Horridon sets his eyes on PLAYERNAME and stamps his tail!
 
+	L.chain_lightning_message = "¡Tu foco está casteando Cadena de relámpagos!"
+	L.chain_lightning_bar = "Foco: Cadena de relámpagos"
+
+	L.fireball_message = "¡Tu foco está casteando Bola de Fuego!"
+	L.fireball_bar = "Foco: Bola de Fuego"
+
+	L.venom_bolt_volley_message = "¡Tu foco está casteando Salva!"
+	L.venom_bolt_volley_bar = "Foco: Salva"
+
+	L.adds = "Aparecen adds"
+	L.adds_desc = "Avisa cuando los Farraki, los Gurubashi, los Drakkari, los Amani y dios de la guerra Jalak aparecen."
+
+	L.orb_message = "¡Orbe de control suelto!"
+
+	L.focus_only = "|cffff0000Solo alertas de Foco.|r "
+
+	L.door_opened = "¡Puerta abierta!"
+	L.door_bar = "Siguiente Puerta (%d)"
+	L.balcony_adds = "Balcony adds"
+	L.door_trigger = "pour" -- "<160.1 21:33:04> CHAT_MSG_RAID_BOSS_EMOTE#Farraki forces pour from the Farraki Tribal Door!#War-God Jalak#####0#0##0#1107#nil#0#false#false", -- [1]
 end
 
 L = BigWigs:NewBossLocale("Council of Elders", "esES")
@@ -16,7 +40,7 @@ if L then
 	L.priestess_adds_message = "Priestess add"
 
 	L.full_power = "Full power"
-	L.assault_message = "Assault"
+	L.assault_message = "Asalto"
 	L.hp_to_go_power = "HP to go: %d%% - Power: %d"
 end
 
@@ -47,7 +71,8 @@ end
 
 L = BigWigs:NewBossLocale("Dark Animus", "esES")
 if L then
-
+	L.engage_trigger = "The orb explodes!"
+	L.slam_message = "Slam"
 end
 L = BigWigs:NewBossLocale("Iron Qon", "esES")
 if L then
