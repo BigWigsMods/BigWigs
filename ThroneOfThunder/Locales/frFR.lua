08e6a11047a7fa3fa2bdbf37211960ff7d302b54
tborlee <tborlee@users.noreply.github.com>
2013-03-06 19:54:49 +0000

    frFR Update + fix encoding

--- ThroneOfThunder/Locales/frFR.lua
+++ ThroneOfThunder/Locales/frFR.lua
@@ -1,31 +1,31 @@
 local L = BigWigs:NewBossLocale("Jin'rokh the Breaker", "frFR")
 if not L then return end
 if L then
-	L.storm_duration = "Durée de Tempête de foudre"
-	L.storm_duration_desc = "Une barre d'alerte à part pour la durée d'incantation de Tempête de foudre."
+	L.storm_duration = "DurÃ©e de TempÃªte de foudre"
+	L.storm_duration_desc = "Une barre d'alerte Ã  part pour la durÃ©e d'incantation de TempÃªte de foudre."
 
-	L.in_water = "Vous êtes dans l'eau !"
+	L.in_water = "Vous Ãªtes dans l'eauÂ !"
 end
 
 L = BigWigs:NewBossLocale("Horridon", "frFR")
 if L then
-	L.charge_trigger = "pose ses yeux" -- Horridon sets his eyes on PLAYERNAME and stamps his tail! -- à vérifier
+	L.charge_trigger = "pose ses yeux" -- Horridon sets his eyes on PLAYERNAME and stamps his tail! -- Ã  vÃ©rifier
 
-	L.chain_lightning_message = "Votre focalisation est entrain d'incanter Chaîne d'éclairs !"
-	L.chain_lightning_bar = "Focalisation : Chaîne d'éclairs"
+	L.chain_lightning_message = "Votre focalisation est entrain d'incanter ChaÃ®ne d'Ã©clairsÂ !"
+	L.chain_lightning_bar = "FocalisationÂ : ChaÃ®ne d'Ã©clairs"
 
-	L.fireball_message = "Votre focalisation est en train d'incanter Boule de feu !"
-	L.fireball_bar = "Focalisation : Boule de feu"
+	L.fireball_message = "Votre focalisation est en train d'incanter Boule de feuÂ !"
+	L.fireball_bar = "FocalisationÂ : Boule de feu"
 
-	L.venom_bolt_volley_message = "Votre focalisation est en train d'incanter Salve !"
-	L.venom_bolt_volley_bar = "Focalisation : Salve"
+	L.venom_bolt_volley_message = "Votre focalisation est en train d'incanter SalveÂ !"
+	L.venom_bolt_volley_bar = "FocalisationÂ : Salve"
 
 	L.adds = "Apparition des renforts"
-	L.adds_desc = "Prévient quand les Farraki, les Gurubashi, les Drakkari, les Amani et le Dieu-guerrier Jalak apparaissent."
+	L.adds_desc = "PrÃ©vient quand les Farraki, les Gurubashi, les Drakkari, les Amani et le Dieu-guerrier Jalak apparaissent."
 
-	L.orb_message = "Orbe de contrôle lâchée !"
+	L.orb_message = "Orbe de contrÃ´le lÃ¢chÃ©eÂ !"
 
-	L.door_opened = "Porte ouverte !"
+	L.door_opened = "Porte ouverteÂ !"
 	L.door_bar = "Proch. porte (%d)"
 	L.balcony_adds = "Adds du balcon"
 	L.door_trigger = "pour" -- "<160.1 21:33:04> CHAT_MSG_RAID_BOSS_EMOTE#Farraki forces pour from the Farraki Tribal Door!#War-God Jalak#####0#0##0#1107#nil#0#false#false", -- [1]
@@ -33,52 +33,52 @@ end
 
 L = BigWigs:NewBossLocale("Council of Elders", "frFR")
 if L then
-	L.priestess_adds = "Adds de la prêtresse"
-	L.priestess_adds_desc = "Prévient de l'arrivée des différents genres de renforts de la Grande prêtresse Mar'li."
-	L.priestess_adds_message = "Add de la prêtresse"
+	L.priestess_adds = "Adds de la prÃªtresse"
+	L.priestess_adds_desc = "PrÃ©vient de l'arrivÃ©e des diffÃ©rents genres de renforts de la Grande prÃªtresse Mar'li."
+	L.priestess_adds_message = "Add de la prÃªtresse"
 
 	L.full_power = "Puissance maximale"
 	L.assault_message = "Assaut"
-	L.hp_to_go_power = "PV à faire : %d%% - Puissance : %d"
+	L.hp_to_go_power = "PV Ã  faireÂ : %d%% - PuissanceÂ : %d"
 end
 
 L = BigWigs:NewBossLocale("Tortos", "frFR")
 if L then
 	L.kick = "Coup de pied"
-	L.kick_desc = "Effectue un suivi du nombre de tortues qui peuvent être bottées."
-	L.kickable_turtles = "Tortues à botter : %d"
+	L.kick_desc = "Effectue un suivi du nombre de tortues qui peuvent Ãªtre bottÃ©es."
+	L.kickable_turtles = "Tortues Ã  botterÂ : %d"
 
-	L.crystal_shell_removed = "Carapace de cristal enlevé !"
+	L.crystal_shell_removed = "Carapace de cristal enlevÃ©Â !"
 	L.no_crystal_shell = "PAS de Carapace de cristal"
 end
 
 L = BigWigs:NewBossLocale("Megaera", "frFR")
 if L then
 	L.breaths = "Souffles"
-	L.breaths_desc = "Alertes relatives aux différents types de souffles."
+	L.breaths_desc = "Alertes relatives aux diffÃ©rents types de souffles."
 
-	L.arcane_adds = "Wyrms du Néant"
+	L.arcane_adds = "Wyrms du NÃ©ant"
 end
 
 L = BigWigs:NewBossLocale("Ji-Kun", "frFR")
 if L then
-	L.flight_over = "Vol terminé"
-	L.young_egg_hatching = "Éclosion d'un jeune œuf"
-	L.lower_hatch_trigger = "The eggs in one of the lower nests begin to hatch !" -- à traduire
-	L.upper_hatch_trigger = "The eggs in one of the upper nests begin to hatch !" -- à traduire
-	L.upper_nest = "Nid |c00008000supérieur|r"
-	L.lower_nest = "Nid |c00FF0000inférieur|r"
-	L.lower_upper_nest = "Nid |c00FF0000inférieur|r + |c00008000supérieur|r"
+	L.flight_over = "Vol terminÃ©"
+	L.young_egg_hatching = "Ã‰closion d'un jeune Å“uf"
+	L.lower_hatch_trigger = "The eggs in one of the lower nests begin to hatchÂ !" -- Ã  traduire
+	L.upper_hatch_trigger = "The eggs in one of the upper nests begin to hatchÂ !" -- Ã  traduire
+	L.upper_nest = "Nid |c00008000supÃ©rieur|r"
+	L.lower_nest = "Nid |c00FF0000infÃ©rieur|r"
+	L.lower_upper_nest = "Nid |c00FF0000infÃ©rieur|r + |c00008000supÃ©rieur|r"
 	L.nest = "Nids"
-	L.nest_desc = "Alertes relatives aux nids. |c00FF0000Décochez ceci pour désactiver les alertes si vous n'êtes pas assigné à la gestion des nids !|r"
+	L.nest_desc = "Alertes relatives aux nids. |c00FF0000DÃ©cochez ceci pour dÃ©sactiver les alertes si vous n'Ãªtes pas assignÃ© Ã  la gestion des nidsÂ !|r"
 	L.big_add = "Gros add au %s"
 end
 
 L = BigWigs:NewBossLocale("Durumu the Forgotten", "frFR")
 if L then
 	L.rays_spawn = "Apparition des rayons"
-	L.red_spawn_trigger = "The Infrared Light reveals a Crimson Fog !" -- à traduire
-	L.blue_spawn_trigger = "The Blue Rays reveal an Azure Eye !" -- à traduire
+	L.red_spawn_trigger = "The Infrared Light reveals a Crimson FogÂ !" -- Ã  traduire
+	L.blue_spawn_trigger = "The Blue Rays reveal an Azure EyeÂ !" -- Ã  traduire
 	L.red_add = "Add |c00FF0000rouge|r"
 	L.blue_add = "Add |c000000FFbleu|r"
 	L.clockwise = "Sens horaire"
@@ -94,37 +94,37 @@ end
 
 L = BigWigs:NewBossLocale("Dark Animus", "frFR")
 if L then
-	L.engage_trigger = "L'orbe explose !" -- à vérifier
+	L.engage_trigger = "L'orbe exploseÂ !" -- Ã  vÃ©rifier
 	L.slam_message = "Heurt"
 end
 
 L = BigWigs:NewBossLocale("Iron Qon", "frFR")
 if L then
-	L.molten_energy = "Énergie magmatique"
+	L.molten_energy = "Ã‰nergie magmatique"
 
 	L.overload_casting = "Incantation de Surcharge de magma"
-	L.overload_casting_desc = "Prévient quand Surcharge de magma est incanté."
+	L.overload_casting_desc = "PrÃ©vient quand Surcharge de magma est incantÃ©."
 
-	L.arcing_lightning_cleared = "Le raid est libéré de Foudre en arc" -- à traduire
+	L.arcing_lightning_cleared = "Le raid est libÃ©rÃ© de Foudre en arc"
 end
 
 L = BigWigs:NewBossLocale("Twin Consorts", "frFR")
 if L then
-	L.barrage_fired = "Barrage invoqué !"
-	L.last_phase_yell_trigger = "Just this once..." -- à traduire
+	L.barrage_fired = "Barrage invoquÃ©Â !"
+	L.last_phase_yell_trigger = "Just this once..." -- Ã  traduire
 end
 
 L = BigWigs:NewBossLocale("Lei Shen", "frFR")
 if L then
 	L.conduit_abilities = "Techniques du conduit"
-	L.conduit_abilities_desc = "Barres approximatives des temps de recharge des techniques spécifiques au conduit."
+	L.conduit_abilities_desc = "Barres approximatives des temps de recharge des techniques spÃ©cifiques au conduit."
 	L.conduit_ability_meassage = "Proch. tech. du conduit"
 
 	L.intermission = "Entracte"
-	L.overchargerd_message = "Pulsation de zone étourdissante"
-	L.static_shock_message = "Dégâts de zone à partager"
+	L.overchargerd_message = "Pulsation de zone Ã©tourdissante"
+	L.static_shock_message = "DÃ©gÃ¢ts de zone Ã  partager"
 	L.diffusion_add_message = "Foudres diffuses"
-	L.diffusion_chain_message = "Foudres diffuses imminentes - DISPERSEZ-VOUS !"
+	L.diffusion_chain_message = "Foudres diffuses imminentes - DISPERSEZ-VOUSÂ !"
 end
 
 L = BigWigs:NewBossLocale("Ra-den", "frFR")
