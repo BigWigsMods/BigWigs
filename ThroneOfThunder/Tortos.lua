c06a935399262e42834436c67d62ce6ff14806e9
Kyle Buller <bullerk@gmail.com>
2013-03-24 18:10:30 +0000

    ThroneOfThunder/Tortos: aaand don't start the no shell spam if you're not in combat

--- ThroneOfThunder/Tortos.lua
+++ ThroneOfThunder/Tortos.lua
@@ -94,7 +94,7 @@ end
 --
 
 function mod:CrystalShellRemoved(args)
-	if not self:Me(args.destGUID) or self:Tank() then return end
+	if not self:Me(args.destGUID) or self:Tank() or not UnitAffectingCombat("player") then return end
 	self:Flash(args.spellId)
 	self:Message(args.spellId, "Urgent", "Alarm", CL["removed"]:format(args.spellName)) -- I think this should stay Urgent Alarm
 	crystalTimer = self:ScheduleRepeatingTimer(warnCrystalShell, 3)
