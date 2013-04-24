55a63f3246c15182806d7f3d5a0c6d2d005a0f45
Caleb <calebzor@gmail.com>
2013-04-24 20:01:14 +0000

    add phase 2 balls add furthest sensitivity warning add win trigger use the bosses power to improve tank ability bars try to be intelligent with proximity meters count them balls

--- ThroneOfThunder/Raden.lua
+++ ThroneOfThunder/Raden.lua
@@ -1,10 +1,3 @@
---[[
-TODO:
-	fix timers, need longer logs
-	use boss power to update the tank ability bars maybe
-	restrict tank abilities with option key flag once sure that timers are accurate
-	make intelligent proximity warnings
-]]--
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
@@ -18,6 +11,9 @@ mod:RegisterEnableMob(69473)
 --
 
 local animaCounter = 1
+local ballCounter = 1
+--local cooldownCounter = 1
+local lastPower = nil
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -33,6 +29,15 @@ if L then
 	L.balls = "Balls"
 	L.balls_desc = "Anima (red) and Vita (blue) balls, that determine which abilities will Ra-den gain"
 	L.balls_icon = 138321
+	L.corruptedballs = "Corrupted balls"
+	L.corruptedballs_desc = "Corrupted Vita and Anima balls, that either increases damage dealt (Vita) or maximum hp (Anima)"
+	L.corruptedballs_icon = 139071
+	L.unstablevitajumptarget = "Unstable vita jump target"
+	L.unstablevitajumptarget_desc = "Tell you when you are the furthest from a player with Unstable Vita. If you emphasize this, you'll also get a countdown for when Unstable Vita is about to jump FROM you."
+	L.unstablevitajumptarget_icon = 138297
+	L.unstablevitajumptarget_message = "You're furthest from Unstable Vita"
+	L.sensitivityfurthestbad = "Vita Sensitivity + Furthest = |cffff0000BAD|r!"
+	L.kill_trigger = "Wait!" -- Wait! I am... I am not your enemy. You are powerful. More powerful than he was, even.... Perhaps you are right. Perhaps there is still hope.
 end
 L = mod:GetLocale()
 
@@ -42,12 +47,15 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		"worm", 138333, {138288, "FLASH", "ICON"},
-		138339, 138334, {138297, "FLASH", "ICON"}, {138372, "FLASH"},
-		"balls", "berserk", "bosskill",
+		"worm", {138333, "TANK_HEALER"}, {138288, "FLASH", "ICON", "PROXIMITY"},
+		138339, {138334, "TANK_HEALER"}, {138297, "FLASH", "ICON"}, "unstablevitajumptarget", {138372, "FLASH"},
+		"corruptedballs",
+		"balls", "stages", "berserk", "bosskill",
+		-- XXX 139040 fix desc last phase balls
 	}, {
 		["worm"] = L.anima_abilities,
 		[138339] = L.vita_abilities,
+		["corruptedballs"] = CL.phase:format(2),
 		["balls"] = "general",
 	}
 end
@@ -56,25 +64,35 @@ function mod:OnBossEnable()
 	-- Anima abilities
 	self:Log("SPELL_CAST_START", "Worm", 138338)
 	self:Log("SPELL_CAST_SUCCESS", "MurderousStrike", 138333)
-	self:Log("SPELL_AURA_APPLIED", "UnstableAnima", 138288)
+	self:Log("SPELL_AURA_APPLIED", "UnstableAnimaApplied", 138288)
+	self:Log("SPELL_AURA_REMOVED", "UnstableAnimaRemoved", 138288)
 	self:Log("SPELL_DAMAGE", "UnstableAnimaRepeatedDamage", 138295)
+	self:Log("SPELL_AURA_APPLIED", "AnimaSensitivityApplied", 139318)
+	self:Log("SPELL_AURA_APPLIED", "AnimaSensitivityRemoved", 139318)
 	-- Vita abilities
 	self:Log("SPELL_CAST_START", "CracklingStalker", 138339)
 	self:Log("SPELL_CAST_SUCCESS", "FatalStrike", 138334)
-	self:Log("SPELL_AURA_APPLIED", "UnstableVita", 138297, 138308) -- initial cast, jumps
+	self:Log("SPELL_AURA_APPLIED", "UnstableVitaApplied", 138297, 138308) -- initial cast, jumps
+	self:Log("SPELL_AURA_REMOVED", "UnstableVitaRemoved", 138297, 138308) -- initial cast, jumps
 	self:Log("SPELL_AURA_APPLIED", "VitaSensitivity", 138372)
 	-- General
+	self:Yell("Win", L["kill_trigger"])
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "Boss1Succeeded", "boss1")
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:RegisterUnitEvent("UNIT_POWER_FREQUENT", "TankAbilityUpdate", "boss1")
 	self:Log("SPELL_AURA_APPLIED", "Anima", 138331) -- on boss to start/stop timers
 	self:Log("SPELL_AURA_APPLIED", "Vita", 138332)  -- on boss to start/stop timers
 	self:Log("SPELL_CAST_START", "Balls", 138321)
-	self:Death("Win", 69473) -- XXX this is probably not the proper win event
 end
 
 function mod:OnEngage()
-	self:Bar("balls", 2, L["balls"], 138321)
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+	self:Bar("balls", 11, L["balls"], 138321)
 	self:Berserk(600) -- XXX assumed
 	animaCounter = 1
+	ballCounter = 1
+	--cooldownCounter = 1
+	lastPower = nil
 end
 
 --------------------------------------------------------------------------------
@@ -86,6 +104,7 @@ end
 --
 
 function mod:Anima(args)
+	lastPower = "Anima"
 	self:StopBar(138339) -- Crackling Stalker
 	self:StopBar(138334) -- Fatal Strike
 	self:StopBar(138297) -- Unstable Vita
@@ -94,7 +113,19 @@ function mod:Anima(args)
 end
 
 do
-	local prev = 0
+	-- XXX this proximity management might need some more testing ( our tactics ignore this phase )
+	local prev, sensitiveToAnima = 0, {}
+	local function proximityAfterUnstableAnimaDamage()
+		local targets = {}
+		for k, v in pairs(sensitiveToAnima) do
+			if v then
+				targets[#targets+1] = k
+			end
+		end
+		if UnitDebuff("player", mod:SpellName(138288)) then -- Unstable Anima
+			mod:OpenProximity(138288, 8, targets)
+		end
+	end
 	function mod:UnstableAnimaRepeatedDamage(args)
 		local t = GetTime()
 		if t-prev > 1 then
@@ -102,18 +133,39 @@ do
 			self:Message(138288, "Attention", nil, CL["count"]:format(args.spellName, animaCounter))
 			animaCounter = animaCounter + 1
 			self:Bar(138288, 15, CL["count"]:format(args.spellName, animaCounter))
+			self:ScheduleTimer(proximityAfterUnstableAnimaDamage, 0.2)
+		end
+	end
+
+	function mod:AnimaSensitivityApplied(args)
+		if self:Me(args.destGUID) then
+			self:CloseProximity(138288)
+			self:OpenProximity(138288, 8, args.sourceName)
+		end
+		sensitiveToAnima[args.destName] = true
+	end
+	function mod:AnimaSensitivityRemoved(args)
+		sensitiveToAnima[args.destName] = false
+		if self:Me(args.destGUID) then
+			self:CloseProximity(138288)
 		end
 	end
 end
 
-function mod:UnstableAnima(args)
+function mod:UnstableAnimaApplied(args)
 	if self:Me(args.destGUID) then
 		self:Flash(args.spellId)
+	else
+		self:OpenProximity(args.spellId, 8, args.destName, true)
 	end
-	self:SecondaryIcon(args.spellId, args.destName) -- XXX should not conflict, but to be safe for now just use different for this
+	self:PrimaryIcon(args.spellId, args.destName)
 	self:TargetMessage(args.spellId, args.destName, "Attention")
 	animaCounter = animaCounter + 1
 	self:Bar(args.spellId, 15, CL["count"]:format(args.spellName, animaCounter))
+end
+
+function mod:UnstableAnimaRemoved(args)
+	self:CloseProximity(args.spellId)
 end
 
 function mod:Worm(args)
@@ -130,6 +182,7 @@ end
 --
 
 function mod:Vita(args)
+	lastPower = "Vita"
 	self:StopBar(CL["count"]:format(self:SpellName(138288), animaCounter)) -- Unstable Anima
 	self:StopBar(L["worm"]) -- Worm
 	self:StopBar(138333) -- Murderous Strike
@@ -149,13 +202,83 @@ function mod:VitaSensitivity(args)
 	end
 end
 
-function mod:UnstableVita(args)
-	if self:Me(args.destGUID) then
-		self:Flash(args.spellId)
+do
+	local SetMapToCurrentZone = BigWigsLoader.SetMapToCurrentZone
+	local function getDistance(unit1, unit2)
+		local tx, ty = GetPlayerMapPosition(unit1)
+		local px, py = GetPlayerMapPosition(unit2)
+
+		local dx, dy = (tx - px), (ty - py)
+		local distance = (dx * dx + dy * dy) ^ 0.5
+
+		return distance
 	end
-	self:TargetMessage(args.spellId, args.destName, "Personal", "Info") -- XXX might be too spammy? leave it in for now people can just tick "Only on me"
-	self:Bar(args.spellId, 5)
-	self:PrimaryIcon(args.spellId, args.destName)
+
+	local timer, last, player = nil, nil, nil
+	local function warnJumpTarget()
+		if not player then
+			mod:CancelTimer(timer)
+			timer = nil
+			return
+		end
+
+		SetMapToCurrentZone()
+		local furthest, highestDistance = nil, 0
+		for i=1, GetNumGroupMembers() do
+			local unit = ("raid%d"):format(i)
+			if UnitAffectingCombat(unit) and not UnitIsUnit(unit, player) then
+				local distance = getDistance(unit, player)
+				if distance > highestDistance then
+					highestDistance = distance
+					furthest = unit
+				end
+			end
+		end
+
+		if furthest and furthest ~= last then
+			mod:SecondaryIcon("unstablevitajumptarget", furthest)
+			if UnitIsUnit(furthest, "player") then
+
+				if UnitDebuff("player", mod:SpellName(138372)) then -- Vita Sensitivity
+					mod:Flash(138372) -- Vita Sensitivity
+					mod:Message("unstablevitajumptarget", "Personal", "Long", L["sensitivityfurthestbad"], 138372)
+				else
+					mod:Message("unstablevitajumptarget", "Personal", "Info", L["unstablevitajumptarget_message"], L.unstablevitajumptarget_icon)
+				end
+			end
+			last = furthest
+		end
+	end
+
+	function mod:UnstableVitaApplied(args)
+		local target = self:NewTargetList()
+		if self:Me(args.destGUID) then
+			self:Flash(138297)
+			self:Bar("unstablevitajumptarget", 5, CL["you"]:format(args.spellName), args.spellId)
+		else
+			target[1] = args.destName
+			self:TargetBar(138297, 5, target[1])
+		end
+		self:TargetMessage(138297, args.destName, "Personal", "Info")
+		self:PrimaryIcon(138297, args.destName)
+		if args.spellId == 138297 then
+			if args.destName == "Marilina" and UnitName("player", "Calebh") then
+				self:TargetMessage(138372, args.destName, "Personal", "Info", "Move out now because:")
+			end
+		end
+
+		last = nil
+		player = args.destName
+		if not timer and self.db.profile.unstablevitajumptarget > 0 then -- pretty wasteful to do the scanning if the option isn't on
+			timer = self:ScheduleRepeatingTimer(warnJumpTarget, 0.5)
+		end
+	end
+	function mod:UnstableVitaRemoved(args)
+		self:StopBar(args.spellId)
+		self:SecondaryIcon("unstablevitajumptarget")
+		self:PrimaryIcon(args.spellId)
+		player = nil
+	end
 end
 
 function mod:FatalStrike(args)
@@ -167,8 +290,61 @@ end
 -- General
 --
 
+function mod:Boss1Succeeded(unitId, spellName, _, _, spellId)
+	if spellId == 139040 then
+		self:CDBar(spellId, 16)
+		self:Message("corruptedballs", "Important", "Alarm", L["corruptedballs"], 139071)
+	end
+end
+
+function mod:UNIT_HEALTH_FREQUENT(unitId)
+	local hp = UnitHealth(unitId) / UnitHealthMax(unitId) * 100
+	if hp < 43 then
+		self:Message("stages", "Neutral", "Info", CL["soon"]:format(CL["phase"]:format(2)), false)
+		self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", unitId)
+	end
+end
+
+function mod:TankAbilityUpdate(unit)
+	local power = UnitPower(unit)
+	if UnitBuff(unit, self:SpellName(138331)) then -- Anima - Murderous Strike
+		if power == 30 then
+			self:Bar(138333, 25)
+		elseif power == 60 then
+			self:Bar(138333, 15)
+		elseif power == 90 then
+			self:Bar(138333, 5)
+		end
+	elseif UnitBuff(unit, self:SpellName(138332)) then -- Vita - Fatal Strike
+		if power == 20 then
+			self:Bar(138334, 8)
+		elseif power == 40 then
+			self:Bar(138334, 6)
+		elseif power == 50 then
+			self:Bar(138334, 5)
+		-- don't know if there is much point starting a bar shorter than 5 sec
+		end
+	end
+end
+
 function mod:Balls(args)
-	self:Message("balls", "Important", "Warning", L["balls"], args.spellId)
-	self:Bar("balls", 33, L["balls"], args.spellId)
+	self:Message("balls", "Urgent", "Warning", CL["count"]:format(L["balls"], ballCounter), args.spellId)
+	ballCounter = ballCounter + 1
+	self:Bar("balls", 33, CL["count"]:format(L["balls"], ballCounter), args.spellId)
+	-- XXX Maybe work this into a custom_off_suggestedkillorder for at least 25 man
+	--if ballCounter == 5 or ballCounter == 8 or ballCounter == 11 or ballCounter == 14 or ballCounter == 17 then
+	--	self:Message("balls", "Urgent", nil, CL["count"]:format("Balls touching",cooldownCounter), args.spellId)
+	--	cooldownCounter = cooldownCounter + 1
+	--	ballCounter = ballCounter + 1
+	--	self:Bar("balls", 33, CL["count"]:format("Kill: |cffff0000RED|r ball", ballCounter), args.spellId)
+	--elseif ballCounter == 4 or ballCounter == 7 or ballCounter == 10 or ballCounter == 13 or ballCounter == 16 then
+	--	self:Message("balls", "Urgent", "Warning", CL["count"]:format("Kill: |cffff0000RED|r ball", ballCounter), args.spellId)
+	--	ballCounter = ballCounter + 1
+	--	self:Bar("balls", 33, CL["count"]:format("Balls touching", cooldownCounter), args.spellId)
+	--else
+	--	self:Message("balls", "Urgent", "Warning", CL["count"]:format("Kill: |cffff0000RED|r ball", ballCounter), args.spellId)
+	--	ballCounter = ballCounter + 1
+	--	self:Bar("balls", 33, CL["count"]:format("Kill: |cffff0000RED|r ball", ballCounter), args.spellId)
+	--end
 end
 
