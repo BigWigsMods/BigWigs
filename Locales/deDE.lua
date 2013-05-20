1104912af6208719a906742f9fd7ac777deb7d81
Dynaletik <Dynaletik@users.noreply.wowace.com>
2013-05-20 15:01:57 +0000

    - deDE update

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -14,7 +14,7 @@ L.stages_desc = "Funktionen für bestimmte Phasen von Bossbegegnungen wie Abstan
 
 L.already_registered = "|cffff0000WARNUNG:|r |cff00ff00%s|r (|cffffff00%s|r) existiert bereits als Modul in Big Wigs, aber irgend etwas versucht es erneut anzumelden. Dies bedeutet normalerweise, dass du zwei Kopien des Moduls aufgrund eines Fehlers beim Aktualisieren in deinem Addon-Ordner hast. Es wird empfohlen, jegliche Big Wigs Ordner zu löschen und dann von Grund auf neu zu installieren."
 
---L.movieBlocked = "You've seen this movie before, skipping it."
+L.movieBlocked = "Da Du diese Zwischensequenz bereits gesehen hast, wird sie übersprungen."
 L.roleUpdate = "Deine Rolle wurde aktualisiert."
 
 -- Loader / Options.lua
