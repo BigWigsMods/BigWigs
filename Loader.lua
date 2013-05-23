08028aeb12c8ca996bcece0e9eb7f34ca30fb490
funkydude <funkydude@users.noreply.github.com>
2013-05-23 23:09:28 +0000

    bump DBM faker

--- Loader.lua
+++ Loader.lua
@@ -464,8 +464,8 @@ end
 --
 
 do
-	local DBMdotRevision = "9413"
-	local DBMdotDisplayVersion = "5.2.5"
+	local DBMdotRevision = "9592"
+	local DBMdotDisplayVersion = "5.3.0"
 	function loader:DBM_AddonMessage(channel, sender, prefix, revision, _, displayVersion)
 		if prefix == "H" and (BigWigs and BigWigs.db.profile.fakeDBMVersion or self.isFakingDBM) then
 			SendAddonMessage("D4", "V\t"..DBMdotRevision.."\t"..DBMdotRevision.."\t"..DBMdotDisplayVersion.."\t"..GetLocale(), IsPartyLFG() and "INSTANCE_CHAT" or "RAID")
