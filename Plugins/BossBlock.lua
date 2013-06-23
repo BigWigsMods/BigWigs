b1a4a4fcaa1a0337ada3287f1cdf962f93c6a060
funkydude <funkydude@users.noreply.github.com>
2013-06-23 20:43:32 +0000

    BossBlock: DBM no longer does dumb RW pulling spam, remove chat filtering entirely, just emote filtering now.

--- Plugins/BossBlock.lua
+++ Plugins/BossBlock.lua
@@ -9,51 +9,17 @@ if not plugin then return end
 -- Event Handlers
 --
 
-local function chatFilter(_, _, msg)
-	if msg:find("^Pull in %d sec$") or msg:find("Pull now!", nil, true) then
-		return true
-	end
-end
-
 function plugin:OnPluginEnable()
 	if not BigWigs.db.profile.showBlizzardWarnings then
 		RaidBossEmoteFrame:UnregisterEvent("RAID_BOSS_EMOTE")
 		RaidBossEmoteFrame:UnregisterEvent("RAID_BOSS_WHISPER")
 	end
-
-	RaidWarningFrame:UnregisterEvent("CHAT_MSG_RAID_WARNING")
-	self:RegisterEvent("CHAT_MSG_RAID_WARNING")
-
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID", chatFilter)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_WARNING", chatFilter)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_LEADER", chatFilter)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_INSTANCE_CHAT", chatFilter)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_INSTANCE_CHAT_LEADER", chatFilter)
 end
 
 function plugin:OnPluginDisable()
 	if not BigWigs.db.profile.showBlizzardWarnings then
 		RaidBossEmoteFrame:RegisterEvent("RAID_BOSS_EMOTE")
 		RaidBossEmoteFrame:RegisterEvent("RAID_BOSS_WHISPER")
-	end
-
-	RaidWarningFrame:RegisterEvent("CHAT_MSG_RAID_WARNING")
-
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID", chatFilter)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID_WARNING", chatFilter)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID_LEADER", chatFilter)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_INSTANCE_CHAT", chatFilter)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_INSTANCE_CHAT_LEADER", chatFilter)
-end
-
-do
-	-- We still need this to block DBM's stupid countdown broadcast
-	local RaidWarningFrame = RaidWarningFrame
-	local RaidWarningFrame_OnEvent = RaidWarningFrame_OnEvent
-	function plugin:CHAT_MSG_RAID_WARNING(event, msg, ...)
-		if not msg:find("^Pull in %d sec$") and not msg:find("Pull now!", nil, true) then
-			RaidWarningFrame_OnEvent(RaidWarningFrame, event, msg, ...)
-		end
 	end
 end
 
