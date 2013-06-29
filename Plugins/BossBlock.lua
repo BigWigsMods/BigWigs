f0e49fc902e6561fce03f5e834de9d42a958b2a4
funkydude <funkydude@users.noreply.github.com>
2013-06-29 11:21:24 +0000

    BossBlock: test out only blocking emotes when a boss is active that we have a module for.

--- Plugins/BossBlock.lua
+++ Plugins/BossBlock.lua
@@ -10,13 +10,23 @@ if not plugin then return end
 --
 
 function plugin:OnPluginEnable()
+	self:RegisterMessage("BigWigs_OnBossEngage")
+	self:RegisterMessage("BigWigs_OnBossWin")
+	self:RegisterMessage("BigWigs_OnBossWipe", "BigWigs_OnBossWin")
+
+	if IsEncounterInProgress() then -- Just assume we logged into an encounter after a DC
+		self:BigWigs_OnBossEngage()
+	end
+end
+
+function plugin:BigWigs_OnBossEngage()
 	if not BigWigs.db.profile.showBlizzardWarnings then
 		RaidBossEmoteFrame:UnregisterEvent("RAID_BOSS_EMOTE")
 		RaidBossEmoteFrame:UnregisterEvent("RAID_BOSS_WHISPER")
 	end
 end
 
-function plugin:OnPluginDisable()
+function plugin:BigWigs_OnBossWin()
 	if not BigWigs.db.profile.showBlizzardWarnings then
 		RaidBossEmoteFrame:RegisterEvent("RAID_BOSS_EMOTE")
 		RaidBossEmoteFrame:RegisterEvent("RAID_BOSS_WHISPER")
