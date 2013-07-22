8ad555f7351eeff914d6473adc86754129ff7a06
Caleb <calebzor@gmail.com>
2013-07-22 14:56:15 +0000

    start populating the modules: SpoilsOfPandaria.lua, Thok.lua

--- SiegeOfOrgrimmar/SpoilsOfPandaria.lua
+++ SiegeOfOrgrimmar/SpoilsOfPandaria.lua
@@ -1,4 +1,8 @@
 --[[
+TODO:
+	need win trigger
+	could maybe hook into the world state timer, but I'm not sure if there is much point to work on a code just for that
+]]--
 if select(4, GetBuildInfo()) < 50400 then return end
 --------------------------------------------------------------------------------
 -- Module Declaration
@@ -6,12 +10,14 @@ if select(4, GetBuildInfo()) < 50400 then return end
 
 local mod, CL = BigWigs:NewBoss("Spoils of Pandaria", 953, 870)
 if not mod then return end
-mod:RegisterEnableMob(100000)
+mod:RegisterEnableMob(73152) -- Storeroom Guard ( trash guy )
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
+local setToBlow = {}
+local remaining
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -19,9 +25,14 @@ mod:RegisterEnableMob(100000)
 
 local L = mod:NewLocale("enUS", true)
 if L then
+	L.enable_zone = "Artifact Storage"
+	L.matter_scramble_explosion = "Matter Scramble explosion" -- shorten maybe?
 
+	L.custom_off_mark_brewmaster = "Brewmaster marker"
+	L.custom_off_mark_brewmaster_desc = "Mark the Ancient Brewmaster Spirit with %s"
 end
 L = mod:GetLocale()
+L.custom_off_mark_brewmaster_desc = L.custom_off_mark_brewmaster_desc:format("\124TInterface\\TARGETINGFRAME\\UI-RaidTargetingIcon_4.blp:15\124t")
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -29,24 +40,214 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		--"berserk", "bosskill",
+		145288, {145461, "TANK"}, {142947, "TANK"}, -- Mogu crate
+		{145987, "PROXIMITY", "FLASH"}, 145747, {145692, "TANK"}, 145715, 145786,-- Mantid crate
+		{146217, "FLASH"}, 146222, 146257, -- Crate of Panderan Relics
+		"custom_off_mark_brewmaster",
+		"proximity", "bosskill",
 	}, {
-		--[] = "general",
+		[145288] = -8434, -- Mogu crate
+		[145987] = -8439, -- Mantid crate
+		[146217] = -8366, -- Crate of Panderan Relics
+		["custom_off_mark_brewmaster"] = L.custom_off_mark_brewmaster,
+		["proximity"] = "general",
 	}
 end
 
+function mod:OnRegister()
+	-- Kel'Thuzad v3
+	local f = CreateFrame("Frame")
+	local func = function()
+		if not mod:IsEnabled() and GetSubZoneText() == L["enable_zone"] then
+			mod:Enable()
+		end
+	end
+	f:SetScript("OnEvent", func)
+	f:RegisterEvent("ZONE_CHANGED_INDOORS")
+	func()
+end
+
 function mod:OnBossEnable()
-	--self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:Log("SPELL_CAST_SUCCESS", "Engage", 145687)
+
+	-- Crate of Panderan Relics
+	self:Log("SPELL_DAMAGE", "PathOfBlossoms", 146257)
+	self:Log("SPELL_CAST_START", "BreathOfFire", 146222)
+	self:Log("SPELL_AURA_APPLIED", "KegToss", 146217)
+	-- Mogu crate
+	self:Log("SPELL_CAST_START", "CrimsonReconstitution", 142947)
+	self:Log("SPELL_CAST_START", "MoguRuneOfPower", 145461)
+	self:Log("SPELL_CAST_START", "MatterScramble", 145288)
+	-- Mantid crate
+	self:Log("SPELL_CAST_START", "Residue", 145790)
+	self:Log("SPELL_CAST_START", "ResidueStart", 145786)
+	self:Log("SPELL_DAMAGE", "BlazingCharge", 145715)
+	self:Log("SPELL_AURA_APPLIED", "BlazingCharge", 145716)
+	self:Log("SPELL_AURA_APPLIED", "WarcallerEnrage", 145692)
+	self:Log("SPELL_DAMAGE", "BubblingAmber", 145748)
+	self:Log("SPELL_AURA_APPLIED", "BubblingAmber", 145747)
+	self:Log("SPELL_AURA_APPLIED", "SetToBlowApplied", 145987)
+	self:Log("SPELL_AURA_REMOVED", "SetToBlowRemoved", 145987)
 
 	--self:Death("Win", 100000)
 end
 
 function mod:OnEngage()
-
+	wipe(setToBlow)
+	self:OpenProximity("proximity", 3)
+	remaining = nil
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
-]]--
+
+-- Crate of Panderan Relics
+
+do
+	local prev = 0
+	function mod:PathOfBlossoms(args)
+		local t = GetTime()
+		if t-prev > 2 and self:Me(args.destGUID) then -- don't spam
+			prev = t
+			self:Message(args.spellId, "Personal", "Info", CL["under_you"]:format(args.spellName))
+		end
+	end
+end
+
+do
+	local timer
+	local function markBrewmaster(GUID)
+		local brewmaster = mod:GetUnitIdByGUID(GUID)
+		if brewmaster then
+			mod:CancelTimer(timer)
+			timer = nil
+			SetRaidTarget(brewmaster, 4)
+		end
+	end
+	function mod:BreathOfFire(args)
+		if self.db.profile.custom_off_mark_brewmaster and not timer then
+			timer = self:ScheduleRepeatingTimer(markBrewmaster, 0.1, args.sourceGUID)
+		end
+		local debuffed =  UnitDebuff("player", self:SpellName(146217)) -- Keg Toss
+		self:Message(args.spellId, "Attention", debuffed and "Long")
+		if debuffed then
+			self:Flash(146217) -- flash again
+		end
+	end
+	function mod:KegToss(args)
+		if self.db.profile.custom_off_mark_brewmaster and not timer then
+			timer = self:ScheduleRepeatingTimer(markBrewmaster, 0.1, args.sourceGUID)
+		end
+		markBrewmaster(args.sourceGUID)
+		if self:Me(args.destGUID) then
+			self:Message(args.spellId, "Personal", "Info")
+			self:Flash(args.spellId)
+		end
+	end
+end
+
+-- Mogu crate
+function mod:CrimsonReconstitution(args)
+	self:Message(args.spellId, "Urgent", "Alarm")
+end
+
+function mod:MoguRuneOfPower(args)
+	self:Message(args.spellId, "Urgent", "Alarm")
+end
+
+function mod:MatterScramble(args)
+	self:Message(args.spellId, "Important", "Alert")
+	self:Bar(args.spellId, 8, L["matter_scramble_explosion"])
+end
+
+-- Mantid crate
+do
+	local prev = 0
+	function mod:ResidueStart(args)
+		local t = GetTime()
+		if t-prev > 2 and self:Dispeller("magic", true) then
+			self:Message(args.spellId, "Urgent", "Alarm")
+		end
+	end
+end
+
+function mod:ResidueStart(args)
+	if self:Dispeller("magic", true) then
+		self:Message(args.spellId, "Attention", nil, CL["casting"]:format(args.spellName))
+	end
+end
+
+function mod:WarcallerEnrage(args)
+	self:Message(args.spellId, "Urgent", "Alarm")
+end
+
+do
+	local prev = 0
+	function mod:BlazingCharge(args)
+		local t = GetTime()
+		if t-prev > 2 and self:Me(args.destGUID) then -- don't spam
+			prev = t
+			self:Message(args.spellId, "Personal", "Info", CL["under_you"]:format(args.spellName))
+		end
+	end
+end
+
+do
+	local prev = 0
+	function mod:BubblingAmber(args)
+		local t = GetTime()
+		if t-prev > 2 and self:Me(args.destGUID) then -- don't spam
+			prev = t
+			self:Message(args.spellId, "Personal", "Info", CL["under_you"]:format(args.spellName))
+		end
+	end
+end
+
+do
+	local function findPlayerInIndexedTable(t, player)
+		for k, name in next, t do
+			if name == player then
+				return k
+			end
+		end
+		return false
+	end
+	function mod:SetToBlowRemoved(args)
+		if self:Me(args.destGUID) then
+			self:Message(args.spellId, "Positive", nil, CL["over"]:format(args.spellName))
+			self:StopBar(args.spellId, args.destName)
+			if #setToBlow > 0 then
+				self:CloseProximity("proximity")
+				self:OpenProximity(args.spellId, 12, setToBlow)
+			end
+		else
+			local index = findPlayerInIndexedTable(setToBlow, args.destName)
+			if index then
+				table.remove(setToBlow, index)
+			end
+			if #setToBlow == 0 then
+				self:CloseProximity(args.spellId)
+				self:OpenProximity("proximity", 3)
+			else
+				self:CloseProximity("proximity")
+				self:OpenProximity(args.spellId, 12, setToBlow)
+			end
+		end
+	end
+end
+
+function mod:SetToBlowApplied(args)
+	if self:Me(args.destGUID) then
+		self:CloseProximity("proximity")
+		self:OpenProximity(args.spellId, 12) -- 10, but be more safe
+		self:Message(args.spellId, "Important", "Warning", CL["you"]:format(args.spellName))
+		self:TargetBar(args.spellId, 30, args.destName)
+		self:Flash(args.spellId)
+	else
+		setToBlow[#setToBlow+1] = args.destName
+		self:CloseProximity("proximity")
+		self:OpenProximity(args.spellId, 12, setToBlow)
+	end
+end
 
--- SiegeOfOrgrimmar/Thok.lua
+++ SiegeOfOrgrimmar/Thok.lua
@@ -1,4 +1,7 @@
 --[[
+TODO:
+
+]]--
 if select(4, GetBuildInfo()) < 50400 then return end
 --------------------------------------------------------------------------------
 -- Module Declaration
@@ -6,12 +9,13 @@ if select(4, GetBuildInfo()) < 50400 then return end
 
 local mod, CL = BigWigs:NewBoss("Thok the Bloodthirsty", 953, 851)
 if not mod then return end
-mod:RegisterEnableMob(100000)
+mod:RegisterEnableMob(71529)
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
+local frozenSolid = mod:NewTargetList()
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -19,7 +23,9 @@ mod:RegisterEnableMob(100000)
 
 local L = mod:NewLocale("enUS", true)
 if L then
-
+	L.tank_debuffs = "Tank debuffs"
+	L.tank_debuffs_desc = "Warnings for the different types of tank debuffs associated with Fearsome Roar"
+	L.tank_debuffs_icon = 143766
 end
 L = mod:GetLocale()
 
@@ -29,24 +35,114 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		--"berserk", "bosskill",
+		{"tank_debuffs", "TANK"}, -7963, 143428, 143777, 143783, -- stage 1
+		-7981, {-7980, "ICON", "FLASH", "SAY"}, -- stage 2
+		"proximity", "bosskill",
 	}, {
-		--[] = "general",
+		["tank_debuffs"] = -7960, -- stage 1
+		[-7981] = -7961, -- stage 2
+		["proximity"] = "general",
 	}
 end
 
 function mod:OnBossEnable()
-	--self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 
-	--self:Death("Win", 100000)
+	-- stage 2
+	self:Log("SPELL_AURA_REMOVED", "BloodFrenzyOver", 143440)
+	self:Log("SPELL_AURA_REMOVED", "FixateRemoved", 146540)
+	self:Log("SPELL_AURA_APPLIED", "FixateApplied", 146540)
+	self:Emoty("BloodFrenzy", "143440")
+	-- stage 1
+	self:Log("SPELL_PERIODIC_DAMAGE", "BurningBlood", 143783)
+	self:Log("SPELL_DAMAGE", "BurningBlood", 143783)
+	self:Log("SPELL_AURA_APPLIED", "FrozenSolid", 143777)
+	self:Log("SPELL_CAST_SUCCESS", "TailLash", 143428)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "Acceleration", 143411)
+	self:Log("SPELL_CAST_SUCCESS", "FearsomeRoar", 143426, 143780, 143773, 143767) -- Fearsome Roar, Acid Breath, Freezing Breath, Scorching Breath
+	self:Log("SPELL_AURA_APPLIED", "TankDebuff", 71529, 143780, 143773, 143767) -- Panic, Acid Breath, Freezing Breath, Scorching Breath
+	self:Log("SPELL_AURA_APPLIED_DOSE", "TankDebuff", 71529, 143780)
+
+	self:Death("Win", 71529)
 end
 
 function mod:OnEngage()
-
+	self:OpenProximity("proximity", 10) -- it is so you know if you are too close to another group
+	self:Bar(-7963, 25) -- Acceleration, not much point for more timers than the initial one since then it is too frequent
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
-]]--
+
+-- stage 2
+
+function mod:BloodFrenzyOver(args)
+	self:OpenProximity("proximity", 10)
+	self:Message(-7981, "Neutral", "Long", CL["over"]:format(args.spellName))
+	self:Bar(-7963, 25) -- Acceleration, not much point for more timers than the initial one since then it is too frequent
+end
+
+function mod:FixateRemoved(args)
+	self:PrimaryIcon(-7980)
+	if self:Me(args.destGUID) then
+		self:Message(-7980, "Positive", nil, CL["over"]:format(args.spellName))
+	end
+end
+
+function mod:FixateApplied(args)
+	if self:Me(args.destGUID) then
+		self:Say(-7980)
+		self:Flash(-7980)
+	end
+	self:TargetMessage(-7980, "Urgent", "Alarm")
+	self:PrimaryIcon(-7980, args.destName)
+end
+
+function mod:BloodFrenzy()
+	self:StopBar(143428) -- Tail Lash
+	self:StopBar(143426) -- Fearsome Roar
+	self:StopBar(143780) -- Acid Breath
+	self:StopBar(143773) -- Freezing Breath
+	self:StopBar(143767) -- Scorching Breath
+	self:CloseProximity("proximity")
+	self:Message(-7981, "Neutral", "Long")
+end
+
+-- stage 1
+
+do
+	local prev = 0
+	function mod:BurningBlood(args)
+		local t = GetTime()
+		if t-prev > 2 and self:Me(args.destGUID) then -- don't spam
+			prev = t
+			self:Message(args.spellId, "Personal", "Info", CL["under_you"]:format(args.spellName))
+		end
+	end
+end
+
+function mod:FrozenSolid(args)
+	frozenSolid[#frozenSolid+1] = args.destName
+	self:ScheduleTimer("TargetMessage", 0.1, 143777, frozenSolid, "Attention")
+end
+
+function mod:TailLash(args)
+	self:CDBar(args.spellId, 10) -- don't think this needs a message
+end
+
+function mod:Acceleration(args)
+	if args.amount > 5 and args.amount % 3 == 0 then
+		self:Message(-7963, "Attention", nil, CL["count"]:format(args.spellName, args.amount))
+	end
+end
+
+function mod:FearsomeRoar(args)
+	self:Bar("tank_debuffs", 11, args.spellName, args.spellId)
+end
+
+function mod:TankDebuff(args)
+	local amount = args.amount or 1
+	self:StackMessage("tank_debuffs", args.destName, amount, "Attention", self:Me(args.destGUID) and nil or "Warning", args.spellName, args.spellId)
+end
 
