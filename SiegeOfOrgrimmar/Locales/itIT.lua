4441bf09db651c798e6fee0e086e0d42ce9e1f41
simoflorence <simoflorence@users.noreply.github.com>
2013-07-23 20:01:19 +0000

    itIT Locale updates

--- SiegeOfOrgrimmar/Locales/itIT.lua
+++ SiegeOfOrgrimmar/Locales/itIT.lua
@@ -19,12 +19,23 @@ end
 
 L = BigWigs:NewBossLocale("Norushen", "itIT")
 if L then
-
+	L.big_adds = "Add Maggiori"
+	L.big_adds_desc = "Avvisa quando uccidi gli Add Maggiori dentro/fuori"
+	L.big_add = "Add Maggiore! (%d)"
+	L.big_add_killed = "Add MAggiore ucciso! (%d)"
 end
 
 L = BigWigs:NewBossLocale("Sha of Pride", "itIT")
 if L then
+	L.custom_off_titan_mark = "Marcatore Potenza dei Titani"
+	L.custom_off_titan_mark_desc = "Per aiutare a trovare gli altri con Potenza dei Titani, evidenzia i giocatori che hanno Potenza dei Titani su di lorocon %s%s%s%s%s%s%s%s (i giocatori con Aura d'Orgoglio non sono evidenziati), richiede capo-incursione o assistente."
 
+	L.projection_message = "Vai verso la freccia |cFF00FF00VERDE|r!"
+	L.projection_explosion = "Proiezione explosione"
+
+	L.big_add_bar = "Add Maggiore"
+	L.big_add_spawning = "Add Maggiore in arrivo!"
+	L.small_adds = "Add Minori"
 end
 
 L = BigWigs:NewBossLocale("Galakras", "itIT")
@@ -53,18 +64,14 @@ if L then
 
 	L.stance_bar = "%s (ADESSO: %s)"
 
-	L.adds_trigger1 = "Defend the gate!"
+	L.adds_trigger1 = "Defend the gate!" --triggers need verify
 	L.adds_trigger2 = "Rally the forces!"
 	L.adds_trigger3 = "Next squad, to the front!"
 	L.adds_trigger4 = "Warriors, on the double!"
 	L.adds_trigger5 = "Kor'kron, at my side!"
 
-	L.chain_heal, L.chain_heal_desc = EJ_GetSectionInfo(7935)
-	L.chain_heal_icon = 1064
 	L.chain_heal_message = "Il tuo focus sta lanciando Catena di Guarigione Potenziata!"
 
-	L.arcane_shock, L.arcane_shock_desc = EJ_GetSectionInfo(7928)
-	L.arcane_shock_icon = 114003
 	L.arcane_shock_message = "Il tuo focus sta lanciando Folgore Arcana!"
 
 	L.focus_only = "|cffff0000Avvisi solo dei bersagli focus.|r "
@@ -79,12 +86,17 @@ end
 
 L = BigWigs:NewBossLocale("Spoils of Pandaria", "itIT")
 if L then
+	L.enable_zone = "Artifact Storage"
+	L.matter_scramble_explosion = "Esplosione Scambio di Materia" -- shorten maybe?
 
+	L.custom_off_mark_brewmaster = "Marcatore Mastro Birraio"
+	L.custom_off_mark_brewmaster_desc = "Evidenzia il Mastro Birraio Antico con %s"
 end
 
 L = BigWigs:NewBossLocale("Thok the Bloodthirsty", "itIT")
 if L then
-
+	L.tank_debuffs = "Malefici Difensori"
+	L.tank_debuffs_desc = "Avvisi per i vari tipi di malefici sui Difensori associati a Ruggito Temibile"
 end
 
 L = BigWigs:NewBossLocale("Siegecrafter Blackfuse", "itIT")
@@ -102,7 +114,7 @@ if L then
 	-- for getting all those calculate emotes:
 	-- cat Transcriptor.lua | sed "s/\t//g" | grep -E "(CHAT_MSG_RAID_BOSS_EMOTE].*Iyyokuk)" | sed "s/.*EMOTE//" | sed "s/Iyyo.*//" | sed "s/#/\"/g" | sort | uniq
 	-- XXX incomplete this should be 15 strings
-	L.bomb = "Detonate every Bomb!"
+	L.bomb = "Detonate every Bomb!" -- all those triggers need checking
 	L.sword = "Slice the Swords!"
 	L.drums = "Sound the Drums!"
 	L.mantid = "PH"
@@ -122,7 +134,7 @@ if L then
 	--------------------------------
 	L.edge_message = "Sei uno dei limiti"
 	L.custom_off_edge_marks = "Marcatori dei Limiti"
-	L.custom_off_edge_marks_desc = "PEvidenzia i giocatori che, dopo i calcoli, saranno i limiti di Confine Ardente con %s%s%s%s%s%s%s (in questo ordine)(possono non essere usati tutti i simboli), richiede capo incursione o assistenteho will be edges based on the calculations %s%s%s%s%s%s, requires promoted or leader."
+	L.custom_off_edge_marks_desc = "Evidenzia i giocatori che, dopo i calcoli, saranno i limiti di Confine Ardente con %s%s%s%s%s%s%s (in questo ordine)(possono non essere usati tutti i simboli), richiede capo incursione o assistenteho will be edges based on the calculations %s%s%s%s%s%s, requires promoted or leader."
 	L.injection_over_soon = "Fine di Iniezione tra poco (%s)!"
 end
 
