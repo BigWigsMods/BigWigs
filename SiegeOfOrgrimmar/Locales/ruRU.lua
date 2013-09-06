ef682a1050c6b9d7bfa832fa538040becc53bc3c
Wetxius <wetxius@gmail.com>
2013-09-06 10:07:39 +0000

    ruRU update.

--- SiegeOfOrgrimmar/Locales/ruRU.lua
+++ SiegeOfOrgrimmar/Locales/ruRU.lua
@@ -118,11 +118,11 @@ end
 
 L = BigWigs:NewBossLocale("Siegecrafter Blackfuse", "ruRU")
 if L then
-	-- L.shredder_engage_trigger = "An Automated Shredder draws near!"
+	L.shredder_engage_trigger = "Приближается автоматический крошшер!"
 	L.laser_on_you = "Лазер на тебе ПИУ-ПИУ!"
 	L.laser_say = "Лазер ПИУ-ПИУ!"
 
-	-- L.assembly_line_trigger = "Unfinished weapons begin to roll out on the assembly line."
+	L.assembly_line_trigger = "На сборочную линию начинает поступать незаконченное оружие."
 	L.assembly_line_message = "Незавершенные оружия (%d)"
 
 	-- L.shockwave_missile_trigger = "Presenting... the beautiful new ST-03 Shockwave missile turret!"
