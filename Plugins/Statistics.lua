39fe9f55d48f5e1d3e7e3c7c7f76a19b7d4f89f6
funkydude <funkydude@users.noreply.github.com>
2013-09-12 12:26:55 +0000

    Statistics: add flex logging

--- Plugins/Statistics.lua
+++ Plugins/Statistics.lua
@@ -11,7 +11,25 @@ if not plugin then return end
 
 local L = LibStub("AceLocale-3.0"):GetLocale("Big Wigs: Plugins")
 local activeDurations = {}
-local difficultyTable = {"5", "5h", "10", "25", "10h", "25h", "lfr"}
+local difficultyTable = {false, false, "10", "25", "10h", "25h", "lfr", false, false, false, false, false, false, "flex"}
+
+--[[
+1."Normal"
+2."Heroic"
+3."10 Player"
+4."25 Player"
+5."10 Player (Heroic)"
+6."25 Player (Heroic)"
+7."Looking For Raid"
+8."Challenge Mode"
+9."40 Player"
+10.nil
+11."Heroic Scenario"
+12."Normal Scenario"
+13.nil
+14."Flexible"
+http://wowpedia.org/API_GetDifficultyInfo
+]]--
 
 -------------------------------------------------------------------------------
 -- Options
@@ -127,7 +145,7 @@ end
 --
 
 function plugin:BigWigs_OnBossEngage(event, module, diff)
-	if module.encounterId and diff and diff > 2 and diff < 8 and not module.worldBoss then -- Raid restricted for now
+	if module.encounterId and diff and difficultyTable[diff] and not module.worldBoss then -- Raid restricted for now
 		activeDurations[module.encounterId] = GetTime()
 
 		local sDB = BigWigsStatisticsDB
