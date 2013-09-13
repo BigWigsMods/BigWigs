d8ed241c01ea51062aa92d3538b2850d9b63172c
cremor <cremor@gmx.net>
2013-09-13 14:30:17 +0000

    deDE: Changed file encoding to UTF-8

--- SiegeOfOrgrimmar/Locales/deDE.lua
+++ SiegeOfOrgrimmar/Locales/deDE.lua
@@ -6,39 +6,39 @@ end
 
 L = BigWigs:NewBossLocale("The Fallen Protectors", "deDE")
 if L then
-	L.defile = "Geschändeter Boden"
+	L.defile = "GeschÃ¤ndeter Boden"
 
 	L.custom_off_bane_marks = "Schattenwort: Unheil markieren"
-	L.custom_off_bane_marks_desc = "Um bei der Einteilung zum Bannen zu helfen, werden die anfangs von Schattenwort: Unheil betroffenen Spieler mit %s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benötigt Leiter oder Assistent."
+	L.custom_off_bane_marks_desc = "Um bei der Einteilung zum Bannen zu helfen, werden die anfangs von Schattenwort: Unheil betroffenen Spieler mit %s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benÃ¶tigt Leiter oder Assistent."
 
 	L.no_meditative_field = "KEIN Meditationsfeld!"
 
-	L.intermission = "Verzweifelte Maßnahmen"
-	L.intermission_desc = "Warnt, wenn Du Dich nahe bei einem Boss befindest, welcher Verzweifelte Maßnahmen benutzt"
+	L.intermission = "Verzweifelte MaÃŸnahmen"
+	L.intermission_desc = "Warnt, wenn Du Dich nahe bei einem Boss befindest, welcher Verzweifelte MaÃŸnahmen benutzt"
 end
 
 L = BigWigs:NewBossLocale("Norushen", "deDE")
 if L then
 	L.pre_pull = "Pre-Pull"
-	L.pre_pull_desc = "Leiste für das Event, bevor der Boss angegriffen wird"
-	L.pre_pull_trigger = "Nun gut, ich werde ein Feld erschaffen, das Eure Verderbnis eindämmt."
+	L.pre_pull_desc = "Leiste fÃ¼r das Event, bevor der Boss angegriffen wird"
+	L.pre_pull_trigger = "Nun gut, ich werde ein Feld erschaffen, das Eure Verderbnis eindÃ¤mmt."
 
-	L.big_adds = "Große Adds"
-	L.big_adds_desc = "Warnungen für das Besiegen der großen Adds (Drinnen/Draußen)"
-	L.big_add = "Großes Add! (%d)"
-	L.big_add_killed = "Großes Add getötet! (%d)"
+	L.big_adds = "GroÃŸe Adds"
+	L.big_adds_desc = "Warnungen fÃ¼r das Besiegen der groÃŸen Adds (Drinnen/DrauÃŸen)"
+	L.big_add = "GroÃŸes Add! (%d)"
+	L.big_add_killed = "GroÃŸes Add getÃ¶tet! (%d)"
 end
 
 L = BigWigs:NewBossLocale("Sha of Pride", "deDE")
 if L then
 	L.custom_off_titan_mark = "Gabe der Titanen markieren"
-	L.custom_off_titan_mark_desc = "Um andere Spieler mit Gabe der Titanen einfacher zu erkennen, werden Spieler mit Gabe der Titanen mit %s%s%s%s%s%s%s%s markiert (Spieler mit Aura des Stolzes werden nicht markiert), aber trotzdem im Nähe-Fenster angezeigt (noch keine Entfernungsanzeige). Benötigt Leiter oder Assistent."
+	L.custom_off_titan_mark_desc = "Um andere Spieler mit Gabe der Titanen einfacher zu erkennen, werden Spieler mit Gabe der Titanen mit %s%s%s%s%s%s%s%s markiert (Spieler mit Aura des Stolzes werden nicht markiert), aber trotzdem im NÃ¤he-Fenster angezeigt (noch keine Entfernungsanzeige). BenÃ¶tigt Leiter oder Assistent."
 
-	L.projection_message = "Gehe zum |cFF00FF00GRÜNEN|r Pfeil!"
+	L.projection_message = "Gehe zum |cFF00FF00GRÃœNEN|r Pfeil!"
 	L.projection_explosion = "Projektion Explosion"
 
-	L.big_add_bar = "Großes Add"
-	L.big_add_spawning = "Großes Add ercsheint!"
+	L.big_add_bar = "GroÃŸes Add"
+	L.big_add_spawning = "GroÃŸes Add ercsheint!"
 	L.small_adds = "Kleine Adds"
 
 	L.titan_pride = "Titanen+Stolz: %s"
@@ -46,24 +46,24 @@ end
 
 L = BigWigs:NewBossLocale("Galakras", "deDE")
 if L then
-	L.demolisher = "Verwüster"
-	L.demolisher_desc = "Zeigt an, wann die Verwüster der Kor'kron in den Kampf eintreten"
-	L.towers = "Türme"
-	L.towers_desc = "Warnungen für das Durchbrechen der Tore zu den Türmen"
-	L.south_tower_trigger = "Das Tor zum Südturm ist durchbrochen!"
-	L.south_tower = "Südlicher Turm"
+	L.demolisher = "VerwÃ¼ster"
+	L.demolisher_desc = "Zeigt an, wann die VerwÃ¼ster der Kor'kron in den Kampf eintreten"
+	L.towers = "TÃ¼rme"
+	L.towers_desc = "Warnungen fÃ¼r das Durchbrechen der Tore zu den TÃ¼rmen"
+	L.south_tower_trigger = "Das Tor zum SÃ¼dturm ist durchbrochen!"
+	L.south_tower = "SÃ¼dlicher Turm"
 	L.north_tower_trigger = "Das Tor zum Nordturm ist durchbrochen!"
-	L.north_tower = "Nördlicher Turm"
+	L.north_tower = "NÃ¶rdlicher Turm"
 	L.tower_defender = "Turmverteidiger"
 
 	L.custom_off_shaman_marker = "Schamanen markieren"
-	L.custom_off_shaman_marker_desc = "Um bei der Einteilung zum Unterbrechen zu helfen, werden die Gezeitenschamanen des Drachenmals mit %s%s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benötigt Leiter oder Assistent."
+	L.custom_off_shaman_marker_desc = "Um bei der Einteilung zum Unterbrechen zu helfen, werden die Gezeitenschamanen des Drachenmals mit %s%s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benÃ¶tigt Leiter oder Assistent."
 end
 
 L = BigWigs:NewBossLocale("Iron Juggernaut", "deDE")
 if L then
 	L.custom_off_mine_marks = "Minen markieren"
-	L.custom_off_mine_marks_desc = "Um bei der Einteilung zum Einstampfen zu helfen, werden die Kriecherminen mit %s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benötigt Leiter oder Assistent."
+	L.custom_off_mine_marks_desc = "Um bei der Einteilung zum Einstampfen zu helfen, werden die Kriecherminen mit %s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benÃ¶tigt Leiter oder Assistent."
 end
 
 L = BigWigs:NewBossLocale("Kor'kron Dark Shaman", "deDE")
@@ -71,13 +71,13 @@ if L then
 	L.blobs = "Schleim"
 
 	L.custom_off_mist_marks = "Toxischer Nebel"
-	L.custom_off_mist_marks_desc = "Um bei der Einteilung zum Heilen zu helfen, werden Spieler mit Toxischem Nebel mit %s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt)(Tanks werden nicht markiert), benötigt Leiter oder Assistent."
+	L.custom_off_mist_marks_desc = "Um bei der Einteilung zum Heilen zu helfen, werden Spieler mit Toxischem Nebel mit %s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt)(Tanks werden nicht markiert), benÃ¶tigt Leiter oder Assistent."
 end
 
 L = BigWigs:NewBossLocale("General Nazgrim", "deDE")
 if L then
 	L.custom_off_bonecracker_marks = "Knochenknacker"
-	L.custom_off_bonecracker_marks_desc = "Um bei der Einteilung zum Heilen zu helfen, werden Spieler mit Knochenknacker mit %s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benötigt Leiter oder Assistent."
+	L.custom_off_bonecracker_marks_desc = "Um bei der Einteilung zum Heilen zu helfen, werden Spieler mit Knochenknacker mit %s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benÃ¶tigt Leiter oder Assistent."
 
 	L.stance_bar = "%s(JETZT:%s)"
 	L.battle = "Kampf"
@@ -86,23 +86,23 @@ if L then
 
 	L.adds_trigger1 = "Verteidigt das Tor!"
 	L.adds_trigger2 = "Truppen, sammelt euch!"
-	L.adds_trigger3 = "Nächste Staffel, nach vorn!"
+	L.adds_trigger3 = "NÃ¤chste Staffel, nach vorn!"
 	L.adds_trigger4 = "Krieger, im Laufschritt!"
 	L.adds_trigger5 = "Kor'kron, zu mir!"
-	L.adds_trigger_extra_wave = "Alle Kor'kron... unter meinem Befehl... tötet sie... JETZT"
-	L.extra_adds = "Zusätzliche Adds"
+	L.adds_trigger_extra_wave = "Alle Kor'kron... unter meinem Befehl... tÃ¶tet sie... JETZT"
+	L.extra_adds = "ZusÃ¤tzliche Adds"
 
 	L.chain_heal_message = "Dein Fokusziel wirkt Kettenheilung!"
 
 	L.arcane_shock_message = "Dein Fokusziel wirkt Arkaner Schock!"
 
-	L.focus_only = "|cffff0000Nur Meldungen für Fokusziele.|r "
+	L.focus_only = "|cffff0000Nur Meldungen fÃ¼r Fokusziele.|r "
 end
 
 L = BigWigs:NewBossLocale("Malkorok", "deDE")
 if L then
-	L.custom_off_energy_marks = "Verdrängte Energie markieren"
-	L.custom_off_energy_marks_desc = "Um bei der Einteilung zum Bannen zu helfen, werden Spieler mit Verdrängter Energie mit %s%s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benötigt Leiter oder Assistent."
+	L.custom_off_energy_marks = "VerdrÃ¤ngte Energie markieren"
+	L.custom_off_energy_marks_desc = "Um bei der Einteilung zum Bannen zu helfen, werden Spieler mit VerdrÃ¤ngter Energie mit %s%s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benÃ¶tigt Leiter oder Assistent."
 end
 
 L = BigWigs:NewBossLocale("Spoils of Pandaria", "deDE")
@@ -116,38 +116,38 @@ end
 
 L = BigWigs:NewBossLocale("Thok the Bloodthirsty", "deDE")
 if L then
-	L.tank_debuffs = "Tank Schwächungszauber"
-	L.tank_debuffs_desc = "Warnungen für die verschiedenen Typen von Schwächungszaubern auf den Tanks in Verbindung mit Fürchterlichem Brüllen"
+	L.tank_debuffs = "Tank SchwÃ¤chungszauber"
+	L.tank_debuffs_desc = "Warnungen fÃ¼r die verschiedenen Typen von SchwÃ¤chungszaubern auf den Tanks in Verbindung mit FÃ¼rchterlichem BrÃ¼llen"
 
-	L.cage_opened = "Käfig geöffnet"
+	L.cage_opened = "KÃ¤fig geÃ¶ffnet"
 end
 
 L = BigWigs:NewBossLocale("Siegecrafter Blackfuse", "deDE")
 if L then
-	L.shredder_engage_trigger = "Ein Automatisierter Schredder nähert sich!"
+	L.shredder_engage_trigger = "Ein Automatisierter Schredder nÃ¤hert sich!"
 	L.laser_on_you = "Laser auf Dir PEW PEW!"
 	L.laser_say = "Laser PEW PEW!"
 
-	L.assembly_line_trigger = "Unfertige Waffen befinden sich auf dem Fließband."
+	L.assembly_line_trigger = "Unfertige Waffen befinden sich auf dem FlieÃŸband."
 	L.assembly_line_message = "Unfertige Waffen (%d)"
 
-	L.shockwave_missile_trigger = "Ich präsentiere... den wunderschönen Erschütterungsraketenturm ST-03!"
+	L.shockwave_missile_trigger = "Ich prÃ¤sentiere... den wunderschÃ¶nen ErschÃ¼tterungsraketenturm ST-03!"
 end
 
 L = BigWigs:NewBossLocale("Paragons of the Klaxxi", "deDE")
 if L then
-	L.one = "Iyyokuk wählt: Eins!"
-	L.two = "Iyyokuk wählt: Zwei!"
-	L.three = "Iyyokuk wählt: Drei!"
-	L.four = "Iyyokuk wählt: Vier!"
-	L.five = "Iyyokuk wählt: Fünf!"
+	L.one = "Iyyokuk wÃ¤hlt: Eins!"
+	L.two = "Iyyokuk wÃ¤hlt: Zwei!"
+	L.three = "Iyyokuk wÃ¤hlt: Drei!"
+	L.four = "Iyyokuk wÃ¤hlt: Vier!"
+	L.five = "Iyyokuk wÃ¤hlt: FÃ¼nf!"
 	--------------------------------
 	L.edge_message = "Brennendes Band auf Dir"
 	L.custom_off_edge_marks = "Brennendes Band markieren"
-	L.custom_off_edge_marks_desc = "Markiert die Spieler mit Brennendem Band, basierend auf den Kalkulationen, mit %s%s%s%s%s%s, benötigt Leiter oder Assistent."
+	L.custom_off_edge_marks_desc = "Markiert die Spieler mit Brennendem Band, basierend auf den Kalkulationen, mit %s%s%s%s%s%s, benÃ¶tigt Leiter oder Assistent."
 	L.injection_over_soon = "Injektion bald vorbei (%s)!"
 	L.custom_off_mutate_marks = "Mutation: Bernskorpion markieren"
-	L.custom_off_mutate_marks_desc = "Markiert die Spieler mit Mutation: Bernskorpion mit %s%s%s um bei der Einteilung zum Heilen zu helfen, benötigt Leiter oder Assistent."
+	L.custom_off_mutate_marks_desc = "Markiert die Spieler mit Mutation: Bernskorpion mit %s%s%s um bei der Einteilung zum Heilen zu helfen, benÃ¶tigt Leiter oder Assistent."
 end
 
 L = BigWigs:NewBossLocale("Garrosh Hellscream", "deDE")
@@ -155,15 +155,15 @@ if L then
 	L.intermission = "Unterbrechung"
 	L.mind_control = "Gedankenkontrolle"
 
-	L.chain_heal_desc = "Heilt ein verbündetes Ziel um 40% ihrer maximalen Gesundheit. Springt auf nahe verbündete Ziele über."
+	L.chain_heal_desc = "Heilt ein verbÃ¼ndetes Ziel um 40% ihrer maximalen Gesundheit. Springt auf nahe verbÃ¼ndete Ziele Ã¼ber."
 	L.chain_heal_message = "Dein Fokusziel wirkt Kettenheilung!"
 	L.chain_heal_bar = "Fokusziel: Kettenheilung"
 
 	L.farseer_trigger = "Scharfseher, heilt unsere Wunden!"
 	L.custom_off_shaman_marker = "Scharfseher markieren"
-	L.custom_off_shaman_marker_desc = "Um bei der Einteilung zum Unterbrechen zu helfen, werden die Scharfseherwolfsreiter mit %s%s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benötigt Leiter oder Assistent."
+	L.custom_off_shaman_marker_desc = "Um bei der Einteilung zum Unterbrechen zu helfen, werden die Scharfseherwolfsreiter mit %s%s%s%s%s%s%s markiert (in dieser Reihenfolge)(vielleicht werden nicht alle Symbole genutzt), benÃ¶tigt Leiter oder Assistent."
 
-	L.focus_only = "|cffff0000Nur Meldungen für Fokusziele.|r "
+	L.focus_only = "|cffff0000Nur Meldungen fÃ¼r Fokusziele.|r "
 end
 
 L = BigWigs:NewBossLocale("Siege of Orgrimmar Trash", "deDE")
