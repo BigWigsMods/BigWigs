f0f44b21b8aec28e311dc0595cbde19c612a8afa
funkydude <funkydude@users.noreply.github.com>
2013-09-23 15:42:06 +0000

    test out using the wowace localization for the common locale

--- Locales/common.frFR.lua
+++ Locales/common.frFR.lua
@@ -1,42 +1,5 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Big Wigs: Common", "frFR")
 if not L then return end
 -- Prototype.lua common words
-L.you = "%s sur VOUS !"
-L.underyou = "%s en dessous de VOUS !"
-L.other = "%s : %s"
-L.onboss = "%s sur le BOSS !"
-L.say = "%s sur MOI !"
-L.on = "%s sur %s"
-L.stack = "%dx %s sur %s"
-L.cast = "<%s incanté>"
-L.casting = "%s en incantation !"
-L.soon = "%s imminent(e) !"
-L.count = "%s (%d)"
-L.near = "%s près de VOUS !"
-
-L.phase = "Phase %d"
-L.normal = "Mode normal"
-L.heroic = "Mode héroïque"
-L.hard = "Mode difficile"
-L.general = "Général" -- General settings, i.e. things that apply to both normal and hard mode.
-
-L.duration = "%s pendant %s sec."
-L.over = "%s terminé !"
-L.removed = "%s enlevé"
-L.incoming = "Arrivée |2 %s !"
-L.interrupted = "%s interrompu"
-
-L.add_spawned = "Add apparu !"
-L.next_add = "Prochain Add"
-L.add_killed = "Add tué ! (%d/%d)"
-L.add_remaining = "Add tué, il en reste %d"
-
-L.mob_killed = "%s tué ! (%d/%d)"
-L.mob_remaining = "%s tué, il en reste %d"
-
-L.custom_start = "%s engagé - %s dans %d min."
-L.custom_start_s = "%s engagé - %s dans %d sec."
-L.custom_end = "%s devient %s !"
-L.custom_min = "%s dans %d min."
-L.custom_sec = "%s dans %d sec."
+--@localization(locale="frFR", namespace="Common", format="lua_additive_table", handle-unlocalized="ignore")@
 
