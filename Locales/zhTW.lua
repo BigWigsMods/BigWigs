574c3a8f4600590816c2b4902874f06e915e685c
funkydude <funkydude@users.noreply.github.com>
2013-09-24 12:38:39 +0000

    add some notes about translations

--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -1,4 +1,5 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Big Wigs", "zhTW")
 if not L then return end
+-- These localization strings are translated on WoWAce: http://www.wowace.com/addons/big-wigs/localization/
 --@localization(locale="zhTW", namespace="Core", format="lua_additive_table", handle-unlocalized="ignore")@
 
