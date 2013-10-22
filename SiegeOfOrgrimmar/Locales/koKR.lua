6a2c4cc1d5f6aaa03860ee87e6266e0d1ba7e392
funkydude <funkydude@users.noreply.github.com>
2013-10-22 21:22:07 +0000

    koKR update

--- SiegeOfOrgrimmar/Locales/koKR.lua
+++ SiegeOfOrgrimmar/Locales/koKR.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Immerseus", "koKR")
 if not L then return end
 if L then
-
+	L.win_yell = "아, 역시 해냈군! 골짜기의 물이 다시 깨끗해졌네."
 end
 
 L = BigWigs:NewBossLocale("The Fallen Protectors", "koKR")
@@ -11,7 +11,7 @@ end
 
 L = BigWigs:NewBossLocale("Norushen", "koKR")
 if L then
-
+	L.pre_pull_trigger = "그래, 좋다. 너희 타락을 가두어 둘 공간을 만들겠다."
 end
 
 L = BigWigs:NewBossLocale("Sha of Pride", "koKR")
@@ -36,7 +36,13 @@ end
 
 L = BigWigs:NewBossLocale("General Nazgrim", "koKR")
 if L then
-
+	L.adds_trigger1 = "놈들을 막아라!"
+	L.adds_trigger2 = "병력 집결!"
+	L.adds_trigger3 = "다음 분대, 앞으로!"
+	L.adds_trigger4 = "전사들이여! 이리로!"
+	L.adds_trigger5 = "코르크론! 날 지원하라!"
+	L.adds_trigger_extra_wave = "전 코르크론, 내 명령을 따르라. 모두 죽여!"
+	L.extra_adds = "다음 병력: "
 end
 
 L = BigWigs:NewBossLocale("Malkorok", "koKR")
@@ -56,7 +62,8 @@ end
 
 L = BigWigs:NewBossLocale("Siegecrafter Blackfuse", "koKR")
 if L then
-
+	L.shredder_engage_trigger = "자동 분쇄기가 다가옵니다!"
+	L.assembly_line_trigger = "생산 설비에서 미완성 무기가 나오기 시작합니다."
 end
 
 L = BigWigs:NewBossLocale("Paragons of the Klaxxi", "koKR")
