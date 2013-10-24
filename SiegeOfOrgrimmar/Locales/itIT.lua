8a09ab61abeea4c07556673817dde5b868b084ea
simoflorence <simoflorence@users.noreply.github.com>
2013-10-24 21:28:39 +0000

    itIT Locale updates

--- SiegeOfOrgrimmar/Locales/itIT.lua
+++ SiegeOfOrgrimmar/Locales/itIT.lua
@@ -68,7 +68,7 @@ end
 L = BigWigs:NewBossLocale("Iron Juggernaut", "itIT")
 if L then
 	L.custom_off_mine_marks = "Marcatore delle Mine"
-	L.custom_off_mine_marks_desc = "Per aiutare l'assegnazione degli assorbimenti, evidenzia le Mine Striscianti con {rt1}{rt2}{rt3}, richiede capo incursione o assistente.\n|cFFFF0000OSolo 1 persona nell'incursione dovrebbe attivare questa opzione per evitare conflitti con le assegnazioni.|r\n|cFFADFF2FSUGGERIMENTO: Se l'Incursione ha scelto te per abilitare questa opzione, muovere velocemente il mouse sopra tutte le mine è il modo più rapido per evidenziarle.|r"
+	L.custom_off_mine_marks_desc = "Per aiutare l'assegnazione degli assorbimenti, evidenzia le Mine Striscianti con {rt1}{rt2}{rt3}, richiede capo incursione o assistente.\n|cFFFF0000Solo 1 persona nell'incursione dovrebbe attivare questa opzione per evitare conflitti con le assegnazioni.|r\n|cFFADFF2FSUGGERIMENTO: Se l'Incursione ha scelto te per abilitare questa opzione, muovere velocemente il mouse sopra tutte le mine è il modo più rapido per evidenziarle.|r"
 end
 
 L = BigWigs:NewBossLocale("Kor'kron Dark Shaman", "itIT")
@@ -127,6 +127,9 @@ if L then
 	L.tank_debuffs_desc = "Avvisi per i vari tipi di malefici sui Difensori associati a Ruggito Temibile"
 
 	L.cage_opened = "Gabbia Aperta"
+
+	L.akolik = "Akolik"
+	L.waterspeaker_gorai = "Oratore dell'Acqua Gorai"	
 end
 
 L = BigWigs:NewBossLocale("Siegecrafter Blackfuse", "itIT")
@@ -140,49 +143,61 @@ if L then
 
 	L.disabled = "Disabilitato"
 
-	L.shredder_engage_trigger = "Un Segatronchi Automatizzato si avvicina!" -- needs verify
+	L.shredder_engage_trigger = "Un Segatronchi Automatizzato si avvicina!" -- verified
 	L.laser_on_you = "Laser su di te PEW PEW!"
 	L.laser_say = "Laser PEW PEW"
 
-	L.assembly_line_trigger = "Unfinished weapons begin to roll out on the assembly line."--need check/translation
+	L.assembly_line_trigger = "Armi incomplete iniziano a uscire dalla catena di montaggio." --verified
 	L.assembly_line_message = "Armi non finite (%d)"
-	--L.assembly_line_items = "Items (%d): %s"
-	--L[71606] = "Missile" -- Deactivated Missile Turret
-	--L[71790] = "Mines" -- Disassembled Crawler Mines
-	--L[71751] = "Laser" -- Deactivated Laser Turret
-	--L[71694] = "Magnet" -- Deactivated Electromagnet
+	L.assembly_line_items = "Oggetti (%d): %s"
+	L[71606] = "Missile" -- Deactivated Missile Turret
+	L[71790] = "Mine" -- Disassembled Crawler Mines
+	L[71751] = "Laser" -- Deactivated Laser Turret
+	L[71694] = "Magnete" -- Deactivated Electromagnet
 
-	L.shockwave_missile_trigger = "Vi presento... la nuova, magnifica torretta lanciamissili a onda d'urto ST-03!" --need check
+	L.shockwave_missile_trigger = "Vi presento... la nuova, magnifica torretta lanciamissili a onda d'urto ST-03!" --verified
 end
 
 L = BigWigs:NewBossLocale("Paragons of the Klaxxi", "itIT")
 if L then
 	L.catalyst_match = "Catalizzatore: |c%sHA SCELTO TE|r" -- might not be best for colorblind?
-	--L.you_ate = "You ate a parasite (%d left)"
-	--L.other_ate = "%s ate a %sparasite (%d left)"
-	--L.parasites_up = "%d |4Parasite:Parasites; up"
+	L.you_ate = "Hai mangiato un parassita (%d rimasti)"
+	L.other_ate = "%s ha mangiato un %sparassita (%d rimasti)"
+	L.parasites_up = "%d |4Parassita:Parassiti; attivi"
 	L.dance = "Danza"
-	L.prey_message = "Usa Preda sul parassita"
-	L.one = "Iyyokuk selects: One!"
-	L.two = "Iyyokuk selects: Two!"
-	L.three = "Iyyokuk selects: Three!"
-	L.four = "Iyyokuk selects: Four!"
-	L.five = "Iyyokuk selects: Five!"
+	L.prey_message = "Usa Preda sul Parassita"
+	L.one = "Iyyokuk seleziona: Uno!"
+	L.two = "Iyyokuk seleziona: Due!"
+	L.three = "Iyyokuk seleziona: Tre!"
+	L.four = "Iyyokuk seleziona: Quattro!"
+	L.five = "Iyyokuk seleziona: Cinque!"
 	L.edge_message = "Sei uno dei limiti"
 	L.custom_off_edge_marks = "Marcatori dei Limiti"
 	L.custom_off_edge_marks_desc = "Evidenzia i giocatori che saranno i limiti in base ai calcoli {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}{rt7}{rt8}, richiede capoincursione o assistente.\n|cFFFF0000Solo 1 persona nell'incursione dovrebbe attivare questa opzione per evitare conflitti con le assegnazioni.|r"
 	L.injection_over_soon = "Fine di Iniezione tra poco (%s)!"
-	--L.custom_off_parasite_marks = "Parasite marker"
+	L.custom_off_parasite_marks = "Marcatore Parassita"
 	L.custom_off_parasite_marks_desc = "Evidenzia i parassiti da controllare e le assegnazioni di Preda con {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}{rt7}, richiede capoincursione o assistente.\n|cFFFF0000Solo 1 persona nell'incursione dovrebbe attivare questa opzione per evitare conflitti con le assegnazioni.|r"
 end
 
 L = BigWigs:NewBossLocale("Garrosh Hellscream", "itIT")
 if L then
-	L.intermission = "Intermezzo"
-	L.mind_control = "Controllo della Mente"
+	L.manifest_rage = "Manifestazione della Rabbia"
+	L.manifest_rage_desc = "Quando Garrosh raggiungwe 100 inizierà a prelanciare Manifestazione della Rabbia per 2 secondi, e poi la canalizzerà. Mentre canalizza, evoca degli add grandi. Porta la Pirostella su Garrosh per incapacitarlo ed interrompere il suo lancio."	
 
-	--L.ironstar_impact_desc = "A timer bar for when the Iron Star will impact the wall at the other side."
-	--L.ironstar_rolling = "Iron Star Rolling!"
+	L.phase_3_end_trigger = "Pensate di aver VINTO? Siete CIECHI. VI COSTRINGERÒ AD APRIRE GLI OCCHI."
+	
+	L.clump_check = "CControllo ammucchiamento"
+	L.clump_check_desc = "Controlla ogni 3 secondi durante il Bombardamento i giocatori ammucchiati, se viene rilevato un gruppo, verrà creata una Pirostella Kor'kron."
+	
+	L.bombardment = "Bombardmento"
+	L.bombardment_desc = "Bombardamendo di Roccavento che lascia dei fuochi sul terreno. Le Pirostelle Kor'kron possono apparire soltanto durante il bombardmento."
+	
+	L.spread = "Allargarsi!"
+	L.intermission = "Intermezzo"
+	L.mind_control = "Controllo della Mente"	
+	
+	L.ironstar_impact_desc = "Una barra a tempo per quando la Pirostella si schianterà contro l'altra parte della stanza."
+	L.ironstar_rolling = "Pirostella in movimento!"
 
 	L.chain_heal_desc = "Cura un bersaglio amico per il 40% della sua vita massima, e a catena anche i bersagli amici vicini."
 	L.chain_heal_message = "Il tuo focus sta lanciando Catena di Guarigione!"
@@ -192,6 +207,9 @@ if L then
 	L.custom_off_shaman_marker = "Marcatore Chiaroveggenti"
 	L.custom_off_shaman_marker_desc = "Per aiutare l'assegnazione delle interruzioni, evidenzia i Cavalcalupi Chiaroveggenti con {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}{rt7} (in questo ordine, non tutti i simboli possono essere usati), richiede capo incursione o assistente."
 
+	L.custom_off_minion_marker = "Marcatore servitori"
+	L.custom_off_minion_marker_desc = "Per aiutare a separare i servitori, evidenziali con {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}{rt7}{rt8}, richiede capoincursione o assistente."
+	
 	L.focus_only = "|cffff0000Avvisi solo dei bersagli focus.|r "
 end
 
