3785e36ffba86c581e856f1592a7d337ae11c3b1
jongt23 <jongt23@gmail.com>
2013-11-28 13:41:42 +0000

    esES update

--- SiegeOfOrgrimmar/Locales/esES.lua
+++ SiegeOfOrgrimmar/Locales/esES.lua
@@ -20,6 +20,12 @@ end
 L = BigWigs:NewBossLocale("Sha of Pride", "esES") or BigWigs:NewBossLocale("Sha of Pride", "esMX")
 if L then
 --@localization(locale="esES", namespace="SiegeOfOrgrimmar/ShaOfPride", format="lua_additive_table", handle-unlocalized="ignore")@
+
+	L.custom_off_titan_mark = "Marcador de Ofrenda de los titanes"
+	L.custom_off_titan_mark_desc = "Marca la gente que tiene Ofrenda de los titanes con {rt1}{rt2}{rt3}{rt4}{rt5}{rt6}{rt7}{rt8}, requiere ayudante o líder.\n|cFFFF0000Solo 1 persona de la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r"
+
+	L.custom_off_fragment_mark = "Marcador de Fragmento corrupto"
+	L.custom_off_fragment_mark_desc = "Marca los Fragmentos corruptos con {rt8}{rt7}{rt6}, requiere ayudante o líder.\n|cFFFF0000Solo 1 persona de la raid deberia tener activada esta opción para prevenir conflictos con las marcas.\nEn modo de 25 jugadores, estó provocará conflictos con el marcador de Ofrenda de los titanes.|r"
 end
 
 L = BigWigs:NewBossLocale("Galakras", "esES") or BigWigs:NewBossLocale("Galakras", "esMX")
