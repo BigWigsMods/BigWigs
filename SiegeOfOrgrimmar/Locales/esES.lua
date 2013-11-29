60f8559f07679f3496581647a938a764362846c1
jongt23 <jongt23@gmail.com>
2013-11-29 12:17:38 +0000

    esES update

--- SiegeOfOrgrimmar/Locales/esES.lua
+++ SiegeOfOrgrimmar/Locales/esES.lua
@@ -31,20 +31,51 @@ end
 L = BigWigs:NewBossLocale("Galakras", "esES") or BigWigs:NewBossLocale("Galakras", "esMX")
 if L then
 --@localization(locale="esES", namespace="SiegeOfOrgrimmar/Galakras", format="lua_additive_table", handle-unlocalized="ignore")@
+
+	L.custom_off_shaman_marker = "Marcador del Chamán"
+	L.custom_off_shaman_marker_desc = "Para ayudar con las interrupciones, marca los Chamanes de mareas Faucedraco con {rt1}{rt2}{rt3}{rt4}{rt5}, requiere ayudante o líder.\n|cFFFF0000Solo 1 persona de la raid debería tener esta opción activada para prevenir conflictos con las marcas.|r\n|cFFADFF2FCONSEJO: Si la raid te ha elegido para que la actives, mover rápidamente el ratón por encima de los chamanes es la manera más rápida de marcarlos.|r"
 end
 
 L = BigWigs:NewBossLocale("Iron Juggernaut", "esES") or BigWigs:NewBossLocale("Iron Juggernaut", "esMX")
 if L then
+	L.custom_off_mine_marks = "Marcador de Minas"
+	L.custom_off_mine_marks_desc = "Para ayudar con las asignaciones de las minas, marca las Minas reptadoras con {rt1}{rt2}{rt3}, requiere ayudante o líder.\n|cFFFF0000Solo 1 persona de la raid debería tener activada esta opción para prevenir coflictos con las marcas.|r\n|cFFADFF2FCONSEJO: Si la raid te ha elegido para que la actives, mover rápidamente el ratón por encima de todas las minas es la manera más rápida de marcarlas.|r"
 
 end
 
 L = BigWigs:NewBossLocale("Kor'kron Dark Shaman", "esES") or BigWigs:NewBossLocale("Kor'kron Dark Shaman", "esMX")
 if L then
+	L.blobs = "Blobs"
 
+	L.custom_off_mist_marks = "Marcador de Niebla tóxica"
+	L.custom_off_mist_marks_desc = "Para ayudar con las tareas de sanación, marca la gente que tiene Niebla tóxica con {rt1}{rt2}{rt3}{rt4}{rt5}, requiere ayudante o líder.\n|cFFFF0000Solo 1 persona de la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r"
 end
 
 L = BigWigs:NewBossLocale("General Nazgrim", "esES") or BigWigs:NewBossLocale("General Nazgrim", "esMX")
 if L then
+	L.custom_off_bonecracker_marks = "Marcador de Partehuesos"
+	L.custom_off_bonecracker_marks_desc = "Para ayudar en las tareas de sanación, marca la gente que tiene Partehuesos con {rt1}{rt2}{rt3}{rt4}{rt5}, requiere ayudante o líder.\n|cFFFF0000Solo 1 persona de la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r"
+
+	L.stance_bar = "%s(AHORA:%s)"
+	-- shorten stances so they fit on the bars
+	L.battle = "Batalla"
+	L.berserker = "Rabiosa"
+	L.defensive = "Defensiva"
+
+	L.adds_trigger1 = "¡Defended la puerta!"
+	L.adds_trigger2 = "¡Reunid a las tropas!"
+	L.adds_trigger3 = "¡Siguiente escuadrón, al frente!"
+	L.adds_trigger4 = "¡Guerreros, paso ligero!"
+	L.adds_trigger5 = "¡Kor'kron, conmigo!"
+	--L.adds_trigger_extra_wave = "All Kor'kron... under my command... kill them... NOW!"
+	L.extra_adds = "Adds extra"
+	L.final_wave = "Oleada final"
+
+	L.chain_heal_message = "¡Tu foco está casteando Sanación en cadena!"
+
+	L.arcane_shock_message = "¡Tu foco está casteando Choque Arcano!"
+
+	L.focus_only = "|cffff0000Alertas solo para objetivos en Foco.|r "
 
 end
 
