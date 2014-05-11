f47ec76d8046ac07e763c2d65529edc454c13a54
funkydude <funkydude@users.noreply.github.com>
2014-05-11 21:24:42 +0000

    SiegecrafterBlackfuse: Sometimes Electrostatic Charge can be dodged, or misses, or something. Use the SUCCESS event to track the cooldown instead.

--- SiegeOfOrgrimmar/SiegecrafterBlackfuse.lua
+++ SiegeOfOrgrimmar/SiegecrafterBlackfuse.lua
@@ -111,7 +111,9 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED_DOSE", "Overload", 145444)
 	self:Log("SPELL_AURA_APPLIED", "Overload", 145444)
 	-- Siegecrafter Blackfuse
-	self:Log("SPELL_AURA_APPLIED_DOSE", "ElectrostaticCharge", 143385)
+	self:Log("SPELL_CAST_SUCCESS", "ElectrostaticCharge", 143385)
+	self:Log("SPELL_AURA_APPLIED", "ElectrostaticChargeApplied", 143385)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "ElectrostaticChargeApplied", 143385)
 	self:Log("SPELL_AURA_APPLIED", "ProtectiveFrenzy", 145365)
 	self:Log("SPELL_CAST_START", "Sawblade", 143265)
 	self:Log("SPELL_CAST_SUCCESS", "SawbladeFallback", 143265)
@@ -320,6 +322,9 @@ end
 
 function mod:ElectrostaticCharge(args)
 	self:CDBar(args.spellId, 17)
+end
+
+function mod:ElectrostaticChargeApplied(args)
 	self:StackMessage(args.spellId, args.destName, args.amount, "Attention", "Info")
 end
 
