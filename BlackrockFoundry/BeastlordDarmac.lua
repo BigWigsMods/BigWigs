68fe48e1ee83f052bdbf767f66423eedbd51eb17
Benjamin Staneck <staneck@gmail.com>
2014-07-22 17:17:56 +0000

    Blackrock Foundry skeleton module

---
+++ BlackrockFoundry/BeastlordDarmac.lua
@@ -0,0 +1,52 @@
+
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Beastlord Darmac", 953, 868)
+if not mod then return end
+mod:RegisterEnableMob(
+	72249, 72358, -- Galakras, Kor'kron Cannon
+	72560, 72561, 73909, -- Horde: Lor'themar Theron, Lady Sylvanas Windrunner, Archmage Aethas Sunreaver
+	72311, 72302, 73910 -- Alliance: King Varian Wrynn, Lady Jaina Proudmoore, Vereesa Windrunner
+)
+mod.engageId = 1622
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:NewLocale("enUS", true)
+if L then
+	
+end
+L = mod:GetLocale()
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		
+	}
+end
+
+function mod:OnBossEnable()
+	if self.lastKill and (GetTime() - self.lastKill) < 120 then -- Temp for outdated users enabling us
+		self:ScheduleTimer("Disable", 5)
+		return
+	end
+
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
---
+++ BlackrockFoundry/Gruul.lua
@@ -0,0 +1,50 @@
+
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Gruul", 1161, 868)
+if not mod then return end
+mod:RegisterEnableMob(
+	76877, -- Gruul	
+)
+mod.engageId = 1691
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:NewLocale("enUS", true)
+if L then
+	
+end
+L = mod:GetLocale()
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		
+	}
+end
+
+function mod:OnBossEnable()
+	if self.lastKill and (GetTime() - self.lastKill) < 120 then -- Temp for outdated users enabling us
+		self:ScheduleTimer("Disable", 5)
+		return
+	end
+
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
