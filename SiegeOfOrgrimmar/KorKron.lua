de5906a93914cb2e3a2e103a7d00e22ed894bba3
Kyle Buller <bullerk@gmail.com>
2014-07-26 12:17:28 +0000

    KorKron: fix Falling Ash numbering

--- SiegeOfOrgrimmar/KorKron.lua
+++ SiegeOfOrgrimmar/KorKron.lua
@@ -182,8 +182,8 @@ end
 function mod:FallingAsh(args)
 	-- this is for when the damage happens
 	self:ScheduleTimer("Message", 14, args.spellId, "Attention", self:Healer() and "Info", CL.soon:format(CL.count:format(args.spellName, ashCounter)))
-	ashCounter = ashCounter + 1
 	self:Bar(args.spellId, 17, CL.count:format(args.spellName, ashCounter))
+	ashCounter = ashCounter + 1
 end
 
 function mod:FoulGeyser(args) -- Blobs
