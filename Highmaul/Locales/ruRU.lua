6251858d2ad8de97c20c0381934601284ae50e27
Wetxius <wetxius@gmail.com>
2014-09-06 08:54:41 +0000

    ruRU update.

--- Highmaul/Locales/ruRU.lua
+++ Highmaul/Locales/ruRU.lua
@@ -22,10 +22,10 @@ if L then
 	--L.shard = "Shard"
 	--L.motes = "Motes"
 
-	--L.custom_off_barrage_marker = "Crystalline Barrage marker"
-	--L.custom_off_barrage_marker_desc = "Marks targets of Crystalline Barrage with {rt1}{rt2}{rt3}{rt4}{rt5}, requires promoted or leader."
+	L.custom_off_barrage_marker = "Маркировка Кристаллического залпа"
+	L.custom_off_barrage_marker_desc = "Отмечать людей с Кристаллическим залпом {rt1}{rt2}{rt3}{rt4}{rt5}, требуется быть помощником или лидером."
 
-	--L.adds_desc = "Timers for when new adds enter the fight."
+	L.adds_desc = "Таймеры, когда новые помощники вступят в бой."
 end
 
 L = BigWigs:NewBossLocale("Brackenspore", "ruRU")
@@ -35,8 +35,8 @@ end
 
 L = BigWigs:NewBossLocale("Twin Ogron", "ruRU")
 if L then
-	--L.custom_off_volatility_marker = "Arcane Volatility marker"
-	--L.custom_off_volatility_marker_desc = "Marks targets of Arcane Volatility with {rt1}{rt2}{rt3}{rt4}, requires promoted or leader."
+	L.custom_off_volatility_marker = "Маркировка Непостоянной тайной магии"
+	L.custom_off_volatility_marker_desc = "Отмечать людей с Непостоянной тайной магии {rt1}{rt2}{rt3}{rt4}, требуется быть помощником или лидером."
 end
 
 L = BigWigs:NewBossLocale("Ko'ragh", "ruRU")
@@ -46,12 +46,12 @@ if L then
 	--L.suppression_field_trigger3 = "I will crush you!"
 	--L.suppression_field_trigger4 = "Silence!"
 
-	--L.fire_bar = "Everyone Explodes!"
+	L.fire_bar = "Все взорвутся!"
 end
 
 L = BigWigs:NewBossLocale("Imperator Mar'gok", "ruRU")
 if L then
-	--L.custom_off_fixate_marker = "Fixate Marker"
-	--L.custom_off_fixate_marker_desc = "Mark Gorian Warmage's Fixate targets with {rt1}{rt2}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r"
+	L.custom_off_fixate_marker = "Маркировка Сосредоточение внимания"
+	L.custom_off_fixate_marker_desc = "Отмечать цели Горианских боевых магов метками {rt1}{rt2}, требуется быть помощником или лидером.\n|cFFFF0000Только 1 человек в рейде должен включить эту опцию, чтобы предотвратить конфликты.|r"
 end
 
