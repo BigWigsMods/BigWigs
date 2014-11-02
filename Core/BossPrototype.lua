9f83b5c4b8f49b999fc579c1975f16ca5e6c9be9
funkydude <funkydude@users.noreply.github.com>
2014-11-02 22:40:02 +0000

    BossPrototype: tweak :IterateGroup.

--- Core/BossPrototype.lua
+++ Core/BossPrototype.lua
@@ -653,10 +653,12 @@ end
 --
 
 do
-	local raidList = {}
-	for i = 1, 40 do
-		raidList[i] = format("raid%d", i)
-	end
+	local raidList = { -- Not using a for loop because... REASONS. P.S. I love Torgue.
+		"raid1", "raid2", "raid3", "raid4", "raid5", "raid6", "raid7", "raid8", "raid9", "raid10",
+		"raid11", "raid12", "raid13", "raid14", "raid15", "raid16", "raid17", "raid18", "raid19", "raid20",
+		"raid21", "raid22", "raid23", "raid24", "raid25", "raid26", "raid27", "raid28", "raid29", "raid30",
+		"raid31", "raid32", "raid33", "raid34", "raid35", "raid36", "raid37", "raid38", "raid39", "raid40"
+	}
 	local partyList = {"player", "party1", "party2", "party3", "party4"}
 
 	local i = 0
@@ -668,9 +670,12 @@ do
 		end
 	end
 
+	local GetNumGroupMembers = GetNumGroupMembers
+	local IsInRaid = IsInRaid
 	function boss:IterateGroup()
 		i = 0
-		size = GetNumGroupMembers() > 0 and GetNumGroupMembers() or 1
+		local num = GetNumGroupMembers()
+		size = num > 0 and num or 1
 		return iter, (IsInRaid() and raidList or partyList)
 	end
 end
