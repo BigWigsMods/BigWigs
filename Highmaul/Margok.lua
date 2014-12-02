f0452d2afed023ad505e377485bc08be63a9b164
Kyle Buller <bullerk@gmail.com>
2014-12-02 21:46:04 +0000

    Margok: add a say for Mark of Chaos: Fortification and add mythic phase warnings

--- Highmaul/Margok.lua
+++ Highmaul/Margok.lua
@@ -37,14 +37,14 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		{159515, "TANK"}, 156238, 156467, 156471, {158605, "ICON", "PROXIMITY", "FLASH"}, 157349,
+		{159515, "TANK"}, 156238, 156467, 156471, {158605, "ICON", "PROXIMITY", "FLASH", "SAY"}, 157349,
 		"volatile_anomaly",
 		{157801, "DISPEL"}, {157763, "FLASH"}, "custom_off_fixate_marker",
 		{158553, "TANK"}, {158563, "TANK"},
 		"stages", "bosskill"
 	}, {
 		[159515] = mod.displayName,
-		["volatile_anomaly"] = CL.intermission,
+		["volatile_anomaly"] = "intermission",
 		[157801] = -9922,
 		[158553] = -9921,
 		["stages"] = "general",
@@ -89,7 +89,12 @@ end
 
 function mod:UNIT_HEALTH_FREQUENT(unit)
 	local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
-	if (phase == 1 and hp < 90) or (phase == 2 and hp < 60) or (phase == 3 and hp < 30) then
+	if self:Mythic() then
+		if (phase == 1 and hp < 71) or (phase == 2 and hp < 38) then -- phases at 66% and 33%
+			self:Message("stages", "Neutral", "Info", CL.soon:format(CL.phase:format(phase+1)), false)
+			self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", "boss1")
+		end
+	elseif (phase == 1 and hp < 90) or (phase == 2 and hp < 60) or (phase == 3 and hp < 30) then -- phases at 85%, 55%, and 25%
 		self:Message("stages", "Neutral", "Info", CL.soon:format(CL.phase:format(phase+1)), false)
 		self:UnregisterUnitEvent("UNIT_HEALTH_FREQUENT", "boss1")
 	end
@@ -116,7 +121,9 @@ function mod:Phases(unit, spellName, _, _, spellId)
 		self:CDBar(158605, 28) -- Arcane Aberration
 		self:CDBar(156471, 38) -- Mark of Chaos
 		self:CDBar(157349, 48) -- Force Nova
-		self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+		if spellId ~= 157964 then -- Replication is the last phase
+			self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
+		end
 	end
 end
 
@@ -133,7 +140,7 @@ end
 
 function mod:ArcaneWrathApplied(args)
 	-- custom marking? replication makes it geometric so after three jumps we'd be capped
-	-- also might be worth doing a proximity for the third person on (50->25->13)
+	-- also might be worth doing a proximity at some point as the jump range lowers (200->100->50->25->13)
 	if self:Me(args.destGUID) then
 		self:Message(156238, "Personal", "Alarm", CL.you:format(self:SpellName(156238)))
 	end
@@ -163,6 +170,9 @@ function mod:MarkOfChaosApplied(args)
 	if self:Me(args.destGUID) then
 		self:Flash(158605)
 		self:OpenProximity(158605, 35)
+		if args.spellId == 164178 then -- Fortification (you're rooted)
+			self:Say(158605)
+		end
 	else
 		if args.spellId == 164178 and self:Range(args.destName) < 35 then -- Fortification (target rooted)
 			self:Flash(158605)
