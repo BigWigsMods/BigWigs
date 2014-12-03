550aef3a59530cc3dd9efce7d6658499fbf60333
jongt23 <jongt23@gmail.com>
2014-12-03 16:31:09 +0000

    esES update

--- Highmaul/Locales/esES.lua
+++ Highmaul/Locales/esES.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Kargath Bladefist", "esES")
 if not L then return end
 if L then
-	--L.blade_dance_bar = "Dancing"
+	L.blade_dance_bar = "Danza"
 end
 
 L = BigWigs:NewBossLocale("The Butcher", "esES")
@@ -18,25 +18,25 @@ if L then
 	--L.berserker_trigger2 = "Kral'ach" --Kral'ach.... The darkness speaks.... A VOICE!
 	--L.berserker_trigger3 = "Graaagh!" --Graaagh! KAHL...  AHK... RAAHHHH!
 
-	--L.tectus = "Tectus"
-	--L.shard = "Shard"
-	--L.motes = "Motes"
+	L.tectus = "Tectus"
+	L.shard = "Esquirla"
+	L.motes = "Motas"
 
-	--L.custom_off_barrage_marker = "Crystalline Barrage marker"
-	--L.custom_off_barrage_marker_desc = "Marks targets of Crystalline Barrage with {rt1}{rt2}{rt3}{rt4}{rt5}, requires promoted or leader."
+	L.custom_off_barrage_marker = "Marcador de Tromba cristalina"
+	L.custom_off_barrage_marker_desc = "Marca los jugadores afectados por Tromba cristalina con {rt1}{rt2}{rt3}{rt4}{rt5}, requiere ayudante o líder."
 
-	--L.adds_desc = "Timers for when new adds enter the fight."
+	L.adds_desc = "Temporizadores para cuando los nuevos adds entren al combate."
 end
 
 L = BigWigs:NewBossLocale("Brackenspore", "esES")
 if L then
-	--L.creeping_moss_heal = "Creeping Moss under BOSS (healing)"
+	L.creeping_moss_heal = "Musgo trepador debajo del jefe (sanando)"
 end
 
 L = BigWigs:NewBossLocale("Twin Ogron", "esES")
 if L then
-	--L.custom_off_volatility_marker = "Arcane Volatility marker"
-	--L.custom_off_volatility_marker_desc = "Marks targets of Arcane Volatility with {rt1}{rt2}{rt3}{rt4}, requires promoted or leader."
+	L.custom_off_volatility_marker = "Marcador de Volatilidad Arcana"
+	L.custom_off_volatility_marker_desc = "Marca los jugadores afectados por Volatilidad Arcana con {rt1}{rt2}{rt3}{rt4}, requiere ayudante o líder."
 end
 
 L = BigWigs:NewBossLocale("Ko'ragh", "esES")
@@ -46,15 +46,15 @@ if L then
 	--L.suppression_field_trigger3 = "I will crush you!"
 	--L.suppression_field_trigger4 = "Silence!"
 
-	--L.fire_bar = "Everyone Explodes!"
+	L.fire_bar = "Todo el mundo explota!"
 
-	--L.custom_off_fel_marker = "Expel Magic: Fel Marker"
-	--L.custom_off_fel_marker_desc = "Mark Expel Magic: Fel targets with {rt1}{rt2}{rt3}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r"
+	L.custom_off_fel_marker = "Marcador de Expulsar magia: Vil"
+	L.custom_off_fel_marker_desc = "Marca los jugadores afectados por Expulsar magia: Vil con {rt1}{rt2}{rt3}, requiere ayudante o líder.\n|cFFFF0000Sólo 1 persona en la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r"
 end
 
 L = BigWigs:NewBossLocale("Imperator Mar'gok", "esES")
 if L then
-	--L.custom_off_fixate_marker = "Fixate Marker"
-	--L.custom_off_fixate_marker_desc = "Mark Gorian Warmage's Fixate targets with {rt1}{rt2}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r"
+	L.custom_off_fixate_marker = "Marcador de Fijar"
+	L.custom_off_fixate_marker_desc = "Marca los objetivos de los Magos de guerra gorianos Fijar con {rt1}{rt2}, requiere ayudante o líder.\n|cFFFF0000Sólo 1 persona en la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r"
 end
 
