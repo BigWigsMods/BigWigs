f1f8d15f59fd89b189daef0e254d8677bf3d8997
funkydude <funkydude@users.noreply.github.com>
2014-12-07 15:14:19 +0000

    fix esMX

--- BlackrockFoundry/Locales/esES.lua
+++ BlackrockFoundry/Locales/esES.lua
@@ -1,10 +1,10 @@
-local L = BigWigs:NewBossLocale("Gruul", "esES")
+local L = BigWigs:NewBossLocale("Gruul", "esES") or BigWigs:NewBossLocale("Gruul", "esMX")
 if not L then return end
 if L then
 
 end
 
-L = BigWigs:NewBossLocale("Oregorger", "esES")
+L = BigWigs:NewBossLocale("Oregorger", "esES") or BigWigs:NewBossLocale("Oregorger", "esMX")
 if L then
 	--L.berserk_trigger = "Oregorger has gone insane from hunger!"
 
@@ -14,29 +14,29 @@ if L then
 	--L.hunger_drive_power = "%dx %s - %d ore to go!"
 end
 
-L = BigWigs:NewBossLocale("The Blast Furnace", "esES")
+L = BigWigs:NewBossLocale("The Blast Furnace", "esES") or BigWigs:NewBossLocale("The Blast Furnace", "esMX")
 if L then
 	--L.heat_increased_message = "Heat increased! Blast every %ss"
 end
 
-L = BigWigs:NewBossLocale("Hans'gar and Franzok", "esES")
+L = BigWigs:NewBossLocale("Hans'gar and Franzok", "esES") or BigWigs:NewBossLocale("Hans'gar and Franzok", "esMX")
 if L then
 
 end
 
-L = BigWigs:NewBossLocale("Flamebender Ka'graz", "esES")
+L = BigWigs:NewBossLocale("Flamebender Ka'graz", "esES") or BigWigs:NewBossLocale("Flamebender Ka'graz", "esMX")
 if L then
 	--L.molten_torrent_self = "Molten Torrent on you"
 	--L.molten_torrent_self_desc = "Special countdown when Molten Torrent is on you."
 	--L.molten_torrent_self_bar = "You explode!"
 end
 
-L = BigWigs:NewBossLocale("Kromog", "esES")
+L = BigWigs:NewBossLocale("Kromog", "esES") or BigWigs:NewBossLocale("Kromog", "esMX")
 if L then
 
 end
 
-L = BigWigs:NewBossLocale("Beastlord Darmac", "esES")
+L = BigWigs:NewBossLocale("Beastlord Darmac", "esES") or BigWigs:NewBossLocale("Beastlord Darmac", "esMX")
 if L then
 	--L.next_mount = "Mounting soon!"
 
@@ -47,7 +47,7 @@ if L then
 	--L.custom_off_conflag_marker_desc = "Mark conflagration targets with {rt1}{rt2}{rt3}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r"
 end
 
-L = BigWigs:NewBossLocale("Operator Thogar", "esES")
+L = BigWigs:NewBossLocale("Operator Thogar", "esES") or BigWigs:NewBossLocale("Operator Thogar", "esMX")
 if L then
 	--L.cauterizing_bolt_message = "Your focus is casting Cauterizing Bolt!"
 
@@ -62,7 +62,7 @@ if L then
 	--L.random = "Random trains"
 end
 
-L = BigWigs:NewBossLocale("The Iron Maidens", "esES")
+L = BigWigs:NewBossLocale("The Iron Maidens", "esES") or BigWigs:NewBossLocale("The Iron Maidens", "esMX")
 if L then
 	--L.ship_trigger = "prepares to man the Dreadnaught's Main Cannon!"
 
@@ -74,7 +74,7 @@ if L then
 	--L.power_message = "%d Iron Fury!"
 end
 
-L = BigWigs:NewBossLocale("Blackhand", "esES")
+L = BigWigs:NewBossLocale("Blackhand", "esES") or BigWigs:NewBossLocale("Blackhand", "esMX")
 if L then
 	--L.custom_off_markedfordeath_marker = "Marked for Death marker"
 	--L.custom_off_markedfordeath_marker_desc = "Mark Marked for Death targets with {rt1}{rt2}, requires promoted or leader."
--- Highmaul/Locales/esES.lua
+++ Highmaul/Locales/esES.lua
@@ -1,15 +1,15 @@
-local L = BigWigs:NewBossLocale("Kargath Bladefist", "esES")
+local L = BigWigs:NewBossLocale("Kargath Bladefist", "esES") or BigWigs:NewBossLocale("Kargath Bladefist", "esMX")
 if not L then return end
 if L then
 	L.blade_dance_bar = "Danza"
 end
 
-L = BigWigs:NewBossLocale("The Butcher", "esES")
+L = BigWigs:NewBossLocale("The Butcher", "esES") or BigWigs:NewBossLocale("The Butcher", "esMX")
 if L then
 	--L.adds_multiple = "Adds x%d"
 end
 
-L = BigWigs:NewBossLocale("Tectus", "esES")
+L = BigWigs:NewBossLocale("Tectus", "esES") or BigWigs:NewBossLocale("Tectus", "esMX")
 if L then
 	L.earthwarper_trigger1 = "Yjj'rmr" -- Yjj'rmr... Xzzolos...
 	L.earthwarper_trigger2 = "Sí, Tectus" -- Sí, Tectus. Es... la voluntad... de nuestro amo...
@@ -28,19 +28,19 @@ if L then
 	L.adds_desc = "Temporizadores para cuando los nuevos adds entren al combate."
 end
 
-L = BigWigs:NewBossLocale("Brackenspore", "esES")
+L = BigWigs:NewBossLocale("Brackenspore", "esES") or BigWigs:NewBossLocale("Brackenspore", "esMX")
 if L then
 	L.creeping_moss_boss_heal = "Musgo debajo del jefe (sanando)"
 	L.creeping_moss_add_heal = "Musgo debajo del ADD GRANDE (sanando)"
 end
 
-L = BigWigs:NewBossLocale("Twin Ogron", "esES")
+L = BigWigs:NewBossLocale("Twin Ogron", "esES") or BigWigs:NewBossLocale("Twin Ogron", "esMX")
 if L then
 	L.custom_off_volatility_marker = "Marcador de Volatilidad Arcana"
 	L.custom_off_volatility_marker_desc = "Marca los jugadores afectados por Volatilidad Arcana con {rt1}{rt2}{rt3}{rt4}, requiere ayudante o líder."
 end
 
-L = BigWigs:NewBossLocale("Ko'ragh", "esES")
+L = BigWigs:NewBossLocale("Ko'ragh", "esES") or BigWigs:NewBossLocale("Ko'ragh", "esMX")
 if L then
 	L.suppression_field_trigger1 = "¡Callad!"
 	L.suppression_field_trigger2 = "¡Os partiré en dos!"
@@ -53,7 +53,7 @@ if L then
 	L.custom_off_fel_marker_desc = "Marca los jugadores afectados por Expulsar magia: Vil con {rt1}{rt2}{rt3}, requiere ayudante o líder.\n|cFFFF0000Sólo 1 persona en la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r"
 end
 
-L = BigWigs:NewBossLocale("Imperator Mar'gok", "esES")
+L = BigWigs:NewBossLocale("Imperator Mar'gok", "esES") or BigWigs:NewBossLocale("Imperator Mar'gok", "esMX")
 if L then
 	L.custom_off_fixate_marker = "Marcador de Fijar"
 	L.custom_off_fixate_marker_desc = "Marca los objetivos de los Magos de guerra gorianos Fijar con {rt1}{rt2}, requiere ayudante o líder.\n|cFFFF0000Sólo 1 persona en la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r"
