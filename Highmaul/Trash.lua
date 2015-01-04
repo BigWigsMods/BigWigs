0fd55653a1d35b24e388b80df38fe31ac8129faa
funkydude <funkydude@users.noreply.github.com>
2015-01-04 22:06:15 +0000

    Trash: add wild flames warning

--- Highmaul/Trash.lua
+++ Highmaul/Trash.lua
@@ -8,6 +8,7 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	86072, -- Oro
+	-- Wild Flames mob
 )
 
 --------------------------------------------------------------------------------
@@ -32,14 +33,20 @@ L = mod:GetLocale()
 function mod:GetOptions()
 	return {
 		{172066, "FLASH", "PROXIMITY", "SAY"}, -- Radiating Poison
+		{173827, "FLASH"}, -- Wild Flames
 	}, {
 		[172066] = L.oro,
+		--[173827] = L.oro,
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "RadiatingPoison", 172066)
 	self:Log("SPELL_AURA_REMOVED", "RadiatingPoisonRemoved", 172066)
+
+	self:Log("SPELL_AURA_APPLIED", "WildFlames", 173827)
+	self:Log("SPELL_PERIODIC_DAMAGE", "WildFlames", 173827)
+	self:Log("SPELL_PERIODIC_MISSED", "WildFlames", 173827)
 
 	self:Death("DisableOnCombatExit", 86072)
 end
@@ -65,6 +72,13 @@ function mod:RadiatingPoisonRemoved(args)
 	self:StopBar(args.spellId, args.destName)
 	if self:Me(args.destGUID) then
 		self:CloseProximity(args.spellId)
+	end
+end
+
+function mod:WildFlames(args)
+	if self:Me(args.destGUID) then
+		self:Flash(args.spellId)
+		self:Message(args.spellId, "Urgent", "Alert", CL.underyou:format(args.destName))
 	end
 end
 
