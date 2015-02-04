1c8aa4920a5dbf14175929d6fca238a638fe3897
funkydude <funkydude@users.noreply.github.com>
2015-02-04 15:13:29 +0000

    Rukhmar: Fix spell ids.

--- Draenor/Rukhmar.lua
+++ Draenor/Rukhmar.lua
@@ -25,7 +25,10 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		{167615, "TANK"}, 167687, 167650, "bosskill"
+		{167615, "TANK"}, -- Pierce Armor
+		167679, -- Solar Breath
+		167647, -- Loose Quills
+		"bosskill"
 	}
 end
 
@@ -33,10 +36,11 @@ function mod:OnBossEnable()
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
 
-	self:Log("SPELL_AURA_APPLIED", "PierceArmor", 167615)
-	self:Log("SPELL_AURA_APPLIED_DOSE", "PierceArmor", 167615)
-	self:Log("SPELL_CAST_START", "SolarBreath", 167687)
-	self:Log("SPELL_AURA_APPLIED", "LooseQuills", 167650)
+	self:Log("SPELL_AURA_APPLIED", "PiercedArmor", 167615)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "PiercedArmor", 167615)
+	self:Log("SPELL_CAST_START", "SolarBreath", 167679)
+	self:Log("SPELL_AURA_APPLIED", "LooseQuills", 167647)
+	self:Log("SPELL_AURA_REMOVED", "LooseQuillsOver", 167647)
 
 	self:Death("Win", 83746)
 end
@@ -49,16 +53,23 @@ end
 -- Event Handlers
 --
 
-function mod:PierceArmor(args)
+function mod:PiercedArmor(args)
 	self:StackMessage(args.spellId, args.destName, args.amount, "Attention", "Warning")
 end
 
 function mod:SolarBreath(args)
 	self:Message(args.spellId, "Urgent")
+	self:CDBar(args.spellId, 28)
 end
 
 function mod:LooseQuills(args)
-	self:Message(args.spellId, "Attention")
+	self:Message(args.spellId, "Attention", "Long")
 	self:Bar(args.spellId, 30)
+	self:StopBar(167679) -- Solar Breath
+end
+
+function mod:LooseQuillsOver(args)
+	self:Message(args.spellId, "Attention", nil, CL.over:format(args.spellName))
+	self:CDBar(167679, 24) -- Solar Breath
 end
 
