f2b21fc5241ca4f834fc35cb8c261d670b1f328c
elvador <reysptools@googlemail.com>
2015-02-08 11:55:32 +0000

    Gruul: Fix me not being awake

--- BlackrockFoundry/Gruul.lua
+++ BlackrockFoundry/Gruul.lua
@@ -69,7 +69,6 @@ function mod:OnEngage()
 	self:CDBar(155326, 21, L.first_ability, "ability_kilruk_reave") -- both!?
 	--self:CDBar(155301, 21) -- Overhead Smash
 	--self:CDBar(155326, 21) -- Petrifying Slam
-	self:Berserk(360)
 	self:Berserk(self:Normal() and 480 or 360) --Normal: 8min, Heroic: 6min
 end
 
