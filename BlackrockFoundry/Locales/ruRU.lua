3ae21bd3834fe4382f5e413b4ba1ad24c3093f6f
andaril <andaril@users.noreply.github.com>
2015-02-13 12:27:10 +0000

    ruRu update

--- BlackrockFoundry/Locales/ruRU.lua
+++ BlackrockFoundry/Locales/ruRU.lua
@@ -11,17 +11,17 @@ if L then
 	L.shard_explosion = "Подрыв Взрывчатого осколка"
 	L.shard_explosion_desc = "Отдельный увеличенный таймер для взрыва."
 
-	--L.hunger_drive_power = "%dx %s - %d ore to go!"
+	L.hunger_drive_power = "%dx %s - %d ore to go!"
 end
 
 L = BigWigs:NewBossLocale("The Blast Furnace", "ruRU")
 if L then
-	--L.custom_on_shieldsdown_marker = "Shields Down marker"
-	--L.custom_on_shieldsdown_marker_desc = "Mark a vulnerable Primal Elementalist with {rt8}, requires promoted or leader."
+	L.custom_on_shieldsdown_marker = "Метка спадения щита"
+	L.custom_on_shieldsdown_marker_desc = "Отмечать уязвимость Повелительниц изначальных стихий меткой {rt8}, требуется быть лидером или помощником."
 
-	--L.heat_increased_message = "Heat increased! Blast every %ss"
+	L.heat_increased_message = "Жар увеличен! Взрыв каждые %s сек."
 
-	--L.bombs_dropped = "Bombs dropped! (%d)"
+	L.bombs_dropped = "Бомб упало! (%d)"
 end
 
 L = BigWigs:NewBossLocale("Hans'gar and Franzok", "ruRU")
@@ -54,30 +54,30 @@ end
 
 L = BigWigs:NewBossLocale("Operator Thogar", "ruRU")
 if L then
-	-- L.custom_off_firemender_marker = "Grom'kar Firemender marker"
-	-- L.custom_off_firemender_marker_desc = "Marks Firemenders with {rt1}{rt2}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r\n|cFFADFF2FTIP: If the raid has chosen you to turn this on, quickly mousing over the mobs is the fastest way to mark them.|r"
+	L.custom_off_firemender_marker = "Метка Гром'карской повелительницы огня"
+	L.custom_off_firemender_marker_desc = "Отмечать Гром'карских повелительниц огня метками {rt1}{rt2}, требуется быть помощником или лидером.\n|cFFFF0000Только 1 человек в рейде должен включить эту опцию, чтобы предотвратить конфликты.|r\n|cFFADFF2FTIP: Если вы выбраны для этой задачи, быстро проведите указателем мыши по целям, метки сразу же поставятся.|r"
 
-	--L.trains = "Train warnings"
-	--L.trains_desc = "Shows timers and messages for each lane for when the next train is coming. Lanes are numbered from the boss to the entrace, ie, Boss 1 2 3 4 Entrance."
+	L.trains = "Предупреждения о поездах"
+	L.trains_desc = "Показывать таймеры и сообщения для каждого пути, когда прибывает новый поезд. Пути пронумерованы от босса и до выхода(Босс, 1, 2, 3, 4, выход)."
 
-	--L.lane = "Lane %s: %s"
-	--L.train = "Train"
-	--L.adds_train = "Adds train"
-	--L.big_add_train = "Big add train"
-	--L.cannon_train = "Cannon train"
-	--L.random = "Random trains"
+	L.lane = "Путь %s: %s"
+	L.train = "Поезд"
+	L.adds_train = "Поезд с мобами"
+	L.big_add_train = "Большой поезд с мобами"
+	L.cannon_train = "Поезд с пушкой"
+	L.random = "Случайные поезда"
 end
 
 L = BigWigs:NewBossLocale("The Iron Maidens", "ruRU")
 if L then
 	--L.ship_trigger = "prepares to man the Dreadnaught's Main Cannon!"
 
-	--L.ship = "Jump to Ship"
+	L.ship = "Прыжок на корабль"
 
 	L.custom_off_heartseeker_marker = "Маркировка Окровавленных пронзателей сердец"
 	L.custom_off_heartseeker_marker_desc = "На Окровавленных пронзателей сердец будут поставлены метки {rt1}{rt2}{rt3}, требуется быть помощником или лидером."
 
-	--L.power_message = "%d Iron Fury!"
+	L.power_message = "%d железной ярости!"
 end
 
 L = BigWigs:NewBossLocale("Blackhand", "ruRU")
