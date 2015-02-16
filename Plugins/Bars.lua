62a297c676411410911dbfba7f2da32aeaa956a1
Benjamin Staneck <staneck@gmail.com>
2015-02-16 18:24:51 +0000

    fix path to sound files for pull timer, we use Amy for that.

--- Plugins/Bars.lua
+++ Plugins/Bars.lua
@@ -1592,7 +1592,7 @@ do
 		elseif timeLeft < 11 then
 			plugin:SendMessage("BigWigs_Message", plugin, nil, L.pullIn:format(timeLeft), "Attention")
 			if timeLeft < 6 and BigWigs.db.profile.sound then
-				PlaySoundFile(("Interface\\AddOns\\BigWigs\\Sounds\\%d.ogg"):format(timeLeft), "Master")
+				PlaySoundFile(("Interface\\AddOns\\BigWigs\\Sounds\\Amy\\%d.ogg"):format(timeLeft), "Master")
 			end
 		end
 	end
