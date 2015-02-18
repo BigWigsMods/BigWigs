67a047b4fe642462c0e1169e569b4d8090f64b15
funkydude <funkydude@users.noreply.github.com>
2015-02-18 21:56:49 +0000

    Trash: add Fire Bomb

--- BlackrockFoundry/Trash.lua
+++ BlackrockFoundry/Trash.lua
@@ -8,6 +8,7 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	78978, -- Darkshard Gnasher
+	79208, -- Blackrock Enforcer
 	87366 -- Blast Furnace Exhaust, this isn't going to work
 )
 
@@ -23,6 +24,7 @@ mod:RegisterEnableMob(
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.gnasher = "Darkshard Gnasher"
+	L.enforcer = "Blackrock Enforcer"
 	L.furnace = "Blast Furnace Exhaust"
 end
 L = mod:GetLocale()
@@ -35,10 +37,13 @@ function mod:GetOptions()
 	return {
 		--[[ Darkshard Gnasher ]]--
 		{159632, "FLASH"}, -- Insatiable Hunger
+		--[[ Blackrock Enforcer ]]--
+		{160260, "FLASH"}, -- Fire Bomb
 		--[[ Blast Furnace Exhaust ]]--
 		{174773, "FLASH"}, -- Exhaust Fumes
 	}, {
 		[159632] = L.gnasher,
+		[160260] = L.enforcer,
 		[174773] = L.furnace,
 	}
 end
@@ -48,6 +53,10 @@ function mod:OnBossEnable()
 
 	self:Log("SPELL_AURA_APPLIED", "InsatiableHunger", 159632)
 	self:Log("SPELL_AURA_REMOVED", "InsatiableHungerRemoved", 159632)
+
+	self:Log("SPELL_AURA_APPLIED", "FireBomb", 160260)
+	self:Log("SPELL_PERIODIC_DAMAGE", "FireBomb", 160260)
+	self:Log("SPELL_PERIODIC_MISSED", "FireBomb", 160260)
 
 	self:Log("SPELL_AURA_APPLIED", "ExhaustFumes", 174773)
 	self:Log("SPELL_PERIODIC_DAMAGE", "ExhaustFumes", 174773)
@@ -72,6 +81,20 @@ function mod:InsatiableHungerRemoved(args)
 	self:StopBar(args.spellId, args.destName)
 	if self:Me(args.destGUID) then
 		self:Message(args.spellId, "Positive", nil, CL.over:format(args.spellName))
+	end
+end
+
+--[[ Blackrock Enforcer ]]--
+
+do
+	local prev = 0
+	function mod:FireBomb(args)
+		local t = GetTime()
+		if self:Me(args.destGUID) and t-prev > 1.5 then
+			prev = t
+			self:Flash(args.spellId)
+			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
+		end
 	end
 end
 
