6d32d78b6e763d989269b29456d760b74ca16307
funkydude <funkydude@users.noreply.github.com>
2015-02-22 21:02:57 +0000

    Trash: fix stray comma.

--- BlackrockFoundry/Trash.lua
+++ BlackrockFoundry/Trash.lua
@@ -9,7 +9,7 @@ mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	78978, -- Darkshard Gnasher
 	79208, -- Blackrock Enforcer
-	76906, -- Operator Thogar, alternative for Blast Furnace Exhaust
+	76906 -- Operator Thogar, alternative for Blast Furnace Exhaust
 	--87366 -- Blast Furnace Exhaust
 )
 
