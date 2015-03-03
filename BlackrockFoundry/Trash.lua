48811ab1dcaf54f61cd6dbd43c53f16549e52390
funkydude <funkydude@users.noreply.github.com>
2015-03-03 02:28:58 +0000

    Trash: add Living Blaze & Burning.

--- BlackrockFoundry/Trash.lua
+++ BlackrockFoundry/Trash.lua
@@ -10,8 +10,9 @@ mod:RegisterEnableMob(
 	87719, -- Ogron Hauler
 	78978, -- Darkshard Gnasher
 	79208, -- Blackrock Enforcer
-	76906 -- Operator Thogar, alternative for Blast Furnace Exhaust
+	76906, -- Operator Thogar, alternative for Blast Furnace Exhaust
 	--87366 -- Blast Furnace Exhaust
+	87989 -- Forgemistress Flamehand
 )
 
 --------------------------------------------------------------------------------
@@ -29,6 +30,7 @@ if L then
 	L.gnasher = "Darkshard Gnasher"
 	L.enforcer = "Blackrock Enforcer"
 	L.furnace = "Blast Furnace Exhaust"
+	L.mistress = "Forgemistress Flamehand"
 end
 L = mod:GetLocale()
 
@@ -46,17 +48,21 @@ function mod:GetOptions()
 		{160260, "FLASH"}, -- Fire Bomb
 		--[[ Blast Furnace Exhaust ]]--
 		{174773, "FLASH"}, -- Exhaust Fumes
+		--[[ Forgemistress Flamehand ]]--
+		{175583, "FLASH", "SAY", "PROXIMITY"}
+		{175594, "TANK"}
 	}, {
 		[175765] = L.hauler,
 		[159632] = L.gnasher,
 		[160260] = L.enforcer,
 		[174773] = L.furnace,
+		[175583] = L.mistress,
 	}
 end
 
 function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
-	self:Death("Disable", 87719) -- Ogron Hauler
+	self:Death("Disable", 87719, 87989) -- Ogron Hauler, Forgemistress Flamehand
 
 	self:Log("SPELL_AURA_APPLIED", "OverheadSmash", 175765)
 	self:Log("SPELL_AURA_REMOVED", "OverheadSmashRemoved", 175765)
@@ -71,6 +77,13 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "ExhaustFumes", 174773)
 	self:Log("SPELL_PERIODIC_DAMAGE", "ExhaustFumes", 174773)
 	self:Log("SPELL_PERIODIC_MISSED", "ExhaustFumes", 174773)
+
+	self:Log("SPELL_CAST_START", "LivingBlazeCast", 175583)
+	self:Log("SPELL_AURA_APPLIED", "LivingBlaze", 175583)
+	self:Log("SPELL_AURA_REMOVED", "LivingBlazeRemoved", 175583)
+
+	self:Log("SPELL_AURA_APPLIED_DOSE", "Burning", 175594)
+	self:Log("SPELL_AURA_REMOVED", "BurningRemoved", 175594)
 end
 
 --------------------------------------------------------------------------------
@@ -134,6 +147,44 @@ do
 			self:Flash(args.spellId)
 			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
 		end
+	end
+end
+
+--[[ Forgemistress Flamehand ]]--
+
+function mod:LivingBlazeCast(args)
+	self:Bar(args.spellId, 23)
+end
+
+function mod:LivingBlaze(args)
+	self:TargetBar(args.spellId, 10, args.destName)
+	if self:Me(args.destGUID) then
+		if not self:LFR() then
+			self:Say(args.spellId)
+		end
+		self:OpenProximity(args.spellId, 6)
+		self:Flash(args.spellId)
+		self:TargetMessage(args.spellId, args.destName, "Personal", "Alarm")
+	end
+end
+
+function mod:LivingBlazeRemoved(args)
+	self:StopBar(args.spellId, args.destName)
+	if self:Me(args.destGUID) then
+		self:Message(args.spellId, "Positive", nil, CL.over:format(args.spellName))
+		self:CloseProximity(args.spellId)
+	end
+end
+
+function mod:Burning(args)
+	if args.amount % 2 == 0 then
+		self:StackMessage(args.spellId, args.destName, args.amount, "Urgent", args.amount > 6 and "Warning")
+	end
+end
+
+function mod:BurningRemoved(args)
+	if self:Me(args.destGUID) then
+		self:Message(args.spellId, "Positive", "Warning", CL.over:format(args.spellName))
 	end
 end
 
