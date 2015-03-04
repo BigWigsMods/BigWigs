70bf19e39a9871bb355f1259e95d62cb1fa1ff55
funkydude <funkydude@users.noreply.github.com>
2015-03-04 21:55:59 +0000

    Trash: Add electrical storm and remove some duplicate code.

--- BlackrockFoundry/Trash.lua
+++ BlackrockFoundry/Trash.lua
@@ -8,6 +8,7 @@ if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
 	87719, -- Ogron Hauler
+	80423, -- Thunderlord Beast-Tender
 	78978, -- Darkshard Gnasher
 	79208, -- Blackrock Enforcer
 	76906, -- Operator Thogar, alternative for Blast Furnace Exhaust (87366)
@@ -21,6 +22,7 @@ mod:RegisterEnableMob(
 local L = mod:NewLocale("enUS", true)
 if L then
 	L.hauler = "Ogron Hauler"
+	L.beasttender = "Thunderlord Beast-Tender"
 	L.gnasher = "Darkshard Gnasher"
 	L.enforcer = "Blackrock Enforcer"
 	L.furnace = "Blast Furnace Exhaust"
@@ -36,6 +38,8 @@ function mod:GetOptions()
 	return {
 		--[[ Ogron Hauler ]]--
 		{175765, "TANK"}, -- Overhead Smash
+		--[[ Thunderlord Beast-Tender ]]--
+		{162663, "FLASH"}, -- Electrical Storm
 		--[[ Darkshard Gnasher ]]--
 		{159632, "FLASH"}, -- Insatiable Hunger
 		--[[ Blackrock Enforcer ]]--
@@ -47,6 +51,7 @@ function mod:GetOptions()
 		{175594, "TANK"},
 	}, {
 		[175765] = L.hauler,
+		[162663] = L.beasttender,
 		[159632] = L.gnasher,
 		[160260] = L.enforcer,
 		[174773] = L.furnace,
@@ -58,19 +63,15 @@ function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
 	self:Death("Disable", 87719, 87989) -- Ogron Hauler, Forgemistress Flamehand
 
+	self:Log("SPELL_AURA_APPLIED", "BadStuffUnderYou", 162663, 160260, 174773) -- Electrical Storm, Fire Bomb, Exhaust Fumes
+	self:Log("SPELL_PERIODIC_DAMAGE", "BadStuffUnderYou", 162663, 160260, 174773)
+	self:Log("SPELL_PERIODIC_MISSED", "BadStuffUnderYou", 162663, 160260, 174773)
+
 	self:Log("SPELL_AURA_APPLIED", "OverheadSmash", 175765)
 	self:Log("SPELL_AURA_REMOVED", "OverheadSmashRemoved", 175765)
 
 	self:Log("SPELL_AURA_APPLIED", "InsatiableHunger", 159632)
 	self:Log("SPELL_AURA_REMOVED", "InsatiableHungerRemoved", 159632)
-
-	self:Log("SPELL_AURA_APPLIED", "FireBomb", 160260)
-	self:Log("SPELL_PERIODIC_DAMAGE", "FireBomb", 160260)
-	self:Log("SPELL_PERIODIC_MISSED", "FireBomb", 160260)
-
-	self:Log("SPELL_AURA_APPLIED", "ExhaustFumes", 174773)
-	self:Log("SPELL_PERIODIC_DAMAGE", "ExhaustFumes", 174773)
-	self:Log("SPELL_PERIODIC_MISSED", "ExhaustFumes", 174773)
 
 	self:Log("SPELL_CAST_START", "LivingBlazeCast", 175583)
 	self:Log("SPELL_AURA_APPLIED", "LivingBlaze", 175583)
@@ -83,6 +84,18 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+do
+	local prev = 0
+	function mod:BadStuffUnderYou(args)
+		local t = GetTime()
+		if self:Me(args.destGUID) and t-prev > 1.5 then
+			prev = t
+			self:Flash(args.spellId)
+			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
+		end
+	end
+end
 
 --[[ Ogron Hauler ]]--
 
@@ -113,34 +126,6 @@ function mod:InsatiableHungerRemoved(args)
 	self:StopBar(args.spellId, args.destName)
 	if self:Me(args.destGUID) then
 		self:Message(args.spellId, "Positive", nil, CL.over:format(args.spellName))
-	end
-end
-
---[[ Blackrock Enforcer ]]--
-
-do
-	local prev = 0
-	function mod:FireBomb(args)
-		local t = GetTime()
-		if self:Me(args.destGUID) and t-prev > 1.5 then
-			prev = t
-			self:Flash(args.spellId)
-			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
-		end
-	end
-end
-
---[[ Blast Furnace Exhaust ]]--
-
-do
-	local prev = 0
-	function mod:ExhaustFumes(args) -- Dem trolls yo
-		local t = GetTime()
-		if self:Me(args.destGUID) and t-prev > 1.5 then
-			prev = t
-			self:Flash(args.spellId)
-			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
-		end
 	end
 end
 
