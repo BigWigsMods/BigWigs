07881010cf8bfdcfeb073215e4a1a3ccb810fe79
funkydude <funkydude@users.noreply.github.com>
2015-03-09 01:57:41 +0000

    Trash: Add Spinning Blade & Lumbering Strength.

--- BlackrockFoundry/Trash.lua
+++ BlackrockFoundry/Trash.lua
@@ -7,8 +7,10 @@ local mod, CL = BigWigs:NewBoss("Blackrock Foundry Trash", 988)
 if not mod then return end
 mod.displayName = CL.trash
 mod:RegisterEnableMob(
+	87411, -- Workshop Guardian
 	87719, -- Ogron Hauler
 	80423, -- Thunderlord Beast-Tender
+	87780, -- Slagshop Brute
 	78978, -- Darkshard Gnasher
 	79208, -- Blackrock Enforcer
 	80708, -- Iron Taskmaster
@@ -22,13 +24,19 @@ mod:RegisterEnableMob(
 
 local L = mod:NewLocale("enUS", true)
 if L then
+	L.guardian = "Workshop Guardian"
 	L.hauler = "Ogron Hauler"
 	L.beasttender = "Thunderlord Beast-Tender"
+	L.brute = "Slagshop Brute"
 	L.gnasher = "Darkshard Gnasher"
 	L.enforcer = "Blackrock Enforcer"
 	L.taskmaster = "Iron Taskmaster"
 	L.furnace = "Blast Furnace Exhaust"
 	L.mistress = "Forgemistress Flamehand"
+
+	L.furnace_msg1 = "Hmm, kinda toasty isn't it?"
+	L.furnace_msg2 = "It's marshmallow time!"
+	L.furnace_msg3 = "This can't be good..."
 end
 L = mod:GetLocale()
 
@@ -38,24 +46,31 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
+		--[[ Workshop Guardian ]]--
+		{175643, "FLASH"}, -- Spinning Blade
 		--[[ Ogron Hauler ]]--
 		{175765, "TANK"}, -- Overhead Smash
 		--[[ Thunderlord Beast-Tender ]]--
 		{162663, "FLASH"}, -- Electrical Storm
+		--[[ Slagshop Brute ]]--
+		{175993, "FLASH"}, -- Lumbering Strength
 		--[[ Darkshard Gnasher ]]--
 		{159632, "FLASH"}, -- Insatiable Hunger
 		--[[ Blackrock Enforcer ]]--
 		{160260, "FLASH"}, -- Fire Bomb
 		--[[ Iron Taskmaster ]]--
 		163121, -- Held to Task
+		177806, -- Furnace Flame (via furnace next to Taskmaster)
 		--[[ Blast Furnace Exhaust ]]--
 		{174773, "FLASH"}, -- Exhaust Fumes
 		--[[ Forgemistress Flamehand ]]--
 		{175583, "FLASH", "SAY", "PROXIMITY"},
 		{175594, "TANK"},
 	}, {
+		[175643] = L.guardian,
 		[175765] = L.hauler,
 		[162663] = L.beasttender,
+		[175993] = L.brute,
 		[159632] = L.gnasher,
 		[160260] = L.enforcer,
 		[163121] = L.taskmaster,
@@ -66,19 +81,22 @@ end
 
 function mod:OnBossEnable()
 	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
-	self:Death("Disable", 87719, 87989) -- Ogron Hauler, Forgemistress Flamehand
+	self:Death("Disable", 87989) -- Forgemistress Flamehand
 
-	self:Log("SPELL_AURA_APPLIED", "BadStuffUnderYou", 162663, 160260, 174773) -- Electrical Storm, Fire Bomb, Exhaust Fumes
-	self:Log("SPELL_PERIODIC_DAMAGE", "BadStuffUnderYou", 162663, 160260, 174773)
-	self:Log("SPELL_PERIODIC_MISSED", "BadStuffUnderYou", 162663, 160260, 174773)
+	self:Log("SPELL_AURA_APPLIED", "BadStuffUnderYou", 175643, 162663, 160260, 174773) -- Spinning Blade, Electrical Storm, Fire Bomb, Exhaust Fumes
+	self:Log("SPELL_PERIODIC_DAMAGE", "BadStuffUnderYou", 175643, 162663, 160260, 174773)
+	self:Log("SPELL_PERIODIC_MISSED", "BadStuffUnderYou", 175643, 162663, 160260, 174773)
 
 	self:Log("SPELL_AURA_APPLIED", "OverheadSmash", 175765)
 	self:Log("SPELL_AURA_REMOVED", "OverheadSmashRemoved", 175765)
+
+	self:Log("SPELL_AURA_APPLIED", "LumberingStrength", 175993)
 
 	self:Log("SPELL_AURA_APPLIED", "InsatiableHunger", 159632)
 	self:Log("SPELL_AURA_REMOVED", "InsatiableHungerRemoved", 159632)
 
 	self:Log("SPELL_AURA_APPLIED", "HeldToTask", 163121)
+	self:Log("SPELL_AURA_APPLIED", "FurnaceFlameFun", 177806)
 
 	self:Log("SPELL_CAST_START", "LivingBlazeCast", 175583)
 	self:Log("SPELL_AURA_APPLIED", "LivingBlaze", 175583)
@@ -119,6 +137,21 @@ function mod:OverheadSmashRemoved(args)
 	end
 end
 
+--[[ Slagshop Brute ]]--
+
+do
+	local function printTarget(self, player, guid)
+		if self:Me(guid) then -- Only if it's targetting you
+			self:Message(175993, "Important", "Warning")
+			self:Bar(175993, 8)
+			self:Flash(175993)
+		end
+	end
+	function mod:LumberingStrength(args)
+		self:GetUnitTarget(printTarget, 0, args.destGUID)
+	end
+end
+
 --[[ Darkshard Gnasher ]]--
 
 function mod:InsatiableHunger(args)
@@ -140,9 +173,15 @@ end
 
 function mod:HeldToTask(args)
 	if self:Dispeller("enrage", true) then
-		self:TargetMessage(args.spellId, args.destName, "Urgent", "Warning")
+		self:TargetMessage(args.spellId, args.destName, "Urgent", "Warning", nil, nil, true)
 	else
 		self:TargetMessage(args.spellId, args.destName, "Attention")
+	end
+end
+
+function mod:FurnaceFlameFun(args)
+	if self:Me(args.destGUID) then
+		self:Message(args.spellId, "Positive", nil, L[("furnace_msg%d"):format(random(1,3))])
 	end
 end
 
