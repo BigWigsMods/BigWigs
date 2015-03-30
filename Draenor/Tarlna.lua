fddff0c952187ad034d721b7788ca0dd7002085a
funkydude <funkydude@users.noreply.github.com>
2015-03-30 20:04:01 +0000

    Tarlna: Add Noxious Spit warning, tweak Genesis.

--- Draenor/Tarlna.lua
+++ Draenor/Tarlna.lua
@@ -19,14 +19,19 @@ function mod:GetOptions()
 		175979, -- Genesis
 		176013, -- Grow Untamed Mandragora
 		{176001, "DISPEL"} -- Savage Vines
+		{176037, "FLASH"}, -- Noxious Spit
 	}
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "ColossalBlow", 175973)
-	self:Log("SPELL_CAST_START", "Genesis", 175979, 169613) -- XXX need to verify only 169613 is used now
+	self:Log("SPELL_CAST_START", "Genesis", 175979, 169613)
 	self:Log("SPELL_CAST_SUCCESS", "GrowMandragora", 176013)
 	self:Log("SPELL_CAST_SUCCESS", "SavageVines", 176001)
+
+	self:Log("SPELL_AURA_APPLIED", "NoxiousSpit", 176037)
+	self:Log("SPELL_PERIODIC_DAMAGE", "NoxiousSpit", 176037)
+	self:Log("SPELL_PERIODIC_MISSED", "NoxiousSpit", 176037)
 
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
@@ -34,7 +39,7 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
-	--
+	self:CDBar(175979, 25)
 end
 
 --------------------------------------------------------------------------------
@@ -47,16 +52,29 @@ end
 
 function mod:Genesis(args)
 	self:Message(175979, "Attention", "Long")
-	self:Bar(175979, 17, CL.cast:format(args.spellName))
+	self:Bar(175979, 14, CL.cast:format(args.spellName)) -- 2 sec cast, 12 sec channel
+	self:CDBar(175979, 45)
 end
 
 function mod:GrowMandragora(args)
-	self:Message(args.spellId, "Urgent", nil, CL.big_add)
+	self:Message(args.spellId, "Urgent", nil, CL.spawning:format(CL.big_add))
 end
 
 function mod:SavageVines(args)
 	if self:Dispeller("magic", nil, args.spellId) then
 		self:Message(args.spellId, "Personal", "Alert")
+	end
+end
+
+do
+	local prev = 0
+	function mod:NoxiousSpit(args)
+		local t = GetTime()
+		if self:Me(args.destGUID) and t-prev > 1.5 then
+			prev = t
+			self:Flash(args.spellId)
+			self:Message(args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
+		end
 	end
 end
 
