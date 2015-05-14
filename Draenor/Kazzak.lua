334efdb62947969990e3e85edcc039c89c7ab4bf
funkydude <funkydude@users.noreply.github.com>
2015-05-14 17:00:16 +0000

    Kazzak: add Mark of Kazzak.

--- Draenor/Kazzak.lua
+++ Draenor/Kazzak.lua
@@ -17,6 +17,7 @@ function mod:GetOptions()
 	return {
 		187664, -- Fel Breath
 		187466, -- Supreme Doom
+		{187668, "SAY", "PROXIMITY"}, -- Mark of Kazzak
 	}
 end
 
@@ -24,6 +25,8 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "FelBreath", 187664)
 	self:Log("SPELL_AURA_REMOVED", "FelBreathRemoved", 187664)
 	self:Log("SPELL_CAST_START", "SupremeDoom", 187466)
+	self:Log("SPELL_AURA_APPLIED", "MarkOfKazzak", 187668)
+	self:Log("SPELL_AURA_REMOVED", "MarkOfKazzakRemoved", 187668)
 
 	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
 	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
@@ -54,5 +57,21 @@ end
 
 function mod:SupremeDoom(args)
 	self:Message(args.spellId, "Important", "Info", CL.incoming:format(args.spellName))
+end
+
+function mod:MarkOfKazzak(args)
+	if self:Me(args.destGUID) then
+		self:TargetMessage(args.spellId, args.destName, "Personal", "Alarm")
+		self:TargetBar(args.spellId, 15, args.destName)
+		self:Say(args.spellId)
+		self:OpenProximity(args.spellId, 8)
+	end
+end
+
+function mod:MarkOfKazzakRemoved(args)
+	if self:Me(args.destGUID) then
+		self:StopBar(args.spellName, args.destName)
+		self:CloseProximity(args.spellId)
+	end
 end
 
