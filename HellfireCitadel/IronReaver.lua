e2227979ca87c3f336bd0371a8a2cf1530fa61ed
funkydude <funkydude@users.noreply.github.com>
2015-05-15 19:31:40 +0000

    IronReaver: add timers from LFR.

--- HellfireCitadel/IronReaver.lua
+++ HellfireCitadel/IronReaver.lua
@@ -20,6 +20,11 @@ mod.respawnTime = 5
 --
 
 local phase = 1
+local barrageCount = 1
+local artilleryCount = 1
+local blitzCount = 1
+local poundingCount = 1
+local firebombCount = 1
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -37,7 +42,7 @@ L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		{182280, "ICON", "PROXIMITY", "FLASH", "SAY"}, -- Artillery
+		{182280, "PROXIMITY", "FLASH", "SAY"}, -- Artillery
 		182020, -- Pounding
 		185282, -- Barrage
 		182001, -- Unstable Orb
@@ -46,10 +51,10 @@ function mod:GetOptions()
 		179889, -- Blitz
 		182055, -- Full Charge
 		181999, -- Firebomb
-		182534, -- Volatile Firebomb
-		186667, -- Burning Firebomb
-		186676, -- Reactive Firebomb
-		186652, -- Quick-Fuse Firebomb
+		--182534, -- Volatile Firebomb
+		--186667, -- Burning Firebomb
+		--186676, -- Reactive Firebomb
+		--186652, -- Quick-Fuse Firebomb
 		"proximity",
 		"berserk",
 	}
@@ -61,11 +66,10 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "Pounding", 182020)
 	self:Log("SPELL_CAST_START", "Barrage", 185282)
 	self:Log("SPELL_CAST_START", "FallingSlam", 182066)
+	self:Log("SPELL_CAST_SUCCESS", "FallingSlamSuccess", 182066)
 	self:Log("SPELL_CAST_START", "Blitz", 179889)
 	self:Log("SPELL_CAST_START", "FullCharge", 182055)
 	self:Log("SPELL_CAST_START", "Firebomb", 181999)
-
-	--self:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED") -- Firebombs are hidden casts, will they be on the boss frames is the question. One would hope so.
 
 	self:Log("SPELL_AURA_APPLIED", "UnstableOrbDamage", 182001)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "UnstableOrbDamage", 182001)
@@ -74,11 +78,19 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
+	phase = 1
+	barrageCount = 1
+	artilleryCount = 1
+	blitzCount = 1
+	poundingCount = 1
 	self:Berserk(600)
+	--self:Bar(182280, 10.3) -- Artillery APPLICATION
+	self:Bar(185282, 13.3, CL.count:format(self:SpellName(185282), barrageCount)) -- Barrage
+	self:Bar(182020, 34.4, CL.count:format(self:SpellName(182020), poundingCount)) -- Pounding
+	self:Bar(179889, 64.3) -- Blitz
 	if self:Healer() or self:Damager() == "RANGED" then
 		self:OpenProximity("proximity", 8)
 	end
-	phase = 1
 end
 
 --------------------------------------------------------------------------------
@@ -86,17 +98,22 @@ end
 --
 
 do
+	-- local timers = {9, 30, 15, 9, 24, 15} -- 15s during p2
 	local list = mod:NewTargetList()
 	function mod:Artillery(args)
 		if phase == 2 then
 			list[#list+1] = args.destName
 			if #list == 1 then
 				self:ScheduleTimer("TargetMessage", 0.3, args.spellId, list, "Urgent", "Alarm")
+				self:Bar(args.spellId, 13)
 			end
 		else
 			self:TargetMessage(args.spellId, args.destName, "Urgent", "Alarm")
 			self:TargetBar(args.spellId, 13, args.destName)
-			self:PrimaryIcon(args.spellId, args.destName)
+			-- Do we care about application timers?
+			-- Expiry is the real concern, are application timers important enough
+			-- to potentially distract/confuse from expiry timers?
+			-- Commented out for now
 		end
 		if self:Me(args.destGUID) then
 			self:OpenProximity(args.spellId, 20)
@@ -107,7 +124,6 @@ do
 end
 
 function mod:ArtilleryRemoved(args)
-	self:PrimaryIcon(args.spellId)
 	self:StopBar(args.spellName, args.destName)
 	if self:Me(args.destGUID) then
 		self:CloseProximity(args.spellId)
@@ -117,50 +133,27 @@ function mod:ArtilleryRemoved(args)
 	end
 end
 
-function mod:Pounding(args)
-	self:Message(args.spellId, "Attention", "Long", CL.casting:format(args.spellName))
-	self:Bar(args.spellId, 6)
+do
+	local timers = {0, 54, 24}
+	function mod:Pounding(args)
+		self:Message(args.spellId, "Attention", "Long", CL.count:format(args.spellName, poundingCount))
+		poundingCount = poundingCount + 1
+		if timers[poundingCount] then
+			self:Bar(args.spellId, timers[poundingCount], CL.count:format(args.spellName, poundingCount))
+		end
+	end
 end
 
-function mod:Barrage(args)
-	self:Message(args.spellId, "Attention", "Long", CL.casting:format(args.spellName))
-	self:Bar(args.spellId, 4)
+do
+	local timers = {0, 30, 12, 45}
+	function mod:Barrage(args)
+		self:Message(args.spellId, "Attention", "Long", CL.count:format(args.spellName, barrageCount))
+		barrageCount = barrageCount + 1
+		if timers[barrageCount] then
+			self:Bar(args.spellId, timers[barrageCount], CL.count:format(args.spellName, barrageCount))
+		end
+	end
 end
-
---do
---	local prev1, prev2, prev3, prev4 = 0, 0, 0, 0
---	function mod:UNIT_SPELLCAST_SUCCEEDED(event, unit, spellName, _, _, spellId)
---		if spellName == "Volatile Firebomb" or spellId == 182534 then
---			local t = GetTime()
---			if t-prev1 > 5 then
---				prev1 = t
---				self:Message(182534, "Important", "Alarm", "Volatile Firebomb exlodes in 45sec")
---				self:Bar(182534, 45, "Volatile Firebomb EXPLODES")
---			end
---		elseif spellName == "Burning Firebomb" or spellId == 186667 then
---			local t = GetTime()
---			if t-prev2 > 5 then
---				prev2 = t
---				self:Message(186667, "Important", "Alarm", "Burning Firebomb exlodes in 40sec")
---				self:Bar(186667, 40, "Burning Firebomb EXPLODES")
---			end
---		elseif spellName == "Reactive Firebomb" or spellId == 186676 then
---			local t = GetTime()
---			if t-prev3 > 5 then
---				prev3 = t
---				self:Message(186667, "Important", "Alarm", "Reactive Firebomb - Tank jump on it")
---				self:Bar(186667, 30, "Reactive Firebomb EXPLODES")
---			end
---		elseif spellName == "Quick-Fuse Firebomb" or spellId == 186652 then
---			local t = GetTime()
---			if t-prev4 > 5 then
---				prev4 = t
---				self:Message(186652, "Important", "Alarm", "Quick-Fuse Firebomb explodes in 20sec")
---				self:Bar(186652, 20, "Quick-Fuse Firebomb EXPLODES")
---			end
---		end
---	end
---end
 
 do
 	local prev = 0
@@ -185,23 +178,44 @@ do
 end
 
 function mod:FallingSlam(args)
-	phase = 1
-	self:Message(args.spellId, "Important")
+	self:Message(args.spellId, "Important", "Info")
 	self:Bar(args.spellId, 6)
+end
+
+function mod:FallingSlamSuccess(args)
+	phase = 1
+	blitzCount = 1
+	barrageCount = 1
+	poundingCount = 1
+	artilleryCount = 1
+	self:Bar(179889, 63.3) -- Blitz
+	self:Bar(185282, 12.3) -- Barrage
+	self:Bar(182020, 33.3) -- Pounding
+	--self:Bar(182280, 9.3) -- Artillery APPLICATION
 end
 
 function mod:Blitz(args)
 	self:Message(args.spellId, "Important", "Info")
-	self:Bar(args.spellId, 1.5)
+	if blitzCount == 2 then -- Blitz is casted twice each cooldown, filter out the first cast.
+		self:Bar(args.spellId, 58.3)
+	end
+	blitzCount = blitzCount + 1
 end
 
 function mod:FullCharge(args)
 	phase = 2
+	firebombCount = 1
 	self:Message(args.spellId, "Important", "Info")
 	self:Bar(args.spellId, 3)
+	--self:Bar(182280, 9) -- Artillery APPLICATION
+	self:Bar(181999, 11, CL.count:format(self:SpellName(181999), firebombCount)) -- Firebomb
 end
 
 function mod:Firebomb(args)
-	self:Message(args.spellId, "Important", "Alarm", CL.incoming:format(args.spellName))
+	self:Message(args.spellId, "Important", "Alarm", CL.count:format(args.spellName, firebombCount))
+	firebombCount = firebombCount + 1
+	if firebombCount < 4 then
+		self:Bar(args.spellId, 15, CL.count:format(args.spellName, firebombCount))
+	end
 end
 
