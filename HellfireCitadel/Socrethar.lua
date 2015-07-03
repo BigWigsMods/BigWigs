99ebc3127def7761f5b6229e6e00499ea972a765
Ironii <Ironii@users.noreply.github.com>
2015-07-03 20:56:30 +0000

    Socrethar.lua:Dominator timers

--- HellfireCitadel/Socrethar.lua
+++ HellfireCitadel/Socrethar.lua
@@ -201,7 +201,7 @@ end
 
 function mod:FelBarrier(args)
 	dominatorCount = dominatorCount + 1
-	self:CDBar(-11456, dominatorCount % 2 == 0 and 70 or 60, nil, "achievement_boss_kiljaedan") -- Sargerei Dominator
+	self:CDBar(-11456, (self:Mythic() and 130) or (dominatorCount % 2 == 0 and 70) or 60, nil, "achievement_boss_kiljaedan") -- Sargerei Dominator
 	--self:CDBar(184124, 11) -- Gift Of The Manari XXX goes out when they spawn/shield boss, doesn't seem like they cast it again
 	self:TargetMessage(args.spellId, args.destName, "Positive")
 end
