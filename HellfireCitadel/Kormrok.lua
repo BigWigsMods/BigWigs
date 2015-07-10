d5e659554989cc874cd8355fa4ee6ab7951075e1
funkydude <funkydude@users.noreply.github.com>
2015-07-10 16:00:52 +0000

    Kormrok: Fix most LFR timers.

--- HellfireCitadel/Kormrok.lua
+++ HellfireCitadel/Kormrok.lua
@@ -76,14 +76,21 @@ function mod:ShadowEnergy(args)
 	shadowCount = self:Normal() and 2 or self:Mythic() and 4 or 3
 	phase = 3
 	tankDebuffCount = 1
-
 	self:Message("stages", "Neutral", "Info", args.spellName, false)
-	self:Bar(181292, 16 * enrageMod, 181293) -- Empowered Fel Outpouring
-	self:Bar(181305, 38 * enrageMod) -- Swat
-	self:CDBar(180244, 45 * enrageMod) -- Pound
-	self:Bar(181296, 63 * enrageMod, explosiveCount > 0 and 181297) -- [Empowered] Explosive Runes
-	self:Bar(181299, 85 * enrageMod, foulCount > 0 and 181300)-- Grasping Hands / Dragging Hands
-	self:CDBar("stages", 145 * enrageMod, 180068) -- Leap
+
+	if args.spellId == 189197 then -- LFR
+		self:Bar(181292, 13) -- Fel Outpouring
+		self:Bar(181305, 31) -- Swat
+		self:CDBar(180244, 40) -- Pound
+		--self:CDBar("stages", 145 * enrageMod, 180068) -- Leap
+	else
+		self:Bar(181292, 16 * enrageMod, 181293) -- Empowered Fel Outpouring
+		self:Bar(181305, 38 * enrageMod) -- Swat
+		self:CDBar(180244, 45 * enrageMod) -- Pound
+		self:Bar(181296, 63 * enrageMod, explosiveCount > 0 and 181297) -- [Empowered] Explosive Runes
+		self:Bar(181299, 85 * enrageMod, foulCount > 0 and 181300)-- Grasping Hands / Dragging Hands
+		self:CDBar("stages", 145 * enrageMod, 180068) -- Leap
+	end
 end
 
 function mod:ExplosiveEnergy(args)
@@ -91,14 +98,21 @@ function mod:ExplosiveEnergy(args)
 	explosiveCount = self:Normal() and 2 or self:Mythic() and 4 or 3
 	phase = 1
 	tankDebuffCount = 1
-
 	self:Message("stages", "Neutral", "Info", args.spellName, false)
-	self:Bar(181296, 13 * enrageMod, 181297) -- Empowered Explosive Runes
-	self:Bar(181306, 25 * enrageMod) -- Explosive Burst
-	self:CDBar(180244, 33 * enrageMod) -- Pound
-	self:Bar(181299, 53 * enrageMod, foulCount > 0 and 181300) -- Grasping Hands / Dragging Hands
-	self:Bar(181292, 74 * enrageMod, shadowCount > 0 and 181293) -- [Empowered] Fel Outpouring
-	self:CDBar("stages", 133 * enrageMod, 180068) -- Leap
+
+	if args.spellId == 189198 then -- LFR
+		self:Bar(181296, 10) -- Explosive Runes
+		self:Bar(181306, 20) -- Explosive Burst
+		self:CDBar(180244, 30) -- Pound
+		self:CDBar("stages", 92, 180068) -- Leap
+	else
+		self:Bar(181296, 13 * enrageMod, 181297) -- Empowered Explosive Runes
+		self:Bar(181306, 25 * enrageMod) -- Explosive Burst
+		self:CDBar(180244, 33 * enrageMod) -- Pound
+		self:Bar(181299, 53 * enrageMod, foulCount > 0 and 181300) -- Grasping Hands / Dragging Hands
+		self:Bar(181292, 74 * enrageMod, shadowCount > 0 and 181293) -- [Empowered] Fel Outpouring
+		self:CDBar("stages", 133 * enrageMod, 180068) -- Leap
+	end
 end
 
 function mod:FoulEnergy(args)
@@ -106,14 +120,21 @@ function mod:FoulEnergy(args)
 	foulCount = self:Normal() and 2 or self:Mythic() and 4 or 3
 	phase = 2
 	tankDebuffCount = 1
-
 	self:Message("stages", "Neutral", "Info", args.spellName, false)
-	self:Bar(181299, 15 * enrageMod, 181300) -- Dragging Hands
-	self:Bar(181307, 25 * enrageMod) -- Foul Crush
-	self:CDBar(180244, 33 * enrageMod) -- Pound
-	self:Bar(181292, 54 * enrageMod, shadowCount > 0 and 181293) -- [Empowered] Fel Outpouring
-	self:Bar(181296, 83 * enrageMod, explosiveCount > 0 and 181297) -- [Empowered] Explosive Runes
-	self:CDBar("stages", 133 * enrageMod, 180068) -- Leap
+
+	if args.spellId == 189199 then -- LFR
+		self:Bar(181299, 14) -- Grasping Hands
+		self:Bar(181307, 22) -- Foul Crush
+		self:CDBar(180244, 30) -- Pound
+		self:CDBar("stages", and 92, 180068) -- Leap
+	else
+		self:Bar(181299, 15 * enrageMod, 181300) -- Dragging Hands
+		self:Bar(181307, 25 * enrageMod) -- Foul Crush
+		self:CDBar(180244, 33 * enrageMod) -- Pound
+		self:Bar(181292, 54 * enrageMod, shadowCount > 0 and 181293) -- [Empowered] Fel Outpouring
+		self:Bar(181296, 83 * enrageMod, explosiveCount > 0 and 181297) -- [Empowered] Explosive Runes
+		self:CDBar("stages", 133 * enrageMod, 180068) -- Leap
+	end
 end
 
 -- Tank Debuffs
@@ -122,7 +143,11 @@ function mod:ExplosiveBurst(args)
 	self:TargetMessage(args.spellId, args.destName, "Urgent", "Warning", nil, nil, true)
 	self:TargetBar(args.spellId, 10, args.destName)
 	tankDebuffCount = tankDebuffCount + 1
-	if tankDebuffCount < 4 then
+	if self:LFR() then
+		if tankDebuffCount == 2 then -- Only time for 1 more in LFR
+			self:Bar(args.spellId, 50)
+		end
+	elseif tankDebuffCount < 4 then
 		self:Bar(args.spellId, tankDebuffCount == 2 and (38 * enrageMod) or (50 * enrageMod))
 	end
 	self:PrimaryIcon(args.spellId, args.destName)
@@ -145,7 +170,11 @@ end
 function mod:FoulCrush(args)
 	self:TargetMessage(args.spellId, args.destName, "Urgent", self:Tank() and "Warning")
 	tankDebuffCount = tankDebuffCount + 1
-	if tankDebuffCount < 4 then
+	if self:LFR() then
+		if tankDebuffCount == 2 then -- Only time for 1 more in LFR
+			self:Bar(args.spellId, 50)
+		end
+	elseif tankDebuffCount < 4 then
 		self:Bar(args.spellId, tankDebuffCount == 2 and (50 * enrageMod) or (38 * enrageMod))
 	end
 end
@@ -153,7 +182,11 @@ end
 function mod:Swat(args)
 	self:Message(args.spellId, "Attention", self:Tank() and "Warning", args.spellName)
 	tankDebuffCount = tankDebuffCount + 1
-	if tankDebuffCount < 4 then
+	if self:LFR() then
+		--if tankDebuffCount == 2 then -- Only time for 1 more in LFR
+		--	self:Bar(args.spellId, 50)
+		--end
+	elseif tankDebuffCount < 4 then
 		self:Bar(args.spellId, 38 * enrageMod)
 	end
 end
@@ -163,13 +196,25 @@ end
 function mod:FelOutpouring(args)
 	shadowCount = shadowCount - 1
 	self:Message(181292, "Attention", "Long", args.spellId)
-	self:CDBar(181292, 108 * enrageMod, shadowCount > 0 and 181293) -- [Empowered] Fel Outpouring
+	if self:LFR() then
+		if shadowCount > 0 then
+			self:CDBar(args.spellId, 45) -- No Empowered on LFR
+		end
+	else
+		self:CDBar(181292, 108 * enrageMod, shadowCount > 0 and 181293) -- [Empowered] Fel Outpouring
+	end
 end
 
 function mod:ExplosiveRunes(args)
 	explosiveCount = explosiveCount - 1
 	self:Message(181296, "Urgent", "Info", args.spellId)
-	self:CDBar(181296, 108 * enrageMod, explosiveCount > 0 and 181297) -- [Empowered] Explosive Runes
+	if self:LFR() then
+		if explosiveCount > 0 then
+			self:CDBar(args.spellId, 35) -- No Empowered on LFR
+		end
+	else
+		self:CDBar(181296, 108 * enrageMod, explosiveCount > 0 and 181297) -- [Empowered] Explosive Runes
+	end
 end
 
 function mod:GraspingHandsStart(args)
@@ -179,15 +224,25 @@ end
 function mod:GraspingHands(args)
 	foulCount = foulCount - 1
 	self:Message(181299, "Important", nil, args.spellId)
-	self:CDBar(181299, 108 * enrageMod, foulCount > 0 and 181300) -- Grasping Hands / Dragging Hands
 	self:ScheduleTimer("CloseProximity", 4, 181299) -- Hands spawn delayed and you still have time to move
+	if self:LFR() then
+		if foulCount > 0 then
+			self:CDBar(args.spellId, 35) -- No Empowered (Dragging) on LFR
+		end
+	else
+		self:CDBar(181299, 108 * enrageMod, foulCount > 0 and 181300) -- Grasping Hands / Dragging Hands
+	end
 end
 
 function mod:Pound(args)
 	isPounding = true
 	self:Message(args.spellId, "Urgent", "Alert", CL.count:format(args.spellName, poundCount))
 	poundCount = poundCount + 1
-	self:CDBar(args.spellId, phase == 3 and (50 * enrageMod) or (62 * enrageMod), CL.count:format(args.spellName, poundCount)) -- start->start = 50 / 42 enraged for shadow, 62 / 52 enraged for other
+	if self:LFR() then
+		self:CDBar(args.spellId, phase == 3 and 35 or 25, CL.count:format(args.spellName, poundCount))
+	else
+		self:CDBar(args.spellId, phase == 3 and (50 * enrageMod) or (62 * enrageMod), CL.count:format(args.spellName, poundCount)) -- start->start = 50 / 42 enraged for shadow, 62 / 52 enraged for other
+	end
 	self:OpenProximity(args.spellId, 5) -- 4 + 1 safety
 end
 
