2e1da3b017358a4ee2c76ecb88fc53460798e089
funkydude <funkydude@users.noreply.github.com>
2015-07-12 21:34:15 +0000

    Xhulhorac: Add respawn time, add health to info messages, only play fel/void strike sounds in p3+ and if a tank, cleanups.

--- HellfireCitadel/Xhulhorac.lua
+++ HellfireCitadel/Xhulhorac.lua
@@ -7,6 +7,7 @@ local mod, CL = BigWigs:NewBoss("Xhul'horac", 1026, 1447)
 if not mod then return end
 mod:RegisterEnableMob(93068)
 mod.engageId = 1800
+mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
 -- Locals
@@ -34,12 +35,12 @@ L = mod:GetLocale()
 function mod:GetOptions()
 	return {
 		--[[ Phase 1 ]]--
-		{186271, "TANK_HEALER"}, -- Fel Strike
+		{190223, "TANK_HEALER"}, -- Fel Strike
 		{186407, "SAY", "PROXIMITY", "FLASH"}, -- Fel Surge
 		{186448, "TANK"}, -- Felblaze Flurry
 		186500, -- Chains of Fel
 		--[[ Phase 2 ]]--
-		{186292, "TANK_HEALER"}, -- Void Strike
+		{190224, "TANK_HEALER"}, -- Void Strike
 		{186333, "SAY", "PROXIMITY", "FLASH"}, -- Void Surge
 		{186785, "TANK"}, -- Withering Gaze
 		186546, -- Black Hole
@@ -54,27 +55,26 @@ function mod:GetOptions()
 		"voidfiend", -- Unstable Voidfiend
 		"stages",
 	}, {
-		[186271] = CL.phase:format(1),
-		[186292] = CL.phase:format(2),
+		[190223] = CL.phase:format(1),
+		[190224] = CL.phase:format(2),
 		[187204] = CL.phase:format(4),
 		[186134] = "general",
 	}
 end
 
 function mod:OnBossEnable()
-	-- XXX this stacking may not work out in all cases
 	self:Log("SPELL_AURA_APPLIED", "Touched", 186134, 186135) -- Feltouched, Voidtouched
 	self:Log("SPELL_AURA_APPLIED", "Felsinged_WastingVoid", 186073, 186063) -- Felsinged, Wasting Void
 	self:Log("SPELL_AURA_APPLIED_DOSE", "Felsinged_WastingVoid", 186073, 186063) -- Felsinged, Wasting Void
 	self:Log("SPELL_AURA_APPLIED", "Surge", 186407, 186333) -- Fel Surge, Void Surge
 	self:Log("SPELL_AURA_REMOVED", "SurgeRemoved", 186407, 186333) -- Fel Surge, Void Surge
 	self:Log("SPELL_AURA_APPLIED", "ChainsOfFel", 186500, 189775) -- Normal, Empowered
-	self:Log("SPELL_AURA_APPLIED", "FelblazeFlurry", 186448) -- Felblaze Flurry
-	self:Log("SPELL_AURA_APPLIED_DOSE", "FelblazeFlurry", 186448) -- Felblaze Flurry
-	self:Log("SPELL_AURA_APPLIED", "WitheringGaze", 186785) -- Withering Gaze
-	self:Log("SPELL_AURA_APPLIED_DOSE", "WitheringGaze", 186785) -- Withering Gaze
-	self:Log("SPELL_CAST_START", "FelStrike", 186271, 190223) -- Fel Strike XXX 186271 still used?
-	self:Log("SPELL_CAST_START", "VoidStrike", 186292, 190224) -- Void Strike XXX 186292 still used?
+	self:Log("SPELL_AURA_APPLIED", "FelblazeFlurry", 186448)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "FelblazeFlurry", 186448)
+	self:Log("SPELL_AURA_APPLIED", "WitheringGaze", 186785)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "WitheringGaze", 186785)
+	self:Log("SPELL_CAST_START", "FelStrike", 190223)
+	self:Log("SPELL_CAST_START", "VoidStrike", 190224)
 	self:Log("SPELL_AURA_APPLIED", "OverwhelmingChaos", 187204)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "OverwhelmingChaos", 187204)
 	self:Log("SPELL_CAST_START", "BlackHole", 186546, 189779) -- Normal, Empowered
@@ -82,14 +82,15 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "Voidstep", 188939)
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 
-	self:Death("Deaths", 94185, 94239) -- Vanguard Akkelion, Omnus
+	self:Death("AkkelionDies", 94185) -- Vanguard Akkelion
+	self:Death("OmnusDies", 94239) -- Omnus
 end
 
 function mod:OnEngage()
 	phase = 1
 	blackHoleCount = 1
 	wipe(mobCollector)
-	self:CDBar(186271, 8) -- Fel Strike
+	self:CDBar(190223, 8) -- Fel Strike
 	self:CDBar(186407, 19) -- Fel Surge 19-24
 end
 
@@ -102,7 +103,7 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(unit, spellName, _, _, spellId)
 		self:Bar("imps", 14.5, L.imps, L.imps_icon)
 
 	elseif spellId == 187196 then -- Fel Feedback (Vanguard Akkelion Spawned)
-		self:Message("stages", "Neutral", nil, CL.spawned:format(self:SpellName(-11691)), false)
+		self:Message("stages", "Neutral", "Info", "90% - ".. CL.spawned:format(self:SpellName(-11691)), false)
 		self:CDBar(186500, 31) -- Chains of Fel 31-36
 		self:CDBar(186448, 12) -- Felblaze Flurry
 
@@ -110,46 +111,46 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(unit, spellName, _, _, spellId)
 		self:Bar("voidfiend", 14.5, L.voidfiend, L.voidfiend_icon)
 
 	elseif spellId == 189806 then -- Void Feedback (Omnus Spawned)
-		self:Message("stages", "Neutral", nil, CL.spawned:format(self:SpellName(-11688)), false)
+		self:Message("stages", "Neutral", "Info", "60% - ".. CL.spawned:format(self:SpellName(-11688)), false)
 		self:CDBar(186546, 18) -- Black Hole
 		self:CDBar(186785, 6) -- Withering Gaze
 
 	elseif spellId == 187209 then -- Overwhelming Chaos
 		self:UnregisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", unit)
-		self:StopBar(186271) -- Fel Strike
+		self:StopBar(190223) -- Fel Strike
 		self:StopBar(186407) -- Fel Surge
-		self:StopBar(186292) -- Void Strike
+		self:StopBar(190224) -- Void Strike
 		self:StopBar(186333) -- Void Surge
-		self:Message("stages", "Neutral", "Info", CL.phase:format(4), false)
+		self:Message("stages", "Neutral", "Info", "20% - ".. CL.phase:format(4), false)
 		self:Bar(187204, 10) -- Overwhelming Chaos
 	end
 end
 
-function mod:Deaths(args)
-	if args.mobId == 94185 then -- Vanguard Akkelion
-		self:StopBar(186271) -- Fel Strike
-		self:StopBar(186407) -- Fel Surge
-		self:StopBar(186448) -- Felblaze Flurry
-		self:StopBar(186500) -- Chains of Fel
-		phase = 2
-		self:Message("stages", "Neutral", "Info", CL.phase:format(2), false)
-		self:CDBar(186292, 20) -- Void Strike
-		self:CDBar(186333, 28) -- Void Surge
-		if self:Mythic() then
-			self:CDBar(186500, 30) -- Empowered Chains of Fel
-		end
-	elseif args.mobId == 94239 then -- Omnus
-		self:StopBar(186292) -- Void Strike
-		self:StopBar(186333) -- Void Surge
-		self:StopBar(186785) -- Withering Gaze
-		self:StopBar(186546) -- Black Hole
-		phase = 3
-		self:Message("stages", "Neutral", "Info", CL.phase:format(3), false)
-		-- self:CDBar(186292, 4) -- Void Strike
-		-- self:CDBar(186333, 28) -- Void Surge
-		if self:Mythic() then
-			self:CDBar(186546, 21) -- Empowered Black Hole
-		end
+function mod:AkkelionDies()
+	self:StopBar(190223) -- Fel Strike
+	self:StopBar(186407) -- Fel Surge
+	self:StopBar(186448) -- Felblaze Flurry
+	self:StopBar(186500) -- Chains of Fel
+	phase = 2
+	self:Message("stages", "Neutral", "Info", "65% - ".. CL.phase:format(2), false)
+	self:CDBar(190224, 20) -- Void Strike
+	self:CDBar(186333, 28) -- Void Surge
+	if self:Mythic() then
+		self:CDBar(186500, 30) -- Empowered Chains of Fel
+	end
+end
+
+function mod:OmnusDies()
+	self:StopBar(190224) -- Void Strike
+	self:StopBar(186333) -- Void Surge
+	self:StopBar(186785) -- Withering Gaze
+	self:StopBar(186546) -- Black Hole
+	phase = 3
+	self:Message("stages", "Neutral", "Info", "35% - ".. CL.phase:format(3), false)
+	-- self:CDBar(190224, 4) -- Void Strike
+	-- self:CDBar(186333, 28) -- Void Surge
+	if self:Mythic() then
+		self:CDBar(186546, 21) -- Empowered Black Hole
 	end
 end
 
@@ -223,20 +224,26 @@ function mod:SurgeRemoved(args)
 end
 
 function mod:FelStrike(args)
-	self:Message(186271, "Urgent", "Warning", CL.casting:format(args.spellName))
+	self:Message(args.spellId, "Urgent")
 	if phase < 3 then
-		self:CDBar(186271, 16) -- 15.8
+		self:CDBar(args.spellId, 16) -- 15.8
 	else -- alternates
-		self:CDBar(186292, 8) -- Void Strike
+		if self:Tank() then
+			self:PlaySound(args.spellId, "Warning")
+		end
+		self:CDBar(190224, 8) -- Void Strike
 	end
 end
 
 function mod:VoidStrike(args)
-	self:Message(186292, "Urgent", "Warning", CL.casting:format(args.spellName))
+	self:Message(args.spellId, "Urgent")
 	if phase < 3 then
-		self:CDBar(186292, 17) -- 17.1/18.2
+		self:CDBar(args.spellId, 17) -- 17.1/18.2
 	else -- alternates
-		self:CDBar(186271, 7) -- Fel Strike
+		if self:Tank() then
+			self:PlaySound(args.spellId, "Warning")
+		end
+		self:CDBar(190223, 7) -- Fel Strike
 	end
 end
 
@@ -272,3 +279,4 @@ function mod:OverwhelmingChaos(args)
 	self:StackMessage(args.spellId, args.destName, amount, "Important")
 	self:Bar(args.spellId, 10, CL.count:format(args.spellName, amount + 1))
 end
+
