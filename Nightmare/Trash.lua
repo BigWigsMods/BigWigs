6f20d28feb843c0a3e8672df27fafb448e922dcd
elvador <reysptools@googlemail.com>
2016-04-28 21:48:28 +0000

    NightmareRaid: Add trash module

---
+++ Nightmare/Trash.lua
@@ -0,0 +1,49 @@
+if select(4, GetBuildInfo()) < 70000 then return end -- XXX legion check for live
+
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Emerald Nightmare Trash", 1094)
+if not mod then return end
+mod.displayName = CL.trash
+mod:RegisterEnableMob(
+	111004 -- Gelatinized Decay
+)
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:NewLocale("enUS", true)
+if L then
+	L.gelatinizedDecay = "Gelatinized Decay"
+end
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		--[[ Gelatinized Decay ]]--
+		221059, -- Wave of Decay
+	}, {
+		[221059] = L.gelatinizedDecay,
+	}
+end
+
+function mod:OnBossEnable()
+	--[[ Gelatinized Decay ]]--
+	self:Log("SPELL_CAST_START", "WaveofDecay", 221059)
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+--[[ Gelatinized Decay ]]--
+
+function mod:WaveofDecay(args)
+	self:Message(args.spellId, "Urgent", self:Interrupter(args.sourceGUID) and "Info", CL.casting:format(args.spellName))
+end
