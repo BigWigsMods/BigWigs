d0c444c6d7b70269443b4fb9ecf2acbe1126b742
funkydude <funkydude@users.noreply.github.com>
2016-08-06 02:04:36 +0100

    Nighthold/Krosus: Fix bar errors.

--- Nighthold/Krosus.lua
+++ Nighthold/Krosus.lua
@@ -24,10 +24,10 @@ mod.engageId = 1842
 --
 
 local timers = {
-	["Left Beam"] = {40.0, 75.0, 32.0, 30.0, 82.0, 31.0, 26.0, 24.0, 18.0},
-	["Right Beam"] = {11.0, 59.0, 61.0, 30.0, 43.0, 81.0, 26.0, 17.0, 17.0},
-	["Orb of Destruction"] = {22.0, 58.0, 23.0, 63.0, 26.0, 25.0, 15.0, 15.0, 15.0, 30.0, 55.0},
-	["Burning Pitch"] = {52.0, 84.0, 90.0, 93.0},
+	--["Left Beam"] = {40.0, 75.0, 32.0, 30.0, 82.0, 31.0, 26.0, 24.0, 18.0},
+	--["Right Beam"] = {11.0, 59.0, 61.0, 30.0, 43.0, 81.0, 26.0, 17.0, 17.0},
+	[205344] = {22.0, 58.0, 23.0, 63.0, 26.0, 25.0, 15.0, 15.0, 15.0, 30.0, 55.0}, -- Orb of Destruction
+	[205420] = {52.0, 84.0, 90.0, 93.0}, -- Burning Pitch
 }
 local leftBeamCount = 1
 local rightBeamCount = 1
@@ -87,10 +87,10 @@ function mod:OnEngage()
 	self:Bar(206677, 15)
 	self:Bar(205862, 33, CL.count:format(self:SpellName(205862), slamCount))
 	self:Bar(205862, 93, CL.count:format(L.smashingBridge, 1))
-	self:Bar(205368, timers["Left Beam"][leftBeamCount], L.sidebeam:format(L.left))
-	self:Bar(205368, timers["Right Beam"][rightBeamCount], L.sidebeam:format(L.right))
-	self:Bar(205344, timers["Orb of Destruction"][orbCount])
-	self:Bar(205420, timers["Burning Pitch"][burningPitchCount])
+	--self:Bar(205368, timers["Left Beam"][leftBeamCount], L.sidebeam:format(L.left))
+	--self:Bar(205368, timers["Right Beam"][rightBeamCount], L.sidebeam:format(L.right))
+	self:Bar(205344, timers[205344][orbCount])
+	self:Bar(205420, timers[205420][burningPitchCount])
 end
 
 --------------------------------------------------------------------------------
@@ -103,34 +103,46 @@ function mod:FelBrand(args)
 end
 
 function mod:FelBeamLeftCast(args)
-  self:Message(205368, "Attention", "Info", CL.casting:format(L.sidebeam:format(L.left)))
+	self:Message(205368, "Attention", "Info", CL.casting:format(L.sidebeam:format(L.left)))
 end
 
 function mod:FelBeamRightCast(args)
-  self:Message(args.spellId, "Attention", "Info", CL.casting:format(L.sidebeam:format(L.right)))
+	self:Message(args.spellId, "Attention", "Info", CL.casting:format(L.sidebeam:format(L.right)))
 end
 
 function mod:FelBeamLeftSuccess(args)
-  self:Message(205368, "Attention", nil, L.sidebeam:format(L.left))
+	self:Message(205368, "Attention", nil, L.sidebeam:format(L.left))
 	leftBeamCount = leftBeamCount + 1
-	self:Bar(205368, timers["Left Beam"][leftBeamCount], L.sidebeam:format(L.left))
+	--self:Bar(205368, timers["Left Beam"][leftBeamCount], L.sidebeam:format(L.left))
 end
 
 function mod:FelBeamRightSuccess(args)
-  self:Message(args.spellId, "Attention", nil, L.sidebeam:format(L.right))
+	self:Message(args.spellId, "Attention", nil, L.sidebeam:format(L.right))
 	rightBeamCount = rightBeamCount + 1
-	self:Bar(args.spellId, timers["Right Beam"][rightBeamCount], L.sidebeam:format(L.right))
+	--self:Bar(args.spellId, timers["Right Beam"][rightBeamCount], L.sidebeam:format(L.right))
 end
 
-function mod:OrbOfDescructionApplied(args)
-	self:TargetMessage(args.spellId, args.destName, "Urgent", "Alarm", CL.count:format(args.spellName, orbCount))
-	self:TargetBar(args.spellId, 5, args.destName)
-	if self:Me(args.destGUID) then
-		self:Flash(args.spellId)
-		self:Say(args.spellId)
+do
+	local prev = 0
+	function mod:OrbOfDescructionApplied(args)
+		self:TargetMessage(args.spellId, args.destName, "Urgent", "Alarm", CL.count:format(args.spellName, orbCount))
+		self:TargetBar(args.spellId, 5, args.destName)
+		if self:Me(args.destGUID) then
+			self:Flash(args.spellId)
+			self:Say(args.spellId)
+		end
+		orbCount = orbCount + 1
+
+		local t = timers[args.spellId][orbCount]
+		if t then
+			self:Bar(args.spellId, t)
+			prev = GetTime()
+		else
+			t = GetTime() - prev
+			print("Unknown bigwigs timer:", args.spellId, args.spellName, orbCount, t)
+			prev = GetTime()
+		end
 	end
-	orbCount = orbCount + 1
-	self:Bar(args.spellId, timers["Orb of Destruction"][orbCount])
 end
 
 function mod:SlamCast(args)
@@ -148,8 +160,21 @@ function mod:SlamSuccess(args)
 	end
 end
 
-function mod:BurningPitchCast(args)
-	self:Message(args.spellId, "Attention", "Info")
-	burningPitchCount = burningPitchCount + 1
-	self:Bar(args.spellId, timers["Burning Pitch"][burningPitchCount])
+do
+	local prev = 0
+	function mod:BurningPitchCast(args)
+		self:Message(args.spellId, "Attention", "Info")
+		burningPitchCount = burningPitchCount + 1
+
+		local t = timers[args.spellId][burningPitchCount]
+		if t then
+			self:Bar(args.spellId, t)
+			prev = GetTime()
+		else
+			t = GetTime() - prev
+			print("Unknown bigwigs timer:", args.spellId, args.spellName, burningPitchCount, t)
+			prev = GetTime()
+		end
+	end
 end
+
