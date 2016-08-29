5a48ac7aa1abd9a7b65540c23a87c4c421385fbc
funkydude <funkydude@users.noreply.github.com>
2016-08-29 06:01:52 +0100

    Remove WoD content

--- Draenor/Locales/frFR.lua
+++
@@ -1,5 +0,0 @@
-local L = BigWigs:NewBossLocale("Supreme Lord Kazzak", "frFR")
-if L then
-L.engage_yell = "Affrontez la puissance de la Légion ardente !"
-
-end
--- Draenor/Locales/koKR.lua
+++
@@ -1,5 +0,0 @@
-local L = BigWigs:NewBossLocale("Supreme Lord Kazzak", "koKR")
-if L then
-L.engage_yell = "불타는 군단의 압도적인 힘을 느껴라!"
-
-end
