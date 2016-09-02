fe5cdc50ac2acf9276874322f9ffa056e7bcefce
funkydude <funkydude@users.noreply.github.com>
2016-09-02 03:11:28 +0100

    Pull: add automatic combat logging option.

--- Plugins/Pull.lua
+++ Plugins/Pull.lua
@@ -10,7 +10,7 @@ if not plugin then return end
 --
 
 local L = LibStub("AceLocale-3.0"):GetLocale("BigWigs: Plugins")
-
+local isLogging = false
 
 -------------------------------------------------------------------------------
 -- Options
@@ -18,6 +18,7 @@ local L = LibStub("AceLocale-3.0"):GetLocale("BigWigs: Plugins")
 
 plugin.defaultDB = {
 	countType = "emphasized",
+	combatLog = false,
 }
 
 do
@@ -38,15 +39,15 @@ do
 				},
 				width = "full",
 			},
+			combatLog = {
+				type = "select",
+				name = "Automatic Combat logging",
+				order = 2,
+				width = "full",
+			},
 			--gearCheck = {
 			--	type = "toggle",
 			--	name = "Low Gear Check",
-			--	order = 2,
-			--	width = "full",
-			--},
-			--combatLog = {
-			--	type = "select",
-			--	name = "Automatic Combat logging",
 			--	order = 3,
 			--	width = "full",
 			--},
@@ -61,37 +62,39 @@ end
 function plugin:OnPluginEnable()
 	BigWigs:AddSyncListener(self, "BWPull", 0)
 	self:RegisterMessage("DBM_AddonMessage", "OnDBMSync")
+
+	self:RegisterMessage("BigWigs_OnBossWin")
+	self:RegisterMessage("BigWigs_OnBossWipe", "BigWigs_OnBossWin")
 end
 
 -------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-local startPull
 do
 	local timer, timeLeft = nil, 0
-	local function printPull()
+	local function printPull(self)
 		timeLeft = timeLeft - 1
 		if timeLeft == 0 then
-			plugin:CancelTimer(timer)
+			self:CancelTimer(timer)
 			timer = nil
-			plugin:SendMessage("BigWigs_Message", plugin, nil, L.pulling, "Attention", "Interface\\Icons\\ability_warrior_charge")
-			plugin:SendMessage("BigWigs_Sound", plugin, nil, "Alarm")
+			self:SendMessage("BigWigs_Message", self, nil, L.pulling, "Attention", "Interface\\Icons\\ability_warrior_charge")
+			self:SendMessage("BigWigs_Sound", self, nil, "Alarm")
 		elseif timeLeft > 2 and IsEncounterInProgress() then -- Cancel the pull timer if we ninja pulled
 			startPull(0, COMBAT)
 		elseif timeLeft < 11 then
-			if plugin.db.profile.countType == "normal" then
-				plugin:SendMessage("BigWigs_Message", plugin, nil, L.pullIn:format(timeLeft), "Attention")
+			if self.db.profile.countType == "normal" then
+				self:SendMessage("BigWigs_Message", self, nil, L.pullIn:format(timeLeft), "Attention")
 			else
-				plugin:SendMessage("BigWigs_EmphasizedCountdownMessage", timeLeft)
+				self:SendMessage("BigWigs_EmphasizedCountdownMessage", timeLeft)
 			end
 			local module = BigWigs:GetPlugin("Sounds", true)
 			if timeLeft < 6 and module and module.db.profile.sound then
-				plugin:SendMessage("BigWigs_PlayCountdownNumber", plugin, timeLeft)
+				self:SendMessage("BigWigs_PlayCountdownNumber", self, timeLeft)
 			end
 		end
 	end
-	function startPull(seconds, nick, isDBM)
+	function self:StartPull(seconds, nick, isDBM)
 		if (not UnitIsGroupLeader(nick) and not UnitIsGroupAssistant(nick) and not UnitIsUnit(nick, "player")) or (IsEncounterInProgress() and nick ~= COMBAT) then return end
 		seconds = tonumber(seconds)
 		if not seconds or seconds < 0 or seconds > 60 then return end
@@ -99,36 +102,53 @@ do
 		if timeLeft == seconds then return end -- Throttle
 		timeLeft = seconds
 		if timer then
-			plugin:CancelTimer(timer)
+			self:CancelTimer(timer)
 			if seconds == 0 then
 				timeLeft = 0
 				BigWigs:Print(L.pullStopped:format(nick))
-				plugin:SendMessage("BigWigs_StopBar", plugin, L.pull)
-				plugin:SendMessage("BigWigs_StopPull", plugin, seconds, nick, isDBM)
+				self:SendMessage("BigWigs_StopBar", self, L.pull)
+				self:SendMessage("BigWigs_StopPull", self, seconds, nick, isDBM)
 				return
 			end
 		end
 		FlashClientIcon()
+
+		if self.db.profile.combatLog then
+			isLogging = true
+			LoggingCombat(isLogging)
+		end
+
 		BigWigs:Print(L.pullStarted:format(isDBM and "DBM" or "BigWigs", nick))
-		timer = plugin:ScheduleRepeatingTimer(printPull, 1)
-		plugin:SendMessage("BigWigs_Message", plugin, nil, L.pullIn:format(timeLeft), "Attention")
-		plugin:SendMessage("BigWigs_Sound", plugin, nil, "Long")
-		plugin:SendMessage("BigWigs_StartBar", plugin, nil, L.pull, seconds, "Interface\\Icons\\ability_warrior_charge")
-		plugin:SendMessage("BigWigs_StartPull", plugin, seconds, nick, isDBM)
+		timer = self:ScheduleRepeatingTimer(printPull, 1, self)
+		self:SendMessage("BigWigs_Message", self, nil, L.pullIn:format(timeLeft), "Attention")
+		self:SendMessage("BigWigs_Sound", self, nil, "Long")
+		self:SendMessage("BigWigs_StartBar", self, nil, L.pull, seconds, "Interface\\Icons\\ability_warrior_charge")
+		self:SendMessage("BigWigs_StartPull", self, seconds, nick, isDBM)
 	end
 end
 
 function plugin:OnDBMSync(_, sender, prefix, seconds, text)
 	if prefix == "PT" then
-		startPull(seconds, sender, true)
+		self:StartPull(seconds, sender, true)
 	end
 end
 
 function plugin:OnSync(sync, seconds, nick)
 	if seconds and nick then
-		startPull(seconds, nick)
+		self:StartPull(seconds, nick)
 	end
 end
+
+function plugin:BigWigs_OnBossWin()
+	if isLogging then
+		isLogging = false
+		LoggingCombat(isLogging)
+	end
+end
+
+-------------------------------------------------------------------------------
+-- Slash Handler
+--
 
 SlashCmdList.BIGWIGSPULL = function(input)
 	if not plugin:IsEnabled() then BigWigs:Enable() end
