62dfca67a69d9a93dafe0d2a1feda9c0b5e74df9
funkydude <funkydude@users.noreply.github.com>
2016-09-15 12:29:31 +0100

    Add skeleton for Trial of Valor

---
+++ TrialOfValor/Guarm.lua
@@ -0,0 +1,56 @@
+
+--------------------------------------------------------------------------------
+-- TODO List:
+
+
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Guarm-TrialOfValor", 1114, 1830)
+if not mod then return end
+--mod:RegisterEnableMob(0)
+--mod.engageId = 0
+--mod.respawnTime = 0
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+if L then
+	
+end
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		"berserk",
+	}
+end
+
+function mod:OnBossEnable()
+	
+end
+
+function mod:OnEngage()
+	
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
+
+function mod:Go(args)
+	
+end
+
---
+++ TrialOfValor/Locales/ruRU.lua
@@ -0,0 +1,6 @@
+local L = BigWigs:NewBossLocale("Odyn-TrialOfValor", "ruRU")
+if not L then return end
+if L then
+
+end
+
