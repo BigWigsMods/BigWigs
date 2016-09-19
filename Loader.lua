f6725be6bb407e851e895adc2725b136d2f8a0ae
funkydude <funkydude@users.noreply.github.com>
2016-09-19 20:01:12 +0100

    bump DBM version

--- Loader.lua
+++ Loader.lua
@@ -580,7 +580,7 @@ end
 
 do
 	-- This is a crapfest mainly because DBM's actual handling of versions is a crapfest, I'll try explain how this works...
-	local DBMdotRevision = "15192" -- The changing version of the local client, changes with every alpha revision using an SVN keyword.
+	local DBMdotRevision = "15226" -- The changing version of the local client, changes with every alpha revision using an SVN keyword.
 	local DBMdotDisplayVersion = "7.0.5" -- Same as above but is changed between alpha and release cycles e.g. "N.N.N" for a release and "N.N.N alpha" for the alpha duration
 	local DBMdotReleaseRevision = DBMdotRevision -- This is manually changed by them every release, they use it to track the highest release version, a new DBM release is the only time it will change.
 
