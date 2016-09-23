4a06fbf193fd7418f4f1ba8c542a64afa3b7e027
funkydude <funkydude@users.noreply.github.com>
2016-09-23 17:49:10 +0100

    Nightmare: Update locale files, closes #44

--- Nightmare/Locales/ruRU.lua
+++ Nightmare/Locales/ruRU.lua
@@ -1,6 +1,37 @@
-local L = BigWigs:NewBossLocale("Ursoc", "ruRU")
+local L = BigWigs:NewBossLocale("Cenarius", "ruRU")
 if not L then return end
 if L then
-
+	--L.forces = "Forces"
 end
 
+L = BigWigs:NewBossLocale("Elerethe Renferal", "ruRU")
+if L then
+	--L.isLinkedWith = "%s is linked with %s"
+	--L.yourLink = "You are linked with %s"
+	--L.yourLinkShort = "Linked with %s"
+end
+
+L = BigWigs:NewBossLocale("Il'gynoth", "ruRU")
+if L then
+	--L.custom_off_deathglare_marker = "Deathglare Tentacle marker"
+	--L.custom_off_deathglare_marker_desc = "Mark Deathglare Tentacles with {rt6}{rt5}{rt4}{rt3}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r\n|cFFADFF2FTIP: If the raid has chosen you to turn this on, having nameplates enabled or quickly mousing over the spears is the fastest way to mark them.|r"
+end
+
+L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "ruRU")
+if L then
+	--L.gelatinizedDecay = "Gelatinized Decay"
+end
+
+L = BigWigs:NewBossLocale("Ursoc", "ruRU")
+if L then
+	--L.custom_off_gaze_assist = "Focused Gaze Assist"
+	--L.custom_off_gaze_assist_desc = "Show raid icons in bars and messages for Focused Gaze. Using {rt4} for odd, {rt6} for even soaks. Requires promoted or leader."
+end
+
+L = BigWigs:NewBossLocale("Xavius", "ruRU")
+if L then
+	--L.custom_off_blade_marker = "Nightmare Blade marker"
+	--L.custom_off_blade_marker_desc = "Mark the targets of Nightmare Blades with {rt1}{rt2}, requires promoted or leader."
+
+	--L.linked = "Bonds of Terror on YOU! - Linked with %s!"
+end
