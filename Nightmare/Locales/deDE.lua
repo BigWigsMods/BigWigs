0f5039f07058f6547a73f51254081b35b685a4e2
bullsei <bullsei@users.noreply.github.com>
2016-09-24 18:22:48 +0200

    Update deDE (#45)

--- Nightmare/Locales/deDE.lua
+++ Nightmare/Locales/deDE.lua
@@ -1,37 +1,37 @@
 local L = BigWigs:NewBossLocale("Cenarius", "deDE")
 if not L then return end
 if L then
-	--L.forces = "Forces"
+	L.forces = "Mächte"
 end
 
 L = BigWigs:NewBossLocale("Elerethe Renferal", "deDE")
 if L then
-	--L.isLinkedWith = "%s is linked with %s"
-	--L.yourLink = "You are linked with %s"
-	--L.yourLinkShort = "Linked with %s"
+	L.isLinkedWith = "%s ist mit %s verbunden"
+	L.yourLink = "Du bist mit %s verbunden"
+	L.yourLinkShort = "Verbunden mit %s"
 end
 
 L = BigWigs:NewBossLocale("Il'gynoth", "deDE")
 if L then
-	--L.custom_off_deathglare_marker = "Deathglare Tentacle marker"
-	--L.custom_off_deathglare_marker_desc = "Mark Deathglare Tentacles with {rt6}{rt5}{rt4}{rt3}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r\n|cFFADFF2FTIP: If the raid has chosen you to turn this on, having nameplates enabled or quickly mousing over the spears is the fastest way to mark them.|r"
+	L.custom_off_deathglare_marker = "Todesblicktentakel markieren"
+	L.custom_off_deathglare_marker_desc = "Markiert Todesblicktentakel mit {rt6}{rt5}{rt4}{rt3}, benötigt Leiter oder Assistent.\n|cFFFF0000Nur eine Person im Schlachtzug sollte diese Option aktiviert haben, um Markierungskonflikte zu verhindern.|r\n|cFFADFF2FTIP: Wenn der Schlachtzug sich dafür entschieden hat, dass du diese Option aktivierst, ist der schnellste Weg mit der Maus über die Mobs zu fahren, um sie zu markieren.|r"
 end
 
 L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "deDE")
 if L then
-	--L.gelatinizedDecay = "Gelatinized Decay"
+	L.gelatinizedDecay = "Verrottender Schleim"
 end
 
 L = BigWigs:NewBossLocale("Ursoc", "deDE")
 if L then
-	--L.custom_off_gaze_assist = "Focused Gaze Assist"
-	--L.custom_off_gaze_assist_desc = "Show raid icons in bars and messages for Focused Gaze. Using {rt4} for odd, {rt6} for even soaks. Requires promoted or leader."
+	L.custom_off_gaze_assist = "Hilfe für Fokussierter Blick"
+	L.custom_off_gaze_assist_desc = "Zeigt Schlachtzugssymbole in Leisten und Nachrichten für Fokussierter Blick. Verwendet {rt4} für ungerade, {rt6} für gerade Blicke (Blicke werden durchnummeriert). Benötigt Leiter oder Assistent."
 end
 
 L = BigWigs:NewBossLocale("Xavius", "deDE")
 if L then
-	--L.custom_off_blade_marker = "Nightmare Blade marker"
-	--L.custom_off_blade_marker_desc = "Mark the targets of Nightmare Blades with {rt1}{rt2}, requires promoted or leader."
+	L.custom_off_blade_marker = "Alptraumklingen markieren"
+	L.custom_off_blade_marker_desc = "Markiert die Ziele von Alptraumklingen mit {rt1}{rt2}, benötigt Leiter oder Assistent."
 
-	--L.linked = "Bonds of Terror on YOU! - Linked with %s!"
+	L.linked = "Schreckensbindungen auf DIR! – Verbunden mit %s!"
 end
