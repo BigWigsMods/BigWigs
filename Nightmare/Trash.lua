56085a9c86c6bd4c9871b17d3a13cc32241a3746
funkydude <funkydude@users.noreply.github.com>
2016-09-26 22:00:51 +0100

    Nightmare/Trash: Add Unstable Decay

--- Nightmare/Trash.lua
+++ Nightmare/Trash.lua
@@ -27,14 +27,18 @@ function mod:GetOptions()
 	return {
 		--[[ Gelatinized Decay ]]--
 		221059, -- Wave of Decay
+		{221028, "SAY"}, -- Unstable Decay
 	}, {
 		[221059] = L.gelatinizedDecay,
 	}
 end
 
 function mod:OnBossEnable()
+	self:RegisterMessage("BigWigs_OnBossEngage", "Disable")
+
 	--[[ Gelatinized Decay ]]--
 	self:Log("SPELL_CAST_START", "WaveofDecay", 221059)
+	self:Log("SPELL_AURA_APPLIED", "UnstableDecay", 221028)
 end
 
 --------------------------------------------------------------------------------
@@ -45,4 +49,12 @@ end
 
 function mod:WaveofDecay(args)
 	self:Message(args.spellId, "Urgent", self:Interrupter(args.sourceGUID) and "Info", CL.casting:format(args.spellName))
+end
+
+function mod:UnstableDecay(args)
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId)
+		self:TargetBar(args.spellId, 9, args.destName)
+		self:TargetMessage(args.spellId, args.destName, "Personal", "Warning")
+	end
 end
