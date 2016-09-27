e42ebd8f75cedb5df1bce3c583dbf2492ef51ce8
EKE00372 <eke00372@gmail.com>
2016-09-28 02:45:01 +0800

    zhTW localization (#48)

--- Nightmare/Locales/zhTW.lua
+++ Nightmare/Locales/zhTW.lua
@@ -1,40 +1,40 @@
 local L = BigWigs:NewBossLocale("Cenarius", "zhTW")
 if not L then return end
 if L then
-	--L.forces = "Forces"
-	--L.bramblesSay = "Brambles near %s"
+	L.forces = "夢魘大軍"
+	L.bramblesSay = "荊棘在%s附近"
 end
 
 L = BigWigs:NewBossLocale("Elerethe Renferal", "zhTW")
 if L then
-	--L.isLinkedWith = "%s is linked with %s"
-	--L.yourLink = "You are linked with %s"
-	--L.yourLinkShort = "Linked with %s"
+	L.isLinkedWith = ">%s< 與 >%s< 相連"
+	L.yourLink = ">你< 與 >%s< 相連"
+	L.yourLinkShort = "與 >%s< 相連"
 end
 
 L = BigWigs:NewBossLocale("Il'gynoth", "zhTW")
 if L then
-	--L.custom_off_deathglare_marker = "Deathglare Tentacle marker"
-	--L.custom_off_deathglare_marker_desc = "Mark Deathglare Tentacles with {rt6}{rt5}{rt4}{rt3}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r\n|cFFADFF2FTIP: If the raid has chosen you to turn this on, having nameplates enabled or quickly mousing over the spears is the fastest way to mark them.|r"
+	L.custom_off_deathglare_marker = "死凝觸手標記"
+	L.custom_off_deathglare_marker_desc = "使用 {rt6}{rt5}{rt4}{rt3} 標記死凝觸手，需要權限。\n|cFFFF0000團隊中只需一名玩家啟用此選項以防止衝突。|r\n|cFFADFF2F提示：如果團隊選擇由你開啟此選項，開啟姓名板並滑鼠指向來標記死凝觸手是最快的方式。|r"
 
-	--L.bloods_remaining = "%d Bloods remaining"
+	L.bloods_remaining = "剩餘%d黏液"
 end
 
 L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "zhTW")
 if L then
-	--L.gelatinizedDecay = "Gelatinized Decay"
+	L.gelatinizedDecay = "膠化腐泥怪"
 end
 
 L = BigWigs:NewBossLocale("Ursoc", "zhTW")
 if L then
-	--L.custom_off_gaze_assist = "Focused Gaze Assist"
-	--L.custom_off_gaze_assist_desc = "Show raid icons in bars and messages for Focused Gaze. Using {rt4} for odd, {rt6} for even soaks. Requires promoted or leader."
+	L.custom_off_gaze_assist = "專注凝視助手"
+	L.custom_off_gaze_assist_desc = "在計時條和訊息顯示專注凝視團隊標記。使用 {rt4} 標記奇數輪， {rt6} 標記偶數輪，需要權限。"
 end
 
 L = BigWigs:NewBossLocale("Xavius", "zhTW")
 if L then
-	--L.custom_off_blade_marker = "Nightmare Blade marker"
-	--L.custom_off_blade_marker_desc = "Mark the targets of Nightmare Blades with {rt1}{rt2}, requires promoted or leader."
+	L.custom_off_blade_marker = "夢魘之刃標記"
+	L.custom_off_blade_marker_desc = "使用 {rt1}{rt2} 標記夢魘之刃的目標，需要權限。"
 
-	--L.linked = "Bonds of Terror on YOU! - Linked with %s!"
+	L.linked = ">你< 恐懼束縛！- 與 >%s< 相連！"
 end
