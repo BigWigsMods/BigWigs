74185efd315fd4f8b4587723437105ee006b267a
bullsei <bullsei@users.noreply.github.com>
2016-09-28 14:03:07 +0200

    Update deDE.lua (#50)

--- Nightmare/Locales/deDE.lua
+++ Nightmare/Locales/deDE.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Cenarius", "deDE")
 if not L then return end
 if L then
 	L.forces = "Mächte"
-	--L.bramblesSay = "Brambles near %s"
+	L.bramblesSay = "Gestrüpp bei %s"
 end
 
 L = BigWigs:NewBossLocale("Elerethe Renferal", "deDE")
@@ -17,7 +17,7 @@ if L then
 	L.custom_off_deathglare_marker = "Todesblicktentakel markieren"
 	L.custom_off_deathglare_marker_desc = "Markiert Todesblicktentakel mit {rt6}{rt5}{rt4}{rt3}, benötigt Leiter oder Assistent.\n|cFFFF0000Nur eine Person im Schlachtzug sollte diese Option aktiviert haben, um Markierungskonflikte zu verhindern.|r\n|cFFADFF2FTIP: Wenn der Schlachtzug sich dafür entschieden hat, dass du diese Option aktivierst, ist der schnellste Weg mit der Maus über die Mobs zu fahren, um sie zu markieren.|r"
 
-	--L.bloods_remaining = "%d Bloods remaining"
+	L.bloods_remaining = "%d Blut verbleibend"
 end
 
 L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "deDE")
