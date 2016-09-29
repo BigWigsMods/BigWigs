c9d541a6f8f911f55488d57d67c020db50df95fe
Grow <tborlee@users.noreply.github.com>
2016-09-29 09:05:58 +0200

    frFR Update

--- Nightmare/Locales/frFR.lua
+++ Nightmare/Locales/frFR.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Cenarius", "frFR")
 if not L then return end
 if L then
 	L.forces = "Forces"
-	--L.bramblesSay = "Brambles near %s"
+	L.bramblesSay = "Ronces près |2 %s"
 end
 
 L = BigWigs:NewBossLocale("Elerethe Renferal", "frFR")
@@ -17,7 +17,7 @@ if L then
 	L.custom_off_deathglare_marker = "Marquage Oeil meurtrier tentaculaire"
 	L.custom_off_deathglare_marker_desc = "Marque les Yeux meurtriers tentaculaires avec {rt6}{rt5}{rt4}{rt3}. Nécessite d'être assistant ou chef de raid.\n|cFFFF0000Seule 1 personne du raid doit activer ceci afin d'éviter les conflits de marquage.|r\n|cFFADFF2FASTUCE : si le raid a décidé que c'est vous qui devez l'activer, survoler rapidement tous les yeux est le moyen le plus rapide de les marquer.|r"
 
-	--L.bloods_remaining = "%d Bloods remaining"
+	L.bloods_remaining = "Il reste %d |4sang:sangs;"
 end
 
 L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "frFR")
