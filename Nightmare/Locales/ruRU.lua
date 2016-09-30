7811fbeaa31059dc62d0a1281fa030e8e65cd3ff
Gennady G <me@andaril.net>
2016-09-30 19:49:53 +0300

    ruRu update

--- Nightmare/Locales/ruRU.lua
+++ Nightmare/Locales/ruRU.lua
@@ -1,23 +1,23 @@
 local L = BigWigs:NewBossLocale("Cenarius", "ruRU")
 if not L then return end
 if L then
-	--L.forces = "Forces"
-	--L.bramblesSay = "Brambles near %s"
+	L.forces = "Силы кошмара"
+	L.bramblesSay = "Колючки рядом с %s"
 end
 
 L = BigWigs:NewBossLocale("Elerethe Renferal", "ruRU")
 if L then
-	--L.isLinkedWith = "%s is linked with %s"
-	--L.yourLink = "You are linked with %s"
-	--L.yourLinkShort = "Linked with %s"
+	L.isLinkedWith = "%s связан(а) с %s"
+	L.yourLink = "Вы связаны с %s"
+	L.yourLinkShort = "Связаны с %s"
 end
 
 L = BigWigs:NewBossLocale("Il'gynoth", "ruRU")
 if L then
-	--L.custom_off_deathglare_marker = "Deathglare Tentacle marker"
-	--L.custom_off_deathglare_marker_desc = "Mark Deathglare Tentacles with {rt6}{rt5}{rt4}{rt3}, requires promoted or leader.\n|cFFFF0000Only 1 person in the raid should have this enabled to prevent marking conflicts.|r\n|cFFADFF2FTIP: If the raid has chosen you to turn this on, having nameplates enabled or quickly mousing over the spears is the fastest way to mark them.|r"
+	L.custom_off_deathglare_marker = "Маркировка Щупалец смертоносного взгляда"
+	L.custom_off_deathglare_marker_desc = "Помечать Щупальца смертоносного взгляда метками {rt6}{rt5}{rt4}{rt3}, требуется быть помощником или лидером рейда.\n|cFFFF0000Только один человек в рейде должен включить данную опцию чтобы избежать конфликтов.|r\n|cFFADFF2FПодсказка: Проведите мышкой над щупальцами, чтобы быстрее отметить их.|r"
 
-	--L.bloods_remaining = "%d |4Blood:Bloods; remaining"
+	L.bloods_remaining = "%d |4Кровь:Крови; осталось" --huh?
 end
 
 L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "ruRU")
@@ -29,14 +29,14 @@ end
 
 L = BigWigs:NewBossLocale("Ursoc", "ruRU")
 if L then
-	--L.custom_off_gaze_assist = "Focused Gaze Assist"
-	--L.custom_off_gaze_assist_desc = "Show raid icons in bars and messages for Focused Gaze. Using {rt4} for odd, {rt6} for even soaks. Requires promoted or leader."
+	L.custom_off_gaze_assist = "Помощник Пристального взгляда"
+	L.custom_off_gaze_assist_desc = "Отображать рейдовые метки на полосах таймера и сообщениях для Пристального взгляда. Используются {rt4} для нечетных и {rt6} для четных взглядов. Требуется быть помощником или лидером рейда."
 end
 
 L = BigWigs:NewBossLocale("Xavius", "ruRU")
 if L then
-	--L.custom_off_blade_marker = "Nightmare Blade marker"
-	--L.custom_off_blade_marker_desc = "Mark the targets of Nightmare Blades with {rt1}{rt2}, requires promoted or leader."
+	L.custom_off_blade_marker = "Маркер Клинков кошмара"
+	L.custom_off_blade_marker_desc = "Отмечать цели Клинков Кошмара метками {rt1}{rt2}. Требуется быть помощником или лидером рейда."
 
-	--L.linked = "Bonds of Terror on YOU! - Linked with %s!"
+	L.linked = "Узы ужаса на ВАС! - Связаны с %s!"
 end
