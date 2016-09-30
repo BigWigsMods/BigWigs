d8b9f8c7d851c50698a57ece2fae741dd539cf5d
Jon <jongt23@gmail.com>
2016-09-30 15:55:39 +0200

    esES update. (#52)
    [skip ci]

--- Nightmare/Locales/esES.lua
+++ Nightmare/Locales/esES.lua
@@ -2,7 +2,7 @@ local L = BigWigs:NewBossLocale("Cenarius", "esES") or BigWigs:NewBossLocale("Ce
 if not L then return end
 if L then
 	L.forces = "Fuerzas"
-	--L.bramblesSay = "Brambles near %s"
+	L.bramblesSay = "Zarzas cerca %s"
 end
 
 L = BigWigs:NewBossLocale("Elerethe Renferal", "esES") or BigWigs:NewBossLocale("Elerethe Renferal", "esMX")
@@ -17,7 +17,7 @@ if L then
 	L.custom_off_deathglare_marker = "Marcador de Tentáculo ojoletal"
 	L.custom_off_deathglare_marker_desc = "Marca los Tentáculos ojoletal con {rt6}{rt5}{rt4}{rt3}, requiere ayudante o líder.\n|cFFFF0000Sólo 1 persona en la raid debería tener activada esta opción para prevenir conflictos con las marcas.|r\n|cFFADFF2FCONSEJO: Si la raid te elige a ti para activarla, teniendo las placas de nombre activadas o moviendo rápidamente el ratón por encima de los adds es la forma más rápida de marcarlos.|r"
 
-	--L.bloods_remaining = "%d |4Blood:Bloods; remaining"
+	L.bloods_remaining = "Mata %d |4Sangre:Sangres; más"
 end
 
 L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "esES") or BigWigs:NewBossLocale("Emerald Nightmare Trash", "esMX")
@@ -30,7 +30,7 @@ end
 L = BigWigs:NewBossLocale("Ursoc", "esES") or BigWigs:NewBossLocale("Ursoc", "esMX")
 if L then
 	L.custom_off_gaze_assist = "Asistente de Mirada enfocada"
-	L.custom_off_gaze_assist_desc = "Muestra los iconos de raid en barras y mensajes para Mirada enfocada. Using {rt4} for odd, {rt6} for even soaks. Requires promoted or leader."
+	L.custom_off_gaze_assist_desc = "Muestra los iconos de raid en barras y mensajes para Mirada enfocada. Usando {rt4} para los impares, y {rt6} para los pares. Requiere ayudante o líder."
 end
 
 L = BigWigs:NewBossLocale("Xavius", "esES") or BigWigs:NewBossLocale("Xavius", "esMX")
