9d47227b233d93d39d5754b3677d70e96c639d16
funkydude <funkydude@users.noreply.github.com>
2016-10-05 16:25:39 +0100

    BrokenIsles/WitheredJim Cleanups & add locale.

--- BrokenIsles/WitheredJim.lua
+++ BrokenIsles/WitheredJim.lua
@@ -15,8 +15,8 @@ mod.worldBoss = 102075
 
 local L = mod:GetLocale()
 if L then
-	L.custom_on_mark_boss = "Mark the Boss"
-	L.custom_on_mark_boss_desc = "Mark the main Withered Jim with {rt8}. Requires promoted or leader."
+	L.custom_on_mark_boss = "Mark Withered J'im"
+	L.custom_on_mark_boss_desc = "Mark the real Withered J'im with {rt8}, requires promoted or leader."
 	L.custom_on_mark_boss_icon = 8
 end
 
@@ -44,9 +44,11 @@ end
 
 function mod:OnEngage()
 	self:CheckForWipe()
+	self:CDBar(223623, 4) -- Nightshifted Bolts
+	self:CDBar(223614, 13) -- Resonance
+	self:CDBar(223715, 30, 74511, 223715) -- More... MORE MORE MORE!
 	if self:GetOption("custom_on_mark_boss") then
-		self:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
-		self:RegisterEvent("PLAYER_TARGET_CHANGED")
+		self:RegisterTargetEvents("MarkBoss")
 	end
 end
 
@@ -54,47 +56,26 @@ end
 -- Event Handlers
 --
 
-local function checkTarget(unit)
+function mod:MarkBoss(event, unit)
 	local guid = UnitGUID(unit)
-	local mobId = guid and mod:MobId(guid)
+	local mobId = self:MobId(guid)
 	if mobId == 102075 then
-		mod:UnregisterEvent("UPDATE_MOUSEOVER_UNIT")
-		mod:UnregisterEvent("PLAYER_TARGET_CHANGED")
-		if not GetRaidTargetIndex(unit) then
-			SetRaidTarget(unit, 8)
-		end
+		SetRaidTarget(unit, 8)
+		self:UnregisterTargetEvents()
 	end
 end
-function mod:PLAYER_TARGET_CHANGED() checkTarget("target") end
-function mod:UPDATE_MOUSEOVER_UNIT() checkTarget("mouseover") end
 
 function mod:More(args)
 	self:Message(args.spellId, "Neutral", "Info", 74511, args.spellId) -- 74511 = Summon Clone
-	self:CDBar(args.spellId, 31, 74511, args.spellId) -- 30.6-31.0
+	self:CDBar(args.spellId, 30, 74511, args.spellId) -- 30.6-31.0
 end
 
--- The clones also cast Resonance, Nightshifted Bolts, and Nightstable Energy (on a ~31s cd)
--- Could track by guid, but it seems unnecessary (with a bunch of clones up, cast can happen very frequently)
-
-do
-	local prev = 0
-	function mod:NightshiftedBolts(args)
-		local t = GetTime()
-		if t-prev > 3 then
-			prev = t
-			self:Message(args.spellId, "Attention", "Alarm")
-			-- 3s channel, but it can be interrupted, not sure if it stops the effect, though
-		end
-	end
+function mod:NightshiftedBolts(args)
+	self:Message(args.spellId, "Attention", "Alarm")
+	self:CDBar(args.spellId, 31.5)
 end
 
-do
-	local prev = 0
-	function mod:Resonance(args)
-		local t = GetTime()
-		if t-prev > 3 then
-			prev = t
-			self:Message(args.spellId, "Urgent", "Alert")
-		end
-	end
+function mod:Resonance(args)
+	self:Message(args.spellId, "Urgent", "Alert")
+	self:CDBar(args.spellId, 31)
 end
