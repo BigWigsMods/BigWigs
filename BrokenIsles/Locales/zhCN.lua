a35028eaa022ba558baa79d0ee4f806cbf5a7f9f
Adavak LEE <adavak@adavak.com>
2016-10-07 00:14:16 +0800

    zhCN update. (#64)

--- BrokenIsles/Locales/zhCN.lua
+++ BrokenIsles/Locales/zhCN.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Withered J'im", "zhCN")
 if not L then return end
 if L then
-	--L.custom_on_mark_boss = "Mark Withered J'im"
-	--L.custom_on_mark_boss_desc = "Mark the real Withered J'im with {rt8}, requires promoted or leader."
+	L.custom_on_mark_boss = "标记凋零者吉姆"
+	L.custom_on_mark_boss_desc = "使用 {rt8} 标记凋零者吉姆真身，需要权限。"
 end
