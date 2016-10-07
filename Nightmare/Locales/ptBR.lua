faddc92ac411971058f11347e29b25073dd2d10b
david95alex <david95alex@gmail.com>
2016-10-07 11:26:06 -0300

    Update ptBR.lua
    Final version

--- Nightmare/Locales/ptBR.lua
+++ Nightmare/Locales/ptBR.lua
@@ -30,7 +30,7 @@ end
 L = BigWigs:NewBossLocale("Ursoc", "ptBR")
 if L then
 	L.custom_off_gaze_assist = "Assistente de Olhar Concentrado"
-	L.custom_off_gaze_assist_desc = "Mostra marcadores de alvo nas barras e mensagens para Olhar Concentrado. Marca o alvo com {rt4} em casts ímpares e {rt6} em casts pares. Requer assistente ou líder.
+	L.custom_off_gaze_assist_desc = "Mostra marcadores de alvo nas barras e mensagens para Olhar Concentrado. Marca o alvo com {rt4} em casts ímpares e {rt6} em casts pares. Requer assistente ou líder."
 end
 
 L = BigWigs:NewBossLocale("Xavius", "ptBR")
