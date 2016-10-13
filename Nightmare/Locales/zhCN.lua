43e34a4fcdba9ead6312fce9f8f1c4b1ed585181
Adavak LEE <adavak@adavak.com>
2016-10-14 03:14:33 +0800

    zhCN update. (#72)

--- Nightmare/Locales/zhCN.lua
+++ Nightmare/Locales/zhCN.lua
@@ -3,8 +3,8 @@ if not L then return end
 if L then
 	L.forces = "梦魇大军"
 	L.bramblesSay = ">%s< 附近荆棘"
-	--L.custom_off_multiple_breath_bar = "Show multiple Rotten Breath bars"
-	--L.custom_off_multiple_breath_bar_desc = "Per default BigWigs will only show the Rotten Breath bar of one drake. Enable this option if you want to see the timer for each drake."
+	L.custom_off_multiple_breath_bar = "显示多重腐败之息计时条"
+	L.custom_off_multiple_breath_bar_desc = "默认 BigWigs 将只显示一条幼龙的腐败之息。启用此选项将看到每条幼龙的腐败之息。"
 end
 
 L = BigWigs:NewBossLocale("Elerethe Renferal", "zhCN")
@@ -24,17 +24,17 @@ end
 
 L = BigWigs:NewBossLocale("Emerald Nightmare Trash", "zhCN")
 if L then
-	L.gelatinizedDecay = "胶状腐烂"
-	--L.befouler = "Taintheart Befouler"
-	--L.shaman = "Dire Shaman"
-	--L.custom_on_mark_totem = "Mark the Totems"
-	--L.custom_on_mark_totem_desc = "Mark the Totems with {rt8}{rt7}, requires promoted or leader."
+	L.gelatinizedDecay = "腐朽软泥怪"
+	L.befouler = "污心腐蚀者"
+	L.shaman = "可怕的萨满"
+	L.custom_on_mark_totem = "图腾标记"
+	L.custom_on_mark_totem_desc = "使用 {rt8}{rt7} 标记图腾，需要权限。"
 end
 
 L = BigWigs:NewBossLocale("Ursoc", "zhCN")
 if L then
-	L.custom_on_gaze_assist = "专注凝视助手"
-	L.custom_on_gaze_assist_desc = "在计时条和信息显示专注凝视团队标记。使用 {rt4} 和 {rt6} 分别标记。需要权限。"
+	L.custom_off_gaze_assist = "专注凝视助手"
+	L.custom_off_gaze_assist_desc = "在计时条和信息显示专注凝视团队标记。使用 {rt4} 和 {rt6} 分别标记。需要权限。"
 end
 
 L = BigWigs:NewBossLocale("Xavius", "zhCN")
