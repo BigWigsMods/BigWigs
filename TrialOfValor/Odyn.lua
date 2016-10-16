82b63f08feff4aa66d9c6d72ab5d53f95882a99f
elvador <reysptools@googlemail.com>
2016-10-16 02:35:04 +0200

    TrialOfValor/Odyn: Updates after heroic testing

--- TrialOfValor/Odyn.lua
+++ TrialOfValor/Odyn.lua
@@ -2,6 +2,7 @@ if not IsTestBuild() then return end
 
 --------------------------------------------------------------------------------
 -- TODO List:
+-- - P2 -> P3 Transition
 
 
 --------------------------------------------------------------------------------
@@ -18,7 +19,11 @@ mod.respawnTime = 30
 -- Locals
 --
 
-
+local hornCount = 1
+local runesUp = 0
+local myAddGUID = ""
+local isHymdallFighting, isHyrjaFighting = nil, nil
+local revivifyBarTexts = {}
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -26,6 +31,10 @@ mod.respawnTime = 30
 
 local L = mod:GetLocale()
 if L then
+	L.odyn = -14003
+	L.hymdall = -14005
+	L.hyrja = -14005
+
 	L[227490] = "|cFF800080Top Right|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Purple
 	L[227491] = "|cFFFFA500Bottom Right|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
 	L[227498] = "|cFFFFFF00Bottom Left|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
@@ -39,21 +48,46 @@ end
 
 function mod:GetOptions()
 	return {
-		227629, -- Unerring Blast
-		{228162, "SAY", "ICON"}, -- Shield of Light
-		228012, -- Horn of Valor
-		{227807, "SAY"}, -- Storm of Justice
+		--[[ General ]]--
+		"stages",
+
+		--[[ Stage One: Might of the Valarjar ]]--
 		228018, -- Valarjar's Bond
-		{227626, "TANK"}, -- Odyn's Test
-		{228918, "SAY"}, -- Stormforged Spear
 		228171, -- Revivify
+
+		--[[ Hymdall ]]--
+		228012, -- Horn of Valor
+
+		--[[ Hyrja ]]--
 		{228029, "SAY", "ICON"}, -- Expel Light
+		{228162, "SAY", "ICON"}, -- Shield of Light
+
+		--[[ Odyn ]]--
+		227503, -- Draw Power
+		227629, -- Unerring Blast
+		{227626, "TANK"}, -- Odyn's Test
 		-14495, -- Runic Brand
+
+		--[[ Stage Two: The Prime Designate ]]--
+		-14404, -- Test of the Ages
+
+		--[[ Stage Three: The Final Test ]]--
+		{228918, "SAY"}, -- Stormforged Spear
+		{227807, "SAY"}, -- Storm of Justice
 		227475, -- Cleansing Flame
+	},{
+		["stages"] = "general",
+		[228018] = -14002, -- Stage One: Might of the Valarjar
+		[228012] = -14005, -- Hymdall
+		[228029] = -14006, -- Hyrja
+		[227503] = -14003, -- Odyn
+		[-14404] = -14010, -- Stage Two: The Prime Designate
+		[228918] = -14011, -- Stage Three: The Final Test
 	}
 end
 
 function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 	self:Log("SPELL_CAST_START", "UnerringBlast", 227629)
 	self:Log("SPELL_CAST_START", "ShieldOfLight", 228162)
 	self:Log("SPELL_CAST_SUCCESS", "ShieldOfLightEnd", 228162)
@@ -66,25 +100,93 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_REMOVED", "ExpelLightRemoved", 228029)
 	self:Log("SPELL_CAST_START", "Revivify", 228171)
 
-	self:Log("SPELL_AURA_APPLIED", "Branded", 227491, 227498, 227490, 227500, 227499)
+	self:Log("SPELL_AURA_APPLIED", "DrawPower", 227976, 227974, 227961, 227975, 227973) -- Buffs on Odyn when Runes are up
+	self:Log("SPELL_AURA_REMOVED", "DrawPowerRemoved", 227976, 227974, 227961, 227975, 227973) -- Buffs on Odyn when Runes are up
+	self:Log("SPELL_AURA_APPLIED", "Branded", 227491, 227498, 227490, 227500, 227499) -- Debuffs on players
+	self:Log("SPELL_AURA_APPLIED", "BrandedRemoved", 227491, 227498, 227490, 227500, 227499) -- Debuffs on players
+	self:Log("SPELL_CAST_SUCCESS", "RunicShield", 227594, 227595, 227596, 227597, 227598) -- Add regains shield
 
-	self:Log("SPELL_AURA_APPLIED", "CleansingFlameDamage", 227475)
-	self:Log("SPELL_PERIODIC_DAMAGE", "CleansingFlameDamage", 227475)
-	self:Log("SPELL_PERIODIC_MISSED", "CleansingFlameDamage", 227475)
+	self:Log("SPELL_AURA_APPLIED", "CleansingFlameDamage", 227475) -- ?
+	self:Log("SPELL_PERIODIC_DAMAGE", "CleansingFlameDamage", 227475) -- ?
+	self:Log("SPELL_PERIODIC_MISSED", "CleansingFlameDamage", 227475) -- ?
 end
 
 function mod:OnEngage()
-	self:CDBar(228012, 8) -- Horn of Valor
-	self:CDBar(228162, 24) -- Shield of Light
+	hornCount = 1
+	runesUp = 0
+	myAddGUID = ""
+	isHymdallFighting = true
+	isHyrjaFighting = true
+	wipe(revivifyBarTexts)
+	self:Bar(228012, 8) -- Horn of Valor
+	self:Bar(228162, 24) -- Shield of Light
+	self:Bar(228028, 32) -- Expel Light
+	self:Bar(227503, 40) -- Draw Power
+	self:Bar(227629, 70) -- Unerring Blast
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, _, spellId)
+	if spellId == 229168 then -- Test for Players (Phase 1 end)
+		self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+		self:Message("stages", "Neutral", "Long", CL.stage:format(2), false)
+		for _,barText in pairs(revivifyBarTexts) do
+			self:StopBar(barText)
+		end
+		self:Bar("stages", 5.6, self:SpellName(L.odyn))
+		isHymdallFighting = nil
+		isHyrjaFighting = nil
+	elseif spellId == 227882 then -- Leap into Battle (Phase 2 start)
+		self:Bar(-14404, 16, L.hyrja) -- could be percentage based
+		self:Bar(227503, 43) -- Draw Power
+		self:Bar(227629, 73) -- Unerring Blast
+	end
+end
+
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	local hymdallFound, hyrjaFound = nil, nil
+	for i = 1, 5 do
+		local unit = ("boss%d"):format(i)
+		local guid = UnitGUID(unit)
+		if guid then
+			local mobId = self:MobId(guid)
+			if mobId == 114361 then -- Hymdall
+				hymdallFound = true
+				if not isHymdallFighting then
+					isHymdallFighting = true
+					self:Message(-14404, "Neutral", "Info", self:SpellName(L.hymdall), false)
+					self:CDBar(228012, 10) -- Horn of Valor
+				end
+			elseif mobId == 114360 then
+				hyrjaFound = true
+				if not isHyrjaFighting then
+					isHyrjaFighting = true
+					self:Message(-14404, "Neutral", "Info", self:SpellName(L.hyrja), false)
+					self:CDBar(228029, 5) -- Expel Light
+					self:CDBar(228162, 9.5) -- Shield of Light
+				end
+			end
+		end
+	end
+	if not hymdallFound then
+		isHymdallFighting = nil
+		self:StopBar(228012)
+	end
+	if not hyrjaFound then
+		isHyrjaFighting = nil
+		self:StopBar(228162) -- Shield of Light
+		self:StopBar(228029) -- Expel Light
+	end
+end
+
 function mod:UnerringBlast(args)
 	self:Message(args.spellId, "Urgent", "Alert", CL.casting:format(args.spellName))
 	self:Bar(args.spellId, 3, CL.cast:format(args.spellName))
+	self:Bar(227503, 40) -- Draw Power
+	self:Bar(args.spellId, 70)
 end
 
 do
@@ -94,11 +196,14 @@ do
 		end
 		self:PrimaryIcon(228162, player)
 		self:TargetMessage(228162, player, "Important", "Alarm", nil, nil, true)
+		self:TargetBar(228162, 4, player)
 	end
+
 	function mod:ShieldOfLight(args)
 		self:GetBossTarget(printTarget, 0.4, args.sourceGUID)
 		self:CDBar(args.spellId, 32)
 	end
+
 	function mod:ShieldOfLightEnd(args)
 		self:PrimaryIcon(args.spellId)
 	end
@@ -106,7 +211,8 @@ end
 
 function mod:HornOfValor(args)
 	self:Message(args.spellId, "Urgent", "Alert", CL.casting:format(args.spellName))
-	self:CDBar(args.spellId, 32)
+	hornCount = hornCount + 1
+	self:CDBar(args.spellId, hornCount % 2 == 0 and 27 or 43)
 end
 
 function mod:StormOfJustice(args)
@@ -143,6 +249,7 @@ do
 			self:Say(args.spellId)
 		end
 	end
+
 	function mod:ExpelLightRemoved(args)
 		self:PrimaryIcon(args.spellId)
 	end
@@ -150,11 +257,43 @@ end
 
 function mod:Revivify(args)
 	self:TargetMessage(args.spellId, args.sourceName, "Positive", "Long")
+	local text = CL.other:format(args.sourceName, args.spellName)
+	revivifyBarTexts[#revivifyBarTexts+1] = text
+	self:Bar(args.spellId, 10, text)
+end
+
+do
+	local prev = 0
+	function mod:DrawPower()
+		runesUp = runesUp + 1
+		local t = GetTime()
+		if t-prev > 5 then
+			self:Message(227503, "Attention", "Long")
+		end
+	end
+end
+
+function mod:DrawPowerRemoved()
+	runesUp = runesUp - 1
+	self:Message(227503, "Positive", nil, CL.add_remaining:format(runesUp))
 end
 
 function mod:Branded(args)
 	if self:Me(args.destGUID) then
 		self:Message(-14495, "Personal", "Warning", L[args.spellId], args.spellId)
+		myAddGUID = args.sourceGUID
+	end
+end
+
+function mod:BrandedRemoved(args)
+	if self:Me(args.destGUID) then
+		myAddGUID = ""
+	end
+end
+
+function mod:RunicShield(args)
+	if args.sourceGUID == myAddGUID then
+		self:Message(-14495, "Personal", "Warning", CL.on:format(args.spellName, args.sourceName), args.spellId)
 	end
 end
 
