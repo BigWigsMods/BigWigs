1a21137b80c0c51132741ba558906c5716630e08
funkydude <funkydude@users.noreply.github.com>
2016-10-17 00:42:33 +0100

    bump version

--- Loader.lua
+++ Loader.lua
@@ -7,7 +7,7 @@ local bwFrame = CreateFrame("Frame")
 -- Generate our version variables
 --
 
-local BIGWIGS_VERSION = 18
+local BIGWIGS_VERSION = 19
 local BIGWIGS_RELEASE_STRING = ""
 local versionQueryString, versionResponseString = "Q:%d-%s", "V:%d-%s"
 
