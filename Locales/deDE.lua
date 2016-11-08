df9dd1177218fa200844ad3c84d20ab63f8c3d92
bullsei <bullsei@users.noreply.github.com>
2016-11-08 17:42:15 +0100

    Update deDE.lua

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -12,10 +12,10 @@ L.options = "Optionen"
 L.raidBosses = "Schlachtzugsbosse"
 L.dungeonBosses = "Dungeonbosse"
 
---L.infobox = "Information Box"
---L.infobox_desc = "Display a box with information related to the encounter."
---L.INFOBOX = L.infobox
---L.INFOBOX_desc = L.infobox_desc
+L.infobox = "Informationsbox"
+L.infobox_desc = "Zeigt eine Box mit Informationen zur Bossbegegnung an"
+L.INFOBOX = L.infobox
+L.INFOBOX_desc = L.infobox_desc
 
 L.activeBossModules = "Aktive Bossmodule:"
 L.advanced = "Erweiterte Optionen"
