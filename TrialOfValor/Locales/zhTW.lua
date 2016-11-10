7f6e95a2aa8819dc7ce4b922727236c85b6ff640
Adavak LEE <adavak@adavak.com>
2016-11-10 10:08:30 +0800

    Update zhTW.lua

--- TrialOfValor/Locales/zhTW.lua
+++ TrialOfValor/Locales/zhTW.lua
@@ -5,6 +5,6 @@ if L then
 	L[227491] = "|cFFFFA500右下|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
 	L[227498] = "|cFFFFFF00左下|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
 	L[227499] = "|cFF0000FF左上|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Blue
-  L[227500] = "|cFF008000上|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
+	L[227500] = "|cFF008000上|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
 end
 
