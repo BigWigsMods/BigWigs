71552af68a4678444b635d627ff4944450fa96a5
Adavak LEE <adavak@adavak.com>
2016-11-10 19:24:12 +0800

    zhCN/zhTW update. (#93)

--- TrialOfValor/Locales/zhTW.lua
+++ TrialOfValor/Locales/zhTW.lua
@@ -1,6 +1,10 @@
 local L = BigWigs:NewBossLocale("Odyn-TrialOfValor", "zhTW")
 if not L then return end
 if L then
-
+	L[227490] = "|cFF800080右上|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Purple
+	L[227491] = "|cFFFFA500右下|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
+	L[227498] = "|cFFFFFF00左下|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
+	L[227499] = "|cFF0000FF左上|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Blue
+	L[227500] = "|cFF008000上|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
 end
 
