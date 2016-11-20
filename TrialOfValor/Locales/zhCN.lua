dfb8a74ff92f61d9ac36afd5125c0f622fdb305d
funkydude <funkydude@users.noreply.github.com>
2016-11-20 16:43:59 +0000

    Update zhCN/zhTW locale

--- TrialOfValor/Locales/zhCN.lua
+++ TrialOfValor/Locales/zhCN.lua
@@ -1,20 +1,20 @@
 local L = BigWigs:NewBossLocale("Odyn-TrialOfValor", "zhCN")
 if not L then return end
 if L then
-	--L.yields = "%s yields" -- used when Hymdall and Helya leave the fight in P2: "Hymdall yields"
+	L.yields = "%s离场" -- used when Hymdall and Helya leave the fight in P2: "Hymdall yields"
 	L[227490] = "|cFF800080右上|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Purple
 	L[227491] = "|cFFFFA500右下|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
 	L[227498] = "|cFFFFFF00左下|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
 	L[227499] = "|cFF0000FF左上|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Blue
-	L[227500] = "|cFF008000上|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
+	L[227500] = "|cFF008000正上|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
 end
 
 L = BigWigs:NewBossLocale("Helya-TrialOfValor", "zhCN")
 if L then
-	--L.nearTrigger = "near" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges near Helya! -- Translate this comment with the emote
-	--L.farTrigger = "far" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges far from Helya! -- Translate this comment with the emote
-	L.tentacle_near = "海拉“近点”触须"
-	L.tentacle_near_desc = "此选项可用于醒目或隐藏触须打击信息刷新在海拉近点。"
-	L.tentacle_far = "海拉“远点”触须"
-	L.tentacle_far_desc = "此选项可用于醒目或隐藏触须打击信息刷新在海拉远点。"
+	L.nearTrigger = "附近" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t一头%s出现在海拉的附近！
+	L.farTrigger = "远处" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t一头%s出现在海拉的远处！
+	L.tentacle_near = "海拉“附近”触须"
+	L.tentacle_near_desc = "此选项可用于醒目或隐藏触须打击信息刷新在海拉附近。"
+	L.tentacle_far = "海拉“远处”触须"
+	L.tentacle_far_desc = "此选项可用于醒目或隐藏触须打击信息刷新在海拉远处。"
 end
--- TrialOfValor/Locales/zhTW.lua
+++ TrialOfValor/Locales/zhTW.lua
@@ -1,20 +1,20 @@
 local L = BigWigs:NewBossLocale("Odyn-TrialOfValor", "zhTW")
 if not L then return end
 if L then
-	--L.yields = "%s yields" -- used when Hymdall and Helya leave the fight in P2: "Hymdall yields"
+	L.yields = "%s撤退" -- used when Hymdall and Helya leave the fight in P2: "Hymdall yields"
 	L[227490] = "|cFF800080右上|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Purple
 	L[227491] = "|cFFFFA500右下|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
 	L[227498] = "|cFFFFFF00左下|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
 	L[227499] = "|cFF0000FF左上|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Blue
-	L[227500] = "|cFF008000上|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
+	L[227500] = "|cFF008000正上|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
 end
 
 L = BigWigs:NewBossLocale("Helya-TrialOfValor", "zhTW")
 if L then
-	--L.nearTrigger = "near" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges near Helya! -- Translate this comment with the emote
-	--L.farTrigger = "far" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges far from Helya! -- Translate this comment with the emote
-	--L.tentacle_near = "Tentacle NEAR Helya"
-	--L.tentacle_near_desc = "This option can be used to emphasize or hide the messages when a Striking Tentacle spawns near Helya."
-	--L.tentacle_far = "Tentacle FAR from Helya"
-	--L.tentacle_far_desc = "This option can be used to emphasize or hide the messages when a Striking Tentacle spawns far from Helya."
+	L.nearTrigger = "附近" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t一個進擊的觸手在黑爾雅附近出現了！
+	L.farTrigger = "較遠" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t一個進擊的觸手在離黑爾雅較遠的地方出現了！
+	L.tentacle_near = "近處觸手提示"
+	L.tentacle_near_desc = "提示出現在黑爾雅近處的觸手，調整這個選項可以強調或隱蔵。"
+	L.tentacle_far = "遠處觸手提示"
+	L.tentacle_far_desc = "提示出現在黑爾雅遠處的觸手，調整這個選項可以強調或隱蔵。"
 end
