5129f5441057602a0d82e6e829fcf4b1baa17c34
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2016-11-30 11:26:05 +0100

    TrialOfValor/Odyn: Add icon to Stormforged Spear

--- TrialOfValor/Odyn.lua
+++ TrialOfValor/Odyn.lua
@@ -104,7 +104,7 @@ function mod:GetOptions()
 		-14404, -- Test of the Ages
 
 		--[[ Stage Three: The Final Test ]]--
-		{228918, "SAY"}, -- Stormforged Spear
+		{228918, "SAY", "ICON"}, -- Stormforged Spear
 		{227807, "SAY"}, -- Storm of Justice
 		227475, -- Cleansing Flame
 		{197961, "PROXIMITY", "SAY", "PULSE"}, -- Runic Brand
@@ -358,6 +358,7 @@ function mod:StormforgedSpear(args)
 	self:TargetMessage(args.spellId, args.destName, "Important", "Alarm")
 	self:TargetBar(args.spellId, 6, args.destName)
 	self:Bar(args.spellId, spearCount % 3 == 0 and 13.5 or 11)
+	self:PrimaryIcon(args.spellId, args.destName)
 	if self:Me(args.destGUID) then
 		self:Say(args.spellId)
 	end
