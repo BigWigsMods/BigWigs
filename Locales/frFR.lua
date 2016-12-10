e0d3378a7d8a2545ca4394b287fe8bc4b4560ee3
Grow <tborlee@users.noreply.github.com>
2016-12-10 17:02:44 +0100

    frFR Update (#131)

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -17,7 +17,7 @@ L.infobox_desc = "Affiche une boîte d'information concernant la rencontre."
 L.INFOBOX = L.infobox
 L.INFOBOX_desc = L.infobox_desc
 
---L.disabledAddOn = "You have the |cFF436EEE%s|r addon disabled, timers will not be shown."
+L.disabledAddOn = "L'addon |cFF436EEE%s|r est désactivé, les délais ne seront pas affichés."
 
 L.activeBossModules = "Modules de boss actifs :"
 L.advanced = "Options avancées"
