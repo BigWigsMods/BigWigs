3960e15d748af5964652db0bfe49f504dba25c64
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2016-12-26 02:52:05 -0300

    Review and update ptBR translation
    The ptBR translation was completed, all the phrases are now translated.
    Also some phrases have been changed to be more meaningfull, correct machine translations or take into consideration localization differences from enUS and ptPT.

--- TrialOfValor/Locales/ptBR.lua
+++ TrialOfValor/Locales/ptBR.lua
@@ -1,37 +1,37 @@
-local L = BigWigs:NewBossLocale("Odyn-TrialOfValor", "ptBR")
-if not L then return end
+local L = BigWigs:NewBossLocale("Guarm-TrialOfValor", "ptBR")
 if L then
-	--L.yields = "%s yields" -- used when Hymdall and Helya leave the fight in P2: "Hymdall yields"
-	--L[227490] = "|cFF800080Top Right|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Purple
-	--L[227491] = "|cFFFFA500Bottom Right|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
-	--L[227498] = "|cFFFFFF00Bottom Left|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
-	--L[227499] = "|cFF0000FFTop Left|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Blue
-	--L[227500] = "|cFF008000Top|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
-end
-
-L = BigWigs:NewBossLocale("Guarm-TrialOfValor", "ptBR")
-if L then
-	--L.lick = "Lick" -- For translators: common name of 228248, 228253, 228228
-	--L.lick_desc = "Show bars for the different licks."
+	L.lick = "Lambida"
+	L.lick_desc = "Mostra barras para as lambidas."
 end
 
 L = BigWigs:NewBossLocale("Helya-TrialOfValor", "ptBR")
 if L then
-	--L.nearTrigger = "near" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges near Helya! -- Translate this comment with the emote
-	--L.farTrigger = "far" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges far from Helya! -- Translate this comment with the emote
-	--L.tentacle_near = "Tentacle NEAR Helya"
-	--L.tentacle_near_desc = "This option can be used to emphasize or hide the messages when a Striking Tentacle spawns near Helya."
-	--L.tentacle_far = "Tentacle FAR from Helya"
-	--L.tentacle_far_desc = "This option can be used to emphasize or hide the messages when a Striking Tentacle spawns far from Helya."
+	L.nearTrigger = "perto de" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges near Helya! -- Translate this comment with the emote
+	L.farTrigger = "longe de" -- |TInterface\\Icons\\inv_misc_monsterhorn_03.blp:20|t A %s emerges far from Helya! -- Translate this comment with the emote
+	L.tentacle_near = "Tentáculo PERTO DE Helya"
+	L.tentacle_near_desc = "Esta opção pode ser usada para enfatizar ou esconder a mensagem quando uma Batida com Tentáculo aparece perto de Heyla."
+	L.tentacle_far = "Tentáculo LONGE DE Helya"
+	L.tentacle_far_desc = "Esta opção pode ser usada para enfatizar ou esconder a mensagem quando uma Batida com Tentáculo aparece longe de Heyla."
 
-	--L.orb_melee = "Orb: Melee timer"
-	--L.orb_melee_desc = "Show the timer for the Orbs that spawn on Melee."
-	--L.orb_melee_bar = "Melee Orb"
+	L.orb_melee = "Orbe: contador de Corpo a corpo" -- TODO: needs reviewing
+	L.orb_melee_desc = "Mostra um contador para Orbes que surgem em jogadores corpo a corpo."-- TODO: needs reviewing
+	L.orb_melee_bar = "Orbe corpo a corpo"-- TODO: needs reviewing
 
-	--L.orb_ranged = "Orb: Ranged timer"
-	--L.orb_ranged_desc = "Show the timer for the Orbs that spawn on Ranged."
-	--L.orb_ranged_bar = "Ranged Orb"
+	L.orb_ranged = "Orbe: contador de Distância"-- TODO: needs reviewing
+	L.orb_ranged_desc = "Mostra um contador para Orbes que surgem em jogadores a distância."-- TODO: needs reviewing
+	L.orb_ranged_bar = "Orbe a distância"-- TODO: needs reviewing
 
 	L.orb_say = "Orbe"
 	L.taint_say = "Mácula"
+end
+
+L = BigWigs:NewBossLocale("Odyn-TrialOfValor", "ptBR")
+if not L then return end
+if L then
+	L.yields = "%s cedeu" -- used when Hymdall and Helya leave the fight in P2: "Hymdall yields"
+	L[227490] = "|cFF800080Cima Direita|r (|T1323037:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Purple
+	L[227491] = "|cFFFFA500Baixo Direita|r (|T1323039:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Orange
+	L[227498] = "|cFFFFFF00Baixo Esquerda|r (|T1323038:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Yellow
+	L[227499] = "|cFF0000FFCima Esquerda|r (|T1323035:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Blue
+	L[227500] = "|cFF008000Cima|r (|T1323036:15:15:0:0:64:64:4:60:4:60|t)" -- Boss_OdunRunes_Green
 end
