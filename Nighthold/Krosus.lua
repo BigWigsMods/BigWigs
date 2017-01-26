6c77cfcad22087425f689b8008328f32dc5d7ee2
elvador <reysptools@googlemail.com>
2017-01-26 16:46:24 +0100

    Krosus: minor tweak

--- Nighthold/Krosus.lua
+++ Nighthold/Krosus.lua
@@ -171,7 +171,7 @@ do
 	local prev = 0
 	function mod:OrbOfDescructionApplied(args)
 		self:TargetMessage(args.spellId, args.destName, "Urgent", "Alarm", CL.count:format(args.spellName, orbCount))
-		self:TargetBar(args.spellId, 5, args.destName)
+		self:TargetBar(args.spellId, 5, args.destName, 230932) -- Orb
 		if self:Me(args.destGUID) then
 			self:Flash(args.spellId)
 			self:Say(args.spellId)
