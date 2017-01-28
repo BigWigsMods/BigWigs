e6343ccb197e86f05172955339be72ee72ec0b68
EKE00372 <eke00372@gmail.com>
2017-01-29 06:10:31 +0800

    Update zhTW.lua

--- Nighthold/Locales/zhTW.lua
+++ Nighthold/Locales/zhTW.lua
@@ -1,6 +1,13 @@
 local L = BigWigs:NewBossLocale("Skorpyron", "zhTW")
 if not L then return end
 if L then
-
+	L.blue = "藍"
+	L.red = "紅"
+	L.green = "綠"
+	L.mode = "%s階段"  
 end
 
+local L = BigWigs:NewBossLocale("Krosus", "zhTW")
+if L then
+	L.smashingBridge = "斷橋"
+end
