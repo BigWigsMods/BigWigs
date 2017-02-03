b95de9211bc902d5e07f0d28b57fb27c3bed5b80
EKE00372 <eke00372@gmail.com>
2017-02-03 21:36:16 +0800

    Update zhTW (#158)

--- Nighthold/Locales/zhTW.lua
+++ Nighthold/Locales/zhTW.lua
@@ -1,6 +1,23 @@
-local L = BigWigs:NewBossLocale("Skorpyron", "zhTW")
+local L = BigWigs:NewBossLocale("Krosus", "zhTW")
 if not L then return end
 if L then
+	L.leftBeam = "左手光束"
+	L.rightBeam = "右手光束"
 
+	L.goRight = "> 往右 >"
+	L.goLeft = "< 往左 <"
+
+	L.smashingBridge = "斷橋"
+	L.smashingBridge_desc = "斷橋計時器。啟用此選項可強調或倒數。"
+	L.smashingBridge_icon = 205862
+
+	L.removedFromYou = "%s結束了"
 end
 
+L = BigWigs:NewBossLocale("Skorpyron", "zhTW")
+if L then
+	L.blue = "藍色"
+	L.red = "紅色"
+	L.green = "綠色"
+	L.mode = "%s模式"
+end
