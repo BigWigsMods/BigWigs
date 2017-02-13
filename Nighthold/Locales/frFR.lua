8dfdb511a8e3211f76391638d8987efd0e4be7b5
Grow <tborlee@users.noreply.github.com>
2017-02-13 22:59:38 +0100

    frFR Update (#166)

--- Nighthold/Locales/frFR.lua
+++ Nighthold/Locales/frFR.lua
@@ -1,76 +1,76 @@
 local L = BigWigs:NewBossLocale("Skorpyron", "frFR")
 if not L then return end
 if L then
-	--L.blue = "Blue"
-	--L.red = "Red"
-	--L.green = "Green"
-	--L.mode = "%s Mode"
+	L.blue = "Bleu"
+	L.red = "Rouge"
+	L.green = "Vert"
+	L.mode = "Mode %s"
 end
 
 L = BigWigs:NewBossLocale("Chronomatic Anomaly", "frFR")
 if L then
-	--L.affected = "Affected"
-	--L.totalAbsorb = "Total Absorb"
+	L.affected = "Affecté"
+	L.totalAbsorb = "Absorption totale"
 end
 
 L = BigWigs:NewBossLocale("Trilliax", "frFR")
 if L then
-	--L.yourLink = "You are linked with %s"
-	--L.yourLinkShort = "Linked with %s"
-	--L.imprint = "Imprint"
+	L.yourLink = "Vous êtes lié(s) avec %s"
+	L.yourLinkShort = "Lié(e) avec %s"
+	L.imprint = "Double"
 end
 
 L = BigWigs:NewBossLocale("Tichondrius", "frFR")
 if L then
-	--L.addsKilled = "Adds killed"
+	L.addsKilled = "Adds tués"
 	--L.gotEssence = "Got Essence"
 
-	--L.adds_desc = "Timers and warnings for the add spawns."
+	L.adds_desc = "Délais et alertes concernant l'apparition des renforts."
 	--L.adds_trigger1 = "Underlings! Get in here!"
 	--L.adds_trigger2 = "Show these pretenders how to fight!"
 end
 
 L = BigWigs:NewBossLocale("Krosus", "frFR")
 if L then
-	--L.leftBeam = "Left Beam"
-	--L.rightBeam = "Right Beam"
+	L.leftBeam = "Faisceau de gauche"
+	L.rightBeam = "Faisceau de droite"
 
-	--L.goRight = "> GO RIGHT >"
-	--L.goLeft = "< GO LEFT <"
+	L.goRight = "> ALLEZ A DROITE>"
+	L.goLeft = "< ALLEZ À GAUCHE <"
 
-	--L.smashingBridge = "Smashing Bridge"
-	--L.smashingBridge_desc = "Slams which break the bridge. You can use this option to emphasize or enable countdown."
+	L.smashingBridge = "Destruction du pont"
+	L.smashingBridge_desc = "Heurtoirs détruisant le pont. Vous pouvez utiliser cette option comme mise en évidence ou pour activer un compte à rebours."
 
 	--L.removedFromYou = "%s removed from you" -- "Searing Brand removed from YOU!"
 end
 
 L = BigWigs:NewBossLocale("Star Augur Etraeus", "frFR")
 if L then
-	--L.yourSign = "Your sign"
-	--L.with = "with"
-	--L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00Crab|r"
-	--L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000Wolf|r"
-	--L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00Hunter|r"
-	--L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFFDragon|r"
+	L.yourSign = "Votre signe"
+	L.with = "avec"
+	L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00Crabe|r"
+	L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000Loup|r"
+	L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00Chasseur|r"
+	L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFFDragon|r"
 
-	--L.nameplate_requirement = "This feature is currently only supported by KuiNameplates. Mythic only."
+	L.nameplate_requirement = "Cette fonctionnalité est actuellement uniquement supportée par KuiNameplates. Mythique uniquement."
 
-	--L.custom_off_icy_ejection_nameplates = "Show {206936} on friendly nameplates" -- Icy Ejection
-	--L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_off_icy_ejection_nameplates = "Afficher {206936} sur les barres d'info des unités amicales" -- Icy Ejection
+	L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_fel_ejection_nameplates = "Show {205649} on friendly nameplates" -- Fel Ejection
-	--L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_on_fel_ejection_nameplates = "Afficher {205649} sur les barres d'info des unités amicales" -- Fel Ejection
+	L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_gravitational_pull_nameplates = "Show {214335} on friendly nameplates" -- Gravitational Pull
-	--L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
+	L.custom_on_gravitational_pull_nameplates = "Afficher {214335} sur les barres d'info des unités amicales" -- Gravitational Pull
+	L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_grand_conjunction_nameplates = "Show {205408} on friendly nameplates" -- Grand Conjunction
-	--L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
+	L.custom_on_grand_conjunction_nameplates = "Afficher {205408} sur les barres d'info des unités amicales" -- Grand Conjunction
+	L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
 end
 
 L = BigWigs:NewBossLocale("Grand Magistrix Elisande", "frFR")
 if L then
-	--L.elisande = "Elisande"
+	L.elisande = "Elisande"
 
 	--L.ring_msg = "Let the waves of time crash over you!"
 	--L.orb_msg = "You'll find time can be quite volatile."
@@ -86,32 +86,32 @@ end
 L = BigWigs:NewBossLocale("Nighthold Trash", "frFR")
 if L then
 	--[[ Skorpyron to Trilliax ]]--
-	--L.torm = "Torm the Brute"
-	--L.fulminant = "Fulminant"
-	--L.pulsauron = "Pulsauron"
+	L.torm = "Torm le Brutal"
+	L.fulminant = "Fulminant"
+	L.pulsauron = "Pulsauron"
 
 	--[[ Chronomatic Anomaly to Trilliax ]]--
-	--L.sludgerax = "Sludgerax"
+	L.sludgerax = "Bourberax"
 
 	--[[ Trilliax to Aluriel ]]--
-	--L.karzun = "Kar'zun"
-	--L.guardian = "Gilded Guardian"
-	--L.battle_magus = "Duskwatch Battle-Magus"
-	--L.chronowraith = "Chronowraith"
-	--L.protector = "Nighthold Protector"
+	L.karzun = "Kar'zun"
+	L.guardian = "Gardien doré"
+	L.battle_magus = "Magus de bataille de la Garde crépusculair"
+	L.chronowraith = "Chronâme en peine"
+	L.protector = "Protecteur du palais Sacrenuit"
 
 	--[[ Aluriel to Etraeus ]]--
-	--L.jarin = "Astrologer Jarin"
+	L.jarin = "Astrologue Jarin"
 
 	--[[ Aluriel to Telarn ]]--
-	--L.weaver = "Duskwatch Weaver"
-	--L.archmage = "Shal'dorei Archmage"
-	--L.manasaber = "Domesticated Manasaber"
+	L.weaver = "Tisserand de la Garde crépusculaire"
+	L.archmage = "Archimage shal’dorei"
+	L.manasaber = "Sabre-de-mana domestiqué"
 
 	--[[ Aluriel to Krosos ]]--
-	--L.infernal = "Searing Infernal"
+	L.infernal = "Infernal incendiaire"
 
 	--[[ Aluriel to Tichondrius ]]--
-	--L.watcher = "Abyss Watcher"
+	L.watcher = "Guetteur des abîmes"
 end
 
