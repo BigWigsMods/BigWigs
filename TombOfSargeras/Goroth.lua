3cd0784d0f9f9bee9fefb73bd4bc71621c773b22
Justw8 <Justw8@users.noreply.github.com>
2017-02-14 18:37:21 +0100

    Tomb of Sargeras skeleton (#165)

---
+++ TombOfSargeras/Goroth.lua
@@ -0,0 +1,43 @@
+if not IsTestBuild() then return end -- XXX dont load on live
+--------------------------------------------------------------------------------
+-- TODO List:
+
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("Goroth", 1147, 1862) 
+if not mod then return end
+mod:RegisterEnableMob(115844) -- XXX Guestimate
+mod.engageId = 2032
+--mod.respawnTime = 30
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		"berserk",
+	}
+end
+
+function mod:OnBossEnable()
+end
+
+function mod:OnEngage()
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
---
+++ TombOfSargeras/TheDesolateHost.lua
@@ -0,0 +1,43 @@
+if not IsTestBuild() then return end -- XXX dont load on live
+--------------------------------------------------------------------------------
+-- TODO List:
+
+--------------------------------------------------------------------------------
+-- Module Declaration
+--
+
+local mod, CL = BigWigs:NewBoss("The Desolate Host", 1147, 1896)
+if not mod then return end
+mod:RegisterEnableMob(118460, 118462, 119072) -- XXX Guestimate
+mod.engageId = 2054
+--mod.respawnTime = 30
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:GetLocale()
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
+
+function mod:GetOptions()
+	return {
+		"berserk",
+	}
+end
+
+function mod:OnBossEnable()
+end
+
+function mod:OnEngage()
+end
+
+--------------------------------------------------------------------------------
+-- Event Handlers
+--
