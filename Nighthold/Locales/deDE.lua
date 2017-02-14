0830c1fdeaf462b6a9fc40fff654144fddd058fa
funkydude <funkydude@users.noreply.github.com>
2017-02-14 02:10:33 +0000

    Nighthold/Guldan: Update locale.

--- Nighthold/Locales/deDE.lua
+++ Nighthold/Locales/deDE.lua
@@ -80,7 +80,8 @@ end
 
 L = BigWigs:NewBossLocale("Gul'dan", "deDE")
 if L then
-	L[211152] = "(E) %s" -- (E) Eye of Gul'dan
+	--L[211152] = "(E) %s" -- (E) Eye of Gul'dan
+	--L.gains = "Gul'dan gains %s"
 end
 
 L = BigWigs:NewBossLocale("Nighthold Trash", "deDE")
