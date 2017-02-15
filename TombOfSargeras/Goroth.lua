a4482aa5bfb642bf4f838ceb867e4e60a9d45d88
Justw8 <Justw8@users.noreply.github.com>
2017-02-15 18:28:24 +0100

    Tomb of Sargeras: Update Goroth & Harjatan (#171)

--- TombOfSargeras/Goroth.lua
+++ TombOfSargeras/Goroth.lua
@@ -1,4 +1,5 @@
 if not IsTestBuild() then return end -- XXX dont load on live
+
 --------------------------------------------------------------------------------
 -- TODO List:
 
@@ -8,13 +9,17 @@ if not IsTestBuild() then return end -- XXX dont load on live
 
 local mod, CL = BigWigs:NewBoss("Goroth", 1147, 1862) 
 if not mod then return end
-mod:RegisterEnableMob(115844) -- XXX Guestimate
-mod.engageId = 2032
---mod.respawnTime = 30
+mod:RegisterEnableMob(115844)
+mod.engageId = 2032 
+mod.respawnTime = 15
 
 --------------------------------------------------------------------------------
 -- Locals
 --
+
+local burningCount = 1
+local shatteringCount = 1
+local shatteringTimers = {24, 60, 60, 50}
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -28,16 +33,116 @@ local L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		{231363, "SAY"}, -- Burning Armor
+		{230345, "SAY"}, -- Crashing Comet
+		233021, -- Infernal Spike
+		{233279, "FLASH", "SAY"}, -- Shattering Star
+		233062, -- Infernal Burning
+		234346, -- Fel Eruption
+		238588, -- Rain of Brimstone
+	},{
+		[231363] = "general",
+		[238588] = "mythic",
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_AURA_APPLIED", "BurningArmor", 231363)		
+	self:Log("SPELL_CAST_SUCCESS", "CrashingComent", 230345)
+	self:Log("SPELL_AURA_APPLIED", "CrashingCometDebuff", 232249)	
+	self:Log("SPELL_CAST_SUCCESS", "InfernalSpike", 233021)	
+	--self:Log("SPELL_CAST_SUCCESS", "ShatteringStar", 233279)
+	self:Log("SPELL_AURA_APPLIED", "ShatteringStarDebuff", 233272)	
+	self:Log("SPELL_CAST_START", "InfernalBurning", 233062)	
+	
+	self:Log("SPELL_AURA_APPLIED", "FelPool", 230348) -- Fel Pool
+	self:Log("SPELL_PERIODIC_DAMAGE", "FelPool", 230348)
+	self:Log("SPELL_PERIODIC_MISSED", "FelPool", 230348)
+	
+	-- Mythic
+	self:Log("SPELL_CAST_SUCCESS", "RainofBrimstone", 238588)	
 end
 
 function mod:OnEngage()
+	burningCount = 1
+	shatteringCount = 1
+	
+	self:Bar(231363, 10) -- Burning Armor
+	self:Bar(233279, shatteringTimers[shatteringCount]) -- Shattering Star
+	self:Bar(233062, 54) -- Infernal Burning
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:BurningArmor(args)
+	self:TargetMessage(args.spellId, args.destName, "Attention", "Alert")
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId)
+	end
+	self:Bar(args.spellId, 24)
+end
+
+function mod:CrashingComent(args)
+	--self:Message(args.spellId, "Neutral", "Long")
+	--self:Bar(args.spellId, 10)
+end
+
+do
+	local list = mod:NewTargetList()
+	function mod:CrashingCometDebuff(args)
+		list[#list+1] = args.destName
+		if #list == 1 then
+			self:ScheduleTimer("TargetMessage", 0.1, 230345, list, "Important", "Warning", nil, nil, true)
+		end
+		if self:Me(args.destGUID) then
+			self:Say(230345)
+		end
+	end
+end
+
+function mod:InfernalSpike(args)
+	self:Message(args.spellId, "Neutral", "Info")
+	--self:Bar(args.spellId, 10)
+end
+
+--function mod:ShatteringStar(args)
+	--self:Message(args.spellId, "Attention", "Warning")
+	--self:Bar(args.spellId, 10)
+--end
+
+function mod:ShatteringStarDebuff(args)
+	shatteringCount = shatteringCount + 1
+	self:TargetMessage(233279, args.destName, "Attention", "Alarm")
+	self:Bar(233279, 6, 233283) -- Shattering Nova
+	self:Bar(233279, shatteringTimers[shatteringCount] or shatteringCount % 2 == 1 and 20 or 40, 233283) -- Shattering Nova
+	if self:Me(args.destGUID) then
+		self:Say(233279)
+		self:Flash(233279)
+	end
+end
+
+function mod:InfernalBurning(args)
+	burningCount = burningCount + 1
+	self:Message(args.spellId, "Urgent", "Warning", CL.casting:format(args.spellName))
+	self:Bar(args.spellId, 6, CL.casting:format(args.spellName))
+	self:Bar(args.spellId, burningCount <= 3 and 60 or 64)
+end
+
+do
+	local prev = 0
+	function mod:FelPool(args)
+		local t = GetTime()
+		if self:Me(args.destGUID) and t-prev > 1.5 then
+			prev = t
+			self:Message(args.spellId, "Personal", "Alarm", CL.underyou:format(args.spellName))
+		end
+	end
+end
+
+-- Mythic
+function mod:RainofBrimstone(args)
+	self:Message(args.spellId, "Attention", "Alert")
+	--self:Bar(args.spellId, 10)
+end
--- TombOfSargeras/Harjatan.lua
+++ TombOfSargeras/Harjatan.lua
@@ -10,11 +10,16 @@ local mod, CL = BigWigs:NewBoss("Harjatan the Bludger", 1147, 1856)
 if not mod then return end
 mod:RegisterEnableMob(116407)
 mod.engageId = 2036
---mod.respawnTime = 30
+mod.respawnTime = 30
 
 --------------------------------------------------------------------------------
 -- Locals
 --
+
+local uncheckedRageCounter = 1
+local uncheckedRageTimers = {20.5, 21.5, 55, 20.6, 74, 20.5}
+
+local drawInCounter = 1
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -28,16 +33,120 @@ local L = mod:GetLocale()
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		{231998, "TANK"}, -- Jagged Abrasion
+		231854, -- Unchecked Rage 
+		232192, -- Commanding Roar
+		232061, -- Draw In
+		233429, -- Frigid Blows
+		232174, -- Frosty Discharge
+		{231729, "SAY", "FLASH"}, -- Aqueous Burst
+		231904, -- Tend Wounds
+		{234128, "SAY", "FLASH"}, -- Driven Assault
+		240319, -- Hatching
+	},{
+		[231998] = "general",
+		[231729] = -14555,
+		[234128] = -14722,
+		[240319] = "mythic",
 	}
 end
 
 function mod:OnBossEnable()
+	-- Boss
+	self:Log("SPELL_AURA_APPLIED", "JaggedAbrasion", 231998)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "JaggedAbrasion", 231998)
+	self:Log("SPELL_CAST_START", "UncheckedRage", 231854)
+	self:Log("SPELL_CAST_START", "CommandingRoar", 232192)	
+	self:Log("SPELL_CAST_START", "DrawIn", 232061)	
+	--self:Log("SPELL_AURA_APPLIED", "FrigidBlows", 233429)
+	--self:Log("SPELL_AURA_APPLIED_DOSE", "FrigidBlows", 233429)
+	self:Log("SPELL_AURA_REMOVED_DOSE", "FrigidBlows", 233429)
+	--self:Log("SPELL_AURA_REMOVED", "FrigidBlowsRemoved", 233429)	
+	self:Log("SPELL_CAST_START", "FrostyDischarge", 232174)
+	
+	-- Adds
+	self:Log("SPELL_AURA_APPLIED", "AqueousBurst", 231729)
+	self:Log("SPELL_CAST_START", "TendWounds", 231904)	
+	self:Log("SPELL_AURA_APPLIED", "DrivenAssault", 234016)
+	
+	-- Mythic	
+	self:Log("SPELL_CAST_START", "Hatching", 240319)
 end
 
 function mod:OnEngage()
+	uncheckedRageCounter = 1
+	drawInCounter = 1
+	
+	self:Bar(231854, uncheckedRageTimers[uncheckedRageCounter]) -- Unchecked Rage
+	self:Bar(232061, 60) -- Draw In
+	
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+
+function mod:JaggedAbrasion(args)
+	local amount = args.amount or 1
+	self:StackMessage(args.spellId, args.destName, amount, "Urgent", amount > 4 and "Warning")
+end
+
+function mod:UncheckedRage(args)
+	uncheckedRageCounter = uncheckedRageCounter + 1
+	self:Message(args.spellId, "Attention", "Alert")
+	self:Bar(args.spellId, uncheckedRageTimers[uncheckedRageCounter] or 20.5)
+end
+
+function mod:CommandingRoar(args)
+	self:Message(args.spellId, "Important", "Warning")
+end
+
+function mod:DrawIn(args)
+	drawInCounter = drawInCounter + 1
+	self:Message(args.spellId, "Important", "Alert", CL.casting:format(args.spellName))
+	self:Bar(args.spellId, 10, CL.casting:format(args.spellName))
+	self:Bar(args.spellId, drawInCounter == 2 and 76 or 94)
+end
+
+function mod:FrigidBlows(args)
+	local amount = args.amount or 1
+	if amount < 5 then
+		self:StackMessage(args.spellId, args.destName, amount, "Urgent", amount < 2 and "Warning")
+	end
+end
+
+--function mod:FrigidBlowsRemoved(args)
+--	self:Message(args.spellId, "Positive", "Info", CL.removed:format(args.spellName))
+--end
+
+function mod:FrostyDischarge(args)
+	self:Message(args.spellId, "Urgent", "Alert", CL.casting:format(args.spellName))
+end
+
+function mod:AqueousBurst(args)
+	self:TargetMessage(args.spellId, args.destName, "Attention", "Warning")
+	if self:Me(args.sourceGUID) then
+		self:Say(args.spellId)
+		self:Flash(args.spellId)
+	end
+end
+
+function mod:TendWounds(args)
+	if self:Interrupter(args.sourceGUID) then
+		self:Message(args.spellId, "Important", "Alert")
+	end
+end
+
+function mod:DrivenAssault(args)
+	self:TargetMessage(args.spellId, args.destName, "Attention", "Warning")
+	if self:Me(args.sourceGUID) then
+		self:Say(args.spellId)
+		self:Flash(args.spellId)
+	end
+end
+
+function mod:Hatching(args)
+	self:Message(args.spellId, "Important", "Long")
+end
+
