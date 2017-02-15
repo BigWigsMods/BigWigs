a2b9ff279dfb96fa6f05cdb5abcabbe438ed0ede
Adavak LEE <adavak@adavak.com>
2017-02-15 21:07:04 +0800

    zhCN update. (#170)

--- Nighthold/Locales/zhCN.lua
+++ Nighthold/Locales/zhCN.lua
@@ -1,87 +1,93 @@
 local L = BigWigs:NewBossLocale("Skorpyron", "zhCN")
 if not L then return end
 if L then
-	--L.blue = "Blue"
-	--L.red = "Red"
-	--L.green = "Green"
-	--L.mode = "%s Mode"
+	L.blue = "蓝"
+	L.red = "红"
+	L.green = "绿"
+	L.mode = "%s模式"
 end
 
 L = BigWigs:NewBossLocale("Chronomatic Anomaly", "zhCN")
 if L then
-	--L.affected = "Affected"
-	--L.totalAbsorb = "Total Absorb"
+	L.affected = "已影响"
+	L.totalAbsorb = "全部吸收"
 end
 
 L = BigWigs:NewBossLocale("Trilliax", "zhCN")
 if L then
-	--L.yourLink = "You are linked with %s"
-	--L.yourLinkShort = "Linked with %s"
-	--L.imprint = "Imprint"
+	L.yourLink = ">%s< 与你相连"
+	L.yourLinkShort = "相连 %s"
+	L.imprint = "烙印"
 end
 
 L = BigWigs:NewBossLocale("Tichondrius", "zhCN")
 if L then
-	--L.addsKilled = "Adds killed"
-	--L.gotEssence = "Got Essence"
+	L.addsKilled = "增援已击杀"
+	L.gotEssence = "获得精华"
 
-	--L.adds_desc = "Timers and warnings for the add spawns."
+	L.adds_desc = "增援刷新计时器和警报。"
 	--L.adds_trigger1 = "Underlings! Get in here!"
 	--L.adds_trigger2 = "Show these pretenders how to fight!"
 end
 
 L = BigWigs:NewBossLocale("Krosus", "zhCN")
 if L then
-	--L.leftBeam = "Left Beam"
-	--L.rightBeam = "Right Beam"
+	L.leftBeam = "左侧光束"
+	L.rightBeam = "右侧光束"
 
-	--L.goRight = "> GO RIGHT >"
-	--L.goLeft = "< GO LEFT <"
+	L.goRight = "> 往右 >"
+	L.goLeft = "< 往左 <"
 
-	--L.smashingBridge = "Smashing Bridge"
-	--L.smashingBridge_desc = "Slams which break the bridge. You can use this option to emphasize or enable countdown."
+	L.smashingBridge = "断桥"
+	L.smashingBridge_desc = "猛击断桥。可以使用此选项醒目或启用冷却。"
 
-	--L.removedFromYou = "%s removed from you" -- "Searing Brand removed from YOU!"
+	L.removedFromYou = "%s已从你移除"
 end
 
 L = BigWigs:NewBossLocale("Star Augur Etraeus", "zhCN")
 if L then
-	--L.yourSign = "Your sign"
-	--L.with = "with"
-	--L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00Crab|r"
-	--L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000Wolf|r"
-	--L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00Hunter|r"
-	--L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFFDragon|r"
+	L.yourSign = "标记"
+	L.with = "与"
+	L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00螃蟹|r"
+	L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000狼|r"
+	L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00猎人|r"
+	L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFF龙|r"
 
-	--L.nameplate_requirement = "This feature is currently only supported by KuiNameplates. Mythic only."
+	L.nameplate_requirement = "此功能当前只支持 KuiNameplates。只限史诗难度。"
 
-	--L.custom_off_icy_ejection_nameplates = "Show {206936} on friendly nameplates" -- Icy Ejection
-	--L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_off_icy_ejection_nameplates = "显示 {206936} 到友方姓名板。" -- Icy Ejection
+	L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_fel_ejection_nameplates = "Show {205649} on friendly nameplates" -- Fel Ejection
-	--L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_on_fel_ejection_nameplates = "显示 {205649} 到友方姓名板。" -- Fel Ejection
+	L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_gravitational_pull_nameplates = "Show {214335} on friendly nameplates" -- Gravitational Pull
-	--L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
+	L.custom_on_gravitational_pull_nameplates = "显示 {214335} 到友方姓名板。" -- Gravitational Pull
+	L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_grand_conjunction_nameplates = "Show {205408} on friendly nameplates" -- Grand Conjunction
-	--L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
+	L.custom_on_grand_conjunction_nameplates = "显示 {205408} 到友方姓名板。" -- Grand Conjunction
+	L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
+
+	L.custom_off_gc_replacement_icons = "{205408} 使用更明亮些的图标"
+	L.custom_off_gc_replacement_icons_desc = "替换姓名板上强力联结图标获得更加的视觉效果："
+
+	L.custom_off_gc_redgreen_icons = "{205408} 只使用红色和绿色"
+	L.custom_off_gc_redgreen_icons_desc = "更改姓名板匹配星座标记图标为 |T876914:15:15:0:0:64:64:4:60:4:60|t 不匹配星座标记为 |T876915:15:15:0:0:64:64:4:60:4:60|t。"
 end
 
 L = BigWigs:NewBossLocale("Grand Magistrix Elisande", "zhCN")
 if L then
-	--L.elisande = "Elisande"
+	L.elisande = "艾利桑德"
 
 	--L.ring_msg = "Let the waves of time crash over you!"
 	--L.orb_msg = "You'll find time can be quite volatile."
 
-	--L.fastTimeZone = "Fast Time Zone"
+	L.fastTimeZone = "时间加速区域"
 end
 
 L = BigWigs:NewBossLocale("Gul'dan", "zhCN")
 if L then
-	--L[211152] = "(E) %s" -- (E) Eye of Gul'dan
-	--L.gains = "Gul'dan gains %s"
+	L[211152] = "（强化）%s"
+	L.gains = "古尔丹获得 >%s<"
 end
 
 L = BigWigs:NewBossLocale("Nighthold Trash", "zhCN")
