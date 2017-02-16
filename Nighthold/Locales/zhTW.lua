402cfbe5c792edb7a05064202aa545440abc931c
EKE00372 <eke00372@gmail.com>
2017-02-16 23:30:57 +0800

    update zhTW (#174)

--- Nighthold/Locales/zhTW.lua
+++ Nighthold/Locales/zhTW.lua
@@ -9,25 +9,25 @@ end
 
 L = BigWigs:NewBossLocale("Chronomatic Anomaly", "zhTW")
 if L then
-	--L.affected = "Affected"
-	--L.totalAbsorb = "Total Absorb"
+	L.affected = "被影響"
+	L.totalAbsorb = "總吸收"
 end
 
 L = BigWigs:NewBossLocale("Trilliax", "zhTW")
 if L then
-	--L.yourLink = "You are linked with %s"
-	--L.yourLinkShort = "Linked with %s"
-	--L.imprint = "Imprint"
+	L.yourLink = "你和%s相連"
+	L.yourLinkShort = "和%s相連"
+	L.imprint = "印記"
 end
 
 L = BigWigs:NewBossLocale("Tichondrius", "zhTW")
 if L then
-	--L.addsKilled = "Adds killed"
-	--L.gotEssence = "Got Essence"
+	L.addsKilled = "援軍擊殺"
+	L.gotEssence = "獲得精華"
 
-	--L.adds_desc = "Timers and warnings for the add spawns."
-	--L.adds_yell1 = "Underlings! Get in here!"
-	--L.adds_yell2 = "Show these pretenders how to fight!"
+	L.adds_desc = "援軍刷新計時器和警報。"
+	L.adds_yell1 = "手下們！都進來！"
+	L.adds_yell2 = "讓這些笨蛋見識真正的戰鬥！"
 end
 
 L = BigWigs:NewBossLocale("Krosus", "zhTW")
@@ -46,47 +46,47 @@ end
 
 L = BigWigs:NewBossLocale("Star Augur Etraeus", "zhTW")
 if L then
-	--L.yourSign = "Your sign"
-	--L.with = "with"
-	--L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00Crab|r"
-	--L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000Wolf|r"
-	--L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00Hunter|r"
-	--L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFFDragon|r"
+	L.yourSign = "你的星座"
+	L.with = "和"
+	L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00巨蟹|r"
+	L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000貪狼|r"
+	L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00獵戶|r"
+	L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFF飛龍|r"
 
-	--L.nameplate_requirement = "This feature is currently only supported by KuiNameplates. Mythic only."
+	L.nameplate_requirement = "這個功能目前只有KuiNameplates支援。傳奇難度限定。"
 
-	--L.custom_off_icy_ejection_nameplates = "Show {206936} on friendly nameplates" -- Icy Ejection
-	--L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_off_icy_ejection_nameplates = "在友方姓名板顯示 {206936} " -- Icy Ejection
+	L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_fel_ejection_nameplates = "Show {205649} on friendly nameplates" -- Fel Ejection
-	--L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_on_fel_ejection_nameplates = "在友方姓名板顯示 {205649} " -- Fel Ejection
+	L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_gravitational_pull_nameplates = "Show {214335} on friendly nameplates" -- Gravitational Pull
-	--L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
+	L.custom_on_gravitational_pull_nameplates = "在友方姓名板顯示 {214335} " -- Gravitational Pull
+	L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_grand_conjunction_nameplates = "Show {205408} on friendly nameplates" -- Grand Conjunction
-	--L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
+	L.custom_on_grand_conjunction_nameplates = "在友方姓名板顯示 {205408} " -- Grand Conjunction
+	L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
 end
 
 L = BigWigs:NewBossLocale("Grand Magistrix Elisande", "zhTW")
 if L then
-	--L.elisande = "Elisande"
+	L.elisande = "艾莉珊德"
 
-	--L.ring_yell = "Let the waves of time crash over you!"
-	--L.orb_yell = "You'll find time can be quite volatile."
+	L.ring_msg = "時間的浪潮會粉碎你！"
+	L.orb_msg = "你會發現時光有時很不穩定。"
 
-	--L.fastTimeZone = "Fast Time Zone"
+	L.fastTimeZone = "加速區域"
 end
 
 L = BigWigs:NewBossLocale("Gul'dan", "zhTW")
 if L then
-	--L[211152] = "(E) %s" -- (E) Eye of Gul'dan
-	--L.gains = "Gul'dan gains %s"
+	L[211152] = "(強化) %s" -- (E) Eye of Gul'dan
+	L.gains = "古爾丹獲得%s"
 	--L.p4_mythic_start_yell = "Time to return the demon hunter's soul to his body... and deny the Legion's master a host!"
 
-	--L.nightorb_desc = "Summons a Nightorb, killing it will spawn a Time Zone."
+	L.nightorb_desc = "召喚一個夜之球，擊殺它會生成一個時間區域。"
 
-	--L.manifest_desc = "Summons a Soul Fragment of Azzinoth, killing it will spawn a Demonic Essence."
+	L.manifest_desc = "召喚一個埃辛諾斯靈魂碎片，擊殺它會生成一個惡魔精華。"
 end
 
 L = BigWigs:NewBossLocale("Nighthold Trash", "zhTW")
@@ -97,22 +97,22 @@ if L then
 	--L.pulsauron = "Pulsauron"
 
 	--[[ Chronomatic Anomaly to Trilliax ]]--
-	--L.sludgerax = "Sludgerax"
+	L.sludgerax = "淤泥雷克斯"
 
 	--[[ Trilliax to Aluriel ]]--
 	--L.karzun = "Kar'zun"
-	--L.guardian = "Gilded Guardian"
+	L.guardian = "金輝守護者"
 	--L.battle_magus = "Duskwatch Battle-Magus"
-	--L.chronowraith = "Chronowraith"
+	L.chronowraith = "時光怨靈"
 	--L.protector = "Nighthold Protector"
 
 	--[[ Aluriel to Etraeus ]]--
-	--L.jarin = "Astrologer Jarin"
+	L.jarin = "占星師賈倫"
 
 	--[[ Aluriel to Telarn ]]--
 	--L.weaver = "Duskwatch Weaver"
-	--L.archmage = "Shal'dorei Archmage"
-	--L.manasaber = "Domesticated Manasaber"
+	L.archmage = "夏多雷大法師"
+	L.manasaber = "被馴養的法力刃豹"
 
 	--[[ Aluriel to Krosos ]]--
 	--L.infernal = "Searing Infernal"
