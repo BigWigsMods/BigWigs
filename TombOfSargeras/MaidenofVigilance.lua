b74a040b249ce8654c52ed5f3c3cf74a4a69bf7f
MethodWigs <justwait@method.gg>
2017-02-25 03:11:23 +0100

    Maiden of Vigilance: Heroic testing updates

--- TombOfSargeras/MaidenofVigilance.lua
+++ TombOfSargeras/MaidenofVigilance.lua
@@ -10,13 +10,18 @@ local mod, CL = BigWigs:NewBoss("Maiden of Vigilance", 1147, 1897)
 if not mod then return end
 mod:RegisterEnableMob(118289) -- Maiden of Vigilance
 mod.engageId = 2052
---mod.respawnTime = 30
+mod.respawnTime = 30 -- XXX Unconfirmed
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
+local phase = 1
 local shieldActive = false
+local massInstabilityCounter = 0
+local hammerofCreationCounter = 0
+local hammerofObliterationCounter = 0
+local infusionCounter = 0
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -35,11 +40,11 @@ end
 function mod:GetOptions()
 	return {
 		240209, -- Unstable Soul
-		236420, -- Aegwynn's Ward
+		241593, -- Aegwynn's Ward
 		{235271, "PROXIMITY"}, -- Infusion
 		"custom_on_infusion_plates",
-		235569, -- Hammer of Creation
-		235573, -- Hammer of Obliteration
+		241635, -- Hammer of Creation
+		241636, -- Hammer of Obliteration
 		235267, -- Mass Instability
 		237722, -- Blowback
 		235028, -- Titanic Bulwark
@@ -53,30 +58,46 @@ end
 
 function mod:OnBossEnable()
 	-- General
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 	self:Log("SPELL_AURA_APPLIED", "UnstableSoul", 240209) -- Unstable Soul
-	self:Log("SPELL_AURA_APPLIED", "AegwynnsWardApplied", 236420) -- Aegwynn's Ward
+	self:Log("SPELL_AURA_APPLIED", "AegwynnsWardApplied", 241593) -- Aegwynn's Ward
 
-	-- Stage One: Divide and Conquer
+	-- Stage One: Divide and Conquer	
+	self:Log("SPELL_CAST_START", "Infusion", 235271) -- Infusion
 	self:Log("SPELL_AURA_APPLIED", "FelInfusion", 235240) -- Fel Infusion
 	self:Log("SPELL_AURA_APPLIED", "LightInfusion", 235213) -- Light Infusion
-	self:Log("SPELL_CAST_SUCCESS", "HammerofCreation", 235569) -- Hammer of Creation
-	self:Log("SPELL_CAST_SUCCESS", "HammerofObliteration", 235573) -- Hammer of Obliteration
-	self:Log("SPELL_CAST_SUCCESS", "MassInstability", 235267) -- Mass Instability
+	self:Log("SPELL_CAST_SUCCESS", "HammerofCreation", 241635) -- Hammer of Creation
+	self:Log("SPELL_CAST_SUCCESS", "HammerofObliteration", 241636) -- Hammer of Obliteration
 
 	-- Stage Two: Watcher's Wrath
 	self:Log("SPELL_CAST_SUCCESS", "Blowback", 237722) -- Blowback
 	self:Log("SPELL_CAST_START", "TitanicBulwark", 235028) -- Titanic Bulwark
+	self:Log("SPELL_AURA_APPLIED", "TitanicBulwarkApplied", 235028) -- Light Infusion
 	self:Log("SPELL_AURA_REMOVED", "TitanicBulwarkRemoved", 235028) -- Light Infusion
-	self:Log("SPELL_CAST_START", "WrathoftheCreators", 234891) -- Wrath of the Creators
-
-
+	self:Log("SPELL_CAST_SUCCESS", "WrathoftheCreators", 234891) -- Wrath of the Creators
+	self:Log("SPELL_AURA_APPLIED", "WrathoftheCreatorsApplied", 237339) -- Wrath of the Creators
+	self:Log("SPELL_AURA_APPLIED_DOSE", "WrathoftheCreatorsApplied", 237339) -- Wrath of the Creators
+	self:Log("SPELL_AURA_REMOVED", "WrathoftheCreatorsInterrupted", 234891) -- Wrath of the Creators
 	if 	self:GetOption("custom_on_infusion_plates") then
 		self:ShowFriendlyNameplates()
 	end
 end
 
 function mod:OnEngage()
+	phase = 1
 	shieldActive = false
+	
+	massInstabilityCounter = 0
+	hammerofCreationCounter = 0
+	hammerofObliterationCounter = 0
+	infusionCounter = 0
+	
+	self:Bar(235271, 2.0) -- Infusion
+	self:Bar(241635, 14.0) -- Hammer of Creation
+	self:Bar(235267, 22.0) -- Mass Instability
+	self:Bar(241636, 32.0) -- Hammer of Obliteration
+	self:Bar(237722, 41.0) -- Blowback
+	self:Bar(234891, 43.5) -- Wrath of the Creators
 end
 
 function mod:OnBossDisable()
@@ -86,6 +107,17 @@ end
 -- Event Handlers
 --
 
+function mod:UNIT_SPELLCAST_SUCCEEDED(unit, spellName, _, _, spellId)
+	if spellId == 235267 then -- Mass Instability
+		massInstabilityCounter = massInstabilityCounter + 1
+		self:Message(spellId, "Attention", "Alert", spellName)
+		if massInstabilityCounter == 2 then
+			massInstabilityCounter = 1
+			self:Bar(spellId, 36.0)
+		end
+	end
+end
+
 function mod:UnstableSoul(args)
 	if self:Me(args.destGUID) then
 		self:TargetMessage(args.spellId, args.destName, "Personal", "Alarm")
@@ -94,82 +126,68 @@ end
 
 function mod:AegwynnsWardApplied(args)
 	if self:Me(args.destGUID) then
-		self:Message(args.spellId, "Positive", "Info")
+		self:Message(args.spellId, "Neutral", "Info")
+	end
+end
+
+function mod:Infusion(args)
+	infusionCounter = infusionCounter + 1
+	self:Message(args.spellId, "Neutral", "Info", CL.casting:format(args.spellName))
+	if infusionCounter == 2 then
+		infusionCounter = 1
+		self:Bar(spellId, 38.0)
 	end
 end
 
 do
 	local lightList, felList = {}, {}
-
+	
 	function mod:FelInfusion(args)
 		felList[#felList+1] = args.destName
+		tDeleteItem(lightList, args.destName)
 		if 	self:GetOption("custom_on_infusion_plates") then
+			self:RemovePlate(nil, args.destName) -- Can only have 1 debuff
 			self:AddPlate(args.spellId, args.destName)
 		end
 		if self:Me(args.destGUID) then
-			self:TargetMessage(args.spellId, args.destName, "Personal", "Alert")
-			self:Flash(args.spellId)
-			self:Say(args.spellId)
-			self:OpenProximity(args.spellId, 5, lightList) -- Avoid people with Light debuff
+			self:TargetMessage(235271, args.destName, "Personal", "Warning", args.spellName, args.spellId)
+			self:OpenProximity(235271, 5, lightList) -- Avoid people with Light debuff
 		end
 	end
 
 	function mod:LightInfusion(args)
 		lightList[#lightList+1] = args.destName
+		tDeleteItem(felList, args.destName)
 		if 	self:GetOption("custom_on_infusion_plates") then
+			self:RemovePlate(nil, args.destName) -- Can only have 1 debuff
 			self:AddPlate(args.spellId, args.destName)
 		end
 		if self:Me(args.destGUID) then
-			self:TargetMessage(args.spellId, args.destName, "Personal", "Alert")
-			self:Flash(args.spellId)
-			self:Say(args.spellId)
-			self:OpenProximity(args.spellId, 5, felList) -- Avoid people with Fel debuff
+			self:TargetMessage(235271, args.destName, "Personal", "Warning", args.spellName, args.spellId)
+			self:OpenProximity(235271, 5, felList) -- Avoid people with Fel debuff
 		end
-	end
-
-	function mod:FelInfusionRemoved(args)
-		if self:Me(args.destGUID) then
-			self:CloseProximity(args.spellId)
-		end
-		if 	self:GetOption("custom_on_infusion_plates") then
-			self:RemovePlate(args.spellId, args.destName)
-		end
-		tDeleteItem(felList, args.destName)
-	end
-
-	function mod:LightInfusionRemoved(args)
-		if self:Me(args.destGUID) then
-			self:CloseProximity(args.spellId)
-		end
-		if 	self:GetOption("custom_on_infusion_plates") then
-			self:RemovePlate(args.spellId, args.destName)
-		end
-		tDeleteItem(lightList, args.destName)
 	end
 end
 
 function mod:HammerofCreation(args)
+	hammerofCreationCounter = hammerofCreationCounter + 1
 	self:Message(args.spellId, "Urgent", "Alert", args.spellName)
-	--self:Bar(args.spellId, 10)
+	if hammerofCreationCounter == 2 then
+		self:Bar(args.spellId, 36)
+	end
 end
 
 function mod:HammerofObliteration(args)
+	hammerofObliterationCounter = hammerofObliterationCounter + 1
 	self:Message(args.spellId, "Urgent", "Alert", args.spellName)
-	--self:Bar(args.spellId, 10)
-end
-
-function mod:MassInstability(args)
-	self:Message(args.spellId, "Important", "Warning", args.spellName)
-	--self:Bar(args.spellId, 10)
+	if hammerofObliterationCounter == 2 then
+		self:Bar(args.spellId, 36)
+	end
 end
 
 function mod:Blowback(args)
-	self:Message(args.spellId, "Attention", "Info", args.spellName)
-end
-
-function mod:TitanicBulwark(args)
-	self:Message(args.spellId, "Important", "Warning", CL.casting:format(args.spellName))
-	--self:Bar(args.spellId, 10)
+	phase = 2
+	self:Message(args.spellId, "Important", "Warning", args.spellName)
 end
 
 function mod:TitanicBulwarkApplied(args)
@@ -182,7 +200,27 @@ function mod:TitanicBulwarkRemoved(args)
 end
 
 function mod:WrathoftheCreators(args)
+	self:Message(args.spellId, "Attention", "Alert", CL.casting:format(args.spellName))
+end
+
+function mod:WrathoftheCreatorsApplied(args)
 	if self:Interrupter(args.sourceGUID) and not shieldActive then
-		self:Message(args.spellId, "Important", "Alert")
+		self:Message(234891, "Important", "Warning", args.spellName)
 	end
+end
+	
+function mod:WrathoftheCreatorsInterrupted(args)
+	phase = 1 
+	self:Message(args.spellId, "Positive", "Long", CL.interrupted:format(args.spellName))
+	massInstabilityCounter = 1
+	hammerofCreationCounter = 1
+	hammerofObliterationCounter = 1
+	infusionCounter = 1
+	
+	self:Bar(235271, 2.0) -- Infusion
+	self:Bar(241635, 14.0) -- Hammer of Creation
+	self:Bar(235267, 22.0) -- Mass Instability
+	self:Bar(241636, 32.0) -- Hammer of Obliteration
+	self:Bar(237722, 81) -- Blowback
+	self:Bar(234891, 83.5) -- Wrath of the Creators
 end
