8c33811e37ec0a3fc744a312aa6a2a11f72cd116
WetU <yuk6196@gmail.com>
2017-03-15 11:25:52 +0900

    Update koKR.lua (#224)
    [skip ci]

--- Nighthold/Locales/koKR.lua
+++ Nighthold/Locales/koKR.lua
@@ -1,110 +1,110 @@
 local L = BigWigs:NewBossLocale("Skorpyron", "koKR")
 if not L then return end
 if L then
-	--L.blue = "Blue"
-	--L.red = "Red"
-	--L.green = "Green"
-	--L.mode = "%s Mode"
+	L.blue = "푸른색"
+	L.red = "붉은색"
+	L.green = "녹색"
+	L.mode = "%s 모드"
 end
 
 L = BigWigs:NewBossLocale("Chronomatic Anomaly", "koKR")
 if L then
-	--L.affected = "Affected"
-	--L.totalAbsorb = "Total Absorb"
+	L.affected = "영향 받음"
+	L.totalAbsorb = "총 흡수"
 end
 
 L = BigWigs:NewBossLocale("Trilliax", "koKR")
 if L then
-	--L.yourLink = "You are linked with %s"
-	--L.yourLinkShort = "Linked with %s"
-	--L.imprint = "Imprint"
+	L.yourLink = "당신은 %s|1과;와; 연결됨"
+	L.yourLinkShort = "%s|1과;와; 연결됨"
+	L.imprint = "피조물"
 end
 
 L = BigWigs:NewBossLocale("Tichondrius", "koKR")
 if L then
-	--L.addsKilled = "Adds killed"
-	--L.gotEssence = "Got Essence"
+	L.addsKilled = "쫄 처치"
+	L.gotEssence = "정수 획득"
 
-	--L.adds_desc = "Timers and warnings for the add spawns."
-	--L.adds_yell1 = "Underlings! Get in here!"
-	--L.adds_yell2 = "Show these pretenders how to fight!"
+	L.adds_desc = "쫄 생성에 대한 타이머와 경보입니다."
+	L.adds_yell1 = "부하들아! 이리 와라!"
+	L.adds_yell2 = "이 멍청이들에게 싸우는 법을 알려 줘라!"
 end
 
 L = BigWigs:NewBossLocale("Krosus", "koKR")
 if L then
-	--L.leftBeam = "Left Beam"
-	--L.rightBeam = "Right Beam"
+	L.leftBeam = "왼쪽 광선"
+	L.rightBeam = "오른쪽 광선"
 
-	--L.goRight = "> GO RIGHT >"
-	--L.goLeft = "< GO LEFT <"
+	L.goRight = "> 오른쪽 이동 >"
+	L.goLeft = "< 왼쪽 이동 <"
 
-	--L.smashingBridge = "Smashing Bridge"
-	--L.smashingBridge_desc = "Slams which break the bridge. You can use this option to emphasize or enable countdown."
+	L.smashingBridge = "다리 파괴"
+	L.smashingBridge_desc = "다리를 파괴하는 격돌입니다. 이 옵션을 사용하여 강조하거나 초읽기를 활성화할 수 있습니다."
 
-	--L.removedFromYou = "%s removed from you" -- "Searing Brand removed from YOU!"
+	L.removedFromYou = "당신에게서 %s 사라짐" -- "Searing Brand removed from YOU!"
 end
 
 L = BigWigs:NewBossLocale("Star Augur Etraeus", "koKR")
 if L then
-	--L.yourSign = "Your sign"
-	--L.with = "with"
-	--L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00Crab|r"
-	--L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000Wolf|r"
-	--L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00Hunter|r"
-	--L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFFDragon|r"
+	L.yourSign = "당신의 별자리"
+	L.with = "같은"
+	L[205429] = "|T1391538:15:15:0:0:64:64:4:60:4:60|t|cFFFFDD00게|r"
+	L[205445] = "|T1391537:15:15:0:0:64:64:4:60:4:60|t|cFFFF0000늑대|r"
+	L[216345] = "|T1391536:15:15:0:0:64:64:4:60:4:60|t|cFF00FF00사냥꾼|r"
+	L[216344] = "|T1391535:15:15:0:0:64:64:4:60:4:60|t|cFF00DDFF용|r"
 
-	--L.nameplate_requirement = "This feature is currently only supported by KuiNameplates. Mythic only."
+	L.nameplate_requirement = "이 기능은 현재 KuiNameplates만 지원됩니다. 신화 난이도만."
 
-	--L.custom_off_icy_ejection_nameplates = "Show {206936} on friendly nameplates" -- Icy Ejection
-	--L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_off_icy_ejection_nameplates = "아군 이름표에 {206936} 표시" -- Icy Ejection
+	L.custom_off_icy_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_fel_ejection_nameplates = "Show {205649} on friendly nameplates" -- Fel Ejection
-	--L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
+	L.custom_on_fel_ejection_nameplates = "아군 이름표에 {205649} 표시" -- Fel Ejection
+	L.custom_on_fel_ejection_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_gravitational_pull_nameplates = "Show {214335} on friendly nameplates" -- Gravitational Pull
-	--L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
+	L.custom_on_gravitational_pull_nameplates = "아군 이름표에 {214335} 표시" -- Gravitational Pull
+	L.custom_on_gravitational_pull_nameplates_desc = L.nameplate_requirement
 
-	--L.custom_on_grand_conjunction_nameplates = "Show {205408} on friendly nameplates" -- Grand Conjunction
-	--L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
+	L.custom_on_grand_conjunction_nameplates = "아군 이름표에 {205408} 표시" -- Grand Conjunction
+	L.custom_on_grand_conjunction_nameplates_desc = L.nameplate_requirement
 end
 
 L = BigWigs:NewBossLocale("Grand Magistrix Elisande", "koKR")
 if L then
-	--L.elisande = "Elisande"
+	L.elisande = "엘리산드"
 
-	--L.ring_yell = "Let the waves of time crash over you!"
-	--L.orb_yell = "You'll find time can be quite volatile."
+	L.ring_yell = "시간의 파도가 널 덮치기를!"
+	L.orb_yell = "시간은 제멋대로 사라져 버리지."
 
-	--L.slowTimeZone = "Slow Time Zone"
-	--L.fastTimeZone = "Fast Time Zone"
+	L.slowTimeZone = "느린 시간 지역"
+	L.fastTimeZone = "빠른 시간 지역"
 
-	--L.boss_active = "Elisande Active"
-	--L.boss_active_desc = "Time until Elisande is active after clearing the trash event."
-	--L.elisande_trigger = "I foresaw your coming, of course. The threads of fate that led you to this place. Your desperate attempt to stop the Legion."
+	L.boss_active = "엘리산드 활성화"
+	L.boss_active_desc = "일반몹 이벤트를 끝낸 후에 엘리산드가 활성화되기까지의 시간입니다."
+	L.elisande_trigger = "모두 예견했다. 너희를 여기로 이끈 운명의 실마리를. 군단을 막으려는 너희의 필사적인 몸부림을."
 end
 
 L = BigWigs:NewBossLocale("Gul'dan", "koKR")
 if L then
-	--L.empowered = "(E) %s" -- (E) Eye of Gul'dan
-	--L.gains = "Gul'dan gains %s"
-	--L.p4_mythic_start_yell = "Time to return the demon hunter's soul to his body... and deny the Legion's master a host!"
+	L.empowered = "(강화) %s" -- (E) Eye of Gul'dan
+	L.gains = "굴단 %s 획득"
+	L.p4_mythic_start_yell = "Time to return the demon hunter's soul to his body... and deny the Legion's master a host!"
 
-	--L.nightorb_desc = "Summons a Nightorb, killing it will spawn a Time Zone."
+	L.nightorb_desc = "Nightorb를 소환합니다, 처치하면 시간 지역이 생성됩니다."
 
-	--L.manifest_desc = "Summons a Soul Fragment of Azzinoth, killing it will spawn a Demonic Essence."
+	L.manifest_desc = "아지노스의 영혼 조각을 소환합니다, 처치하면 악마의 정수가 생성됩니다."
 
-	--L.winds_desc = "Gul'dan summons Violent Winds to push the players off the platform."
+	L.winds_desc = "굴단이 난폭한 바람을 소환하여 플레이어를 단상에서 밀어냅니다."
 end
 
 L = BigWigs:NewBossLocale("Nighthold Trash", "koKR")
 if L then
 	--[[ Skorpyron to Trilliax ]]--
-	--L.torm = "Torm the Brute"
+	L.torm = "육중한 토름"
 	--L.fulminant = "Fulminant"
-	--L.pulsauron = "Pulsauron"
+	L.pulsauron = "펄사우론n"
 
 	--[[ Chronomatic Anomaly to Trilliax ]]--
-	--L.sludgerax = "Sludgerax"
+	L.sludgerax = "슬러지락스"
 
 	--[[ Trilliax to Aluriel ]]--
 	--L.karzun = "Kar'zun"
@@ -128,4 +128,3 @@ if L then
 	--[[ Aluriel to Tichondrius ]]--
 	--L.watcher = "Abyss Watcher"
 end
-
