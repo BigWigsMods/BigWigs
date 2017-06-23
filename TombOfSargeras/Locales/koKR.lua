33ab65286d7942e066f6175dbdd86b9c3801758d
WetU <yuk6196@gmail.com>
2017-06-23 21:52:32 +0900

    Update koKR.lua
    Trash mob names are not verified yet.

--- TombOfSargeras/Locales/koKR.lua
+++ TombOfSargeras/Locales/koKR.lua
@@ -1,28 +1,28 @@
 local L = BigWigs:NewBossLocale("Harjatan the Bludger", "koKR")
 if not L then return end
 if L then
-	--L.custom_on_fixate_plates = "Fixate icon on Enemy Nameplate"
-	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+	L.custom_on_fixate_plates = "적 이름표에 시선 고정 아이콘"
+	L.custom_on_fixate_plates_desc = "당신에게 시선을 고정한 대상 이름표에 아이콘을 표시합니다.\n적 이름표를 사용해야 합니다. 이 기능은 현재 KuiNameplates만 지원합니다."
 end
 
 L = BigWigs:NewBossLocale("The Desolate Host", "koKR")
 if L then
-	--L.infobox_players = "Players"
-	--L.armor_remaining = "%s Remaining (%d)" -- Bonecage Armor Remaining (#)
+	L.infobox_players = "플레이어"
+	L.armor_remaining = "%s 남음 (%d)" -- Bonecage Armor Remaining (#)
 end
 
 L = BigWigs:NewBossLocale("Maiden of Vigilance", "koKR")
 if L then
-	--L.infusionChanged = "Infusion CHANGED: %s"
-	--L.sameInfusion = "Same Infusion: %s"
-	--L.fel = "Fel"
-	--L.light = "Light"
+	L.infusionChanged = "주입 변경: %s"
+	L.sameInfusion = "같은 주입: %s"
+	L.fel = "지옥"
+	L.light = "빛"
 end
 
 L = BigWigs:NewBossLocale("Tomb of Sargeras Trash", "koKR")
 if L then
-	--L.chaosbringer = "Infernal Chaosbringer"
-	--L.rez = "Rez the Tombwatcher"
-	--L.custodian = "Undersea Custodian"
-	--L.sentry = "Guardian Sentry"
+	L.chaosbringer = "Infernal Chaosbringer"
+	L.rez = "Rez the Tombwatcher"
+	L.custodian = "Undersea Custodian"
+	L.sentry = "Guardian Sentry"
 end
