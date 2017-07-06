f209c5ef44b48fef531d6724bdc2c1c83c755fbf
Pettigrow <pettigrow@pettigrow.be>
2017-07-06 21:27:39 +0200

    TombOfSargeras: frFR Update

--- TombOfSargeras/Locales/frFR.lua
+++ TombOfSargeras/Locales/frFR.lua
@@ -1,56 +1,56 @@
 local L = BigWigs:NewBossLocale("Harjatan the Bludger", "frFR")
 if not L then return end
 if L then
-	--L.custom_on_fixate_plates = "Fixate icon on Enemy Nameplate"
-	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+	L.custom_on_fixate_plates = "Icône Fixer sur les barres d'info des ennemis"
+	L.custom_on_fixate_plates_desc = "Affiche une icône sur la barre d'info de la cible qui est fixée sur vous.\nNécessite l'utlisation des barres d'info des ennemis. Cette fonctionnalité est actuellement uniquement supportée par KuiNameplates."
 end
 
 L = BigWigs:NewBossLocale("Demonic Inquisition", "frFR")
 if L then
-	--L.custom_on_stop_timers = "Always show ability bars"
-	--L.custom_on_stop_timers_desc = "Demonic Inquisition has some spells which are delayed by interupts/other casts. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "Toujours afficher les barres de capacité"
+	L.custom_on_stop_timers_desc = "L'Inquisition démoniaque a certains sorts qui sont retardés par les interruptions/les autres incantations. Quand cette option est activée, les barres de ces capacités resteront à l'écran."
 end
 
 L = BigWigs:NewBossLocale("The Desolate Host", "frFR")
 if L then
-	--L.infobox_players = "Players"
-	--L.armor_remaining = "%s Remaining (%d)" -- Bonecage Armor Remaining (#)
-	--L.tormentingCriesSay = "Cries" -- Tormenting Cries (short say)
+	L.infobox_players = "Joueurs"
+	L.armor_remaining = "%s restant (%d)" -- Bonecage Armor Remaining (#)
+	L.tormentingCriesSay = "Cris" -- Tormenting Cries (short say)
 end
 
 L = BigWigs:NewBossLocale("Maiden of Vigilance", "frFR")
 if L then
-	--L.infusionChanged = "Infusion CHANGED: %s"
-	--L.sameInfusion = "Same Infusion: %s"
-	--L.fel = "Fel"
-	--L.light = "Light"
-	--L.felHammer = "Fel Hammer" -- Better name for "Hammer of Obliteration"
-	--L.lightHammer = "Light Hammer" -- Better name for "Hammer of Creation"
+	L.infusionChanged = "Nouvelle Infusion : %s"
+	L.sameInfusion = "Même Infusion : %s"
+	L.fel = "Gangrené"
+	L.light = "Lumière"
+	L.felHammer = "Marteau gangrené" -- Better name for "Hammer of Obliteration"
+	L.lightHammer = "Marteau de Lumière" -- Better name for "Hammer of Creation"
 end
 
 L = BigWigs:NewBossLocale("Fallen Avatar", "frFR")
 if L then
-	--L.touch_impact = "Touch Impact" -- Touch of Sargeras Impact (short)
+	L.touch_impact = "Impact du Toucher" -- Touch of Sargeras Impact (short)
 
-	--L.custom_on_stop_timers = "Always show ability bars"
-	--L.custom_on_stop_timers_desc = "Fallen Avatar randomizes which off-cooldown ability he uses next. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "Toujours afficher les barres de capacité"
+	L.custom_on_stop_timers_desc = "L'Avatar déchu décide aléatoirement quel capacité hors cooldown il utilise ensuite. Quand cette option est activée, les barres de ces capacités resteront à l'écran."
 end
 
 L = BigWigs:NewBossLocale("Kil'jaeden", "frFR")
 if L then
-	--L.singularityImpact = "Singularity Impact"
-	--L.obeliskExplosion = "Obelisk Explosion"
+	L.singularityImpact = "Impact Singularité"
+	L.obeliskExplosion = "Explosion Obélisque"
 
-	--L.darkness = "Darkness" -- Shorter name for Darkness of a Thousand Souls (238999)
-	--L.reflectionErupting = "Reflection: Erupting" -- Shorter name for Shadow Reflection: Erupting (236710)
-	--L.reflectionWailing = "Reflection: Wailing" -- Shorter name for Shadow Reflection: Wailing (236378)
-	--L.reflectionHopeless = "Reflection: Hopeless" -- Shorter name for Shadow Reflection: Hopeless (237590)
+	L.darkness = "Ténèbres" -- Shorter name for Darkness of a Thousand Souls (238999)
+	L.reflectionErupting = "Reflet : éruptif" -- Shorter name for Shadow Reflection: Erupting (236710)
+	L.reflectionWailing = "Reflet : gémissant" -- Shorter name for Shadow Reflection: Wailing (236378)
+	L.reflectionHopeless = "Reflet : désespéré" -- Shorter name for Shadow Reflection: Hopeless (237590)
 end
 
 L = BigWigs:NewBossLocale("Tomb of Sargeras Trash", "frFR")
 if L then
-	--L.chaosbringer = "Infernal Chaosbringer"
-	--L.rez = "Rez the Tombwatcher"
-	--L.custodian = "Undersea Custodian"
-	--L.sentry = "Guardian Sentry"
+	L.chaosbringer = "Infernal porte-chaos"
+	L.rez = "Rez le Garde-Tombe"
+	L.custodian = "Gardien des fonds marins"
+	L.sentry = "Factionnaire gardien"
 end
