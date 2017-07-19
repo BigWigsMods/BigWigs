d64c5c768a17ef0353511cd21d70a73a61db3604
elvador <reysptools@googlemail.com>
2017-07-19 11:46:29 +0200

    SistersoftheMoon: Wait a bit longer before announcing Lunar Beacon (fixes #315)

--- TombOfSargeras/SistersoftheMoon.lua
+++ TombOfSargeras/SistersoftheMoon.lua
@@ -314,7 +314,7 @@ do
 		end
 	end
 	function mod:LunarBeacon(args)
-		self:GetBossTarget(printTarget, 0.5, args.sourceGUID) -- Faster than waiting for debuff/cast end
+		self:GetBossTarget(printTarget, 1.5, args.sourceGUID) -- Faster than waiting for debuff/cast end, sometimes takes really long
 		lunarBeaconCounter = lunarBeaconCounter + 1
 		self:Bar(args.spellId, lunarBeaconCounter == 2 and 21.9 or 35) -- XXX Need Data longer than 4 casts
 	end
