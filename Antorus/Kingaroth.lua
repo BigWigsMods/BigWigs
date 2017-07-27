5dc4c7d1e02f892f528d918e1abcb08c4fd43698
funkydude <funkydude@users.noreply.github.com>
2017-07-27 16:14:52 +0100

    whitespace cleanup

--- Antorus/Kingaroth.lua
+++ Antorus/Kingaroth.lua
@@ -4,7 +4,7 @@ if not IsTestBuild() then return end -- XXX dont load on live
 -- Module Declaration
 --
 
-local mod, CL = BigWigs:NewBoss("Kin'garoth", nil, 2004, 1712) 
+local mod, CL = BigWigs:NewBoss("Kin'garoth", nil, 2004, 1712)
 if not mod then return end
 --mod:RegisterEnableMob(000000)
 mod.engageId = 2088
