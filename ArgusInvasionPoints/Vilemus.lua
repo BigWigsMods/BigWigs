15766794322e4923c1532d6b73d34892b91e213c
Justwait <justwait@method.gg>
2017-10-04 14:53:45 +0200

    ArgusInvasionPoints/Vilemus: Fix warnings and add cooldowns

--- ArgusInvasionPoints/Vilemus.lua
+++ ArgusInvasionPoints/Vilemus.lua
@@ -13,7 +13,7 @@ mod:RegisterEnableMob(124719)
 
 function mod:GetOptions()
 	return {
-		{247739, "TANK"}, -- Drain
+		{247739, "FLASH"}, -- Drain
 		247733, -- Stomp
 		247731, -- Fel Breath
 	}
@@ -22,7 +22,12 @@ end
 function mod:OnBossEnable()
 	self:ScheduleTimer("CheckForEngage", 1)
 
+
+	self:Log("SPELL_CAST_SUCCESS", "DrainSuccess", 247739)
 	self:Log("SPELL_AURA_APPLIED", "Drain", 247739)
+	self:Log("SPELL_AURA_APPLIED", "DrainStacks", 247742)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "DrainStacks", 247742)
+
 	self:Log("SPELL_CAST_START", "Stomp", 247733)
 	self:Log("SPELL_CAST_START", "FelBreath", 247731)
 
@@ -36,19 +41,36 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
-
-function mod:Drain(args)
-	self:TargetMessage(args.spellId, args.destName, "Attention", "Alert", nil, nil, true)
-	--self:CDBar(args.spellId, 34)
+function mod:DrainSuccess(args)
+	self:CDBar(args.spellId, 17)
 end
 
+do
+	local playerList = mod:NewTargetList()
+	function mod:Drain(args)
+		playerList[#playerList+1] = args.destName
+		if self:Me(args.destGUID) then
+			self:Flash(args.spellId)
+		end
+		if #playerList == 1 then
+			self:ScheduleTimer("TargetMessage", 0.3, args.spellId, playerList, "Attention", "Alert")
+		end
+	end
+end
+
+function mod:DrainStacks(args)
+	if self:Me(args.destGUID) then
+		local amount = args.amount or 1
+		self:StackMessage(247739, args.destName, amount, "Neutral", amount % 2 == 0 and "Alarm", args.spellId)
+	end
+end
 
 function mod:Stomp(args)
 	self:Message(args.spellId, "Urgent", "Warning")
-	--self:CDBar(args.spellId, 34)
+	self:CDBar(args.spellId, 17)
 end
 
 function mod:FelBreath(args)
 	self:Message(args.spellId, "Important", "Long")
-	--self:CDBar(args.spellId, 34)
+	self:CDBar(args.spellId, 15.5)
 end
