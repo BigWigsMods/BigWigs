1b42281e1c9f8f77fcbed88eaa99fe231caf7e11
Justwait <justwait@method.gg>
2017-11-26 02:28:18 +0100

    Antorus/Locales: Add locales

---
+++ Antorus/Locales/ptBR.lua
@@ -0,0 +1,46 @@
+local L = BigWigs:NewBossLocale("Argus the Unmaker", "ptBR")
+if not L then return end
+if L then
+	--L.stage2_early = "Let the fury of the sea wash away this corruption!"
+	--L.stage3_early = "No hope. Just pain. Only pain!"
+end
+
+L = BigWigs:NewBossLocale("The Coven of Shivarra", "ptBR")
+if L then
+	--L.torment_of_the_titans_desc = "The Shivvara will force the titan souls to use their abilities against the players."
+end
+
+L = BigWigs:NewBossLocale("Eonar the Life-Binder", "ptBR")
+if L then
+	--L.warp_in_desc = "Shows timers and messages for each wave, along with any special adds in the wave."
+
+	--L.top_lane = "Top"
+	--L.mid_lane = "Mid"
+	--L.bot_lane = "Bot"
+
+	--L.purifier = "Purifier" -- Fel-Powered Purifier
+	--L.destructor = "Destructor" -- Fel-Infused Destructor
+	--L.obfuscator = "Obfuscator" -- Fel-Charged Obfuscator
+	--L.bats = "Fel Bats"
+end
+
+L = BigWigs:NewBossLocale("Portal Keeper Hasabel", "ptBR")
+if L then
+	--L.custom_on_filter_platforms = "Filter Side Platform Warnings and Bars"
+	--L.custom_on_filter_platforms_desc = "Removes unnecessary messages and bars if you are not on a side platform. It will always show bars and warnings from the main Platform: Nexus."
+	--L.platform_active = "%s Active!" -- Platform: Xoroth Active!
+end
+
+L = BigWigs:NewBossLocale("Kin'garoth", "ptBR")
+if L then
+	--L.empowered = "(E) %s" -- (E) Ruiner
+	--L.gains = "Kin'garoth gains %s" -- Kin'garoth gains Empowered Ruiner
+end
+
+L = BigWigs:NewBossLocale("Gorothi Worldbreaker", "ptBR")
+if L then
+	--L.cannon_ability_desc = "Display Messages and Bars related to the 2 cannons on the Gorothi Worldbreaker's back."
+end
+
+
+
