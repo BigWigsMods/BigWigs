BigWigsMods/BigWigs

commit 7e9d5f0d4a74b028cef659148759e19cd6284788

Replace EJ_GetSectionInfo usage with C_EncounterJournal.GetSectionInfo for WoW patch 7.3.5

Core/Constants.lua
@@ -5,7 +5,7 @@ local BigWigs = BigWigs
 local names = {}
 local descriptions = {}
 
-local GetSpellInfo, GetSpellTexture, GetSpellDescription, EJ_GetSectionInfo = GetSpellInfo, GetSpellTexture, GetSpellDescription, EJ_GetSectionInfo
+local GetSpellInfo, GetSpellTexture, GetSpellDescription, C_EncounterJournal_GetSectionInfo = GetSpellInfo, GetSpellTexture, GetSpellDescription, C_EncounterJournal.GetSectionInfo
 local type, next, tonumber, gsub, lshift, band = type, next, tonumber, gsub, bit.lshift, bit.band
 
 -- Option bitflags
@@ -105,7 +105,12 @@ local function replaceIdWithName(msg)
 	if id > 0 then
 		return GetSpellInfo(id) or BigWigs:Print(("No spell name found for boss option using id %d."):format(id))
 	else
-		return EJ_GetSectionInfo(-id) or BigWigs:Print(("No journal name found for boss option using id %d."):format(id))
+		local tbl = C_EncounterJournal_GetSectionInfo(-id)
+		if not tbl then
+			BigWigs:Print(("No journal name found for boss option using id %d."):format(id))
+		else
+			return tbl.title
+		end
 	end
 end
 local function replaceIdWithDescription(msg)
@@ -113,8 +118,12 @@ local function replaceIdWithDescription(msg)
 	if id > 0 then
 		return GetSpellDescription(id) or BigWigs:Print(("No spell description found for boss option using id %d."):format(id))
 	else
-		local _, d = EJ_GetSectionInfo(-id)
-		return d or BigWigs:Print(("No journal description found for boss option using id %d."):format(id))
+		local tbl = C_EncounterJournal_GetSectionInfo(-id)
+		if not tbl then
+			BigWigs:Print(("No journal description found for boss option using id %d."):format(id))
+		else
+			return tbl.description
+		end
 	end
 end
 
@@ -163,8 +172,8 @@ function BigWigs:GetBossOptionDetails(module, bossOption)
 				elseif icon > 0 then
 					icon = icon + 137000 -- Texture id list for raid icons 1-8 is 137001-137008. Base texture path is Interface\\TARGETINGFRAME\\UI-RaidTargetingIcon_%d
 				else
-					local _
-					_, _, _, icon = EJ_GetSectionInfo(-icon)
+					local tbl = C_EncounterJournal_GetSectionInfo(-icon)
+					icon = tbl.abilityIcon
 				end
 				if not icon then
 					BigWigs:Print(("No icon found for %s using id %d."):format(module.name, L[option .. "_icon"]))
@@ -190,11 +199,14 @@ function BigWigs:GetBossOptionDetails(module, bossOption)
 			return option, spellName..roleIcon, roleDesc..desc, icon
 		else
 			-- This is an EncounterJournal ID
-			local title, description, _, abilityIcon = EJ_GetSectionInfo(-option)
-			if not title then
+			local tbl = C_EncounterJournal_GetSectionInfo(-icon)
+			local title, description, abilityIcon
+			if not tbl then
 				BigWigs:Error(("Invalid option %d in module %s."):format(option, module.name))
 				title = option
 				description = option
+			else
+				title, description, abilityIcon = tbl.title, tbl.description, tbl.abilityIcon
 			end
 
 			local roleIcon, roleDesc = getRoleStrings(module, option)

Core/BossPrototype.lua
@@ -19,7 +19,7 @@
 
 local L = BigWigsAPI:GetLocale("BigWigs: Common")
 local UnitAffectingCombat, UnitIsPlayer, UnitGUID, UnitPosition, UnitIsConnected = UnitAffectingCombat, UnitIsPlayer, UnitGUID, UnitPosition, UnitIsConnected
-local EJ_GetSectionInfo, GetSpellInfo, GetSpellTexture, GetTime, IsSpellKnown = EJ_GetSectionInfo, GetSpellInfo, GetSpellTexture, GetTime, IsSpellKnown
+local C_EncounterJournal_GetSectionInfo, GetSpellInfo, GetSpellTexture, GetTime, IsSpellKnown = C_EncounterJournal.GetSectionInfo, GetSpellInfo, GetSpellTexture, GetTime, IsSpellKnown
 local UnitGroupRolesAssigned = UnitGroupRolesAssigned
 local SendChatMessage, GetInstanceInfo = BigWigsLoader.SendChatMessage, BigWigsLoader.GetInstanceInfo
 local format, find, gsub, band = string.format, string.find, string.gsub, bit.band
@@ -102,8 +102,12 @@ local icons = setmetatable({}, {__index =
 				-- Texture id list for raid icons 1-8 is 137001-137008. Base texture path is Interface\\TARGETINGFRAME\\UI-RaidTargetingIcon_%d
 				value = key + 137000
 			else
-				local _, _, _, abilityIcon = EJ_GetSectionInfo(-key)
-				value = abilityIcon or false
+				local tbl = C_EncounterJournal_GetSectionInfo(-key)
+				if not tbl then
+					core:Print(format("An invalid journal id (%d) is being used in a boss module.", key))
+				else
+					value = tbl.abilityIcon or false
+				end
 			end
 		else
 			value = "Interface\\Icons\\" .. key
@@ -121,9 +125,11 @@ local spells = setmetatable({}, {__index =
 				core:Print(format("An invalid spell id (%d) is being used in a boss module.", key))
 			end
 		else
-			value = EJ_GetSectionInfo(-key)
-			if not value then
+			local tbl = C_EncounterJournal_GetSectionInfo(-key)
+			if not tbl then
 				core:Print(format("An invalid journal id (%d) is being used in a boss module.", key))
+			else
+				value = tbl.title
 			end
 		end
 		self[key] = value

Core/Core.lua
@@ -409,7 +409,7 @@ end
 --
 
 do
-	local GetSpellInfo, EJ_GetSectionInfo = GetSpellInfo, EJ_GetSectionInfo
+	local GetSpellInfo, C_EncounterJournal_GetSectionInfo = GetSpellInfo, C_EncounterJournal.GetSectionInfo
 
 	local errorAlreadyRegistered = "%q already exists as a module in BigWigs, but something is trying to register it again."
 	local function new(core, moduleName, mapId, journalId, instanceId)
@@ -478,9 +478,9 @@ do
 						if not n then addon:Error(("Invalid spell ID %d in the optionHeaders for module %s."):format(v, module.name)) end
 						module.optionHeaders[k] = n or v
 					else
-						local n = EJ_GetSectionInfo(-v)
-						if not n then addon:Error(("Invalid journal ID (-)%d in the optionHeaders for module %s."):format(-v, module.name)) end
-						module.optionHeaders[k] = n or v
+						local tbl = C_EncounterJournal_GetSectionInfo(-v)
+						if not tbl then addon:Error(("Invalid journal ID (-)%d in the optionHeaders for module %s."):format(-v, module.name)) end
+						module.optionHeaders[k] = tbl.title or v
 					end
 				end
 			end
@@ -524,8 +524,8 @@ do
 						if not n then addon:Error(("Invalid spell ID %d in the toggleOptions for module %s."):format(v, module.name)) end
 						module.toggleDefaults[v] = bitflags
 					else
-						local n = EJ_GetSectionInfo(-v)
-						if not n then addon:Error(("Invalid journal ID (-)%d in the toggleOptions for module %s."):format(-v, module.name)) end
+						local tbl = C_EncounterJournal_GetSectionInfo(-v)
+						if not tbl then addon:Error(("Invalid journal ID (-)%d in the toggleOptions for module %s."):format(-v, module.name)) end
 						module.toggleDefaults[v] = bitflags
 					end
 				end

Options/Options.lua
@@ -593,14 +593,19 @@ do
 						currentSize = currentSize + #link + 1
 					end
 				else
-					local _, _, _, _, _, _, _, _, link = EJ_GetSectionInfo(-o)
-					if currentSize + #link + 1 > 255 then
-						printList(channel, header, abilities)
-						wipe(abilities)
-						currentSize = 0
+					local tbl = C_EncounterJournal.GetSectionInfo(-o)
+					if not tbl or not tbl.link then
+						BigWigs:Print(("Failed to fetch the link for journal id (-)%d."):format(-o))
+					else
+						local link = tbl.link
+						if currentSize + #link + 1 > 255 then
+							printList(channel, header, abilities)
+							wipe(abilities)
+							currentSize = 0
+						end
+						abilities[#abilities + 1] = link
+						currentSize = currentSize + #link + 1
 					end
-					abilities[#abilities + 1] = link
-					currentSize = currentSize + #link + 1
 				end
 			end
 		end

commit 4c35afc749dcdabe794e61f92029411624199b68

Core/Constants: Fix journal entries in config, regression from 7e9d5f0d4a74b028cef659148759e19cd6284788

Core/Constants.lua
@@ -199,7 +199,7 @@ function BigWigs:GetBossOptionDetails(module, bossOption)
 			return option, spellName..roleIcon, roleDesc..desc, icon
 		else
 			-- This is an EncounterJournal ID
-			local tbl = C_EncounterJournal_GetSectionInfo(-icon)
+			local tbl = C_EncounterJournal_GetSectionInfo(-option)
 			local title, description, abilityIcon
 			if not tbl then
 				BigWigs:Error(("Invalid option %d in module %s."):format(option, module.name))