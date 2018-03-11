BigWigsMods/BigWigs

commit 6a8ec0a41bb9d41025defc6c6412d56fe6ba16e5

Antorus/CovenOfShivarra: Split up the GroundEffectDamage function

Antorus/CovenofShivarra.lua
@@ -124,11 +124,15 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_REMOVED", "CosmicGlareRemoved", 250757)
 
 	--[[ Ground effects ]]--
-	self:Log("SPELL_AURA_APPLIED", "GroundEffectDamage", 245634, 253020) -- Whirling Saber, Storm of Darkness
-	self:Log("SPELL_PERIODIC_DAMAGE", "GroundEffectDamage", 245634, 253020)
-	self:Log("SPELL_PERIODIC_MISSED", "GroundEffectDamage", 245634, 253020)
-	self:Log("SPELL_DAMAGE", "GroundEffectDamage", 245629) -- Whirling Saber (Impact)
-	self:Log("SPELL_MISSED", "GroundEffectDamage", 245629)
+	self:Log("SPELL_AURA_APPLIED", "WhirlingSaberDamage", 245634)
+	self:Log("SPELL_PERIODIC_DAMAGE", "WhirlingSaberDamage", 245634, 253020)
+	self:Log("SPELL_PERIODIC_MISSED", "WhirlingSaberDamage", 245634, 253020)
+	self:Log("SPELL_DAMAGE", "WhirlingSaberDamage", 245629) -- Whirling Saber (Impact)
+	self:Log("SPELL_MISSED", "WhirlingSaberDamage", 245629)
+
+	self:Log("SPELL_AURA_APPLIED", "StormOfDarknessDamage", 253020)
+	self:Log("SPELL_PERIODIC_DAMAGE", "StormOfDarknessDamage", 253020)
+	self:Log("SPELL_PERIODIC_MISSED", "StormOfDarknessDamage", 253020)
 end
 
 function mod:OnEngage()
@@ -550,16 +554,22 @@ end
 --[[ Ground effects ]]--
 do
 	local prev = 0
-	local optionIds = {
-		[245629] = 245627, -- Whirling Saber
-		[245634] = 245627, -- Whirling Saber
-		[253020] = 252861, -- Storm of Darkness
-	}
-	function mod:GroundEffectDamage(args)
+	function mod:WhirlingSaberDamage(args)
 		local t = GetTime()
 		if self:Me(args.destGUID) and t-prev > 1.5 then
 			prev = t
-			self:Message(optionIds[args.spellId] or args.spellId, "Personal", "Alert", CL.underyou:format(args.spellName))
+			self:Message(245627, "Personal", "Alert", CL.underyou:format(args.spellName))
+		end
+	end
+end
+
+do
+	local prev = 0
+	function mod:StormOfDarknessDamage(args)
+		local t = GetTime()
+		if self:Me(args.destGUID) and t-prev > 1.5 then
+			prev = t
+			self:Message(252861, "Personal", "Alert", CL.underyou:format(args.spellName))
 		end
 	end
 end

commit d0aea47ff77235711edd1dee563367422687e7ec

Antorus/CovenofShivarra: Fix a regression caused by 6a8ec0a

Antorus/CovenofShivarra.lua
@@ -125,8 +125,8 @@ function mod:OnBossEnable()
 
 	--[[ Ground effects ]]--
 	self:Log("SPELL_AURA_APPLIED", "WhirlingSaberDamage", 245634)
-	self:Log("SPELL_PERIODIC_DAMAGE", "WhirlingSaberDamage", 245634, 253020)
-	self:Log("SPELL_PERIODIC_MISSED", "WhirlingSaberDamage", 245634, 253020)
+	self:Log("SPELL_PERIODIC_DAMAGE", "WhirlingSaberDamage", 245634)
+	self:Log("SPELL_PERIODIC_MISSED", "WhirlingSaberDamage", 245634)
 	self:Log("SPELL_DAMAGE", "WhirlingSaberDamage", 245629) -- Whirling Saber (Impact)
 	self:Log("SPELL_MISSED", "WhirlingSaberDamage", 245629)
 