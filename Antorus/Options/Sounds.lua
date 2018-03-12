f55fdfec078376c8018d28cca5837db35b4f9500
Kyle Buller <bullerk@gmail.com>
2018-03-11 23:15:08 -0500

    Add module sound and color files

---
+++ Antorus/Options/Sounds.lua
@@ -0,0 +1,182 @@
+
+BigWigs:AddSounds("Gorothi Worldbreaker", {
+	[246220] = {"Warning","Alarm"},
+	[244969] = "Warning",
+	[244410] = "Warning",
+	[244761] = "Alert",
+	[244106] = "Alarm",
+	[240277] = {"Info","Long","Alarm"},
+})
+
+BigWigs:AddSounds("Felhounds of Sargeras", {
+	[244072] = "Warning",
+	[244768] = "Warning",
+	[251445] = "Alarm",
+	[244057] = "Alert",
+	[244054] = "Warning",
+	[244131] = "Alert",
+	[248819] = "Warning",
+	[248815] = "Warning",
+	[244050] = "Warning",
+	[254429] = "Warning",
+	[251356] = "Info",
+	[244056] = "Alert",
+	[244055] = "Warning",
+	[245098] = "Alarm",
+})
+
+BigWigs:AddSounds("Antoran High Command", {
+	[245227] = "Long",
+	[245546] = "Alarm",
+	[244892] = "Warning",
+	[253039] = "Alert",
+	[246505] = "Alert",
+	[244737] = "Warning",
+	[245161] = "Info",
+	[244172] = "Warning",
+	[244910] = "Info",
+	[244625] = "Warning",
+})
+
+BigWigs:AddSounds("Portal Keeper Hasabel", {
+	[244689] = "Info",
+	[244926] = "Warning",
+	[244000] = "Alert",
+	["stages"] = "Long",
+	[244849] = "Info",
+	[245504] = "Alarm",
+	[244613] = "Info",
+	[244598] = "Alert",
+	[245040] = "Warning",
+	[245075] = "Info",
+	[255805] = "Alarm",
+	[246075] = "Alarm",
+	[245118] = "Info",
+	[243983] = "Warning",
+	[244607] = "Alarm",
+	[246316] = "Alarm",
+	[244016] = "Alarm",
+	[245050] = "Alert",
+})
+
+BigWigs:AddSounds("Eonar the Life-Binder", {
+	[250693] = "Alarm",
+	[250140] = "Alarm",
+	[248861] = "Warning",
+	[249121] = {"Warning","Info"},
+	[250691] = "Alarm",
+	[248332] = "Alarm",
+	[250048] = {"Info","Long"},
+	["warp_in"] = "Alert",
+})
+
+BigWigs:AddSounds("Imonar the Soulhunter", {
+	[247687] = {"Warning","Alarm"},
+	[254244] = {"Alarm","Alert"},
+	[247376] = "Alert",
+	[248068] = "Alert",
+	[248070] = "Warning",
+	[247367] = {"Warning","Alarm"},
+	[248254] = "Warning",
+	[250255] = "Alarm",
+	["stages"] = "Long",
+	[247923] = "Alert",
+})
+
+BigWigs:AddSounds("Kin'garoth", {
+	[254919] = {"Alert","Warning"},
+	[246686] = "Warning",
+	[246664] = "Alarm",
+	[246516] = {"Info","Long"},
+	[246833] = {"Warning","Info"},
+	[249680] = "Warning",
+	[254926] = {"Alert","Info"},
+	[246698] = "Warning",
+	[248375] = "Warning",
+	[248214] = {"Alarm","Info"},
+})
+
+BigWigs:AddSounds("Varimathras", {
+	[243960] = "Warning",
+	["stages"] = "Long",
+	[-16350] = "Alarm",
+	[244094] = {"Warning","Info"},
+	[243999] = "Alert",
+	[244042] = "Alarm",
+	[243961] = "Alarm",
+})
+
+BigWigs:AddSounds("The Coven of Shivarra", {
+	[244899] = "Info",
+	["torment_of_the_titans"] = {"Warning","Info"},
+	[253650] = "Alert",
+	[245627] = "Alert",
+	[252861] = {"Alarm","Alert"},
+	[250757] = "Alarm",
+	[250648] = "Alarm",
+	["stages"] = "Long",
+	[246329] = "Alert",
+	[245586] = "Alarm",
+	[253203] = "Info",
+	[253520] = "Alarm",
+	[245518] = "Info",
+})
+
+BigWigs:AddSounds("Aggramar", {
+	[246037] = "Warning",
+	[245994] = "Warning",
+	[245983] = "Warning",
+	[254452] = "Warning",
+	[245301] = "Warning",
+	["stages"] = "Long",
+	[245458] = "Alert",
+	["track_ember"] = "Info",
+	[255058] = "Alarm",
+	[255061] = "Warning",
+	[245463] = "Alarm",
+	[245990] = "Alarm",
+	[244693] = "Alert",
+})
+
+BigWigs:AddSounds("Argus the Unmaker", {
+	[248165] = "Warning",
+	[255826] = "Alarm",
+	[256388] = "Warning",
+	[-17077] = "Alert",
+	[255594] = {"Long","Info"},
+	[250669] = "Alarm",
+	["stages"] = "Long",
+	[257966] = "Warning",
+	[256544] = {"Warning","Info"},
+	[257296] = "Alarm",
+	[258834] = "Info",
+	[255200] = "Info",
+	[248167] = "Alarm",
+	[258838] = {"Alert","Alarm","Warning"},
+	[258068] = "Warning",
+	[248396] = "Warning",
+	[252616] = "Alarm",
+	[257214] = "Long",
+	[251570] = "Warning",
+	[258039] = {"Alert","Alarm","Warning"},
+	[248317] = "Alert",
+	[252729] = "Warning",
+	[248499] = {"Alert","Alarm","Warning"},
+	[255935] = "Alert",
+	[255199] = "Long",
+})
+
+BigWigs:AddSounds("Antorus Trash", {
+	[254512] = "Alert",
+	[245861] = "Alert",
+	[251612] = "Alert",
+	[246199] = "Alert",
+	[252621] = "Alarm",
+	[253600] = {"Alarm","Info"},
+	[249297] = "Warning",
+	[246209] = "Long",
+	[254122] = "Alarm",
+	[252760] = "Warning",
+	[249212] = "Warning",
+	[254500] = {"Warning","Long"},
+})
---
+++ Nightmare/Options/Sounds.lua
@@ -0,0 +1,113 @@
+
+BigWigs:AddSounds("Nythendra", {
+	[203045] = "Alert",
+	[225943] = "Long",
+	[203096] = "Warning",
+	[203552] = "Info",
+	[204463] = "Warning",
+	[204504] = "Warning",
+	[202977] = "Alarm",
+})
+
+BigWigs:AddSounds("Il'gynoth", {
+	[210099] = "Info",
+	[223121] = "Info",
+	[215128] = {"Alert","Warning"},
+	["shriveled_eyestalk"] = "Info",
+	[208697] = "Info",
+	[212886] = "Alert",
+	["stages"] = {"Long","Info"},
+	[218415] = {"Alarm","Long"},
+	["nightmare_horror"] = "Info",
+	[210984] = "Warning",
+	[208689] = "Alarm",
+	[208929] = {"Info","Alert"},
+})
+
+BigWigs:AddSounds("Elerethe Renferal", {
+	[212707] = "Long",
+	[212364] = "Info",
+	[215443] = {"Long","Warning"},
+	[215582] = "Long",
+	[213124] = "Alert",
+	[218124] = "Info",
+	["stages"] = "Info",
+	[210547] = "Alarm",
+	[210864] = {"Long","Warning"},
+	[210948] = "Info",
+	[215300] = "Warning",
+	[214348] = "Alarm",
+})
+
+BigWigs:AddSounds("Ursoc", {
+	[197969] = "Alarm",
+	[198006] = "Warning",
+	[204859] = {"Warning","Info"},
+	[205611] = "Alert",
+	[198388] = {"Info","Long"},
+	[197943] = {"Alarm","Warning"},
+})
+
+BigWigs:AddSounds("Dragons of Nightmare", {
+	[-12809] = {"Warning","Info"},
+	[204078] = "Alarm",
+	[-13460] = "Long",
+	[204767] = "Long",
+	[203147] = "Alert",
+	[204100] = "Long",
+	[205341] = "Alarm",
+	[205298] = "Long",
+	[203770] = {"Alert","Alarm"},
+	[203787] = "Alert",
+	[204040] = "Warning",
+	[203888] = "Alert",
+	[203028] = "Info",
+	[204245] = "Alarm",
+	[205300] = "Info",
+})
+
+BigWigs:AddSounds("Cenarius", {
+	[211192] = "Alert",
+	[210279] = {"Warning","Info"},
+	[212726] = "Info",
+	[211989] = "Alert",
+	[211471] = "Alert",
+	["stages"] = "Long",
+	[212681] = "Alert",
+	[226821] = "Long",
+	[214505] = "Alarm",
+	[214884] = "Info",
+	[213162] = "Alert",
+	[214529] = "Warning",
+	[214876] = "Alert",
+	[211368] = "Alarm",
+	[210290] = "Alarm",
+	[210346] = {"Alarm","Info"},
+})
+
+BigWigs:AddSounds("Xavius", {
+	[224508] = "Info",
+	[206005] = "Info",
+	[208431] = "Alarm",
+	[210264] = "Info",
+	[205843] = "Long",
+	["stages"] = "Long",
+	[207409] = "Alarm",
+	[205588] = "Info",
+	[205771] = "Long",
+	[211634] = "Alert",
+	[209034] = {"Alert","Warning"},
+	[206651] = "Warning",
+	[226194] = "Alert",
+	[209158] = "Warning",
+	[211802] = "Alert",
+	[205741] = "Alert",
+	[209443] = "Alarm",
+})
+
+BigWigs:AddSounds("Emerald Nightmare Trash", {
+	[221059] = "Info",
+	[221028] = "Warning",
+	[222719] = "Alert",
+	["totem"] = "Long",
+})
