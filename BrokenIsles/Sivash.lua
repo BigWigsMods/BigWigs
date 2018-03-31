5fc75e5ce2060326591244703d386816438ba4e3
Kyle Buller <bullerk@gmail.com>
2018-03-30 23:45:48 -0500

    BrokenIsles/Sivash: Fix Tidal Wave key

--- BrokenIsles/Sivash.lua
+++ BrokenIsles/Sivash.lua
@@ -53,8 +53,8 @@ function mod:BOSS_KILL(_, id)
 end
 
 function mod:TidalWave(args)
-	self:Message(args.spellId, "Urgent", "Warning")
-	self:CDBar(args.spellId, 22)
+	self:Message(233996, "Urgent", "Warning")
+	self:CDBar(233996, 22)
 end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, _, castGUID, spellId)
