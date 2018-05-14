cd38a1b573c42078493b5207677fbc11248d339f
funkydude <funkydude@users.noreply.github.com>
2018-05-14 13:17:28 +0100

    Update option files

---
+++ Uldir/Options/Colors.lua
@@ -0,0 +1,102 @@
+
+BigWigs:AddColors("Taloc", {
+	[270290] = {"blue","Personal"},
+	[271224] = {"Personal","yellow"},
+	[271296] = "red",
+	[271728] = "orange",
+	[271895] = "yellow",
+	[271965] = "green",
+	[275270] = {"blue","Personal"},
+	[275432] = {"blue","Personal"},
+	["stages"] = "green",
+})
+
+BigWigs:AddColors("MOTHER", {
+	[267787] = {"Personal","red","yellow"},
+	[267795] = {"blue","Personal","yellow"},
+	[267878] = "red",
+	[268198] = "orange",
+	[268253] = {"blue","Personal","yellow"},
+	[269051] = {"cyan","red"},
+	[274205] = {"green","Personal"},
+})
+
+BigWigs:AddColors("Fetid Devourer", {
+	[262277] = "red",
+	[262288] = "orange",
+	[262292] = "yellow",
+	[262313] = {"orange","Personal"},
+	[262314] = {"blue","Personal"},
+	[262364] = {"cyan","red"},
+	[262378] = "cyan",
+})
+
+BigWigs:AddColors("Zek'voz, Herald of N'zoth", {
+	[-18397] = "cyan",
+	[-18390] = "cyan",
+	[264382] = {"Personal","yellow"},
+	[265231] = "orange",
+	[265248] = "cyan",
+	[265360] = {"Personal","yellow"},
+	[265530] = "red",
+	[265662] = {"blue","Personal"},
+	[267180] = "yellow",
+	[267239] = "yellow",
+	["stages"] = "green",
+})
+
+BigWigs:AddColors("Vectis", {
+	[265178] = {"Personal","red"},
+	[265212] = {"orange","Personal"},
+	[265217] = "cyan",
+	[266459] = "red",
+	[267242] = "orange",
+})
+
+BigWigs:AddColors("Zul", {
+	[269936] = {"blue","Personal"},
+	[273288] = "yellow",
+	[273350] = "orange",
+	[273360] = "orange",
+	[273365] = {"Personal","yellow"},
+	[273451] = "red",
+	[273889] = "cyan",
+	[274168] = "green",
+	[274271] = {"orange","Personal"},
+	[274358] = {"Personal","red"},
+	[276299] = "yellow",
+})
+
+BigWigs:AddColors("Mythrax the Unraveler", {
+	[272115] = "orange",
+	[272404] = "red",
+	[272536] = {"Personal","yellow"},
+	[273282] = {"Personal","red","yellow"},
+	[273538] = "orange",
+	[273810] = "yellow",
+	[273949] = "red",
+	[274230] = "green",
+})
+
+BigWigs:AddColors("G'huun", {
+	[263235] = {"Personal","red"},
+	[263307] = "orange",
+	[263482] = "red",
+	[267409] = "red",
+	[267412] = "yellow",
+	[267427] = "yellow",
+	[267462] = "orange",
+	[267509] = "cyan",
+	[270287] = {"blue","Personal"},
+	[270428] = "yellow",
+	[270447] = {"Personal","red"},
+	[272506] = {"orange","Personal"},
+	[274577] = "red",
+	[275160] = "orange",
+	["stages"] = "cyan",
+})
+
+BigWigs:AddColors("Uldir Trash", {
+	[276540] = "Important",
+	[277047] = {"Attention","Personal"},
+})
---
+++ Uldir/Options/Sounds.lua
@@ -0,0 +1,101 @@
+
+BigWigs:AddSounds("Taloc", {
+	[270290] = "alarm",
+	[271224] = "warning",
+	[271296] = "warning",
+	[271728] = "alarm",
+	[271895] = "alert",
+	[271965] = "long",
+	[275270] = "warning",
+	[275432] = "alarm",
+})
+
+BigWigs:AddSounds("MOTHER", {
+	[267787] = {"alarm","alert"},
+	[267795] = {"alarm","alert"},
+	[267878] = "warning",
+	[268198] = "alert",
+	[268253] = {"alarm","alert"},
+	[269051] = {"alarm","info"},
+	[274205] = "long",
+})
+
+BigWigs:AddSounds("Fetid Devourer", {
+	[262277] = "alert",
+	[262288] = "alarm",
+	[262292] = "alert",
+	[262313] = "info",
+	[262314] = "warning",
+	[262364] = {"long","warning"},
+	[262378] = "info",
+})
+
+BigWigs:AddSounds("Zek'voz, Herald of N'zoth", {
+	[-18397] = "long",
+	[-18390] = "long",
+	[264382] = "alert",
+	[265231] = "alarm",
+	[265248] = "alert",
+	[265360] = "warning",
+	[265530] = "long",
+	[265662] = "long",
+	[267180] = "alert",
+	[267239] = "alert",
+	["stages"] = "Long",
+})
+
+BigWigs:AddSounds("Vectis", {
+	[265178] = "alert",
+	[265212] = "alert",
+	[265217] = {"info","long"},
+	[266459] = "warning",
+	[267242] = "alarm",
+})
+
+BigWigs:AddSounds("Zul", {
+	[269936] = "warning",
+	[273288] = "alert",
+	[273350] = "alert",
+	[273360] = "info",
+	[273365] = "warning",
+	[273451] = "alarm",
+	[273889] = "long",
+	[274168] = "long",
+	[274271] = "alarm",
+	[274358] = "warning",
+	[276299] = "alarm",
+})
+
+BigWigs:AddSounds("Mythrax the Unraveler", {
+	[272115] = "alert",
+	[272404] = "warning",
+	[272536] = "alert",
+	[273282] = {"alarm","alert"},
+	[273538] = "alert",
+	[273810] = "long",
+	[273949] = "warning",
+	[274230] = "long",
+})
+
+BigWigs:AddSounds("G'huun", {
+	[263235] = "warning",
+	[263307] = "alert",
+	[263482] = "long",
+	[267409] = "alert",
+	[267412] = "alarm",
+	[267427] = "alert",
+	[267462] = "warning",
+	[267509] = "info",
+	[270287] = "alarm",
+	[270428] = "alarm",
+	[270447] = "alarm",
+	[272506] = "warning",
+	[274577] = "alarm",
+	[275160] = "warning",
+	["stages"] = "long",
+})
+
+BigWigs:AddSounds("Uldir Trash", {
+	[276540] = "long",
+	[277047] = "warning",
+})
