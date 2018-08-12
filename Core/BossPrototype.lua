83dc5c80cf3fd19d63f5895e5107447ed308d790
funkydude <funkydude@users.noreply.github.com>
2018-08-12 23:18:24 +0100

    Core/BossPrototype: You can now disable messages without disabling sounds.

--- Core/BossPrototype.lua
+++ Core/BossPrototype.lua
@@ -2073,26 +2073,28 @@ do
 	-- @string sound the sound to play
 	-- @string[opt] voice command to play when using a voice pack
 	function boss:PlaySound(key, sound, voice, player)
-		if player then
-			local meOnly = checkFlag(self, key, C.ME_ONLY)
-			if type(player) == "table" then
-				if meOnly then
-					if player[#player] == cpName then
+		if checkFlag(self, key, C.SOUND) then
+			if player then
+				local meOnly = checkFlag(self, key, C.ME_ONLY)
+				if type(player) == "table" then
+					if meOnly then
+						if player[#player] == cpName then
+							self:SendMessage("BigWigs_Sound", self, key, tmp[sound] or sound)
+						end
+					elseif #player == 1 then
 						self:SendMessage("BigWigs_Sound", self, key, tmp[sound] or sound)
 					end
-				elseif #player == 1 then
-					self:SendMessage("BigWigs_Sound", self, key, tmp[sound] or sound)
+				else
+					if not meOnly or (meOnly and player == pName) then
+						self:SendMessage("BigWigs_Sound", self, key, tmp[sound] or sound)
+					end
 				end
 			else
-				if not meOnly or (meOnly and player == pName) then
+				if hasVoice and checkFlag(self, key, C.VOICE) then
+					self:SendMessage("BigWigs_Voice", self, key, tmp[sound] or sound)
+				else
 					self:SendMessage("BigWigs_Sound", self, key, tmp[sound] or sound)
 				end
-			end
-		elseif checkFlag(self, key, C.MESSAGE) then
-			if hasVoice and checkFlag(self, key, C.VOICE) then
-				self:SendMessage("BigWigs_Voice", self, key, tmp[sound] or sound)
-			else
-				self:SendMessage("BigWigs_Sound", self, key, tmp[sound] or sound)
 			end
 		end
 	end
