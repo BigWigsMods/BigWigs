4b2367794dea19d375d1476ef5fe0b1f0b9e1989
funkydude <funkydude@users.noreply.github.com>
2018-09-09 04:29:23 +0100

    Add "Noto Sans SemiCondensed".

--- Media/Media.lua
+++ Media/Media.lua
@@ -42,6 +42,7 @@ end
 do
 	local FONT = media.MediaType and media.MediaType.FONT or "font"
 	media:Register(FONT, "Noto Sans Regular", "Interface\\AddOns\\BigWigs\\Media\\Fonts\\Noto.ttf", 130) -- 130 latin + cyr
+	media:Register(FONT, "Noto Sans SemiCondensed", "Interface\\AddOns\\BigWigs\\Media\\Fonts\\NotoSans-SemiCondensed.ttf", 130) -- 130 latin + cyr
 end
 
 -- XXX temp
