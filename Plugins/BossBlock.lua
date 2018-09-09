72b7142d18c378b6b85a781cd688c6c8a7339e20
funkydude <funkydude@users.noreply.github.com>
2018-09-09 22:17:52 +0100

    Plugins/BossBlock: Add Zul cinematic.

--- Plugins/BossBlock.lua
+++ Plugins/BossBlock.lua
@@ -218,6 +218,7 @@ do
 		[-914] = true, -- Antorus, teleportation to "The burning throne"
 		[-917] = true, -- Antorus, magni portal to argus room
 		[-1004] = true, -- Kings' Rest, before the last boss "Dazar"
+		[-1152] = true, -- Uldir, raising stairs for Zul
 	}
 
 	-- Cinematic skipping hack to workaround an item (Vision of Time) that creates cinematics in Siege of Orgrimmar.
