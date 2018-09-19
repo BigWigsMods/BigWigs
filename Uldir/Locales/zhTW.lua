80d15355108c772d2adf24c6179f13bc68d18a80
EK <eke00372@gmail.com>
2018-09-19 10:16:03 +0800

    Update zhTW.lua

--- Uldir/Locales/zhTW.lua
+++ Uldir/Locales/zhTW.lua
@@ -10,7 +10,7 @@ L = BigWigs:NewBossLocale("Zek'voz, Herald of N'zoth", "zhTW")
 if L then
 	L.surging_darkness_eruption = "Eruption (%d)"
 	L.mythic_adds = "傳奇模式增援"
-	--L.mythic_adds_desc = "Show timers for when the adds will spawn in Mythic (both Qiraji Warrior and Anub'ar Voidweaver spawn at the same time)."
+	L.mythic_adds_desc = "替傳奇模式特有的小怪組合顯示計時器（異種蠍戰士和奈幽虛織者同時出現的那一波）。"
 end
 
 L = BigWigs:NewBossLocale("Fetid Devourer", "zhTW")
@@ -20,25 +20,25 @@ end
 
 L = BigWigs:NewBossLocale("Zul", "zhTW")
 if L then
-	L.crawg_msg = "克洛格Crawg" -- Short for 'Bloodthirsty Crawg'
-	--L.crawg_desc = "Warnings and timers for when the Bloodthirsty Crawg spawns."
+	L.crawg_msg = "克洛格" -- Short for 'Bloodthirsty Crawg'
+	L.crawg_desc = "替克洛格的生成顯示計時器和警告"
 
 	L.bloodhexer_msg = "血咒師" -- Short for 'Nazmani Bloodhexer'
-	--L.bloodhexer_desc = "Warnings and timers for when the Nazmani Bloodhexer spawns."
+	L.bloodhexer_desc = "替血咒師的生成顯示計時器和警告"
 
 	L.crusher_msg = "粉碎者" -- Short for 'Nazmani Crusher'
-	--L.crusher_desc = "Warnings and timers for when the Nazmani Crusher spawns."
+	L.crusher_desc = "替粉碎者的生成顯示計時器和警告"
 
-	--L.custom_off_decaying_flesh_marker = "Decaying Flesh Marker"
-	--L.custom_off_decaying_flesh_marker_desc = "Mark the enemy forces afflicted by Decaying Flesh with {rt8}, requires promoted or leader."
+	L.custom_off_decaying_flesh_marker = "腐朽肉體標記"
+	L.custom_off_decaying_flesh_marker_desc = "用 {rt8} 標記受到腐朽肉體效果的敵人，需要權限。"
 end
 
 L = BigWigs:NewBossLocale("Mythrax the Unraveler", "zhTW")
 if L then
 	L.destroyer_cast = "恩拉奇毀滅者：%s" -- npc id: 139381
 	L.xalzaix_returned = "薩爾札克斯出現！Xalzaix returned!" -- npc id: 138324
-	--L.add_blast = "Add Blast"
-	--L.boss_blast = "Boss Blast"
+	L.add_blast = "小怪滅寂衝擊"
+	L.boss_blast = "首領滅寂衝擊"
 end
 
 L = BigWigs:NewBossLocale("G'huun", "zhTW")
@@ -46,6 +46,6 @@ if L then
 	L.orbs_deposited = "充能（%d／3） - %.1f秒"
 	L.orb_spawning = "能量矩陣出現"
 
-	--L.custom_on_fixate_plates = "Fixate icon on Enemy Nameplate"
-	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+	L.custom_on_fixate_plates = "在敵方名條顯示追擊圖示"
+	L.custom_on_fixate_plates_desc = "當你被凝視時，在敵方名條上顯示一個圖示。\n需要啟用敵方名條，此功能目前只支援KuiNameplates。"
 end
