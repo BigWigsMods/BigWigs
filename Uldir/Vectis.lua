0d9bf8c5368234124b5a2df830143f38a72d4d25
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2018-10-12 12:36:22 -0500

    Uldir/Vectis: Always play a sound when Gestate is cast, not only when on you (#562 & #563)

--- Uldir/Vectis.lua
+++ Uldir/Vectis.lua
@@ -344,10 +344,10 @@ function mod:Gestate(args)
 		self:CDBar(265212, timer)
 	end
 	if self:Me(args.destGUID) then
-		self:PlaySound(265212, "alert")
 		self:Say(265212)
 	end
 	self:TargetMessage2(265212, "orange", args.destName)
+	self:PlaySound(265212, "alert", nil, args.destName)
 	self:PrimaryIcon(265212, args.destName)
 	immunosuppressionCount = 1
 	self:CDBar(265206, 6, CL.count:format(self:SpellName(265206), immunosuppressionCount)) -- Immunosuppression
