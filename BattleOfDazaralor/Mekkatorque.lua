5784cd6a8e8e0727fc8b01aa55c6be0305726daa
Justwait <justwait@method.gg>
2018-10-25 01:14:17 +0200

    BattleOfDazaralor/Mekkatorque: Drycode

--- BattleOfDazaralor/Mekkatorque.lua
+++ BattleOfDazaralor/Mekkatorque.lua
@@ -1,5 +1,4 @@
 if not IsTestBuild() then return end
-if UnitFactionGroup("player") ~= "Horde" then return end
 
 --------------------------------------------------------------------------------
 -- Module Declaration
@@ -30,11 +29,29 @@ mod.engageId = 2276
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		-- General
+		282153, -- Buster Cannon
+		282205, -- Blast Off
+		{283409, "SAY", "SAY_COUNTDOWN"}, -- Gigavolt Charge
+		287952, -- Dimensional Ripper XL
+		284042, -- Deploy Spark Bot
+		288049, -- Shrink Ray
+		286051, -- Hyperdrive
+		-- Intermission
+		287929, -- Signal Exploding Sheep
 	}
 end
 
 function mod:OnBossEnable()
+	self:Log("SPELL_CAST_START", "BusterCannon", 282153)
+	self:Log("SPELL_CAST_START", "BlastOff", 282205)
+	self:Log("SPELL_AURA_APPLIED", "GigavoltChargeApplied", 283409)
+	self:Log("SPELL_AURA_REMOVED", "GigavoltChargeRemoved", 283409)
+	self:Log("SPELL_CAST_START", "DimensionalRipperXL", 287952)
+	self:Log("SPELL_CAST_SUCCESS", "DeploySparkBot", 284042)
+	self:Log("SPELL_CAST_START", "ShrinkRay", 288049)
+	self:Log("SPELL_AURA_APPLIED", "Hyperdrive", 286051)
+	self:Log("SPELL_CAST_START", "SignalExplodingSheep", 287929)
 end
 
 function mod:OnEngage()
@@ -43,3 +60,53 @@ end
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:BusterCannon(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:BlastOff(args)
+	self:Message2(args.spellId, "orange")
+	self:PlaySound(args.spellId, "alarm")
+end
+
+function mod:GigavoltChargeApplied(args)
+	self:TargetMessage2(args.spellId, "yellow", args.destName)
+	if self:Me(args.destGUID) then
+		self:PlaySound(args.spellId, "warning")
+		self:Say(args.spellId)
+		self:SayCountdown(args.spellId, 15)
+	end
+end
+
+function mod:GigavoltChargeRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
+	end
+end
+
+function mod:DimensionalRipperXL(args)
+	self:Message2(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
+end
+
+function mod:DeploySparkBot(args)
+	self:Message2(args.spellId, "cyan")
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:ShrinkRay(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "long")
+end
+
+function mod:Hyperdrive(args)
+	self:Message2(args.spellId, "cyan")
+	self:PlaySound(args.spellId, "info")
+end
+
+function mod:SignalExplodingSheep(args)
+	self:Message2(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "long")
+end
