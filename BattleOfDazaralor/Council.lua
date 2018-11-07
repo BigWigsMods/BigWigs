b15d19879048197d26c71e1e0b4ab46a51344f48
Justwait <justwait@method.gg>
2018-11-08 00:02:43 +0100

    BattleOfDazaralor/Council: Initial updates with Aspect abilities

--- BattleOfDazaralor/Council.lua
+++ BattleOfDazaralor/Council.lua
@@ -1,4 +1,8 @@
 if not IsTestBuild() then return end
+--------------------------------------------------------------------------------
+-- TODO:
+-- - Loa Abilities
+-- - Stop/Start bars on boss kills/swaps
 
 --------------------------------------------------------------------------------
 -- Module Declaration
@@ -6,7 +10,7 @@ if not IsTestBuild() then return end
 
 local mod, CL = BigWigs:NewBoss("Loa Council", 2070, 2330)
 if not mod then return end
-mod:RegisterEnableMob(0)
+mod:RegisterEnableMob(144747, 144767, 144963, 144941)
 mod.engageId = 2268
 --mod.respawnTime = 31
 
@@ -38,7 +42,7 @@ function mod:GetOptions()
 		285945, -- Hastening Winds
 		282107, -- Pa'ku's Wrath
 		-- Gonk's Aspect
-		{282135, "ICON", "SAY", "SAY_COUNTDOWN"}, -- Crawling Hex
+		{282135, "SAY", "SAY_COUNTDOWN"}, -- Crawling Hex
 		285893, -- Wild Maul
 		282155, -- Gonk's Wrath
 		{282209, "FLASH"}, -- Mark of Prey
@@ -66,25 +70,30 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "PakusWrath", 282107)
 
 	-- Gonk's Aspect
+	self:Log("SPELL_CAST_START", "CrawlingHex", 283193)
 	self:Log("SPELL_AURA_APPLIED", "CrawlingHexApplied", 282135)
 	self:Log("SPELL_AURA_REMOVED", "CrawlingHexRemoved", 282135)
 	self:Log("SPELL_CAST_SUCCESS", "WildMaul", 285893)
 	self:Log("SPELL_CAST_START", "GonksWrath", 282155)
-	self:Log("SPELL_AURA_APPLIED", "MarkofPrey", 282135)
+	self:Log("SPELL_AURA_APPLIED", "MarkofPrey", 282209)
 
 	-- Kimbul's Aspect
-	self:Log("SPELL_AURA_APPLIED", "LaceratingClawsApplied", 285945)
-	self:Log("SPELL_AURA_APPLIED_DOSE", "LaceratingClawsApplied", 285945)
+	self:Log("SPELL_CAST_START", "LaceratingClaws", 282444)
+	self:Log("SPELL_AURA_APPLIED", "LaceratingClawsApplied", 282444)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "LaceratingClawsApplied", 282444)
 	self:Log("SPELL_CAST_SUCCESS", "KimbulsWrath", 282447)
 
 	-- Akunda's Aspect
 	self:Log("SPELL_CAST_START", "ThunderingStorm", 282411)
 	self:Log("SPELL_CAST_START", "MindWipe", 285878)
 	self:Log("SPELL_AURA_APPLIED", "AkundasWrathApplied", 286811)
-	self:Log("SPELL_AURA_APPLIED", "AkundasWrathRemoved", 286811)
+	self:Log("SPELL_AURA_REMOVED", "AkundasWrathRemoved", 286811)
 end
 
 function mod:OnEngage()
+	self:CDBar(282098, 5) -- Gift of Wind
+	self:CDBar(282135, 13.5) -- Crawling Hex
+	self:CDBar(285893, 17) -- Wild Maul
 end
 
 --------------------------------------------------------------------------------
@@ -110,11 +119,12 @@ end
 function mod:GiftofWind(args)
 	self:Message2(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 32)
 end
 
 function mod:HasteningWinds(args)
 	local amount = args.amount or 1
-	if amount % 2 == 1 and amount > 4 then
+	if amount % 4 == 1 and amount > 12 then
 		self:StackMessage(args.spellId, args.destName, args.amount, "purple")
 		self:PlaySound(args.spellId, "alarm", nil, args.destName)
 	end
@@ -125,26 +135,35 @@ function mod:PakusWrath(args)
 	self:PlaySound(args.spellId, "warning")
 end
 
-function mod:CrawlingHexApplied(args)
-	self:TargetMessage2(args.spellId, "yellow", args.destName)
-	self:PrimaryIcon(args.spellId, args.destName)
-	if self:Me(args.destGUID) then
-		self:PlaySound(args.spellId, "warning")
-		self:Say(args.spellId)
-		self:SayCountdown(args.spellId, 5)
-	end
+function mod:CrawlingHex(args)
+	self:Message2(282135, "orange")
+	self:PlaySound(282135, "alarm")
+	self:CDBar(282135, 25.5)
 end
 
-function mod:CrawlingHexRemoved(args)
-	if self:Me(args.destGUID) then
-		self:CancelSayCountdown(args.spellId)
+do
+	local playerList = mod:NewTargetList()
+	function mod:CrawlingHexApplied(args)
+		playerList[#playerList+1] = args.destName
+		self:TargetsMessage(args.spellId, "orange", playerList)
+		if self:Me(args.destGUID) then
+			self:PlaySound(args.spellId, "warning")
+			self:Say(args.spellId)
+			self:SayCountdown(args.spellId, 5)
+		end
 	end
-	self:PrimaryIcon(args.spellId, args.destName)
+
+	function mod:CrawlingHexRemoved(args)
+		if self:Me(args.destGUID) then
+			self:CancelSayCountdown(args.spellId)
+		end
+	end
 end
 
 function mod:WildMaul(args)
 	self:Message2(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 17)
 end
 
 function mod:GonksWrath(args)
@@ -161,6 +180,10 @@ function mod:MarkofPrey(args)
 	end
 end
 
+function mod:LaceratingClaws(args)
+	self:CDBar(args.spellId, 28) -- 27.9-30.4~
+end
+
 function mod:LaceratingClawsApplied(args)
 	self:StackMessage(args.spellId, args.destName, args.amount, "purple")
 	self:PlaySound(args.spellId, "alarm", nil, args.destName)
@@ -174,28 +197,25 @@ end
 function mod:ThunderingStorm(args)
 	self:Message2(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
+	self:CDBar(args.spellId, 19.5)
 end
 
 function mod:MindWipe(args)
 	self:Message2(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
+	self:CDBar(args.spellId, 30.5)
 end
 
-do
-	local playerList = mod:NewTargetList()
-	function mod:AkundasWrathApplied(args)
-		playerList[#playerList+1] = args.destName
-		self:TargetsMessage(args.spellId, "orange", playerList)
-		if self:Me(args.destGUID) then
-			self:PlaySound(args.spellId, "warning")
-			self:Say(args.spellId)
-			self:SayCountdown(args.spellId, 6)
-		end
+function mod:AkundasWrathApplied(args)
+	if self:Me(args.destGUID) then
+		self:PlaySound(args.spellId, "warning")
+		self:Say(args.spellId)
+		self:SayCountdown(args.spellId, 6)
 	end
+end
 
-	function mod:AkundasWrathRemoved(args)
-		if self:Me(args.destGUID) then
-			self:CancelSayCountdown(args.spellId)
-		end
+function mod:AkundasWrathRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
 	end
 end
