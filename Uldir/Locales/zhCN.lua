796988b048efd8fd10a94736b7b4a29f3b7d9fc0
Adavak LEE <adavak@adavak.com>
2018-12-15 16:01:15 +0800

    zhCN update.

--- Uldir/Locales/zhCN.lua
+++ Uldir/Locales/zhCN.lua
@@ -1,16 +1,16 @@
 local L = BigWigs:NewBossLocale("MOTHER", "zhCN")
 if not L then return end
 if L then
-	L.sideLaser = "侧面光束" -- short for: (location) Uldir Defensive Beam
-	L.upLaser = "顶部光束"
-	L.mythic_beams = "光束"
+	L.sideLaser = "侧面射线" -- short for: (location) Uldir Defensive Beam
+	L.upLaser = "顶部射线"
+	L.mythic_beams = "射线"
 end
 
 L = BigWigs:NewBossLocale("Zek'voz, Herald of N'zoth", "zhCN")
 if L then
 	L.surging_darkness_eruption = "翻腾黑暗（%d）"
-	L.mythic_adds = "史诗模式增援"
-	L.mythic_adds_desc = "替史詩模式特有的小怪組合顯示計時器（异种虫战士和蛛魔虚空编织者同时出现的那一波）。"
+	L.mythic_adds = "史诗增援"
+	L.mythic_adds_desc = "当在史诗模式下即将出现增援时显示计时器。（异种虫战士和蛛魔虚空编织者同时出现）"
 end
 
 L = BigWigs:NewBossLocale("Fetid Devourer", "zhCN")
@@ -21,13 +21,13 @@ end
 L = BigWigs:NewBossLocale("Zul", "zhCN")
 if L then
 	L.crawg_msg = "抱齿兽" -- Short for 'Bloodthirsty Crawg'
-	L.crawg_desc = "替抱齿兽的生成显示计时器和警告。"
+	L.crawg_desc = "当嗜血的抱齿兽出现时的计时器和警报。"
 
 	L.bloodhexer_msg = "鲜血妖术师" -- Short for 'Nazmani Bloodhexer'
-	L.bloodhexer_desc = "替鲜血妖术师的生成显示计时器和警告。"
+	L.bloodhexer_desc = "当纳兹曼尼鲜血妖术师出现时的计时器和警报。"
 
 	L.crusher_msg = "碾压者" -- Short for 'Nazmani Crusher'
-	L.crusher_desc = "替碾压者的生成显示计时器和警告。"
+	L.crusher_desc = "当纳兹曼尼碾压者出现时的计时器和警报。"
 
 	L.custom_off_decaying_flesh_marker = "腐烂血肉标记"
 	L.custom_off_decaying_flesh_marker_desc = "使用 {rt8} 标记受到腐烂血肉效果的敌人，需要权限。"
@@ -36,8 +36,8 @@ end
 L = BigWigs:NewBossLocale("Mythrax the Unraveler", "zhCN")
 if L then
 	L.destroyer_cast = "恩拉其毁灭者：%s" -- npc id: 139381
-	L.xalzaix_returned = "夏尔扎克斯出现！" -- npc id: 138324
-	L.add_blast = "小怪湮灭冲击"
+	L.xalzaix_returned = "夏尔扎克斯返场！" -- npc id: 138324
+	L.add_blast = "增援湮灭冲击"
 	L.boss_blast = "首领湮灭冲击"
 end
 
@@ -51,6 +51,9 @@ if L then
 
 	L.custom_on_fixate_plates = "敌对姓名板凝视图标"
 	L.custom_on_fixate_plates_desc = "当你被凝视时在目标姓名板上显示一个图标。\n需要使用敌对姓名板。此功能目前只支持 KuiNameplates。"
+
+	L.custom_on_stop_timers = "总是显示技能条"
+	L.custom_on_stop_timers_desc = "戈霍恩可以延迟它的技能。当此选项开启时，这些技能条将会保留在屏幕上。"
 end
 
 L = BigWigs:NewBossLocale("Uldir Trash", "zhCN")
