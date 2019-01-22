a06614c836986567180ff7167ff185b0cbd818e8
Adavak LEE <adavak@adavak.com>
2019-01-22 09:42:59 +0800

    BattleOfDazaralor: Add Locales (#574)

---
+++ BattleOfDazaralor/Locales/ptBR.lua
@@ -0,0 +1,37 @@
+local L = BigWigs:NewBossLocale("Flamefist and the Illuminated", "ptBR")
+if not L then return end
+if L then
+	--L.custom_on_fixate_plates = "Stalking icon on Enemy Nameplate"
+	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is stalking on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+
+	--L.absorb = "Absorb"
+	--L.absorb_text = "%s (|cff%s%.0f%%|r)"
+	--L.cast = "Cast"
+	--L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
+
+	--L.interrupted_after = "%s interrupted by %s (%.1f seconds remaining)"
+end
+
+L = BigWigs:NewBossLocale("Grimfang and Firecaller", "ptBR")
+if L then
+	--L.custom_on_fixate_plates = "Stalking icon on Enemy Nameplate"
+	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is stalking on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+
+	--L.absorb = "Absorb"
+	--L.absorb_text = "%s (|cff%s%.0f%%|r)"
+	--L.cast = "Cast"
+	--L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
+
+	--L.interrupted_after = "%s interrupted by %s (%.1f seconds remaining)"
+end
+
+L = BigWigs:NewBossLocale("Treasure Guardian", "ptBR")
+if L then
+	--L.custom_on_hand_timers = "The Hand of In'zashi"
+	--L.custom_on_hand_timers_desc = "Show warnings and bars for The Hand of In'zashi's abilities."
+	--L.hand_cast = "Hand: %s"
+
+	--L.custom_on_bulwark_timers = "Yalat's Bulwark"
+	--L.custom_on_bulwark_timers_desc = "Show warnings and bars for Yalat's Bulwark's abilities."
+	--L.bulwark_cast = "Bulwark: %s"
+end
---
+++ BattleOfDazaralor/Locales/zhCN.lua
@@ -0,0 +1,37 @@
+local L = BigWigs:NewBossLocale("Flamefist and the Illuminated", "zhCN")
+if not L then return end
+if L then
+	L.custom_on_fixate_plates = "追踪图标位于敌对姓名板"
+	L.custom_on_fixate_plates_desc = "当你被追踪时在目标姓名板上显示一个图标。\n需要使用敌对姓名板。此功能目前只支持 KuiNameplates。"
+
+	L.absorb = "吸收"
+	L.absorb_text = "%s（|cff%s%.0f%%|r）"
+	L.cast = "施放"
+	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
+
+	L.interrupted_after = "%s被%s中断（%.1f秒剩余）"
+end
+
+L = BigWigs:NewBossLocale("Grimfang and Firecaller", "zhCN")
+if L then
+	L.custom_on_fixate_plates = "追踪图标位于敌对姓名板"
+	L.custom_on_fixate_plates_desc = "当你被追踪时在目标姓名板上显示一个图标。\n需要使用敌对姓名板。此功能目前只支持 KuiNameplates。"
+
+	L.absorb = "吸收"
+	L.absorb_text = "%s（|cff%s%.0f%%|r）"
+	L.cast = "施放"
+	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
+
+	L.interrupted_after = "%s被%s中断（%.1f秒剩余）"
+end
+
+L = BigWigs:NewBossLocale("Treasure Guardian", "zhCN")
+if L then
+	L.custom_on_hand_timers = "因扎希之手"
+	L.custom_on_hand_timers_desc = "显示因扎希之手技能警报和计时条。"
+	L.hand_cast = "手：%s"
+
+	L.custom_on_bulwark_timers = "亚拉特的堡垒"
+	L.custom_on_bulwark_timers_desc = "显示亚拉特的堡垒技能警报和计时条。"
+	L.bulwark_cast = "堡垒：%s"
+end
