9b9c9d2b1b9947bb0ea79d2cdca1b3271fe07610
funkydude <funkydude@users.noreply.github.com>
2019-01-24 20:35:36 +0000

    BattleOfDazaralor/GrongAlliance: Fix engage id

--- BattleOfDazaralor/GrongAlliance.lua
+++ BattleOfDazaralor/GrongAlliance.lua
@@ -6,7 +6,7 @@ if UnitFactionGroup("player") ~= "Alliance" then return end
 local mod, CL = BigWigs:NewBoss("Grong the Revenant", 2070, 2340)
 if not mod then return end
 mod:RegisterEnableMob(144638)
-mod.engageId = 2263
+mod.engageId = 2284
 --mod.respawnTime = 31
 
 --------------------------------------------------------------------------------
