6402204546d6292fa9cb34c3b71d316790756774
EK <eke00372@gmail.com>
2019-01-25 10:12:12 +0800

    Update zhTW.lua

--- BattleOfDazaralor/Locales/zhTW.lua
+++ BattleOfDazaralor/Locales/zhTW.lua
@@ -1,37 +1,37 @@
 local L = BigWigs:NewBossLocale("Flamefist and the Illuminated", "zhTW")
 if not L then return end
 if L then
-	L.custom_on_fixate_plates = "追蹤圖示位於敵方名條"
-	L.custom_on_fixate_plates_desc = "當你被追蹤時在目標名條上顯示一個圖示。\n需要使用敵方名條。此功能目前只支援 KuiNameplates。"
+	L.custom_on_fixate_plates = "在敵方名條顯示追獵圖示"
+	L.custom_on_fixate_plates_desc = "當你被追獵時，在敵方名條上顯示一個圖示。\n需要啟用敵方名條，此功能目前只有KuiNameplates支援。"
 
 	L.absorb = "吸收"
 	L.absorb_text = "%s（|cff%s%.0f%%|r）"
 	L.cast = "施放"
 	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
 
-	L.interrupted_after = "%s被%s中斷（%.1f秒剩餘）"
+	L.interrupted_after = "%s被%s中斷（尚餘%.1f秒）"
 end
 
 L = BigWigs:NewBossLocale("Grimfang and Firecaller", "zhTW")
 if L then
-	L.custom_on_fixate_plates = "追蹤圖示位於敵方名條"
-	L.custom_on_fixate_plates_desc = "當你被追蹤時在目標名條上顯示一個圖示。\n需要使用敵方名條。此功能目前只支援 KuiNameplates。"
+	L.custom_on_fixate_plates = "在敵方名條顯示追獵圖示"
+	L.custom_on_fixate_plates_desc = "當你被追獵時，在敵方名條上顯示一個圖示。\n需要啟用敵方名條，此功能目前只有KuiNameplates支援。"
 
 	L.absorb = "吸收"
 	L.absorb_text = "%s（|cff%s%.0f%%|r）"
 	L.cast = "施放"
 	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
 
-	L.interrupted_after = "%s被%s中斷（%.1f秒剩餘）"
+	L.interrupted_after = "%s被%s中斷（尚餘%.1f秒）"
 end
 
 L = BigWigs:NewBossLocale("Treasure Guardian", "zhTW")
 if L then
 	L.custom_on_hand_timers = "因扎希之手"
-	L.custom_on_hand_timers_desc = "顯示因扎希之手技能警報和計時條。"
+	L.custom_on_hand_timers_desc = "顯示因扎希之手的技能警報和計時器。"
 	L.hand_cast = "手：%s"
 
 	L.custom_on_bulwark_timers = "雅菈特的壁壘"
-	L.custom_on_bulwark_timers_desc = "顯示雅菈特的壁壘技能警報和計時條。"
+	L.custom_on_bulwark_timers_desc = "顯示雅菈特的壁壘的技能警報和計時器。"
 	L.bulwark_cast = "壁壘：%s"
 end
