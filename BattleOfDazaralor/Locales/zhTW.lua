1c82bffe001315004a2e2ac617aa7e75a43eb1ec
EK <eke00372@gmail.com>
2019-01-28 02:47:24 +0800

    Update zhTW. (#581)

--- BattleOfDazaralor/Locales/zhTW.lua
+++ BattleOfDazaralor/Locales/zhTW.lua
@@ -1,61 +1,61 @@
 local L = BigWigs:NewBossLocale("Champion of the Light Horde", "zhTW")
 if not L then return end
 if L then
-	--L.disorient_desc = "Bar for the |cff71d5ff[Blinding Faith]|r cast.\nThis is probably the bar you want to have the countdown on." -- Blinding Faith = 283650
+	L.disorient_desc = "為|cff71d5ff[盲信]|r顯示施法條，如果你需要一個倒數計時的話。" -- Blinding Faith = 283650
 end
 
 L = BigWigs:NewBossLocale("Champion of the Light Alliance", "zhTW")
 if L then
-	--L.disorient_desc = "Bar for the |cff71d5ff[Blinding Faith]|r cast.\nThis is probably the bar you want to have the countdown on." -- Blinding Faith = 283650
+	L.disorient_desc = "為|cff71d5ff[盲信]|r顯示施法條，如果你需要一個倒數計時的話。" -- Blinding Faith = 283650
 end
 
 L = BigWigs:NewBossLocale("Jadefire Masters Horde", "zhTW")
 if L then
-	L.custom_on_fixate_plates = "追蹤圖示位於敵方名條"
-	L.custom_on_fixate_plates_desc = "當你被追蹤時在目標名條上顯示一個圖示。\n需要使用敵方名條。此功能目前只支援 KuiNameplates。"
+	L.custom_on_fixate_plates = "在敵方名條顯示追獵圖示"
+	L.custom_on_fixate_plates_desc = "當你被追獵時，在敵方名條上顯示一個圖示。\n需要啟用敵方名條，此功能目前只有KuiNameplates支援。"
 
 	L.absorb = "吸收"
 	L.absorb_text = "%s（|cff%s%.0f%%|r）"
 	L.cast = "施放"
 	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
 
-	L.interrupted_after = "%s被%s中斷（%.1f秒剩餘）"
+	L.interrupted_after = "%s被%s中斷（尚餘%.1f秒）"
 end
 
 L = BigWigs:NewBossLocale("Jadefire Masters Alliance", "zhTW")
 if L then
-	L.custom_on_fixate_plates = "追蹤圖示位於敵方名條"
-	L.custom_on_fixate_plates_desc = "當你被追蹤時在目標名條上顯示一個圖示。\n需要使用敵方名條。此功能目前只支援 KuiNameplates。"
+	L.custom_on_fixate_plates = "在敵方名條顯示追獵圖示"
+	L.custom_on_fixate_plates_desc = "當你被追獵時，在敵方名條上顯示一個圖示。\n需要啟用敵方名條，此功能目前只有KuiNameplates支援。"
 
 	L.absorb = "吸收"
 	L.absorb_text = "%s（|cff%s%.0f%%|r）"
 	L.cast = "施放"
 	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
 
-	L.interrupted_after = "%s被%s中斷（%.1f秒剩餘）"
+	L.interrupted_after = "%s被%s中斷（尚餘%.1f秒）"
 end
 
 L = BigWigs:NewBossLocale("Opulence", "zhTW")
 if L then
 	L.custom_on_hand_timers = "因扎希之手"
-	L.custom_on_hand_timers_desc = "顯示因扎希之手技能警報和計時條。"
+	L.custom_on_hand_timers_desc = "顯示因扎希之手的技能警報和計時條。"
 	L.hand_cast = "手：%s"
 
 	L.custom_on_bulwark_timers = "雅菈特的壁壘"
-	L.custom_on_bulwark_timers_desc = "顯示雅菈特的壁壘技能警報和計時條。"
+	L.custom_on_bulwark_timers_desc = "顯示雅菈特的壁壘的技能警報和計時條。"
 	L.bulwark_cast = "壁壘：%s"
 end
 
 L = BigWigs:NewBossLocale("Conclave of the Chosen", "zhTW")
 if L then
-	--L.killed = "%s killed!"
-	L.count_of = "%s （%d/%d）"
+	L.killed = "%s%已擊殺！"
+	L.count_of = "%s（%d／%d）"
 end
 
 L = BigWigs:NewBossLocale("High Tinker Mekkatorque", "zhTW")
 if L then
-	--L.gigavolt_alt_text = "Bomb"
+	L.gigavolt_alt_text = "炸彈"
 
-	--L.custom_off_sparkbot_marker = "Spark Bot Marker"
-	--L.custom_off_sparkbot_marker_desc = "Mark Spark Bots with {rt4}{rt5}{rt6}{rt7}{rt8}."
+	L.custom_off_sparkbot_marker = "火花機器人標記"
+	L.custom_off_sparkbot_marker_desc = "使用 {rt4}{rt5}{rt6}{rt7}{rt8} 標記火花機器人，需要權限。"
 end
