69bfa9da378e7e6bb46b9a292d89528276480b3d
elvador <reysptools@googlemail.com>
2019-02-05 00:04:43 +0100

    BattleOfDazaralor/Mekkatorque: Fixed all timers for mythic and heroic

--- BattleOfDazaralor/Mekkatorque.lua
+++ BattleOfDazaralor/Mekkatorque.lua
@@ -19,9 +19,126 @@ mod.respawnTime = 33
 --
 
 local stage = 1
+local cannonCount = 1
+local blastCount = 1
+local chargeCount = 1
+local wormholeCount = 1
 local sparkBotCount = 1
+local enlargerCount = 1
+local sheepCount = 1
 local botMarkCount = 0
 local mobCollector = {}
+
+local lfrTimers = { -- XXX add me
+}
+
+local normalTimers = { -- XXX add me
+}
+
+local heroicTimers = {
+	-- "SPELL_CAST_START", "BusterCannon", 282153
+	[282153] = {
+		[1] = {13, 33, 64.5, 40, 26.5, 65, 28},
+		[2] = {},
+		[3] = {17.7, 29, 64.5, 40, 26.5, 65, 28}
+	},
+
+	-- "SPELL_CAST_START", "BlastOff", 282205
+	[282205] = {
+		[1] = {41.1, 26.9, 37.5, 34.3, 50.2, 75.6,},
+		[2] = {},
+		[3] = {41.7, 29, 35.5, 34.3, 50.2, 75, 30.5},
+	},
+
+	-- "SPELL_CAST_SUCCESS", "GigavoltCharge", 287757, 286597
+	[286646] = {
+		[1] = {21.5, 40, 40, 33, 42, 40, 44.5},
+		[2] = {17.4, 34.1},
+		[3] = {22.2, 40, 40, 35, 40, 40, 47.6, 27.4},
+	},
+
+	-- "SPELL_CAST_START", "WormholeGenerator", 287952
+	[287952] = {
+		[1] = {38, 98.7, 125.8},
+		[2] = {46.9},
+		[3] = {38.6, 98.8, 122.7}
+	},
+
+	-- "SPELL_CAST_SUCCESS", "DeploySparkBot", 288410, 287691
+	[288410] = {
+		[1] = {6.5, 22.5, 27.5, 42.5, 22.5, 25, 42.5, 22.5, 22.5},
+		[2] = {21.5},
+		[3] = {17.2, 40, 42.5, 47.4, 42.5, 45, 55}
+	},
+
+	-- "SPELL_CAST_START", "WorldEnlarger", 286693, 288041, 289537
+	[286693] = {
+		[1] = {75, 90, 90},
+		[2] = {8, 31},
+		[3] = {75.7, 90, 90}
+	},
+
+	-- "SPELL_CAST_START", "SignalExplodingSheep", 287929
+	[287929] = {
+		[1] = {},
+		[2] = {12.8, 30, 12},
+		[3] = {28.2, 100.5, 82},
+	},
+}
+
+local mythicTimers = {
+	-- "SPELL_CAST_START", "BusterCannon", 282153
+	[282153] = {
+		[1] = {13.1, 33.0, 33.5, 31.0, 40.0, 26.5, 65.0, 28.0, 30.5},
+		[2] = {},
+		[3] = {17.8, 29.0, 33.5, 31.0, 40.0, 26.5},
+	},
+
+	-- "SPELL_CAST_START", "BlastOff", 282205
+	[282205] = {
+		[1] = {41.5, 28.5, 35.5, 34.5, 49.9, 75.5, 30.0},
+		[2] = {},
+		[3] = {42.2, 28.5, 35.5, 34.6},
+	},
+
+	-- "SPELL_CAST_SUCCESS", "GigavoltCharge", 287757, 286597
+	[286646] = {
+		[1] = {20, 41.5, 38.5, 34.5, 82, 43, 30.5},
+		[2] = {},
+		[3] = {20.5, 40, 40, 35, 40},
+	},
+
+	-- "SPELL_CAST_START", "WormholeGenerator", 287952
+	[287952] = {
+		[1] = {38.0, 98.7, 125.8},
+		[2] = {50.3},
+		[3] = {38.7, 98.7},
+	},
+
+	-- "SPELL_CAST_SUCCESS", "DeploySparkBot", 288410, 287691
+	[288410] = {
+		[1] = {6.6, 22.5, 27.5, 42.5, 22.5, 25.0, 42.5, 22.5, 22.5, 55.0, 22.5},
+		[2] = {21.3},
+		[3] = {17.2, 40.0, 42.5, 48.5, 41.5},
+	},
+
+	-- "SPELL_CAST_START", "WorldEnlarger", 286693, 288041, 289537
+	[286693] = {
+		[1] = {75.0, 90.0},
+		[2] = {7.8, 31.0},
+		[3] = {75.8, 90.0, 90.0},
+	},
+
+	-- "SPELL_CAST_START", "SignalExplodingSheep", 287929
+	[287929] = {
+		[1] = {},
+		[2] = {12.8, 30.0, 12.0},
+		[3] = {29.8, 92.5},
+	},
+}
+
+-- local timers = mod:Mythic() and mythicTimers or mod:Heroic() and heroicTimers or mod:Normal() and normalTimers or lfrTimers
+local timers = mod:Mythic() and mythicTimers or heroicTimers
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -75,16 +192,25 @@ end
 
 function mod:OnEngage()
 	stage = 1
-	botMarkCount = 0
+	cannonCount = 1
+	blastCount = 1
+	chargeCount = 1
+	wormholeCount = 1
 	sparkBotCount = 1
-	mobCollector = {}
+	enlargerCount = 1
+	sheepCount = 1
+	botMarkCount = 0
+	wipe(mobCollector)
 
-	self:CDBar(288410, 6.5, CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
-	self:CDBar(282153, 13) -- Buster Cannon
-	self:CDBar(286646, 20, L.gigavolt_alt_text) -- Bombs // Gigavolt Charge
-	self:CDBar(287952, 38) -- Wormhole Generator
-	self:CDBar(282205, 41) -- Blast Off
-	self:CDBar(286693, 75) -- World Enlarger
+	-- local timers = mod:Mythic() and mythicTimers or mod:Heroic() and heroicTimers or mod:Normal() and normalTimers or lfrTimers
+	timers = mythicTimers
+
+	self:Bar(288410, timers[288410][1][sparkBotCount], CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
+	self:Bar(282153, timers[282153][1][cannonCount], CL.count:format(self:SpellName(282153), cannonCount)) -- Buster Cannon
+	self:Bar(286646, timers[286646][1][chargeCount], CL.count:format(L.gigavolt_alt_text, chargeCount)) -- Bombs // Gigavolt Charge
+	self:Bar(287952, timers[287952][1][wormholeCount], CL.count:format(self:SpellName(287952), wormholeCount)) -- Wormhole Generator
+	self:Bar(282205, timers[282205][1][blastCount], CL.count:format(self:SpellName(282205), blastCount)) -- Blast Off
+	self:Bar(286693, timers[286693][1][enlargerCount], CL.count:format(self:SpellName(286693), enlargerCount)) -- World Enlarger
 
 	if self:GetOption("custom_off_sparkbot_marker") then
 		self:RegisterTargetEvents("sparkBotMark")
@@ -105,22 +231,22 @@ function mod:UNIT_HEALTH_FREQUENT(event, unit)
 end
 
 function mod:BusterCannon(args)
-	self:Message2(args.spellId, "yellow")
+	self:Message2(args.spellId, "yellow", CL.count:format(args.spellName, cannonCount))
 	self:PlaySound(args.spellId, "warning")
-	-- XXX Improve Fix Timers
-	self:CDBar(args.spellId, 25)
+	cannonCount = cannonCount + 1
+	self:Bar(args.spellId, timers[args.spellId][stage][cannonCount], CL.count:format(args.spellName, cannonCount))
 end
 
 function mod:BlastOff(args)
-	self:Message2(args.spellId, "orange")
+	self:Message2(args.spellId, "orange", CL.count:format(args.spellName, blastCount))
 	self:PlaySound(args.spellId, "alarm")
-	-- XXX Improve Fix Timers
-	self:CDBar(args.spellId, 30)
+	blastCount = blastCount + 1
+	self:Bar(args.spellId, timers[args.spellId][stage][blastCount], CL.count:format(args.spellName, blastCount))
 end
 
 function mod:GigavoltCharge(args)
-	-- XXX Improve Fix Timers
-	self:CDBar(286646, 30, L.gigavolt_alt_text)
+	chargeCount = chargeCount + 1
+	self:Bar(286646, timers[286646][stage][chargeCount], CL.count:format(args.spellName, chargeCount))
 end
 
 do
@@ -157,18 +283,17 @@ do
 end
 
 function mod:WormholeGenerator(args)
-	self:Message2(args.spellId, "red")
+	self:Message2(args.spellId, "red", CL.count:format(args.spellName, wormholeCount))
 	self:PlaySound(args.spellId, "alert")
-	-- XXX Improve Timers
-	self:CDBar(args.spellId, 44)
+	wormholeCount = wormholeCount + 1
+	self:Bar(args.spellId, timers[args.spellId][stage][wormholeCount], CL.count:format(args.spellName, wormholeCount))
 end
 
 function mod:DeploySparkBot(args)
 	self:Message2(288410, "cyan", CL.count:format(args.spellName, sparkBotCount))
 	self:PlaySound(288410, "info")
 	sparkBotCount = sparkBotCount + 1
-	-- XXX Improve Timers
-	self:CDBar(288410, stage == 1 and (sparkBotCount % 3 == 1 and 42.5 or 22.5) or 40, CL.count:format(args.spellName, sparkBotCount))
+	self:Bar(288410, timers[288410][stage][sparkBotCount], CL.count:format(args.spellName, sparkBotCount))
 end
 
 function mod:sparkBotMark(event, unit, guid)
@@ -180,25 +305,38 @@ function mod:sparkBotMark(event, unit, guid)
 end
 
 function mod:WorldEnlarger(args)
-	self:Message2(286693, "yellow")
+	self:Message2(286693, "yellow", CL.count:format(args.spellName, enlargerCount))
 	self:PlaySound(286693, "long")
-	-- XXX Improve timers
+	enlargerCount = enlargerCount + 1
+	self:Bar(286693, timers[286693][stage][enlargerCount], CL.count:format(args.spellName, enlargerCount))
 end
 
 function mod:EvasiveManeuvers(args)
 	stage = 2
 	self:Message2("stages", "cyan", CL.stage:format(stage), false)
 	self:PlaySound("stages", "long")
-	self:StopBar(282153) -- Buster Cannon
-	self:StopBar(282205) -- Blast Off
-	self:StopBar(L.gigavolt_alt_text) -- Bombs
-	self:StopBar(287952) -- Wormhole Generator
-	self:StopBar(286693) -- World Enlarger
+	self:StopBar(CL.count:format(self:SpellName(282153), cannonCount)) -- Buster Cannon
+	self:StopBar(CL.count:format(self:SpellName(282205), blastCount)) -- Blast Off
+	self:StopBar(CL.count:format(L.gigavolt_alt_text, chargeCount)) -- Bombs
+	self:StopBar(CL.count:format(self:SpellName(287952), wormholeCount)) -- Wormhole Generator
+	self:StopBar(CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
+	self:StopBar(CL.count:format(self:SpellName(286693), enlargerCount)) -- World Enlarger
 
-	self:CDBar(286693, 7.5) -- World Enlarger
-	self:CDBar(286646, 17.5, L.gigavolt_alt_text) -- Bombs // Gigavolt Charge
-	self:CDBar(288410, 21.3, CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
-	self:CDBar(287952, 46.5) -- Wormhole Generator
+	cannonCount = 1
+	blastCount = 1
+	chargeCount = 1
+	wormholeCount = 1
+	sparkBotCount = 1
+	enlargerCount = 1
+	sheepCount = 1
+
+	if not self:Mythic() then
+		self:Bar(286646, timers[286646][2][chargeCount], CL.count:format(L.gigavolt_alt_text, chargeCount)) -- Bombs // Gigavolt Charge
+	end
+	self:Bar(287952, timers[287952][2][wormholeCount], CL.count:format(self:SpellName(287952), wormholeCount)) -- Wormhole Generator
+	self:Bar(288410, timers[288410][2][sparkBotCount], CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
+	self:Bar(286693, timers[286693][2][enlargerCount], CL.count:format(self:SpellName(286693), enlargerCount)) -- World Enlarger
+	self:Bar(287929, timers[287929][2][sheepCount], CL.count:format(self:SpellName(287929), sheepCount)) -- Signal Exploding Sheep
 	self:CDBar("stages", 65, 287797) -- Crash Down
 end
 
@@ -206,17 +344,34 @@ function mod:CrashDown(args)
 	stage = 3
 	self:Message2("stages", "cyan", CL.stage:format(3), false)
 	self:PlaySound("stages", "long")
+	self:StopBar(CL.count:format(self:SpellName(282153), cannonCount)) -- Buster Cannon
+	self:StopBar(CL.count:format(self:SpellName(282205), blastCount)) -- Blast Off
+	self:StopBar(CL.count:format(L.gigavolt_alt_text, chargeCount)) -- Bombs
+	self:StopBar(CL.count:format(self:SpellName(287952), wormholeCount)) -- Wormhole Generator
+	self:StopBar(CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
+	self:StopBar(CL.count:format(self:SpellName(286693), enlargerCount)) -- World Enlarger
+	self:StopBar(CL.count:format(self:SpellName(287929), sheepCount)) -- Signal Exploding Sheep
 
-	self:CDBar(288410, 17, CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
-	self:CDBar(282153, 17.5) -- Buster Cannon
-	self:CDBar(286646, 22, L.gigavolt_alt_text) -- Bombs // Gigavolt Charge
-	self:CDBar(287952, 38.5) -- Wormhole Generator
-	self:CDBar(282205, 41.5) -- Blast Off
-	self:CDBar(286693, 75.5) -- World Enlarger
+	cannonCount = 1
+	blastCount = 1
+	chargeCount = 1
+	wormholeCount = 1
+	sparkBotCount = 1
+	enlargerCount = 1
+	sheepCount = 1
+
+	self:Bar(282153, timers[282153][3][cannonCount], CL.count:format(self:SpellName(282153), cannonCount)) -- Buster Cannon
+	self:Bar(282205, timers[282205][3][blastCount], CL.count:format(self:SpellName(282205), blastCount)) -- Blast Off
+	self:Bar(286646, timers[286646][3][chargeCount], CL.count:format(L.gigavolt_alt_text, chargeCount)) -- Bombs // Gigavolt Charge
+	self:Bar(287952, timers[287952][3][wormholeCount], CL.count:format(self:SpellName(287952), wormholeCount)) -- Wormhole Generator
+	self:Bar(288410, timers[288410][3][sparkBotCount], CL.count:format(self:SpellName(288410), sparkBotCount)) -- Deploy Spark Bot
+	self:Bar(286693, timers[286693][3][enlargerCount], CL.count:format(self:SpellName(286693), enlargerCount)) -- World Enlarger
+	self:Bar(287929, timers[287929][3][sheepCount], CL.count:format(self:SpellName(287929), sheepCount)) -- Signal Exploding Sheep
 end
 
 function mod:SignalExplodingSheep(args)
-	self:Message2(args.spellId, "yellow")
+	self:Message2(args.spellId, "yellow", CL.count:format(args.spellName, sheepCount))
 	self:PlaySound(args.spellId, "long")
-	-- XXX Improve/Add Timers?
+	sheepCount = sheepCount + 1
+	self:Bar(args.spellId, timers[args.spellId][stage][sheepCount], CL.count:format(args.spellName, sheepCount))
 end
