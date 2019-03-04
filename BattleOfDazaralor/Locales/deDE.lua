2255f22ff95aa6be532a9bb337616e238cced80e
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2019-03-04 19:00:05 +0100

    deDE update (#615)

--- BattleOfDazaralor/Locales/deDE.lua
+++ BattleOfDazaralor/Locales/deDE.lua
@@ -65,8 +65,8 @@ end
 
 L = BigWigs:NewBossLocale("Conclave of the Chosen", "deDE")
 if L then
-	-- L.custom_on_fixate_plates = "Mark of Prey icon on Enemy Nameplate"
-	-- L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+	L.custom_on_fixate_plates = "Mal der Beute Symbol an Namensplaketten fixierender Gegner"
+	L.custom_on_fixate_plates_desc = "Zeigt ein Symbol an der Namensplakette des Dich fixierenden Ziels an.\nBenötigt die Verwendung gegnerischer Namensplaketten. Diese Funktion wird derzeit nur von KuiNameplates unterstützt."
 	L.killed = "%s getötet!"
 	L.count_of = "%s (%d/%d)"
 end
