641f54b662755e2cbe0ebfc6ca01c47fc99435af
funkydude <funkydude@users.noreply.github.com>
2019-03-15 01:27:49 +0000

    Fix module names

--- BattleOfDazaralor/Locales/esES.lua
+++ BattleOfDazaralor/Locales/esES.lua
@@ -1,4 +1,4 @@
-local L = BigWigs:NewBossLocale("Basura de Batalla de Dazar'alor", "esES") or BigWigs:NewBossLocale("Basura de Batalla de Dazar'alor", "esMX")
+local L = BigWigs:NewBossLocale("Battle of Dazar'alor Trash", "esES") or BigWigs:NewBossLocale("Battle of Dazar'alor Trash", "esMX")
 if not L then return end
 if L then
 	L.prelate = "Prelado Akk'al"
@@ -11,17 +11,17 @@ if L then
 	L.witness = "¡%s ha apuñalado a %s con %s!"
 end
 
-L = BigWigs:NewBossLocale("Campeones de la Luz Horda", "esES") or BigWigs:NewBossLocale("Campeones de la Luz Horda", "esMX")
+L = BigWigs:NewBossLocale("Champion of the Light Horde", "esES") or BigWigs:NewBossLocale("Champion of the Light Horde", "esMX")
 if L then
 	L.disorient_desc = "Barra para el lanzamiento de |cff71d5ff[Fe cegadora]|r.\nEsta es probablemente la barra que querrás tener con la cuenta atrás activada." -- Blinding Faith = 283650
 end
 
-L = BigWigs:NewBossLocale("Campeones de la Luz Alianza", "esES") or BigWigs:NewBossLocale("Campeones de la Luz Alianza", "esMX")
+L = BigWigs:NewBossLocale("Champion of the Light Alliance", "esES") or BigWigs:NewBossLocale("Champion of the Light Alliance", "esMX")
 if L then
 	L.disorient_desc = "Barra para el lanzamiento de |cff71d5ff[Fe cegadora]|r.\nEsta es probablemente la barra que querrás tener con la cuenta atrás activada." -- Blinding Faith = 283650
 end
 
-L = BigWigs:NewBossLocale("Maestros Fuego de Jade Horda", "esES") or BigWigs:NewBossLocale("Maestros Fuego de Jade Horda", "esMX")
+L = BigWigs:NewBossLocale("Jadefire Masters Horde", "esES") or BigWigs:NewBossLocale("Jadefire Masters Horde", "esMX")
 if L then
 	L.custom_on_fixate_plates = "Icono de acecho en la placa de identificación del enemigo"
 	L.custom_on_fixate_plates_desc = "Muestra un ícono en la placa de identificación del objetivo que te está acechando.\nRequiere el uso de placas de identificación enemigas. Esta característica actualmente solo es compatible con KuiNameplates."
@@ -34,7 +34,7 @@ if L then
 	L.interrupted_after = "%s interrumpido por %s (%.1f segundos restantes)"
 end
 
-L = BigWigs:NewBossLocale("Maestros Fuego de Jade Alianza", "esES") or BigWigs:NewBossLocale("Maestros Fuego de Jade Alianza", "esMX")
+L = BigWigs:NewBossLocale("Jadefire Masters Alliance", "esES") or BigWigs:NewBossLocale("Jadefire Masters Alliance", "esMX")
 if L then
 	L.custom_on_fixate_plates = "Icono de acecho en la placa de identificación del enemigo"
 	L.custom_on_fixate_plates_desc = "Muestra un ícono en la placa de identificación del objetivo que te está acechando.\nRequiere el uso de placas de identificación enemigas. Esta característica actualmente solo es compatible con KuiNameplates."
@@ -47,7 +47,7 @@ if L then
 	L.interrupted_after = "%s interrumpido por %s (%.1f segundos restantes)"
 end
 
-L = BigWigs:NewBossLocale("Opulencia", "esES") or BigWigs:NewBossLocale("Opulencia", "esMX")
+L = BigWigs:NewBossLocale("Opulence", "esES") or BigWigs:NewBossLocale("Opulence", "esMX")
 if L then
 	L.room = "Sala (%d/8)"
 	L.no_jewel = "Sin Gemas:"
@@ -64,7 +64,7 @@ if L then
 	L.bulwark_cast = "Baluarte: %s"
 end
 
-L = BigWigs:NewBossLocale("Cónclave de los Elegidos", "esES") or BigWigs:NewBossLocale("Cónclave de los Elegidos", "esMX")
+L = BigWigs:NewBossLocale("Conclave of the Chosen", "esES") or BigWigs:NewBossLocale("Conclave of the Chosen", "esMX")
 if L then
 	L.custom_on_fixate_plates = "Icono de marca de la presa en la placa de identificación del enemigo"
 	L.custom_on_fixate_plates_desc = "Muestra un ícono en la placa de identificación del objetivo que te está fijando.\nRequiere el uso de placas de identificación enemigas. Esta característica actualmente solo es compatible con KuiNameplates."
@@ -72,12 +72,12 @@ if L then
 	--L.count_of = "%s (%d/%d)"
 end
 
-L = BigWigs:NewBossLocale("Rey Rastakhan", "esES") or BigWigs:NewBossLocale("Rey Rastakhan", "esMX")
+L = BigWigs:NewBossLocale("King Rastakhan", "esES") or BigWigs:NewBossLocale("King Rastakhan", "esMX")
 if L then
 	L.leap_cancelled = "Salto Cancelado"
 end
 
-L = BigWigs:NewBossLocale("Manitas Mayor Mekkatorque", "esES") or BigWigs:NewBossLocale("Manitas Mayor Mekkatorque", "esMX")
+L = BigWigs:NewBossLocale("High Tinker Mekkatorque", "esES") or BigWigs:NewBossLocale("High Tinker Mekkatorque", "esMX")
 if L then
 	L.gigavolt_alt_text = "Bomba"
 
@@ -91,7 +91,7 @@ if L then
 	L.custom_off_repeating_tampering_say_desc = "Dices repetidamente tu nombre cuando estás controlando un Chispabot."
 end
 
-L = BigWigs:NewBossLocale("Bloqueo de la Tormenta", "esES") or BigWigs:NewBossLocale("Bloqueo de la Tormenta", "esMX")
+L = BigWigs:NewBossLocale("Stormwall Blockade", "esES") or BigWigs:NewBossLocale("Stormwall Blockade", "esMX")
 if L then
 	L.killed = "¡%s muerto!"
 
@@ -99,7 +99,7 @@ if L then
 	--L.custom_on_fade_out_bars_desc = "Fade out bars which belong to the boss who isn't active on your boat in stage 1."
 end
 
-L = BigWigs:NewBossLocale("Lady Jaina Valiente", "esES") or BigWigs:NewBossLocale("Lady Jaina Valiente", "esMX")
+L = BigWigs:NewBossLocale("Lady Jaina Proudmoore", "esES") or BigWigs:NewBossLocale("Lady Jaina Proudmoore", "esMX")
 if L then
 	L.starbord_ship_emote = "¡Un corsario de Kul Tiran se acerca en el lado de estribor!"
 	L.port_side_ship_emote = "¡Un corsario de Kul Tiran se acerca en el lado de babor!"
