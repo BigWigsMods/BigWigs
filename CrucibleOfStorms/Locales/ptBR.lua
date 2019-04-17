319565f65858caffbdc5dd4280ad4e8356e15576
Adavak LEE <adavak@adavak.com>
2019-04-17 20:46:46 +0800

    CoS locale.

---
+++ CrucibleOfStorms/Locales/ptBR.lua
@@ -0,0 +1,6 @@
+local L = BigWigs:NewBossLocale("The Restless Cabal", "ptBR")
+if not L then return end
+if L then
+	--L.custom_off_eldritch_marker = "Eldritch Abomination Marker"
+	--L.custom_off_eldritch_marker_desc = "Mark Eldritch Abomination with {rt4}{rt5}{rt6}."
+end
---
+++ CrucibleOfStorms/Locales/zhCN.lua
@@ -0,0 +1,6 @@
+local L = BigWigs:NewBossLocale("The Restless Cabal", "zhCN")
+if not L then return end
+if L then
+	L.custom_off_eldritch_marker = "恐怖的憎恶体标记"
+	L.custom_off_eldritch_marker_desc = "使用 {rt4}{rt5}{rt6} 标记恐怖的憎恶体。"
+end
