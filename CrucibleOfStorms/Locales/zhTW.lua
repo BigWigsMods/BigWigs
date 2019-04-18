b82269d5cc7756fee6d177f25b88e68f8e748264
EK <eke00372@gmail.com>
2019-04-19 05:27:37 +0800

    Update zhTW.lua

--- CrucibleOfStorms/Locales/zhTW.lua
+++ CrucibleOfStorms/Locales/zhTW.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("The Restless Cabal", "zhTW")
 if not L then return end
 if L then
-	L.custom_off_eldritch_marker = "異法畸怪"
+	L.custom_off_eldritch_marker = "異法畸怪標記"
 	L.custom_off_eldritch_marker_desc = "使用 {rt4}{rt5}{rt6} 標記異法畸怪，需要權限。"
 end
