874c1170620228a9d2cc0fe2cf03c2bcff4179f9
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2019-04-19 14:20:04 +0200

    Update deDE.lua

--- CrucibleOfStorms/Locales/deDE.lua
+++ CrucibleOfStorms/Locales/deDE.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("The Restless Cabal", "deDE")
 if not L then return end
 if L then
-	L.custom_off_eldritch_marker = "Unheimliche Monstrosität markieren"
+	L.custom_off_eldritch_marker = "Unheimliche Monstrositäten markieren"
 	L.custom_off_eldritch_marker_desc = "Markiert Unheimliche Monstrositäten mit {rt3}{rt4}{rt5}."
 
 	L.absorb = "Absorbtion"
@@ -10,6 +10,6 @@ end
 
 L = BigWigs:NewBossLocale("Uu'nat, Harbinger of the Void", "deDE")
 if L then
-	--L.custom_off_mindbender_marker = "Primordial Mindbender Marker"
-	--L.custom_off_mindbender_marker_desc = "Mark Primordial Mindbender with {rt1}{rt2}{rt3}."
+	L.custom_off_mindbender_marker = "Urzeitliche Geistbeuger markieren"
+	L.custom_off_mindbender_marker_desc = "Markiert Urzeitliche Geistbeuger mit {rt1}{rt2}{rt3}."
 end
