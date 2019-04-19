b92715702092a998a640aae55b8c9433cae537f8
BlueNightSky <afan1128@hotmail.com>
2019-04-19 20:31:54 +0800

    Update zhTW.lua

--- CrucibleOfStorms/Locales/zhTW.lua
+++ CrucibleOfStorms/Locales/zhTW.lua
@@ -10,6 +10,6 @@ end
 
 L = BigWigs:NewBossLocale("Uu'nat, Harbinger of the Void", "zhTW")
 if L then
-	--L.custom_off_mindbender_marker = "Primordial Mindbender Marker"
-	--L.custom_off_mindbender_marker_desc = "Mark Primordial Mindbender with {rt1}{rt2}{rt3}."
+	L.custom_off_mindbender_marker = "原始屈心魔標記"
+	L.custom_off_mindbender_marker_desc = "使用 {rt1}{rt2}{rt3} 標記原始屈心魔，需要權限。"
 end
