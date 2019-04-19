4d8be525c8cfe07d541855ee59f44fe4d20e171d
Justwait <justwait@method.gg>
2019-04-19 02:28:17 +0200

    CrucibleOfStorms/Locales: Update Locale files

--- CrucibleOfStorms/Locales/zhTW.lua
+++ CrucibleOfStorms/Locales/zhTW.lua
@@ -2,5 +2,14 @@ local L = BigWigs:NewBossLocale("The Restless Cabal", "zhTW")
 if not L then return end
 if L then
 	L.custom_off_eldritch_marker = "異法畸怪標記"
-	L.custom_off_eldritch_marker_desc = "使用 {rt4}{rt5}{rt6} 標記異法畸怪，需要權限。"
+	L.custom_off_eldritch_marker_desc = "使用 {rt3}{rt4}{rt5} 標記異法畸怪，需要權限。"
+
+	L.absorb = "吸收"
+	L.absorb_text = "%s（|cff%s%.0f%%|r）"
+end
+
+L = BigWigs:NewBossLocale("Uu'nat, Harbinger of the Void", "zhTW")
+if L then
+	--L.custom_off_mindbender_marker = "Primordial Mindbender Marker"
+	--L.custom_off_mindbender_marker_desc = "Mark Primordial Mindbender with {rt1}{rt2}{rt3}."
 end
