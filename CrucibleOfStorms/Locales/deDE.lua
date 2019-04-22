865ad13b6cd4d09246bbcf7100d6fa6f7650d7b3
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2019-04-23 00:15:03 +0200

    Update deDE.lua

--- CrucibleOfStorms/Locales/deDE.lua
+++ CrucibleOfStorms/Locales/deDE.lua
@@ -3,25 +3,25 @@ if not L then return end
 if L then
 	L.absorb = "Absorbtion"
 	L.absorb_text = "%s (|cff%s%.0f%%|r)"
-	--L.bubble = "Bubble" -- Custody of the Deep Bubble
+	L.bubble = "Blase" -- Custody of the Deep Bubble
 	L.cast = "Zauber"
 	L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
 end
 
 L = BigWigs:NewBossLocale("Uu'nat, Harbinger of the Void", "deDE")
 if L then
-	--L.custom_on_stop_timers = "Always show ability bars"
-	--L.custom_on_stop_timers_desc = "Uunat can delay some of his abilities. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "Fähigkeiten-Leisten immer anzeigen"
+	L.custom_on_stop_timers_desc = "Uunat kann einige seiner Fähigkeiten verzögern. Durch Aktivieren dieser Option werden die Leisten dieser Fähigkeiten weiterhin angezeigt."
 
 	L.absorb = "Absorbtion"
 	L.absorb_text = "%s (|cff%s%.0f%%|r)"
-	--L.bubble = "Bubble" -- Custody of the Deep Bubble
+	L.bubble = "Blase" -- Custody of the Deep Bubble
 	L.cast = "Zauber"
 	L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
 
-	--L.void = "Void", -- Unstable Resonance: Void
-	--L.ocean = "Ocean", -- Unstable Resonance: Ocean
-	--L.storm = "Storm", -- Unstable Resonance: Storm
-	--L.custom_on_repeating_resonance_say = "Repeating Unstable Resonance Say"
-	--L.custom_on_repeating_resonance_say_desc = "Spam the icons {rt3}{rt5}{rt6} (Void, Ocean and Storm) in say chat to be avoided during Unstable Resonance."
+	L.void = "Leere", -- Unstable Resonance: Void
+	L.ocean = "Ozean", -- Unstable Resonance: Ocean
+	L.storm = "Sturm", -- Unstable Resonance: Storm
+	L.custom_on_repeating_resonance_say = "Instabile Resonanz-Ansage wiederholen"
+	L.custom_on_repeating_resonance_say_desc = "Wiederholt die Symbole {rt3}{rt5}{rt6} (Leere, Ozean und Sturm) im Chat, um diesen während Instabiler Resonanz auszuweichen."
 end
