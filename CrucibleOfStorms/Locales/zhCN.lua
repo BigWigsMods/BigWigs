62e7b08e6ca972e4623db50f368e97db4679a4ff
Adavak LEE <adavak@adavak.com>
2019-04-23 13:14:52 +0800

    update.

--- CrucibleOfStorms/Locales/zhCN.lua
+++ CrucibleOfStorms/Locales/zhCN.lua
@@ -3,27 +3,27 @@ if not L then return end
 if L then
 	L.absorb = "吸收"
 	L.absorb_text = "%s（|cff%s%.0f%%|r）"
-	--L.bubble = "Bubble" -- Custody of the Deep Bubble
+	L.bubble = "气泡" -- Custody of the Deep Bubble
 	L.cast = "施放"
 	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
 end
 
 L = BigWigs:NewBossLocale("Uu'nat, Harbinger of the Void", "zhCN")
 if L then
-	--L.custom_on_stop_timers = "Always show ability bars"
-	--L.custom_on_stop_timers_desc = "Uunat can delay some of his abilities. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "总是显示技能条"
+	L.custom_on_stop_timers_desc = "乌纳特某些技能可能延迟施放。 当此选项启用，这些技能条将保留在屏幕上。"
 
 	L.absorb = "吸收"
 	L.absorb_text = "%s（|cff%s%.0f%%|r）"
-	--L.bubble = "Bubble" -- Custody of the Deep Bubble
+	L.bubble = "气泡" -- Custody of the Deep Bubble
 	L.cast = "施放"
 	L.cast_text = "%.1f秒（|cff%s%.0f%%|r）"
 
-	--L.void = "Void" -- Unstable Resonance: Void
-	--L.ocean = "Ocean" -- Unstable Resonance: Ocean
-	--L.storm = "Storm" -- Unstable Resonance: Storm
-	--L.custom_on_repeating_resonance_say = "Repeating Unstable Resonance Say"
-	--L.custom_on_repeating_resonance_say_desc = "Spam the icons {rt3}{rt5}{rt6} (Void, Ocean and Storm) in say chat to be avoided during Unstable Resonance."
+	L.void = "虚空" -- Unstable Resonance: Void
+	L.ocean = "海洋" -- Unstable Resonance: Ocean
+	L.storm = "风暴" -- Unstable Resonance: Storm
+	L.custom_on_repeating_resonance_say = "重复动荡共鸣喊话"
+	L.custom_on_repeating_resonance_say_desc = "在聊天中重复喊话 {rt3}{rt5}{rt6}（虚空、海洋和风暴）图标，以在动荡共鸣期间避免爆炸。"
 end
 
 L = BigWigs:NewBossLocale("Uu'nat, Harbinger of the Void", "zhCN")
