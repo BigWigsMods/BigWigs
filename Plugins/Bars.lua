701c65957d843e475ea97981df9b13baa6bc54e5
EK <eke00372@gmail.com>
2019-04-26 14:12:56 +0800

    add new bar style: Aurora

--- Plugins/Bars.lua
+++ Plugins/Bars.lua
@@ -171,6 +171,107 @@ do
 end
 
 do
+	-- Aurora style (classic version), half height bar and shadow border
+
+	local backdropBorder = {
+		bgFile = "Interface\\Buttons\\WHITE8X8",
+		edgeFile = "Interface\\AddOns\\BigWigs\\Media\\Textures\\glow",
+		tile = false, tileSize = 0, edgeSize = 3,
+		insets = {left = 3, right = 3, top = 3, bottom = 3}
+	}
+
+	local function removeStyle(bar)
+		local cbb = bar.candyBarBar
+
+		bar.candyBarBackdrop:Hide()
+		local height = bar:Get("bigwigs:restoreheight")
+		if height then
+			bar:SetHeight(height)
+		end
+
+		local tex = bar:Get("bigwigs:restoreicon")
+		if tex then
+			bar:SetIcon(tex)
+			bar:Set("bigwigs:restoreicon", nil)
+			bar.candyBarIconFrameBackdrop:Hide()
+		end
+		
+		local timer = bar.candyBarDuration
+		timer:ClearAllPoints()
+		timer:SetPoint("TOPLEFT", cbb, "TOPLEFT", 2, 0)
+		timer:SetPoint("BOTTOMRIGHT", cbb, "BOTTOMRIGHT", -2, 0)
+	
+		local label = bar.candyBarLabel
+		label:ClearAllPoints()
+		label:SetPoint("TOPLEFT", cbb, "TOPLEFT", 2, 0)
+		label:SetPoint("BOTTOMRIGHT", cbb, "BOTTOMRIGHT", -2, 0)
+	end
+
+	local function styleBar(bar)
+		local cbb = bar.candyBarBar
+		
+		local height = bar:GetHeight()
+		bar:Set("bigwigs:restoreheight", height)
+		bar:SetHeight(height/2)		
+
+		local bd = bar.candyBarBackdrop
+		bd:SetBackdrop(backdropBorder)
+		bd:SetBackdropColor(.15, .15, .15, .4)
+		bd:SetBackdropBorderColor(0, 0, 0, 1)
+		bd:ClearAllPoints()
+		bd:SetPoint("TOPLEFT", bar, "TOPLEFT", -3, 3)
+		bd:SetPoint("BOTTOMRIGHT", bar, "BOTTOMRIGHT", 3, -3)
+		bd:Show()
+
+		local tex = bar:GetIcon()
+		if tex then
+			local icon = bar.candyBarIconFrame
+			bar:SetIcon(nil)			
+			icon:SetTexture(tex)
+			icon:Show()
+			if bar.iconPosition == "RIGHT" then
+				icon:SetPoint("BOTTOMLEFT", bar, "BOTTOMRIGHT", 5, 0)
+			else
+				icon:SetPoint("BOTTOMRIGHT", bar, "BOTTOMLEFT", -5, 0)
+			end
+			icon:SetSize(height+2, height+2)
+			bar:Set("bigwigs:restoreicon", tex)
+
+			local iconBd = bar.candyBarIconFrameBackdrop
+			iconBd:SetBackdrop(backdropBorder)
+			iconBd:SetBackdropColor(.15, .15, .15, .4)
+			iconBd:SetBackdropBorderColor(0, 0, 0, 1)
+			iconBd:ClearAllPoints()
+			iconBd:SetPoint("TOPLEFT", icon, "TOPLEFT", -3, 3)
+			iconBd:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 3, -3)
+			iconBd:Show()
+		end
+		
+		local label = bar.candyBarLabel		
+		label:SetShadowOffset(0, 0)
+		label:ClearAllPoints()
+		label:SetPoint("BOTTOMLEFT", cbb, "TOPLEFT", 2, -height/4+2)		
+		
+		local timer = bar.candyBarDuration
+		timer:SetShadowOffset(0, 0)
+		timer:ClearAllPoints()
+		timer:SetPoint("BOTTOMRIGHT", cbb, "TOPRIGHT", -2, -height/4+2)
+	end
+
+	barStyles.Aurora = {
+		apiVersion = 1,
+		version = 10,
+		barHeight = 20,
+		barSpacing = 20,
+		fontSizeNormal = 14,
+		fontSizeEmphasized = 14,
+		ApplyStyle = styleBar,
+		BarStopped = removeStyle,
+		GetStyleName = function() return "Aurora" end,
+	}
+end
+
+do
 	-- MonoUI
 	local backdropBorder = {
 		bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
