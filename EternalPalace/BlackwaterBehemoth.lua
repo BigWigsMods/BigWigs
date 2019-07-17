71490f88eb3abff9f960c3b198627ee18e3f1576
funkydude <funkydude@users.noreply.github.com>
2019-07-17 16:00:48 +0100

    EternalPalace/BlackwaterBehemoth: Fix Piercing Barb timer

--- EternalPalace/BlackwaterBehemoth.lua
+++ EternalPalace/BlackwaterBehemoth.lua
@@ -50,7 +50,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "ToxicSpine", 292159)
 	self:Log("SPELL_AURA_APPLIED", "ToxicSpineApplied", 292167)
 	self:Log("SPELL_CAST_START", "ShockPulse", 292270)
-	self:Log("SPELL_CAST_SUCCESS", "PiercingBarb", 301477)
+	self:Log("SPELL_CAST_SUCCESS", "PiercingBarb", 301494)
 	self:Log("SPELL_AURA_APPLIED", "PiercingBarbApplied", 301494)
 
 	self:Log("SPELL_AURA_APPLIED", "SlipstreamApplied", 301180)
@@ -65,7 +65,7 @@ function mod:OnEngage()
 	self:Bar(292270, 23) -- Shock Pulse
 
 	if self:Mythic() then
-		self:Bar(301494, 11) -- Piercing Barb
+		self:Bar(301494, 13) -- Piercing Barb
 	end
 
 	intermissionTime = GetTime() + 100
@@ -190,7 +190,7 @@ function mod:Interupted(args)
 		self:Bar(292270, 23) -- Shock Pulse
 
 		if self:Mythic() then
-			self:Bar(301494, 11) -- Piercing Barb
+			self:Bar(301494, 13) -- Piercing Barb
 		end
 
 		if stage < 3 then -- Does not move again at stage 3
