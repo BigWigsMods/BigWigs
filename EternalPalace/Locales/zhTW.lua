9c449d96067bbeba14ca81e2d38e8cfe102c2577
BlueNightSky <afan1128@hotmail.com>
2019-08-14 12:25:16 +0800

    Update zhTW.lua

--- EternalPalace/Locales/zhTW.lua
+++ EternalPalace/Locales/zhTW.lua
@@ -1,7 +1,7 @@
 local L = BigWigs:NewBossLocale("Za'qul, Herald of Ny'alotha", "zhTW")
 if not L then return end
 if L then
-	--L.stage3_early = "Za'qul tears open the pathway to Delirium Realm!"  -- Yell is 14.5s before the actual cast start
+	L.stage3_early = "札奎爾撕開了一條通往狂亂領域的通道！"  -- Yell is 14.5s before the actual cast start
 end
 
 L = BigWigs:NewBossLocale("Lady Ashvane", "zhTW")
@@ -11,15 +11,15 @@ end
 
 L = BigWigs:NewBossLocale("Queen Azshara", "zhTW")
 if L then
-	--L[299249] = "%s (Soak Orbs)"
-	--L[299251] = "%s (Avoid Orbs)"
-	--L[299254] = "%s (Hug Others)"
-	--L[299255] = "%s (Avoid Everyone)"
-	--L[299252] = "%s (Keep Moving)"
-	--L[299253] = "%s (Stand Still)"
-	--L.hulk_killed = "%s killed - %.1f sec"
-	--L.fails_message = "%s (%d Sanction stack fails)"
-	--L.reversal = "Reversal"
-	--L.greater_reversal = "Reversal (Greater)"
-	--L.you_die = "You die"
+	L[299249] = "%s (吸收球)"
+	L[299251] = "%s (避開球)"
+	L[299254] = "%s (蹭隊友)"
+	L[299255] = "%s (單獨散開)"
+	L[299252] = "%s (保持移動)"
+	L[299253] = "%s (不要動)"
+	L.hulk_killed = "%s 已擊殺 - %.1f 秒"
+	L.fails_message = "%s (%d 制裁堆疊失敗)"
+	L.reversal = "反轉"
+	L.greater_reversal = "反轉 (強力)"
+	L.you_die = "你死定了"
 end
