1e94b46c07cc70227750a226796b431d16d2646c
funkydude <funkydude@users.noreply.github.com>
2019-11-06 16:54:49 +0000

    Plugins/Proximity: Disable checking range on items that don't exist in classic, closes #724

--- Plugins/Proximity.lua
+++ Plugins/Proximity.lua
@@ -96,21 +96,21 @@ do
 		end
 
 		local items	= {
-			[5] = 37727, -- Ruby Acorn
-			[8] = 63427, -- Worgsaw
-			[11] = 33278, -- Burning Torch
-			[13] = 32321, -- Sparrowhawk Net
-			[18] = 133940, -- Silkweave Bandage
+			--[5] = 37727, -- Ruby Acorn
+			--[8] = 63427, -- Worgsaw
+			--[11] = 33278, -- Burning Torch
+			--[13] = 32321, -- Sparrowhawk Net
+			[18] = 14530, -- Heavy Runecloth Bandage
 			[23] = 21519, -- Mistletoe
-			[28] = 31463, -- Zezzak's Shard
-			[33] = 34191, -- Handful of Snowflakes
+			--[28] = 31463, -- Zezzak's Shard
+			--[33] = 34191, -- Handful of Snowflakes
 			[38] = 18904, -- Zorbin's Ultra-Shrinker
-			[43] = 34471, -- Vial of the Sunwell
-			[48] = 32698, -- Wrangling Rope
-			[53] = 116139, -- Haunting Memento
-			[63] = 32825, -- Soul Cannon
-			[73] = 41265, -- Eyesore Blaster
-			[83] = 35278, -- Reinforced Net
+			--[43] = 34471, -- Vial of the Sunwell
+			--[48] = 32698, -- Wrangling Rope
+			--[53] = 116139, -- Haunting Memento
+			--[63] = 32825, -- Soul Cannon
+			--[73] = 41265, -- Eyesore Blaster
+			--[83] = 35278, -- Reinforced Net
 		}
 		for range, item in next, items do
 			ranges[range] = function(unit)
