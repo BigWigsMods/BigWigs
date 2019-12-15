d03a687fd87aeb23d05bc88b64287f187debfaf1
Justwait <justwait@method.gg>
2019-12-15 14:20:46 +0100

    Nyalotha/Skitra: Module improvements

--- Nyalotha/Skitra.lua
+++ Nyalotha/Skitra.lua
@@ -19,6 +19,7 @@ mod.engageId = 2334
 --
 
 local phsycheCount = 1
+local nextStageWarning = 83
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -26,6 +27,7 @@ local phsycheCount = 1
 
 function mod:GetOptions()
 	return {
+		"stages",
 		{308059, "TANK"}, -- Shadow Shock
 		{307950, "SAY", "SAY_COUNTDOWN", "FLASH"}, -- Shred Psyche
 		309687, -- Psychic Outburst
@@ -45,20 +47,34 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "ImagesofAbsolution", 313239)
 
 	self:Log("SPELL_CAST_START", "IllusionaryProjection", 307725)
+	self:Log("SPELL_CAST_SUCCESS", "IllusionaryProjectionSuccess", 307725)
 	self:Log("SPELL_AURA_APPLIED", "IllusionaryProjectionDebuffsApplied", 307784, 307785) -- Clouded Mind, Twisted Mind
-	self:Log("SPELL_AURA_APPLIED", "IllusionaryProjectionDebuffsRemoved", 307784, 307785) -- Clouded Mind, Twisted Mind
 end
 
 function mod:OnEngage()
 	phsycheCount = 1
+	nextStageWarning = 83
 
 	self:Bar(307950, 12.5, CL.count:format(self:SpellName(307950), phsycheCount)) -- Shred Psyche
 	self:Bar(313239, 31.5) -- Images of Absolution
+
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "boss1")
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:UNIT_HEALTH_FREQUENT(event, unit)
+	local hp = UnitHealth(unit) / UnitHealthMax(unit) * 100
+	if hp < nextStageWarning then
+		self:Message2("stages", "green", CL.soon:format(self:SpellName(307725)), false)
+		nextStageWarning = nextStageWarning - 20
+		if nextStageWarning < 20 then
+			self:UnregisterUnitEvent(event, unit)
+		end
+	end
+end
 
 function mod:ShadowShockApplied(args)
 	local amount = args.amount or 1
@@ -118,16 +134,24 @@ function mod:IllusionaryProjection(args)
 	self:StopBar(313239) -- Images of Absolution
 end
 
+function mod:IllusionaryProjectionSuccess()
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+end
+
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	local guid = UnitGUID("boss1")
+	if guid then -- Boss 1 can only be Skitra, so the boss has returned
+		self:UnregisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+		self:Message2(307725, "green", CL.over:format(self:SpellName(307725))) -- Illusionary Projection
+		self:PlaySound(307725, "long")
+
+		self:Bar(307950, 15, CL.count:format(self:SpellName(307950), phsycheCount)) -- Shred Psyche
+		self:Bar(313239, 32.5) -- Images of Absolution
+	end
+end
+
 function mod:IllusionaryProjectionDebuffsApplied(args)
 	if self:Me(args.destGUID) then
 		self:PersonalMessage(307725, nil, args.spellName, args.spellId)
 	end
-end
-
-function mod:IllusionaryProjectionDebuffsRemoved(args)
-	self:Message2(307725, "green", CL.over:format(self:SpellName(307725))) -- Illusionary Projection
-	self:PlaySound(307725, "long")
-
-	self:Bar(307950, 15, CL.count:format(self:SpellName(307950), phsycheCount)) -- Shred Psyche
-	self:Bar(313239, 32.5) -- Images of Absolution
 end
