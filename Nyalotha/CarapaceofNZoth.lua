dfd27f05ada801076b86c3d23636f5845cc56256
elvador <reysptools@googlemail.com>
2020-01-23 00:07:24 +0100

    Nyalotha/CarapaceofNZoth: Fix phase messages, timer updates, add Infinite Darkness

--- Nyalotha/CarapaceofNZoth.lua
+++ Nyalotha/CarapaceofNZoth.lua
@@ -20,7 +20,14 @@ mod.engageId = 2337
 -- Locals
 --
 
+-- To handle the "half" stages properly we just count up our stage variable:
+--   Blizz stage | variable value
+--        1      |       1
+--        2      |       2
+--       2.5     |       3
+--   ...
 local stage = 1
+local lastSynthesisMsg = 0
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -47,6 +54,7 @@ function mod:GetOptions()
 		--[[  Stage Three: Locus of Infinite Truths ]]--
 		{306984, "SAY", "SAY_COUNTDOWN", "PROXIMITY"}, -- Insanity Bomb
 		-21069, -- Thrashing Tentacle
+		313039, -- Infinite Darkness
 
 		--[[  General ]]--
 		"stages",
@@ -65,7 +73,7 @@ function mod:OnBossEnable()
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
 
 	--[[  Stage One: Exterior Carapace ]]--
-	self:Log("SPELL_CAST_SUCCESS", "MadnessBomb", 306973)
+	self:Log("SPELL_CAST_SUCCESS", "MadnessBomb", 306971)
 	self:Log("SPELL_AURA_APPLIED", "MadnessBombApplied", 306973)
 	self:Log("SPELL_AURA_REMOVED", "MadnessBombRemoved", 306973)
 	self:Log("SPELL_CAST_SUCCESS", "AdaptiveMembrane", 306988)
@@ -84,15 +92,16 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "InsanityBomb", 306986)
 	self:Log("SPELL_AURA_APPLIED", "InsanityBombApplied", 306984)
 	self:Log("SPELL_AURA_REMOVED", "InsanityBombRemoved", 306984)
+	self:Log("SPELL_CAST_START", "InfiniteDarkness", 313039)
 end
 
 function mod:OnEngage()
 	stage = 1
-	self:Berserk(721) -- To be confirmed, PTR heroic
-	self:Bar(306973, 9.5) -- Madness Bomb
-	self:CDBar(-20565, 14, nil, "INV_EyeofNzothPet") -- Gaze of Madness
-	self:Bar(306988, 24) -- Adaptive Membrane
-	self:Bar(-20560, 41.3, nil, "INV_MISC_MONSTERHORN_04") -- Growth-Covered Tentacle
+	--self:Berserk(721) -- To be confirmed, PTR heroic
+	self:Bar(306973, 5) -- Madness Bomb
+	self:CDBar(-20565, 10, nil, "INV_EyeofNzothPet") -- Gaze of Madness
+	self:Bar(306988, 16) -- Adaptive Membrane
+	self:Bar(-20560, 30, nil, "INV_MISC_MONSTERHORN_04") -- Growth-Covered Tentacle
 end
 
 --------------------------------------------------------------------------------
@@ -103,20 +112,21 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, msg)
 	if msg:find("INV_MISC_MONSTERHORN_04", nil, true) then -- Growth-Covered Tentacle
 		self:Message2(-20560, "red", nil, "INV_MISC_MONSTERHORN_04")
 		self:PlaySound(-20560, "info")
-		self:Bar(-20560, 61, nil, "INV_MISC_MONSTERHORN_04")
+		self:Bar(-20560, 60, nil, "INV_MISC_MONSTERHORN_04")
 	elseif msg:find("INV_EyeofNzothPet", nil, true) then -- Gaze of Madness
 		self:Message2(-20565, "red", nil, "INV_EyeofNzothPet")
 		self:PlaySound(-20565, "alert")
-		self:Bar(-20565, 61, nil, "INV_EyeofNzothPet")
+		self:Bar(-20565, 58, nil, "INV_EyeofNzothPet")
 	end
 end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
-	if spellId == 45313 and stage == 2 then -- Good old Anchor Here
-		stage = 3
-		self:Message2("stages", "cyan", CL.stage:format(stage), false)
+	if spellId == 45313 and stage == 3 then -- Blizz Stage 3 start (good old Anchor Here)
+		stage = 4
+		self:Message2("stages", "cyan", CL.stage:format(3), false)
 		self:PlaySound("stages", "long")
 		self:Bar(-20565, 22.2, nil, 315673) -- Thrashing Tentacle
+		self:Bar(313039, 54) -- Infinite Darkness
 	elseif spellId == 315673 then -- Thrashing Tentacle
 		self:Message2(-21069, "red", nil, 315673)
 		self:PlaySound(-21069, "alert")
@@ -126,20 +136,25 @@ end
 
 --[[  Stage One: Exterior Carapace ]]--
 function mod:MadnessBomb(args)
-	self:Bar(args.spellId, stage == 1 and 26.6 or stage == 2 and 33.3 or 22.2)
+	self:Bar(306973, stage == 1 and 26.6 or stage == 2 and 33.3 or 22.2)
+	if stage == 2 then -- on NPCs only in stage 2, so no TargetsMessage needed
+		self:Message2(args.spellId, "yellow")
+	end
 end
 
 do
 	local playerList = mod:NewTargetList()
 	function mod:MadnessBombApplied(args)
-		playerList[#playerList+1] = args.destName
+		if UnitIsPlayer(args.destName) then
+			playerList[#playerList+1] = args.destName
+			self:TargetsMessage(args.spellId, "yellow", playerList)
+		end
 		if self:Me(args.destGUID) then
 			self:PlaySound(args.spellId, "warning")
 			self:Say(args.spellId)
 			self:SayCountdown(args.spellId, 12)
 			self:OpenProximity(args.spellId, 10)
 		end
-		self:TargetsMessage(args.spellId, "yellow", playerList)
 	end
 end
 
@@ -152,12 +167,12 @@ end
 function mod:AdaptiveMembrane(args)
 	self:Message2(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
-	self:Bar(args.spellId, stage == 1 and 27.7 or stage == 2 and 21.2 or 33.3)
+	self:Bar(args.spellId, stage == 1 and 28.8 or stage == 2 and 21.2 or stage == 3 and 33.3 or 31.8)
 end
 
 function mod:MandibleSlam(args)
 	self:Message2(args.spellId, "orange", CL.casting:format(args.spellName))
-	self:Bar(args.spellId, stage == 1 and 16.7 or 22.2)
+	self:Bar(args.spellId, stage == 1 and 13 or stage == 3 and 22.2 or 11)
 end
 
 function mod:BlackScar(args)
@@ -179,7 +194,8 @@ end
 function mod:Synthesis(args)
 	if stage == 1 then
 			stage = 2
-			self:Message2("stages", "cyan", CL.stage:format(stage), false)
+			lastSynthesisMsg = 100 -- Random large number
+			self:Message2("stages", "cyan", CL.stage:format(2), false)
 			self:PlaySound("stages", "long")
 			self:StopBar(-20565) -- Gaze of Madness
 			self:StopBar(-20560) -- Growth-Covered Tentacle
@@ -192,11 +208,13 @@ end
 function mod:SynthesisRemoved(args)
 	local amount = args.amount or 0
 	if amount < 1 then
+		stage = 3
 		self:Message2("stages", "green", CL.over:format(args.spellName), args.spellId)
 		self:PlaySound("stages", "info")
 		self:CDBar(307092, 4) -- Occipital Blast, mostly a guess
 		self:CDBar(315947, 15.5) -- Mandible Slam
-	elseif amount % 3 == 0 or amount < 4 then
+	elseif amount % 3 == 0 or lastSynthesisMsg-amount > 3 or amount < 4 then
+		lastSynthesisMsg = amount -- Events can be skipped, so this is our fallback
 		self:StackMessage(args.spellId, args.destName, amount, "green")
 	end
 end
@@ -210,7 +228,7 @@ end
 --[[  Stage Three: Locus of Infinite Truths ]]--
 function mod:InsanityBomb(args)
 	self:Message2(306984, "yellow")
-	self:Bar(306984, 49)
+	self:Bar(306984, 67)
 end
 
 function mod:InsanityBombApplied(args)
@@ -224,6 +242,12 @@ end
 
 function mod:InsanityBombRemoved(args)
 	if self:Me(args.destGUID) then
-		self:CloseProximity()
+		self:CloseProximity(args.spellId)
 	end
+end
+
+function mod:InfiniteDarkness(args)
+	self:Message2(args.spellId, "red", CL.incoming:format(args.spellName))
+	self:CastBar(args.spellId, 2.5)
+	self:Bar(args.spellId, 54)
 end
