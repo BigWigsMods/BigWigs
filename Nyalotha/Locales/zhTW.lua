f8f850ec06ecb5a35bf21bc77b461eef91818e1a
Justwait <justwait@method.gg>
2020-02-11 00:17:26 +0100

    Nyalotha/Locales: Add locale files

---
+++ Nyalotha/Locales/zhTW.lua
@@ -0,0 +1,60 @@
+local L = BigWigs:NewBossLocale("Maut", "zhTW")
+if not L then return end
+if L then
+	-- L.stage2_over = "Stage 2 Over - %.1f sec"
+end
+
+L = BigWigs:NewBossLocale("Shad'har the Insatiable", "zhTW")
+if L then
+	-- L.custom_on_stop_timers = "Always show ability bars"
+	-- L.custom_on_stop_timers_desc = "Shad'har randomizes which off-cooldown ability she uses next. When this option is enabled, the bars for those abilities will stay on your screen."
+end
+
+L = BigWigs:NewBossLocale("Drest'agath", "zhTW")
+if L then
+	-- L.adds_desc = "Warnings and Messages for the Eye, Tentacle and Maw of Drest'agath."
+	-- L.adds_icon = "achievement_nzothraid_drestagath"
+
+	-- L.eye_killed = "Eye Killed!"
+	-- L.tentacle_killed = "Tentacle Killed!"
+	-- L.maw_killed = "Maw Killed!"
+end
+
+L = BigWigs:NewBossLocale("Il'gynoth, Corruption Reborn", "zhTW")
+if L then
+	-- L.custom_on_fixate_plates = "Fixate icon on Enemy Nameplate"
+	-- L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+end
+
+L = BigWigs:NewBossLocale("Vexiona", "zhTW")
+if L then
+	-- L.killed = "%s killed"
+end
+
+L = BigWigs:NewBossLocale("Ra-den the Despoiled", "zhTW")
+if L then
+	-- L.essences = "Essences"
+	-- L.essences_desc = "Ra-den periodically draws essences from other realms. Essences that reach Ra-den empower him with energy of that type."
+end
+
+L = BigWigs:NewBossLocale("Carapace of N'Zoth", "zhTW")
+if L then
+	-- L.player_membrane = "Player Membrane" -- In stage 3
+	-- L.boss_membrane = "Boss Membrane" -- In stage 3
+end
+
+L = BigWigs:NewBossLocale("N'Zoth, the Corruptor", "zhTW")
+if L then
+	-- L.realm_switch = "Realm Switched" -- When you leave the Mind of N'zoth
+
+	-- L.custom_on_repeating_paranoia_say = "Repeating Paranoia Say"
+	-- L.custom_on_repeating_paranoia_say_desc = "Spam a say message in chat to be avoided while you have paranoia."
+	-- L.custom_on_repeating_paranoia_say_icon = 315927
+
+	-- L.gateway_yell = "Warning: Chamber of Heart compromised. Hostile forces inbound." -- Yelled by MOTHER to trigger mythic only stage
+	-- L.gateway_open = "Gateway Open!"
+
+	-- L.laser_left = "Lasers (Left)"
+	-- L.laser_right = "Lasers (Right)"
+end
+end
