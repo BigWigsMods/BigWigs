b47cb43beefb1257373c46ac4006e1dbc90a9294
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2020-02-11 07:36:15 +0100

    Update deDE.lua

--- Nyalotha/Locales/deDE.lua
+++ Nyalotha/Locales/deDE.lua
@@ -1,59 +1,59 @@
 local L = BigWigs:NewBossLocale("Maut", "deDE")
 if not L then return end
 if L then
-	-- L.stage2_over = "Stage 2 Over - %.1f sec"
+	L.stage2_over = "Phase 2 vorbei - %.1f Sek"
 end
 
 L = BigWigs:NewBossLocale("Shad'har the Insatiable", "deDE")
 if L then
-	-- L.custom_on_stop_timers = "Always show ability bars"
-	-- L.custom_on_stop_timers_desc = "Shad'har randomizes which off-cooldown ability she uses next. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "Fähigkeitenleisten immer anzeigen"
+	L.custom_on_stop_timers_desc = "Shad'har wählt die nächste Off-Colldown-Fähigkeit zufällig aus. Wenn diese Option ausgewählt ist, werden die Leisten für diese Fähigkeiten auf dem Bildschirm angezeigt bleiben."
 end
 
 L = BigWigs:NewBossLocale("Drest'agath", "deDE")
 if L then
-	-- L.adds_desc = "Warnings and Messages for the Eye, Tentacle and Maw of Drest'agath."
+	L.adds_desc = "Warnungen und Nachrichten für Augen, Tentakel und Schlunde von Drest'agath."
 	-- L.adds_icon = "achievement_nzothraid_drestagath"
 
-	-- L.eye_killed = "Eye Killed!"
-	-- L.tentacle_killed = "Tentacle Killed!"
-	-- L.maw_killed = "Maw Killed!"
+	L.eye_killed = "Auge getötet!"
+	L.tentacle_killed = "Tentakel getötet!"
+	L.maw_killed = "Schlund getötet!"
 end
 
 L = BigWigs:NewBossLocale("Il'gynoth, Corruption Reborn", "deDE")
 if L then
-	-- L.custom_on_fixate_plates = "Fixate icon on Enemy Nameplate"
-	-- L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+	L.custom_on_fixate_plates = "Fixieren-Symbol an gegnerischen Namensplaketten"
+	L.custom_on_fixate_plates_desc = "Zeigt ein Symbol an der Namensplakette des Dich fixierenden Ziels an.\nBenötigt die Verwendung gegnerischer Namensplaketten. Diese Funktion wird derzeit nur von KuiNameplates unterstützt."
 end
 
 L = BigWigs:NewBossLocale("Vexiona", "deDE")
 if L then
-	-- L.killed = "%s killed"
+	L.killed = "%s getötet"
 end
 
 L = BigWigs:NewBossLocale("Ra-den the Despoiled", "deDE")
 if L then
-	-- L.essences = "Essences"
-	-- L.essences_desc = "Ra-den periodically draws essences from other realms. Essences that reach Ra-den empower him with energy of that type."
+	L.essences = "Essenzen"
+	L.essences_desc = "Ra-den beschwört regelmäßig Essenzen aus anderen Reichen. Wenn diese Essenzen Ra-den erreichen verstärken sie ihn entsprechend."
 end
 
 L = BigWigs:NewBossLocale("Carapace of N'Zoth", "deDE")
 if L then
-	-- L.player_membrane = "Player Membrane" -- In stage 3
-	-- L.boss_membrane = "Boss Membrane" -- In stage 3
+	L.player_membrane = "Spieler Membran" -- In stage 3
+	L.boss_membrane = "Boss Membran" -- In stage 3
 end
 
 L = BigWigs:NewBossLocale("N'Zoth, the Corruptor", "deDE")
 if L then
-	-- L.realm_switch = "Realm Switched" -- When you leave the Mind of N'zoth
+	L.realm_switch = "Reich gewechselt" -- When you leave the Mind of N'zoth
 
-	-- L.custom_on_repeating_paranoia_say = "Repeating Paranoia Say"
-	-- L.custom_on_repeating_paranoia_say_desc = "Spam a say message in chat to be avoided while you have paranoia."
+	L.custom_on_repeating_paranoia_say = "Wiederholte Paranoia Ansage"
+	L.custom_on_repeating_paranoia_say_desc = "Spammt eine Nachricht im Sagen-Chat, damit anderen Dich während Paranoia meiden."
 	-- L.custom_on_repeating_paranoia_say_icon = 315927
 
 	-- L.gateway_yell = "Warning: Chamber of Heart compromised. Hostile forces inbound." -- Yelled by MOTHER to trigger mythic only stage
-	-- L.gateway_open = "Gateway Open!"
+	L.gateway_open = "Tor geöffnet!"
 
-	-- L.laser_left = "Lasers (Left)"
-	-- L.laser_right = "Lasers (Right)"
+	L.laser_left = "Laser (Links)"
+	L.laser_right = "Laser (Rechts)"
 end
