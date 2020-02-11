b0747c0ad4e3019998de214ace0c5dee76eebbe7
EK <eke00372@gmail.com>
2020-02-11 18:27:52 +0800

    Update zhTW.lua

--- Nyalotha/Locales/zhTW.lua
+++ Nyalotha/Locales/zhTW.lua
@@ -1,40 +1,40 @@
 local L = BigWigs:NewBossLocale("Maut", "zhTW")
 if not L then return end
 if L then
-	-- L.stage2_over = "Stage 2 Over - %.1f sec"
+	L.stage2_over = "第二階段結束：%.1f秒"
 end
 
 L = BigWigs:NewBossLocale("Shad'har the Insatiable", "zhTW")
 if L then
-	-- L.custom_on_stop_timers = "Always show ability bars"
-	-- L.custom_on_stop_timers_desc = "Shad'har randomizes which off-cooldown ability she uses next. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "總是顯示計時器"
+	L.custom_on_stop_timers_desc = "夏德哈會在下一次施放技能時隨機施放已經冷卻完畢的技能。啟用此選項後，這些技能的計時條會保持顯示。"
 end
 
 L = BigWigs:NewBossLocale("Drest'agath", "zhTW")
 if L then
-	-- L.adds_desc = "Warnings and Messages for the Eye, Tentacle and Maw of Drest'agath."
+	L.adds_desc = "替卓雷阿葛斯的肢體顯示警告和訊息。"
 	-- L.adds_icon = "achievement_nzothraid_drestagath"
 
-	-- L.eye_killed = "Eye Killed!"
-	-- L.tentacle_killed = "Tentacle Killed!"
-	-- L.maw_killed = "Maw Killed!"
+	L.eye_killed = "卓雷阿葛斯之眼擊殺！"
+	L.tentacle_killed = "卓雷阿葛斯觸手擊殺！"
+	L.maw_killed = "卓雷阿葛斯之口擊殺！"
 end
 
 L = BigWigs:NewBossLocale("Il'gynoth, Corruption Reborn", "zhTW")
 if L then
-	-- L.custom_on_fixate_plates = "Fixate icon on Enemy Nameplate"
-	-- L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
+	L.custom_on_fixate_plates = "在敵方名條顯示鎖定圖示"
+	L.custom_on_fixate_plates_desc = "當你被鎖定時，在敵方名條上顯示一個圖示。\n需要啟用敵方名條，此功能目前只有KuiNameplates支援。"
 end
 
 L = BigWigs:NewBossLocale("Vexiona", "zhTW")
 if L then
-	-- L.killed = "%s killed"
+	L.killed = "%s擊殺"
 end
 
 L = BigWigs:NewBossLocale("Ra-den the Despoiled", "zhTW")
 if L then
-	-- L.essences = "Essences"
-	-- L.essences_desc = "Ra-den periodically draws essences from other realms. Essences that reach Ra-den empower him with energy of that type."
+	L.essences = "精華"
+	L.essences_desc = "萊公會周期性地從其他領域汲取精華，不同的精華會賦予萊公不同的能量強化。"
 end
 
 L = BigWigs:NewBossLocale("Carapace of N'Zoth", "zhTW")
@@ -45,15 +45,15 @@ end
 
 L = BigWigs:NewBossLocale("N'Zoth, the Corruptor", "zhTW")
 if L then
-	-- L.realm_switch = "Realm Switched" -- When you leave the Mind of N'zoth
+	L.realm_switch = "領域轉換" -- When you leave the Mind of N'zoth
 
-	-- L.custom_on_repeating_paranoia_say = "Repeating Paranoia Say"
-	-- L.custom_on_repeating_paranoia_say_desc = "Spam a say message in chat to be avoided while you have paranoia."
+	L.custom_on_repeating_paranoia_say = "重覆妄想喊話"
+	L.custom_on_repeating_paranoia_say_desc = "當你中了妄念時每秒重覆喊話，避免你的搭擋以外的人靠近你。"
 	-- L.custom_on_repeating_paranoia_say_icon = 315927
 
 	-- L.gateway_yell = "Warning: Chamber of Heart compromised. Hostile forces inbound." -- Yelled by MOTHER to trigger mythic only stage
-	-- L.gateway_open = "Gateway Open!"
+	-L.gateway_open = "傳送門開啟！"
 
-	-- L.laser_left = "Lasers (Left)"
-	-- L.laser_right = "Lasers (Right)"
+	L.laser_left = "左轉射線"
+	L.laser_right = "右轉射線"
 end
