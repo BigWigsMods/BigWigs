a0bdad6737b1045c90856251d7a97bf88c5affda
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2020-03-09 01:42:09 +0100

    Update deDE (#778)

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -9,8 +9,8 @@ L.gitHubDesc = "BigWigs ist Open-Source-Software auf GitHub. Wir sind immer auf 
 
 L.testNameplate = "Ziel erkannt, erstelle Test Leiste über Namensplakette des Ziels."
 
---L.classicWarning1 = "|cFF33FF99BigWigs|r: You're using the wrong version of BigWigs on a classic server."
---L.classicWarning2 = "|cFF33FF99BigWigs|r: Please install the classic version of BigWigs."
+L.classicWarning1 = "|cFF33FF99BigWigs|r: Du nutzt die falsche Version von BigWigs auf einem Classic Server."
+L.classicWarning2 = "|cFF33FF99BigWigs|r: Bitte Classic Version von BigWigs installieren."
 
 L.options = "Optionen"
 L.raidBosses = "Schlachtzugsbosse"
