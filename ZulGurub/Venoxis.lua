8f8c9a4cf8a23d59afe00097e5b1f690c65605cc
Kyle Buller <bullerk@gmail.com>
2020-06-17 23:46:17 -0500

    ZulGurub/Venoxis: Add Poison Cloud pre-warning

--- ZulGurub/Venoxis.lua
+++ ZulGurub/Venoxis.lua
@@ -40,6 +40,8 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "HolyFire", 23860)
 	self:Log("SPELL_INTERRUPT", "HolyFireStop", "*")
 	self:Log("SPELL_CAST_SUCCESS", "PoisonCloud", 23861)
+
+	self:RegisterUnitEvent("UNIT_HEALTH_FREQUENT", nil, "target", "focus")
 end
 
 --------------------------------------------------------------------------------
@@ -67,4 +69,16 @@ end
 function mod:PoisonCloud(args)
 	self:Message2(23861, "yellow")
 	self:PlaySound(23861, "info")
+end
+
+function mod:UNIT_HEALTH_FREQUENT(event, unit)
+	if self:MobId(UnitGUID(unit)) == 14507 then
+		local hp = UnitHealth(unit)
+		if hp < 56 then
+			self:UnregisterUnitEvent(event, "target", "focus")
+			if hp > 50 then -- make sure we're not too late
+				self:Message2(23861, "green", CL.soon:format(self:SpellName(23861)), false)
+			end
+		end
+	end
 end
