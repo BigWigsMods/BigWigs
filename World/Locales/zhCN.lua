355de4c0a7e3a66e6811faa9f924478d64c95726
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2020-06-28 10:36:42 +0800

    Update zhCN (#814)

--- World/Locales/zhCN.lua
+++ World/Locales/zhCN.lua
@@ -13,7 +13,7 @@ L = BigWigs:NewBossLocale("Lord Kazzak", "zhCN")
 if L then
 	L.bossName = "卡扎克"
 
-	L.engage_trigger = "为了燃烧军团！为了基尔加丹！"
+	L.engage_trigger = "为了军团！为了基尔加丹！"
 
 	L.supreme_mode = "无敌模式"
 end
@@ -22,26 +22,26 @@ local L = BigWigs:NewBossLocale("Emeriss", "zhCN")
 if L then
 	L.bossName = "艾莫莉丝"
 
-	-- L.engage_trigger = "Hope is a DISEASE of the soul! This land shall wither and die!"
+	L.engage_trigger = "希望是灵魂染上的疾病！这片土地应该枯竭，从此死气腾腾！"
 end
 
 local L = BigWigs:NewBossLocale("Lethon", "zhCN")
 if L then
 	L.bossName = "莱索恩"
 
-	-- L.engage_trigger = "I can sense the SHADOW on your hearts. There can be no rest for the wicked!"
+	L.engage_trigger = "我能感受到你内心的阴影。邪恶的侵蚀永远不会停止！"
 end
 
 local L = BigWigs:NewBossLocale("Taerar", "zhCN")
 if L then
 	L.bossName = "泰拉尔"
 
-	-- L.engage_trigger = "Peace is but a fleeting dream! Let the NIGHTMARE reign!"
+	L.engage_trigger = "和平不过是短暂的梦想！让梦魇统治整个世界吧！"
 end
 
 local L = BigWigs:NewBossLocale("Ysondre", "zhCN")
 if L then
 	L.bossName = "伊森德雷"
 
-	-- L.engage_trigger = "The strands of LIFE have been severed! The Dreamers must be avenged!"
+	L.engage_trigger = "生命的希冀已被切断！梦游者要展开报复！"
 end
