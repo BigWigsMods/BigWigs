3c2564afaf0bb17ee87b665dc445814e53f75049
Justwait <mngrootwow@gmail.com>
2020-08-03 22:54:25 +0200

    CastleNathria/Altimor: Default Spreadshot to an off option, warn faster for Sinseeker and minor cleanups

--- CastleNathria/Altimor.lua
+++ CastleNathria/Altimor.lua
@@ -25,14 +25,24 @@ mod.respawnTime = 30
 local stage = 1
 local sinseekerCount = 1
 local bloodyThrashCount = 1
-local ripSoulCount = 1 
+local ripSoulCount = 1
 local shadesOfBargastCount = 1
 local petrifyingHowlCount = 1
 
 --------------------------------------------------------------------------------
--- Initialization
+-- Localization
 --
 
+local L = mod:GetLocale()
+if L then
+	L.custom_off_spreadshot = 334404
+	L.custom_off_spreadshot_desc = 334404
+	L.custom_off_spreadshot_icon = "ability_hunter_runningshot"
+end
+
+--------------------------------------------------------------------------------
+-- Initialization
+--
 
 local sinseekerMarker = mod:AddMarkerOption(false, "player", 1, 335114, 1, 2, 3) -- Sin Seeker
 function mod:GetOptions()
@@ -41,7 +51,7 @@ function mod:GetOptions()
 		--[[ Huntsman Altimor ]]--
 		{335114, "SAY", "FLASH"}, -- Sinseeker
 		sinseekerMarker,
-		334404, -- Spreadshot
+		"custom_off_spreadshot",
 
 		--[[ Margore ]]--
 		{334971, "TANK"}, -- Jagged Claws
@@ -67,10 +77,10 @@ end
 
 function mod:OnBossEnable()
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
-	
+
 	--[[ Huntsman Altimor ]]--
 	self:Log("SPELL_CAST_START", "Sinseeker", 335114)
-	self:Log("SPELL_AURA_APPLIED", "HuntsmansMarkApplied", 335111, 335112, 335113)
+	self:Log("SPELL_AURA_APPLIED", "HuntsmansMarkApplied", 335111, 335112, 335113) -- Targets 1 -> 2 -> 3
 	self:Log("SPELL_AURA_APPLIED", "HuntsmansMarkRemoved", 335111, 335112, 335113)
 	self:Log("SPELL_CAST_START", "Spreadshot", 334404)
 
@@ -89,7 +99,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED_DOSE", "DevourSoul", 334884)
 	self:Log("SPELL_CAST_START", "ShadesOfBargast", 334757)
 	-- self:Log("SPELL_CAST_START", "DeathlyRoar", 334708)
-	self:Death("ShadeOfBargastDeath", 171557)
+	-- self:Death("ShadeOfBargastDeath", 171557)
 	self:Death("BargastDeath", 169457)
 
 	--[[ Hecutis ]]--
@@ -108,11 +118,11 @@ function mod:OnEngage()
 	stage = 1
 	sinseekerCount = 1
 	bloodyThrashCount = 1
-	ripSoulCount = 1 
+	ripSoulCount = 1
 	shadesOfBargastCount = 1
 	petrifyingHowlCount = 1
 
-	self:Bar(334404, 6.2) -- Spreadshot
+	self:Bar("custom_off_spreadshot", 6.2, self:SpellName(334404), 334404) -- Spreadshot
 	self:Bar(334971, 11.2) -- Jagged Claws
 	self:Bar(334945, 18, CL.count:format(self:SpellName(334945), bloodyThrashCount)) -- Bloody Thrash
 	self:Bar(335114, 51, CL.count:format(self:SpellName(335114), sinseekerCount)) -- Sinseeker
@@ -128,14 +138,14 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(_, unit, _, spellId)
 		if self:MobId(sourceGUID) == 165066 then -- Huntsman Altimor
 			stage = stage + 1
 			if stage == 2 then -- Bargast up
-				ripSoulCount = 1 
+				ripSoulCount = 1
 				shadesOfBargastCount = 1
-			
+
 				self:Bar(334797, 9.5, CL.count:format(self:SpellName(334797), ripSoulCount)) -- Rip Soul
 				self:Bar(334757, 17.5, CL.count:format(self:SpellName(334757), shadesOfBargastCount)) -- Shades Of Bargast
 			elseif stage == 3 then -- Hecutis up
 				petrifyingHowlCount = 1
-				
+
 				self:Bar(334852, 16.2, CL.count:format(self:SpellName(334852), petrifyingHowlCount)) -- Petrifying Howl
 			end
 		end
@@ -145,8 +155,7 @@ end
 --[[ Huntsman Altimor ]]--
 
 function mod:Sinseeker(args)
-	-- self:Message2(args.spellId, "red", CL.count:format(args.spellName, sinseekerCount)) -- XXX Do we want to warn faster, without targets?
-	-- self:PlaySound(args.spellId, "alert")
+	self:Message2(args.spellId, "orange", CL.casting:format(CL.count:format(args.spellName, sinseekerCount)))
 	sinseekerCount = sinseekerCount + 1
 	self:Bar(args.spellId, 51, CL.count:format(args.spellName, sinseekerCount))
 end
@@ -154,16 +163,16 @@ end
 do
 	local playerList, playerIcons = mod:NewTargetList(), {}
 	function mod:HuntsmansMarkApplied(args)
-		local playerIconsCount = #playerIcons+1
-		playerList[#playerList+1] = args.destName
-		playerIcons[playerIconsCount] = playerIconsCount
+		local count = #playerList+1
+		playerList[count] = args.destName
+		playerIcons[count] = count
 		if self:Me(args.destGUID) then
-			self:Say(335114, CL.count_rticon:format(self:SpellName(335114), playerIconsCount, playerIconsCount))
+			self:Say(335114, CL.count_rticon:format(self:SpellName(335114), count, count))
 			self:PlaySound(335114, "warning")
 			self:Flash(335114)
 		end
 		if self:GetOption(sinseekerMarker) then
-			SetRaidTarget(335114, playerIconsCount)
+			SetRaidTarget(335114, count)
 		end
 		self:TargetsMessage(335114, "orange", playerList, 3, CL.count:format(self:SpellName(335114), sinseekerCount-1), nil, 2, playerIcons) -- Debuffs are very delayed
 	end
@@ -176,11 +185,10 @@ function mod:HuntsmansMarkRemoved(args)
 end
 
 function mod:Spreadshot(args)
-	self:Message2(args.spellId, "cyan")
-	self:PlaySound(args.spellId, "info")
-	self:Bar(args.spellId, 12.2)
+	self:Message2("custom_off_spreadshot", "yellow", args.spellName, args.spellId)
+	self:PlaySound("custom_off_spreadshot", "alert")
+	self:Bar("custom_off_spreadshot", 12.2, args.spellName, args.spellId)
 end
-
 
 --[[ Margore ]]--
 
@@ -219,10 +227,10 @@ end
 
 function mod:MargoreDeath(args)
 	self:Message2("stages", "cyan", CL.killed:format(self:SpellName(-22312)), false) -- Margore
-	
+
 	self:StopBar(334971) -- Jagged Claws
 	self:StopBar(CL.count:format(self:SpellName(334945), bloodyThrashCount)) -- Bloody Thrash
-	
+
 	self:Bar("stages", 6, -22311, 334797) -- Bargast, Rip Soul icon
 end
 
@@ -257,29 +265,29 @@ function mod:ShadesOfBargast(args)
 end
 
 -- do
-	-- local prev = 0
-	-- function mod:DeathlyRoar(args)
-		-- local t = args.time
-		-- if t-prev > 10 then
-			-- prev = t
-			-- self:Message2(args.spellId, "red")
-			-- self:PlaySound(args.spellId, "info")
-			-- self:CastBar(args.spellId, 6)
-			-- self:Bar(args.spellId, 36.5)
-		-- end
-	-- end
+-- 	local prev = 0
+-- 	function mod:DeathlyRoar(args)
+-- 		local t = args.time
+-- 		if t-prev > 10 then
+-- 			prev = t
+-- 			self:Message2(args.spellId, "red")
+-- 			self:PlaySound(args.spellId, "info")
+-- 			self:CastBar(args.spellId, 6)
+-- 			self:Bar(args.spellId, 36.5)
+-- 		end
+-- 	end
 -- end
 
-function mod:ShadeOfBargastDeath(args)
-	self:StopBar(CL.cast:format(self:SpellName(334708)))
-end
+-- function mod:ShadeOfBargastDeath(args)
+-- 	self:StopBar(CL.cast:format(self:SpellName(334708)))
+-- end
 
 function mod:BargastDeath(args)
 	self:Message2("stages", "cyan", CL.killed:format(self:SpellName(-22311)), false) -- Bargast
-	
+
 	self:StopBar(CL.count:format(self:SpellName(334797), ripSoulCount)) -- Rip Soul
 	self:StopBar(CL.count:format(self:SpellName(334757), shadesOfBargastCount)) -- Shades Of Bargast
-	
+
 	self:Bar("stages", 6, -22310, 334852) -- Hecutis, Petrifying Howl icon
 end
 
@@ -302,7 +310,7 @@ do
 	local playerList = mod:NewTargetList()
 	function mod:PetrifyingHowlApplied(args)
 		playerList[#playerList+1] = args.destName
-		if #playerList == 1 then 
+		if #playerList == 1 then
 		end
 		if self:Me(args.destGUID) then
 			self:PlaySound(args.spellId, "warning")
@@ -317,7 +325,7 @@ end
 
 function mod:BargastDeath(args)
 	self:Message2("stages", "cyan", CL.killed:format(self:SpellName(-22311)), false) -- Bargast
-	
+
 	self:StopBar(CL.count:format(self:SpellName(334852), petrifyingHowlCount)) -- Petrifying Howl
 end
 
