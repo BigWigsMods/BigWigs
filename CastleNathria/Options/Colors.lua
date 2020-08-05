da2dfe138ff44de012c28e9824ed18589b2db8bf
Justwait <mngrootwow@gmail.com>
2020-08-05 18:56:56 +0200

    CastleNathria/Options: Update options files

--- CastleNathria/Options/Colors.lua
+++ CastleNathria/Options/Colors.lua
@@ -1,12 +1,52 @@
 
+BigWigs:AddColors("Shriekwing", {
+	[328857] = "purple",
+	[328897] = {"blue","green"},
+	[328921] = "green",
+	[329362] = "cyan",
+	[330711] = "red",
+	[336233] = {"blue","yellow"},
+	[336345] = "yellow",
+	[340047] = "orange",
+	[340322] = "red",
+	[340324] = "blue",
+	["stages"] = "green",
+})
+
+BigWigs:AddColors("Huntsman Altimor", {
+	[334757] = "green",
+	[334797] = {"green","red"},
+	[334852] = {"blue","orange"},
+	[334860] = {"blue","purple"},
+	[334884] = "orange",
+	[334893] = "blue",
+	[334945] = {"blue","orange"},
+	[334971] = {"blue","purple"},
+	[335114] = {"blue","orange"},
+	["custom_off_spreadshot"] = "yellow",
+	["stages"] = "cyan",
+})
+
 BigWigs:AddColors("Hungering Destroyer", {
 	[329298] = {"blue","yellow"},
-	[329455] = "purple",
-	[329758] = "red",
-	[329774] = "yellow",
+	[329455] = "yellow",
+	[329725] = "orange",
+	[329774] = "purple",
 	[332295] = {"blue","purple"},
-	[334266] = "orange",
+	[334266] = {"blue","orange"},
 	[334522] = "orange",
+})
+
+BigWigs:AddColors("Artificer Xy'mox", {
+	[325236] = {"blue","purple"},
+	[325399] = "yellow",
+	[326271] = "red",
+	[327414] = {"blue","red"},
+	[327887] = "cyan",
+	[328437] = {"blue","yellow"},
+	[328789] = "orange",
+	[329256] = "orange",
+	[329770] = "cyan",
 })
 
 BigWigs:AddColors("Sun King's Salvation", {
@@ -40,6 +80,23 @@ BigWigs:AddColors("Lady Inerva Darkvein", {
 	[338750] = "cyan",
 })
 
+BigWigs:AddColors("The Council of Blood", {
+	[327497] = "orange",
+	[327619] = "yellow",
+	[327773] = {"blue","purple"},
+	[328334] = {"blue","orange"},
+	[330848] = "red",
+	[330964] = "cyan",
+	[330965] = "cyan",
+	[330967] = "blue",
+	[330978] = "cyan",
+	[331637] = {"blue","red"},
+	[331706] = {"blue","red"},
+	[334948] = "red",
+	[335776] = "yellow",
+	[337110] = "yellow",
+})
+
 BigWigs:AddColors("Sludgefist", {
 	[331209] = {"blue","yellow"},
 	[331314] = "red",
@@ -49,4 +106,41 @@ BigWigs:AddColors("Sludgefist", {
 	[335293] = {"blue","yellow"},
 	[335361] = {"blue","cyan"},
 	[335470] = {"blue","yellow"},
+})
+
+BigWigs:AddColors("Stone Legion Generals", {
+	[329636] = "cyan",
+	[329808] = "cyan",
+	[333387] = "red",
+	[334498] = "orange",
+	[334541] = {"blue","orange"},
+	[334765] = {"blue","orange"},
+	[334929] = "yellow",
+	[337643] = "blue",
+	[337741] = "yellow",
+	[339164] = {"blue","red"},
+	[339690] = "purple",
+	[339728] = "yellow",
+})
+
+BigWigs:AddColors("Sire Denathrius", {
+	[326699] = {"blue","green","red"},
+	[326707] = "purple",
+	[326851] = "blue",
+	[327122] = "orange",
+	[327796] = {"blue","orange"},
+	[327992] = "blue",
+	[328936] = "cyan",
+	[329181] = {"blue","purple","yellow"},
+	[329951] = {"blue","orange"},
+	[330137] = "red",
+	[332585] = {"blue","purple"},
+	[332619] = "orange",
+	[332794] = {"blue","orange"},
+	[332937] = "orange",
+	[333111] = "red",
+	[333932] = "orange",
+	[333979] = "yellow",
+	[335873] = "blue",
+	["stages"] = "green",
 })
--- CastleNathria/Options/Sounds.lua
+++ CastleNathria/Options/Sounds.lua
@@ -1,12 +1,49 @@
 
+BigWigs:AddSounds("Shriekwing", {
+	[328857] = "info",
+	[328897] = {"alert","info"},
+	[328921] = "long",
+	[329362] = "info",
+	[330711] = "long",
+	[336233] = "warning",
+	[336345] = "alert",
+	[340047] = "warning",
+	[340324] = "alarm",
+	["stages"] = "info",
+})
+
+BigWigs:AddSounds("Huntsman Altimor", {
+	[334757] = "long",
+	[334797] = {"alert","info"},
+	[334852] = "warning",
+	[334860] = "info",
+	[334884] = "alarm",
+	[334893] = "alarm",
+	[334945] = "warning",
+	[334971] = "warning",
+	[335114] = "warning",
+	["custom_off_spreadshot"] = "alert",
+})
+
 BigWigs:AddSounds("Hungering Destroyer", {
 	[329298] = "alarm",
-	[329455] = "alarm",
-	[329758] = "warning",
+	[329455] = "alert",
+	[329725] = "warning",
 	[329774] = "alert",
 	[332295] = "alert",
 	[334266] = "warning",
 	[334522] = "long",
+})
+
+BigWigs:AddSounds("Artificer Xy'mox", {
+	[325236] = "warning",
+	[325399] = "alert",
+	[326271] = "alert",
+	[327887] = "long",
+	[328437] = "warning",
+	[328789] = "alarm",
+	[329256] = "alarm",
+	[329770] = "long",
 })
 
 BigWigs:AddSounds("Sun King's Salvation", {
@@ -40,6 +77,23 @@ BigWigs:AddSounds("Lady Inerva Darkvein", {
 	[338750] = "long",
 })
 
+BigWigs:AddSounds("The Council of Blood", {
+	[327497] = "alarm",
+	[327619] = "long",
+	[327773] = "info",
+	[328334] = "warning",
+	[330848] = "info",
+	[330964] = "info",
+	[330965] = "info",
+	[330967] = "warning",
+	[330978] = "info",
+	[331637] = "warning",
+	[331706] = "long",
+	[334948] = "long",
+	[335776] = "long",
+	[337110] = "alert",
+})
+
 BigWigs:AddSounds("Sludgefist", {
 	[331209] = "warning",
 	[331314] = "info",
@@ -49,4 +103,41 @@ BigWigs:AddSounds("Sludgefist", {
 	[335293] = {"info","warning"},
 	[335361] = {"alarm","long"},
 	[335470] = "warning",
+})
+
+BigWigs:AddSounds("Stone Legion Generals", {
+	[329636] = "long",
+	[329808] = "long",
+	[333387] = "alert",
+	[334498] = "long",
+	[334541] = "alarm",
+	[334765] = "alarm",
+	[334929] = "info",
+	[337643] = "alarm",
+	[337741] = "alert",
+	[339164] = "warning",
+	[339690] = "long",
+	[339728] = "info",
+})
+
+BigWigs:AddSounds("Sire Denathrius", {
+	[326699] = {"alarm","info","warning"},
+	[326707] = "alert",
+	[326851] = "alarm",
+	[327122] = "alert",
+	[327796] = "warning",
+	[327992] = "alarm",
+	[328936] = "info",
+	[329181] = {"alert","warning"},
+	[329951] = "warning",
+	[330137] = "alarm",
+	[332585] = "alert",
+	[332619] = "alert",
+	[332794] = "warning",
+	[332937] = "alert",
+	[333111] = "alarm",
+	[333932] = "warning",
+	[333979] = "alert",
+	[335873] = "alarm",
+	["stages"] = "long",
 })
