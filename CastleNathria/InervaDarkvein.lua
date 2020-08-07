d93af78ed905900a7fcaf86a4811ccc4a66e7c3a
elvador <reysptools@googlemail.com>
2020-08-07 16:29:32 +0200

    CastleNathria/InervaDarkvein: Add experimental Anima tracking

--- CastleNathria/InervaDarkvein.lua
+++ CastleNathria/InervaDarkvein.lua
@@ -16,6 +16,7 @@ mod.engageId = 2406
 local cognitionOnMe = nil
 local bottleTimers = {28.5, 36, 20, 24}
 local bottleCount = 1
+local anima = {}
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -24,6 +25,16 @@ local bottleCount = 1
 local L = mod:GetLocale()
 if L then
 	L.times = "%dx %s"
+
+	L.level = "%s (Level |cffffff00%d|r)"
+	L.full = "%s (|cffff0000FULL|r)"
+
+	L.custom_off_experimental = "Enable experimental features"
+	L.custom_off_experimental_desc = "These features are |cffff0000not tested|r and could |cffff0000spam|r."
+
+	L.anima_tracking = "Anima Tracking |cffff0000(Experimental)|r"
+	L.anima_tracking_desc = "Messages and Bars to track anima levels in the containers.|n|cffaaff00Tip: You might want to disable the information box or bars, depending your preference."
+	L.anima_tracking_icon = "achievement_boss_darkanimus"
 end
 
 --------------------------------------------------------------------------------
@@ -32,6 +43,8 @@ end
 
 function mod:GetOptions()
 	return {
+		"custom_off_experimental",
+		{"anima_tracking", "INFOBOX"},
 		338750, -- Enable Container
 		325379, -- Expose Desires
 		325936, -- Shared Cognition
@@ -44,7 +57,7 @@ function mod:GetOptions()
 		{331573, "ME_ONLY"}, -- Unconscionable Guilt
 		334017, -- Condemn
 	}, {
-		[325379] = "general",
+		[338750] = "general",
 		[331573] = -22293,
 		[331550] = -22295,
 	}
@@ -68,20 +81,110 @@ function mod:OnBossEnable()
 	self:Log("SPELL_PERIODIC_DAMAGE", "GroundDamage", 325713, 325718)
 	self:Log("SPELL_PERIODIC_MISSED", "GroundDamage", 325713, 325718)
 	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
+	self:RegisterEvent("UPDATE_UI_WIDGET", "WIDGET") -- need to fix BossPrototype implementation before using mod:RegisterWidgetEvent
 end
 
 function mod:OnEngage()
 	cognitionOnMe = nil
 	bottleCount = 1
+	wipe(anima)
+
 	self:Bar(325064, 18) -- Sins and Suffering
 	self:Bar(325005, 23) -- Shared Suffering
 	self:Bar(325769, bottleTimers[bottleCount]) -- Bottled Anima
 	self:Bar(332664, 56) -- Concentrate Anima
+
+	if self:GetOption("custom_off_experimental") then
+		self:OpenInfo("anima_tracking", L.anima_tracking)
+	end
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+do
+
+	local widgetIds = {
+		[2380] = {pos = 1, name = "Desires", icon = 325379}, -- Expose Desires
+		[2399] = {pos = 2, name = "Bottles", icon = 325769}, -- Bottled Anima
+		[2400] = {pos = 3, name = "Sins", icon = 325064}, -- Sins and Suffering
+		[2401] = {pos = 4, name = CL.add, icon = 332664}, -- Concentrate Anima
+	}
+
+	local getStatusBarInfo = C_UIWidgetManager.GetStatusBarWidgetVisualizationInfo
+
+	function mod:UpdateAnimaInfoBox(pos, text, time)
+		self:SetInfo("anima_tracking", pos*2-1, text)
+		if time > 60 then
+			self:SetInfo("anima_tracking", pos*2, ("%d:%02d"):format(time/60, time%60))
+		elseif time > 0 then
+			self:SetInfo("anima_tracking", pos*2, ("%d"):format(time))
+		else
+			self:SetInfo("anima_tracking", pos*2, "")
+		end
+	end
+
+	local soundPrev = 0
+
+	function mod:WIDGET(tbl)
+		if not self:GetOption("custom_off_experimental") then return end
+
+		local spell = widgetIds[tbl.widgetID]
+		if not spell then return end
+
+		local info = getStatusBarInfo(tbl.widgetID)
+		if not info or not info.barValue then return end
+
+		local oldInfo = anima[spell.icon]
+
+		local curLevel = math.floor(info.barValue / (info.barMax / 3)) + 1
+		if not oldInfo then -- first time we're seeing it
+			anima[spell.icon] = {value = info.barValue, level = curLevel, lastUpdate = GetTime()}
+		else
+			if curLevel ~= oldInfo.level then
+				if curLevel > 3 then -- it's full
+					self:Message2("anima_tracking", "cyan", L.full:format(spell.name), spell.icon)
+					self:StopBar(L.full:format(spell.name))
+				else
+					self:Message2("anima_tracking", "cyan", L.level:format(spell.name, curLevel), spell.icon)
+					self:StopBar(L.level:format(spell.name, oldInfo.level))
+				end
+				local t = GetTime()
+
+				if t-soundPrev > 10 then -- high threshold in case something is really wrong
+					soundPrev = t
+					self:PlaySound("anima_tracking", "info")
+				end
+			end
+
+			local timeDiff = (GetTime() - oldInfo.lastUpdate)
+			local gain = (info.barValue - oldInfo.value) / timeDiff
+			if gain > 0 then
+				local nextLevelAtAnima = (info.barMax / 3) * curLevel
+				local animaToNext = nextLevelAtAnima - info.barValue
+				local t = math.ceil(animaToNext / gain)
+				if animaToNext > 0 then -- safety
+					local text = curLevel > 2 and L.full:format(spell.name) or L.level:format(spell.name, curLevel + 1)
+
+					if math.abs(t - self:BarTimeLeft(text)) > 1 then -- avoid restarting the bar on small differences
+						self:Bar("anima_tracking", t, text, spell.icon)
+					end
+					self:UpdateAnimaInfoBox(spell.pos, text, animaToNext / gain)
+				end
+			elseif gain < 0 then
+				self:StopBar(L.level:format(spell.name, 2)) -- Stopping all for now, just in case
+				self:StopBar(L.level:format(spell.name, 3))
+				self:StopBar(L.full:format(spell.name))
+				self:UpdateAnimaInfoBox(spell.pos, "", 0)
+			end
+
+			anima[spell.icon].value = info.barValue
+			anima[spell.icon].level = curLevel
+			anima[spell.icon].lastUpdate = GetTime()
+		end
+	end
+end
 
 do
 	local vialCount = 0
