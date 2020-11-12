d3e11ee4f3235462c788aaf3b3efc80284828a3a
Kyle Buller <bullerk@gmail.com>
2020-11-12 07:07:37 -0600

    Update option files

---
+++ Naxxramas/Options/Colors.lua
@@ -0,0 +1,98 @@
+
+BigWigs:AddColors("Patchwerk", {
+	[28131] = "orange",
+})
+
+BigWigs:AddColors("Grobbulus", {
+	[28169] = "blue",
+	[28240] = "yellow",
+})
+
+BigWigs:AddColors("Gluth", {
+	[28371] = "red",
+	[29685] = "red",
+	["berserk"] = "yellow",
+	["decimate"] = {"orange","yellow"},
+})
+
+BigWigs:AddColors("Thaddius", {
+	[28089] = {"blue","orange","red","yellow"},
+	[28134] = "red",
+	["stages"] = "cyan",
+	["throw"] = "orange",
+})
+
+BigWigs:AddColors("Noth the Plaguebringer", {
+	[29213] = {"orange","red"},
+	["blink"] = {"red","yellow"},
+	["teleport"] = {"orange","red"},
+	["wave"] = "orange",
+})
+
+BigWigs:AddColors("Heigan the Unclean", {
+	["engage"] = "red",
+	["teleport"] = {"orange","red","yellow"},
+})
+
+BigWigs:AddColors("Loatheb", {
+	[29185] = "green",
+	[29204] = {"orange","red","yellow"},
+	[29234] = "red",
+	[29865] = "yellow",
+	[30281] = "green",
+})
+
+BigWigs:AddColors("Anub'Rekhan", {
+	[28783] = "orange",
+	[28785] = {"red","yellow"},
+})
+
+BigWigs:AddColors("Grand Widow Faerlina", {
+	[28732] = {"green","orange"},
+	[28794] = "blue",
+	[28798] = {"green","orange","red","yellow"},
+})
+
+BigWigs:AddColors("Maexxna", {
+	[28622] = {"blue","red"},
+	[28747] = {"orange","red"},
+	[28776] = {"blue","purple"},
+	[29484] = {"red","yellow"},
+})
+
+BigWigs:AddColors("Instructor Razuvious", {
+	[29060] = {"green","yellow"},
+	[29061] = {"green","orange"},
+	["shout"] = {"orange","red"},
+})
+
+BigWigs:AddColors("Gothik the Harvester", {
+	["add"] = {"orange","red","yellow"},
+	["adddeath"] = "red",
+	["room"] = {"orange","red","yellow"},
+})
+
+BigWigs:AddColors("The Four Horsemen", {
+	[28863] = "orange",
+	[28883] = "yellow",
+	[28884] = "red",
+	[29061] = {"green","yellow"},
+	["mark"] = {"red","yellow"},
+	["stages"] = "green",
+})
+
+BigWigs:AddColors("Sapphiron", {
+	[28522] = {"blue","yellow"},
+	[28542] = "yellow",
+	["deepbreath"] = "red",
+	["stages"] = "cyan",
+})
+
+BigWigs:AddColors("Kel'Thuzad", {
+	[27808] = {"blue","red","yellow"},
+	[27810] = "red",
+	[27819] = {"blue","yellow"},
+	[28410] = {"blue","orange","red"},
+	["guardians"] = "red",
+	["stages"] = {"cyan","yellow"},
+})
---
+++ Naxxramas/Options/Sounds.lua
@@ -0,0 +1,51 @@
+
+BigWigs:AddSounds("Patchwerk", {
+	[28131] = "alarm",
+})
+
+BigWigs:AddSounds("Grobbulus", {
+	[28169] = "alert",
+})
+
+BigWigs:AddSounds("Gluth", {
+	["decimate"] = {"alarm","alert"},
+})
+
+BigWigs:AddSounds("Thaddius", {
+	[28089] = "alert",
+})
+
+BigWigs:AddSounds("Noth the Plaguebringer", {
+	[29213] = "alarm",
+})
+
+BigWigs:AddSounds("Loatheb", {
+	[29185] = "info",
+})
+
+BigWigs:AddSounds("Grand Widow Faerlina", {
+	[28794] = "alarm",
+})
+
+BigWigs:AddSounds("Maexxna", {
+	[28622] = "alert",
+	[28747] = "alarm",
+	[28776] = "info",
+})
+
+BigWigs:AddSounds("Instructor Razuvious", {
+	["shout"] = "Alert",
+})
+
+BigWigs:AddSounds("Sapphiron", {
+	[28542] = "alert",
+	["deepbreath"] = "alarm",
+	["stages"] = "info",
+})
+
+BigWigs:AddSounds("Kel'Thuzad", {
+	[27808] = "alert",
+	[27819] = "alert",
+	[28410] = "alert",
+	["stages"] = "info",
+})
