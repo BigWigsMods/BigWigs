bb759b13c05bdf32ffeb98aab4920f6655dbd3d2
Brayan Henao <bryanhenao96@gmail.com>
2020-11-18 22:15:20 -0500

    Added Core esMX locale file

--- Core/Locales/common.esMX.lua
+++ Core/Locales/common.esMX.lua
@@ -2,85 +2,85 @@ local L = BigWigsAPI:NewLocale("BigWigs: Common", "esMX")
 if not L then return end
 
 -- Prototype.lua common words
-L.you = "%s on YOU"
-L.you_icon = "%s on |T13700%d:0|tYOU"
-L.underyou = "%s under YOU"
+L.you = "%s en TI"
+L.you_icon = "%s en |T13700%d:0|tTI"
+L.underyou = "%s debajo de TI"
 L.other = "%s: %s"
-L.onboss = "%s on BOSS"
-L.buff_boss = "Buff on BOSS: %s"
-L.buff_other = "Buff on %s: %s"
-L.on = "%s on %s"
-L.stack = "%dx %s on %s"
-L.stackyou = "%dx %s on YOU"
-L.cast = "<Cast: %s>"
-L.casting = "Casting %s"
-L.soon = "%s soon"
+L.onboss = "%s en JEFE"
+L.buff_boss = "Efecto en JEFE: %s"
+L.buff_other = "Efecto en %s: %s"
+L.on = "%s en %s"
+L.stack = "%dx %s en %s"
+L.stackyou = "%dx %s en TI"
+L.cast = "<Canaliza: %s>"
+L.casting = "Canalizando %s"
+L.soon = "%s pronto"
 L.count = "%s (%d)"
 L.count_icon = "%s (%d|T13700%d:0|t)"
 L.count_rticon = "%s (%d{rt%d})"
-L.near = "%s near YOU"
+L.near = "%s cerca de TI"
 
-L.phase = "Phase %d"
-L.stage = "Stage %d"
-L.normal = "Normal mode"
-L.heroic = "Heroic mode"
-L.mythic = "Mythic mode"
-L.active = "Active" -- When a boss becomes active, after speech finishes
+L.phase = "Fase %d"
+L.stage = "Etapa %d"
+L.normal = "Modo normal"
+L.heroic = "Modo heroico"
+L.mythic = "Modo mítico"
+L.active = "Activo" -- When a boss becomes active, after speech finishes
 L.general = "General" -- General settings, i.e. things that apply to normal, heroic and mythic mode.
 
-L.duration = "%s for %s sec"
-L.over = "%s Over"
-L.removed = "%s Removed"
-L.removed_from = "%s Removed From %s"
-L.removed_by = "%s removed by %s" -- spell removed by player
-L.incoming = "%s Incoming"
-L.interrupted = "%s Interrupted"
-L.interrupted_by = "%s interrupted by %s" -- spell interrupted by player
+L.duration = "%s durante %s seg"
+L.over = "%s terminado"
+L.removed = "%s Removido"
+L.removed_from = "%s Removido de %s"
+L.removed_by = "%s Removido por %s" -- spell removed by player
+L.incoming = "%s Acercándose"
+L.interrupted = "%s Interrumpido"
+L.interrupted_by = "%s Interrumpido por %s" -- spell interrupted by player
 L.no = "No %s"
-L.intermission = "Intermission"
+L.intermission = "Interrupción"
 L.percent = "%d%% - %s" -- 20% - spell
 
 -- Add related
-L.add_spawned = "Add Spawned"
-L.spawned = "%s Spawned"
-L.spawning = "%s Spawning"
-L.next_add = "Next Add"
-L.add_killed = "Add killed (%d/%d)"
-L.add_remaining = "Add killed, %d remaining"
-L.add = "Add"
-L.adds = "Adds"
-L.big_add = "Big Add"
-L.small_adds = "Small Adds"
+L.add_spawned = "Apareció Esbirro"
+L.spawned = "%s Apareció"
+L.spawning = "%s Aparece"
+L.next_add = "Siguiente Esbirro"
+L.add_killed = "Esbirro muerto (%d/%d)"
+L.add_remaining = "Esbirro muerto, %d restantes"
+L.add = "Esbirro"
+L.adds = "Esbirros"
+L.big_add = "Esbirro grande"
+L.small_adds = "Esbirros pequeños"
 
 -- Mob related
-L.mob_killed = "%s killed (%d/%d)"
-L.mob_remaining = "%s killed, %d remaining"
+L.mob_killed = "%s muerto (%d/%d)"
+L.mob_remaining = "%s muerto, %d restantes"
 
 -- Localizers note:
 -- The default mod:Berserk(600) uses spell ID 26662 to get the Berserk name
-L.custom_start = "%s engaged - %s in %d min"
-L.custom_start_s = "%s engaged - %s in %d sec"
-L.custom_end = "%s goes %s"
-L.custom_min = "%s in %d min"
-L.custom_sec = "%s in %d sec"
+L.custom_start = "%s iniciado - %s en %d min"
+L.custom_start_s = "%s iniciado - %s en %d seg"
+L.custom_end = "%s comienza en %s"
+L.custom_min = "%s en %d min"
+L.custom_sec = "%s en %d seg"
 
-L.focus_only = "|cffff0000Focus target alerts only.|r "
-L.trash = "Trash"
+L.focus_only = "|cffff0000Alertas solo para objetivo en foco.|r "
+L.trash = "Basura"
 
 -- Common raid marking locale
-L.marker = "%s Marker"
-L.marker_player_desc = "Mark players affected by %s with %s, requires promoted or leader."
-L.marker_npc_desc = "Mark %s with %s, requires promoted or leader."
+L.marker = "%s Marcador"
+L.marker_player_desc = "Marca jugadores afectados por %s con %s, requiere ayudante o líder."
+L.marker_npc_desc = "Marca %s con %s, requiere ayudante o líder."
 
 -- Ability where two players have to move close to each other
-L.link = "Linked with %s"
-L.link_short = "Linked: %s"
-L.link_both = "%s linked with %s"
-L.link_removed = "Link removed"
+L.link = "Enlazado con %s"
+L.link_short = "Enlazado: %s"
+L.link_both = "%s enlazado con %s"
+L.link_removed = "Enlace eliminado"
 
 -- Abbreviated numbers
-L.amount_one = "%dB" -- Billions 1,000,000,000
-L.amount_two = "%dM" -- Millions 1,000,000
-L.amount_three = "%dK" -- Thousands 1,000
-L.seconds = "%.1fs" -- 1.1 seconds
+L.amount_one = "%dB" -- Miles de millones 1,000,000,000
+L.amount_two = "%dM" -- Millones 1,000,000
+L.amount_three = "%dK" -- Miles 1,000
+L.seconds = "%.1fs" -- 1.1 segundos
 -
