BigWigsMods/BigWigs

commit 83e56e39d8b84f06f858427deaa298c7ef23624e

Add proper support for a custom version check for guild versions of BigWigs by editing GuildVersion.lua instead of messing with the loader.

Loader.lua
@@ -22,9 +22,13 @@ local ldbi = LibStub("LibDBIcon-1.0")
 
 local BIGWIGS_VERSION = 186
 local BIGWIGS_RELEASE_STRING, BIGWIGS_VERSION_STRING = "", ""
-local versionQueryString, versionResponseString = "Q^%d^%s", "V^%d^%s"
+local versionQueryString, versionResponseString = "Q^%d^%s^%d^%s", "V^%d^%s^%d^%s"
+local customGuildName = false
+local BIGWIGS_GUILD_VERSION = 0
 
 do
+	local _, tbl = ...
+
 	-- START: MAGIC PACKAGER VOODOO VERSION STUFF
 	local REPO = "REPO"
 	local ALPHA = "ALPHA"
@@ -53,9 +57,18 @@ do
 	end
 	BIGWIGS_RELEASE_STRING = releaseString
 	BIGWIGS_VERSION_STRING = ("%d-%s"):format(BIGWIGS_VERSION, myGitHash)
-	-- Format is "V:version-hash"
-	versionQueryString = versionQueryString:format(BIGWIGS_VERSION, myGitHash)
-	versionResponseString = versionResponseString:format(BIGWIGS_VERSION, myGitHash)
+	-- Format is "V:version^hash^guildVersion^guildName"
+	local isVersionNumber = type(tbl.guildVersion) == "number"
+	local isGuildString = type(tbl.guildName) == "string"
+	if isVersionNumber and isGuildString and tbl.guildVersion > 0 and tbl.guildName:gsub(" ", "") ~= "" then
+		customGuildName = tbl.guildName
+		BIGWIGS_GUILD_VERSION = tbl.guildVersion
+		versionQueryString = versionQueryString:format(BIGWIGS_VERSION, myGitHash, tbl.guildVersion, tbl.guildName)
+		versionResponseString = versionResponseString:format(BIGWIGS_VERSION, myGitHash, tbl.guildVersion, tbl.guildName)
+	else
+		versionQueryString = versionQueryString:format(BIGWIGS_VERSION, myGitHash, 0, "")
+		versionResponseString = versionResponseString:format(BIGWIGS_VERSION, myGitHash, 0, "")
+	end
 	-- END: MAGIC PACKAGER VOODOO VERSION STUFF
 end
 
@@ -81,8 +94,11 @@ public.CTimerNewTicker = CTimerNewTicker
 -- Version
 local usersHash = {}
 local usersVersion = {}
+local usersGuildVersion = {}
+local usersGuildName = {}
 local usersDBM = {}
 local highestFoundVersion = BIGWIGS_VERSION
+local highestFoundGuildVersion = BIGWIGS_GUILD_VERSION
 
 -- Loading
 local loadOnCoreEnabled = {} -- BigWigs modulepacks that should load when a hostile zone is entered or the core is manually enabled, this would be the default plugins Bars, Messages etc
@@ -1145,10 +1161,10 @@ function mod:CHAT_MSG_ADDON(prefix, msg, channel, sender)
 	if channel ~= "RAID" and channel ~= "PARTY" and channel ~= "INSTANCE_CHAT" then
 		return
 	elseif prefix == "BigWigs" then
-		local bwPrefix, bwMsg, extra = strsplit("^", msg)
+		local bwPrefix, bwMsg, extra, guildVersion, guildName = strsplit("^", msg)
 		sender = Ambiguate(sender, "none")
 		if bwPrefix == "V" or bwPrefix == "Q" then
-			self:VersionCheck(bwPrefix, bwMsg, extra, sender)
+			self:VersionCheck(bwPrefix, bwMsg, extra, guildVersion, guildName, sender)
 		elseif bwPrefix == "B" then
 			public:SendMessage("BigWigs_BossComm", bwMsg, extra, sender)
 		elseif bwPrefix == "P" then
@@ -1194,11 +1210,16 @@ do
 	end
 
 	local hasWarned = 0
+	local hasGuildWarned = false
 	local verTimer = nil
+	local verGuildTimer = nil
 	function ResetVersionWarning()
 		hasWarned = 0
+		hasGuildWarned = false
 		if verTimer then verTimer:Cancel() end -- We may have left the group whilst a warning is about to show
+		if verGuildTimer then verGuildTimer:Cancel() end
 		verTimer = nil
+		verGuildTimer = nil
 	end
 
 	local function printOutOfDate(tbl)
@@ -1221,12 +1242,16 @@ do
 				hasWarned = 3
 				verTimer = nil
 				local diff = highestFoundVersion - BIGWIGS_VERSION
-				local msg = L.warnSeveralReleases:format(diff)
-				sysprint(msg)
-				Popup(msg)
-				RaidNotice_AddMessage(RaidWarningFrame, msg, {r=1,g=1,b=1}, 40)
+				if not customGuildName then
+					local msg = L.warnSeveralReleases:format(diff)
+					sysprint(msg)
+					Popup(msg)
+					RaidNotice_AddMessage(RaidWarningFrame, msg, {r=1,g=1,b=1}, 40)
+				else
+					sysprint(L.warnCustom:format(diff))
+				end
 			end, 1)
-		elseif warnedReallyOutOfDate > 1 and hasWarned < 2 then
+		elseif warnedReallyOutOfDate > 1 and hasWarned < 2 and not customGuildName then
 			if verTimer then verTimer:Cancel() end
 			verTimer = CTimerNewTicker(3, function()
 				hasWarned = 2
@@ -1234,7 +1259,7 @@ do
 				sysprint(L.warnTwoReleases)
 				RaidNotice_AddMessage(RaidWarningFrame, L.warnTwoReleases, {r=1,g=1,b=1}, 20)
 			end, 1)
-		elseif warnedOutOfDate > 1 and hasWarned < 1 then
+		elseif warnedOutOfDate > 1 and hasWarned < 1 and not customGuildName then
 			if verTimer then verTimer:Cancel() end
 			verTimer = CTimerNewTicker(3, function()
 				hasWarned = 1
@@ -1244,7 +1269,26 @@ do
 		end
 	end
 
-	function mod:VersionCheck(prefix, verString, hash, sender)
+	local function printGuildOutOfDate(tbl)
+		if hasGuildWarned then return end
+		local warnedOutOfDate = 0
+		for k,v in next, tbl do
+			if v > BIGWIGS_GUILD_VERSION and usersGuildName[k] == customGuildName then
+				warnedOutOfDate = warnedOutOfDate + 1
+			end
+		end
+		if warnedOutOfDate > 1 and not hasGuildWarned then
+			if verGuildTimer then verGuildTimer:Cancel() end
+			verGuildTimer = CTimerNewTicker(3, function()
+				hasGuildWarned = true
+				verGuildTimer = nil
+				sysprint(tbl.guildWarn)
+				Popup(tbl.guildWarn)
+			end, 1)
+		end
+	end
+
+	function mod:VersionCheck(prefix, verString, hash, guildVerString, guildName, sender)
 		if prefix == "Q" then
 			if timer then timer:Cancel() end
 			timer = CTimerNewTicker(3, sendMsg, 1)
@@ -1255,6 +1299,21 @@ do
 				usersVersion[sender] = version
 				usersHash[sender] = hash
 				if version > highestFoundVersion then highestFoundVersion = version end
+
+				local guildVersion = tonumber(guildVerString)
+				if guildVersion and guildVersion > 0 then
+					usersGuildVersion[sender] = guildVersion
+					usersGuildName[sender] = guildName
+					if customGuildName and customGuildName == guildName then
+						if guildVersion > highestFoundGuildVersion then
+							highestFoundGuildVersion = guildVersion
+						end
+						if guildVersion > BIGWIGS_GUILD_VERSION then
+							printGuildOutOfDate(usersGuildVersion)
+						end
+					end
+				end
+
 				if version > BIGWIGS_VERSION then
 					printOutOfDate(usersVersion)
 				end
@@ -1448,11 +1507,13 @@ SlashCmdList.BigWigsVersion = function()
 		return
 	end
 
-	local function coloredNameVersion(name, version, hash)
+	local function coloredNameVersion(name, version, hash, guildVersion, guildName)
 		if not version then
 			version = ""
+		elseif customGuildName and customGuildName == guildName then
+			version = ("|cFFCCCCCC(%d/%d%s)|r"):format(guildVersion, version, hash and "-"..hash or "")
 		else
-			version = ("|cFFCCCCCC(%s%s)|r"):format(version, hash and "-"..hash or "")
+			version = ("|cFFCCCCCC(%d%s)|r"):format(version, hash and "-"..hash or "")
 		end
 
 		local _, class = UnitClass(name)
@@ -1478,21 +1539,21 @@ SlashCmdList.BigWigsVersion = function()
 	local good = {} -- highest release users
 	local ugly = {} -- old version users
 	local bad = {} -- no boss mod
-	local crazy = {} -- DBM users
+	local dbm = {} -- DBM users
 
 	for i = 1, #list do
 		local player = list[i]
 		local usesBossMod = nil
 		if usersVersion[player] then
 			if usersVersion[player] < highestFoundVersion then
-				ugly[#ugly + 1] = coloredNameVersion(player, usersVersion[player], usersHash[player])
+				ugly[#ugly + 1] = coloredNameVersion(player, usersVersion[player], usersHash[player], usersGuildVersion[player], usersGuildName[player])
 			else
-				good[#good + 1] = coloredNameVersion(player, usersVersion[player], usersHash[player])
+				good[#good + 1] = coloredNameVersion(player, usersVersion[player], usersHash[player], usersGuildVersion[player], usersGuildName[player])
 			end
 			usesBossMod = true
 		end
 		if usersDBM[player] then
-			crazy[#crazy+1] = coloredNameVersion(player, usersDBM[player])
+			dbm[#dbm+1] = coloredNameVersion(player, usersDBM[player])
 			usesBossMod = true
 		end
 		if not usesBossMod then
@@ -1502,7 +1563,7 @@ SlashCmdList.BigWigsVersion = function()
 
 	if #good > 0 then print(L.upToDate, unpack(good)) end
 	if #ugly > 0 then print(L.outOfDate, unpack(ugly)) end
-	if #crazy > 0 then print(L.dbmUsers, unpack(crazy)) end
+	if #dbm > 0 then print(L.dbmUsers, unpack(dbm)) end
 	if #bad > 0 then print(L.noBossMod, unpack(bad)) end
 end
 

BigWigs.toc
@@ -29,5 +29,6 @@ API.lua
 locales.xml
 
 Media\Media.lua
+GuildVersion.lua
 Loader.lua
 modules.xml

GuildVersion.lua
@@ -1,0 +1,6 @@
+
+local _, tbl = ...
+
+tbl.guildVersion = 0 -- Must use rounded values e.g. 1, 2, 3, you can always reset this after an expansion if high numbers triggers you
+tbl.guildName = "" -- Guild name, used as identification for your version. e.g. "coolguild" or "coolwigs" try to keep it unique
+tbl.guildWarn = "Your guild version of BigWigs is old" -- Message displayed for being 1 version out of date

Locales/deDE.lua
@@ -4,6 +4,7 @@ if not L then return end
 L.getNewRelease = "Dein BigWigs ist veraltet (/bwv), aber du kannst es mit Hilfe des CurseForge Clients einfach aktualisieren. Alternativ kannst du es auch von curseforge.com oder wowinterface.com herunterladen und manuell aktualisieren."
 L.warnTwoReleases = "Dein BigWigs ist 2 Versionen älter als die neueste Version! Deine Version könnte Fehler, fehlende Funktionen oder völlig falsche Timer beinhalten. Es wird dringend empfohlen, BigWigs zu aktualisieren."
 L.warnSeveralReleases = "|cffff0000Dein BigWigs ist um %d Versionen veraltet!! Wir empfehlen dir DRINGEND, BigWigs zu aktualisieren, um Synchronisationsprobleme zwischen dir und anderen Spielern zu verhindern!|r"
+--L.warnCustom = "You are using a custom/guild version of BigWigs, but your base version is %d releases out of date. This may cause issues."
 
 L.gitHubDesc = "|cFF33FF99BigWigs ist Open-Source-Software auf GitHub. Wir sind immer auf der Suche nach neuen Menschen, die uns helfen, und jeder ist willkommen, unseren Code zu inspizieren, Beiträge zu leisten und Fehlerberichte einzureichen. BigWigs ist hauptsächlich durch die großartige WoW-Gemeinschaft im Laufe der Zeit zu etwas Großem geworden.|r"
 

Locales/enUS.lua
@@ -24,6 +24,7 @@ L.sourceCheckout = "You are running a source checkout of BigWigs %s directly fro
 L.getNewRelease = "Your BigWigs is old (/bwv) but you can easily update it using the CurseForge Client. Alternatively, you can update manually from curseforge.com or wowinterface.com."
 L.warnTwoReleases = "Your BigWigs is 2 releases out of date! Your version may have bugs, missing features, or completely incorrect timers. It is strongly recommended you update."
 L.warnSeveralReleases = "|cffff0000Your BigWigs is %d releases out of date!! We HIGHLY recommend you update to prevent syncing issues with other players!|r"
+L.warnCustom = "You are using a custom/guild version of BigWigs, but your base version is %d releases out of date. This may cause issues."
 
 L.tooltipHint = "|cffeda55fRight-Click|r to access options."
 L.activeBossModules = "Active boss modules:"

Locales/esES.lua
@@ -25,6 +25,7 @@ L.sourceCheckout  = "Estás usando la versión de BigWigs %s directamente del re
 L.getNewRelease  = "Tu BigWigs está desfasado (/bwv) pero puedes actualizarlo fácilmente con el cliente de CurseForge. También puedes actualizarlo manualmente desde curseforge.com o wowinterface.com."
 L.warnTwoReleases  = "Tu BigWigs está 2 versiones desfasado! Tu versión puede tener fallos, faltarle características, o temporizadores incorrectos. Es muy recomendable que lo actualices."
 L.warnSeveralReleases  = "|cffff0000Tu BigWigs está desfasado %d actualizaciones!! Te recomendamos MUCHÍSIMO que lo actualices cuanto antes para prevenir problemas de sincronización con otros jugadores!|r"
+--L.warnCustom = "You are using a custom/guild version of BigWigs, but your base version is %d releases out of date. This may cause issues."
 
 L.tooltipHint  = "|cffeda55fRight-Clic|r para acceder a las opciones."
 L.activeBossModules  = "Módulos de jefes activos:"

Locales/frFR.lua
@@ -4,6 +4,7 @@ if not L then return end
 L.getNewRelease = "Votre BigWigs est ancien (/bwv), mais vous pouvez facilement le mettre à jour en utilisant le client CurseForge. Vous pouvez également le mettre à jour à partir de curseforge.com ou wowinterface.com."
 L.warnTwoReleases = "Votre BigWigs est obsolète de 2 versions ! Votre version risque de contenir des bugs, des fonctionnalités manquantes, voire même des délais totalement incorrects. Il est recommandé de faire la mise à jour."
 L.warnSeveralReleases = "|cffff0000Votre BigWigs est %d versions derrière la plus récente !! Il est VIVEMENT recommandé d'effectuer la mise à jour afin d'éviter tout problème de synchronisation avec les autres joueurs !|r"
+--L.warnCustom = "You are using a custom/guild version of BigWigs, but your base version is %d releases out of date. This may cause issues."
 
 L.gitHubDesc = "|cFF33FF99BigWigs est un logiciel open source hébergé sur GitHub. Nous sommes toujours à la recherche de nouvelles personnes pour nous aider et tout le monde est le bienvenu pour inspecter notre code, effectuer des contributions et soumettre des rapports de bogues. BigWigs existe en grande partie grâce à l'aide précieuse de la communauté de WoW.|r"
 

Locales/itIT.lua
@@ -4,6 +4,7 @@ if not L then return end
 --L.getNewRelease = "Your BigWigs is old (/bwv) but you can easily update it using the CurseForge Client. Alternatively, you can update manually from curseforge.com or wowinterface.com."
 --L.warnTwoReleases = "Your BigWigs is 2 releases out of date! Your version may have bugs, missing features, or completely incorrect timers. It is strongly recommended you update."
 --L.warnSeveralReleases = "|cffff0000Your BigWigs is %d releases out of date!! We HIGHLY recommend you update to prevent syncing issues with other players!|r"
+--L.warnCustom = "You are using a custom/guild version of BigWigs, but your base version is %d releases out of date. This may cause issues."
 
 --L.gitHubDesc = "|cFF33FF99BigWigs is open source software hosted on GitHub. We are always looking for new people to help us out and everyone is welcome to inspect our code, make contributions and submit bug reports. BigWigs is as great as it is today largely in part to the great WoW community helping us out.|r"
 

commit ad6016f089e479c10395bc670d20e9ac42925312

Loader: Fix a regression from 83e56e39d8b84f06f858427deaa298c7ef23624e breaking the display of DBM users in the version check

Loader.lua
@@ -1519,9 +1519,9 @@ SlashCmdList.BigWigsVersion = function()
 		if not version then
 			version = ""
 		elseif guildVersion then
-			version = ("|cFFCCCCCC(%d/%d%s)|r"):format(guildVersion, version, hash and "-"..hash or "")
+			version = ("|cFFCCCCCC(%d/%d%s)|r"):format(guildVersion, version, hash and "-"..hash or "") -- %d because BigWigs versions are always stored as a number
 		else
-			version = ("|cFFCCCCCC(%d%s)|r"):format(version, hash and "-"..hash or "")
+			version = ("|cFFCCCCCC(%s%s)|r"):format(version, hash and "-"..hash or "") -- %s because DBM users are stored as a string
 		end
 
 		local _, class = UnitClass(name)