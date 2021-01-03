7ea6616c367a4d783fcb758e1e763c766625da39
Nick Pridorozhko <nick.pridorozhko@gmail.com>
2021-01-04 01:35:02 +0300

    Update for common look, add missing translate

--- Core/Locales/common.ruRU.lua
+++ Core/Locales/common.ruRU.lua
@@ -1,62 +1,72 @@
 local L = BigWigsAPI:NewLocale("BigWigs: Common", "ruRU")
 if not L then return end
 
-L.add = "Помощник"
-L.add_killed = "Помощник убит (%d/%d)"
-L.add_remaining = "Помощник убит, %d осталось"
-L.adds = "Помощники"
-L.add_spawned = "Помощник появился"
-L.big_add = "Большой помощник"
-L.cast = "<Чтение %s>"
-L.casting = "Чтение %s"
-L.count = "%s (%d)"
-L.count_icon = "%s (%d|T13700%d:0|t)"
-L.count_rticon = "%s (%d{rt%d})"
-L.custom_end = "%s входит в %s"
-L.custom_min = "%s через %d мин"
-L.custom_sec = "%s через %d сек"
-L.custom_start = "%s вступает в бой - %s через %d мин"
-L.custom_start_s = "%s вступает в бой - %s через %d сек"
-L.duration = "%s для %s сек"
-L.focus_only = "|cffff0000Оповещения только для фокуса.|r "
-L.general = "Общее" -- General settings, i.e. things that apply to normal, heroic and mythic mode.
-L.heroic = "Героический режим"
-L.incoming = "%s на подходе"
-L.intermission = "Перерыв"
-L.interrupted = "%s прервано"
-L.interrupted_by = "%s прерван %s" -- spell interrupted by player
-L.mob_killed = "%s убит (%d/%d)"
-L.mob_remaining = "%s убит, %d осталось"
-L.mythic = "Эпохальный режим"
-L.near = "%s возле ТЕБЯ"
-L.next_add = "Следующий помощник"
-L.no = "Нет %s"
-L.normal = "Обычный режим"
-L.on = "%s на %s"
+-- Prototype.lua common words
+L.you = "%s на ТЕБЕ"
+L.you_icon = "%s на |T13700%d:0|tТЕБЕ"
+L.underyou = "%s под ТОБОЙ"
+L.other = "%s: %s"
 L.onboss = "%s на БОССЕ"
 L.buff_boss = "Бафф на БОССЕ: %s"
 L.buff_other = "Бафф на %s: %s"
-L.other = "%s: %s"
-L.over = "%s завершается"
---L.percent = "%d%% - %s" -- 20% - spell
+L.on = "%s на %s"
+L.stack = "%dx %s на %s"
+L.stackyou = "%dx %s на ТЕБЕ"
+L.cast = "<Чтение %s>"
+L.casting = "Чтение %s"
+L.soon = "%s скоро"
+L.count = "%s (%d)"
+L.count_icon = "%s (%d|T13700%d:0|t)"
+L.count_rticon = "%s (%d{rt%d})"
+L.near = "%s возле ТЕБЯ"
+L.on_group = "%s на ГРУППЕ" -- spell on group
+
 L.phase = "Фаза %d"
+L.stage = "Этап %d"
+L.normal = "Обычный режим"
+L.heroic = "Героический режим"
+L.mythic = "Эпохальный режим"
+L.active = "Активен" -- When a boss becomes active, after speech finishes
+L.general = "Общее" -- General settings, i.e. things that apply to normal, heroic and mythic mode.
+
+L.duration = "%s для %s сек"
+L.over = "%s завершается"
 L.removed = "%s снято"
 L.removed_from = "%s Снято С %s"
 L.removed_by = "%s был снят %s" -- spell removed by player
-L.small_adds = "Маленькие помощники"
-L.soon = "%s скоро"
+L.incoming = "%s на подходе"
+L.interrupted = "%s прервано"
+L.interrupted_by = "%s прерван %s" -- spell interrupted by player
+L.no = "Нет %s"
+L.intermission = "Перерыв"
+L.percent = "%d%% - %s" -- 20% - spell
+
+-- Add related
+L.add_spawned = "Помощник появился"
 L.spawned = "Появление: %s"
 L.spawning = "%s появление"
-L.stack = "%dx %s на %s"
-L.stackyou = "%dx %s на ТЕБЕ"
-L.stage = "Этап %d"
-L.trash = "Трэш"
-L.underyou = "%s под ТОБОЙ"
-L.you = "%s на ТЕБЕ"
-L.you_icon = "%s на |T13700%d:0|tТЕБЕ"
-L.on_group = "%s на ГРУППЕ" -- spell on group
+L.next_add = "Следующий помощник"
+L.add_killed = "Помощник убит (%d/%d)"
+L.add_remaining = "Помощник убит, %d осталось"
+L.add = "Помощник"
+L.adds = "Помощники"
+L.big_add = "Большой помощник"
+L.small_adds = "Маленькие помощники"
 
-L.active = "Активен" -- When a boss becomes active, after speech finishes
+-- Mob related
+L.mob_killed = "%s убит (%d/%d)"
+L.mob_remaining = "%s убит, %d осталось"
+
+-- Localizers note:
+-- The default mod:Berserk(600) uses spell ID 26662 to get the Berserk name
+L.custom_start = "%s вступает в бой - %s через %d мин"
+L.custom_start_s = "%s вступает в бой - %s через %d сек"
+L.custom_end = "%s входит в %s"
+L.custom_min = "%s через %d мин"
+L.custom_sec = "%s через %d сек"
+
+L.focus_only = "|cffff0000Оповещения только для фокуса.|r "
+L.trash = "Трэш"
 
 -- Common raid marking locale
 L.marker = "Метка %s"
