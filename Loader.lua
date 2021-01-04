e4e181a9e882d3bc9ded238da821c55bed6dfa3b
funkydude <funkydude@users.noreply.github.com>
2021-01-04 04:40:14 +0000

    Loader: Remove another locale print

--- Loader.lua
+++ Loader.lua
@@ -930,7 +930,7 @@ do
 		esES = "Spanish (esES)",
 		--esMX = "Spanish (esMX)",
 		--deDE = "German (deDE)",
-		ptBR = "Portuguese (ptBR)",
+		--ptBR = "Portuguese (ptBR)",
 		--frFR = "French (frFR)",
 	}
 	if locales[L] then
