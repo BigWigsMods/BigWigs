7b9f264662544167832e1b35409dd9ebdd031317
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2021-01-15 20:40:44 +0100

    Update deDE (#956)

--- Core/Locales/common.deDE.lua
+++ Core/Locales/common.deDE.lua
@@ -92,4 +92,4 @@ L.seconds = "%.1fs" -- 1.1 seconds
 L.laser = "Laser" -- Used for abilities that act like a laser. Usually from the boss to a player, or, from the boss to a specific area
 L.bomb = "Bombe" -- Used for debuffs that make players explode
 L.fixate = "Fixieren" -- Used when a boss or add is chasing/fixated on a player
---L.knockback = "Knockback" -- Used when an abily knocks players away from a certain point, like a "smash" type ability that knocks you back 10 meters
+L.knockback = "Rückstoß" -- Used when an abily knocks players away from a certain point, like a "smash" type ability that knocks you back 10 meters
