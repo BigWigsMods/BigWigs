f337f4e5a7497a549772b57e3d2371428538a252
Nyamp <renanmanieroprado@gmail.com>
2021-01-22 15:51:42 -0300

    Update common.ptBR.lua

--- Core/Locales/common.ptBR.lua
+++ Core/Locales/common.ptBR.lua
@@ -91,5 +91,5 @@ L.seconds = "%.1fs" -- 1.1 seconds
 L.laser = "Laser" -- Used for abilities that act like a laser. Usually from the boss to a player, or, from the boss to a specific area
 L.bomb = "Bomba" -- Used for debuffs that make players explode
 L.fixate = "Fixação" -- Used when a boss or add is chasing/fixated on a player
---L.knockback = "Knockback" -- Used when an abily knocks players away from a certain point, like a "smash" type ability that knocks you back 10 meters
+L.knockback = "Empurrão" -- Used when an abily knocks players away from a certain point, like a "smash" type ability that knocks you back 10 meters
 L.traps = "Armadilhas" -- Used for abilities that act like traps on the floor e.g. move into it and something bad happens like you die, or are rooted, etc.
