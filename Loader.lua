34ab25ce4b492a3089f22122c781c0a9c1009e4f
funkydude <funkydude@users.noreply.github.com>
2021-02-02 06:59:34 +0000

    bump version

--- Loader.lua
+++ Loader.lua
@@ -19,7 +19,7 @@ local ldbi = LibStub("LibDBIcon-1.0")
 -- Generate our version variables
 --
 
-local BIGWIGS_VERSION = 210
+local BIGWIGS_VERSION = 211
 local BIGWIGS_RELEASE_STRING, BIGWIGS_VERSION_STRING = "", ""
 local versionQueryString, versionResponseString = "Q^%d^%s^%d^%s", "V^%d^%s^%d^%s"
 local customGuildName = false
