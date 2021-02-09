a73e795992defa23a8eaecfa86b2e3f0f1831e39
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2021-02-09 06:53:54 +0100

    Update deDE.lua

--- CastleNathria/Locales/deDE.lua
+++ CastleNathria/Locales/deDE.lua
@@ -12,8 +12,8 @@ end
 
 L = BigWigs:NewBossLocale("Sun King's Salvation", "deDE")
 if L then
-	--L.shield_removed = "%s removed after %.1fs" -- "Shield removed after 1.1s" s = seconds
-	--L.shield_remaining = "%s remaining: %s (%.1f%%)" -- "Shield remaining: 2.1K (5.3%)"
+	L.shield_removed = "%s entfernt nach %.1fs" -- "Shield removed after 1.1s" s = seconds
+	L.shield_remaining = "%s verbleibend: %s (%.1f%%)" -- "Shield remaining: 2.1K (5.3%)"
 end
 
 L = BigWigs:NewBossLocale("Hungering Destroyer", "deDE")
