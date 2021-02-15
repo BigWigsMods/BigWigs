3a370c92d4890d174c7892b48fe2d13532592b67
Nyamp <renanmanieroprado@gmail.com>
2021-02-15 01:10:30 -0300

    Update ptBR.lua

--- CastleNathria/Locales/ptBR.lua
+++ CastleNathria/Locales/ptBR.lua
@@ -12,8 +12,8 @@ end
 
 L = BigWigs:NewBossLocale("Sun King's Salvation", "ptBR")
 if L then
-	--L.shield_removed = "%s removed after %.1fs" -- "Shield removed after 1.1s" s = seconds
-	--L.shield_remaining = "%s remaining: %s (%.1f%%)" -- "Shield remaining: 2.1K (5.3%)"
+	L.shield_removed = "%s removido depois de %.1fs" -- "Shield removed after 1.1s" s = seconds
+	L.shield_remaining = "%s restante: %s (%.1f%%)" -- "Shield remaining: 2.1K (5.3%)"
 end
 
 L = BigWigs:NewBossLocale("Hungering Destroyer", "ptBR")
