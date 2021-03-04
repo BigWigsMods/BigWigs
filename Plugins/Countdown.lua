a28af06ad55bcb57cf44ca76ff5f462a0424e732
funkydude <funkydude@users.noreply.github.com>
2021-03-04 21:45:17 +0000

    Plugins: Fix a bad copy/paste

--- Plugins/Countdown.lua
+++ Plugins/Countdown.lua
@@ -498,9 +498,10 @@ do
 		local db = plugin.db.profile
 
 		for k, v in next, db do
-			if type(plugin.defaultDB[k]) == "nil" then
+			local defaultType = type(plugin.defaultDB[k])
+			if defaultType == "nil" then
 				db[k] = nil
-			elseif type(v) ~= "boolean" then
+			elseif type(v) ~= defaultType then
 				db[k] = plugin.defaultDB[k]
 			end
 		end
