61edf6ed78aacb249d83d8a7c5f06fb299b2e659
funkydude <funkydude@users.noreply.github.com>
2021-03-09 17:23:07 +0000

    CastleNathria/Trash: Fix the parser failing

--- CastleNathria/Trash.lua
+++ CastleNathria/Trash.lua
@@ -352,18 +352,18 @@ do
 		if amount == 1 then
 			-- If this applies at full health then it's instantly removed
 			-- Prevent pointless applied/removed messages by not alerting until 0.2s has passed
-			local spellId, destName, destGUID = args.spellId, args.destName, args.destGUID -- SetOption:339975:
+			local spellId, destName, destGUID = args.spellId, args.destName, args.destGUID
 			stacks[destGUID] = true
 			self:SimpleTimer(function()
 				if stacks[destGUID] then
 					stacks[destGUID] = nil
 					if self:Tank(destName) then
-						self:TargetMessage(spellId, "purple", destName)
+						self:TargetMessage(spellId, "purple", destName) -- SetOption:339975:
 						if self:Tank() or self:Healer() then
-							self:PlaySound(spellId, "info")
+							self:PlaySound(spellId, "info") -- SetOption:339975:
 						end
 					elseif self:Healer() then
-						self:TargetMessage(spellId, "orange", destName)
+						self:TargetMessage(spellId, "orange", destName) -- SetOption:339975:
 					end
 				end
 			end, 0.2)
