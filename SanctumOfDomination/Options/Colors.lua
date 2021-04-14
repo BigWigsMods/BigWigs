271e6dec3cfd8aedc20a2c57ae73191db3514d77
funkydude <funkydude@users.noreply.github.com>
2021-04-14 15:59:15 +0100

    SanctumOfDomination: Update option files

--- SanctumOfDomination/Options/Colors.lua
+++ SanctumOfDomination/Options/Colors.lua
@@ -1,1 +1,32 @@
---Empty
+
+BigWigs:AddColors("The Tarragrue", {
+	[346985] = {"blue","purple"},
+	[346986] = {"blue","purple"},
+	[347269] = {"blue","yellow"},
+	[347274] = "blue",
+	[347283] = {"blue","green","red"},
+	[347286] = "blue",
+	[347369] = {"green","orange"},
+	[347490] = {"cyan","green","orange"},
+	[347668] = {"blue","red","yellow"},
+	[347671] = "orange",
+	[352368] = "yellow",
+	[352382] = "cyan",
+	[352389] = "cyan",
+	[352398] = "cyan",
+})
+
+BigWigs:AddColors("Guardian of the First Ones", {
+	[347359] = "blue",
+	[350455] = "blue",
+	[350502] = "orange",
+	[350732] = {"blue","purple"},
+	[350734] = {"blue","purple"},
+	[350735] = "cyan",
+	[352385] = "cyan",
+	[352394] = {"green","red"},
+	[352538] = "yellow",
+	[352589] = "orange",
+	[352660] = "yellow",
+	[352833] = "red",
+})
--- SanctumOfDomination/Options/Sounds.lua
+++ SanctumOfDomination/Options/Sounds.lua
@@ -1,1 +1,32 @@
---Empty
+
+BigWigs:AddSounds("The Tarragrue", {
+	[346985] = "warning",
+	[346986] = "warning",
+	[347269] = "warning",
+	[347274] = "alarm",
+	[347283] = {"alarm","info"},
+	[347286] = "alarm",
+	[347369] = "long",
+	[347490] = {"info","warning"},
+	[347668] = {"alarm","alert"},
+	[347671] = "alert",
+	[352368] = "long",
+	[352382] = "info",
+	[352389] = "info",
+	[352398] = "info",
+})
+
+BigWigs:AddSounds("Guardian of the First Ones", {
+	[347359] = "underyou",
+	[350455] = "warning",
+	[350502] = "alarm",
+	[350732] = "warning",
+	[350734] = "alarm",
+	[350735] = "info",
+	[352385] = "info",
+	[352394] = "info",
+	[352538] = "warning",
+	[352589] = "alert",
+	[352660] = "alert",
+	[352833] = "alarm",
+})
