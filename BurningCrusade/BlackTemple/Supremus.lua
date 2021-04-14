729849f9d6c4d356e92fe2f241d8ed64a3264c94
Kyle Buller <bullerk@gmail.com>
2021-04-14 00:42:43 -0500

    BlackTemple: Fix Supremus Fixate
    It was erroring!

--- BurningCrusade/BlackTemple/Supremus.lua
+++ BurningCrusade/BlackTemple/Supremus.lua
@@ -10,6 +10,13 @@ mod.engageId = 602
 --mod.respawnTime = 0 -- Resets, doesn't respawn
 
 --------------------------------------------------------------------------------
+-- Locals
+--
+
+local fixateTimer = nil
+local fixateTarget = nil
+
+--------------------------------------------------------------------------------
 -- Localization
 --
 
@@ -20,6 +27,9 @@ if L then
 	L.normal_phase = "Normal Phase"
 	L.kite_phase = "Kite Phase"
 	L.next_phase = "Next Phase"
+
+	L.fixate = mod:SpellName(40607)
+	L.fixate_icon = 40607
 end
 
 --------------------------------------------------------------------------------
@@ -28,7 +38,7 @@ end
 
 function mod:GetOptions()
 	return {
-		{41951, "SAY", "ICON"}, -- Fixate
+		{"fixate", "SAY", "ICON"}, -- Fixate
 		40126, -- Molten Punch
 		40265, -- Molten Flame
 		"stages",
@@ -37,8 +47,9 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_AURA_APPLIED", "Fixate", 41951)
-	self:Log("SPELL_AURA_REMOVED", "FixateRemoved", 41951)
+	-- This wasn't exposed until after BC?
+	-- self:Log("SPELL_AURA_APPLIED", "Fixate", 41951)
+	-- self:Log("SPELL_AURA_REMOVED", "FixateRemoved", 41951)
 	self:Log("SPELL_CAST_SUCCESS", "MoltenPunch", 40126)
 
 	self:Log("SPELL_DAMAGE", "MoltenFlameDamage", 40265)
@@ -48,6 +59,7 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
+	fixateTimer, fixateTarget = nil, nil
 	self:Berserk(900)
 	self:CDBar(40126, 12) -- Molten Punch
 	self:Bar("stages", 60, L.next_phase, "spell_shadow_summoninfernal")
@@ -57,17 +69,41 @@ end
 -- Event Handlers
 --
 
-function mod:Fixate(args)
-	if self:Me(args.destGUID) then
-		self:Say(args.spellId)
+function mod:FixateScan()
+	local unit = self:GetUnitIdByGUID(22898)
+	if not unit then return end
+
+	local targetUnit = unit .. "target"
+	local target = self:UnitName(targetUnit)
+	if not target then
+		if fixateTarget then
+			self:PrimaryIcon("fixate")
+			fixateTarget = nil
+		end
+		return
 	end
-	self:TargetMessageOld(args.spellId, args.destName, "red", "warning")
-	self:PrimaryIcon(args.spellId, args.destName)
+
+	if target == fixateTarget then return end
+	fixateTarget = target
+
+	if self:Me(self:UnitGUID(targetUnit)) then
+		self:Say("fixate", L.fixate)
+	end
+	self:TargetMessageOld("fixate", target, "red", "warning", L.fixate, L.fixate_icon)
+	self:PrimaryIcon("fixate", target)
 end
 
-function mod:FixateRemoved(args)
-	self:PrimaryIcon(args.spellId)
-end
+-- function mod:Fixate(args)
+-- 	if self:Me(args.destGUID) then
+-- 		self:Say(args.spellId)
+-- 	end
+-- 	self:TargetMessageOld(args.spellId, args.destName, "red", "warning")
+-- 	self:PrimaryIcon(args.spellId, args.destName)
+-- end
+
+-- function mod:FixateRemoved(args)
+-- 	self:PrimaryIcon(args.spellId)
+-- end
 
 function mod:MoltenPunch(args)
 	self:MessageOld(args.spellId, "yellow")
@@ -87,10 +123,17 @@ end
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(_, msg)
 	if msg == L.normal_phase_trigger then
+		if fixateTimer then
+			self:CancelTimer(fixateTimer)
+			self:PrimaryIcon("fixate")
+		end
 		self:MessageOld("stages", "cyan", "info", L.normal_phase, false)
 		self:Bar("stages", 60, L.next_phase, "spell_shadow_summoninfernal")
 	elseif msg == L.kite_phase_trigger then
 		self:MessageOld("stages", "cyan", "info", L.kite_phase, false)
 		self:Bar("stages", 60, L.next_phase, "spell_shadow_summoninfernal")
+		if self:GetOption("fixate") > 0 then
+			fixateTimer = self:ScheduleRepeatingEvent("FixateScan", 1)
+		end
 	end
 end
