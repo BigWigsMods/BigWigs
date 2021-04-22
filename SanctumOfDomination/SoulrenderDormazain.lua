07993a559c7cd7ba3e204ba1fa43a629e8054f7c
funkydude <funkydude@users.noreply.github.com>
2021-04-22 13:27:55 +0100

    SanctumOfDomination/SoulrenderDormazain: Initial guesswork module

--- SanctumOfDomination/SoulrenderDormazain.lua
+++ SanctumOfDomination/SoulrenderDormazain.lua
@@ -1,6 +1,7 @@
 --------------------------------------------------------------------------------
 -- Module Declaration
 --
+
 if not IsTestBuild() then return end
 local mod, CL = BigWigs:NewBoss("Soulrender Dormazain", 2450, 2445)
 if not mod then return end
@@ -25,53 +26,133 @@ mod:SetRespawnTime(30)
 
 local L = mod:GetLocale()
 if L then
-	
+
 end
 
 --------------------------------------------------------------------------------
 -- Initialization
 --
 
+local brandOfTormentMarker = mod:AddMarkerOption(false, "player", 1, 350647, 1, 2) -- Brand of Torment
 function mod:GetOptions()
 	return {
-		"stages",
-		"berserk",
+		-- Soulrender Dormazain
+		350217, -- Torment
+		349985, -- Encore of Torment
+		350647, -- Brand of Torment
+		{350422, "TANK"}, -- Ruinblade
+		351779, -- Agonizing Spike
+		350650, -- Defiance
+		350411, -- Hellscream
+		354231, -- Soul Manacles
+		351229, -- Rendered Soul
 	},{
-		["stages"] = "general",
+		[350217] = mod:SpellName(-22914), -- Soulrender Dormazain
 	}
 end
 
 function mod:OnBossEnable()
-	-- Stage One - Thirst for Blood
-	--self:Log("SPELL_CAST_START", "EarsplittingShriek", 330711)
-	--self:Log("SPELL_AURA_APPLIED", "EcholocationApplied", 342077)
-	--self:Log("SPELL_AURA_REMOVED", "EcholocationRemoved", 342077)
-	--self:Log("SPELL_CAST_START", "EchoingScreech", 342863)
-	--self:Log("SPELL_CAST_START", "WaveOfBlood", 345397)
-	--self:Log("SPELL_CAST_START", "BlindSwipe", 343005)
-	--self:Log("SPELL_CAST_START", "ExsanguinatingBite", 328857)
-	--self:Log("SPELL_AURA_APPLIED", "ExsanguinatedApplied", 328897)
-	--self:Log("SPELL_AURA_APPLIED_DOSE", "ExsanguinatedApplied", 328897)
-	--self:Log("SPELL_AURA_REMOVED", "ExsanguinatedRemoved", 328897)
+	-- Soulrender Dormazain
+	self:Log("SPELL_CAST_SUCCESS", "Torment", 350217)
+	self:Log("SPELL_CAST_SUCCESS", "EncoreOfTorment", 349985)
+	self:Log("SPELL_AURA_APPLIED", "BrandOfTormentApplied", 350647)
+	--self:Log("SPELL_AURA_APPLIED", "BrandOfTormentApplied", 350647)
+	self:Log("SPELL_AURA_APPLIED", "RuinbladeApplied", 350422)
+	self:Log("SPELL_CAST_START", "AgonizingSpike", 351779)
+	self:Log("SPELL_AURA_APPLIED", "DefianceApplied", 350650)
+	self:Log("SPELL_CAST_SUCCESS", "Hellscream", 350411)
+	self:Log("SPELL_AURA_APPLIED", "SoulManacles", 354231)
+	self:Log("SPELL_CAST_SUCCESS", "RenderedSoul", 351229)
 end
 
 function mod:OnEngage()
-	--shriekCount = 1
-	--echoingScreechCount = 1
-	--echolocationCount = 1
-	--blindSwipeCount = 1
-	--waveOfBloodCount = 1
-	--self:SetStage(1)
-	--
-	--if self:Mythic() then
-	--	self:Berserk(400)
-	--else
-	--	self:Berserk(550)
-	--end
+	self:RegisterUnitEvent("UNIT_POWER_FREQUENT", nil, "boss1")
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:UNIT_POWER_FREQUENT(event, unit)
+	local power = UnitPower(unit)
+	if power >= 90 then
+		--self:Message(349985, "cyan", CL.soon:format(self:SpellName(349985)), false) -- Encore of Torment
+		--self:PlaySound(349985, "info")
+		--self:UnregisterUnitEvent(event, unit)
+	end
+end
 
+function mod:Torment(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alert")
+	self:CastBar(args.spellId, 6)
+	--self:Bar(args.spellId, 42)
+end
+
+function mod:EncoreOfTorment(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alert")
+	--self:Bar(args.spellId, 42)
+end
+
+do
+	local playerList = {}
+	local prev = 0
+	function mod:BrandOfTormentApplied(args)
+		local t = args.time
+		if t-prev > 5 then
+			prev = t
+			playerList = {}
+			--self:Bar(args.spellId, 6.3)
+		end
+		local count = #playerList+1
+		playerList[count] = args.destName
+		playerList[args.destName] = count -- Set raid marker
+		if self:Me(args.destGUID) then
+			self:Say(args.spellId)
+			self:PlaySound(args.spellId, "warning")
+		end
+		self:NewTargetsMessage(args.spellId, "orange", playerList)
+		self:CustomIcon(brandOfTormentMarker, args.destName, count)
+	end
+end
+
+function mod:RuinbladeApplied(args)
+	local amount = args.amount or 1
+	self:NewStackMessage(args.spellId, "purple", args.destName, amount)
+	self:PlaySound(args.spellId, "alarm")
+	--self:Bar(args.spellId, 6.3)
+end
+
+function mod:AgonizingSpike(args)
+	local canDo, ready = self:Interrupter()
+	if canDo then
+		self:Message(args.spellId, "yellow")
+		if ready then
+			self:PlaySound(args.spellId, "alert")
+		end
+	end
+end
+
+function mod:DefianceApplied(args)
+	self:TargetMessage(args.spellId, "orange", args.destName)
+end
+
+function mod:Hellscream(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "long")
+	--self:CastBar(args.spellId, 6)
+end
+
+function mod:SoulManacles(args)
+	if self:Me(args.destGUID) then
+		self:PersonalMessage(args.spellId)
+		self:PlaySound(args.spellId, "info")
+	end
+end
+
+function mod:RenderedSoul(args)
+	self:Message(args.spellId, "yellow", CL.incoming:format(args.spellName))
+	--self:PlaySound(args.spellId, "long")
+	--self:CastBar(args.spellId, 6)
+end
