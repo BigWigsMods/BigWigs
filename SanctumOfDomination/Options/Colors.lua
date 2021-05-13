066b7582a1a7aca4545ce0b87e661357280b1b0e
funkydude <funkydude@users.noreply.github.com>
2021-05-13 19:56:50 +0100

    SanctumOfDomination: Update option files

--- SanctumOfDomination/Options/Colors.lua
+++ SanctumOfDomination/Options/Colors.lua
@@ -16,15 +16,28 @@ BigWigs:AddColors("The Tarragrue", {
 	[352398] = "cyan",
 })
 
+BigWigs:AddColors("The Eye of the Jailer", {
+	[348074] = "blue",
+	[349028] = "orange",
+	[349979] = "blue",
+	[350022] = {"blue","yellow"},
+	[350604] = {"blue","cyan"},
+	[350713] = {"blue","orange"},
+	[350828] = "yellow",
+	[350847] = {"blue","red"},
+	[351413] = "yellow",
+	[351825] = "red",
+	[351827] = {"blue","orange"},
+	[355232] = "blue",
+	["stages"] = "green",
+})
+
 BigWigs:AddColors("The Nine", {
-	[350031] = "cyan",
 	[350039] = {"blue","orange"},
-	[350098] = "cyan",
-	[350109] = {"blue","cyan","yellow"},
-	[350157] = "cyan",
+	[350109] = "yellow",
+	[350158] = "red",
 	[350184] = "cyan",
 	[350202] = {"blue","purple"},
-	[350283] = "yellow",
 	[350286] = "yellow",
 	[350339] = "yellow",
 	[350342] = "yellow",
@@ -40,6 +53,17 @@ BigWigs:AddColors("The Nine", {
 	["stages"] = "green",
 })
 
+BigWigs:AddColors("Remnant of Ner'zhul", {
+	[349889] = {"blue","purple"},
+	[349890] = {"blue","purple"},
+	[350073] = "blue",
+	[350388] = "blue",
+	[350469] = {"blue","orange","yellow"},
+	[350489] = "blue",
+	[351066] = "cyan",
+	[353332] = "yellow",
+})
+
 BigWigs:AddColors("Soulrender Dormazain", {
 	[349985] = "red",
 	[350217] = "yellow",
@@ -50,6 +74,17 @@ BigWigs:AddColors("Soulrender Dormazain", {
 	[351229] = "yellow",
 	[351779] = "yellow",
 	[354231] = "blue",
+})
+
+BigWigs:AddColors("Painsmith Raznal", {
+	[348456] = {"blue","orange"},
+	[348508] = {"blue","yellow"},
+	[352052] = "red",
+	[355505] = {"blue","yellow"},
+	[355568] = {"blue","yellow"},
+	[355778] = {"blue","yellow"},
+	[355786] = {"blue","purple"},
+	["stages"] = "green",
 })
 
 BigWigs:AddColors("Guardian of the First Ones", {
@@ -64,4 +99,38 @@ BigWigs:AddColors("Guardian of the First Ones", {
 	[352589] = "orange",
 	[352660] = "yellow",
 	[352833] = "red",
+})
+
+BigWigs:AddColors("Fatescribe Roh-Kalo", {
+	[350421] = "yellow",
+	[350568] = {"blue","orange"},
+	[351680] = {"blue","purple"},
+	[351969] = "red",
+	[353149] = "cyan",
+	[353150] = "cyan",
+	[353195] = {"green","orange"},
+	[353432] = "blue",
+	[353603] = "purple",
+	[353931] = "blue",
+})
+
+BigWigs:AddColors("Kel'Thuzad", {
+	[346459] = {"blue","orange"},
+	[346530] = "blue",
+	[347292] = {"blue","yellow"},
+	[348071] = "purple",
+	[348760] = {"blue","orange"},
+	[348978] = {"blue","green","purple"},
+	[352051] = {"blue","cyan"},
+	[352141] = "yellow",
+	[352293] = "yellow",
+	[352348] = "yellow",
+	[352355] = "red",
+	[352379] = "orange",
+	[352530] = "cyan",
+	[354198] = {"blue","cyan"},
+	[354289] = "blue",
+	[354639] = "blue",
+	[355055] = "cyan",
+	[355389] = "blue",
 })
--- SanctumOfDomination/Options/Sounds.lua
+++ SanctumOfDomination/Options/Sounds.lua
@@ -16,11 +16,28 @@ BigWigs:AddSounds("The Tarragrue", {
 	[352398] = "info",
 })
 
+BigWigs:AddSounds("The Eye of the Jailer", {
+	[348074] = "alarm",
+	[349028] = "alarm",
+	[349979] = "alarm",
+	[350022] = {"alert","warning"},
+	[350604] = "warning",
+	[350713] = {"alarm","alert"},
+	[350828] = "alert",
+	[350847] = "warning",
+	[351413] = "warning",
+	[351825] = "warning",
+	[351827] = {"alert","warning"},
+	[355232] = {"info","warning"},
+	["stages"] = "long",
+})
+
 BigWigs:AddSounds("The Nine", {
 	[350039] = {"alert","warning"},
 	[350109] = "alarm",
+	[350158] = "alarm",
+	[350184] = "warning",
 	[350202] = "alarm",
-	[350283] = "alert",
 	[350286] = "alert",
 	[350339] = "alert",
 	[350342] = "long",
@@ -33,6 +50,18 @@ BigWigs:AddSounds("The Nine", {
 	[350555] = "underyou",
 	[350687] = "info",
 	[351399] = "alert",
+	["stages"] = "long",
+})
+
+BigWigs:AddSounds("Remnant of Ner'zhul", {
+	[349889] = "warning",
+	[349890] = {"alarm","warning"},
+	[350073] = "alarm",
+	[350388] = "alarm",
+	[350469] = "warning",
+	[350489] = "underyou",
+	[351066] = "long",
+	[353332] = "alert",
 })
 
 BigWigs:AddSounds("Soulrender Dormazain", {
@@ -43,6 +72,16 @@ BigWigs:AddSounds("Soulrender Dormazain", {
 	[350647] = "warning",
 	[351779] = "alert",
 	[354231] = "info",
+})
+
+BigWigs:AddSounds("Painsmith Raznal", {
+	[348456] = "warning",
+	[348508] = {"alert","warning"},
+	[352052] = "alarm",
+	[355505] = "warning",
+	[355568] = {"alert","warning"},
+	[355778] = {"alert","warning"},
+	[355786] = {"alarm","warning"},
 })
 
 BigWigs:AddSounds("Guardian of the First Ones", {
@@ -57,4 +96,38 @@ BigWigs:AddSounds("Guardian of the First Ones", {
 	[352589] = "alert",
 	[352660] = "long",
 	[352833] = "alert",
+})
+
+BigWigs:AddSounds("Fatescribe Roh-Kalo", {
+	[350421] = "alert",
+	[350568] = "warning",
+	[351680] = {"alert","warning"},
+	[351969] = "long",
+	[353149] = "info",
+	[353150] = "info",
+	[353195] = "alert",
+	[353432] = "alarm",
+	[353603] = "alert",
+	[353931] = "alarm",
+})
+
+BigWigs:AddSounds("Kel'Thuzad", {
+	[346459] = {"alert","warning"},
+	[346530] = {"info","warning"},
+	[347292] = "warning",
+	[348071] = "alarm",
+	[348760] = {"alert","warning"},
+	[348978] = {"alarm","info","warning"},
+	[352051] = "info",
+	[352141] = "alarm",
+	[352293] = "long",
+	[352348] = "alert",
+	[352355] = "warning",
+	[352379] = "alarm",
+	[352530] = "long",
+	[354198] = {"long","underyou"},
+	[354289] = "alert",
+	[354639] = "underyou",
+	[355055] = "info",
+	[355389] = "alarm",
 })
