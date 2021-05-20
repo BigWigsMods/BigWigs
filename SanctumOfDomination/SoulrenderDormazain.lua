42208daa1f1b6f6065a436c1aa51d25ea2568592
Justwait <mngrootwow@gmail.com>
2021-05-20 18:53:57 +0200

    SanctumOfDomination/SoulrenderDormazain: Add marking for Agonizers, rename spells, general updates

--- SanctumOfDomination/SoulrenderDormazain.lua
+++ SanctumOfDomination/SoulrenderDormazain.lua
@@ -8,13 +8,23 @@ if not mod then return end
 mod:RegisterEnableMob(175727, 175728) -- Soulrender Dormazain, Garrosh Hellscream
 mod:SetEncounterID(2434)
 mod:SetRespawnTime(30)
---mod:SetStage(1)
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
+local timers = {
+	[350411] = {80.5, 162, 132.5, 65.0, 61, 60, 90, 64.5, 60, 63.5}, -- Hellscream _START
+	[350615] = {28.5, 162.5, 60, 97, 60, 100, 60, 101, 60, 97.5}, -- Call Mawsworn _START
+	[350217] = {12, 45.5, 45.5, 74, 45.5, 45.5, 68, 45.5, 45.5, 60, 45.5, 45.5, 80, 45.5, 45.5, 69.5} -- Torment
+}
 
+local brandCount = 1
+local callMawswornCount = 1
+local ruinbladeCount = 1
+local hellscreamCount = 1
+local encoreOfTormentCount = 1
+local tormentCount = 1
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -22,23 +32,33 @@ mod:SetRespawnTime(30)
 
 local L = mod:GetLocale()
 if L then
-
+	L.cones = "Cones" -- Torment
+	L.dance = "Dance" -- Encore of Torment
+	L.brands = "Brands" -- Brand of Torment
+	L.brand = "Brand" -- Single Brand of Torment
+	L.spike = "Spike" -- Short for Agonizing Spike
+	L.chains = "Chains" -- Hellscream
+	L.chain = "Chain" -- Soul Manacles
+	L.souls = "Souls" -- Rendered Soul
 end
 
 --------------------------------------------------------------------------------
 -- Initialization
 --
 
+
 local brandOfTormentMarker = mod:AddMarkerOption(false, "player", 1, 350647, 1, 2) -- Brand of Torment
+local agonizerMarker = mod:AddMarkerOption(false, "npc", 8, -23289, 8, 7, 6, 5) -- Mawsworn Agonizer
 function mod:GetOptions()
 	return {
 		-- Soulrender Dormazain
 		350217, -- Torment
 		349985, -- Encore of Torment
-		{350647, "SAY"}, -- Brand of Torment
+		{350647, "SAY", "SAY_COUNTDOWN"}, -- Brand of Torment
 		brandOfTormentMarker,
 		{350422, "TANK"}, -- Ruinblade
 		350615, -- Call Mawsworn
+		agonizerMarker,
 		351779, -- Agonizing Spike
 		350650, -- Defiance
 		350411, -- Hellscream
@@ -46,18 +66,32 @@ function mod:GetOptions()
 		351229, -- Rendered Soul
 	},{
 		[350217] = mod:SpellName(-22914), -- Soulrender Dormazain
+	},{
+		[350217] = L.cones, -- Torment (Cones)
+		[349985] = L.dance, -- Encore of Torment (Dance)
+		[350647] = L.brands, -- Brand of Torment (Brands)
+		[350615] = CL.adds, -- Call Mawsworn (Adds)
+		[351779] = L.spike, -- Agonizing Spike (Spike)
+		[350411] = L.chains, -- Hellscream (Chains)
+		[354231] = L.chain, -- Soul Manacles (Chain)
+		[351229] = L.souls, -- Rendered Soul (Souls)
 	}
 end
 
 function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
+
 	-- Soulrender Dormazain
-	self:Log("SPELL_CAST_SUCCESS", "Torment", 351581)
+	--self:Log("SPELL_CAST_SUCCESS", "Torment", 351581) -- USSC for the boss cast
 	self:Log("SPELL_CAST_SUCCESS", "EncoreOfTorment", 349985)
+	self:Log("SPELL_CAST_SUCCESS", "BrandOfTorment", 350648)
 	self:Log("SPELL_AURA_APPLIED", "BrandOfTormentApplied", 350647)
 	self:Log("SPELL_AURA_REMOVED", "BrandOfTormentRemoved", 350647)
+	self:Log("SPELL_CAST_SUCCESS", "Ruinblade", 350422)
 	self:Log("SPELL_AURA_APPLIED", "RuinbladeApplied", 350422)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "RuinbladeApplied", 350422)
 	self:Log("SPELL_CAST_START", "CallMawsworn", 350615)
+	self:Log("SPELL_SUMMON", "AgonizerSpawn", 346459)
 	self:Log("SPELL_CAST_START", "AgonizingSpike", 351779)
 	self:Log("SPELL_AURA_APPLIED", "DefianceApplied", 350650)
 	self:Log("SPELL_CAST_START", "Hellscream", 350411)
@@ -65,92 +99,125 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
-	self:Bar(350422, 10.7) -- Ruinblade
-	self:Bar(350217, 12) -- Torment
-	self:Bar(350615, 29.2) -- Call Mawsworn
-	self:Bar(350647, 36.8) -- Brand of Torment
-	self:Bar(350411, 81.1) -- Hellscream
-	self:Bar(349985, 132) -- Encore of Torment
+	callMawswornCount = 1
+	ruinbladeCount = 1
+	hellscreamCount = 1
+	encoreOfTormentCount = 1
+	tormentCount = 1
 
-	-- self:RegisterUnitEvent("UNIT_POWER_FREQUENT", nil, "boss1")
+	self:Bar(350422, 10.7) -- Ruinblade
+	self:Bar(350217, 12, CL.count:format(L.cones, tormentCount)) -- Torment
+	self:Bar(350615, 29.2, CL.count:format(CL.adds, callMawswornCount)) -- Call Mawsworn
+	self:Bar(350411, 81.1, CL.count:format(L.chains, hellscreamCount)) -- Hellscream
+	self:Bar(349985, 132, CL.count:format(L.dance, encoreOfTormentCount)) -- Encore of Torment
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+function mod:RenderedSoul(t, count)
+		self:Bar(351229, t, CL.count:format(L.souls, count))
+		self:ScheduleTimer("Message", t, 351229, "yellow", CL.count:format(L.souls, count))
+		count = count + 1
+		if count < 3 then -- Waves of 2, 2nd is 5s after first
+			self:ScheduleTimer("RenderedSoul", t, 5, count)
+		end
+end
 
--- function mod:UNIT_POWER_FREQUENT(event, unit)
--- 	local power = UnitPower(unit)
--- 	if power >= 90 then
--- 		self:Message(349985, "cyan", CL.soon:format(self:SpellName(349985)), false) -- Encore of Torment
--- 		self:PlaySound(349985, "info")
--- 		self:UnregisterUnitEvent(event, unit)
--- 	end
--- end
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
+	if spellId == 349873 then -- Boss casting Torment
+		self:Message(350217, "yellow", CL.count:format(L.cones, tormentCount))
+		tormentCount = tormentCount + 1
+		self:Bar(350217, timers[350217][tormentCount], CL.count:format(L.cones, tormentCount))
 
-function mod:Torment(args)
-	self:Message(350217, "yellow")
-	self:PlaySound(350217, "alert")
-	-- self:CastBar(350217, 6)
-	self:Bar(350217, 45)
-
-	-- Rendered Soul hits, it targets 3s before hitting
-	self:Bar(351229, 8, CL.count:format(self:SpellName(351229), 1))
-	self:Bar(351229, 13, CL.count:format(self:SpellName(351229), 2))
+		self:RenderedSoul(5, 1)
+	end
 end
 
 function mod:EncoreOfTorment(args)
-	self:Message(args.spellId, "cyan")
+	self:Message(args.spellId, "cyan", CL.count:format(L.dance, encoreOfTormentCount))
 	self:PlaySound(args.spellId, "alert")
-	self:Bar(args.spellId, 163)
-
-	self:Bar(350422, 41) -- Ruinblade
-	self:Bar(350217, 45) -- Torment
-	self:Bar(350615, 59) -- Call Mawsworn
-	self:Bar(350647, 66) -- Brand of Torment
-	self:Bar(350411, 111) -- Hellscream
+	encoreOfTormentCount = encoreOfTormentCount + 1
+	self:Bar(args.spellId, 161.1, CL.count:format(L.dance, encoreOfTormentCount))
 end
 
 do
 	local playerList = {}
-	local prev = 0
-	function mod:BrandOfTormentApplied(args)
-		local t = args.time
-		if t-prev > 5 then
-			prev = t
-			playerList = {}
-			self:Bar(args.spellId, 17)
+	function mod:BrandOfTorment(args)
+		playerList = {}
+		brandCount = brandCount + 1
+		if brandCount < 4 then -- Comes in waves of 3 sets
+			self:Bar(args.spellId, 17, CL.count:format(L.brands, brandCount))
 		end
+	end
+
+	function mod:BrandOfTormentApplied(args)
 		local count = #playerList+1
 		playerList[count] = args.destName
 		playerList[args.destName] = count -- Set raid marker
 		if self:Me(args.destGUID) then
-			self:Say(args.spellId)
+			self:Say(args.spellId, CL.count:format(L.brand, brandCount-1))
 			self:PlaySound(args.spellId, "warning")
-			self:TargetBar(args.spellId, 16, args.destName)
+			self:SayCountdown(args.spellId, 16)
 		end
-		self:NewTargetsMessage(args.spellId, "orange", playerList)
+		self:NewTargetsMessage(args.spellId, "orange", playerList, nil, CL.count:format(L.brand, brandCount-1))
 		self:CustomIcon(brandOfTormentMarker, args.destName, count)
 	end
 
 	function mod:BrandOfTormentRemoved(args)
-		if self:Me(args.destGUID) then
-			self:StopBar(args.spellId, args.destName)
-		end
 		self:CustomIcon(brandOfTormentMarker, args.destName)
 	end
+end
+
+function mod:Ruinblade(args)
+	ruinbladeCount = ruinbladeCount + 1
+	self:Bar(args.spellId, ruinbladeCount % 4 == 1 and 62.5 or 33)
 end
 
 function mod:RuinbladeApplied(args)
 	local amount = args.amount or 1
 	self:NewStackMessage(args.spellId, "purple", args.destName, amount)
 	self:PlaySound(args.spellId, "alarm")
-	self:Bar(args.spellId, 32)
 end
 
-function mod:CallMawsworn(args)
-	self:Message(args.spellId, "yellow")
-	self:PlaySound(args.spellId, "info")
+
+do
+	local agonizersMarked = 0
+	local agonizersSpawned = 0
+	local agonizerTracker = {}
+	function mod:AgonizerMarking(event, unit, guid)
+		if self:MobId(guid) == 177594 and agonizerTracker[guid] then -- Mawsworn Agonizer
+			agonizersMarked = agonizersMarked + 1
+			self:CustomIcon(agonizerMarker, unit, agonizerTracker[guid]) -- 8, 7, 6, 5
+			if agonizersMarked == 4 then -- All 4 marked
+				self:UnregisterTargetEvents()
+			end
+		end
+	end
+
+	function mod:AgonizerSpawn(args)
+		agonizersSpawned = agonizersSpawned + 1
+		agonizerTracker[args.destGUID] = 9-agonizersSpawned -- icon 8, 7, 6... etc
+	end
+
+	function mod:CallMawsworn(args)
+		self:Message(args.spellId, "yellow", CL.count:format(CL.adds, callMawswornCount))
+		self:PlaySound(args.spellId, "info")
+		callMawswornCount = callMawswornCount + 1
+		self:Bar(args.spellId, timers[args.spellId][callMawswornCount], CL.count:format(CL.adds, callMawswornCount))
+
+		-- Start brand timers
+		brandCount = 1
+		self:Bar(350647, 7.5, CL.count:format(L.brands, brandCount))
+
+		if self:GetOption(agonizerMarker) then
+			agonizersSpawned = 0
+			agonizersMarked = 0
+			agonizerTracker = {}
+			self:RegisterTargetEvents("AgonizerMarking")
+			self:ScheduleTimer("UnregisterTargetEvents", 20)
+		end
+	end
 end
 
 function mod:AgonizingSpike(args)
@@ -175,17 +242,18 @@ do
 end
 
 function mod:Hellscream(args)
-	self:StopBar(350217) -- Torment
-	self:StopBar(350647) -- Brand of Torment
-
-	self:Message(args.spellId, "red")
+	self:Message(args.spellId, "red", L.chains)
 	self:PlaySound(args.spellId, "long")
-	-- self:CastBar(args.spellId, 35, CL.cast:format(args.spellName))
+	self:CastBar(args.spellId, 35, CL.cast:format(L.chains))
+	hellscreamCount = hellscreamCount + 1
+	self:Bar(args.spellId, timers[args.spellId][hellscreamCount], CL.count:format(L.chains, hellscreamCount))
+
+	self:RenderedSoul(5, 1)
 end
 
 function mod:SoulManacles(args)
 	if self:Me(args.destGUID) then
-		self:PersonalMessage(args.spellId)
+		self:PersonalMessage(args.spellId, L.chain)
 		self:PlaySound(args.spellId, "info")
 	end
 end
