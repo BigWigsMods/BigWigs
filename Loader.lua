f30ca027e20bc8f6099a07d3783b9c704d1cac9b
funkydude <funkydude@users.noreply.github.com>
2021-06-01 16:35:34 +0100

    bump version

--- Loader.lua
+++ Loader.lua
@@ -18,7 +18,7 @@ public.isBC = WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
 -- Generate our version variables
 --
 
-local BIGWIGS_VERSION = 25
+local BIGWIGS_VERSION = 26
 local BIGWIGS_RELEASE_STRING, BIGWIGS_VERSION_STRING = "", ""
 local versionQueryString, versionResponseString = "Q^%d^%s^%d^%s", "V^%d^%s^%d^%s"
 local customGuildName = false
