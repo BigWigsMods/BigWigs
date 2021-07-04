1851661556dcfe912c65a662ebfeef44b47baaaf
Kyle Buller <bullerk@gmail.com>
2021-07-04 13:50:38 -0500

    Add Reaction APIs from Classic (#1079)

--- Core/BossPrototype.lua
+++ Core/BossPrototype.lua
@@ -1561,6 +1561,33 @@ do
 	end
 end
 
+do
+	local COMBATLOG_OBJECT_REACTION_HOSTILE = COMBATLOG_OBJECT_REACTION_HOSTILE
+	local COMBATLOG_OBJECT_REACTION_FRIENDLY = COMBATLOG_OBJECT_REACTION_FRIENDLY
+	local COMBATLOG_OBJECT_TYPE_PLAYER = COMBATLOG_OBJECT_TYPE_PLAYER
+
+	--- Check if the unit is hostile.
+	-- @string flags unit bit flags
+	-- @return boolean if the unit is hostile
+	function boss:Hostile(flags)
+		return band(flags, COMBATLOG_OBJECT_REACTION_HOSTILE) == COMBATLOG_OBJECT_REACTION_HOSTILE
+	end
+
+	--- Check if the unit is friendly.
+	-- @string flags unit bit flags
+	-- @return boolean if the unit is friendly
+	function boss:Friendly(flags)
+		return band(flags, COMBATLOG_OBJECT_REACTION_FRIENDLY) == COMBATLOG_OBJECT_REACTION_FRIENDLY
+	end
+
+	--- Check if the unit is a player.
+	-- @string flags unit bit flags
+	-- @return boolean if the unit is a player
+	function boss:Player(flags)
+		return band(flags, COMBATLOG_OBJECT_TYPE_PLAYER) == COMBATLOG_OBJECT_TYPE_PLAYER
+	end
+end
+
 -------------------------------------------------------------------------------
 -- Option flag check
 -- @section toggles
