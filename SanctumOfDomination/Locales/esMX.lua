1ddc033b1037b8a60470703770fcfa551cbee561
Justwait <mngrootwow@gmail.com>
2021-07-05 00:30:51 +0200

    SanctumOfDomination/Locales: Add locale files

---
+++ SanctumOfDomination/Locales/esMX.lua
@@ -0,0 +1,136 @@
+local L = BigWigs:NewBossLocale("The Tarragrue", "esMX")
+if not L then return end
+if L then
+	--L.chains = "Chains" -- Chains of Eternity (Chains)
+	--L.remnants = "Remnants" -- Remnant of Forgotten Torments (Remnants)
+
+	--L.physical_remnant = "Physical Remnant"
+	--L.magic_remnant = "Magic Remnant"
+	--L.fire_remnant = "Fire Remnant"
+	--L.fire = "Fire"
+	--L.magic = "Magic"
+	--L.physical = "Physical"
+end
+
+L = BigWigs:NewBossLocale("The Eye of the Jailer", "esMX")
+if L then
+	--L.chains = "Chains" -- Short for Dragging Chains
+	--L.pool = "Pool" -- Spreading Misery
+	--L.pools = "Pools" -- Spreading Misery (multiple)
+	--L.death_gaze = "Death Gaze" -- Short for Titanic Death Gaze
+end
+
+L = BigWigs:NewBossLocale("The Nine", "esMX")
+if L then
+	--L.fragments = "Fragments" -- Short for Fragments of Destiny
+	--L.fragment = "Fragment" -- Singular Fragment of Destiny
+	--L.run_away = "Run Away" -- Wings of Rage
+	--L.song = "Song" -- Short for Song of Dissolution
+	--L.go_in = "Go in" -- Reverberating Refrain
+	--L.valkyr = "Val'kyr" -- Short for Call of the Val'kyr
+	--L.blades = "Blades" -- Agatha's Eternal Blade
+	--L.big_bombs = "Big Bombs" -- Daschla's Mighty Impact
+	--L.big_bomb = "Big Bomb" -- Attached to the countdown
+	--L.shield = "Shield" -- Annhylde's Bright Aegis
+	--L.soaks = "Soaks" -- Aradne's Falling Strike
+	--L.small_bombs = "Small Bombs" -- Brynja's Mournful Dirge
+	--L.recall = "Recall" -- Short for Word of Recall
+
+	--L.blades_yell = "Fall before my blade!"
+	--L.soaks_yell = "You are all outmatched!"
+	--L.shield_yell = "My shield never falters!"
+
+	--L.berserk_stage1 = "Berserk Stage 1"
+	--L.berserk_stage2 = "Berserk Stage 2"
+
+	--L.image_special = "%s [Skyja]" -- Stage 2 boss name
+end
+
+L = BigWigs:NewBossLocale("Remnant of Ner'zhul", "esMX")
+if L then
+	-- L.cones = "Cones" -- Grasp of Malice
+	-- L.orbs = "Orbs" -- Orb of Torment
+	-- L.orb = "Orb" -- Orb of Torment
+end
+
+L = BigWigs:NewBossLocale("Soulrender Dormazain", "esMX")
+if L then
+	-- L.custom_off_nameplate_defiance = "Defiance Nameplate Icon"
+	-- L.custom_off_nameplate_defiance_desc = "Show an icon on the nameplate of Mawsworn that have Defiance.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+
+	-- L.custom_off_nameplate_tormented = "Tormented Nameplate Icon"
+	-- L.custom_off_nameplate_tormented_desc = "Show an icon on the nameplate of Mawsworn that have Tormented.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+
+	-- L.cones = "Cones" -- Torment
+	-- L.dance = "Dance" -- Encore of Torment
+	-- L.brands = "Brands" -- Brand of Torment
+	-- L.brand = "Brand" -- Single Brand of Torment
+	-- L.spike = "Spike" -- Short for Agonizing Spike
+	-- L.chains = "Chains" -- Hellscream
+	-- L.chain = "Chain" -- Soul Manacles
+	-- L.souls = "Souls" -- Rendered Soul
+
+	-- L.chains_remaining = "%d Chains remaining"
+	-- L.all_broken = "All Chains broken"
+end
+
+L = BigWigs:NewBossLocale("Painsmith Raznal", "esMX")
+if L then
+	-- L.hammer = "Hammer" -- Short for Rippling Hammer
+	-- L.axe = "Axe" -- Short for Cruciform Axe
+	-- L.scythe = "Scythe" -- Short for Dualblade Scythe
+	-- L.trap = "Trap" -- Short for Flameclasp Trap
+	-- L.chains = "Chains" -- Short for Shadowsteel Chains
+	-- L.ember = "Ember" -- Short for Shadowsteel Ember
+end
+
+L = BigWigs:NewBossLocale("Guardian of the First Ones", "esMX")
+if L then
+	--L.custom_on_stop_timers = "Always show ability bars"
+	--L.custom_on_stop_timers_desc = "The Guardian can delay its abilities. When this option is enabled, the bars for those abilities will stay on your screen."
+end
+
+L = BigWigs:NewBossLocale("Fatescribe Roh-Kalo", "esMX")
+if L then
+	-- L.rings = "Rings"
+	-- L.rings_active = "Rings Active" -- for when they activate/are movable
+	-- L.runes = "Runes"
+
+	-- L.grimportent_countdown = "Countdown"
+	-- L.grimportent_countdown_desc = "Countdown for the players who are Affected by Grim Portent"
+	-- L.grimportent_countdown_bartext = "Go to rune!"
+end
+
+L = BigWigs:NewBossLocale("Kel'Thuzad", "esMX")
+if L then
+	-- L.spikes = "Spikes" -- Short for Glacial Spikes
+	-- L.spike = "Spike"
+	-- L.miasma = "Miasma" -- Short for Sinister Miasma
+
+	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
+	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate of Frostbound Devoted that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+end
+
+L = BigWigs:NewBossLocale("Sylvanas Windrunner", "esMX")
+if L then
+	-- L.chains_active = "Chains Active"
+	-- L.chains_active_desc = "Show a bar for when the Chains of Domination activate"
+	-- L.chains_active_bartext = "%d Active" -- Chains Active
+
+	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
+	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate of Dark Sentinels that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+
+	-- L.chains = "Chains" -- Short for Domination Chains
+	-- L.chain = "Chain" -- Single Domination Chain
+	-- L.darkness = "Darkness" -- Short for Veil of Darkness
+	-- L.arrow = "Arrow" -- Short for Wailing Arrow
+	-- L.wave = "Wave" -- Short for Haunting Wave
+	-- L.dread = "Dread" -- Short for Crushing Dread
+	-- L.orbs = "Orbs" -- Dark Communion
+	-- L.curse = "Curse" -- Short for Curse of Lethargy
+	-- L.pools = "Pools" -- Banshee's Bane
+	-- L.scream = "Scream" -- Banshee Scream
+
+	-- L.knife_fling = "Knives out!" -- "Death-touched blades fling out"
+end
+
