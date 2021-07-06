360dcb9c076e57ef1957978e160d70d484aa47db
EK <eke00372@gmail.com>
2021-07-06 22:34:16 +0800

    Update zhTW.lua

--- SanctumOfDomination/Locales/zhTW.lua
+++ SanctumOfDomination/Locales/zhTW.lua
@@ -4,7 +4,7 @@ if L then
 	L.chains = "鎖鏈" -- Chains of Eternity (Chains)
 	L.remnants = "殘留" -- Remnant of Forgotten Torments (Remnants)
 
-	L.physical_remnant = "物理殘留" -- 待驗，可以改成更簡單的「物理球/魔法球/火焰球」
+	L.physical_remnant = "物理殘留" -- 待定，可以改成更簡單的「物理球/魔法球/火焰球」
 	L.magic_remnant = "魔法殘留"
 	L.fire_remnant = "火焰殘留"
 	L.fire = "火焰"
@@ -22,19 +22,19 @@ end
 
 L = BigWigs:NewBossLocale("The Nine", "zhTW")
 if L then
-	--L.fragments = "Fragments" -- Short for Fragments of Destiny
-	--L.fragment = "Fragment" -- Singular Fragment of Destiny
+	L.fragments = "碎片" -- Short for Fragments of Destiny
+	L.fragment = "碎片" -- Singular Fragment of Destiny
 	L.run_away = "跑開" -- Wings of Rage
-	--L.song = "Song" -- Short for Song of Dissolution
+	L.song = "唱歌" -- Short for Song of Dissolution
 	L.go_in = "靠近" -- Reverberating Refrain
 	L.valkyr = "華爾琪" -- Short for Call of the Val'kyr
 	L.blades = "飛刀" -- Agatha's Eternal Blade
-	--L.big_bombs = "Big Bombs" -- Daschla's Mighty Impact
-	--L.big_bomb = "Big Bomb" -- Attached to the countdown
-	L.shield = "減傷盾" -- Annhylde's Bright Aegis
-	L.soaks = "接圈" -- Aradne's Falling Strike
-	L.small_bombs = "小炸彈" -- Brynja's Mournful Dirge
-	--L.recall = "Recall" -- Short for Word of Recall
+	L.big_bombs = "大炸彈" -- Daschla's Mighty Impact; 待定
+	L.big_bomb = "大炸彈" -- Attached to the countdown; 待定
+	L.shield = "減傷盾" -- Annhylde's Bright Aegis; 待定
+	L.soaks = "接圈" -- Aradne's Falling Strike; 待定，原文是soak，這是習慣用語差異
+	L.small_bombs = "小炸彈" -- Brynja's Mournful Dirge; 待定
+	L.recall = "撤回" -- Short for Word of Recall; 待定，召回、撤回、或者取消？
 
 	--L.blades_yell = "Fall before my blade!"
 	--L.soaks_yell = "You are all outmatched!"
@@ -55,22 +55,22 @@ end
 
 L = BigWigs:NewBossLocale("Soulrender Dormazain", "zhTW")
 if L then
-	-- L.custom_off_nameplate_defiance = "Defiance Nameplate Icon"
-	-- L.custom_off_nameplate_defiance_desc = "Show an icon on the nameplate of Mawsworn that have Defiance.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
-
-	-- L.custom_off_nameplate_tormented = "Tormented Nameplate Icon"
-	-- L.custom_off_nameplate_tormented_desc = "Show an icon on the nameplate of Mawsworn that have Tormented.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	L.custom_off_nameplate_defiance = "反抗名條圖示"
+	L.custom_off_nameplate_defiance_desc = "在獲得反抗效果強化的小怪名條上顯示圖示。\n\n需要開啟敵方名條，並使用支援此功能的名條插件（如KuiNameplates、Plater）。"
+	
+	L.custom_off_nameplate_tormented = "烙印名條圖示"
+	L.custom_off_nameplate_tormented_desc = "在受到折磨烙印影響的小怪名條上顯示圖示。\n\n需要開啟敵方名條，並使用支援此功能的名條插件（如KuiNameplates、Plater）。"
 
 	-- L.cones = "Cones" -- Torment
-	-- L.dance = "跳舞" -- Encore of Torment
-	-- L.brands = "Brands" -- Brand of Torment
-	-- L.brand = "Brand" -- Single Brand of Torment
+	L.dance = "跳舞" -- Encore of Torment
+	L.brands = "烙印" -- Brand of Torment
+	L.brand = "烙印" -- Single Brand of Torment
 	-- L.spike = "Spike" -- Short for Agonizing Spike
-	-- L.chains = "鎖鏈" -- Hellscream
-	-- L.chain = "鎖鏈" -- Soul Manacles
-	-- L.souls = "Souls" -- Rendered Soul
+	L.chains = "鎖鏈" -- Hellscream
+	L.chain = "鎖鏈" -- Soul Manacles
+	L.souls = "靈魂" -- Rendered Soul
 
-	-- L.chains_remaining = "%d Chains remaining"
+	L.chains_remaining = "還剩 %d 條鎖鏈"
 	L.all_broken = "鎖鏈已全部拉斷"
 end
 
@@ -86,8 +86,8 @@ end
 
 L = BigWigs:NewBossLocale("Guardian of the First Ones", "zhTW")
 if L then
-	--L.custom_on_stop_timers = "Always show ability bars"
-	--L.custom_on_stop_timers_desc = "The Guardian can delay its abilities. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "總是顯示計時器"
+	L.custom_on_stop_timers_desc = "守護者的技能可能延遲施放。啟用此選項後，這些技能的計時條會保持顯示。"
 end
 
 L = BigWigs:NewBossLocale("Fatescribe Roh-Kalo", "zhTW")
