943665c84ace34f201b7abc374de08d85ccd5798
dongminrocks <39166823+dongminrocks@users.noreply.github.com>
2021-07-07 09:34:19 -0400

    Update koKR.lua
    added translations for SoD. going to revise depending on what I see from the raid/watching korean twitch

--- SanctumOfDomination/Locales/koKR.lua
+++ SanctumOfDomination/Locales/koKR.lua
@@ -1,136 +1,136 @@
 local L = BigWigs:NewBossLocale("The Tarragrue", "koKR")
 if not L then return end
 if L then
-	--L.chains = "Chains" -- Chains of Eternity (Chains)
-	--L.remnants = "Remnants" -- Remnant of Forgotten Torments (Remnants)
+	L.chains = "사슬" -- Chains of Eternity (Chains)
+	L.remnants = "잔재" -- Remnant of Forgotten Torments (Remnants)
 
-	--L.physical_remnant = "Physical Remnant"
-	--L.magic_remnant = "Magic Remnant"
-	--L.fire_remnant = "Fire Remnant"
-	--L.fire = "Fire"
-	--L.magic = "Magic"
-	--L.physical = "Physical"
+	L.physical_remnant = "물리 잔재"
+	L.magic_remnant = "마법 잔재"
+	L.fire_remnant = "화염 잔재"
+	L.fire = "화염"
+	L.magic = "마법"
+	L.physical = "물리"
 end
 
 L = BigWigs:NewBossLocale("The Eye of the Jailer", "koKR")
 if L then
-	--L.chains = "Chains" -- Short for Dragging Chains
-	--L.pool = "Pool" -- Spreading Misery
-	--L.pools = "Pools" -- Spreading Misery (multiple)
-	--L.death_gaze = "Death Gaze" -- Short for Titanic Death Gaze
+	L.chains = "사슬" -- Short for Dragging Chains
+	L.pool = "바닥 깔기" -- Spreading Misery
+	L.pools = "바닥 깔기" -- Spreading Misery (multiple)
+	L.death_gaze = "죽음의 시선" -- Short for Titanic Death Gaze
 end
 
 L = BigWigs:NewBossLocale("The Nine", "koKR")
 if L then
-	--L.fragments = "Fragments" -- Short for Fragments of Destiny
-	--L.fragment = "Fragment" -- Singular Fragment of Destiny
-	--L.run_away = "Run Away" -- Wings of Rage
-	--L.song = "Song" -- Short for Song of Dissolution
-	--L.go_in = "Go in" -- Reverberating Refrain
-	--L.valkyr = "Val'kyr" -- Short for Call of the Val'kyr
-	--L.blades = "Blades" -- Agatha's Eternal Blade
-	--L.big_bombs = "Big Bombs" -- Daschla's Mighty Impact
-	--L.big_bomb = "Big Bomb" -- Attached to the countdown
-	--L.shield = "Shield" -- Annhylde's Bright Aegis
-	--L.soaks = "Soaks" -- Aradne's Falling Strike
-	--L.small_bombs = "Small Bombs" -- Brynja's Mournful Dirge
-	--L.recall = "Recall" -- Short for Word of Recall
+	L.fragments = "파편" -- Short for Fragments of Destiny
+	L.fragment = "파편" -- Singular Fragment of Destiny
+	L.run_away = "카이라에서 멀어지기!" -- Wings of Rage
+	L.song = "노래" -- Short for Song of Dissolution
+	L.go_in = "시그니로 접근!" -- Reverberating Refrain
+	L.valkyr = "발키르" -- Short for Call of the Val'kyr
+	L.blades = "칼날" -- Agatha's Eternal Blade
+	L.big_bombs = "쎈 폭탄" -- Daschla's Mighty Impact
+	L.big_bomb = "쎈 폭탄" -- Attached to the countdown
+	L.shield = "방패" -- Annhylde's Bright Aegis
+	L.soaks = "바닥 맞기" -- Aradne's Falling Strike
+	L.small_bombs = "약한 폭탄" -- Brynja's Mournful Dirge
+	L.recall = "진언" -- Short for Word of Recall
 
-	--L.blades_yell = "Fall before my blade!"
-	--L.soaks_yell = "You are all outmatched!"
-	--L.shield_yell = "My shield never falters!"
+	L.blades_yell = "Fall before my blade!"
+	L.soaks_yell = "You are all outmatched!"
+	L.shield_yell = "My shield never falters!"
 
-	--L.berserk_stage1 = "Berserk Stage 1"
-	--L.berserk_stage2 = "Berserk Stage 2"
+	L.berserk_stage1 = "1페이즈 광폭화"
+	L.berserk_stage2 = "2페이즈 광폭화"
 
-	--L.image_special = "%s [Skyja]" -- Stage 2 boss name
+	L.image_special = "%s [스키야]" -- Stage 2 boss name
 end
 
 L = BigWigs:NewBossLocale("Remnant of Ner'zhul", "koKR")
 if L then
-	-- L.cones = "Cones" -- Grasp of Malice
-	-- L.orbs = "Orbs" -- Orb of Torment
-	-- L.orb = "Orb" -- Orb of Torment
+	 L.cones = "바닥" -- Grasp of Malice
+	 L.orbs = "보주" -- Orb of Torment
+	 L.orb = "보주" -- Orb of Torment
 end
 
 L = BigWigs:NewBossLocale("Soulrender Dormazain", "koKR")
 if L then
-	-- L.custom_off_nameplate_defiance = "Defiance Nameplate Icon"
-	-- L.custom_off_nameplate_defiance_desc = "Show an icon on the nameplate of Mawsworn that have Defiance.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	 L.custom_off_nameplate_defiance = "저항 네임플레이트 아이콘"
+	 L.custom_off_nameplate_defiance_desc = "나락살이의 네임플레이트에 저항 아이콘을 표시합니다.\n\n네임플레이트 애드온과 적 네임플레이트 활성화 필요(KuiNameplates, Plater)."
 
-	-- L.custom_off_nameplate_tormented = "Tormented Nameplate Icon"
-	-- L.custom_off_nameplate_tormented_desc = "Show an icon on the nameplate of Mawsworn that have Tormented.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	 L.custom_off_nameplate_tormented = "고문 네임플레이트 아이콘"
+	 L.custom_off_nameplate_tormented_desc = "나락살이의 네임플레이트에 고문 아이콘을 표시합니다.\n\n네임플레이트 애드온과 적 네임플레이트 활성화 필요 (KuiNameplates, Plater)."
 
-	-- L.cones = "Cones" -- Torment
-	-- L.dance = "Dance" -- Encore of Torment
-	-- L.brands = "Brands" -- Brand of Torment
-	-- L.brand = "Brand" -- Single Brand of Torment
-	-- L.spike = "Spike" -- Short for Agonizing Spike
-	-- L.chains = "Chains" -- Hellscream
-	-- L.chain = "Chain" -- Soul Manacles
-	-- L.souls = "Souls" -- Rendered Soul
+	 L.cones = "바닥" -- Torment
+	 L.dance = "바닥" -- Encore of Torment
+	 L.brands = "낙인" -- Brand of Torment
+	 L.brand = "낙인" -- Single Brand of Torment
+	 L.spike = "쐐기" -- Short for Agonizing Spike
+	 L.chains = "족쇄" -- Hellscream
+	 L.chain = "수갑" -- Soul Manacles
+	 L.souls = "영혼" -- Rendered Soul
 
-	-- L.chains_remaining = "%d Chains remaining"
-	-- L.all_broken = "All Chains broken"
+	 L.chains_remaining = "족쇄 %d 개 남음"
+	 L.all_broken = "족쇄 올 클리어"
 end
 
 L = BigWigs:NewBossLocale("Painsmith Raznal", "koKR")
 if L then
-	-- L.hammer = "Hammer" -- Short for Rippling Hammer
-	-- L.axe = "Axe" -- Short for Cruciform Axe
-	-- L.scythe = "Scythe" -- Short for Dualblade Scythe
-	-- L.trap = "Trap" -- Short for Flameclasp Trap
-	-- L.chains = "Chains" -- Short for Shadowsteel Chains
-	-- L.ember = "Ember" -- Short for Shadowsteel Ember
+	 L.hammer = "망치" -- Short for Rippling Hammer
+	 L.axe = "도끼" -- Short for Cruciform Axe
+	 L.scythe = "낫" -- Short for Dualblade Scythe
+	 L.trap = "덫" -- Short for Flameclasp Trap
+	 L.chains = "사슬" -- Short for Shadowsteel Chains
+	 L.ember = "불씨" -- Short for Shadowsteel Ember
 end
 
 L = BigWigs:NewBossLocale("Guardian of the First Ones", "koKR")
 if L then
-	--L.custom_on_stop_timers = "Always show ability bars"
-	--L.custom_on_stop_timers_desc = "The Guardian can delay its abilities. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "능력 바 항상 표시"
+	L.custom_on_stop_timers_desc = "수호자의 능력이 미뤄질수 있습니다. 이 옵션을 킨다면, 그 능력들을 표시하는 바가 화면에 남아있습니다."
 end
 
 L = BigWigs:NewBossLocale("Fatescribe Roh-Kalo", "koKR")
 if L then
-	-- L.rings = "Rings"
-	-- L.rings_active = "Rings Active" -- for when they activate/are movable
-	-- L.runes = "Runes"
+	 L.rings = "굴레"
+	 L.rings_active = "굴레 활성화" -- for when they activate/are movable
+	 L.runes = "룬"
 
-	-- L.grimportent_countdown = "Countdown"
-	-- L.grimportent_countdown_desc = "Countdown for the players who are Affected by Grim Portent"
-	-- L.grimportent_countdown_bartext = "Go to rune!"
+	 L.grimportent_countdown = "초읽기"
+	 L.grimportent_countdown_desc = "음산한 징조에 걸린 플레이어들을 위한 초읽기"
+	 L.grimportent_countdown_bartext = "룬으로 이동!"
 end
 
 L = BigWigs:NewBossLocale("Kel'Thuzad", "koKR")
 if L then
-	-- L.spikes = "Spikes" -- Short for Glacial Spikes
-	-- L.spike = "Spike"
-	-- L.miasma = "Miasma" -- Short for Sinister Miasma
+	 L.spikes = "쐐기" -- Short for Glacial Spikes
+	 L.spike = "쐐기"
+	 L.miasma = "독기" -- Short for Sinister Miasma
 
-	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
-	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate of Frostbound Devoted that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	 L.custom_on_nameplate_fixate = "시선집중 네임플레이트 아이콘"
+	 L.custom_on_nameplate_fixate_desc = "헌신자의 네임플레이트에 시선집중 아이콘을 표시합니다.\n\n네임플레이트 애드온과 적 네임플레이트 활성화 필요(KuiNameplates, Plater)."
 end
 
 L = BigWigs:NewBossLocale("Sylvanas Windrunner", "koKR")
 if L then
-	-- L.chains_active = "Chains Active"
-	-- L.chains_active_desc = "Show a bar for when the Chains of Domination activate"
-	-- L.chains_active_bartext = "%d Active" -- Chains Active
+	 L.chains_active = "사슬 활성화"
+	 L.chains_active_desc = "지배의 사슬이 활성화될때를 보여주는 바 표시"
+	 L.chains_active_bartext = "%d 개 활성화" -- Chains Active
 
-	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
+	-- L.custom_on_nameplate_fixate = "시선집중 네임플레이트 아이콘"
 	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate of Dark Sentinels that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
 
-	-- L.chains = "Chains" -- Short for Domination Chains
-	-- L.chain = "Chain" -- Single Domination Chain
-	-- L.darkness = "Darkness" -- Short for Veil of Darkness
-	-- L.arrow = "Arrow" -- Short for Wailing Arrow
-	-- L.wave = "Wave" -- Short for Haunting Wave
-	-- L.dread = "Dread" -- Short for Crushing Dread
-	-- L.orbs = "Orbs" -- Dark Communion
-	-- L.curse = "Curse" -- Short for Curse of Lethargy
-	-- L.pools = "Pools" -- Banshee's Bane
-	-- L.scream = "Scream" -- Banshee Scream
+	 L.chains = "사슬" -- Short for Domination Chains
+	 L.chain = "사슬" -- Single Domination Chain
+	 L.darkness = "장막" -- Short for Veil of Darkness
+	 L.arrow = "화살" -- Short for Wailing Arrow
+	 L.wave = "파도" -- Short for Haunting Wave
+	 L.dread = "공포" -- Short for Crushing Dread
+	 L.orbs = "보주" -- Dark Communion
+	 L.curse = "저주" -- Short for Curse of Lethargy
+	 L.pools = "파멸" -- Banshee's Bane
+	 L.scream = "비명" -- Banshee Scream
 
-	-- L.knife_fling = "Knives out!" -- "Death-touched blades fling out"
+	 L.knife_fling = "비수 조심!" -- "Death-touched blades fling out"
 end
 
