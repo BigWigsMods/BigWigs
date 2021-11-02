415538969691588584cd8d901441219e295ea963
Kyle Buller <bullerk@gmail.com>
2021-11-02 17:02:30 -0500

    Bump version

--- Loader.lua
+++ Loader.lua
@@ -20,7 +20,7 @@ public.isBC = WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
 -- Generate our version variables
 --
 
-local BIGWIGS_VERSION = 31
+local BIGWIGS_VERSION = 32
 local BIGWIGS_RELEASE_STRING, BIGWIGS_VERSION_STRING = "", ""
 local versionQueryString, versionResponseString = "Q^%d^%s^%d^%s", "V^%d^%s^%d^%s"
 local customGuildName = false
@@ -967,14 +967,13 @@ do
 	local DBMdotDisplayVersion   -- "N.N.N" for a release and "N.N.N alpha" for the alpha duration.
 	local DBMdotReleaseRevision  -- Hardcoded time, manually changed every release, they use it to track the highest release version, a new DBM release is the only time it will change.
 	if public.isBC then
-		DBMdotRevision = "20210914220353"
-		DBMdotDisplayVersion = "2.5.12"
-		DBMdotReleaseRevision = "20210914000000"
+		DBMdotDisplayVersion = "2.5.19"
+		DBMdotReleaseRevision = "20211102000000"
 	else
-		DBMdotRevision = "20210831173812"
-		DBMdotDisplayVersion = "1.13.78"
-		DBMdotReleaseRevision = "20210831000000"
+		DBMdotDisplayVersion = "1.14.3"
+		DBMdotReleaseRevision = "20211102000000"
 	end
+	DBMdotRevision = DBMdotReleaseRevision
 
 	local timer, prevUpgradedUser = nil, nil
 	local function sendMsg()
