7b1033db37d5014558011b183a127bb7613a6623
Justwait <mngrootwow@gmail.com>
2021-12-09 01:22:54 +0100

    SepulcherOfTheFirstOnes/Dausegne: Begin work after testing

--- SepulcherOfTheFirstOnes/Dausegne.lua
+++ SepulcherOfTheFirstOnes/Dausegne.lua
@@ -5,7 +5,7 @@ if not IsTestBuild() then return end
 
 local mod, CL = BigWigs:NewBoss("Dausegne, the Fallen Oracle", 2481, 2459)
 if not mod then return end
-mod:RegisterEnableMob(184529) -- Dausegne - IGC <The Fallen Oracle> XXX Might be wrong? no other npc on wowhead.
+mod:RegisterEnableMob(181224) -- Dausegne
 mod:SetEncounterID(2540)
 mod:SetRespawnTime(30)
 
@@ -18,6 +18,7 @@ local coreCount = 1
 local arcCount = 1
 local haloCount = 1
 local siphonCount = 1
+local nextSiphon = 0
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -65,15 +66,14 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "InfusedStrikesApplied", 361966)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "InfusedStrikesApplied", 361966)
 	self:Log("SPELL_AURA_REMOVED", "InfusedStrikesRemoved", 361966)
-	--self:Log("SPELL_CAST_SUCCESS", "StaggeringBarrage", 360959)
+	self:Log("SPELL_CAST_START", "StaggeringBarrage", 360960)
 	self:Log("SPELL_AURA_APPLIED", "StaggeringBarrageApplied", 361018)
 	self:Log("SPELL_AURA_REMOVED", "StaggeringBarrageRemoved", 361018)
 	self:Log("SPELL_CAST_START", "DominationCore", 359483)
 	self:Log("SPELL_CAST_START", "DominationBolt", 363607)
 	self:Log("SPELL_CAST_START", "ObliterationArc", 361513)
-	self:Log("SPELL_CAST_SUCCESS", "DisintegrationHalo", 363200) -- XXX !!Likely wrong trigger!!
-	self:Log("SPELL_CAST_START", "SiphonReservoir", 361643)
-	--self:Log("SPELL_AURA_APPLIED", "SiphonedBarrierApplied", 361651)
+	--self:Log("SPELL_CAST_SUCCESS", "DisintegrationHalo", 363200) -- XXX Emote
+	self:Log("SPELL_AURA_APPLIED", "SiphonReservoir", 361643)
 	self:Log("SPELL_AURA_REMOVED", "SiphonedBarrierRemoved", 361651)
 	self:Log("SPELL_CAST_START", "TotalDominion", 365418)
 
@@ -88,12 +88,13 @@ function mod:OnEngage()
 	arcCount = 1
 	haloCount = 1
 	siphonCount = 1
+	nextSiphon = GetTime() + 72.5
 
-	-- self:Bar(361018, 10, CL.count:format(L.staggering_barrage, barrageCount)) -- Staggering Barrage
-	-- self:Bar(359483, 10, CL.count:format(L.domination_core, coreCount)) -- Domination Core
-	-- self:Bar(361513, 10, CL.count:format(L.obliteration_arc, arcCount)) -- Obliteration Arc
-	-- self:Bar(363200, 10, CL.count:format(L.disintergration_halo, haloCount)) -- Disintegration Halo
-	-- self:Bar(361643, 10, CL.count:format(L.siphon_reservoir, siphonCount)) -- Siphon Reservoir
+	self:Bar(363200, 5, CL.count:format(L.disintergration_halo, haloCount)) -- Disintegration Halo
+	self:Bar(359483, 6.5, CL.count:format(L.domination_core, coreCount)) -- Domination Core
+	self:Bar(361513, 15, CL.count:format(L.obliteration_arc, arcCount)) -- Obliteration Arc
+	self:Bar(361018, 29, CL.count:format(L.staggering_barrage, barrageCount)) -- Staggering Barrage
+	self:Bar(361643, 72.5, CL.count:format(L.siphon_reservoir, siphonCount)) -- Siphon Reservoir
 end
 
 --------------------------------------------------------------------------------
@@ -117,18 +118,22 @@ function mod:InfusedStrikesRemoved(args)
 	self:StopBar(args.spellId, args.destName)
 end
 
+
+
+-- XXX Make it single target
 do
 	local playerList = {}
-	local prev = 0
-	function mod:StaggeringBarrageApplied(args)
-		local t = args.time
-		if t-prev > 5 then
-			prev = t
-			playerList = {}
-			-- self:StopBar(CL.count:format(L.staggering_barrage, barrageCount))
-			barrageCount = barrageCount + 1
-			-- self:Bar(args.spellId, 10, CL.count:format(L.staggering_barrage, barrageCount))
+	function mod:StaggeringBarrage(args)
+		self:StopBar(CL.count:format(L.staggering_barrage, barrageCount))
+		playerList = {}
+		barrageCount = barrageCount + 1
+		local cd = 35
+		if barrageCount <= 3 and nextSiphon > GetTime()+cd then -- 3 per rotation, except first
+			self:Bar(361018, 35, CL.count:format(L.staggering_barrage, barrageCount))
 		end
+	end
+
+	function mod:StaggeringBarrageApplied(args)
 		local count = #playerList+1
 		playerList[count] = args.destName
 		playerList[args.destName] = count -- Set raid marker
@@ -155,7 +160,10 @@ function mod:DominationCore(args)
 	self:Message(args.spellId, "yellow", CL.incoming:format(CL.count:format(L.domination_core, coreCount)))
 	self:PlaySound(args.spellId, "long")
 	coreCount = coreCount + 1
-	-- self:Bar(args.spellId, 10, CL.count:format(L.domination_core, coreCount))
+	local cd = coreCount == 2 and 33.5 or 35.5
+	if coreCount <= 3 and nextSiphon > GetTime()+cd then -- 3 per rotation, except first
+		self:Bar(args.spellId, cd, CL.count:format(L.domination_core, coreCount))
+	end
 end
 
 function mod:DominationBolt(args)
@@ -172,38 +180,41 @@ function mod:ObliterationArc(args)
 	self:Message(args.spellId, "yellow", CL.count:format(L.obliteration_arc, arcCount))
 	self:PlaySound(args.spellId, "alert")
 	arcCount = arcCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(L.obliteration_arc, arcCount))
+	local cd = 35
+	if arcCount <= 3 and nextSiphon > GetTime()+cd then -- 3 per rotation, except first
+		self:Bar(args.spellId, 35, CL.count:format(L.obliteration_arc, arcCount))
+	end
 end
 
-function mod:DisintegrationHalo(args)
-	self:Message(args.spellId, "orange", CL.incoming:format(CL.count:format(L.disintergration_halo, arcCount)))
-	self:PlaySound(args.spellId, "long")
-	haloCount = haloCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(L.disintergration_halo, haloCount))
-end
+-- EMOTE only
+-- "Disintegration Halo-365373-npc:Dausegne = pull:5.0, 91.1, 70.1, 55.9, 70.0", -- [1]
+-- function mod:DisintegrationHalo(args)
+-- 	self:Message(args.spellId, "orange", CL.incoming:format(CL.count:format(L.disintergration_halo, arcCount)))
+-- 	self:PlaySound(args.spellId, "long")
+-- 	haloCount = haloCount + 1
+-- 	--self:Bar(args.spellId, 20, CL.count:format(L.disintergration_halo, haloCount))
+-- end
 
 function mod:SiphonReservoir(args)
 	self:Message(args.spellId, "cyan", CL.count:format(L.siphon_reservoir, siphonCount))
 	self:PlaySound(args.spellId, "info")
 	siphonCount = siphonCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(L.disintergration_halo, siphonCount))
 end
 
 function mod:SiphonedBarrierRemoved(args)
 	self:Message(args.spellId, "green", CL.removed:format(args.spellName))
 	self:PlaySound(args.spellId, "info")
-	-- XXX Do we reset here? Back to "stage 1"?
-	-- barrageCount = 1
-	-- coreCount = 1
-	-- arcCount = 1
-	-- haloCount = 1
-	-- siphonCount = 1
+	barrageCount = 1
+	coreCount = 1
+	arcCount = 1
+	haloCount = 1
+	siphonCount = 1
 
-	-- self:Bar(361018, 10, CL.count:format(L.staggering_barrage, barrageCount)) -- Staggering Barrage
-	-- self:Bar(359483, 10, CL.count:format(L.domination_core, coreCount)) -- Domination Core
-	-- self:Bar(361513, 10, CL.count:format(L.obliteration_arc, arcCount)) -- Obliteration Arc
-	-- self:Bar(363200, 10, CL.count:format(L.disintergration_halo, haloCount)) -- Disintegration Halo
-	-- self:Bar(361643, 10, CL.count:format(L.siphon_reservoir, siphonCount)) -- Siphon Reservoir
+	self:Bar(363200, 6.5, CL.count:format(L.disintergration_halo, haloCount)) -- Disintegration Halo
+	self:Bar(359483, 8, CL.count:format(L.domination_core, coreCount)) -- Domination Core
+	self:Bar(361513, 16.5, CL.count:format(L.obliteration_arc, arcCount)) -- Obliteration Arc
+	self:Bar(361018, 30.5, CL.count:format(L.staggering_barrage, barrageCount)) -- Staggering Barrage
+	self:Bar(361643, 110.5, CL.count:format(L.siphon_reservoir, siphonCount)) -- Siphon Reservoir
 end
 
 function mod:TotalDominion(args)
