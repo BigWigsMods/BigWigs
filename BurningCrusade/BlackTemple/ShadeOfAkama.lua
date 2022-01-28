b4e83551c91849d1f0dc82a560492253dc147eed
funkydude <funkydude@users.noreply.github.com>
2022-01-28 20:00:02 +0000

    BlackTemple/ShadeOfAkama: Disable module, this was made for retail and would need properly tested on BCC where the IEEU event won't exist.

--- BurningCrusade/BlackTemple/ShadeOfAkama.lua
+++ BurningCrusade/BlackTemple/ShadeOfAkama.lua
@@ -43,21 +43,22 @@ function mod:GetOptions()
 end
 
 function mod:VerifyEnable(unit, mobId)
-	if mobId == 22841 or UnitHealth(unit) == UnitHealthMax(unit) then -- Enable if shade, or if Akama at 100% hp
-		return true
-	end
+	return false
+	--if mobId == 22841 or UnitHealth(unit) == UnitHealthMax(unit) then -- Enable if shade, or if Akama at 100% hp
+	--	return true
+	--end
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_AURA_REMOVED", "StealthRemoved", 34189)
-	self:RegisterMessage("BigWigs_BossComm")
+	--self:Log("SPELL_AURA_REMOVED", "StealthRemoved", 34189)
+	--self:RegisterMessage("BigWigs_BossComm")
 
-	self:Log("SPELL_AURA_APPLIED", "RainOfFireDamage", 42023)
-	self:Log("SPELL_PERIODIC_DAMAGE", "RainOfFireDamage", 42023)
-	self:Log("SPELL_PERIODIC_MISSED", "RainOfFireDamage", 42023)
+	--self:Log("SPELL_AURA_APPLIED", "RainOfFireDamage", 42023)
+	--self:Log("SPELL_PERIODIC_DAMAGE", "RainOfFireDamage", 42023)
+	--self:Log("SPELL_PERIODIC_MISSED", "RainOfFireDamage", 42023)
 
-	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
-	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+	--self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+	--self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
 end
 
 function mod:OnEngage()
