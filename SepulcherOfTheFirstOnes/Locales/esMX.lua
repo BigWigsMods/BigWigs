2fa1c1cc1c0d03365009c0c94e0f3a9bcd66d71c
Justwait <mngrootwow@gmail.com>
2022-02-24 00:23:44 +0100

    SepulcherOfTheFirstOnes/Locales: Populate locales

---
+++ SepulcherOfTheFirstOnes/Locales/esMX.lua
@@ -0,0 +1,62 @@
+local L = BigWigs:NewBossLocale("Artificer Xy'mox v2", "esMX")
+if not L then return end
+if L then
+	-- L.traps = "Traps" -- Stasis Trap
+	-- L.sparknova = "Sparknova" -- Hyperlight Sparknova
+	-- L.relocation = "Tank Bomb" -- Glyph of Relocation
+	-- L.wormholes = "Wormholes" -- Interdimensional Wormholes
+	-- L.wormhole = "Wormhole" -- Interdimensional Wormhole
+	-- L.rings = "Rings P%d" -- Forerunner Rings // Added P1, P2, P3 etc to help identify what rings
+end
+
+L = BigWigs:NewBossLocale("Dausegne, the Fallen Oracle", "esMX")
+if L then
+	-- L.staggering_barrage = "Barrage" -- Staggering Barrage
+	-- L.domination_core = "Add" -- Domination Core
+	-- L.obliteration_arc = "Arc" -- Obliteration Arc
+
+	-- L.disintergration_halo = "Rings" -- Disintegration Halo
+	-- L.rings_x = "Rings x%d"
+	-- L.rings_enrage = "Rings (Enrage)"
+	-- L.ring_count = "Ring (%d/%d)"
+
+	-- L.shield_removed = "%s removed after %.1fs" -- "Shield removed after 1.1s" s = seconds
+end
+
+L = BigWigs:NewBossLocale("Prototype Pantheon", "esMX")
+if L then
+    -- L.necrotic_ritual = "Ritual"
+    -- L.runecarvers_deathtouch = "Deathtouch"
+    -- L.windswept_wings = "Winds"
+    -- L.wild_stampede = "Stampede"
+    -- L.withering_seeds = "Seeds"
+    -- L.hand_of_destruction = "Hand"
+end
+
+L = BigWigs:NewBossLocale("Lihuvim, Principal Architect", "esMX")
+if L then
+    -- L.protoform_cascade = "Frontal"
+    -- L.cosmic_shift = "Pushback"
+    -- L.unstable_mote = "Motes"
+    -- L.mote = "Mote"
+
+    -- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
+    -- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate on Acquisitions Automa that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+end
+
+L = BigWigs:NewBossLocale("Anduin Wrynn", "esMX")
+if L then
+    -- L.custom_off_repeating_blasphemy = "Repeating Blasphemy"
+    -- L.custom_off_repeating_blasphemy_desc = "Repeating Blasphemy say messages with icons {rt1}, {rt3} to find matches to remove your debuffs."
+
+    -- L.kingsmourne_hungers = "Kingsmourne"
+    -- L.blasphemy = "Marks"
+    -- L.befouled_barrier = "Barrier"
+    -- L.wicked_star = "Star"
+    -- L.domination_word_pain = "DW:Pain"
+    -- L.army_of_the_dead = "Army"
+    -- L.grim_reflections = "Fear Adds"
+    -- L.march_of_the_damned = "Walls"
+    -- L.dire_blasphemy = "Marks"
+    -- L.beacon_of_hope = "Beacon"
+end
