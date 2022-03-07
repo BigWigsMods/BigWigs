d74f5b6c679a162a353db82d14a55a4e1f4dcbe4
Kyle Buller <bullerk@gmail.com>
2022-03-06 19:04:28 -0600

    Sepulcher/ArtificerXymox: Add marking options, icon to kick warnings, P4 prewarning, mythic add timers; Tweak Glyph

--- SepulcherOfTheFirstOnes/ArtificerXymox.lua
+++ SepulcherOfTheFirstOnes/ArtificerXymox.lua
@@ -12,13 +12,14 @@ mod:SetRespawnTime(25)
 -- Locals
 --
 
-local tankList = {}
 local ringCount = 1
 local wormholeCount = 1
 local glyphCount = 1
 local sparkCount = 1
 local trapCount = 1
 local nextStageWarning = 77
+local mobCollector = {}
+local mobCount = {}
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -29,9 +30,9 @@ if L then
 	L.traps = "Traps" -- Stasis Trap
 	L.sparknova = "Sparknova" -- Hyperlight Sparknova
 	L.relocation = "Tank Bomb" -- Glyph of Relocation
-	L.relocation_count = "%s (%d)(%d)" -- Tank Bomb (stage) (count)
-	L.wormholes = "Wormholes" -- Interdimensional Wormholes
-	L.wormhole = "Wormhole" -- Interdimensional Wormhole
+	L.relocation_count = "%s P%d (%d)" -- Tank Bomb (stage)(count)
+	L.wormholes = "Wormholes" -- Dimensional Tear
+	L.wormhole = "Wormhole"
 	L.rings = "Rings P%d" -- Forerunner Rings // Added P1, P2, P3 etc to help identify what rings
 end
 
@@ -39,27 +40,37 @@ end
 -- Initialization
 --
 
-local interdimensionalWormholesMarker = mod:AddMarkerOption(false, "player", 1, 362721, 1, 2) -- Interdimensional Wormholes
+local interdimensionalWormholesMarker = mod:AddMarkerOption(false, "player", 1, 362721, 1, 2) -- Dimensional Tear
+local acolyteMarker = mod:AddMarkerOption(false, "npc", 8, -24270, 8, 7) -- Xy Acolyte
+local spellslingerMarker = mod:AddMarkerOption(false, "npc", 1, -24271, 1, 2, 3) -- Xy Spellslinger
+local overseerMarker = mod:AddMarkerOption(false, "npc", 8, -24450, 8, 7) -- Cartel Overseer
 function mod:GetOptions()
 	return {
 		"stages",
 		364465, -- Forerunner Rings
-		364040, -- Hyperlight Ascension
-		364030, -- Debilitating Ray
 		{362721, "SAY", "SAY_COUNTDOWN"}, -- Dimensional Tear
 		interdimensionalWormholesMarker,
 		{362803, "SAY", "SAY_COUNTDOWN"}, -- Glyph of Relocation
-		362849, -- Hyperlight Sparknova
 		362885, -- Stasis Trap
-		363485, -- Xy Decipherers
-		{365681, "TANK"}, -- System Shock
-		365701, -- Overseer's Orders
-	},nil,{
-		[364465] = L.rings, -- Forerunner Rings (Rings)
+		362849, -- Hyperlight Sparknova
+		365681, -- Massive Blast
+		364040, -- Hyperlight Ascension
+		acolyteMarker,
+		364030, -- Debilitating Ray
+		spellslingerMarker,
+		363485, -- The Cartel Elite
+		overseerMarker,
+		-- 365701, -- Overseer's Orders
+	},{
+		[365681] = CL.intermission,
+		[363485] = CL.mythic,
+	},{
+		[364465] = L.rings:format(1), -- Forerunner Rings (Rings)
 		[362721] = L.wormholes, -- Dimensional Tear (Wormholes)
 		[362803] = L.relocation, -- Glyph of Relocation (Tank Bomb)
 		[362849] = L.sparknova, -- Hyperlight Sparknova (Sparknova)
 		[362885] = L.traps, -- Stasis Trap (Traps)
+		[363485] = CL.adds,
 	}
 end
 
@@ -73,20 +84,15 @@ function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "InterdimensionalWormholes", 362721)
 	self:Log("SPELL_AURA_APPLIED", "InterdimensionalWormholesApplied", 362615, 362614)
 	self:Log("SPELL_AURA_REMOVED", "InterdimensionalWormholesRemoved", 362615, 362614)
-	self:Log("SPELL_CAST_START", "GlyphOfRelocation", 362801)
-	self:Log("SPELL_CAST_SUCCESS", "GlyphOfRelocationSuccess", 362801)
 	self:Log("SPELL_AURA_APPLIED", "GlyphOfRelocationApplied", 362803)
 	self:Log("SPELL_AURA_REMOVED", "GlyphOfRelocationRemoved", 362803)
 	self:Log("SPELL_CAST_START", "HyperlightSparknova", 362849)
 	self:Log("SPELL_CAST_SUCCESS", "StasisTrap", 362885)
-	self:Log("SPELL_CAST_START", "XyDecipherers", 363485)
-	self:Log("SPELL_CAST_SUCCESS", "SystemShock", 365681)
-	self:Log("SPELL_AURA_APPLIED", "SystemShockApplied", 365681)
-	self:Log("SPELL_AURA_APPLIED_DOSE", "SystemShockApplied", 365681)
-	self:Log("SPELL_CAST_START", "OverseersOrders", 365701)
-
-	self:RegisterEvent("GROUP_ROSTER_UPDATE")
-	self:GROUP_ROSTER_UPDATE()
+	-- self:Log("SPELL_CAST_START", "XyDecipherers", 363485) -- using RAID_BOSS_EMOTE
+	self:Log("SPELL_CAST_START", "MassiveBlast", 365682)
+	self:Log("SPELL_AURA_APPLIED", "MassiveBlastApplied", 365681)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "MassiveBlastApplied", 365681)
+	-- self:Log("SPELL_CAST_START", "OverseersOrders", 365701)
 end
 
 function mod:OnEngage()
@@ -97,13 +103,22 @@ function mod:OnEngage()
 	sparkCount = 1
 	trapCount = 1
 	nextStageWarning = 77
+	mobCollector = {}
+	mobCount = {}
 
+	if self:Mythic() then
+		self:RegisterEvent("RAID_BOSS_EMOTE") -- for adds
+		self:Bar(363485, 13.6, CL.adds) -- The Cartel Elite
+	end
 	self:Bar(362721, self:Mythic() and 9 or 8, CL.count:format(L.wormholes, wormholeCount)) -- Dimensional Tear
 	self:Bar(362849, self:Mythic() and 15.5 or 14, CL.count:format(L.sparknova, sparkCount)) -- Hyperlight Sparknova
 	self:Bar(362885, self:Mythic() and 23.3 or 21, CL.count:format(L.traps, trapCount)) -- Stasis Trap
 	self:Bar(364465, self:Mythic() and 29 or 26, CL.count:format(L.rings:format(self:GetStage()), ringCount)) -- Forerunner Rings
 	self:Bar(362803, self:Mythic() and 44.5 or 40, L.relocation_count:format(L.relocation, self:GetStage(), glyphCount)) -- Glyph of Relocation
 
+	if self:GetOption(spellslingerMarker) or (self:Mythic() and self:GetOption(overseerMarker)) then
+		self:RegisterTargetEvents("MarkAdds")
+	end
 	self:RegisterUnitEvent("UNIT_HEALTH", nil, "boss1")
 end
 
@@ -113,20 +128,46 @@ end
 
 function mod:UNIT_HEALTH(event, unit)
 	local currentHealth = self:GetHealth(unit)
-	if currentHealth < nextStageWarning then -- Intermission at 75% and 50%
-		self:Message("stages", "green", CL.soon:format(CL.intermission), false)
+	if currentHealth < nextStageWarning then -- Intermission at 75% and 50%, Different timers at 30%
+		if currentHealth > 35 then
+			self:Message("stages", "green", CL.soon:format(CL.intermission), false)
+		else
+			self:Message("stages", "green", CL.soon:format(CL.stage:format(4)), false)
+		end
+		self:PlaySound("stages", "info")
 		nextStageWarning = nextStageWarning - 25
 		if nextStageWarning < 50 then
+			nextStageWarning = 32
+		elseif nextStageWarning < 10 then
 			self:UnregisterUnitEvent(event, unit)
 		end
 	end
 end
 
-function mod:GROUP_ROSTER_UPDATE() -- Compensate for quitters (LFR)
-	tankList = {}
-	for unit in self:IterateGroup() do
-		if self:Tank(unit) then
-			tankList[#tankList+1] = unit
+function mod:MarkAcolytes()
+	for boss = 1, 5 do
+		local unit = ("boss%d"):format(boss)
+		local guid = self:UnitGUID(unit)
+		local id = self:MobId(guid)
+		if id == 184140 and not mobCollector[guid] then
+			self:CustomIcon(acolyteMarker, unit, 8 - (mobCount[id] or 0)) -- 8, 7
+			mobCount[id] = (mobCount[id] or 0) + 1
+			mobCollector[guid] = true
+		end
+	end
+end
+
+function mod:MarkAdds(event, unit, guid)
+	if not mobCollector[guid] then
+		local id = self:MobId(guid)
+		if id == 183707 and self:GetOption(spellslingerMarker) then -- Xy Spellslinger
+			mobCount[id] = (mobCount[id] or 0) + 1
+			self:CustomIcon(spellslingerMarker, unit, mobCount[id]) -- 1, 2, 3
+			mobCollector[guid] = true
+		elseif id == 184792 and self:GetOption(overseerMarker) then -- Cartel Overseer
+			self:CustomIcon(overseerMarker, unit, 8 - (mobCount[id] or 0)) -- 8, 7
+			mobCount[id] = (mobCount[id] or 0) + 1
+			mobCollector[guid] = true
 		end
 	end
 end
@@ -152,6 +193,9 @@ function mod:DecipherRelicSuccess() -- Stage 4
 	glyphCount = 1
 	sparkCount = 1
 
+	if self:Mythic() then
+		self:Bar(363485, 12.3, CL.adds) -- The Cartel Elite
+	end
 	self:Bar(362849, 15.6, CL.count:format(L.sparknova, sparkCount)) -- Hyperlight Sparknova
 	self:Bar(362721, 22.2, CL.count:format(L.wormholes, wormholeCount)) -- Interdimensional Wormholes
 	self:Bar(362885, 23.3, CL.count:format(L.traps, trapCount)) -- Stasis Trap
@@ -167,6 +211,9 @@ function mod:DecipherRelic()
 	self:StopBar(L.relocation_count:format(L.relocation, self:GetStage(), glyphCount)) -- Glyph of Relocation
 	self:StopBar(CL.count:format(L.sparknova, sparkCount)) -- Hyperlight Sparknova
 	self:StopBar(CL.count:format(L.traps, trapCount)) -- Stasis Trap
+	if self:GetOption(acolyteMarker) then
+		self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "MarkAcolytes")
+	end
 end
 
 function mod:DecipherRelicRemoved()
@@ -174,6 +221,11 @@ function mod:DecipherRelicRemoved()
 	self:Message("stages", "cyan", CL.stage:format(stage), false)
 	self:PlaySound("stages", "info")
 	self:SetStage(stage)
+
+	if self:GetOption(acolyteMarker) then
+		self:UnregisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
+	end
+	mobCount = {}
 
 	-- Not resetting traps as they persist throughout the whole fight
 	-- Not resetting wormholes as you get 1 per stage
@@ -184,11 +236,14 @@ function mod:DecipherRelicRemoved()
 	glyphCount = 1
 	sparkCount = 1
 
+	if self:Mythic() then
+		self:Bar(363485, 12.3, CL.adds) -- The Cartel Elite
+	end
 	self:Bar(362721, 8, CL.count:format(L.wormholes, wormholeCount)) -- Interdimensional Wormholes
 	self:Bar(362849, 14, CL.count:format(L.sparknova, sparkCount)) -- Hyperlight Sparknova
 	self:Bar(362885, 21, CL.count:format(L.traps, trapCount)) -- Stasis Trap
 	self:Bar(364465, 26, CL.count:format(L.rings:format(self:GetStage()), ringCount)) -- Forerunner Rings
-	self:Bar(362803, 40, L.relocation_count:format(L.relocation, self:GetStage(), glyphCount)) -- Glyph of Relocation
+	self:Bar(362803, 45, L.relocation_count:format(L.relocation, self:GetStage(), glyphCount)) -- Glyph of Relocation
 end
 
 function mod:ForerunnerRings(args)
@@ -213,7 +268,8 @@ end
 function mod:DebilitatingRay(args)
 	local canDo, ready = self:Interrupter(args.sourceGUID)
 	if canDo then
-		self:Message(args.spellId, "yellow")
+		local icon = CombatLog_String_GetIcon(args.sourceRaidFlags)
+		self:Message(args.spellId, "yellow", icon .. args.spellName)
 		if ready then
 			self:PlaySound(args.spellId, "alert")
 		end
@@ -245,35 +301,17 @@ do
 	end
 end
 
-function mod:GlyphOfRelocation(args)
-	self:StopBar(L.relocation_count:format(L.relocation, self:GetStage(), glyphCount))
-	local bossUnit = self:GetBossId(args.sourceGUID)
-	for i = 1, #tankList do
-		local unit = tankList[i]
-		if bossUnit and self:Tanking(bossUnit, unit) then
-			self:TargetMessage(362803, "yellow", self:UnitName(unit), CL.casting:format(L.relocation_count:format(L.relocation, self:GetStage(), glyphCount)))
-			break
-		elseif i == #tankList then
-			self:Message(362803, "yellow", CL.casting:format(L.relocation_count:format(L.relocation, self:GetStage(), glyphCount)))
-		end
-	end
-	self:PlaySound(362803, "warning")
-end
-
-function mod:GlyphOfRelocationSuccess(args)
-	self:StopBar(L.relocation_count:format(L.relocation, self:GetStage(), glyphCount)) -- Replacing the last 5 seconds with targetbar, if not a tank
-	glyphCount = glyphCount + 1
-	self:CDBar(362803, self:GetStage() == 4 and 66.5 or 60, L.relocation_count:format(L.relocation, self:GetStage(), glyphCount))
-end
-
 function mod:GlyphOfRelocationApplied(args)
-	self:TargetMessage(args.spellId, "purple", args.destName)
+	self:StopBar(L.relocation_count:format(L.relocation, self:GetStage(), glyphCount))
+	self:TargetMessage(args.spellId, "purple", args.destName, L.relocation_count:format(L.relocation, self:GetStage(), glyphCount))
 	self:PlaySound(args.spellId, "warning")
 	self:TargetBar(args.spellId, 5, args.destName, CL.bomb)
 	if self:Me(args.destGUID) then
 		self:Say(args.spellId, CL.bomb)
 		self:SayCountdown(args.spellId, 5)
 	end
+	glyphCount = glyphCount + 1
+	self:CDBar(362803, self:GetStage() == 4 and 40 or 66.5, L.relocation_count:format(L.relocation, self:GetStage(), glyphCount))
 end
 
 function mod:GlyphOfRelocationRemoved(args)
@@ -298,21 +336,31 @@ function mod:StasisTrap(args)
 	self:Bar(args.spellId, self:GetStage() == 4 and 33.3 or 30, CL.count:format(L.traps, trapCount))
 end
 
-function mod:XyDecipherers(args)
-	self:Message(args.spellId, "red")
-	self:PlaySound(args.spellId, "alert")
+function mod:RAID_BOSS_EMOTE(_, msg)
+	if msg:find("363485", nil, true) then -- The Cartel Elite
+		self:Message(363485, "red", CL.incoming:format(CL.adds))
+		self:PlaySound(363485, "alert")
+	end
 end
 
-function mod:SystemShock(args)
-	self:Message(args.spellId, "purple")
-	self:PlaySound(args.spellId, "alarm")
+function mod:MassiveBlast(args)
+	local canDo, ready = self:Interrupter(args.sourceGUID)
+	if canDo then
+		local icon = CombatLog_String_GetIcon(args.sourceRaidFlags)
+		self:Message(365681, "purple", icon .. args.spellName)
+		if self:Tank() then
+			self:PlaySound(365681, "alarm")
+		elseif ready then
+			self:PlaySound(365681, "alert")
+		end
+	end
 end
 
-function mod:SystemShockApplied(args)
+function mod:MassiveBlastApplied(args)
 	self:NewStackMessage(args.spellId, "purple", args.destName, args.amount, 2)
 end
 
-function mod:OverseersOrders(args)
-	self:Message(args.spellId, "yellow")
-	self:PlaySound(args.spellId, "alert")
-end
+-- function mod:OverseersOrders(args)
+-- 	self:Message(args.spellId, "yellow")
+-- 	self:PlaySound(args.spellId, "alert")
+-- end
