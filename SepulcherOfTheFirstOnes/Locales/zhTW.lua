2d1bf5ef51adfdbc73b1185beead086976302350
EK <eke00372@gmail.com>
2022-03-09 06:03:37 +0800

    Update zhTW.lua

--- SepulcherOfTheFirstOnes/Locales/zhTW.lua
+++ SepulcherOfTheFirstOnes/Locales/zhTW.lua
@@ -2,25 +2,25 @@ local L = BigWigs:NewBossLocale("Artificer Xy'mox v2", "zhTW")
 if not L then return end
 if L then
 	L.traps = "陷阱" -- Stasis Trap
-	-- L.sparknova = "火花新星" -- Hyperlight Sparknova
+	L.sparknova = "火花新星" -- Hyperlight Sparknova
 	L.relocation = "坦克炸彈" -- Glyph of Relocation
 	L.wormholes = "蟲洞" -- Interdimensional Wormholes
 	L.wormhole = "蟲洞" -- Interdimensional Wormhole
-	-- L.rings = "Rings P%d" -- Forerunner Rings // Added P1, P2, P3 etc to help identify what rings
+	L.rings = "第 %d 道環" -- Forerunner Rings // Added P1, P2, P3 etc to help identify what rings
 end
 
 L = BigWigs:NewBossLocale("Dausegne, the Fallen Oracle", "zhTW")
 if L then
-	-- L.staggering_barrage = "Barrage" -- Staggering Barrage
+	L.staggering_barrage = "彈幕" -- Staggering Barrage
 	-- L.domination_core = "Add" -- Domination Core
 	-- L.obliteration_arc = "Arc" -- Obliteration Arc
 
-	-- L.disintergration_halo = "Rings" -- Disintegration Halo
+	L.disintergration_halo = "輝環" -- Disintegration Halo
 	-- L.rings_x = "Rings x%d"
-	-- L.rings_enrage = "Rings (Enrage)"
-	-- L.ring_count = "Ring (%d/%d)"
+	L.rings_enrage = "輝環（軟狂暴）"
+	L.ring_count = "輝環（%d/%d）"
 
-	-- L.shield_removed = "%s removed after %.1fs" -- "Shield removed after 1.1s" s = seconds
+	L.shield_removed = "%s 移除，用時 %.1f 秒" -- "Shield removed after 1.1s" s = seconds
 end
 
 L = BigWigs:NewBossLocale("Prototype Pantheon", "zhTW")
@@ -36,9 +36,9 @@ end
 L = BigWigs:NewBossLocale("Lihuvim, Principal Architect", "zhTW")
 if L then
 	-- L.protoform_cascade = "Frontal"
-	-- L.cosmic_shift = "Pushback"
-	-- L.unstable_mote = "Motes"
-	-- L.mote = "Mote"
+	L.cosmic_shift = "擊退"
+	L.unstable_mote = "微粒"
+	L.mote = "微粒"
 
 	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
 	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate on Acquisitions Automa that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
@@ -49,16 +49,16 @@ if L then
 	-- L.custom_off_repeating_blasphemy = "Repeating Blasphemy"
 	-- L.custom_off_repeating_blasphemy_desc = "Repeating Blasphemy say messages with icons {rt1}, {rt3} to find matches to remove your debuffs."
 
-	-- L.kingsmourne_hungers = "Kingsmourne"
-	-- L.blasphemy = "Marks"
-	-- L.befouled_barrier = "Barrier"
-	-- L.wicked_star = "星星"
-	-- L.domination_word_pain = "DW:Pain"
-	-- L.army_of_the_dead = "Army"
-	-- L.grim_reflections = "Fear Adds"
-	-- L.march_of_the_damned = "Walls"
-	-- L.dire_blasphemy = "Marks"
-	-- L.beacon_of_hope = "信標"
+	L.kingsmourne_hungers = "王之哀傷"
+	L.blasphemy = "印記"
+	L.befouled_barrier = "屏障"
+	L.wicked_star = "星星"
+	-- L.domination_word_pain = "DW:Pain" -- 御言術痛
+	L.army_of_the_dead = "大軍"
+	L.grim_reflections = "幻影"
+	L.march_of_the_damned = "影牆"
+	L.dire_blasphemy = "印記"
+	L.beacon_of_hope = "信標"
 
 	-- L.remnant_active = "Remnant Active"
 end
