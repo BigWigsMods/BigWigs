f7edc92f456cd0c9f5c0a407f664013f22033f48
Levanah <7429333+levan4h@users.noreply.github.com>
2022-03-10 23:00:42 +0100

    Update frFR (#1158)

--- SepulcherOfTheFirstOnes/Locales/frFR.lua
+++ SepulcherOfTheFirstOnes/Locales/frFR.lua
@@ -1,70 +1,70 @@
 local L = BigWigs:NewBossLocale("Artificer Xy'mox v2", "frFR")
 if not L then return end
 if L then
-	--L.traps = "Traps" -- Stasis Trap
-	--L.sparknova = "Sparknova" -- Hyperlight Sparknova
-	--L.relocation = "Tank Bomb" -- Glyph of Relocation
-	--L.relocation_count = "%s S%d (%d)" -- Tank Bomb S1 (1) // Tank Bomb (stage)(count)
-	--L.wormholes = "Wormholes" -- Interdimensional Wormholes
-	--L.wormhole = "Wormhole" -- Interdimensional Wormhole
-	--L.rings = "Rings S%d" -- Rings S1 // Forerunner Rings Stage 1/2/3/4
+	L.traps = "Pièges" -- Stasis Trap
+	L.sparknova = "Nova éclair" -- Hyperlight Sparknova
+	L.relocation = "Bombe sur le tank" -- Glyph of Relocation
+	L.relocation_count = "%s S%d (%d)" -- Tank Bomb S1 (1) // Tank Bomb (stage)(count)
+	L.wormholes = "Tunnels spatiotemporels" -- Interdimensional Wormholes
+	L.wormhole = "Tunnel spatiotemporel" -- Interdimensional Wormhole
+	L.rings = "Anneaux S%d" -- Rings S1 // Forerunner Rings Stage 1/2/3/4
 end
 
 L = BigWigs:NewBossLocale("Dausegne, the Fallen Oracle", "frFR")
 if L then
-	--L.staggering_barrage = "Barrage" -- Staggering Barrage
-	--L.domination_core = "Add" -- Domination Core
-	--L.obliteration_arc = "Arc" -- Obliteration Arc
+	L.staggering_barrage = "Barrage" -- Staggering Barrage
+	L.domination_core = "Add" -- Domination Core
+	L.obliteration_arc = "Arc" -- Obliteration Arc
 
-	--L.disintergration_halo = "Rings" -- Disintegration Halo
-	--L.rings_x = "Rings x%d"
-	--L.rings_enrage = "Rings (Enrage)"
-	--L.ring_count = "Ring (%d/%d)"
+	L.disintergration_halo = "Anneaux" -- Disintegration Halo
+	L.rings_x = "Anneaux x%d"
+	L.rings_enrage = "Anneaux (Enragé)"
+	L.ring_count = "Anneau (%d/%d)"
 
-	--L.shield_removed = "%s removed after %.1fs" -- "Shield removed after 1.1s" s = seconds
+	L.shield_removed = "%s retiré au bout de %.1fs" -- "Shield removed after 1.1s" s = seconds
 end
 
 L = BigWigs:NewBossLocale("Prototype Pantheon", "frFR")
 if L then
-	--L.necrotic_ritual = "Ritual"
-	--L.runecarvers_deathtouch = "Deathtouch"
-	--L.windswept_wings = "Winds"
-	--L.wild_stampede = "Stampede"
-	--L.withering_seeds = "Seeds"
-	--L.hand_of_destruction = "Hand"
+	L.necrotic_ritual = "Rituel"
+	L.runecarvers_deathtouch = "Toucher de la mort"
+	L.windswept_wings = "Rafale"
+	L.wild_stampede = "Ruée"
+	L.withering_seeds = "Graines"
+	L.hand_of_destruction = "Main"
 end
 
 L = BigWigs:NewBossLocale("Lihuvim, Principal Architect", "frFR")
 if L then
-	--L.protoform_cascade = "Frontal"
-	--L.cosmic_shift = "Pushback"
-	--L.unstable_mote = "Motes"
-	--L.mote = "Mote"
+	L.protoform_cascade = "Frontal"
+	L.cosmic_shift = "Poussée"
+	L.unstable_mote = "Granules"
+	L.mote = "Granule"
 
-	--L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
-	--L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate on Acquisitions Automa that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	L.custom_on_nameplate_fixate = "Icône de barre d'unité fixée"
+	L.custom_on_nameplate_fixate_desc = "Affiche une icône sur la barre de nom d'unité des Automas pourvoyeurs qui vous fixent.\n\nNécessite d'avoir activé les barres de noms des unités ennemies et un addon de barres de noms compatible (KuiNameplates, Plater)."
 end
 
 L = BigWigs:NewBossLocale("Anduin Wrynn", "frFR")
 if L then
-	--L.custom_off_repeating_blasphemy = "Repeating Blasphemy"
-	--L.custom_off_repeating_blasphemy_desc = "Repeating Blasphemy say messages with icons {rt1}, {rt3} to find matches to remove your debuffs."
+	L.custom_off_repeating_blasphemy = "Répéter Blasphème"
+	L.custom_off_repeating_blasphemy_desc = "Répéter Blasphème affiche un message en /dire avec les icônes {rt1}, {rt3} pour trouver une marque permettant d'enlever le débuff."
 
-	--L.kingsmourne_hungers = "Kingsmourne"
-	--L.blasphemy = "Marks"
-	--L.befouled_barrier = "Barrier"
-	--L.wicked_star = "Star"
-	--L.domination_word_pain = "DW:Pain"
-	--L.army_of_the_dead = "Army"
-	--L.grim_reflections = "Fear Adds"
-	--L.march_of_the_damned = "Walls"
-	--L.dire_blasphemy = "Marks"
-	--L.beacon_of_hope = "Beacon"
+	L.kingsmourne_hungers = "Deuilleroi"
+	L.blasphemy = "Marques"
+	L.befouled_barrier = "Barrière"
+	L.wicked_star = "Etoile"
+	L.domination_word_pain = "MD : Douleur"
+	L.army_of_the_dead = "Armée"
+	L.grim_reflections = "Adds qui fear"
+	L.march_of_the_damned = "Murs"
+	L.dire_blasphemy = "Marques"
+	L.beacon_of_hope = "Signal"
 
-	--L.remnant_active = "Remnant Active"
+	L.remnant_active = "Vestige actif"
 end
 
 L = BigWigs:NewBossLocale("Skolex, the Insatiable Ravener", "frFR")
 if L then
-	--L.tank_combo_desc = "Timer for Riftmaw/Rend casts at 100 energy."
+	L.tank_combo_desc = "Timer pour les cast de Mâche-faille/Pourfendre à 100 d'energie."
 end
