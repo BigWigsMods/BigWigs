5ff2dec45e62a5b6a8719d8f4b71d3d061c07db7
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2022-03-12 22:54:44 +0800

    Update zhCN.lua

--- SepulcherOfTheFirstOnes/Locales/zhCN.lua
+++ SepulcherOfTheFirstOnes/Locales/zhCN.lua
@@ -1,9 +1,9 @@
 local L = BigWigs:NewBossLocale("Vigilant Guardian", "zhCN")
 if not L then return end
 if L then
-	L.sentry = "卫士小怪"
-	L.materium = "小怪"
-	-- L.shield = "Shield" -- Global locale canidate?
+	L.sentry = "哨兵"
+	L.materium = "素材"
+	L.shield = "核心" -- Global locale canidate?
 end
 
 L = BigWigs:NewBossLocale("Skolex, the Insatiable Ravener", "zhCN")
@@ -16,7 +16,7 @@ if L then
 	L.traps = "陷阱" -- Stasis Trap
 	L.sparknova = "火花新星" -- Hyperlight Sparknova
 	L.relocation = "坦克炸弹" -- Glyph of Relocation
-	--L.relocation_count = "%s S%d (%d)" -- Tank Bomb S1 (1) // Tank Bomb (stage)(count)
+	L.relocation_count = "%s :阶段%d (%d)" -- Tank Bomb S1 (1) // Tank Bomb (stage)(count)
 	L.wormholes = "虫洞" -- Interdimensional Wormholes
 	L.wormhole = "虫洞" -- Interdimensional Wormhole
 	L.rings = "法环 %d 阶段" -- Rings S1 // Forerunner Rings Stage 1/2/3/4
@@ -26,61 +26,42 @@ L = BigWigs:NewBossLocale("Dausegne, the Fallen Oracle", "zhCN")
 if L then
 	L.staggering_barrage = "弹幕" -- Staggering Barrage
 	L.domination_core = "小怪" -- Domination Core
-	L.obliteration_arc = "连奏" -- Obliteration Arc
+	L.obliteration_arc = "湮灭" -- Obliteration Arc
 
 	L.disintergration_halo = "光环" -- Disintegration Halo
 	L.rings_x = "光环 x%d"
 	L.rings_enrage = "光环 (激怒)"
 	L.ring_count = "光环 (%d/%d)"
-
-	--L.custom_on_ring_timers = "Individual Halo Timers"
-	--L.custom_on_ring_timers_desc = "Disintegration Halo triggers a set of rings, this will show bars for when each of the rings starts moving. Uses settings from Disintegration Halo."
-
+	
+	L.custom_on_ring_timers = "衰变光环计时条"
+	L.custom_on_ring_timers_desc = "使用衰变光环设置：这是显示衰变光环在触发能量环时,开始移动的计时条。"
+	
 	L.shield_removed = "%s 在 %.1fs 移除" -- "Shield removed after 1.1s" s = seconds
 end
 
 L = BigWigs:NewBossLocale("Prototype Pantheon", "zhCN")
 if L then
-	L.necrotic_ritual = "仪式"
+	L.necrotic_ritual = "死灵仪式"
 	L.runecarvers_deathtouch = "死亡之触"
 	L.windswept_wings = "啸风"
-	L.wild_stampede = "践踏"
+	L.wild_stampede = "兽群"
 	L.withering_seeds = "种子"
 	L.hand_of_destruction = "毁灭之手"
 end
 
 L = BigWigs:NewBossLocale("Lihuvim, Principal Architect", "zhCN")
 if L then
-	L.protoform_cascade = "正面"
-	L.cosmic_shift = "推开"
+	L.protoform_cascade = "散射"
+	L.cosmic_shift = "击退"
 	--L.cosmic_shift_mythic = "Shift: %s"
 	L.unstable_mote = "微粒"
 	L.mote = "微粒"
 
 	L.custom_on_nameplate_fixate = "被征用姓名板图标"
-	L.custom_on_nameplate_fixate_desc = "在征用自动体姓名板上显示你是被征用.\n\n需要使用敌对姓名板和支持姓名板的插件（KuiNameplates，Plater）。"
+	L.custom_on_nameplate_fixate_desc = "在目标是你的自动体的姓名板上显示图标。\n\n需要使用敌对姓名板和支持姓名板的插件（KuiNameplates,Plater）。"
 
 	--L.harmonic = "Push"
 	--L.melodic = "Pull"
-end
-
-L = BigWigs:NewBossLocale("Anduin Wrynn", "zhCN")
-if L then
-	L.custom_off_repeating_blasphemy = "渎神之光"
-	L.custom_off_repeating_blasphemy_desc = "中了渎神之光说带有图标 {rt1}、{rt3} 的消息,找到匹配项来消除你的减益。"
-
-	L.kingsmourne_hungers = "王之哀伤"
-	L.blasphemy = "标记"
-	L.befouled_barrier = "屏障"
-	L.wicked_star = "邪恶"
-	L.domination_word_pain = "DW:痛"
-	L.army_of_the_dead = "大军"
-	L.grim_reflections = "黑暗镜像"
-	L.march_of_the_damned = "阶段"
-	L.dire_blasphemy = "标记"
-	L.beacon_of_hope = "信标"
-
-	L.remnant_active = "剩余活跃"
 end
 
 L = BigWigs:NewBossLocale("Halondrus the Reclaimer", "zhCN")
@@ -92,4 +73,42 @@ if L then
 
 	L.custom_on_stop_timers = "总是显示技能条"
 	L.custom_on_stop_timers_desc = "黑伦度斯可能延迟它的技能。启用此选项后,这些技能将保留在您的屏幕上。"
+end
+
+L = BigWigs:NewBossLocale("Anduin Wrynn", "zhCN")
+if L then
+	L.custom_off_repeating_blasphemy = "重复渎神之光"
+	L.custom_off_repeating_blasphemy_desc = "以{rt1}和{rt3} 说话两种渎神之光印记,方便你快速地找到相反印记的队友，并消除自身印记"
+
+	L.kingsmourne_hungers = "王之哀伤"
+	L.blasphemy = "标记"
+	L.befouled_barrier = "屏障"
+	L.wicked_star = "邪恶之星"
+	L.domination_word_pain = "御言术:痛"
+	L.army_of_the_dead = "大军"
+	L.grim_reflections = "黑暗镜像"
+	L.march_of_the_damned = "阶段"
+	L.dire_blasphemy = "标记"
+	L.beacon_of_hope = "信标"
+
+	L.remnant_active = "剩余活跃"
+end
+
+L = BigWigs:NewBossLocale("Lords of Dread", "zhCN")
+if L then
+	L.unto_darkness = "AoE 阶段"-- Unto Darkness
+	L.cloud_of_carrion = "腐臭" -- Cloud of Carrion
+	L.manifest_shadows = "小怪" -- Manifest Shadows
+	L.leeching_claws = "正面 (玛)" -- Leeching Claws
+	L.infiltration_of_dread = "渗透" -- Infiltration of Dread
+	L.fearful_trepidation = "爆裂恐惧" -- Fearful Trepidation
+	L.slumber_cloud = "迷雾" -- Slumber Cloud
+	L.anguishing_strike = "正面 (金)" -- Anguishing Strike
+end
+
+L = BigWigs:NewBossLocale("Rygelon", "zhCN")
+if L then
+	L.celestial_collapse = "类星体" -- Celestial Collapse
+	L.manifest_cosmos = "宇宙核心" -- Manifest Cosmos
+	L.stellar_shroud = "吸收治疗量" -- Stellar Shroud
 end
