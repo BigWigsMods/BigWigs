2f7f659ca48eabd50c1339dc13e599e23422c432
Kyle Buller <bullerk@gmail.com>
2022-03-16 17:59:15 -0500

    Sepulcher: Update locale files

--- SepulcherOfTheFirstOnes/Locales/ruRU.lua
+++ SepulcherOfTheFirstOnes/Locales/ruRU.lua
@@ -87,6 +87,36 @@ if L then
 	L.tank_combo_desc = "Таймер для танковской комбы на 100 энергии."
 end
 
+L = BigWigs:NewBossLocale("Halondrus the Reclaimer", "ruRU")
+if L then
+	--L.seismic_tremors = "Motes + Tremors"
+	--L.earthbreaker_missiles = "Missiles"
+	--L.crushing_prism = "Prisms"
+	--L.prism = "Prism"
+
+	--L.custom_on_stop_timers = "Always show ability bars"
+	--L.custom_on_stop_timers_desc = "Halondrus can delay its abilities. When this option is enabled, the bars for those abilities will stay on your screen."
+end
+
+L = BigWigs:NewBossLocale("Lords of Dread", "ruRU")
+if L then
+	--L.unto_darkness = "AoE Phase"
+	--L.cloud_of_carrion = "Carrion"
+	--L.manifest_shadows = "Adds"
+	--L.leeching_claws = "Frontal (M)"
+	--L.infiltration_of_dread = "Among Us"
+	--L.fearful_trepidation = "Fears"
+	--L.slumber_cloud = "Clouds"
+	--L.anguishing_strike = "Frontal (K)"
+end
+
+L = BigWigs:NewBossLocale("Rygelon", "ruRU")
+if L then
+	--L.celestial_collapse = "Quasars"
+	--L.manifest_cosmos = "Cores"
+	--L.stellar_shroud = "Heal Absorb"
+end
+
 L = BigWigs:NewBossLocale("The Jailer", "ruRU")
 if L then
 	--L.rune_of_damnation_countdown = "Countdown"
