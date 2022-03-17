53eb66fda9037520e038ef3e6c245c3d53280f62
Ayharu <101816860+Ayharu@users.noreply.github.com>
2022-03-17 14:48:11 -0500

    Update esES.lua
    I just translated some things about guardian, artificer and prototype.
    I'm LATAM player... lmk if i need to translate on EsMX too or just esES.

--- SepulcherOfTheFirstOnes/Locales/esES.lua
+++ SepulcherOfTheFirstOnes/Locales/esES.lua
@@ -2,8 +2,8 @@ local L = BigWigs:NewBossLocale("Vigilant Guardian", "esES")
 if not L then return end
 if L then
 	-- L.sentry = "Tank Add"
-	-- L.materium = "Small Adds"
-	-- L.shield = "Shield" -- Global locale canidate?
+	-- L.materium = "Adds pequeños"
+	-- L.shield = "Escudo" -- Global locale canidate?
 end
 
 L = BigWigs:NewBossLocale("Skolex, the Insatiable Ravener", "esES")
@@ -13,25 +13,25 @@ end
 
 L = BigWigs:NewBossLocale("Artificer Xy'mox v2", "esES")
 if L then
-	--L.traps = "Traps" -- Stasis Trap
-	--L.sparknova = "Sparknova" -- Hyperlight Sparknova
-	--L.relocation = "Tank Bomb" -- Glyph of Relocation
-	--L.relocation_count = "%s S%d (%d)" -- Tank Bomb S1 (1) // Tank Bomb (stage)(count)
-	--L.wormholes = "Wormholes" -- Interdimensional Wormholes
-	--L.wormhole = "Wormhole" -- Interdimensional Wormhole
-	--L.rings = "Rings S%d" -- Rings S1 // Forerunner Rings Stage 1/2/3/4
+	--L.traps = "Trampa de estasis" -- Stasis Trap
+	--L.sparknova = "Chispanova de hiperluz" -- Hyperlight Sparknova
+	--L.relocation = "Bomba de tank" -- Glyph of Relocation
+	--L.relocation_count = "%s S%d (%d)" -- Bomba de tank S1 (1) // Bomba de tank (stage)(count)
+	--L.wormholes = "Ruptura dimensional" -- Interdimensional Wormholes
+	--L.wormhole = "Ruptura dimensional" -- Interdimensional Wormhole
+	--L.rings = "Anillos S%d" -- Anillos S1 // Anillos genesiacos Stage 1/2/3/4
 end
 
 L = BigWigs:NewBossLocale("Dausegne, the Fallen Oracle", "esES")
 if L then
-	--L.staggering_barrage = "Barrage" -- Staggering Barrage
+	--L.staggering_barrage = "Barrera" -- Staggering Barrage
 	--L.domination_core = "Add" -- Domination Core
-	--L.obliteration_arc = "Arc" -- Obliteration Arc
+	--L.obliteration_arc = "Arco" -- Obliteration Arc
 
-	--L.disintergration_halo = "Rings" -- Disintegration Halo
-	--L.rings_x = "Rings x%d"
-	--L.rings_enrage = "Rings (Enrage)"
-	--L.ring_count = "Ring (%d/%d)"
+	--L.disintergration_halo = "Anillos" -- Disintegration Halo
+	--L.rings_x = "Anillos x%d"
+	--L.rings_enrage = "Anillos (Enrage)"
+	--L.ring_count = "Anillo (%d/%d)"
 
 	--L.custom_on_ring_timers = "Individual Halo Timers"
 	--L.custom_on_ring_timers_desc = "Disintegration Halo triggers a set of rings, this will show bars for when each of the rings starts moving. Uses settings from Disintegration Halo."
@@ -42,11 +42,11 @@ end
 L = BigWigs:NewBossLocale("Prototype Pantheon", "esES")
 if L then
 	--L.necrotic_ritual = "Ritual"
-	--L.runecarvers_deathtouch = "Deathtouch"
-	--L.windswept_wings = "Winds"
-	--L.wild_stampede = "Stampede"
-	--L.withering_seeds = "Seeds"
-	--L.hand_of_destruction = "Hand"
+	--L.runecarvers_deathtouch = "Toque mortal"
+	--L.windswept_wings = "Alas"
+	--L.wild_stampede = "Estampida"
+	--L.withering_seeds = "Semillas"
+	--L.hand_of_destruction = "Mano"
 end
 
 L = BigWigs:NewBossLocale("Lihuvim, Principal Architect", "esES")
@@ -54,8 +54,8 @@ if L then
 	--L.protoform_cascade = "Frontal"
 	--L.cosmic_shift = "Pushback"
 	--L.cosmic_shift_mythic = "Shift: %s"
-	--L.unstable_mote = "Motes"
-	--L.mote = "Mote"
+	--L.unstable_mote = "Motas"
+	--L.mote = "Mota"
 
 	--L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
 	--L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate on Acquisitions Automa that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
