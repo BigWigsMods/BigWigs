f8b4214b1a057d5b06c9581c5b5efe18b48caee0
simoflorence <kikuchi.yume@gmail.com>
2022-04-26 19:28:39 +0200

    Update common.itIT.lua

--- Core/Locales/common.itIT.lua
+++ Core/Locales/common.itIT.lua
@@ -93,7 +93,7 @@ L.amount_three = "%dk" -- Thousands 1,000
 L.seconds = "%.1fs" -- 1.1 seconds
 
 -- Common ability name replacements
---L.tank_combo = "Tank Combo" -- Used for tank swap mechanics where the boss casts a sequence of tank buster attacks
+L.tank_combo = "Combinazione Difensore" -- Used for tank swap mechanics where the boss casts a sequence of tank buster attacks
 L.laser = "Laser" -- Used for abilities that act like a laser. Usually from the boss to a player, or, from the boss to a specific area
 L.beam = "Raggio" -- Similar to "Laser" (a beam from boss to player/boss to area) but can be used to better describe certain abilities that don't look like a Laser
 L.beams = "Raggi" -- Plural of L.beam
