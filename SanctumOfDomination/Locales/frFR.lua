21c08dd900c9db9fdcd8978a9af622b5fa32229c
nanor65 <diasronan@netscape.net>
2022-04-28 00:12:36 +0200

    Update frFR.lua
    Perform initial translation to french

--- SanctumOfDomination/Locales/frFR.lua
+++ SanctumOfDomination/Locales/frFR.lua
@@ -1,140 +1,140 @@
 local L = BigWigs:NewBossLocale("The Tarragrue", "frFR")
 if not L then return end
 if L then
-	--L.chains = "Chains" -- Chains of Eternity (Chains)
-	--L.remnants = "Remnants" -- Remnant of Forgotten Torments (Remnants)
+	L.chains = "Chaînes" -- Chains of Eternity (Chains)
+	L.remnants = "Vestiges" -- Remnant of Forgotten Torments (Remnants)
 
-	--L.physical_remnant = "Physical Remnant"
-	--L.magic_remnant = "Magic Remnant"
-	--L.fire_remnant = "Fire Remnant"
-	--L.fire = "Fire"
-	--L.magic = "Magic"
-	--L.physical = "Physical"
+	L.physical_remnant = "Vestiage dommage physique"
+	L.magic_remnant = "Vestige dommage magique"
+	L.fire_remnant = "Vestiage dommage feu"
+	L.fire = "Feu"
+	L.magic = "Magique"
+	L.physical = "Physique"
 end
 
 L = BigWigs:NewBossLocale("The Eye of the Jailer", "frFR")
 if L then
-	--L.chains = "Chains" -- Short for Dragging Chains
-	--L.pool = "Pool" -- Spreading Misery
-	--L.pools = "Pools" -- Spreading Misery (multiple)
-	--L.death_gaze = "Death Gaze" -- Short for Titanic Death Gaze
+	L.chains = "Grappin" -- Short for Dragging Chains
+	L.pool = "Flaque" -- Spreading Misery
+	L.pools = "Flaques" -- Spreading Misery (multiple)
+	L.death_gaze = "Regard de mort" -- Short for Titanic Death Gaze
 end
 
 L = BigWigs:NewBossLocale("The Nine", "frFR")
 if L then
-	--L.fragments = "Fragments" -- Short for Fragments of Destiny
-	--L.fragment = "Fragment" -- Singular Fragment of Destiny
-	--L.run_away = "Run Away" -- Wings of Rage
-	--L.song = "Song" -- Short for Song of Dissolution
-	--L.go_in = "Go in" -- Reverberating Refrain
-	--L.valkyr = "Val'kyr" -- Short for Call of the Val'kyr
-	--L.blades = "Blades" -- Agatha's Eternal Blade
-	--L.big_bombs = "Big Bombs" -- Daschla's Mighty Impact
-	--L.big_bomb = "Big Bomb" -- Attached to the countdown
-	--L.shield = "Shield" -- Annhylde's Bright Aegis
-	--L.soaks = "Soaks" -- Aradne's Falling Strike
-	--L.small_bombs = "Small Bombs" -- Brynja's Mournful Dirge
-	--L.recall = "Recall" -- Short for Word of Recall
+	L.fragments = "Fragments" -- Short for Fragments of Destiny
+	L.fragment = "Fragment" -- Singular Fragment of Destiny
+	L.run_away = "Fuit" -- Wings of Rage
+	L.song = "Chant" -- Short for Song of Dissolution
+	L.go_in = "Rassemblement" -- Reverberating Refrain
+	L.valkyr = "Val'kyr" -- Short for Call of the Val'kyr
+	L.blades = "Lames" -- Agatha's Eternal Blade
+	L.big_bombs = "Impact" -- Daschla's Mighty Impact
+	L.big_bomb = "Impact" -- Attached to the countdown
+	L.shield = "Bouclier" -- Annhylde's Bright Aegis
+	L.soaks = "Encaissé" -- Aradne's Falling Strike
+	L.small_bombs = "Chant éploré" -- Brynja's Mournful Dirge
+	L.recall = "Mot de rappel" -- Short for Word of Recall
 
-	--L.blades_yell = "Fall before my blade!"
-	--L.soaks_yell = "You are all outmatched!"
-	--L.shield_yell = "My shield never falters!"
+	L.blades_yell = "Tombez devant mes lames!"
+	L.soaks_yell = "Vous êtes complètement dépassé!"
+	L.shield_yell = "Mon bouclier ne faiblira jamais!"
 
-	--L.berserk_stage1 = "Berserk Stage 1"
-	--L.berserk_stage2 = "Berserk Stage 2"
+	L.berserk_stage1 = "Berserk Etape 1"
+	L.berserk_stage2 = "Berserk Etape 2"
 
-	--L.image_special = "%s [Skyja]" -- Stage 2 boss name
+	L.image_special = "%s [Skyja]" -- Stage 2 boss name
 end
 
 L = BigWigs:NewBossLocale("Remnant of Ner'zhul", "frFR")
 if L then
-	-- L.cones = "Cones" -- Grasp of Malice
-	-- L.orbs = "Orbs" -- Orb of Torment
-	-- L.orb = "Orb" -- Orb of Torment
+	L.cones = "Cones" -- Grasp of Malice
+	L.orbs = "Orbes" -- Orb of Torment
+	L.orb = "Orbe" -- Orb of Torment
 end
 
 L = BigWigs:NewBossLocale("Soulrender Dormazain", "frFR")
 if L then
-	-- L.custom_off_nameplate_defiance = "Defiance Nameplate Icon"
-	-- L.custom_off_nameplate_defiance_desc = "Show an icon on the nameplate of Mawsworn that have Defiance.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	L.custom_off_nameplate_defiance = "Icône sur les bars de nom lors du Tourment"
+	 L.custom_off_nameplate_defiance_desc = "Affiche une icône sur les bars de nom des antreliges ayant le Tourment.\n\nIl est obligatoire d'utiliser l'affiche des bars de nom ennemie et d'addon de bar de nom compatible (KuiNameplates, Plater)."
 
-	-- L.custom_off_nameplate_tormented = "Tormented Nameplate Icon"
-	-- L.custom_off_nameplate_tormented_desc = "Show an icon on the nameplate of Mawsworn that have Tormented.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	L.custom_off_nameplate_tormented = "Icône pour les bars de nom qui sont tourmentée"
+	L.custom_off_nameplate_tormented_desc = "Affiche une icône sur les bar de nom des antreliges étant tourmentées.\n\nIl est obligatoire d'utiliser l'affiche des bars de nom ennemie et d'addon de bar de nom compatible (KuiNameplates, Plater)."
 
-	-- L.cones = "Cones" -- Torment
-	-- L.dance = "Dance" -- Encore of Torment
-	-- L.brands = "Brands" -- Brand of Torment
-	-- L.brand = "Brand" -- Single Brand of Torment
-	-- L.spike = "Spike" -- Short for Agonizing Spike
-	-- L.chains = "Chains" -- Hellscream
-	-- L.chain = "Chain" -- Soul Manacles
-	-- L.souls = "Souls" -- Rendered Soul
+	L.cones = "Cones" -- Torment
+	L.dance = "Dancer" -- Encore of Torment
+	L.brands = "Marques" -- Brand of Torment
+	L.brand = "Marque" -- Single Brand of Torment
+	L.spike = "Agoniseur" -- Short for Agonizing Spike
+	L.chains = "Chaines" -- Hellscream
+	L.chain = "Chaine" -- Soul Manacles
+	L.souls = "Ames" -- Rendered Soul
 
-	-- L.chains_remaining = "%d Chains remaining"
-	-- L.all_broken = "All Chains broken"
+	L.chains_remaining = "%d Chaînes restantes"
+	L.all_broken = "Toutes les chaines sont brisées"
 end
 
 L = BigWigs:NewBossLocale("Painsmith Raznal", "frFR")
 if L then
-	-- L.hammer = "Hammer" -- Short for Rippling Hammer
-	-- L.axe = "Axe" -- Short for Cruciform Axe
-	-- L.scythe = "Scythe" -- Short for Dualblade Scythe
-	-- L.trap = "Trap" -- Short for Flameclasp Trap
-	-- L.chains = "Chains" -- Short for Shadowsteel Chains
-	-- L.embers = "Embers" -- Short for Shadowsteel Embers
-	-- L.adds_embers = "Embers (%d) - Adds Next!"
-	-- L.adds_killed = "Adds killed in %.2fs"
-	-- L.spikes = "Spiked Death" -- Soft enrage spikes
+	L.hammer = "Marteau" -- Short for Rippling Hammer
+	L.axe = "Hache" -- Short for Cruciform Axe
+	L.scythe = "Faux" -- Short for Dualblade Scythe
+	L.trap = "Piège" -- Short for Flameclasp Trap
+	L.chains = "Chaines" -- Short for Shadowsteel Chains
+	L.embers = "Horreurs" -- Short for Shadowsteel Embers
+	L.adds_embers = "Horreur (%d) - Prochaine vague!"
+	L.adds_killed = "Vague terminée en %.2fs"
+	L.spikes = "Balles cloutées" -- Soft enrage spikes
 end
 
 L = BigWigs:NewBossLocale("Guardian of the First Ones", "frFR")
 if L then
-	--L.custom_on_stop_timers = "Always show ability bars"
-	--L.custom_on_stop_timers_desc = "The Guardian can delay its abilities. When this option is enabled, the bars for those abilities will stay on your screen."
+	L.custom_on_stop_timers = "Toujours afficher les bars des techniques"
+	L.custom_on_stop_timers_desc = "Le gardin peut temporisé ses techniques. Lorsque cette option est active, les bars pour ces techniques resteront sur votre écran."
 
-	-- L.bomb_missed = "%dx Bombs Missed"
+	L.bomb_missed = "%dx noyaux ratés"
 end
 
 L = BigWigs:NewBossLocale("Fatescribe Roh-Kalo", "frFR")
 if L then
-	-- L.rings = "Rings"
-	-- L.rings_active = "Rings Active" -- for when they activate/are movable
-	-- L.runes = "Runes"
+	L.rings = "Anneaux"
+	L.rings_active = "Anneaux actif" -- for when they activate/are movable
+	L.runes = "Runes"
 
-	-- L.grimportent_countdown = "Countdown"
-	-- L.grimportent_countdown_desc = "Countdown for the players who are Affected by Grim Portent"
-	-- L.grimportent_countdown_bartext = "Go to rune!"
+	L.grimportent_countdown = "Compte à rebours"
+	L.grimportent_countdown_desc = "Compte à rebours pour les joueurs qui sont affecté par Sombre Présage"
+	L.grimportent_countdown_bartext = "Dans la rune!"
 end
 
 L = BigWigs:NewBossLocale("Kel'Thuzad", "frFR")
 if L then
-	-- L.spikes = "Spikes" -- Short for Glacial Spikes
-	-- L.spike = "Spike"
-	-- L.miasma = "Miasma" -- Short for Sinister Miasma
+	L.spikes = "Eclats" -- Short for Glacial Spikes
+	L.spike = "Eclat"
+	L.miasma = "Miasme" -- Short for Sinister Miasma
 
-	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
-	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate of Frostbound Devoted that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	L.custom_on_nameplate_fixate = "Icon pour les bars de nom Ame Marquée"
+	L.custom_on_nameplate_fixate_desc = "Affiche une icône sur les bars de nom des Fidèle givre-liée qui sont fixé sur vous.\n\nIl est obligatoire d'utiliser l'affiche des bars de nom ennemie et d'addon de bar de nom compatible (KuiNameplates, Plater)."
 end
 
 L = BigWigs:NewBossLocale("Sylvanas Windrunner", "frFR")
 if L then
-	-- L.chains_active = "Chains Active"
-	-- L.chains_active_desc = "Show a bar for when the Chains of Domination activate"
+	L.chains_active = "Chaînes actives"
+	L.chains_active_desc = "Affiché une bar quand la technique Chaînes de Domination est active"
 
-	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
-	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate of Dark Sentinels that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	L.custom_on_nameplate_fixate = "Icon sur les bars de nom fixé"
+	L.custom_on_nameplate_fixate_desc = "Affiche une icône sur les bars de nom des Sentinelles sombre qui vous fixe.\n\nIl est obligatoire d'utiliser l'affiche des bars de nom ennemie et d'addon de bar de nom compatible (KuiNameplates, Plater)."
 
-	-- L.chains = "Chains" -- Short for Domination Chains
-	-- L.chain = "Chain" -- Single Domination Chain
-	-- L.darkness = "Darkness" -- Short for Veil of Darkness
-	-- L.arrow = "Arrow" -- Short for Wailing Arrow
-	-- L.wave = "Wave" -- Short for Haunting Wave
-	-- L.dread = "Dread" -- Short for Crushing Dread
-	-- L.orbs = "Orbs" -- Dark Communion
-	-- L.curse = "Curse" -- Short for Curse of Lethargy
-	-- L.pools = "Pools" -- Banshee's Bane
-	-- L.scream = "Scream" -- Banshee Scream
+	L.chains = "Chaînes" -- Short for Domination Chains
+	L.chain = "Chaîne" -- Single Domination Chain
+	L.darkness = "Ténèbre" -- Short for Veil of Darkness
+	L.arrow = "Flèche" -- Short for Wailing Arrow
+	L.wave = "Vague" -- Short for Haunting Wave
+	L.dread = "Effroi" -- Short for Crushing Dread
+	L.orbs = "Orbes" -- Dark Communion
+	L.curse = "Malédiction" -- Short for Curse of Lethargy
+	L.pools = "Taches" -- Banshee's Bane
+	L.scream = "Cri" -- Banshee Scream
 
-	-- L.knife_fling = "Knives out!" -- "Death-touched blades fling out"
+	L.knife_fling = "Dagues volantes!" -- "Death-touched blades fling out"
 end
 
