77e1ca2274e8f3931f3f3d8d71bb256cd99d463e
Sacha Canicio <sacha.canicio@gmail.com>
2022-08-06 12:58:54 +0200

    Some of French translation for Sepulcher of the FirstOnes

--- SepulcherOfTheFirstOnes/Locales/frFR.lua
+++ SepulcherOfTheFirstOnes/Locales/frFR.lua
@@ -1,37 +1,37 @@
 local L = BigWigs:NewBossLocale("Artificer Xy'mox v2", "frFR")
 if not L then return end
 if L then
-	--L.traps = "Traps" -- Stasis Trap
-	--L.sparknova = "Sparknova" -- Hyperlight Sparknova
-	--L.relocation = "Tank Bomb" -- Glyph of Relocation
-	--L.relocation_count = "%s S%d (%d)" -- Tank Bomb S1 (1) // Tank Bomb (stage)(count)
-	--L.wormholes = "Wormholes" -- Interdimensional Wormholes
-	--L.wormhole = "Wormhole" -- Interdimensional Wormhole
-	--L.rings = "Rings S%d" -- Rings S1 // Forerunner Rings Stage 1/2/3/4
+	L.traps = "Pièges" -- Stasis Trap
+	L.sparknova = "Nova éclair" -- Hyperlight Sparknova
+	L.relocation = "Tank bombe" -- Glyph of Relocation
+	L.relocation_count = "%s S%d (%d)" -- Tank Bomb S1 (1) // Tank Bomb (stage)(count)
+	L.wormholes = "Déchirures dimensionnelle" -- Interdimensional Wormholes
+	L.wormhole = "Déchirure dimensionnelle" -- Interdimensional Wormhole
+	L.rings = "Anneaux S%d" -- Rings S1 // Forerunner Rings Stage 1/2/3/4
 end
 
 L = BigWigs:NewBossLocale("Dausegne, the Fallen Oracle", "frFR")
 if L then
-	--L.staggering_barrage = "Barrage" -- Staggering Barrage
-	--L.domination_core = "Add" -- Domination Core
-	--L.obliteration_arc = "Arc" -- Obliteration Arc
+	L.staggering_barrage = "Barrage" -- Staggering Barrage
+	L.domination_core = "Add" -- Domination Core
+	L.obliteration_arc = "Arc" -- Obliteration Arc
 
-	--L.disintergration_halo = "Rings" -- Disintegration Halo
-	--L.rings_x = "Rings x%d"
-	--L.rings_enrage = "Rings (Enrage)"
-	--L.ring_count = "Ring (%d/%d)"
+	L.disintergration_halo = "Anneaux" -- Disintegration Halo
+	L.rings_x = "Anneaux x%d"
+	L.rings_enrage = "Anneaux (Enrage)"
+	L.ring_count = "Anneau (%d/%d)"
 
-	--L.shield_removed = "%s removed after %.1fs" -- "Shield removed after 1.1s" s = seconds
+	L.shield_removed = "%s retiré après %.1fs" -- "Shield removed after 1.1s" s = seconds
 end
 
 L = BigWigs:NewBossLocale("Prototype Pantheon", "frFR")
 if L then
-	--L.necrotic_ritual = "Ritual"
-	--L.runecarvers_deathtouch = "Deathtouch"
-	--L.windswept_wings = "Winds"
-	--L.wild_stampede = "Stampede"
-	--L.withering_seeds = "Seeds"
-	--L.hand_of_destruction = "Hand"
+	L.necrotic_ritual = "Rituel"
+	L.runecarvers_deathtouch = "Toucher de la mort"
+	L.windswept_wings = "Vents"
+	L.wild_stampede = "Ruée sauvage"
+	L.withering_seeds = "Graines"
+	L.hand_of_destruction = "Main de la destruction"
 end
 
 L = BigWigs:NewBossLocale("Lihuvim, Principal Architect", "frFR")
@@ -47,24 +47,24 @@ end
 
 L = BigWigs:NewBossLocale("Anduin Wrynn", "frFR")
 if L then
-	--L.custom_off_repeating_blasphemy = "Repeating Blasphemy"
-	--L.custom_off_repeating_blasphemy_desc = "Repeating Blasphemy say messages with icons {rt1}, {rt3} to find matches to remove your debuffs."
+	L.custom_off_repeating_blasphemy = "Répétition de blasphème"
+	L.custom_off_repeating_blasphemy_desc = "Répétition de blasphème écrit des messages avec les icônes {rt1}, {rt3} afin de trouver celui qui correspond à votre debuff."
 
-	--L.kingsmourne_hungers = "Kingsmourne"
-	--L.blasphemy = "Marks"
-	--L.befouled_barrier = "Barrier"
-	--L.wicked_star = "Star"
-	--L.domination_word_pain = "DW:Pain"
-	--L.army_of_the_dead = "Army"
+	L.kingsmourne_hungers = "Deuilleroi"
+	L.blasphemy = "Marque"
+	L.befouled_barrier = "Barrière"
+	L.wicked_star = "Étoile"
+	L.domination_word_pain = "Mot de domination:Douleur"
+	L.army_of_the_dead = "Armée des morts"
 	--L.grim_reflections = "Fear Adds"
-	--L.march_of_the_damned = "Walls"
-	--L.dire_blasphemy = "Marks"
-	--L.beacon_of_hope = "Beacon"
+	L.march_of_the_damned = "Murs"
+	L.dire_blasphemy = "Marque"
+	L.beacon_of_hope = "Signal"
 
-	--L.remnant_active = "Remnant Active"
+	L.remnant_active = "Vestige Actif"
 end
 
 L = BigWigs:NewBossLocale("Skolex, the Insatiable Ravener", "frFR")
 if L then
-	--L.tank_combo_desc = "Timer for Riftmaw/Rend casts at 100 energy."
+	L.tank_combo_desc = "Timer avant Mâche-faille/Pourgendre lancé a 100 d'énergie."
 end
