cc39399ee1a33698d3cfe80340d0448ddf9ff434
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2022-08-10 23:35:06 +0200

    Update deDE (#1253)

--- CastleNathria/Locales/deDE.lua
+++ CastleNathria/Locales/deDE.lua
@@ -168,11 +168,11 @@ end
 
 L = BigWigs:NewBossLocale("Castle Nathria Affixes", "deDE")
 if L then
-	--L.custom_on_bar_icon = "Bar Icon"
-	--L.custom_on_bar_icon_desc = "Show the Fated Raid icon on bars."
+	L.custom_on_bar_icon = "Leistensymbol"
+	L.custom_on_bar_icon_desc = "Zeigt das Schicksalhafte Schlachtzugssymbol in den Leisten."
 
-	--L.chaotic_essence = "Essence"
+	L.chaotic_essence = "Essenz"
 	L.creation_spark = "Funken"
 	L.protoform_barrier = "Barriere"
-	--L.reconfiguration_emitter = "Interrupt Add"
+	L.reconfiguration_emitter = "Zauber-Add"
 end
