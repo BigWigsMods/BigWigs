a4c3f78f78b86376753d68e13bc5a172fffc4bfc
Nick Towle <ntowle@gmail.com>
2022-09-04 19:29:44 -0400

    don't write carriage returns

--- gen_option_values.lua
+++ gen_option_values.lua
@@ -322,10 +322,11 @@ local function dumpValues(path, name, options_table)
 
 	if data:gsub("\r", "") ~= old_data:gsub("\r", "") then
 		if not opt.dryrun then
-			f = io.open(file, "wb")
+			f = io.open(file, "w")
 			if not f then
 				error(string.format("    %s: File not found!", file))
 			else
+				data = data:gsub("\r", "")
 				f:write(data)
 				f:close()
 				warn("    Updated " .. file)
