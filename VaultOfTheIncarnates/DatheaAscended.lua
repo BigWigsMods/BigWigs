30cd43313102f0732bf6e20c0006f82d60bc7d2e
Justwait <mngrootwow@gmail.com>
2022-10-01 00:28:17 +0200

    VaultOfTheIncarnates/DatheaAscended: Initial test updates

--- VaultOfTheIncarnates/DatheaAscended.lua
+++ VaultOfTheIncarnates/DatheaAscended.lua
@@ -13,6 +13,8 @@ mod:SetRespawnTime(30)
 -- Locals
 --
 
+local crosswindsCount = 1
+local zephyrSlamCount = 1
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -34,7 +36,7 @@ function mod:GetOptions()
 		387849, -- Coalescing Storm
 
 		387943, -- Diverted Essence
-		388562, -- Unstable Gusts
+		381688, -- Unstable Gusts
 		{385812, "TANK"}, -- Aerial Slash
 
 		384273, -- Storm Bolt
@@ -42,7 +44,7 @@ function mod:GetOptions()
 
 		388302, -- Raging Burst
 		{391686, "SAY"}, -- Empowered Conductive Mark
-		"custom_on_empowered_conductive_mark_yell",
+		--"custom_on_empowered_conductive_mark_yell",
 		376943, -- Cyclone
 		388410, -- Crosswinds
 		{375580, "TANK"}, -- Zephyr Slam
@@ -54,9 +56,11 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
+
 	self:Log("SPELL_CAST_START", "CoalescingStorm", 387849)
 	self:Log("SPELL_CAST_START", "DivertedEssence", 387943)
-	self:Log("SPELL_CAST_SUCCESS", "UnstableGusts", 388562)
+	self:Log("SPELL_CAST_SUCCESS", "UnstableGusts", 381688)
 	self:Log("SPELL_CAST_START", "AerialSlash", 385812)
 	self:Log("SPELL_CAST_START", "StormBolt", 384273)
 	self:Log("SPELL_AURA_APPLIED", "StaticClingApplied", 390450)
@@ -73,17 +77,31 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
+	crosswindsCount = 1
+	zephyrSlamCount = 1
 
+	self:CDBar(391686, 4) -- Empowered Conductive Mark
+	self:CDBar(388302, 7) -- Raging Burst
+	self:CDBar(375580, 16) -- ZephyrSlam
+	self:CDBar(388410, 25) -- Crosswinds
+	self:CDBar(376943, 37) -- Cyclone
+	self:CDBar(387849, 75) -- Coalescing Storm
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
+	if spellId == 391595 then -- Empowered Conductive Mark
+		self:CDBar(391686, 28)
+	end
+end
+
 function mod:CoalescingStorm(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "long")
-	--self:Bar(args.spellId, 20)
+	self:CDBar(args.spellId, 80)
 end
 
 function mod:DivertedEssence(args)
@@ -120,15 +138,18 @@ end
 
 function mod:StaticClingApplied(args)
 	if self:Me(args.destGUID) then
-		self:StackMessage(args.spellId, "blue", args.destName, args.amount, args.amount)
-		self:PlaySound(args.spellId, "alarm")
+		local amount = args.amount or 1
+		if amount % 3 == 0 or amount > 10 then
+			self:StackMessage(args.spellId, "blue", args.destName, args.amount, args.amount)
+			self:PlaySound(args.spellId, "alarm")
+		end
 	end
 end
 
 function mod:RagingBurst(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
-	--self:Bar(args.spellId, 20)
+	self:CDBar(args.spellId, 80)
 end
 
 function mod:EmpoweredConductiveMarkApplied(args)
@@ -138,8 +159,8 @@ function mod:EmpoweredConductiveMarkApplied(args)
 		self:PlaySound(args.spellId, "warning")
 		if amount == 1 then -- Initial Say
 			self:Say(args.spellId)
-		elseif amount > 4 and self:GetOption("custom_on_empowered_conductive_mark_yell") then -- start stack yells from 5+
-			self:Yell(false, L.marks_on_me:format(amount), true)
+		-- elseif amount > 4 and self:GetOption("custom_on_empowered_conductive_mark_yell") then -- start stack yells from 5+
+		-- 	self:Yell(false, L.marks_on_me:format(amount), true)
 		end
 	end
 end
@@ -147,13 +168,14 @@ end
 function mod:Cyclone(args)
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alarm")
-	--self:Bar(args.spellId, 20)
+	self:CDBar(args.spellId, 80)
 end
 
 function mod:Crosswinds(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
-	--self:Bar(args.spellId, 20)
+	crosswindsCount = crosswindsCount + 1
+	self:CDBar(args.spellId, crosswindsCount % 2 == 1 and 45 or 36)
 end
 
 function mod:ZephyrSlam(args)
@@ -162,7 +184,8 @@ function mod:ZephyrSlam(args)
 	if self:Tanking(bossUnit) then
 		self:PlaySound(args.spellId, "alert")
 	end
-	--self:Bar(args.spellId, 20)
+	zephyrSlamCount = zephyrSlamCount + 1
+	self:CDar(args.spellId, zephyrSlamCount % 2 == 1 and 17 or 28)
 end
 
 function mod:ZephyrSlamApplied(args)
