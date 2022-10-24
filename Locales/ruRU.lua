cc5cc58d65647d701ce638dd4829070a8417ff49
IKnewOne <41551327+IKnewOne@users.noreply.github.com>
2022-10-24 21:00:44 +0300

    Update ruRU (#1279)

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -41,7 +41,7 @@ L.noBossMod = "Нет аддона:"
 L.offline = "Не в сети"
 
 L.missingPlugin = "Обратите внимание, что эта зона требует дополнение |cFF436EEE%s|r для показа таймеров."
---L.missingAddOn = "The |cFF436EEE%s|r addon is missing!"
+L.missingAddOn = "Отсутствует модификация |cFF436EEE%s|r."
 L.disabledAddOn = "У вас выключена модификация |cFF436EEE%s|r, таймеры не будут показываться."
 
 L.removeAddOn = "Пожалуйста, удалите '|cFF436EEE%s|r', ему на смену пришло '|cFF436EEE%s|r'."
