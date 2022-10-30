a26e5adcdc0da1e6223e115d8dc1f7e660547f1b
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2022-10-30 08:31:41 +0100

    Update deDE.lua

--- VaultOfTheIncarnates/Locales/deDE.lua
+++ VaultOfTheIncarnates/Locales/deDE.lua
@@ -1,27 +1,56 @@
 local L = BigWigs:NewBossLocale("Eranog", "deDE")
 if not L then return end
 if L then
+	L.custom_on_nameplate_fixate = "Fixieren-Symbol an gegnerischen Namensplaketten"
+	L.custom_on_nameplate_fixate_desc = "Zeigt ein Symbol an der Namensplakette des Dich fixierenden rasenden Tarasek an.\n\nBenötigt die Nutzung von Namensplaketten sowie ein unterstütztes Addon (KuiNameplates, Plater)."
 
+	L.flamerifts = "Adds" -- Multiple Flamerifts
+	L.flamerift = "Add" -- Rift on X
+	L.molten_cleave = "Frontal"
+	L.incinerating_roar = "Brüllen"
+	L.molten_spikes = "Stacheln"
+	L.collapsing_army = "Armee"
+	L.greater_flamerift = "Mythic Add"
+	L.leaping_flames = "Flammen"
 end
 
 L = BigWigs:NewBossLocale("Terros", "deDE")
 if L then
-
+	L.rock_blast = "Soak"
+	L.resonating_annihilation = "Vernichtung"
+	L.awakened_earth = "Säule"
+	L.shattering_impact = "Einschlag"
+	L.concussive_slam = "Tank Linie"
+	L.infused_fallout = "Staub"
 end
 
 L = BigWigs:NewBossLocale("The Primal Council", "deDE")
 if L then
-
+	L.conductive_mark = "Zeichen"
 end
 
 L = BigWigs:NewBossLocale("Sennarth, The Cold Breath", "deDE")
 if L then
-
+	L.ascend = "Aufsteigen"
+	L.ascend_desc = "Sennarth steigt vom Raum zur Spitze des Eises auf."
+	L.chilling_blast = "Verteilen"
+	L.frost_expulsion = "Explosion"
+	L.freezing_breath = "Add Atem"
+	L.webs = "Spinnweben"
+	L.web = "Spinnwebe"
+	L.gossamer_burst = "Heranziehen"
+	L.repelling_burst = "Zurückstoßen"
 end
 
 L = BigWigs:NewBossLocale("Dathea, Ascended", "deDE")
 if L then
+	L.marks_on_me = "%d Zeichen" -- {Stacks} Conductive Mark on the player
 
+  L.conductive_marks = "Zeichen"
+	L.conductive_mark = "Zeichen"
+	L.raging_burst = "Neue Tornados"
+	L.cyclone = "Heranziehen"
+	L.crosswinds = "Bewegende Tornados"
 end
 
 L = BigWigs:NewBossLocale("Kurog Grimtotem", "deDE")
@@ -31,7 +60,7 @@ end
 
 L = BigWigs:NewBossLocale("Broodkeeper Diurna", "deDE")
 if L then
-
+  L.eggs_remaining = "%d Eier verbleiben!"
 end
 
 L = BigWigs:NewBossLocale("Raszageth the Storm-Eater", "deDE")
