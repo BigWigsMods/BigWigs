0c0628a7392412f4a64ea276a9cb5d1cccfcb9ea
dongminrocks <39166823+dongminrocks@users.noreply.github.com>
2022-12-15 10:10:32 +0900

    Update koKR (#1299)

--- VaultOfTheIncarnates/Locales/koKR.lua
+++ VaultOfTheIncarnates/Locales/koKR.lua
@@ -1,63 +1,63 @@
 local L = BigWigs:NewBossLocale("Eranog", "koKR")
 if not L then return end
 if L then
-	-- L.custom_on_nameplate_fixate = "Fixate Nameplate Icon"
-	-- L.custom_on_nameplate_fixate_desc = "Show an icon on the nameplate of Frenzied Tarasek that are fixed on you.\n\nRequires the use of Enemy Nameplates and a supported nameplate addon (KuiNameplates, Plater)."
+	 L.custom_on_nameplate_fixate = "시선 고정 네임플레이트 아이콘"
+	 L.custom_on_nameplate_fixate_desc = "당신에게 시선이 고정된 타라세크의 네임플레이트에 아이콘을 표시합니다.\n\n 네임플레이트를 지원하는 애드온(KuiNameplates, Plater)을 사용하고 적 네임플레이트가 활성화된 상태여야함."
 
-	-- L.molten_cleave = "Frontal"
-	-- L.incinerating_roar = "Roar"
-	-- L.molten_spikes = "Spikes"
-	-- L.collapsing_army = "Army"
-	-- L.greater_flamerift = "Mythic Add"
-	-- L.leaping_flames = "Flames"
+	 L.molten_cleave = "전방기"
+	 L.incinerating_roar = "포효"
+	 L.molten_spikes = "쐐기"
+	 L.collapsing_army = "쫄 소환"
+	 L.greater_flamerift = "신화 쫄"
+	 L.leaping_flames = "불꽃"
 end
 
 L = BigWigs:NewBossLocale("Terros", "koKR")
 if L then
-	-- L.rock_blast = "Soak"
-	-- L.resonating_annihilation = "Annihilation"
-	-- L.awakened_earth = "Pillar"
-	-- L.shattering_impact = "Slam"
-	-- L.concussive_slam = "Tank Line"
+	 L.rock_blast = "같이 맞아주기"
+	 L.resonating_annihilation = "절멸 - 잔해 조심"
+	 L.awakened_earth = "기둥 생성"
+	 L.shattering_impact = "충격 - 잔해 조심"
+	 L.concussive_slam = "탱커 조심"
 	-- L.infused_fallout = "Dust"
 end
 
 L = BigWigs:NewBossLocale("The Primal Council", "koKR")
 if L then
-	-- L.primal_blizzard = "Blizzard" -- Primal Blizzard
-	-- L.earthen_pillars = "Pillars" -- Earthen Pillars
-	-- L.meteor_axes = "Axes" -- Meteor Axes
-	-- L.meteor_axe = "Axe" -- Singular
-	-- L.meteor_axes_melee = "Melee Axe"
-	-- L.meteor_axes_ranged = "Ranged Axe"
-	-- L.conductive_marks = "Marks" -- Conductive Marks
-	-- L.conductive_mark = "Mark" -- Singular
+	 L.primal_blizzard = "눈보라" -- Primal Blizzard
+	 L.earthen_pillars = "기둥" -- Earthen Pillars
+	 L.meteor_axes = "도끼" -- Meteor Axes
+	 L.meteor_axe = "도끼" -- Singular
+	 L.meteor_axes_melee = "근딜 도끼"
+	 L.meteor_axes_ranged = "원딜 도끼"
+	 L.conductive_marks = "표식" -- Conductive Marks
+	 L.conductive_mark = "표식" -- Singular
 
-	-- L.custom_off_chain_lightning = "Chain Lightning is off by default, enable this to enable chain lightning."
+	 L.custom_off_chain_lightning = "연쇄 번개는 기본적으로 꺼져있습니다. 활성화해서 연쇄 번개를 활성화하십시오."
 
-	-- L.custom_on_stop_timers = "Always show ability bars"
-	-- L.custom_on_stop_timers_desc = "Abilities that will always be shown: Conductive Mark"
+	 L.custom_on_stop_timers = "항상 능력 바 표시Always show ability bars"
+	 L.custom_on_stop_timers_desc = "항상 표시될 능력: 전도성 표식"
 end
 
 L = BigWigs:NewBossLocale("Sennarth, The Cold Breath", "koKR")
 if L then
-	-- L.ascend = "Ascend"
-	-- L.ascend_desc = "Sennarth ascends the room towards the frozen percipice."
-	-- L.chilling_blast = "Spread"
-	-- L.freezing_breath = "Add Breath"
-	-- L.webs = "Webs"
-	-- L.web = "Web"
-	-- L.gossamer_burst = "Grip"
-	-- L.repelling_burst = "Pushback"
+	 L.ascend = "올라감"
+	 L.ascend_desc = "세나스가 얼어붙은 절벽쪽으로 올라갑니다."
+	 L.chilling_blast = "산개"
+	 L.freezing_breath = "쫄 브레스"
+	 L.webs = "거미줄"
+	 L.web = "거미줄"
+	 L.gossamer_burst = "낙사 조심"
+	 L.repelling_burst = "넉백"
 end
 
 L = BigWigs:NewBossLocale("Dathea, Ascended", "koKR")
 if L then
-	-- L.conductive_marks = "Marks"
-	-- L.conductive_mark = "Mark"
-	-- L.raging_burst = "New Tornadoes"
-	-- L.cyclone = "Pull In"
-	-- L.crosswinds = "Moving Tornadoes"
+	 L.conductive_marks = "표식"
+	 L.conductive_mark = "표식"
+	 L.raging_burst = "회오리 생성"
+	 L.cyclone = "당기기"
+	 L.crosswinds = "회오리 재배치"
 end
 
 L = BigWigs:NewBossLocale("Kurog Grimtotem", "koKR")
@@ -75,44 +75,44 @@ if L then
 	-- L.ultimate_desc = "Display timers for Ultimate abilities (Searing Carnage, Absolute Zero, Seismic Rupture, Thundering Strike) when we don't know what alter the boss is at."
 	-- L.ultimate_bartext = "%s [Ult]" -- {Spell} [Ult]
 
-	-- -- Fire
-	-- L.magma_burst = "Pools"
-	-- L.molten_rupture = "Waves"
-	-- L.searing_carnage = "Dance"
+	 -- 불
+	 L.magma_burst = "웅덩이 생성"
+	 L.molten_rupture = "웨이브 조심"
+	 L.searing_carnage = "살육"
 
-	-- -- Frost
-	-- L.biting_chill = "Chill DoT"
-	-- L.frigid_torrent = "Orbs"
-	-- L.absolute_zero = "Soaks"
-	-- L.absolute_zero_melee = "Melee Soak"
-	-- L.absolute_zero_ranged = "Ranged Soak"
+	 -- 냉기
+	 L.biting_chill = "추위 도트"
+	 L.frigid_torrent = "구슬 방출"
+	 L.absolute_zero = "같이 맞아주기"
+	 L.absolute_zero_melee = "근딜 나눠맞기"
+	 L.absolute_zero_ranged = "원딜 나눠맞기"
 
-	-- -- Earth
-	-- L.enveloping_earth = "Heal Absorb"
-	-- L.erupting_bedrock = "Quakes"
+	 -- 대지
+	 L.enveloping_earth = "치유 흡수"
+	 L.erupting_bedrock = "지진 넉백"
 
-	-- -- Storm
-	-- L.lightning_crash = "Zaps"
-	-- L.thundering_strike = "Soaks"
+	 -- 폭풍
+	 L.lightning_crash = "번개 충돌"
+	 L.thundering_strike = "맞아주기"
 
-	-- -- General
-	-- L.primal_attunement = "Soft Enrage"
+	 -- 일반
+	 L.primal_attunement = "미니 광폭"
 
-	-- -- Stage 2
-	-- L.violent_upheaval = "Pillars"
+	 -- Stage 2
+	 L.violent_upheaval = "기둥 "
 end
 
 L = BigWigs:NewBossLocale("Broodkeeper Diurna", "koKR")
 if L then
-	-- L.eggs_remaining = "%d Eggs Remaining!"
-	-- L.broodkeepers_bond = "Eggs Remaining"
-	-- L.greatstaff_of_the_broodkeeper = "Greatstaff"
-	-- L.greatstaffs_wrath = "Laser"
-	-- L.clutchwatchers_rage = "Rage"
-	-- L.rapid_incubation = "Infuse Eggs"
-	-- L.icy_shroud = "Heal Absorb"
-	-- L.broodkeepers_fury = "Fury"
-	-- L.frozen_shroud = "Root Absorb"
+	 L.eggs_remaining = "알 %d 개 남음!"
+	 L.broodkeepers_bond = "알 남아있음"
+	 L.greatstaff_of_the_broodkeeper = "대지팡이"
+	 L.greatstaffs_wrath = "레이저"
+	 L.clutchwatchers_rage = "분노"
+	 L.rapid_incubation = "급속 배양"
+	 L.icy_shroud = "치유량 흡수 껍질"
+	 L.broodkeepers_fury = "분노"
+	 L.frozen_shroud = "속박"
 end
 
 L = BigWigs:NewBossLocale("Raszageth the Storm-Eater", "koKR")
