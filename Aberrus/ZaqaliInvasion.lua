7f622a2af5a3450d94087eeef6cb2b8a2ba13913
Kyle Buller <bullerk@gmail.com>
2023-03-23 18:48:52 -0500

    Aberrus/ZaqaliInvasion: Updates

--- Aberrus/ZaqaliInvasion.lua
+++ Aberrus/ZaqaliInvasion.lua
@@ -14,9 +14,11 @@ mod:SetStage(1)
 -- Locals
 --
 
+local heavyCudgelCount = 1
 local devastatingSunderCount = 1
 local flaringFirestormCount = 1
 local zaqaliAideCount = 1
+local magmaMysticCount = 1
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -24,6 +26,7 @@ local zaqaliAideCount = 1
 
 local L = mod:GetLocale()
 if L then
+	L.final_assault_soon = "Final Assault soon" -- hopefully there will be some spell/aura to use instead
 end
 
 --------------------------------------------------------------------------------
@@ -32,33 +35,44 @@ end
 
 function mod:GetOptions()
 	return {
-		-- Stage 1
-		{401241, "TANK"}, -- Molten Swing
-		{401258, "TANK"}, -- Heavy Cudgel
-		397515, -- Devastating Sunder
+		"stages",
+		-- Warlord Kagni
+		{401241, "TANK_HEALER"}, -- Molten Swing
+		401258, -- Heavy Cudgel
+		398938, -- Devastating Leap
+		-- Ignara
 		407024, -- Flaring Firestorm
-		-- Stage 2
-		404382, -- Zaqali Aide
+		407017, -- Vigorous Gale
+		-- Magma Mystic
+		397383, -- Molten Havoc
+		409271, -- Lava Flow
+		-- Zaqali Aide
+		404382,
 		{401867, "SAY", "SAY_COUNTDOWN"}, -- Volcanic Shield
 		{401108, "SAY", "SAY_COUNTDOWN"}, -- Phoenix Rush
 		401401, -- Blazing Spear
-		397383, -- Molten Gateway
-		397403, -- Lava Flow
-		397505, -- Volcanic Ground
-		407017, -- Vigorous Gale
+	}, {
+		[401241] = -26616, -- Warlord Kagni
+		[407024] = -26602, -- Ignara
+		[397383] = -26217, -- Magma Mystic
+		[404382] = -26274, -- Zaqali Aide
 	}
 end
 
 function mod:OnBossEnable()
-	-- Stage 1
-	self:Log("SPELL_CAST_START", "MoltenSwing", 401241)
+	-- Warlord Kagni
+	self:Log("SPELL_CAST_SUCCESS", "MoltenSwing", 401241)
 	self:Log("SPELL_AURA_APPLIED", "MoltenSwingApplied", 401241)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "MoltenSwingApplied", 401241)
 	self:Log("SPELL_CAST_START", "HeavyCudgel", 401258)
-	self:Log("SPELL_CAST_SUCCESS", "DevastatingSunder", 397515)
+	self:Log("SPELL_CAST_SUCCESS", "DevastatingLeap", 398938)
+	-- Ignara
 	self:Log("SPELL_CAST_SUCCESS", "FlaringFirestorm", 407024)
-
-	-- Stage 2
+	self:Log("SPELL_CAST_SUCCESS", "VigorousGale", 407017)
+	-- Magma Mystic
+	self:Log("SPELL_CAST_START", "MoltenHavoc", 397383)
+	self:Log("SPELL_CAST_START", "LavaFlow", 409271)
+	-- Zaqali Aide
 	self:Log("SPELL_CAST_START", "ZaqaliAide", 404382)
 	self:Log("SPELL_AURA_APPLIED", "VolcanicShieldApplied", 401867)
 	self:Log("SPELL_AURA_REMOVED", "VolcanicShieldRemoved", 401867)
@@ -66,70 +80,105 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "PhoenixRushApplied", 401108)
 	self:Log("SPELL_AURA_REMOVED", "PhoenixRushRemoved", 401108)
 	self:Log("SPELL_CAST_SUCCESS", "BlazingSpear", 401401)
-	self:Log("SPELL_CAST_START", "MoltenGateway", 397383)
-	self:Log("SPELL_CAST_START", "LavaFlow", 397403)
-	self:Log("SPELL_CAST_SUCCESS", "VigorousGale", 407017)
-
-	self:Log("SPELL_AURA_APPLIED", "GroundDamage", 397505) -- Volcanic Ground
-	self:Log("SPELL_PERIODIC_DAMAGE", "GroundDamage", 397505)
-	self:Log("SPELL_PERIODIC_MISSED", "GroundDamage", 397505)
 end
 
 function mod:OnEngage()
+	heavyCudgelCount = 1
 	devastatingSunderCount = 1
 	flaringFirestormCount = 1
-
-	--self:Bar(397515, 30, CL.count:format(self:SpellName(397515), devastatingSunderCount)) -- Devastating Sunder
-	--self:Bar(407024, 30, CL.count:format(self:SpellName(407024), flaringFirestormCount)) -- Flaring Firestorm
-
-	-- Stage 2
 	zaqaliAideCount = 1
+	magmaMysticCount = 1
+
+	-- self:Bar(401241, 30) -- Molten Swing
+	-- self:Bar(401258, 30, CL.count:format(self:SpellName(401258), heavyCudgelCount)) -- Heavy Cudgel
+	-- self:Bar(398938, 96, CL.count:format(self:SpellName(398938), devastatingSunderCount)) -- Devastating Leap
+	-- self:Bar(407024, 30, CL.count:format(self:SpellName(407024), flaringFirestormCount)) -- Flaring Firestorm
+	-- self:Bar(404382, 30, CL.count:format(self:SpellName(404382), zaqaliAideCount)) -- Zaqali Aide
+	-- self:Bar(397383, 30, CL.count:format(self:SpellName(397383), magmaMysticCount)) -- Molten Havoc
+
+	-- self:RegisterUnitEvent("UNIT_HEALTH", nil, "boss1")
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:MoltenSwing(args)
-	local bossUnit = self:UnitTokenFromGUID(args.sourceGUID)
-	self:Message(args.spellId, "purple")
-	if self:Tanking(bossUnit) then
-		self:PlaySound(args.spellId, "alert")
+function mod:UNIT_HEALTH(event, unit)
+	if self:GetHealth(unit) < 43 then -- "Final Assault" at 40%
+		self:Message("stages", "cyan", L.final_assault_soon, false)
+		self:PlaySound("stages", "info")
+		self:UnregisterUnitEvent(event, unit)
 	end
-	--self:CDBar(args.spellId, 17)
+end
+
+-- Warlord Kagni
+function mod:MoltenSwing(args)
+	-- self:Bar(args.spellId, 27)
 end
 
 function mod:MoltenSwingApplied(args)
-	local bossUnit = self:UnitTokenFromGUID(args.sourceGUID)
 	local amount = args.amount or 1
 	self:StackMessage(args.spellId, "purple", args.destName, amount, 2)
-	if amount > 2 and not self:Tanking(bossUnit) then -- Maybe swap?
+	local bossUnit = self:UnitTokenFromGUID(args.sourceGUID)
+	if amount > 2 and self:Tank() and not self:Tanking(bossUnit) then -- Maybe swap?
 		self:PlaySound(args.spellId, "warning")
+	elseif self:Me(args.destGUID) then
+		self:PlaySound(args.spellId, "alarm")
 	end
 end
 
 function mod:HeavyCudgel(args)
-	self:Message(args.spellId, "purple")
-	self:PlaySound(args.spellId, "alarm")
-	--self:Bar(args.spellId, 30)
+	self:StopBar(CL.count:format(args.spellName, heavyCudgelCount))
+	self:Message(args.spellId, "purple", CL.count:format(args.spellName, heavyCudgelCount))
+	self:PlaySound(args.spellId, "alert")
+	heavyCudgelCount = heavyCudgelCount + 1
+	-- self:Bar(args.spellId, 30, CL.count:format(args.spellName, heavyCudgelCount))
 end
 
-function mod:DevastatingSunder(args)
+function mod:DevastatingLeap(args)
 	self:StopBar(CL.count:format(args.spellName, devastatingSunderCount))
 	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, devastatingSunderCount))
 	self:PlaySound(args.spellId, "alert")
 	devastatingSunderCount = devastatingSunderCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, devastatingSunderCount))
+	-- self:Bar(args.spellId, 96, CL.count:format(args.spellName, devastatingSunderCount))
 end
 
+-- Ignara
 function mod:FlaringFirestorm(args)
 	self:StopBar(CL.count:format(args.spellName, flaringFirestormCount))
 	self:Message(args.spellId, "red", CL.count:format(args.spellName, flaringFirestormCount))
 	self:PlaySound(args.spellId, "alert")
 	flaringFirestormCount = flaringFirestormCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, flaringFirestormCount))
+	-- self:Bar(args.spellId, 30, CL.count:format(args.spellName, flaringFirestormCount))
 end
 
+function mod:VigorousGale(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "alarm")
+	-- self:Bar(args.spellId, 30)
+end
+
+-- Magma Mystic
+function mod:MoltenHavoc(args)
+	self:StopBar(CL.count:format(args.spellName, magmaMysticCount))
+	self:Message(args.spellId, "cyan", CL.count:format(args.spellName, magmaMysticCount))
+	self:PlaySound(args.spellId, "info")
+	magmaMysticCount = magmaMysticCount + 1
+	-- self:Bar(args.spellId, 60, CL.count:format(args.spellName, magmaMysticCount))
+end
+
+function mod:LavaFlow(args)
+	local canDo, ready = self:Interrupter(args.sourceGUID)
+	if canDo then
+		self:Message(args.spellId, "orange")
+		if ready then
+			self:PlaySound(args.spellId, "alert") -- interrupt
+		end
+	end
+	-- self:Bar(args.spellId, 30)
+end
+
+-- Zaqali Aide
 function mod:ZaqaliAide(args)
 	self:StopBar(CL.count:format(args.spellName, zaqaliAideCount))
 	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, zaqaliAideCount))
@@ -179,33 +228,4 @@ function mod:BlazingSpear(args)
 	self:Message(args.spellId, "red")
 	self:PlaySound(args.spellId, "alarm")
 	--self:Bar(args.spellId, 30)
-end
-
-function mod:MoltenGateway(args)
-	self:Message(args.spellId, "cyan")
-	self:PlaySound(args.spellId, "info")
-	--self:Bar(args.spellId, 30)
-end
-
-function mod:LavaFlow(args)
-	self:Message(args.spellId, "orange")
-	self:PlaySound(args.spellId, "alert")
-	--self:Bar(args.spellId, 30)
-end
-
-function mod:VigorousGale(args)
-	self:Message(args.spellId, "red")
-	self:PlaySound(args.spellId, "alarm")
-	--self:Bar(args.spellId, 30)
-end
-
-do
-	local prev = 0
-	function mod:GroundDamage(args)
-		if self:Me(args.destGUID) and args.time - prev > 2 then
-			prev = args.time
-			self:PlaySound(args.spellId, "underyou")
-			self:PersonalMessage(args.spellId, "underyou")
-		end
-	end
 end
