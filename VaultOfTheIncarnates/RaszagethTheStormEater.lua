533011a601ccea2f155a9c1dbd5163d6b63fd5d3
funkydude <funkydude@users.noreply.github.com>
2023-03-24 19:46:09 +0000

    VaultOfTheIncarnates/RaszagethTheStormEater: Add an extra heroic timer

--- VaultOfTheIncarnates/RaszagethTheStormEater.lua
+++ VaultOfTheIncarnates/RaszagethTheStormEater.lua
@@ -93,7 +93,7 @@ local timersHeroic = {
 		[377594] = {31.3, 43.5, 42.0}, -- Lightning Breath
 		[385574] = {65.9, 64.0, 22.0}, -- Tempest Wing
 		[377467] = {40.9, 60.0, 60.0}, -- Fulminating Charge
-		[386410] = {21.8, 30.0, 30.0, 30.0, 30.0}, -- Thunderous Blast
+		[386410] = {21.8, 30, 30, 30, 30, 30}, -- Thunderous Blast
 		[399713] = {25.9, 63.0, 34.0}, -- Magnetic Charge
 	},
 }
