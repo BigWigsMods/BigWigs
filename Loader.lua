0786a73563df0289567e3b0fa12e127df0e083f0
funkydude <funkydude@users.noreply.github.com>
2023-03-29 20:52:43 +0100

    April fools code

--- Loader.lua
+++ Loader.lua
@@ -77,6 +77,10 @@ do
 	end
 
 	BIGWIGS_RELEASE_STRING = releaseString
+	local dateTable = date("*t")
+	if dateTable.day and dateTable.month and dateTable.day == 1 and dateTable.month == 4 then
+		BIGWIGS_RELEASE_STRING = BIGWIGS_RELEASE_STRING:gsub("BigWigs", "DBM")
+	end
 	-- END: MAGIC PACKAGER VOODOO VERSION STUFF
 end
 
@@ -473,7 +477,12 @@ function dataBroker.OnClick(self, button)
 end
 
 function dataBroker.OnTooltipShow(tt)
-	tt:AddLine("BigWigs")
+	local dateTable = date("*t")
+	if dateTable.day and dateTable.month and dateTable.day == 1 and dateTable.month == 4 then
+		tt:AddLine("DBM")
+	else
+		tt:AddLine("BigWigs")
+	end
 	if BigWigs and BigWigs:IsEnabled() then
 		local added = false
 		for name, module in BigWigs:IterateBossModules() do
--- Options/Options.lua
+++ Options/Options.lua
@@ -27,10 +27,26 @@ local configFrame, isPluginOpen
 local showToggleOptions, getAdvancedToggleOption = nil, nil
 local toggleOptionsStatusTable = {}
 
+local textName = "BigWigs"
+local textIntro = L.introduction
+local textSlashVersion = L.slashDescVersion
+local textSlashConfig = L.slashDescConfig
+local textGHDesc = L.gitHubDesc
+do
+	local dateTable = date("*t")
+	if dateTable.day and dateTable.month and dateTable.day == 1 and dateTable.month == 4 then
+		textName = "DBM"
+		textIntro = textIntro:gsub("BigWigs", "DBM")
+		textSlashVersion = textSlashVersion:gsub("BigWigs", "DBM")
+		textSlashConfig = textSlashConfig:gsub("BigWigs", "DBM")
+		textGHDesc = textGHDesc:gsub("BigWigs", "DBM")
+	end
+end
+
 local getOptions
 local acOptions = {
 	type = "group",
-	name = "BigWigs",
+	name = textName,
 	get = function(info)
 		return BigWigs.db.profile[info[#info]]
 	end,
@@ -43,11 +59,11 @@ local acOptions = {
 		general = {
 			order = 20,
 			type = "group",
-			name = "BigWigs",
+			name = textName,
 			args = {
 				introduction = {
 					type = "description",
-					name = L.introduction,
+					name = textIntro,
 					order = 12,
 					fontSize = "medium",
 					width = "full",
@@ -188,21 +204,21 @@ local acOptions = {
 				},
 				slashDescVersion = {
 					type = "description",
-					name = "  ".. L.slashDescVersion,
+					name = "  ".. textSlashVersion,
 					fontSize = "medium",
 					order = 49,
 					width = "full",
 				},
 				slashDescConfig = {
 					type = "description",
-					name = "  ".. L.slashDescConfig,
+					name = "  ".. textSlashConfig,
 					fontSize = "medium",
 					order = 50,
 					width = "full",
 				},
 				gitHubDesc = {
 					type = "description",
-					name = "\n".. L.gitHubDesc .."\n",
+					name = "\n".. textGHDesc .."\n",
 					fontSize = "medium",
 					order = 51,
 					width = "full",
@@ -1378,7 +1394,7 @@ do
 
 		local bw = AceGUI:Create("Frame")
 		configFrame = bw
-		bw:SetTitle("BigWigs")
+		bw:SetTitle(textName)
 		bw:SetStatusText(" "..loader:GetReleaseString())
 		bw:SetWidth(858)
 		bw:SetHeight(660)
