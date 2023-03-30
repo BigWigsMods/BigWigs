238878ef7dc61f1cd7d70ab0b0d23f5ea2851b7f
Kyle Buller <bullerk@gmail.com>
2023-03-29 19:39:49 -0500

    Aberrus/Experiments: Updates

--- Aberrus/ExperimentationOfDracthyr.lua
+++ Aberrus/ExperimentationOfDracthyr.lua
@@ -14,13 +14,17 @@ mod:SetStage(1)
 -- Locals
 --
 
-local mutilationCount = 1
-local massiveSlamCount = 1
-local forcefulRoarCount = 1
+local thadrionEngaged = false
+local rionthusEngaged = false
 
+local rendingChargeCount = 1
+local massiveSlamCount = 1
+local bellowingRoarCount = 1
+
+local unstableEssenceCount = 1
 local essenceMarksUsed = {}
 local volatileSpewCount = 1
-local uncontrollableFrenzyCount = 1
+local violentEruptionCount = 1
 
 local deepBreathCount = 1
 local temporalAnomalyCount = 1
@@ -38,7 +42,6 @@ end
 -- Initialization
 --
 
-local mutilationMarker = mod:AddMarkerOption(true, "player", 8, 406358, 8, 7, 6) -- Mutilation
 local unstableEssenceMarker = mod:AddMarkerOption(true, "player", 1, 407327, 1, 2, 3) -- Unstable Essence
 function mod:GetOptions()
 	return {
@@ -47,15 +50,14 @@ function mod:GetOptions()
 		{406311, "TANK"}, -- Infused Strikes
 		407302, -- Infused Explosion
 		-- Neldris
-		{406358, "SAY", "SAY_COUNTDOWN"}, -- Mutilation
-		mutilationMarker,
+		406358, -- Rending Charge
 		404472, -- Massive Slam
-		404713, -- Forceful Roar
+		404713, -- Bellowing Roar
 		-- Thadrion
 		{407327, "SAY"}, -- Unstable Essence
 		unstableEssenceMarker,
 		405492, -- Volatile Spew
-		405375, -- Uncontrollable Frenzy
+		405375, -- Violent Eruption
 		-- Rionthus
 		406227, -- Deep Breath
 		407552, -- Temporal Anomaly
@@ -69,7 +71,6 @@ end
 
 function mod:OnBossEnable()
 	-- General
-	self:RegisterUnitEvent("UNIT_TARGETABLE_CHANGED", nil, "boss1", "boss2", "boss3")
 	self:Death("Deaths", 200912, 200913, 200918)
 
 	self:Log("SPELL_AURA_APPLIED", "InfusedStrikesApplied", 406311)
@@ -78,18 +79,16 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED_DOSE", "InfusedExplosionApplied", 407302)
 
 	-- Neldris
-	self:Log("SPELL_CAST_START", "Mutilation", 406358)
-	self:Log("SPELL_AURA_APPLIED", "MutilationApplied", 406317)
-	self:Log("SPELL_AURA_REMOVED", "MutilationRemoved", 406317)
+	self:Log("SPELL_CAST_START", "RendingCharge", 406358)
 	self:Log("SPELL_CAST_START", "MassiveSlam", 404472)
-	self:Log("SPELL_CAST_START", "ForcefulRoar", 404713)
+	self:Log("SPELL_CAST_START", "BellowingRoar", 404713)
 
 	-- Thadrion
 	self:Log("SPELL_CAST_START", "UnstableEssence", 405042)
 	self:Log("SPELL_AURA_APPLIED", "UnstableEssenceApplied", 407327)
 	self:Log("SPELL_AURA_REMOVED", "UnstableEssenceRemoved", 407327)
 	self:Log("SPELL_CAST_START", "VolatileSpew", 405492)
-	self:Log("SPELL_CAST_START", "UncontrollableFrenzy", 405375)
+	self:Log("SPELL_CAST_START", "ViolentEruption", 405375)
 
 	-- Rionthus
 	self:Log("SPELL_CAST_START", "DeepBreath", 406227)
@@ -99,56 +98,67 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
-	mutilationCount = 1
-	massiveSlamCount = 1
-	forcefulRoarCount = 1
+	self:SetStage(1)
+	thadrionEngaged = false
+	rionthusEngaged = false
+	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT")
 
+	rendingChargeCount = 1
+	massiveSlamCount = 1
+	bellowingRoarCount = 1
+
+	unstableEssenceCount = 1
 	essenceMarksUsed = {}
 	volatileSpewCount = 1
-	uncontrollableFrenzyCount = 1
+	violentEruptionCount = 1
 
 	deepBreathCount = 1
 	temporalAnomalyCount = 1
 	disintergrateCount = 1
 
-	-- self:Bar(406358, 30, CL.count:format(self:SpellName(406358), mutilationCount)) -- Mutilation
-	-- self:Bar(404472, 30, CL.count:format(self:SpellName(404472), massiveSlamCount)) -- Massive Slam
-	-- self:Bar(404713, 30, CL.count:format(self:SpellName(404713), forcefulRoarCount)) -- Forceful Roar
+	self:Bar(404713, 5, CL.count:format(self:SpellName(404713), bellowingRoarCount)) -- Forceful Roar
+	self:Bar(406358, 14.5, CL.count:format(self:SpellName(406358), rendingChargeCount)) -- Rending Charge
+	self:Bar(404472, 30, CL.count:format(self:SpellName(404472), massiveSlamCount)) -- Massive Slam
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:UNIT_TARGETABLE_CHANGED(_, unit)
-	-- XXX maybe IEEU with flags for the bosses?
-	local mobId = self:MobId(self:UnitGUID(unit))
-	if mobId == 200913 then -- Thadrion
-		if UnitCanAttack("player", unit) then
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	-- XXX better event? initial timers have quite a bit of variance
+	if not thadrionEngaged then
+		if self:GetBossId(200913) then -- Thadrion
+			thadrionEngaged = true
 			self:Message("stages", "cyan", -26322, false)
 			self:PlaySound("stages", "long")
-			-- self:Bar(405492, 30, CL.count:format(self:SpellName(405492), volatileSpewCount)) -- Volatile Spew
-			-- self:Bar(405375, 30, CL.count:format(self:SpellName(405375), uncontrollableFrenzyCount)) -- Uncontrollable Frenzy
+
+			self:CDBar(405492, 5, CL.count:format(self:SpellName(405492), volatileSpewCount)) -- Volatile Spew
+			self:CDBar(407327, 18, CL.count:format(self:SpellName(407327), unstableEssenceCount)) -- Unstable Essence
+			self:CDBar(405375, 46, CL.count:format(self:SpellName(405375), violentEruptionCount)) -- Violent Eruption
 		end
-	elseif mobId == 200918 then -- Rionthus
-		if UnitCanAttack("player", unit) then
+	elseif not rionthusEngaged then
+		if self:GetBossId(200918) then -- Rionthus
+			rionthusEngaged = true
 			self:Message("stages", "cyan", -26329, false)
 			self:PlaySound("stages", "long")
-			-- self:Bar(406227, 30, CL.count:format(self:SpellName(406227), deepBreathCount)) -- Deep Breath
-			-- self:Bar(407552, 30, CL.count:format(self:SpellName(407552), temporalAnomalyCount)) -- Temporal Anomaly
-			-- self:Bar(405392, 30, CL.count:format(self:SpellName(405392), disintergrateCount)) -- Disintegrate
+
+			self:CDBar(405392, 7.8, CL.count:format(self:SpellName(405392), disintergrateCount)) -- Disintegrate
+			self:CDBar(407552, 18.0, CL.count:format(self:SpellName(407552), temporalAnomalyCount)) -- Temporal Anomaly
+			self:CDBar(406227, 33.8, CL.count:format(self:SpellName(406227), deepBreathCount)) -- Deep Breath
 		end
 	end
 end
 
 function mod:Deaths(args)
 	if args.mobId == 200912 then -- Neldris
-		self:StopBar(CL.count:format(self:SpellName(406358), mutilationCount)) -- Mutilation
+		self:StopBar(CL.count:format(self:SpellName(406358), rendingChargeCount)) -- Rending Charge
 		self:StopBar(CL.count:format(self:SpellName(404472), massiveSlamCount)) -- Massive Slam
-		self:StopBar(CL.count:format(self:SpellName(404713), forcefulRoarCount)) -- Forceful Roar
+		self:StopBar(CL.count:format(self:SpellName(404713), bellowingRoarCount)) -- Forceful Roar
 	elseif args.mobId == 200913 then -- Thadrion
+		self:StopBar(CL.count:format(self:SpellName(407327), unstableEssenceCount)) -- Unstable Essence
 		self:StopBar(CL.count:format(self:SpellName(405492), volatileSpewCount)) -- Volatile Spew
-		self:StopBar(CL.count:format(self:SpellName(405375), uncontrollableFrenzyCount)) -- Uncontrollable Frenzy
+		self:StopBar(CL.count:format(self:SpellName(405375), violentEruptionCount)) -- Violent Eruption
 	elseif args.mobId == 200918 then -- Rionthus
 		self:StopBar(CL.count:format(self:SpellName(406227), deepBreathCount)) -- Deep Breath
 		self:StopBar(CL.count:format(self:SpellName(407552), temporalAnomalyCount)) -- Temporal Anomaly
@@ -184,70 +194,36 @@ function mod:InfusedExplosionApplied(args)
 end
 
 -- Neldris
-do
-	local count = 1
-	local playerList = {}
-	local onMe = false
-
-	local function mutilationSound()
-		if onMe then
-			mod:PlaySound(406358, "warning")
-		else
-			mod:PlaySound(406358, "alarm")
-		end
-		onMe = false
-	end
-
-	function mod:Mutilation(args)
-		self:StopBar(CL.count:format(args.spellName, mutilationCount))
-		mutilationCount = mutilationCount + 1
-		--self:Bar(args.spellId, 30, CL.count:format(args.spellName, mutilationCount))
-		count = 8 -- Using 8, 7, 6
-		playerList = {}
-		self:SimpleTimer(mutilationSound, 0.1)
-	end
-
-	function mod:MutilationApplied(args)
-		playerList[#playerList+1] = args.destName
-		playerList[args.destName] = count -- Set raid marker
-		if self:Me(args.destGUID) then
-			self:Say(406358, CL.rticon:format(args.spellName, count))
-			self:SayCountdown(406358, 6, count)
-			onMe = true
-		end
-		self:TargetsMessage(406358, "yellow", playerList, 3, CL.count:format(args.spellName, mutilationCount-1))
-		self:CustomIcon(mutilationMarker, args.destName, count)
-		count = count - 1
-	end
-
-	function mod:MutilationRemoved(args)
-		if self:Me(args.destGUID) then
-			self:CancelSayCountdown(406358)
-			onMe = false
-		end
-		self:CustomIcon(mutilationMarker, args.destName)
-	end
+function mod:RendingCharge(args)
+	self:StopBar(CL.count:format(args.spellName, rendingChargeCount))
+	self:Message(406358, "red", CL.count:format(args.spellName, rendingChargeCount))
+	self:PlaySound(406358, "warning")
+	rendingChargeCount = rendingChargeCount + 1
+	self:Bar(args.spellId, 34, CL.count:format(args.spellName, rendingChargeCount))
 end
 
 function mod:MassiveSlam(args)
 	self:StopBar(CL.count:format(args.spellName, massiveSlamCount))
-	self:Message(args.spellId, "red", CL.count:format(args.spellName, massiveSlamCount))
+	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, massiveSlamCount))
 	self:PlaySound(args.spellId, "alert")
 	massiveSlamCount = massiveSlamCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, massiveSlamCount))
+	-- self:Bar(args.spellId, 30, CL.count:format(args.spellName, massiveSlamCount))
 end
 
-function mod:ForcefulRoar(args)
-	self:StopBar(CL.count:format(args.spellName, forcefulRoarCount))
-	self:Message(args.spellId, "orange", CL.count:format(args.spellName, forcefulRoarCount))
+function mod:BellowingRoar(args)
+	self:StopBar(CL.count:format(args.spellName, bellowingRoarCount))
+	self:Message(args.spellId, "orange", CL.count:format(args.spellName, bellowingRoarCount))
 	self:PlaySound(args.spellId, "alarm")
-	forcefulRoarCount = forcefulRoarCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, forcefulRoarCount))
+	bellowingRoarCount = bellowingRoarCount + 1
+	self:Bar(args.spellId, 36, CL.count:format(args.spellName, bellowingRoarCount))
 end
 
 -- Thadrion
 function mod:UnstableEssence(args)
+	self:StopBar(CL.count:format(args.spellName, unstableEssenceCount))
 	self:Message(407327, "cyan", CL.casting:format(args.spellName))
+	unstableEssenceCount = unstableEssenceCount + 1
+	self:CDBar(407327, 53, CL.count:format(args.spellName, unstableEssenceCount))
 end
 
 function mod:UnstableEssenceApplied(args)
@@ -266,7 +242,7 @@ function mod:UnstableEssenceApplied(args)
 end
 
 function mod:UnstableEssenceRemoved(args)
-	self:CustomIcon(mutilationMarker, args.destName)
+	self:CustomIcon(unstableEssenceMarker, args.destName)
 	for i = 1, 3, 1 do -- 1, 2, 3
 		if essenceMarksUsed[i] == args.destGUID then
 			essenceMarksUsed[i] = nil
@@ -280,15 +256,15 @@ function mod:VolatileSpew(args)
 	self:Message(args.spellId, "orange", CL.count:format(args.spellName, volatileSpewCount))
 	self:PlaySound(args.spellId, "alarm")
 	volatileSpewCount = volatileSpewCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, volatileSpewCount))
+	self:CDBar(args.spellId, volatileSpewCount % 2 == 26 or 30, CL.count:format(args.spellName, volatileSpewCount))
 end
 
-function mod:UncontrollableFrenzy(args)
-	self:StopBar(CL.count:format(args.spellName, uncontrollableFrenzyCount))
-	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, uncontrollableFrenzyCount))
+function mod:ViolentEruption(args)
+	self:StopBar(CL.count:format(args.spellName, violentEruptionCount))
+	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, violentEruptionCount))
 	self:PlaySound(args.spellId, "long")
-	uncontrollableFrenzyCount = uncontrollableFrenzyCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, uncontrollableFrenzyCount))
+	violentEruptionCount = violentEruptionCount + 1
+	-- self:Bar(args.spellId, 60, CL.count:format(args.spellName, violentEruptionCount)) -- XXX longer than 52s >.>
 end
 
 -- Rionthus
@@ -297,7 +273,7 @@ function mod:DeepBreath(args)
 	self:Message(args.spellId, "red", CL.count:format(args.spellName, deepBreathCount))
 	self:PlaySound(args.spellId, "alert")
 	deepBreathCount = deepBreathCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, deepBreathCount))
+	self:Bar(args.spellId, 43.8, CL.count:format(args.spellName, deepBreathCount))
 end
 
 function mod:TemporalAnomaly(args)
@@ -305,14 +281,14 @@ function mod:TemporalAnomaly(args)
 	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, temporalAnomalyCount))
 	self:PlaySound(args.spellId, "info")
 	temporalAnomalyCount = temporalAnomalyCount + 1
-	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, temporalAnomalyCount))
+	self:Bar(args.spellId, 43.8, CL.count:format(args.spellName, temporalAnomalyCount))
 end
 
 function mod:Disintegrate(args)
 	self:StopBar(CL.count:format(args.spellName, disintergrateCount))
 	self:Message(405392, "orange", CL.count:format(args.spellName, disintergrateCount))
 	disintergrateCount = disintergrateCount + 1
-	--self:Bar(405392, 30, CL.count:format(args.spellName, disintergrateCount))
+	self:Bar(405392, 43.8, CL.count:format(args.spellName, disintergrateCount))
 end
 
 function mod:DisintegrateApplied(args)
