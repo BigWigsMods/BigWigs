475e09369ad7360b22ea943f2023ee7dfeec2a57
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2023-05-04 11:51:03 +0800

    Update zhCN.lua

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -41,7 +41,7 @@ L.noBossMod = "没有首领模块："
 L.offline = "离线"
 
 L.missingPlugin = "请注意，这个区域需要 |cFF436EEE%s|r 插件计时器才可被显示。"
-L.missingAddOn = "你的缺失了 |cFF436EEE%s|r 插件!"
+L.missingAddOn = "你缺少 |cFF436EEE%s|r 插件!"
 L.disabledAddOn = "你的 |cFF436EEE%s|r 插件已禁用，计时器将不被显示。"
 
 L.removeAddOn = "请移除“|cFF436EEE%s|r”，其已被“|cFF436EEE%s|r”所替代。"
