bb19e6a35173d8e7e74fd13d9f8c88877d8c9eec
simoflorence <kikuchi.yume@gmail.com>
2023-05-08 16:04:33 +0200

    Update itIT (#1373)

--- Aberrus/Locales/itIT.lua
+++ Aberrus/Locales/itIT.lua
@@ -1,94 +1,94 @@
 local L = BigWigs:NewBossLocale("Kazzara, the Hellforged", "itIT")
 if not L then return end
 if L then
-    --L.dread_rift = "Rift" -- Singular Dread Rift
+    L.dread_rift = "Fenditura" -- Singular Dread Rift
 end
 
 L = BigWigs:NewBossLocale("The Amalgamation Chamber", "itIT")
 if L then
-	-- L.custom_on_fade_out_bars = "Fade out stage 1 bars"
-	-- L.custom_on_fade_out_bars_desc = "Fade out bars which belong to the boss that is out of range in stage 1."
+	L.custom_on_fade_out_bars = "Dissolvenza Barre Fase 1"
+	L.custom_on_fade_out_bars_desc = "Dissolvi le barre del boss che fuori raggio d'azione in Fase 1."
 
-	-- L.coalescing_void = "Run Away"
-	-- L.shadow_convergence = "Orbs"
-	-- L.molten_eruption = "Soaks"
-	-- L.swirling_flame = "Tornadoes"
-	-- L.gloom_conflagration = "Meteor + Run Away"
-	-- L.blistering_twilight = "Bombs + Tornadoes"
-	-- L.convergent_eruption = "Soaks + Orbs"
-	-- L.shadowflame_burst = "Frontal Cone"
+	L.coalescing_void = "Scappa"
+	L.shadow_convergence = "Globi"
+	L.molten_eruption = "Assorbi"
+	L.swirling_flame = "Tornado"
+	L.gloom_conflagration = "Meteora + Scappare"
+	L.blistering_twilight = "Bombe + Tornado"
+	L.convergent_eruption = "Assorbi + Globi"
+	L.shadowflame_burst = "Cono Frontale"
 
-	-- L.shadow_and_flame = "Mythic Debuffs"
+	L.shadow_and_flame = "Penalità Mitico"
 end
 
 L = BigWigs:NewBossLocale("The Forgotten Experiments", "itIT")
 if L then
-	-- L.rending_charge_single = "1st Charge"
-	-- L.massive_slam = "Frontal Cone"
-	-- L.unstable_essence_new = "New Bomb"
-	-- L.custom_on_unstable_essence_high = "High Stacks Unstable Essence Say Messages"
-	-- L.custom_on_unstable_essence_high_desc = "Say messages with the amount of stacks for your Unstable Essence debuff when they are high enough."
-	-- L.volatile_spew = "Dodges"
-	-- L.volatile_eruption = "Eruption"
-	-- L.temporal_anomaly = "Heal Orb"
-	-- L.temporal_anomaly_knocked = "Heal Orb Knocked!"
+	L.rending_charge_single = "Prima Carica"
+	L.massive_slam = "Cono Frontale"
+	L.unstable_essence_new = "Nuova Bomba"
+	L.custom_on_unstable_essence_high = "Messaggio Chat Molti Accumuli di Essenza Instabile"
+	L.custom_on_unstable_essence_high_desc = "Messaggio di chat con il numero di accumuli per il tuo maleficio di Essenza Instabile quando sono troppi."
+	L.volatile_spew = "Evita"
+	L.volatile_eruption = "Erupzione"
+	L.temporal_anomaly = "Globo Curativo"
+	L.temporal_anomaly_knocked = "Globo Curativo Intercettato!"
 end
 
 L = BigWigs:NewBossLocale("Assault of the Zaqali", "itIT")
 if L then
-	-- L.big_adds_timer = "Timers for Huntsman + Guards"
-	-- L.final_assault_soon = "Final Assault soon"
+	L.big_adds_timer = "Timer per Predatori + Guardie"
+	L.final_assault_soon = "Assalto Finale a breve"
 
-	-- L.south_adds_message = "Big Adds Climbing SOUTH!"
+	L.south_adds_message = "Add Maggiori Si Arrampicano Da SUD!"
 	-- L.south_adds = "Commanders ascend the southern battlement!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the southern battlement!
-	-- L.north_adds_message = "Big Adds Climbing NORTH!"
+	L.north_adds_message = "Add Maggiori Si Arrampicano Da NORD"
 	-- L.north_adds = "Commanders ascend the northern battlement!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the northern battlement!
 
-	-- L.wallclimbers_bartext = "Wallclimbers"
+	L.wallclimbers_bartext = "Scalatori"
 end
 
 L = BigWigs:NewBossLocale("Rashok, the Elder", "itIT")
 if L then
-	-- L.doom_flames = "Small Soaks"
-	-- L.shadowlave_blast = "Frontal Cone"
-	-- L.charged_smash = "Big Soak"
-	-- L.energy_gained = "Energy Gained: %d"
+	L.doom_flames = "Assorbimenti Minori"
+	L.shadowlave_blast = "Cono Frontale"
+	L.charged_smash = "Assorbimento Maggiore"
+	L.energy_gained = "Energia Guadagnata: %d"
 
 	-- Mythic
-	-- L.unleash_shadowflame = "Mythic Orbs"
+	L.unleash_shadowflame = "Globi Mitico"
 end
 
 L = BigWigs:NewBossLocale("The Vigilant Steward, Zskarn", "itIT")
 if L then
-	-- L.tactical_destruction = "Dragonheads"
-	-- L.bombs_soaked = "Bombs Soaked" -- Bombs Soaked (2/4)
-	-- L.unstable_embers = "Embers"
-	-- L.unstable_ember = "Ember"
+	L.tactical_destruction = "Statue di Drago"
+	L.bombs_soaked = "Bombe Assorbite" -- Bombs Soaked (2/4)
+	L.unstable_embers = "Braci"
+	L.unstable_ember = "Brace"
 end
 
 L = BigWigs:NewBossLocale("Magmorax", "itIT")
 if L then
-	-- L.energy_gained = "Energy Gained! (-17s)" -- When you fail, you lose 17s on until the boss reaches full energy
+	L.energy_gained = "Energia Guadagnata! (-17s)" -- When you fail, you lose 17s on until the boss reaches full energy
 
 	-- Mythic
-	-- L.explosive_magma = "Soak Pool"
+	L.explosive_magma = "Assorbi Pozza"
 end
 
 L = BigWigs:NewBossLocale("Echo of Neltharion", "itIT")
 if L then
-	-- L.custom_on_repeating_sunder_reality = "Repeating Sunder Reality Warning"
-	-- L.custom_on_repeating_shattered_reality_desc = "Repeat a message during the Ebon Destruction cast until you get inside a portal."
+	L.custom_on_repeating_sunder_reality = "Ripeti Avviso per Frammentazione della Realtà"
+	L.custom_on_repeating_shattered_reality_desc = "Ripeti un messaggio durante il lancio di Distruzione d'Ebano fino a che non entri dentro un portale."
 
-	-- L.twisted_earth = "Walls"
-	-- L.echoing_fissure = "Fissure"
-	-- L.rushing_darkness = "Knock Lines"
+	L.twisted_earth = "Muri"
+	L.echoing_fissure = "Fessura"
+	L.rushing_darkness = "Distruggi Linee"
 
-	-- L.umbral_annihilation = "Annihilation"
-	-- L.sunder_reality = "Portals"
-	-- L.ebon_destruction = "Big Bang"
+	L.umbral_annihilation = "Annientamento"
+	L.sunder_reality = "Portali"
+	L.ebon_destruction = "Big Bang"
 end
 
 L = BigWigs:NewBossLocale("Scalecommander Sarkareth", "itIT")
 if L then
-	--L.claws = "Tank Debuff" -- (Stage 1) Burning Claws / (Stage 2) Void Claws / (Stage 3) Void Slash
+	L.claws = "Penalità Difensori" -- (Fase 1) Artigli Ardenti / (Fase 2) Artigli del Vuoto / (Fase 3) Squarcio del Vuoto
 end
