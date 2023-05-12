e0732a9d166ee0223c7100afe5cda50fc3c64110
KnewOne <illia.fylypovych@apollobase.com>
2023-05-12 18:20:42 +0300

    Emote + boss_returns

--- Aberrus/Locales/ruRU.lua
+++ Aberrus/Locales/ruRU.lua
@@ -38,8 +38,8 @@ L = BigWigs:NewBossLocale("Assault of the Zaqali", "ruRU")
 if L then
 	-- These are in-game emotes and need to match the text shown in-game
 	-- You should also replace the comment (--) with the full emote as it shows in-game
-	L.zaqali_aide_north_emote_trigger = "южное укрепление" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the northern battlement!
-	L.zaqali_aide_south_emote_trigger = "северное укрепление" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the southern battlement!
+	L.zaqali_aide_north_emote_trigger = "Помощники вождя поднимаются на южное укрепление!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the northern battlement!
+	L.zaqali_aide_south_emote_trigger = "Помощники вождя поднимаются на северное укрепление!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the southern battlement!
 
 	L.north = "Север"
 	L.south = "Юг"
@@ -47,7 +47,7 @@ if L then
 
 	L.zaqali_aide_message = "%s лезут:  %s" -- Big Adds Climbing North
 	L.add_bartext = "%s: %s (%d)"
-	L.boss_returns = "Босс: North"
+	L.boss_returns = "Босс: Север"
 
 	L.molten_barrier = "Преграда"
 	L.catastrophic_slam = "Делёжка"
