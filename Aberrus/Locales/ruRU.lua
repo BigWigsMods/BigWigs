6633613f271e6085c03841ee24c5b1d8f2392886
KnewOne <illia.fylypovych@apollobase.com>
2023-05-12 19:09:41 +0300

    Icon

--- Aberrus/Locales/ruRU.lua
+++ Aberrus/Locales/ruRU.lua
@@ -38,8 +38,8 @@ L = BigWigs:NewBossLocale("Assault of the Zaqali", "ruRU")
 if L then
 	-- These are in-game emotes and need to match the text shown in-game
 	-- You should also replace the comment (--) with the full emote as it shows in-game
-	L.zaqali_aide_north_emote_trigger = "Помощники вождя поднимаются на южное укрепление!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the northern battlement!
-	L.zaqali_aide_south_emote_trigger = "Помощники вождя поднимаются на северное укрепление!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the southern battlement!
+	L.zaqali_aide_north_emote_trigger = "|TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Помощники вождя поднимаются на южное укрепление!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the northern battlement!
+	L.zaqali_aide_south_emote_trigger = "|TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Помощники вождя поднимаются на северное укрепление!" -- |TInterface\\ICONS\\Ability_Hunter_KillCommand.blp:20|t Commanders ascend the southern battlement!
 
 	L.north = "Север"
 	L.south = "Юг"
