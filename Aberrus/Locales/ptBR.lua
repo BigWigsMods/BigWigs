f4da7a426fd67a5aa3b6640966536bb58636f385
Túlio Baruk <84753278+tuliobaruk@users.noreply.github.com>
2023-05-24 20:52:15 -0300

    Update ptBR.lua
    Updating Aberrus ptBR locales.

--- Aberrus/Locales/ptBR.lua
+++ Aberrus/Locales/ptBR.lua
@@ -1,106 +1,106 @@
 local L = BigWigs:NewBossLocale("Kazzara, the Hellforged", "ptBR")
 if not L then return end
 if L then
-	--L.dread_rift = "Rift" -- Singular Dread Rift
+	L.dread_rift = "Fenda" -- Singular Dread Rift
 end
 
 L = BigWigs:NewBossLocale("The Amalgamation Chamber", "ptBR")
 if L then
-	--L.custom_on_fade_out_bars = "Fade out stage 1 bars"
-	--L.custom_on_fade_out_bars_desc = "Fade out bars which belong to the boss that is out of range in stage 1."
+	L.custom_on_fade_out_bars = "Esconder barras da primeira fase."
+	L.custom_on_fade_out_bars_desc = "Desaparece com as barras que pertencem ao boss que está fora de alcance na primeira fase."
 
-	--L.coalescing_void = "Run Away"
-	--L.shadow_convergence = "Orbs"
-	--L.molten_eruption = "Soaks"
-	--L.swirling_flame = "Tornadoes"
-	--L.gloom_conflagration = "Meteor + Run Away"
-	--L.blistering_twilight = "Bombs + Tornadoes"
-	--L.convergent_eruption = "Soaks + Orbs"
-	--L.shadowflame_burst = "Frontal Cone"
+	L.coalescing_void = "Fuja"
+	L.shadow_convergence = "Orbes"
+	L.molten_eruption = "Soaks"
+	L.swirling_flame = "Tornados"
+	L.gloom_conflagration = "Meteoro + Fuja"
+	L.blistering_twilight = "Bombas + Tornados"
+	L.convergent_eruption = "Soaks + Orbes"
+	L.shadowflame_burst = "Cone Frontal"
 
-	--L.shadow_and_flame = "Mythic Debuffs"
+	L.shadow_and_flame = "Debuffs Míticos"
 end
 
 L = BigWigs:NewBossLocale("The Forgotten Experiments", "ptBR")
 if L then
-	--L.rending_charge_single = "First Charge"
-	--L.massive_slam = "Frontal Cone"
-	--L.unstable_essence_new = "New Bomb"
-	--L.custom_on_unstable_essence_high = "High Stacks Unstable Essence Say Messages"
-	--L.custom_on_unstable_essence_high_desc = "Say messages with the amount of stacks for your Unstable Essence debuff when they are high enough."
-	--L.volatile_spew = "Dodges"
-	--L.volatile_eruption = "Eruption"
-	--L.temporal_anomaly = "Heal Orb"
-	--L.temporal_anomaly_knocked = "Heal Orb Knocked"
+	L.rending_charge_single = "Primeira Investida"
+	L.massive_slam = "Cone Frontal"
+	L.unstable_essence_new = "Nova Bomba"
+	L.custom_on_unstable_essence_high = "Mensagens de Acumulos Altos de Essência Instável"
+	L.custom_on_unstable_essence_high_desc = "Diga mensagens com a quantidade de acúmulos do seu debuff de Essência Instável quando eles estiverem altos o suficiente."
+	L.volatile_spew = "Esquivar"
+	L.volatile_eruption = "Erupção"
+	L.temporal_anomaly = "Orbe de Cura"
+	L.temporal_anomaly_knocked = "Orbe de Cura Empurrado"
 end
 
 L = BigWigs:NewBossLocale("Assault of the Zaqali", "ptBR")
 if L then
 	-- These are in-game emotes and need to match the text shown in-game
 	-- You should also replace the comment (--) with the full emote as it shows in-game
-	--L.zaqali_aide_north_emote_trigger = "northern battlement" -- Commanders ascend the northern battlement!
-	--L.zaqali_aide_south_emote_trigger = "southern battlement" -- Commanders ascend the southern battlement!
+	L.zaqali_aide_north_emote_trigger = "Reforço Norte" -- Commanders ascend the northern battlement!
+	L.zaqali_aide_south_emote_trigger = "Reforço Sul" -- Commanders ascend the southern battlement!
 
-	--L.north = "North"
-	--L.south = "South"
-	--L.both = "Both"
+	L.north = "Norte"
+	L.south = "Sul"
+	L.both = "Ambos"
 
-	--L.zaqali_aide_message = "%s Climbing %s" -- Big Adds Climbing North
+	L.zaqali_aide_message = "%s Escalando %s" -- Big Adds Climbing North
 	L.add_bartext = "%s: %s (%d)"
-	--L.boss_returns = "Boss Lands: North"
+	L.boss_returns = "Boss Aterrissa: Norte"
 
 	L.molten_barrier = "Barreira"
-	--L.catastrophic_slam = "Door Slam"
+	L.catastrophic_slam = "Batida na Porta"
 end
 
 L = BigWigs:NewBossLocale("Rashok, the Elder", "ptBR")
 if L then
-	--L.doom_flames = "Small Soaks"
-	--L.shadowlave_blast = "Frontal Cone"
-	--L.charged_smash = "Big Soak"
-	--L.energy_gained = "Energy Gained: %d"
+	L.doom_flames = "Soaks Pequenos"
+	L.shadowlave_blast = "Cone Frontal"
+	L.charged_smash = "Soak Grande"
+	L.energy_gained = "Energia Obtida: %d"
 
 	-- Mythic
-	--L.unleash_shadowflame = "Mythic Orbs"
+	L.unleash_shadowflame = "Orbes Míticos"
 end
 
 L = BigWigs:NewBossLocale("The Vigilant Steward, Zskarn", "ptBR")
 if L then
-	--L.tactical_destruction = "Dragonheads"
-	--L.bombs_soaked = "Bombs Soaked" -- Bombs Soaked (2/4)
-	--L.unstable_embers = "Embers"
-	--L.unstable_ember = "Ember"
+	L.tactical_destruction = "Cabeças de Dragão"
+	L.bombs_soaked = "Bombas Limpas" -- Bombs Soaked (2/4)
+	L.unstable_embers = "Brasas"
+	L.unstable_ember = "Brasa"
 end
 
 L = BigWigs:NewBossLocale("Magmorax", "ptBR")
 if L then
-	--L.energy_gained = "Energy Gained (-17s)" -- When you fail, you lose 17 seconds, the boss reaches full energy faster
+	L.energy_gained = "Energia Obtida (-17s)" -- When you fail, you lose 17 seconds, the boss reaches full energy faster
 
 	-- Mythic
-	--L.explosive_magma = "Soak Pool"
+	L.explosive_magma = "Poça de Soak"
 end
 
 L = BigWigs:NewBossLocale("Echo of Neltharion", "ptBR")
 if L then
-	--L.custom_on_repeating_sunder_reality = "Repeating Sunder Reality Warning"
-	--L.custom_on_repeating_sunder_reality_desc = "Repeat a message during the Ebon Destruction cast until you get inside a portal."
+	L.custom_on_repeating_sunder_reality = "Repetir Aviso de Cindir Realidade"
+	L.custom_on_repeating_sunder_reality_desc = "Repetir uma mensagem durante o lançamento de Destruição de Ébano até entrar em um portal."
 
-	--L.twisted_earth = "Walls"
-	--L.echoing_fissure = "Fissure"
-	--L.rushing_darkness = "Knock Lines"
+	L.twisted_earth = "Paredes"
+	L.echoing_fissure = "Fissura"
+	L.rushing_darkness = "Linhas de Empurrão"
 
-	--L.umbral_annihilation = "Annihilation"
-	--L.sunder_reality = "Portals"
-	--L.ebon_destruction = "Big Bang"
+	L.umbral_annihilation = "Aniquilação"
+	L.sunder_reality = "Portais"
+	L.ebon_destruction = "Big Bang"
 end
 
 L = BigWigs:NewBossLocale("Scalecommander Sarkareth", "ptBR")
 if L then
-	--L.claws = "Tank Debuff" -- (Stage 1) Burning Claws / (Stage 2) Void Claws / (Stage 3) Void Slash
-	--L.claws_debuff = "Tank Explodes"
-	--L.emptiness_between_stars = "Emptiness"
-	--L.void_slash = "Tank Frontal"
+	L.claws = "Debuff de Tank" -- (Stage 1) Burning Claws / (Stage 2) Void Claws / (Stage 3) Void Slash
+	L.claws_debuff = "Tank Explode"
+	L.emptiness_between_stars = "Vazio"
+	L.void_slash = "Frontal no Tank"
 
-	--L.boss_immune = "Boss Immune"
-	--L.ebon_might = "Adds Immune"
+	L.boss_immune = "Boss Imune"
+	L.ebon_might = "Adds Imunes"
 end
