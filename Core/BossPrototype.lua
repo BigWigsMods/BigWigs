BigWigsMods/BigWigs

commit 3804b400c8b7ccafea511536206b54f0d6eb0353

Core/BossPrototype: Always error when trying to look up an icon with an invalid/missing texture

Core/BossPrototype.lua
@@ -121,17 +121,15 @@ local icons = setmetatable({}, {__index =
 			if key > 8 then
 				value = GetSpellTexture(key)
 				if not value then
-					core:Print(format("An invalid spell id (%d) is being used in a boss module.", key))
+					core:Error(format("An invalid spell id (%d) is being used as a texture in a boss module.", key))
 				end
 			elseif key > 0 then
 				-- Texture id list for raid icons 1-8 is 137001-137008. Base texture path is Interface\\TARGETINGFRAME\\UI-RaidTargetingIcon_%d
 				value = key + 137000
 			else
 				local tbl = C_EncounterJournal_GetSectionInfo(-key)
-				if not tbl then
-					core:Print(format("An invalid journal id (%d) is being used in a boss module.", key))
-				else
-					value = tbl.abilityIcon or false
+				if not tbl or not tbl.abilityIcon then
+					core:Error(format("An invalid journal id (%d) is being used as a texture in a boss module.", key))
 				end
 			end
 		else

commit 404e5305f3d09dd93f859616f89faf01caf40f44

Core/BossPrototype: Fix journal-sourced ability icons (#1419)
Regression from 3804b400c8b7ccafea511536206b54f0d6eb0353

Core/BossPrototype.lua
@@ -111,6 +111,8 @@ local icons = setmetatable({}, {__index =
 				local tbl = C_EncounterJournal_GetSectionInfo(-key)
 				if not tbl or not tbl.abilityIcon then
 					core:Error(format("The journal id %q has no icon texture but is being used as an icon in a boss module.", key))
+				else
+					value = tbl.abilityIcon
 				end
 			end
 		else