40cb60a40539577329438cc825782786b7934c34
funkydude <funkydude@users.noreply.github.com>
2023-08-18 05:22:38 +0100

    bump version

--- Init_TBC.lua
+++ Init_TBC.lua
@@ -2,7 +2,7 @@
 local _, tbl = ...
 tbl.isClassic = true
 tbl.isTBC = true
-tbl.dbmPrefix = "D5BC"
+tbl.dbmPrefix = "D5"
 tbl.dbmRevision = "20230814090538"
 tbl.dbmDisplayVersion = "2.6.0"
 tbl.dbmReleaseRevision = "20230814000000"
