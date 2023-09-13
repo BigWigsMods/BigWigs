f38f3819260390232e75026da123758e0638f25c
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2023-09-14 01:01:15 +0200

    Amirdrassil/Smolderon: Drycode

--- Amirdrassil/Smolderon.lua
+++ Amirdrassil/Smolderon.lua
@@ -14,6 +14,11 @@ mod:SetStage(1)
 -- Locals
 --
 
+local brandofDamnationCount = 1
+local overheatedCount = 1
+local lavaGeysersCount = 1
+local rotationCount = 1
+
 --------------------------------------------------------------------------------
 -- Localization
 --
@@ -29,16 +34,192 @@ end
 
 function mod:GetOptions()
 	return {
-		"berserk",
+		"stages",
+		-- Stage One: The Firelord's Fury
+		421343, -- Brand of Damnation
+		421656, -- Cauterizing Wound
+		{422577, "SAY", "SAY_COUNTDOWN"}, -- Searing Aftermath
+		{421455, "SAY", "SAY_COUNTDOWN"}, -- Overheated
+		421969, -- Flame Waves
+		422691, -- Lava Geysers
+		421532, -- Smoldering Ground
+		-- Stage Two: World In Flames
+		426725, -- Encroaching Destruction
+		422277, -- Devour Essence
+		421858, -- Ignited Essence
+		422172, -- World In Flames
 	}
 end
 
 function mod:OnBossEnable()
+	-- Stage One: The Firelord's Fury
+	self:Log("SPELL_CAST_START", "BrandofDamnation", 421343)
+	self:Log("SPELL_AURA_APPLIED", "CauterizingWoundApplied", 421656)
+	self:Log("SPELL_AURA_REMOVED", "CauterizingWoundRemoved", 421656)
+	self:Log("SPELL_AURA_APPLIED", "SearingAftermathApplied", 422577)
+	self:Log("SPELL_AURA_REMOVED", "SearingAftermathRemoved", 422577)
+	self:Log("SPELL_AURA_APPLIED", "OverheatedApplied", 421455)
+	self:Log("SPELL_AURA_REMOVED", "OverheatedRemoved", 421455)
+	self:Log("SPELL_CAST_START", "LavaGeysers", 422691)
+
+	-- Stage Two: World In Flames
+	self:Log("SPELL_AURA_APPLIED", "BlazingSoulApplied", 422067)
+	self:Log("SPELL_AURA_REMOVED", "BlazingSoulRemoved", 422067)
+	self:Log("SPELL_CAST_START", "EncroachingDestruction", 426725)
+	self:Log("SPELL_CAST_SUCCESS", "DevourEssence", 422277)
+	self:Log("SPELL_AURA_APPLIED", "IgnitedEssenceApplied", 421858)
+	self:Log("SPELL_AURA_APPLIED_DOSE", "IgnitedEssenceApplied", 421858)
+	self:Log("SPELL_CAST_START", "WorldInFlames", 422172)
+
+	self:Log("SPELL_AURA_APPLIED", "GroundDamage", 421969, 422823, 421532) -- Flame Waves, Lava Geysers, Smoldering Ground
+	self:Log("SPELL_PERIODIC_DAMAGE", "GroundDamage", 421969, 422823, 421532)
+	self:Log("SPELL_PERIODIC_MISSED", "GroundDamage", 421969, 422823, 421532)
 end
 
 function mod:OnEngage()
+	self:SetStage(1)
+	brandofDamnationCount = 1
+	overheatedCount = 1
+	lavaGeysersCount = 1
+	rotationCount = 1
+
+	--self:Bar(421343, 30, CL.count:format(self:SpellName(421343), brandofDamnationCount)) -- Brand of Damnation
+	--self:Bar(421455, 30, CL.count:format(self:SpellName(421455), overheatedCount)) -- Overheated
+	--self:Bar(422691, 30, CL.count:format(self:SpellName(422691), lavaGeysersCount)) -- Lava Geysers
+	--self:Bar("stages", 100, CL.count:format(CL.stage:format(2), rotationCount), 422172) -- Stage 2
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+-- Stage One: The Firelord's Fury
+function mod:BrandofDamnation(args)
+	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, brandofDamnationCount))
+	self:PlaySound(args.spellId, "alert")
+	brandofDamnationCount = brandofDamnationCount + 1
+	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, brandofDamnationCount))
+end
+
+function mod:CauterizingWoundApplied(args)
+	if self:Me(args.destGUID) then
+		self:PersonalMessage(args.spellId)
+		self:PlaySound(args.spellId, "info")
+	end
+end
+
+function mod:CauterizingWoundRemoved(args)
+	if self:Me(args.destGUID) then
+		self:Message(args.spellId, "green", CL.removed:format(args.spellName))
+		self:PlaySound(args.spellId, "info")
+	end
+end
+
+function mod:SearingAftermathApplied(args)
+	if self:Me(args.destGUID) then
+		self:Say(args.spellId)
+		self:SayCountdown(args.spellId, 6)
+		self:PlaySound(args.spellId, "warning")
+	end
+	self:TargetBar(args.spellId, 6, args.destName)
+end
+
+function mod:SearingAftermathRemoved(args)
+	if self:Me(args.destGUID) then
+		self:CancelSayCountdown(args.spellId)
+	end
+end
+
+do
+	local playerList = {}
+	local prev = 0
+	function mod:OverheatedApplied(args)
+		local msg = CL.count:format(args.spellName, overheatedCount)
+		if args.time - prev > 2 then -- reset
+			playerList = {}
+			prev = args.time
+			self:StopBar(msg)
+			overheatedCount = overheatedCount + 1
+			--self:Bar(args.spellId, 30, CL.count:format(args.spellName, overheatedCount))
+		end
+		playerList[#playerList+1] = args.destName
+		if self:Me(args.destGUID) then
+			self:PlaySound(args.spellId, "warning")
+			self:Say(args.spellId)
+			self:SayCountdown(args.spellId, 10)
+		end
+		self:TargetsMessage(args.spellId, "yellow", playerList, 5, msg)
+	end
+
+	function mod:OverheatedRemoved(args)
+		if self:Me(args.destGUID) then
+			self:CancelSayCountdown(args.spellId)
+		end
+	end
+end
+
+function mod:LavaGeysers(args)
+	self:Message(args.spellId, "orange", CL.count:format(args.spellName, lavaGeysersCount))
+	self:PlaySound(args.spellId, "alarm") -- watch feet
+	lavaGeysersCount = lavaGeysersCount + 1
+	--self:Bar(args.spellId, 30, CL.count:format(args.spellName, lavaGeysersCount))
+end
+
+function mod:BlazingSoulApplied(args)
+	self:SetStage(2)
+	self:StopBar(CL.count:format(self:SpellName(421343), brandofDamnationCount)) -- Brand of Damnation
+	self:StopBar(CL.count:format(self:SpellName(421455), overheatedCount)) -- Overheated
+	self:StopBar(CL.count:format(self:SpellName(422691), lavaGeysersCount)) -- Lava Geysers
+	self:StopBar(CL.count:format(CL.stage:format(2), rotationCount))
+end
+
+function mod:BlazingSoulRemoved(args)
+	self:SetStage(1)
+	brandofDamnationCount = 1
+	overheatedCount = 1
+	lavaGeysersCount = 1
+	rotationCount = rotationCount + 1
+
+	--self:Bar(421343, 30, CL.count:format(self:SpellName(421343), brandofDamnationCount)) -- Brand of Damnation
+	--self:Bar(421455, 30, CL.count:format(self:SpellName(421455), overheatedCount)) -- Overheated
+	--self:Bar(422691, 30, CL.count:format(self:SpellName(422691), lavaGeysersCount)) -- Lava Geysers
+	--self:Bar("stages", 100, CL.count:format(CL.stage:format(2), rotationCount), 422172)
+end
+
+function mod:EncroachingDestruction(args)
+	self:Message(args.spellId, "red")
+	self:PlaySound(args.spellId, "warning")
+end
+
+function mod:DevourEssence(args)
+	self:Message(args.spellId, "orange")
+	self:PlaySound(args.spellId, "long")
+end
+
+function mod:IgnitedEssenceApplied(args)
+	if self:Me(args.destGUID) then
+		self:StackMessage(args.spellId, "blue", args.destName, args.amount, 1)
+		self:PlaySound(args.spellId, "info")
+	end
+end
+
+function mod:WorldInFlames(args)
+	self:Message(args.spellId, "yellow")
+	self:PlaySound(args.spellId, "alarm")
+end
+
+do
+	local prev = 0
+	function mod:GroundDamage(args)
+		if self:Me(args.destGUID) and args.time - prev > 2 then
+			local key = args.spellId
+			if args.spellId == 422823 then -- Lava Geysers
+				key = 422691
+			end
+			prev = args.time
+			self:PlaySound(key, "underyou") -- SetOption:421969,422691,421532:::
+			self:PersonalMessage(key, "underyou") -- SetOption:421969,422691,421532:::
+		end
+	end
+end
+
