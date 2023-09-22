c934435e2bcd29445fdd63609091102835dbcb68
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2023-09-23 00:12:05 +0200

    Amirdrassil/Nymue: Initial ptr updates

--- Amirdrassil/Nymue.lua
+++ Amirdrassil/Nymue.lua
@@ -16,13 +16,18 @@ mod:SetStage(1)
 
 local continuumCount = 1
 local violentFloraCount = 1
-local drowsyExpulsionCount = 1
 local threadsOfLifeCount = 1
-local threadedBlastCount = 1
 local weaversBurdenCount = 1
 local viridianRainCount = 1
 
 local inflorescenceOnMe = false
+
+local timers = {
+	[424477] = {15.1, 13.4, 14.4, 12.1, 8.5, 13.5, 33.0, 20.0, 20.1, 21.5, 26.6, 9.9, 15.5, 12.0, 8.0, 12.0, 32.1, 10.4, 15.0, 14.0, 4.9}, -- Violent Flora
+	[426520] = {27.0, 26.6, 23.5, 65.1, 25.5, 64.4, 25.6, 69.5}, -- Weaver's Burden
+	[425745] = {35.0, 13.0, 12.0, 18.7, 39.4, 8.0, 12.0, 19.5, 10.4, 31.0, 11.0, 18.0, 12.0, 18.0, 29.6, 2.1, 10.3, 18.0, 12.0}, -- Threads of Life
+	[420907] = {7.0, 33.0, 37.0, 33.0, 15.0, 20.5, 29.5, 23.1, 7.9, 13.6, 8.0, 10.4, 9.6, 10.5, 7.9}, -- Viridian Rain
+}
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -43,7 +48,6 @@ function mod:GetOptions()
 		420554, -- Verdant Matrix
 		424477, -- Violent Flora
 		423195, -- Inflorescence
-		413442, -- Drowsy Expulsion
 		{425745, "SAY"}, -- Threads of Life
 		{426147, "TANK"}, -- Threaded Blast
 		{426520, "SAY", "SAY_COUNTDOWN"}, -- Weaver's Burden
@@ -52,17 +56,18 @@ function mod:GetOptions()
 end
 
 function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1") -- Violent Flora, Weaver's Burden
+
 	self:Log("SPELL_CAST_START", "Continuum", 420846)
 	self:Log("SPELL_AURA_APPLIED", "VerdantMatrixApplied", 420554)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "VerdantMatrixApplied", 420554)
-	self:Log("SPELL_CAST_START", "ViolentFlora", 424477)
+	--self:Log("SPELL_CAST_START", "ViolentFlora", 424477) -- USCS
 	self:Log("SPELL_AURA_APPLIED", "InflorescenceApplied", 423195)
 	self:Log("SPELL_AURA_REMOVED", "InflorescenceRemoved", 423195)
-	self:Log("SPELL_AURA_APPLIED", "DrowsyExpulsionApplied", 413442)
-	self:Log("SPELL_CAST_SUCCESS", "ThreadsOfLife", 418490)
+	self:Log("SPELL_CAST_SUCCESS", "ThreadsOfLife", 423094)
 	self:Log("SPELL_AURA_APPLIED", "ThreadsOfLifeApplied", 425745)
 	self:Log("SPELL_CAST_START", "ThreadedBlast", 426147)
-	self:Log("SPELL_CAST_SUCCESS", "WeaversBurden", 426520)
+	--self:Log("SPELL_CAST_SUCCESS", "WeaversBurden", 426520) -- USCS
 	self:Log("SPELL_AURA_APPLIED", "WeaversBurdenApplied", 426520)
 	self:Log("SPELL_AURA_REMOVED", "WeaversBurdenRemoved", 426520)
 	self:Log("SPELL_CAST_SUCCESS", "ViridianRain", 420907)
@@ -71,33 +76,44 @@ end
 function mod:OnEngage()
 	continuumCount = 1
 	violentFloraCount = 1
-	drowsyExpulsionCount = 1
 	threadsOfLifeCount = 1
-	threadedBlastCount = 1
 	weaversBurdenCount = 1
 	viridianRainCount = 1
 
 	inflorescenceOnMe = false
 
-	-- self:Bar(420846, 30, CL.count:format(self:SpellName(420846), continuumCount)) -- Continuum
-	-- self:Bar(424477, 30, CL.count:format(self:SpellName(424477), violentFloraCount)) -- Violent Flora
-	-- self:Bar(413442, 30, CL.count:format(self:SpellName(413442), drowsyExpulsionCount)) -- Drowsy Expulsion
-	-- self:Bar(425745, 30, CL.count:format(self:SpellName(425745), threadsOfLifeCount)) -- Threads of Life
-	-- self:Bar(426147, 30, CL.count:format(self:SpellName(426147), threadedBlastCount)) -- Threaded Blast
-	-- self:Bar(426520, 30, CL.count:format(self:SpellName(426520), weaversBurdenCount)) -- Weaver's Burden
-	-- self:Bar(420907, 30, CL.count:format(self:SpellName(420907), viridianRainCount)) -- Viridian Rain
+	self:Bar(420907, 7, CL.count:format(self:SpellName(420907), viridianRainCount)) -- Viridian Rain
+	self:Bar(424477, 15, CL.count:format(self:SpellName(424477), violentFloraCount)) -- Violent Flora
+	self:Bar(426520, 27, CL.count:format(self:SpellName(426520), weaversBurdenCount)) -- Weaver's Burden
+	self:Bar(425745, 35, CL.count:format(self:SpellName(425745), threadsOfLifeCount)) -- Threads of Life
+	self:Bar(420846, 90, CL.count:format(self:SpellName(420846), continuumCount)) -- Continuum
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
 
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
+	if spellId == 423858 then -- Violent Flora
+		self:StopBar(CL.count:format(self:SpellName(423858), violentFloraCount))
+		self:Message(424477, "yellow", CL.count:format(self:SpellName(423858), violentFloraCount))
+		self:PlaySound(424477, "long")
+		violentFloraCount = violentFloraCount + 1
+		self:Bar(424477, timers[424477][violentFloraCount], CL.count:format(self:SpellName(423858), violentFloraCount))
+	elseif spellId == 426519 then -- Weaver's Burden
+		self:StopBar(CL.count:format(self:SpellName(426519), weaversBurdenCount))
+		weaversBurdenCount = weaversBurdenCount + 1
+		self:Bar(426520, timers[424477][weaversBurdenCount], CL.count:format(self:SpellName(426519), weaversBurdenCount))
+	end
+end
+
 function mod:Continuum(args)
 	self:StopBar(CL.count:format(args.spellName, continuumCount))
 	self:Message(args.spellId, "cyan", CL.count:format(args.spellName, continuumCount))
 	self:PlaySound(args.spellId, "info")
 	continuumCount = continuumCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(args.spellName, continuumCount))
+	self:Bar(args.spellId, 90, CL.count:format(args.spellName, continuumCount))
 end
 
 function mod:VerdantMatrixApplied(args)
@@ -108,13 +124,13 @@ function mod:VerdantMatrixApplied(args)
 	end
 end
 
-function mod:ViolentFlora(args)
-	self:StopBar(CL.count:format(args.spellName, violentFloraCount))
-	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, violentFloraCount))
-	self:PlaySound(args.spellId, "long")
-	violentFloraCount = violentFloraCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(args.spellName, violentFloraCount))
-end
+-- function mod:ViolentFlora(args)
+-- 	self:StopBar(CL.count:format(args.spellName, violentFloraCount))
+-- 	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, violentFloraCount))
+-- 	self:PlaySound(args.spellId, "long")
+-- 	violentFloraCount = violentFloraCount + 1
+-- 	--self:Bar(args.spellId, 20, CL.count:format(args.spellName, violentFloraCount))
+-- end
 
 function mod:InflorescenceApplied(args)
 	if self:Me(args.destGUID) then
@@ -130,29 +146,12 @@ function mod:InflorescenceRemoved(args)
 	end
 end
 
-do
-	local prev = 0
-	function mod:DrowsyExpulsionApplied(args)
-		if args.time - prev > 2 then -- reset
-			prev = args.time
-			self:StopBar(CL.count:format(args.spellName, drowsyExpulsionCount))
-			self:Message(args.spellId, "orange", CL.count:format(args.spellName, drowsyExpulsionCount))
-			drowsyExpulsionCount = drowsyExpulsionCount + 1
-			--self:Bar(args.spellId, 30, CL.count:format(args.spellName, drowsyExpulsionCount))
-		end
-
-		if self:Me(args.destGUID) then
-			self:PersonalMessage(args.spellId)
-		end
-	end
-end
-
 function mod:ThreadsOfLife(args)
 	self:StopBar(CL.count:format(args.spellName, threadsOfLifeCount))
 	self:Message(425745, "yellow", CL.count:format(args.spellName, threadsOfLifeCount))
 	self:PlaySound(425745, "alert")
 	threadsOfLifeCount = threadsOfLifeCount + 1
-	--self:Bar(425745, 20, CL.count:format(args.spellName, threadsOfLifeCount))
+	self:Bar(425745, timers[425745][threadsOfLifeCount], CL.count:format(args.spellName, threadsOfLifeCount))
 end
 
 function mod:ThreadsOfLifeApplied(args)
@@ -164,17 +163,8 @@ function mod:ThreadsOfLifeApplied(args)
 end
 
 function mod:ThreadedBlast(args)
-	self:StopBar(CL.count:format(args.spellName, threadedBlastCount))
-	self:Message(args.spellId, "purple", CL.count:format(args.spellName, threadedBlastCount))
-	self:PlaySound(args.spellId, "alert")
-	threadedBlastCount = threadedBlastCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(args.spellName, threadedBlastCount))
-end
-
-function mod:WeaversBurden(args)
-	self:StopBar(CL.count:format(args.spellName, weaversBurdenCount))
-	weaversBurdenCount = weaversBurdenCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(args.spellName, weaversBurdenCount))
+	self:Message(args.spellId, "purple")
+	--self:PlaySound(args.spellId, "alert")
 end
 
 function mod:WeaversBurdenApplied(args)
@@ -198,5 +188,5 @@ function mod:ViridianRain(args)
 	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, viridianRainCount))
 	self:PlaySound(args.spellId, "alarm")
 	viridianRainCount = viridianRainCount + 1
-	--self:Bar(args.spellId, 20, CL.count:format(args.spellName, viridianRainCount))
+	self:Bar(args.spellId, timers[args.spellId][viridianRainCount], CL.count:format(args.spellName, viridianRainCount))
 end
