078c23c6a6a56bc24eaa29bee630edbe9d07aaf2
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2023-09-25 21:07:39 +0200

    Amirdrassil/Volcoross: Renames and updates

--- Amirdrassil/Volcoross.lua
+++ Amirdrassil/Volcoross.lua
@@ -26,7 +26,10 @@ local cataclysmJawsCount = 1
 
 local L = mod:GetLocale()
 if L then
-	L.placeholder = "placeholder"
+	L.flood_of_the_firelands = "Soaks"
+	L.scorchtail_crash = "Tail Slam"
+	L.serpents_fury = "Flames"
+	L.coiling_flames_single = "Flames" -- Do we need a single locale for this?
 end
 
 --------------------------------------------------------------------------------
@@ -39,13 +42,18 @@ function mod:GetOptions()
 		421672, -- Serpent's Fury
 		{421207, "SAY", "SAY_COUNTDOWN"}, -- Coiling Flames
 		420933, -- Flood of the Firelands
-		421616, -- Volcanic Disgorge
+		{421616, "SAY"}, -- Volcanic Disgorge
 		420415, -- Scorchtail Crash
 		423494, -- Tidal Blaze
 		{419054, "TANK"}, -- Molten Venom
 		{423117, "TANK"}, -- Cataclysm Jaws
 		421703, -- Serpent's Wrath
 		424218, -- Combusting Rage
+	}, nil, {
+		[421672] = L.serpents_fury, -- Serpent's Fury (Flames)
+		[420933] = L.flood_of_the_firelands, -- Flood of the Firelands (Soaks)
+		[421616] = CL.pools, -- Volcanic Disgorge (Pools)
+		[420415] = L.scorchtail_crash, -- Scorchtail Crash (Tail Slam)
 	}
 end
 
@@ -77,10 +85,10 @@ function mod:OnEngage()
 	cataclysmJawsCount = 1
 
 	self:Bar(423117, 5, CL.count:format(self:SpellName(423117), cataclysmJawsCount)) -- Cataclysm Jaws
-	self:Bar(421672, 10, CL.count:format(self:SpellName(421672), serpentsFuryCount)) -- Serpent's Fury
-	self:Bar(420415, 20, CL.count:format(self:SpellName(420415), scorchtailCrashCount)) -- Scorchtail Crash
-	self:Bar(421616, 30, CL.count:format(self:SpellName(421616), volcanicDisgorgeCount)) -- Volcanic Disgorge
-	self:Bar(420933, 70, CL.count:format(self:SpellName(420933), floodOfTheFirelandsCount)) -- Flood of the Firelands
+	self:Bar(421672, 10, CL.count:format(L.serpents_fury, serpentsFuryCount)) -- Serpent's Fury
+	self:Bar(420415, 20, CL.count:format(L.scorchtail_crash, scorchtailCrashCount)) -- Scorchtail Crash
+	self:Bar(421616, 30, CL.count:format(CL.pools, volcanicDisgorgeCount)) -- Volcanic Disgorge
+	self:Bar(420933, 70, CL.count:format(L.flood_of_the_firelands, floodOfTheFirelandsCount)) -- Flood of the Firelands
 end
 
 --------------------------------------------------------------------------------
@@ -89,21 +97,26 @@ end
 
 function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
 	if spellId == 421356 or spellId == 421359 or spellId == 421684 then -- Scorchtail Crash
-		self:StopBar(CL.count:format(self:SpellName(420415), scorchtailCrashCount))
-		self:Message(420415, "red", CL.count:format(self:SpellName(420415), scorchtailCrashCount))
+		self:StopBar(CL.count:format(L.scorchtail_crash, scorchtailCrashCount))
+		self:Message(420415, "red", CL.count:format(L.scorchtail_crash, scorchtailCrashCount))
 		self:PlaySound(420415, "alarm")
 		scorchtailCrashCount = scorchtailCrashCount + 1
-		-- This need some more fixing
-		self:Bar(420415, 20, CL.count:format(self:SpellName(420415), scorchtailCrashCount)) -- Scorchtail Crash
+		local cd = 20
+		if scorchtailCrashCount == 4 or scorchtailCrashCount == 9 or scorchtailCrashCount == 14 then
+			cd = 30
+		elseif scorchtailCrashCount < 14 and scorchtailCrashCount > 4 then -- Raid Split Up
+			cd = 10
+		end
+		self:Bar(420415, cd, CL.count:format(L.scorchtail_crash, scorchtailCrashCount)) -- Scorchtail Crash
 	end
 end
 
 function mod:SerpentsFury(args)
-	self:StopBar(CL.count:format(args.spellName, serpentsFuryCount))
-	self:Message(args.spellId, "cyan", CL.count:format(args.spellName, serpentsFuryCount))
+	self:StopBar(CL.count:format(L.serpents_fury, serpentsFuryCount))
+	self:Message(args.spellId, "cyan", CL.count:format(L.serpents_fury, serpentsFuryCount))
 	self:PlaySound(args.spellId, "alert")
 	serpentsFuryCount = serpentsFuryCount + 1
-	self:Bar(args.spellId, 70, CL.count:format(args.spellName, serpentsFuryCount))
+	self:Bar(args.spellId, 70, CL.count:format(L.serpents_fury, serpentsFuryCount))
 end
 
 do
@@ -111,7 +124,7 @@ do
 		if self:Me(args.destGUID) then
 			self:PersonalMessage(args.spellId)
 			self:PlaySound(args.spellId, "warning")
-			self:Say(args.spellId)
+			self:Say(args.spellId, L.coiling_flames_single)
 			self:SayCountdown(args.spellId, 10)
 		end
 	end
@@ -124,24 +137,40 @@ do
 end
 
 function mod:FloodOfTheFirelands(args)
-	self:StopBar(CL.count:format(args.spellName, floodOfTheFirelandsCount))
-	self:Message(args.spellId, "orange", CL.count:format(args.spellName, floodOfTheFirelandsCount))
+	self:StopBar(CL.count:format(L.flood_of_the_firelands, floodOfTheFirelandsCount))
+	self:Message(args.spellId, "orange", CL.count:format(L.flood_of_the_firelands, floodOfTheFirelandsCount))
 	self:PlaySound(args.spellId, "long")
 	floodOfTheFirelandsCount = floodOfTheFirelandsCount + 1
-	self:Bar(args.spellId, 70, CL.count:format(args.spellName, floodOfTheFirelandsCount))
+	self:Bar(args.spellId, 70, CL.count:format(L.flood_of_the_firelands, floodOfTheFirelandsCount))
 end
 
-function mod:VolcanicDisgorge(args)
-	self:StopBar(CL.count:format(args.spellName, volcanicDisgorgeCount))
-	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, volcanicDisgorgeCount))
-	self:PlaySound(args.spellId, "alert")
-	volcanicDisgorgeCount = volcanicDisgorgeCount + 1
-	local cdTable = {
-		[2] = 20,
-		[3] = 40,
-		[8] = 30,
-	}
-	self:Bar(args.spellId, cdTable[volcanicDisgorgeCount] or 10, CL.count:format(args.spellName, volcanicDisgorgeCount))
+do
+	local function printTarget(self, name, guid)
+		self:TargetMessage(421616, "yellow", name, CL.count:format(CL.pools, volcanicDisgorgeCount-1))
+		if self:Me(guid) then
+			self:PlaySound(421616, "warning") -- move
+			self:Say(421616, CL.pools)
+		else
+			self:PlaySound(421616, "alert")
+		end
+	end
+
+	function mod:VolcanicDisgorge(args)
+		self:StopBar(CL.count:format(CL.pools, volcanicDisgorgeCount))
+		--self:Message(args.spellId, "yellow", CL.count:format(CL.pools, volcanicDisgorgeCount))
+		--self:PlaySound(args.spellId, "alert")
+		self:GetBossTarget(printTarget, 0.1, args.sourceGUID)
+		volcanicDisgorgeCount = volcanicDisgorgeCount + 1
+		local cd = 20
+		if volcanicDisgorgeCount == 3 or volcanicDisgorgeCount == 13 then
+			cd = 40
+		elseif volcanicDisgorgeCount == 8 then
+			cd = 30
+		elseif volcanicDisgorgeCount < 13 and volcanicDisgorgeCount > 3 then -- Raid Split Up
+			cd = 10
+		end
+		self:Bar(args.spellId, cd, CL.count:format(CL.pools, volcanicDisgorgeCount))
+	end
 end
 
 -- function mod:ScorchtailCrash(args)
@@ -171,6 +200,8 @@ function mod:CataclysmJaws(args)
 		[4] = 40,
 		[6] = 40,
 		[8] = 25,
+		[9] = 25,
+		[10] = 20,
 	}
 	self:Bar(args.spellId, cdTable[cataclysmJawsCount] or 30, CL.count:format(args.spellName, cataclysmJawsCount))
 end
