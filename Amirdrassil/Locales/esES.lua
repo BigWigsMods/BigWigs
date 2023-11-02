ebfadca825d90a6ad5198b40142f5aeee43ec36a
Eduardo Rosselot <32552126+earosselot@users.noreply.github.com>
2023-11-02 23:19:45 +0100

    Core and Amirdrassil: Locales esES

---
+++ Amirdrassil/Locales/esES.lua
@@ -0,0 +1,90 @@
+local L = BigWigs:NewBossLocale("Gnarlroot", "esES")
+if not L then return end
+if L then
+    L.shadowflame_cleave = "Cono Frontal"
+	L.tortured_scream = "Grito de Tortura"
+end
+
+L = BigWigs:NewBossLocale("Igira the Cruel", "esES")
+if L then
+	L.blistering_spear = "Lanzas"
+	L.blistering_spear_single = "Lanza"
+	L.blistering_torment = "Cadena"
+	L.twisting_blade = "Espadas"
+	L.marked_for_torment = "Tormento"
+	L.wracking_skewer = "Soak"
+	L.heart_stopper = "Absorber curación"
+	L.heart_stopper_single = "Absorber curación"
+end
+
+L = BigWigs:NewBossLocale("Council of Dreams", "esES")
+if L then
+    L.ultimate_boss = "Definitiva (%s)"
+    L.charge = "Carga"
+    L.poisonous_javelin = "Javalina"
+    L.polymorph_bomb = "Patos"
+    L.polymorph_bomb_single = "Pato"
+end
+
+L = BigWigs:NewBossLocale("Volcoross", "esES")
+if L then
+	L.flood_of_the_firelands = "Soaks"
+	L.scorchtail_crash = "Golpe de Cola"
+	L.serpents_fury = "Llamas"
+	L.coiling_flames_single = "Llamas" -- Do we need a single locale for this?
+end
+
+L = BigWigs:NewBossLocale("Nymue, Weaver of the Cycle", "esES")
+if L then
+	L.continuum = "Nuevas Lineas"
+	L.impending_loom = "Esquivar"
+	L.viridian_rain = "Daño de Banda"
+	L.lumbering_slam = "Cono frontal"
+end
+
+L = BigWigs:NewBossLocale("Larodar, Keeper of the Flame")
+if L then
+	-- L.custom_on_repeating_yell_smoldering_suffocation = "Repeating Suffocation Health Yell"
+	-- L.custom_on_repeating_yell_smoldering_suffocation_desc = "Repeating yell messages for Smoldering Suffocation to let others know when you are below 75% health."
+
+	L.currentHealth = "%d%%"
+	L.currentHealthIcon = "{rt%d}%d%%"
+
+	L.scorching_roots = "Raíces"
+	L.furious_charge = "Carga"
+	L.blazing_thorns = "Esquivar"
+	L.falling_embers = "Soaks individuales"
+	L.smoldering_backdraft = "Frontal"
+	L.fire_whirl = "Tornados"
+end
+
+L = BigWigs:NewBossLocale("Smolderon", "esES")
+if L then
+	L.brand_of_damnation = "Sokeo de Tanque"
+	L.lava_geysers = "Géiseres"
+end
+
+L = BigWigs:NewBossLocale("Tindral Sageswift, Seer of the Flame", "esES")
+if L then
+	L.blazing_mushroom = "Hongos"
+	L.fiery_growth = "Pool Dispels"
+	L.falling_stars = "Estrellas"
+	L.falling_stars_single = "Estrella"
+	L.mass_entanglement = "Raíces"
+	L.mass_entanglement_single = "Raiz"
+	L.incarnation_moonkin = "Forma de Pollo"
+	L.incarnation_tree_of_flame = "Forma de Arbol"
+	L.tranquility_of_flame = "Semillas"
+end
+
+L = BigWigs:NewBossLocale("Fyrakk the Blazing", "esES")
+if L then
+	-- L.firestorm = "Meteóros"
+	-- L.dream_rend = "Pull In"
+	-- L.fyralaths_bite = "Mordida de tanque"
+	-- L.fyralaths_mark = "Marca"
+	-- L.incarnate = "Levantar"
+	-- L.greater_firestorm = "Meteóros [G]" -- G de Grande
+	-- L.shadowflame_devastation = "Respiración profunda"
+	-- L.eternal_firestorm = "Meteóros [E]" -- E de Eterno
+end
