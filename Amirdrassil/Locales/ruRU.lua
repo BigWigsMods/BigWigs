68efdd6cb4667506ebee0a7bc3d0dd64fcb5b69a
KnewOne <illia.fylypovych@apollobase.com>
2023-11-11 12:45:27 +0200

    Update ruRu, Fix Trash.lua

---
+++ Amirdrassil/Locales/ruRU.lua
@@ -0,0 +1,94 @@
+local L = BigWigs:NewBossLocale("Gnarlroot", "ruRU")
+if not L then return end
+if L then
+	L.shadowflame_cleave = "Фронтал"
+	L.tortured_scream = "Крик"
+end
+
+L = BigWigs:NewBossLocale("Igira the Cruel", "ruRU")
+if L then
+	L.blistering_spear = "Копья"
+	L.blistering_spear_single = "Копьё"
+	L.blistering_torment = "Цепь"
+	L.twisting_blade = "Фронтал"
+	L.marked_for_torment = "Переходка"
+	L.wracking_skewer = "Соак"
+	L.heart_stopper = "Абсорб"
+	L.heart_stopper_single = "Абсорб"
+end
+
+L = BigWigs:NewBossLocale("Volcoross", "ruRU")
+if L then
+	L.flood_of_the_firelands = "Соки"
+	L.scorchtail_crash = "Хвост"
+	L.serpents_fury = "Дебаффы"
+	L.coiling_flames_single = "Дебафф"
+end
+
+L = BigWigs:NewBossLocale("Council of Dreams", "ruRU")
+if L then
+	L.ultimate_boss = "Ульта (%s)"
+	L.charge = "Чардж"
+	L.poisonous_javelin = "Отравленное копьё"
+	L.polymorph_bomb = "Утки"
+	L.polymorph_bomb_single = "Утка"
+end
+
+L = BigWigs:NewBossLocale("Larodar, Keeper of the Flame", "ruRU")
+if L then
+	L.custom_on_repeating_yell_smoldering_suffocation = "Крик при низком здоровье на Тлеющем Удульше"
+	L.custom_on_repeating_yell_smoldering_suffocation_desc = "Повторяющиеся крики при здоровье < 75% для игроков с Тлеющем Удушье для координации с другими игроками."
+
+	L.currentHealth = "%d%%"
+	L.currentHealthIcon = "{rt%d}%d%%"
+
+	L.scorching_roots = "Корень"
+	L.furious_charge = "Чардж"
+	L.blazing_thorns = "Спирали"
+	L.falling_embers = "Соки"
+	L.smoldering_backdraft = "Фронтал"
+	L.fire_whirl = "Торнадо"
+end
+
+L = BigWigs:NewBossLocale("Nymue, Weaver of the Cycle", "ruRU")
+if L then
+	L.continuum = "Новые Узоры"
+	L.impending_loom = "Доджи"
+	L.viridian_rain = "Рейд урон"
+	L.lumbering_slam = "Фронтал"
+end
+
+L = BigWigs:NewBossLocale("Smolderon", "ruRU")
+if L then
+	L.brand_of_damnation = "Танк Соак"
+	L.lava_geysers = "Гейзеры"
+end
+
+L = BigWigs:NewBossLocale("Tindral Sageswift, Seer of the Flame", "ruRU")
+if L then
+	L.blazing_mushroom = "Грибы"
+	L.fiery_growth = "Диспеллы"
+	L.falling_stars = "Звёзды"
+	L.falling_stars_single = "Звезда"
+	L.mass_entanglement = "Корни"
+	L.mass_entanglement_single = "Корни"
+	L.incarnation_moonkin = "Совиная форма "
+	L.incarnation_tree_of_flame = "Древоформа"
+	L.tranquility_of_flame = "Семена"
+end
+
+L = BigWigs:NewBossLocale("Fyrakk the Blazing", "ruRU")
+if L then
+	L.firestorm = "Метеориты"
+	L.dream_rend = "Притягивание"
+	L.fyralaths_bite = "Танкбастер"
+	L.fyralaths_mark = "Метка"
+	L.incarnate = "Подкидка"
+	L.shadowflame_devastation = "Deep Breath"
+	L.greater_firestorm = "Метеориты" -- "B" for both greater and eternal in ru translation, also pointless distinction
+	L.eternal_firestorm = "Метеориты" -- see above
+end
+
+L = BigWigs:NewBossLocale("Amirdrassil, the Dream's Hope Trash", "ruRU")
+if L then
+end
