976ee1c9a46ac70611a09f9b5b6adba07bfb054a
funkydude <funkydude@users.noreply.github.com>
2023-11-14 04:49:46 +0000

    Update esES (#1442)

--- Amirdrassil/Locales/esES.lua
+++ Amirdrassil/Locales/esES.lua
@@ -1,37 +1,37 @@
 local L = BigWigs:NewBossLocale("Gnarlroot", "esES")
 if not L then return end
 if L then
-	--L.shadowflame_cleave = "Frontal Cone"
-	--L.tortured_scream = "Scream"
+	L.shadowflame_cleave = "Cono Frontal"
+	L.tortured_scream = "Grito de Tortura"
 end
 
 L = BigWigs:NewBossLocale("Igira the Cruel", "esES")
 if L then
-	--L.blistering_spear = "Spears"
-	--L.blistering_spear_single = "Spear"
-	--L.blistering_torment = "Chain"
-	--L.twisting_blade = "Blades"
-	--L.marked_for_torment = "Torment"
-	--L.wracking_skewer = "Soak"
-	--L.heart_stopper = "Heal Absorbs"
-	--L.heart_stopper_single = "Heal Absorb"
+	L.blistering_spear = "Lanzas"
+	L.blistering_spear_single = "Lanza"
+	L.blistering_torment = "Cadena"
+	L.twisting_blade = "Espadas"
+	L.marked_for_torment = "Tormento"
+	L.wracking_skewer = "Soak"
+	L.heart_stopper = "Absorber curación"
+	L.heart_stopper_single = "Absorber curación"
 end
 
 L = BigWigs:NewBossLocale("Volcoross", "esES")
 if L then
-	--L.flood_of_the_firelands = "Soaks"
-	--L.scorchtail_crash = "Tail Slam"
-	--L.serpents_fury = "Flames"
-	--L.coiling_flames_single = "Flames"
+	L.flood_of_the_firelands = "Soaks"
+	L.scorchtail_crash = "Golpe de Cola"
+	L.serpents_fury = "Llamas"
+	L.coiling_flames_single = "Llamas"
 end
 
 L = BigWigs:NewBossLocale("Council of Dreams", "esES")
 if L then
-	--L.ultimate_boss = "Ultimate (%s)"
-	--L.charge = "Charge"
-	--L.poisonous_javelin = "Javelin"
-	--L.polymorph_bomb = "Ducks"
-	--L.polymorph_bomb_single = "Duck"
+	L.ultimate_boss = "Definitiva (%s)"
+	L.charge = "Carga"
+	L.poisonous_javelin = "Javalina"
+	L.polymorph_bomb = "Patos"
+	L.polymorph_bomb_single = "Pato"
 end
 
 L = BigWigs:NewBossLocale("Larodar, Keeper of the Flame", "esES")
@@ -39,39 +39,39 @@ if L then
 	--L.custom_on_repeating_yell_smoldering_suffocation = "Repeating Suffocation Health Yell"
 	--L.custom_on_repeating_yell_smoldering_suffocation_desc = "Repeating yell messages for Smoldering Suffocation to let others know when you are below 75% health."
 
-	--L.scorching_roots = "Roots"
-	--L.furious_charge = "Charge"
-	--L.blazing_thorns = "Dodges"
-	--L.falling_embers = "Soaks"
-	--L.smoldering_backdraft = "Frontal"
-	--L.fire_whirl = "Tornadoes"
+	L.scorching_roots = "Raíces"
+	L.furious_charge = "Carga"
+	L.blazing_thorns = "Esquivar"
+	L.falling_embers = "Soaks individuales"
+	L.smoldering_backdraft = "Frontal"
+	L.fire_whirl = "Tornados"
 end
 
 L = BigWigs:NewBossLocale("Nymue, Weaver of the Cycle", "esES")
 if L then
-	--L.continuum = "New Lines"
-	--L.impending_loom = "Dodges"
-	--L.viridian_rain = "Raid Damage"
-	--L.lumbering_slam = "Frontal Cone"
+	L.continuum = "Nuevas Lineas"
+	L.impending_loom = "Esquivar"
+	L.viridian_rain = "Daño de Banda"
+	L.lumbering_slam = "Cono frontal"
 end
 
 L = BigWigs:NewBossLocale("Smolderon", "esES")
 if L then
-	--L.brand_of_damnation = "Tank Soak"
-	--L.lava_geysers = "Geysers"
+	L.brand_of_damnation = "Sokeo de Tanque"
+	L.lava_geysers = "Géiseres"
 end
 
 L = BigWigs:NewBossLocale("Tindral Sageswift, Seer of the Flame", "esES")
 if L then
-	--L.blazing_mushroom = "Mushrooms"
-	--L.fiery_growth = "Pool Dispels"
-	--L.falling_stars = "Stars"
-	--L.falling_stars_single = "Star"
-	--L.mass_entanglement = "Roots"
-	--L.mass_entanglement_single = "Root"
-	--L.incarnation_moonkin = "Moonkin Form"
-	--L.incarnation_tree_of_flame = "Tree Form"
-	--L.tranquility_of_flame = "Seeds"
+	L.blazing_mushroom = "Hongos"
+	L.fiery_growth = "Pool Dispels"
+	L.falling_stars = "Estrellas"
+	L.falling_stars_single = "Estrella"
+	L.mass_entanglement = "Raíces"
+	L.mass_entanglement_single = "Raiz"
+	L.incarnation_moonkin = "Forma de Pollo"
+	L.incarnation_tree_of_flame = "Forma de Arbol"
+	L.tranquility_of_flame = "Semillas"
 end
 
 --L = BigWigs:NewBossLocale("Fyrakk the Blazing", "esES")
