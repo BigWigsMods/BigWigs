13c6bf15086c2a178069728725bcb8814bec48d1
funkydude <funkydude@users.noreply.github.com>
2023-11-14 04:44:16 +0000

    Update ruRU, closes #1444

--- Amirdrassil/Locales/ruRU.lua
+++ Amirdrassil/Locales/ruRU.lua
@@ -1,77 +1,77 @@
 local L = BigWigs:NewBossLocale("Gnarlroot", "ruRU")
 if not L then return end
 if L then
-	--L.shadowflame_cleave = "Frontal Cone"
-	--L.tortured_scream = "Scream"
+	L.shadowflame_cleave = "Фронтал"
+	L.tortured_scream = "Крик"
 end
 
 L = BigWigs:NewBossLocale("Igira the Cruel", "ruRU")
 if L then
-	--L.blistering_spear = "Spears"
-	--L.blistering_spear_single = "Spear"
-	--L.blistering_torment = "Chain"
-	--L.twisting_blade = "Blades"
-	--L.marked_for_torment = "Torment"
-	--L.wracking_skewer = "Soak"
-	--L.heart_stopper = "Heal Absorbs"
-	--L.heart_stopper_single = "Heal Absorb"
+	L.blistering_spear = "Копья"
+	L.blistering_spear_single = "Копьё"
+	L.blistering_torment = "Цепь"
+	L.twisting_blade = "Фронтал"
+	L.marked_for_torment = "Переходка"
+	L.wracking_skewer = "Соак"
+	L.heart_stopper = "Абсорб"
+	L.heart_stopper_single = "Абсорб"
 end
 
 L = BigWigs:NewBossLocale("Volcoross", "ruRU")
 if L then
-	--L.flood_of_the_firelands = "Soaks"
-	--L.scorchtail_crash = "Tail Slam"
-	--L.serpents_fury = "Flames"
-	--L.coiling_flames_single = "Flames"
+	L.flood_of_the_firelands = "Соки"
+	L.scorchtail_crash = "Хвост"
+	L.serpents_fury = "Дебаффы"
+	L.coiling_flames_single = "Дебафф"
 end
 
 L = BigWigs:NewBossLocale("Council of Dreams", "ruRU")
 if L then
-	--L.ultimate_boss = "Ultimate (%s)"
-	--L.charge = "Charge"
-	--L.poisonous_javelin = "Javelin"
-	--L.polymorph_bomb = "Ducks"
-	--L.polymorph_bomb_single = "Duck"
+	L.ultimate_boss = "Ульта (%s)"
+	L.charge = "Чардж"
+	L.poisonous_javelin = "Отравленное копьё"
+	L.polymorph_bomb = "Утки"
+	L.polymorph_bomb_single = "Утка"
 end
 
 L = BigWigs:NewBossLocale("Larodar, Keeper of the Flame", "ruRU")
 if L then
-	--L.custom_on_repeating_yell_smoldering_suffocation = "Repeating Suffocation Health Yell"
-	--L.custom_on_repeating_yell_smoldering_suffocation_desc = "Repeating yell messages for Smoldering Suffocation to let others know when you are below 75% health."
+	L.custom_on_repeating_yell_smoldering_suffocation = "Крик при низком здоровье на Тлеющем Удульше"
+	L.custom_on_repeating_yell_smoldering_suffocation_desc = "Повторяющиеся крики при здоровье < 75% для игроков с Тлеющем Удушье для координации с другими игроками."
 
-	--L.scorching_roots = "Roots"
-	--L.furious_charge = "Charge"
-	--L.blazing_thorns = "Dodges"
-	--L.falling_embers = "Soaks"
-	--L.smoldering_backdraft = "Frontal"
-	--L.fire_whirl = "Tornadoes"
+	L.scorching_roots = "Корень"
+	L.furious_charge = "Чардж"
+	L.blazing_thorns = "Спирали"
+	L.falling_embers = "Соки"
+	L.smoldering_backdraft = "Фронтал"
+	L.fire_whirl = "Торнадо"
 end
 
 L = BigWigs:NewBossLocale("Nymue, Weaver of the Cycle", "ruRU")
 if L then
-	--L.continuum = "New Lines"
-	--L.impending_loom = "Dodges"
-	--L.viridian_rain = "Raid Damage"
-	--L.lumbering_slam = "Frontal Cone"
+	L.continuum = "Новые Узоры"
+	L.impending_loom = "Доджи"
+	L.viridian_rain = "Рейд урон"
+	L.lumbering_slam = "Фронтал"
 end
 
 L = BigWigs:NewBossLocale("Smolderon", "ruRU")
 if L then
-	--L.brand_of_damnation = "Tank Soak"
-	--L.lava_geysers = "Geysers"
+	L.brand_of_damnation = "Танк Соак"
+	L.lava_geysers = "Гейзеры"
 end
 
 L = BigWigs:NewBossLocale("Tindral Sageswift, Seer of the Flame", "ruRU")
 if L then
-	--L.blazing_mushroom = "Mushrooms"
-	--L.fiery_growth = "Pool Dispels"
-	--L.falling_stars = "Stars"
-	--L.falling_stars_single = "Star"
-	--L.mass_entanglement = "Roots"
-	--L.mass_entanglement_single = "Root"
-	--L.incarnation_moonkin = "Moonkin Form"
-	--L.incarnation_tree_of_flame = "Tree Form"
-	--L.tranquility_of_flame = "Seeds"
+	L.blazing_mushroom = "Грибы"
+	L.fiery_growth = "Диспеллы"
+	L.falling_stars = "Звёзды"
+	L.falling_stars_single = "Звезда"
+	L.mass_entanglement = "Корни"
+	L.mass_entanglement_single = "Корни"
+	L.incarnation_moonkin = "Совиная форма "
+	L.incarnation_tree_of_flame = "Древоформа"
+	L.tranquility_of_flame = "Семена"
 end
 
 --L = BigWigs:NewBossLocale("Fyrakk the Blazing", "ruRU")
