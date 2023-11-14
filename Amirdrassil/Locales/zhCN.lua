7bae18f9e4aa359171eef913426ff14747b36c09
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2023-11-14 12:55:14 +0800

    Update zhCN (#1445)

--- Amirdrassil/Locales/zhCN.lua
+++ Amirdrassil/Locales/zhCN.lua
@@ -1,77 +1,77 @@
 local L = BigWigs:NewBossLocale("Gnarlroot", "zhCN")
 if not L then return end
 if L then
-	--L.shadowflame_cleave = "Frontal Cone"
-	--L.tortured_scream = "Scream"
+	L.shadowflame_cleave = "顺劈"
+	L.tortured_scream = "尖啸"
 end
 
 L = BigWigs:NewBossLocale("Igira the Cruel", "zhCN")
 if L then
-	--L.blistering_spear = "Spears"
-	--L.blistering_spear_single = "Spear"
-	--L.blistering_torment = "Chain"
-	--L.twisting_blade = "Blades"
-	--L.marked_for_torment = "Torment"
-	--L.wracking_skewer = "Soak"
-	--L.heart_stopper = "Heal Absorbs"
-	--L.heart_stopper_single = "Heal Absorb"
+	L.blistering_spear = "长矛"
+	L.blistering_spear_single = "长矛"
+	L.blistering_torment = "链条"
+	L.twisting_blade = "飞刀"
+	L.marked_for_torment = "折磨"
+	L.wracking_skewer = "分摊"
+	L.heart_stopper = "吸收治疗"
+	L.heart_stopper_single = "吸收治疗"
 end
 
 L = BigWigs:NewBossLocale("Volcoross", "zhCN")
 if L then
-	--L.flood_of_the_firelands = "Soaks"
-	--L.scorchtail_crash = "Tail Slam"
-	--L.serpents_fury = "Flames"
-	--L.coiling_flames_single = "Flames"
+	L.flood_of_the_firelands = "分摊"
+	L.scorchtail_crash = "尾击"
+	L.serpents_fury = "怒焰"
+	L.coiling_flames_single = "怒焰"
 end
 
 L = BigWigs:NewBossLocale("Council of Dreams", "zhCN")
 if L then
-	--L.ultimate_boss = "Ultimate (%s)"
-	--L.charge = "Charge"
-	--L.poisonous_javelin = "Javelin"
-	--L.polymorph_bomb = "Ducks"
-	--L.polymorph_bomb_single = "Duck"
+	L.ultimate_boss = "终极 (%s)"
+	L.charge = "冲锋"
+	L.poisonous_javelin = "标枪"
+	L.polymorph_bomb = "鸭子"
+	L.polymorph_bomb_single = "鸭子"
 end
 
 L = BigWigs:NewBossLocale("Larodar, Keeper of the Flame", "zhCN")
 if L then
-	--L.custom_on_repeating_yell_smoldering_suffocation = "Repeating Suffocation Health Yell"
-	--L.custom_on_repeating_yell_smoldering_suffocation_desc = "Repeating yell messages for Smoldering Suffocation to let others know when you are below 75% health."
+	L.custom_on_repeating_yell_smoldering_suffocation = "焖燃窒息血量重复喊话"
+	L.custom_on_repeating_yell_smoldering_suffocation_desc = "当你的血量低于75%时，持续喊话通报你的血量。"
 
-	--L.scorching_roots = "Roots"
-	--L.furious_charge = "Charge"
-	--L.blazing_thorns = "Dodges"
-	--L.falling_embers = "Soaks"
-	--L.smoldering_backdraft = "Frontal"
-	--L.fire_whirl = "Tornadoes"
+	L.scorching_roots = "树根"
+	L.furious_charge = "冲锋"
+	L.blazing_thorns = "躲避"
+	L.falling_embers = "分摊"
+	L.smoldering_backdraft = "震慑波"
+	L.fire_whirl = "龙卷风"
 end
 
 L = BigWigs:NewBossLocale("Nymue, Weaver of the Cycle", "zhCN")
 if L then
-	--L.continuum = "New Lines"
-	--L.impending_loom = "Dodges"
-	--L.viridian_rain = "Raid Damage"
-	--L.lumbering_slam = "Frontal Cone"
+	L.continuum = "新矩阵"
+	L.impending_loom = "躲避"
+	L.viridian_rain = "团队伤害"
+	L.lumbering_slam = "震慑波"
 end
 
 L = BigWigs:NewBossLocale("Smolderon", "zhCN")
 if L then
-	--L.brand_of_damnation = "Tank Soak"
-	--L.lava_geysers = "Geysers"
+	L.brand_of_damnation = "坦克分摊"
+	L.lava_geysers = "喷泉"
 end
 
 L = BigWigs:NewBossLocale("Tindral Sageswift, Seer of the Flame", "zhCN")
 if L then
-	--L.blazing_mushroom = "Mushrooms"
-	--L.fiery_growth = "Pool Dispels"
-	--L.falling_stars = "Stars"
-	--L.falling_stars_single = "Star"
-	--L.mass_entanglement = "Roots"
-	--L.mass_entanglement_single = "Root"
-	--L.incarnation_moonkin = "Moonkin Form"
-	--L.incarnation_tree_of_flame = "Tree Form"
-	--L.tranquility_of_flame = "Seeds"
+	L.blazing_mushroom = "蘑菇"
+	L.fiery_growth = "水池驱散"
+	L.falling_stars = "流星"
+	L.falling_stars_single = "流星"
+	L.mass_entanglement = "藤蔓"
+	L.mass_entanglement_single = "藤蔓"
+	L.incarnation_moonkin = "枭兽形态"
+	L.incarnation_tree_of_flame = "烈焰之树"
+	L.tranquility_of_flame = "种子"
 end
 
 --L = BigWigs:NewBossLocale("Fyrakk the Blazing", "zhCN")
