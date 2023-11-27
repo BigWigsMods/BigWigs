657cd86a0c204fd7dae8cbe27067efad3a492e59
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2023-11-27 01:49:13 +0100

    Update deDE (#1456)

--- Amirdrassil/Locales/deDE.lua
+++ Amirdrassil/Locales/deDE.lua
@@ -30,6 +30,7 @@ if L then
 	L.ultimate_boss = "Ultimate (%s)"
 	L.barreling_charge = "Ansturm"
 	L.poisonous_javelin = "Wurfspeer"
+	L.song_of_the_dragon = "Lied"
 	L.polymorph_bomb = "Enten"
 	L.polymorph_bomb_single = "Ente"
 end
@@ -39,20 +40,24 @@ if L then
 	L.custom_on_repeating_yell_smoldering_suffocation = "Ersticken Gesundheit wiederholen"
 	L.custom_on_repeating_yell_smoldering_suffocation_desc = "Gibt wiederholt Chatnachrichten für Qualmendes Ersticken aus um mitzuteilen, dass unter 75% Gesundheit erreicht sind."
 
-	L.blazing_coalescence_boss = "{426256} (Boss)"
+	L.blazing_coalescence_boss = "{426256} (Boss Buff)"
 
 	L.scorching_roots = "Wurzeln"
 	L.furious_charge = "Ansturm"
 	L.blazing_thorns = "Ausweichen"
 	L.falling_embers = "Soaks"
-	L.smoldering_backdraft = "Frontaler Kegel"
+	L.smoldering_backdraft = "Frontal"
 	L.fire_whirl = "Wirbel"
 end
 
 L = BigWigs:NewBossLocale("Nymue, Weaver of the Cycle", "deDE")
 if L then
+	L.mythic_add_death = "%s getötet"
+
 	L.continuum = "Neue Fäden"
 	L.impending_loom = "Ausweichen"
+	L.surging_growth = "Neue Soaks"
+	L.ephemeral_flora = "Roter Soak"
 	L.viridian_rain = "Schaden + Bomben"
 	L.lumbering_slam = "Frontaler Kegel"
 end
@@ -61,12 +66,15 @@ L = BigWigs:NewBossLocale("Smolderon", "deDE")
 if L then
 	L.brand_of_damnation = "Tank Soak"
 	L.lava_geysers = "Geysire"
+	L.flame_waves = "Wirbel"
 end
 
 L = BigWigs:NewBossLocale("Tindral Sageswift, Seer of the Flame", "deDE")
 if L then
-	--L.seed_soaked = "Seed soaked"
-	--L.all_seeds_soaked = "Seeds done!"
+
+	L.seed_soaked = "Samen gesoaked"
+	L.all_seeds_soaked = "Samen fertig!"
+
 	L.blazing_mushroom = "Pilze"
 	L.fiery_growth = "Dispels"
 	L.mass_entanglement = "Wurzeln"
@@ -75,20 +83,20 @@ if L then
 	L.flaming_germination = "Samen"
 	L.suppressive_ember = "Heilungen absorbiert"
 	L.suppressive_ember_single = "Heilung absorbiert"
-	--L.flare_bomb = "Feathers"
+	L.flare_bomb = "Federn"
 end
 
 L = BigWigs:NewBossLocale("Fyrakk the Blazing", "deDE")
 if L then
 	L.spirit_trigger = "Geist der Kaldorei"
 
-	--L.fyralaths_bite = "Frontal"
-	--L.fyralaths_mark = "Mark"
-	--L.spirits_of_the_kaldorei = "Spirits"
-	--L.greater_firestorm_shortened_bar = "Firestorm [G]" -- G for Greater
-	--L.greater_firestorm_message_full = "Firestorm [Greater]"
-	--L.incarnate_intermission = "Knock Up"
-	--L.incarnate = "Fly Away"
-	--L.eternal_firestorm_shortened_bar = "Firestorm [E]" -- E for Eternal
-	--L.eternal_firestorm_message_full = "Firestorm [Eternal]"
+	L.fyralaths_bite = "Frontal"
+	L.fyralaths_mark = "Mal"
+	L.spirits_of_the_kaldorei = "Geister"
+	L.greater_firestorm_shortened_bar = "Feuersturm [G]" -- G for Greater
+	L.greater_firestorm_message_full = "Feuersturm [Groß]"
+	L.incarnate_intermission = "Zurückstoßen"
+	L.incarnate = "Abheben"
+	L.eternal_firestorm_shortened_bar = "Feuersturm [E]" -- E for Eternal
+	L.eternal_firestorm_message_full = "Feuersturm [Ewig]"
 end
