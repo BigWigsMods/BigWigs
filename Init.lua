f054ed937e1f7d14a0818595ba6505e2198b5452
funkydude <funkydude@users.noreply.github.com>
2023-11-30 23:13:50 +0000

    bump version

--- Init.lua
+++ Init.lua
@@ -2,7 +2,7 @@
 local _, tbl = ...
 tbl.isRetail = true
 tbl.dbmPrefix = "D5"
-tbl.dbmRevision = "20231121040240"
-tbl.dbmDisplayVersion = "10.2.7"
-tbl.dbmReleaseRevision = "20231120000000"
+tbl.dbmRevision = "20231128034103"
+tbl.dbmDisplayVersion = "10.2.8"
+tbl.dbmReleaseRevision = "20231127000000"
 tbl.dbmPForceDisable = 8
--- Loader.lua
+++ Loader.lua
@@ -12,7 +12,7 @@ local strfind = string.find
 -- Generate our version variables
 --
 
-local BIGWIGS_VERSION = 303
+local BIGWIGS_VERSION = 304
 local BIGWIGS_RELEASE_STRING, BIGWIGS_VERSION_STRING
 local versionQueryString, versionResponseString = "Q^%d^%s^%d^%s", "V^%d^%s^%d^%s"
 local customGuildName = false
