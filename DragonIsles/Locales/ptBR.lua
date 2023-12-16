d7409df10ae4d6777975a2aa2471cf485ff9aacf
igorautista <125813184+igorautista@users.noreply.github.com>
2023-12-15 21:02:24 -0300

    ptBR update for dragonisles locales

--- DragonIsles/Locales/ptBR.lua
+++ DragonIsles/Locales/ptBR.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Aurostor", "ptBR")
 if not L then return end
 if L then
-	--L.slumberous_roar = "3x %s - Jump to remove it"
+	L.slumberous_roar = "3x %s - Pule para remover"
 end
