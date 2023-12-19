f2027d0896b48efe5466f95c88e137838263ffbc
funkydude <funkydude@users.noreply.github.com>
2023-12-19 00:26:35 +0000

    bump version

--- Loader.lua
+++ Loader.lua
@@ -12,7 +12,7 @@ local strfind = string.find
 -- Generate our version variables
 --
 
-local BIGWIGS_VERSION = 308
+local BIGWIGS_VERSION = 309
 local BIGWIGS_RELEASE_STRING, BIGWIGS_VERSION_STRING
 local versionQueryString, versionResponseString = "Q^%d^%s^%d^%s", "V^%d^%s^%d^%s"
 local customGuildName = false
