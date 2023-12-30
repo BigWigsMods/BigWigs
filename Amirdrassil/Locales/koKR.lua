1efe6c202929f516905d9c6e600b1d53c5167e30
FlorineWhite <142205155+FlorineWhite@users.noreply.github.com>
2023-12-30 22:25:46 +0900

    Update koKR.lua in Amidrassil
    Where is L.molten_gauntlet used?
    I cannot find, so just made a direct translation.

--- Amirdrassil/Locales/koKR.lua
+++ Amirdrassil/Locales/koKR.lua
@@ -1,123 +1,120 @@
 local L = BigWigs:NewBossLocale("Gnarlroot", "koKR")
 if not L then return end
 if L then
-	--L.tortured_scream = "Scream"
+	L.tortured_scream = "비명"
 end
 
 L = BigWigs:NewBossLocale("Igira the Cruel", "koKR")
 if L then
-	--L.blistering_spear = "Spears"
-	--L.blistering_spear_single = "Spear"
-	--L.blistering_torment = "Chain"
-	--L.twisting_blade = "Blades"
-	--L.marked_for_torment = "Torment"
-	--L.umbral_destruction = "Soak"
-	--L.heart_stopper = "Heal Absorbs"
-	--L.heart_stopper_single = "Heal Absorb"
+	--L.blistering_spear = "창"
+	--L.blistering_spear_single = "창"
+	--L.blistering_torment = "사슬"
+	--L.twisting_blade = "칼날"
+	--L.marked_for_torment = "고문"
+	--L.umbral_destruction = "같이 맞기"
+	--L.heart_stopper = "치유 흡수"
+	--L.heart_stopper_single = "치유 흡수"
 end
 
 L = BigWigs:NewBossLocale("Volcoross", "koKR")
 if L then
-	--L.custom_off_all_scorchtail_crash = "Show All Casts"
-	--L.custom_off_all_scorchtail_crash_desc = "Show timers and messages for all Scorchtail Crash casts instead of just for your side."
+	--L.custom_off_all_scorchtail_crash = "모든 시전 보이기"
+	--L.custom_off_all_scorchtail_crash_desc = "당신의 방향만이 아닌 모든 방향에서의 불꽃꼬리 충돌의 타이머와 메시지를 표시합니다."
 
-	--L.flood_of_the_firelands = "Soaks"
-	--L.flood_of_the_firelands_single_wait = "Wait" -- Wait 3, Wait 2, Wait 1 countdown before soak debuff is applied
-	--L.flood_of_the_firelands_single = "Soak"
-	--L.scorchtail_crash = "Tail Slam"
-	--L.serpents_fury = "Flames"
-	--L.coiling_flames_single = "Flames"
+	L.flood_of_the_firelands = "같이 맞기"
+	--L.flood_of_the_firelands_single_wait = "대기" -- Wait 3, Wait 2, Wait 1 countdown before soak debuff is applied
+	--L.flood_of_the_firelands_single = "같이 맞기"
+	--L.scorchtail_crash = "꼬리치기"
+	--L.serpents_fury = "불꽃"
+	--L.coiling_flames_single = "불꽃"
 end
 
 L = BigWigs:NewBossLocale("Council of Dreams", "koKR")
 if L then
-	--L.agonizing_claws_debuff = "{421022} (Debuff)"
+	L.agonizing_claws_debuff = "{421022} (디버프)"
 
-	--L.custom_off_combined_full_energy = "Combined Full Energy Bars (Mythic only)"
-	--L.custom_off_combined_full_energy_desc = "Combine the bars of the abilities that the bosses use at full energy into one bar, only if they will be cast at the same time."
+	L.custom_off_combined_full_energy = "전체 기력 바 결합 (신화 난이도 전용)"
+	L.custom_off_combined_full_energy_desc = "우두머리가 최대 기력으로 사용하는 능력을 동시에 시전하는 경우에만 하나의 막대로 결합합니다."
 
-	--L.special_mechanic_bar = "%s [Ult] (%d)"
+	L.special_mechanic_bar = "%s [궁극기] (%d)"
 
-	--L.constricting_thicket = "Vines"
-	--L.poisonous_javelin = "Javelin"
-	--L.song_of_the_dragon = "Song"
-	--L.polymorph_bomb = "Ducks"
-	--L.polymorph_bomb_single = "Duck"
+	L.constricting_thicket = "덤불"
+	L.poisonous_javelin = "투창"
+	L.song_of_the_dragon = "노래"
+	L.polymorph_bomb = "오리"
+	L.polymorph_bomb_single = "오리"
 end
 
 L = BigWigs:NewBossLocale("Larodar, Keeper of the Flame", "koKR")
 if L then
-	--L.custom_on_repeating_yell_smoldering_suffocation = "Repeating Suffocation Health Yell"
-	--L.custom_on_repeating_yell_smoldering_suffocation_desc = "Repeating yell messages for Smoldering Suffocation to let others know when you are below 75% health."
+	L.custom_on_repeating_yell_smoldering_suffocation = "질식 시 체력 상황 외치기 반복"
+	L.custom_on_repeating_yell_smoldering_suffocation_desc = "이글거리는 질식으로 당신의 체력이 75% 밑으로 떨어질 경우, 외치기를 반복합니다."
 
-	--L.blazing_coalescence_on_player_note = "When it's on you"
-	--L.blazing_coalescence_on_boss_note = "When it's on the boss"
+	L.blazing_coalescence_on_player_note = "당신이 먹었을 때"
+	L.blazing_coalescence_on_boss_note = "우두머리가 먹었을 때"
 
-	--L.scorching_roots = "Roots"
-	--L.charred_brambles = "Roots Healable"
-	--L.blazing_thorns = "Spiral of Thorns"
-	--L.falling_embers = "Soaks"
-	--L.flash_fire = "Heal Absorbs"
-	--L.flash_fire_single = "Heal Absorb"
+	L.scorching_roots = "뿌리"
+	L.charred_brambles = "치유 가능한 뿌리"
+	L.blazing_thorns = "이글거리는 가시" -- Spiral of Thorn
+	L.falling_embers = "바닥 밟기"
+	L.flash_fire = "치유 흡수"
+	L.flash_fire_single = "치유 흡수"
 end
 
 L = BigWigs:NewBossLocale("Nymue, Weaver of the Cycle", "koKR")
 if L then
-	--L.mythic_add_death = "%s Killed"
+	L.mythic_add_death = "% 죽음"
 
-	--L.continuum = "New Lines"
-	--L.surging_growth = "New Soaks"
-	--L.ephemeral_flora = "Red Soak"
-	--L.viridian_rain = "Damage + Bombs"
-	--L.threads = "Threads" -- From the spell description of Impending Loom (429615) "threads of energy"
+	L.continuum = "선 새로 생김"
+	L.surging_growth = "새로운 바닥 밟기"
+	L.ephemeral_flora = "빨간 바닥 밟기"
+	L.viridian_rain = "데미지 + 폭탄"
+	L.threads = "베틀" -- From the spell description of Impending Loom (429615) "threads of energy"
 end
 
 L = BigWigs:NewBossLocale("Smolderon", "koKR")
 if L then
-	--L.brand_of_damnation = "Tank Soak"
-	--L.lava_geysers = "Geysers"
+	L.brand_of_damnation = "탱커랑 같이맞기"
+	L.lava_geysers = "용암 분출"
 end
 
 L = BigWigs:NewBossLocale("Tindral Sageswift, Seer of the Flame", "koKR")
 if L then
-	--L.seed_soaked = "Seed soaked"
-	--L.all_seeds_soaked = "Seeds done!"
-	--L.blazing_mushroom = "Mushrooms"
-	--L.fiery_growth = "Dispels"
-	--L.mass_entanglement = "Roots"
-	--L.incarnation_moonkin = "Moonkin Form"
-	--L.incarnation_tree_of_flame = "Tree Form"
-	--L.flaming_germination = "Seeds"
-	--L.suppressive_ember = "Heal Absorbs"
-	--L.suppressive_ember_single = "Heal Absorb"
-	--L.flare_bomb = "Feathers"
-	--L.too_close_to_edge = "Too close to the edge"
-	--L.taking_damage_from_edge = "Taking damage from the edge"
-	--L.flying_available = "You can fly now"
+	L.seed_soaked = "씨앗 밟기"
+	L.all_seeds_soaked = "씨앗 완료!"
+	L.blazing_mushroom = "버섯"
+	L.fiery_growth = "해제"
+	L.mass_entanglement = "휘감기"
+	L.incarnation_moonkin = "달빛야수 형태"
+	L.incarnation_tree_of_flame = "나무 형태"
+	L.flaming_germination = "씨앗"
+	L.suppressive_ember = "치유 흡수"
+	L.suppressive_ember_single = "치유 흡수"
+	L.flare_bomb = "깃털"
 end
 
 L = BigWigs:NewBossLocale("Fyrakk the Blazing", "koKR")
 if L then
 	L.spirits_trigger = "칼도레이의 영혼"
 
-	--L.fyralaths_bite = "Frontal"
-	--L.fyralaths_bite_mythic = "Frontals"
-	--L.fyralaths_mark = "Mark"
-	--L.darkflame_shades = "Shades"
-	--L.darkflame_cleave = "Mythic Soaks"
+	L.fyralaths_bite = "정면"
+	L.fyralaths_bite_mythic = "정면"
+	L.fyralaths_mark = "징표"
+	L.darkflame_shades = "망령"
+	L.darkflame_cleave = "같이 맞기"
 
-	--L.incarnate_intermission = "Knock Up"
+	L.incarnate_intermission = "공중에 뜸" -- 현신
 
-	--L.incarnate = "Fly Away"
-	--L.molten_gauntlet = "Gauntlet"
-	--L.mythic_debuffs = "Cages" -- Shadow Cage & Molten Eruption
+	L.incarnate = "현신"
+	L.molten_gauntlet = "건틀릿" -- never used?
+	L.mythic_debuffs = "우리" -- Shadow Cage & Molten Eruption
 
-	--L.greater_firestorm_shortened_bar = "Firestorm [G]" -- G for Greater
-	--L.greater_firestorm_message_full = "Firestorm [Greater]"
-	--L.eternal_firestorm_shortened_bar = "Firestorm [E]" -- E for Eternal
-	--L.eternal_firestorm_message_full = "Firestorm [Eternal]"
+	L.greater_firestorm_shortened_bar = "상급 화염폭풍" -- G for Greater
+	L.greater_firestorm_message_full = "상급 화염폭풍"
+	L.eternal_firestorm_shortened_bar = "영원한 화염폭풍" -- E for Eternal
+	L.eternal_firestorm_message_full = "영원한 화염폭풍"
 
-	--L.eternal_firestorm_swirl = "Eternal Firestorm Swirls"
-	--L.eternal_firestorm_swirl_desc = "Timers for Eternal Firestorm Swirls."
-	--L.eternal_firestorm_swirl_bartext = "Swirls"
+	L.eternal_firestorm_swirl = "영원한 화염폭풍 소용돌이"
+	L.eternal_firestorm_swirl_desc = "영원한 화염폭풍 소용돌이 타이머"
+	L.eternal_firestorm_swirl_bartext = "소용돌이"
 end
