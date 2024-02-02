662ca747ab436a4ec9fd7a2512db3884f50fa841
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2024-02-02 14:36:20 -0500

    Update common.esES.lua

--- Core/Locales/common.esES.lua
+++ Core/Locales/common.esES.lua
@@ -34,7 +34,7 @@ L.mythic = "Modo mítico"
 L.hard = "Modo difícil"
 L.active = "Activo" -- When a boss becomes active, after speech finishes
 L.general = "General" -- General settings, i.e. things that apply to normal, heroic and mythic mode.
---L.hp = "%d%% HP" -- "30% HP" (HP = Health Points)
+L.hp = "%d%% Salud" -- "30% HP" (HP = Health Points)
 L.door_open = "Puerta abierta" -- When a door is open, usually after a speech from an NPC
 L.gate_open = "Puerta abierta" -- When a gate is open, usually after a speech from an NPC
 
