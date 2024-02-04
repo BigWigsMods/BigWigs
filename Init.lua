56c92dae65df6523d2098ab5a13291c6a769fd40
funkydude <funkydude@users.noreply.github.com>
2024-02-04 03:06:05 +0000

    bump version

--- Init.lua
+++ Init.lua
@@ -2,7 +2,7 @@
 local _, tbl = ...
 tbl.isRetail = true
 tbl.dbmPrefix = "D5"
-tbl.dbmRevision = "20240201052448"
-tbl.dbmDisplayVersion = "10.2.20"
-tbl.dbmReleaseRevision = "20240131000000"
+tbl.dbmRevision = "20240203173223"
+tbl.dbmDisplayVersion = "10.2.21"
+tbl.dbmReleaseRevision = "20240203000000"
 tbl.dbmPForceDisable = 9
