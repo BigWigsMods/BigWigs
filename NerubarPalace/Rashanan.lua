658a21da0e8764296cf664782893cf9a8b02bfa8
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2024-06-14 21:51:12 +0200

    NerubarPalace/Rashanan: Early timer table and fix npc id

--- NerubarPalace/Rashanan.lua
+++ NerubarPalace/Rashanan.lua
@@ -6,7 +6,7 @@ if not BigWigsLoader.isBeta then return end -- Beta check
 
 local mod, CL = BigWigs:NewBoss("Rasha'nan", 2657, 2609)
 if not mod then return end
-mod:RegisterEnableMob(224552) -- Rasha'nan XXX Confirm on PTR
+mod:RegisterEnableMob(214504) -- Rasha'nan
 mod:SetEncounterID(2918)
 mod:SetRespawnTime(30)
 
@@ -14,6 +14,19 @@ mod:SetRespawnTime(30)
 -- Locals
 --
 
+local infestedSpawnCount = 1
+local rollingAcidCount = 1
+local erosiveSprayCount = 1
+local spinneretsStrandsCount = 1
+local webReaveCount = 1
+
+local timers = { -- Heroic PTR
+	[455373] = {59.1, 84.8, 76.8, 29.8, 20.2, 78.9, 123.2, 57.4, 29.8, 20.2}, -- Infested Spawn
+	[439789] = {41.5, 78.7, 30.3, 19.7, 96.5, 103.6, 59.5, 29.8, 20.2, 77.1}, -- Rolling Acid
+	[439811] = {3.1, 29.7, 44.4, 59.5, 44.4, 57.7, 44.4, 59.1, 44.4, 59.6, 44.5, 57.4, 44.4}, -- Erosive Spray
+	[439784] = {14.2, 29.8, 20.2, 98.4, 83.0, 78.3, 30.3, 19.7, 79.3, 121.1}, -- Spinneret's Strands
+	[439795] = {107.0, 102.1, 103.6, 104.0, 101.9}, -- Web Reave
+}
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -36,7 +49,7 @@ function mod:GetOptions()
 		439787, -- Acidic Stupor
 		439785, -- Corrosion
 		439776, -- Acid Pool
-		{439815, "PRIVATE"}, -- Infested Spawn
+		{455373, "PRIVATE"}, -- Infested Spawn
 		455287, -- Infested Bite
 		{439784, "PRIVATE"}, -- Spinneret's Strands
 		--439780, -- Sticky Webs XXX gtfo?
@@ -57,7 +70,7 @@ function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "AcidicStuporApplied", 439787)
 	self:Log("SPELL_AURA_APPLIED", "CorrosionApplied", 439785)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "CorrosionApplied", 439785)
-	self:Log("SPELL_CAST_SUCCESS", "InfestedSpawn", 439815)
+	self:Log("SPELL_CAST_START", "InfestedSpawn", 455373)
 	self:Log("SPELL_AURA_APPLIED", "InfestedBiteApplied", 455287)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "InfestedBiteApplied", 455287)
 	self:Log("SPELL_CAST_START", "SpinneretsStrands", 439784)
@@ -74,7 +87,18 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
-	-- self:Bar(444687, 6) -- Savage Assault
+	infestedSpawnCount = 1
+	rollingAcidCount = 1
+	erosiveSprayCount = 1
+	spinneretsStrandsCount = 1
+	webReaveCount = 1
+
+	self:CDBar(455373, timers[455373][infestedSpawnCount], CL.count:format(self:SpellName(455373), infestedSpawnCount)) -- Infested Spawn
+	self:CDBar(439789, timers[439789][rollingAcidCount], CL.count:format(self:SpellName(439789), rollingAcidCount)) -- Rolling Acid
+	self:CDBar(439811, timers[439811][erosiveSprayCount], CL.count:format(self:SpellName(439811), erosiveSprayCount)) -- Erosive Spray
+	self:CDBar(439784, timers[439784][spinneretsStrandsCount], CL.count:format(self:SpellName(439784), spinneretsStrandsCount)) -- Spinneret's Strands
+	self:CDBar(439795, timers[439795][webReaveCount], CL.count:format(self:SpellName(439795), webReaveCount)) -- Web Reave
+
 
 	self:SetPrivateAuraSound(439815) --  Infested Spawn
 	self:SetPrivateAuraSound(439784, 439783) --  Spinneret's Strands
@@ -100,9 +124,11 @@ end
 
 
 function mod:RollingAcid(args)
+	self:StopBar(CL.count:format(args.spellName, rollingAcidCount))
 	self:Message(args.spellId, "yellow", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "alert")
-	-- self:Bar(args.spellId, 10)
+	rollingAcidCount = rollingAcidCount + 1
+	self:CDBar(args.spellId, timers[args.spellId][rollingAcidCount], CL.count:format(args.spellName, rollingAcidCount))
 end
 
 function mod:AcidicStuporApplied(args)
@@ -123,9 +149,11 @@ function mod:CorrosionApplied(args)
 end
 
 function mod:InfestedSpawn(args)
+	self:StopBar(CL.count:format(args.spellName, infestedSpawnCount))
 	self:Message(args.spellId, "yellow", CL.incoming:format(args.spellName))
 	self:PlaySound(args.spellId, "alert")
-	-- self:Bar(args.spellId, 10)
+	infestedSpawnCount = infestedSpawnCount + 1
+	self:CDBar(args.spellId, timers[args.spellId][infestedSpawnCount], CL.count:format(args.spellName, infestedSpawnCount))
 end
 
 function mod:InfestedBiteApplied(args)
@@ -139,22 +167,27 @@ function mod:InfestedBiteApplied(args)
 end
 
 function mod:SpinneretsStrands(args)
+	self:StopBar(CL.count:format(args.spellName, spinneretsStrandsCount))
 	self:Message(args.spellId, "orange")
 	self:PlaySound(args.spellId, "alert")
-	-- self:Bar(args.spellId, 10)
+	spinneretsStrandsCount = spinneretsStrandsCount + 1
+	self:CDBar(args.spellId, timers[args.spellId][spinneretsStrandsCount], CL.count:format(args.spellName, spinneretsStrandsCount))
 end
 
-
 function mod:WebReave(args)
+	self:StopBar(CL.count:format(args.spellName, webReaveCount))
 	self:Message(args.spellId, "red", CL.casting:format(args.spellName))
 	self:PlaySound(args.spellId, "long")
-	-- self:Bar(args.spellId, 10)
+	webReaveCount = webReaveCount + 1
+	self:CDBar(args.spellId, timers[args.spellId][webReaveCount], CL.count:format(args.spellName, webReaveCount))
 end
 
 function mod:ErosiveSpray(args)
+	self:StopBar(CL.count:format(args.spellName, erosiveSprayCount))
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "alert")
-	-- self:Bar(args.spellId, 10)
+	erosiveSprayCount = erosiveSprayCount + 1
+	self:CDBar(args.spellId, timers[args.spellId][erosiveSprayCount], CL.count:format(args.spellName, erosiveSprayCount))
 end
 
 function mod:AcidicEruption(args)
