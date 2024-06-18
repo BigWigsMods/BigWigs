c13f15799c5288f51815866cc5511fa07320d54e
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-06-18 08:45:41 +0800

    Update zhCN.lua

--- NerubarPalace/Locales/zhCN.lua
+++ NerubarPalace/Locales/zhCN.lua
@@ -1,16 +1,16 @@
 local L = BigWigs:NewBossLocale("Ulgrax the Devourer", "zhCN")
 if not L then return end
 if L then
-	--L.chunky_viscera_debuff_msg = "Remember to Feed! (Special Action Button)"
+	L.chunky_viscera_debuff_msg = "记得喂食！（额外快捷键）"
 end
 
 L = BigWigs:NewBossLocale("The Bloodbound Horror", "zhCN")
 if L then
-    --L.grasp_from_beyond_say = "Tentacles"
+    L.grasp_from_beyond_say = "触手"
 end
 
 L = BigWigs:NewBossLocale("Eggtender Ovi'nax", "zhCN")
 if L then
-    --L.unstable_web_say = "Web"
-    --L.casting_infest_on_you = "Casting Infest on YOU!"
+    L.unstable_web_say = "网"
+    L.casting_infest_on_you = "对你施放感染！"
 end
