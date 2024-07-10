fcdf5ac7f7c4e6eb83192035e1cd96e278cf963b
Hugo <37742421+Alyndel@users.noreply.github.com>
2024-07-10 21:31:04 +0200

    Update frFR.lua

--- DragonIsles/Locales/frFR.lua
+++ DragonIsles/Locales/frFR.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Aurostor", "frFR")
 if not L then return end
 if L then
-	--L.slumberous_roar = "3x %s - Jump to remove it"
+	L.slumberous_roar = "3x %s - Sautez pour enlever"
 end
