4f931955312ca4d9b5c241a8274e6755a69b3305
EK <eke00372@gmail.com>
2024-07-14 17:27:51 +0800

    Update common.zhTW.lua

--- Core/Locales/common.zhTW.lua
+++ Core/Locales/common.zhTW.lua
@@ -66,7 +66,7 @@ L.cancelled = "%s取消" -- Spell Cancelled
 L.you_die = "你將死亡" -- You will die
 L.you_die_sec = "你將在%d秒後死亡" -- "You die in 15 sec" (sec = seconds)
 L.next_ability = "下個技能" -- We don't know what ability will be next, we only know when it will happen (showing a bar)
---L.landing = "%s is landing" -- "NPC_NAME is landing" Used when a flying NPC/dragon/boss is landing
+L.landing = "%s即將著陸" -- "NPC_NAME is landing" Used when a flying NPC/dragon/boss is landing 即將著陸、正在降落
 
 -- Add related
 L.add_spawned = "小怪出現" -- singular
