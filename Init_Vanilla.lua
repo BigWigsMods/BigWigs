2158a55bb4a2e0654c3cb60364fe83ef744acbf1
funkydude <funkydude@users.noreply.github.com>
2024-07-27 02:05:54 +0100

    Fix loading Season of Discovery content on Hardcore Classic Era

--- Init_Vanilla.lua
+++ Init_Vanilla.lua
@@ -2,4 +2,4 @@
 local _, tbl = ...
 tbl.isClassic = true
 tbl.isVanilla = true
-tbl.isSeasonOfDiscovery = C_Seasons.GetActiveSeason() and true
+tbl.isSeasonOfDiscovery = C_Seasons.GetActiveSeason() == 2
