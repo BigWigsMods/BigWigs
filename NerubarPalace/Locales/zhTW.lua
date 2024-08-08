71c54b6c64606e80935dcdd85270a51b740e4ec4
EK <eke00372@gmail.com>
2024-08-08 12:37:37 +0800

    Update zhTW.lua

--- NerubarPalace/Locales/zhTW.lua
+++ NerubarPalace/Locales/zhTW.lua
@@ -15,28 +15,28 @@ end
 
 L = BigWigs:NewBossLocale("Rasha'nan", "zhTW")
 if L then
-	--L.rolling_acid = "Waves"
-	--L.spinnerets_strands = "Strands"
-	--L.enveloping_webs = "Webs"
-	--L.enveloping_web_say = "Web" -- Singular of Webs
-	--L.erosive_spray = "Spray"
-	--L.caustic_hail = "Next Position"
+	L.rolling_acid = "波浪"
+	L.spinnerets_strands = "絲線" -- 絲囊?
+	L.enveloping_webs = "蛛網"
+	L.enveloping_web_say = "蛛網" -- Singular of Webs
+	L.erosive_spray = "酸液"
+	L.caustic_hail = "下個位置"
 end
 
 L = BigWigs:NewBossLocale("Broodtwister Ovi'nax", "zhTW")
 if L then
 	L.sticky_web_say = "蛛網"
 	L.infest_message = "正在對你施放寄生！"
-	--L.infest_say = "Parasites"
+	L.infest_say = "寄生"
 	--L.experimental_dosage_say = "Soak Egg"
 	--L.unstable_infusion = "Swirls"
-	--L.custom_on_experimental_dosage_marks = "Experimental Dosage assignments"
-	--L.custom_on_experimental_dosage_marks_desc = "Assign players affected by 'Experimental Dosage' to {rt6}{rt4}{rt3}{rt7} with a melee > ranged > healer priority. Affects Say and Target messages."
+	L.custom_on_experimental_dosage_marks = "實驗療法分配"
+	L.custom_on_experimental_dosage_marks_desc = "將受到「實驗療法」影響的玩家，按照近戰 > 遠程 > 治療的優先級，標記為 {rt6}{rt4}{rt3}{rt7}，包含喊話與目標訊息。"
 end
 
 L = BigWigs:NewBossLocale("Nexus-Princess Ky'veza", "zhTW")
 if L then
-	--L.assasination = "Phantoms"
-	--L.twiligt_massacre = "Dashes"
-	--L.nexus_daggers = "Daggers"
+	L.assasination = "幻影"
+	L.twiligt_massacre = "衝鋒"
+	L.nexus_daggers = "匕首"
 end
