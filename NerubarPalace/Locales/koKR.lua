4553b92e6a3ba27a3e8ecfa2a4d83328eb0ffdf3
007bbb <86834094+007bbb@users.noreply.github.com>
2024-08-21 10:45:45 +0900

    Update koKR.lua

--- NerubarPalace/Locales/koKR.lua
+++ NerubarPalace/Locales/koKR.lua
@@ -25,13 +25,13 @@ end
 
 L = BigWigs:NewBossLocale("Broodtwister Ovi'nax", "koKR")
 if L then
-	--L.sticky_web = "Webs"
+	L.sticky_web = "거미줄"
 	L.sticky_web_say = "거미줄" -- Singular of Webs
 	L.infest_message = "당신에게 감연 시전 중!"
 	L.infest_say = "기생충"
-	--L.experimental_dosage = "Egg Breaks"
-	--L.experimental_dosage_say = "Egg Break"
-	--L.ingest_black_blood = "Next Container"
+	L.experimental_dosage = "알 깨기"
+	L.experimental_dosage_say = "알 깨기"
+	L.ingest_black_blood = "다음 용기"
 	L.unstable_infusion = "이감 바닥"
 
 	L.custom_on_experimental_dosage_marks = "실험용 투여제 할당"
@@ -47,15 +47,15 @@ end
 
 L = BigWigs:NewBossLocale("The Silken Court", "koKR")
 if L then
-	--L.skipped_cast = "Skipped %s (%d)"
+	L.skipped_cast = "건너뛴 %s (%d)"
 
-	--L.venomous_rain = "Rain"
-	--L.burrowed_eruption = "Burrow"
-	--L.stinging_swarm = "Dispel Debuffs"
-	--L.strands_of_reality = "Frontal [S]" -- S for Skeinspinner Takazj
-	--L.impaling_eruption = "Frontal [A]" -- A for Anub'arash
-	--L.entropic_desolation = "Run Out"
-	--L.cataclysmic_entropy = "Big Boom" -- Interrupt before it casts
-	--L.spike_eruption = "Spikes"
-	--L.unleashed_swarm = "Swarm"
+	L.venomous_rain = "독비"
+	L.burrowed_eruption = "잠복"
+	L.stinging_swarm = "디버프 해제"
+	L.strands_of_reality = "전방 [S]" -- S for Skeinspinner Takazj
+	L.impaling_eruption = "전방 [A]" -- A for Anub'arash
+	L.entropic_desolation = "밖으로 도망"
+	L.cataclysmic_entropy = "큰 폭발" -- Interrupt before it casts
+	L.spike_eruption = "가시"
+	L.unleashed_swarm = "무리"
 end
