20eec61d9c698b738d9b5002604ad3deb00150a6
funkydude <funkydude@users.noreply.github.com>
2024-08-31 22:17:14 +0100

    Core/BossPrototype: Update `GetUnitTarget` documentation

--- Core/BossPrototype.lua
+++ Core/BossPrototype.lua
@@ -1158,7 +1158,7 @@ do
 	--- Register a callback to get the first non-tank target of a mob.
 	-- Looks for the unit as defined by the GUID and then returns the target of that unit.
 	-- If the target is a tank, it will keep looking until the designated time has elapsed.
-	-- @param func callback function, passed (module, playerName, playerGUID)
+	-- @param func callback function, passed (module, playerName, playerGUID, elapsed)
 	-- @number tankCheckExpiry seconds to wait, if a tank is still the target after this time, it will return the tank as the target (max 0.8)
 	-- @string guid GUID of the mob to get the target of
 	function boss:GetUnitTarget(func, tankCheckExpiry, guid)
