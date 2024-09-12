3aa817c2bfafde36debaafb4265e771c6efa86b3
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2024-09-13 01:05:33 +0200

    Update deDE (#1814)

--- KhazAlgar/Locales/deDE.lua
+++ KhazAlgar/Locales/deDE.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Aggregation of Horrors", "deDE")
 if not L then return end
 if L then
-	--L.void_rocks = "Void Rocks" -- Plural of Void Rock (452379)
+	L.void_rocks = "Leerenfelsen" -- Plural of Void Rock (452379)
 end
