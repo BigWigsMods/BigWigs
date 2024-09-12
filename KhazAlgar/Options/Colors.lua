3d1d999a2d6afdc79249ab9646aea904c1b1727a
funkydude <funkydude@users.noreply.github.com>
2024-09-12 21:58:33 +0100

    KhazAlgar: Update option files

--- KhazAlgar/Options/Colors.lua
+++ KhazAlgar/Options/Colors.lua
@@ -1,4 +1,7 @@
 
 BigWigs:AddColors("Aggregation of Horrors", {
-
+	[452205] = "yellow",
+	[452980] = "orange",
+	[453271] = "red",
+	[453294] = "purple",
 })
--- KhazAlgar/Options/Sounds.lua
+++ KhazAlgar/Options/Sounds.lua
@@ -1,4 +1,7 @@
 
 BigWigs:AddSounds("Aggregation of Horrors", {
-
+	[452205] = "long",
+	[452980] = "info",
+	[453271] = "warning",
+	[453294] = "alarm",
 })
