4a2402d61b5f87a9fa6e581960e32510f54333c9
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-09-20 08:08:19 +0800

    Update common.zhCN.lua

--- Core/Locales/common.zhCN.lua
+++ Core/Locales/common.zhCN.lua
@@ -232,4 +232,4 @@ L.health_drain = "消耗生命" -- Any ability that drains health from the playe
 L.smash = "重击" -- Short for any ability with the name "smash" in it e.g. "Darkrift Smash" or "Seismic Smash" or "Arcing Smash"
 L.spike = "尖刺" -- Short for any ability with the name "spike" in it e.g. "Glacial Spike" or "Fel Spike" or "Volatile Spike"
 L.spikes = "尖刺" -- Plural of L.spike
---L.waves = "Waves" -- Multiple waves of a bad ability coming from a boss, like waves in the ocean
+L.waves = "波浪" -- Multiple waves of a bad ability coming from a boss, like waves in the ocean
