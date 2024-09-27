a87be70bb0bc91af4d3b8de15187c224bb960e27
Xilver <xilver266@gmail.com>
2024-09-27 20:15:56 +0200

    Update esES (#1856)

--- NerubarPalace/Locales/esES.lua
+++ NerubarPalace/Locales/esES.lua
@@ -1,84 +1,84 @@
 local L = BigWigs:NewBossLocale("Ulgrax the Devourer", "esES")
 if not L then return end
 if L then
-	--L.carnivorous_contest_pull = "Pull In"
-	--L.chunky_viscera_message = "Feed Boss! (Special Action Button)"
+	L.carnivorous_contest_pull = "Atraer"
+	L.chunky_viscera_message = "¡Aliméntalo! (Special Action Button)"
 end
 
 L = BigWigs:NewBossLocale("The Bloodbound Horror", "esES")
 if L then
-	--L.gruesome_disgorge_debuff = "Phase Shift"
-	--L.grasp_from_beyond = "Tentacles"
-	--L.grasp_from_beyond_say = "Tentacles"
-	--L.bloodcurdle = "Spreads"
-	--L.bloodcurdle_on_you = "Spread" -- Singular of Spread
-	--L.goresplatter = "Run Away"
+	L.gruesome_disgorge_debuff = "Cambio de dimensión"
+	L.grasp_from_beyond = "Tentáculos"
+	L.grasp_from_beyond_say = "Tentáculos"
+	L.bloodcurdle = "Propagación"
+	L.bloodcurdle_on_you = "Sepárate" -- Singular of Spread
+	L.goresplatter = "Huye"
 end
 
 L = BigWigs:NewBossLocale("Rasha'nan", "esES")
 if L then
-	--L.spinnerets_strands = "Strands"
-	--L.enveloping_webs = "Webs"
-	--L.enveloping_web_say = "Web" -- Singular of Webs
-	--L.erosive_spray = "Spray"
-	--L.caustic_hail = "Next Position"
+	L.spinnerets_strands = "Ataduras"
+	L.enveloping_webs = "Telarañas"
+	L.enveloping_web_say = "Telaraña" -- Singular of Webs
+	L.erosive_spray = "Escupir"
+	L.caustic_hail = "Siguiente posición"
 end
 
 L = BigWigs:NewBossLocale("Broodtwister Ovi'nax", "esES")
 if L then
-	--L.sticky_web = "Webs"
-	--L.sticky_web_say = "Web" -- Singular of Webs
-	--L.infest_message = "Casting Infest on YOU!"
-	--L.infest_say = "Parasites"
-	--L.experimental_dosage = "Egg Breaks"
-	--L.experimental_dosage_say = "Egg Break"
-	--L.ingest_black_blood = "Next Container"
-	--L.unstable_infusion = "Swirls"
+	L.sticky_web = "Telarañas"
+	L.sticky_web_say = "Telaraña" -- Singular of Webs
+	L.infest_message = "¡Lanzando Infestar en ti!"
+	L.infest_say = "Parásitos"
+	L.experimental_dosage = "Huevo abriéndose"
+	L.experimental_dosage_say = "Apertura de huevo"
+	L.ingest_black_blood = "Siguiente bote"
+	L.unstable_infusion = "Remolinos"
 
-	--L.custom_on_experimental_dosage_marks = "Experimental Dosage assignments"
-	--L.custom_on_experimental_dosage_marks_desc = "Assign players affected by 'Experimental Dosage' to {rt6}{rt4}{rt3}{rt7} with a melee > ranged > healer priority. Affects Say and Target messages."
+	L.custom_on_experimental_dosage_marks = "Asignaciones de Dosis experimentales"
+	L.custom_on_experimental_dosage_marks_desc = "Asigna a las personas afectadas por Dosis experimental a {rt6}{rt4}{rt3}{rt7} con prioridad: melee > ranged > healer. Afecta a los mensajes enviados."
 end
 
 L = BigWigs:NewBossLocale("Nexus-Princess Ky'veza", "esES")
 if L then
-	--L.assasination = "Phantoms"
-	--L.twiligt_massacre = "Dashes"
-	--L.nexus_daggers = "Daggers"
+	L.assasination = "Fantasmas"
+	L.twiligt_massacre = "Arremetidas"
+	L.nexus_daggers = "Dagas"
 end
 
 L = BigWigs:NewBossLocale("The Silken Court", "esES")
 if L then
-	--L.skipped_cast = "Skipped %s (%d)"
-	--L.intermission_trigger = "Apex of power!" -- Skeinspinner Takazj 100 energy yell
+	L.skipped_cast = "Omitido %s (%d)"
+	L.intermission_trigger = "¡Límite de poder!" -- Skeinspinner Takazj 100 energy yell
 
-	--L.venomous_rain = "Rain"
-	--L.burrowed_eruption = "Burrow"
-	--L.stinging_swarm = "Dispel Debuffs"
-	--L.strands_of_reality = "Frontal [S]" -- S for Skeinspinner Takazj
-	--L.strands_of_reality_message = "Frontal [Skeinspinner Takazj]"
-	--L.impaling_eruption = "Frontal [A]" -- A for Anub'arash
-	--L.impaling_eruption_message = "Frontal [Anub'arash]"
-	--L.entropic_desolation = "Run Out"
-	--L.cataclysmic_entropy = "Big Boom" -- Interrupt before it casts
-	--L.spike_eruption = "Spikes"
-	--L.unleashed_swarm = "Swarm"
-	--L.void_degeneration = "Blue Orb"
-	--L.burning_rage = "Red Orb"
+	L.venomous_rain = "Lluvia"
+	L.burrowed_eruption = "Erupción"
+	L.stinging_swarm = "Disipa Debuffs"
+	L.strands_of_reality = "Frontal [T]" -- S for Skeinspinner Takazj
+	L.strands_of_reality_message = "Frontal [Takazj Giramadeja]"
+	L.impaling_eruption = "Frontal [A]" -- A for Anub'arash
+	L.impaling_eruption_message = "Frontal [Anub'arash]"
+	L.entropic_desolation = "Escapa"
+	L.cataclysmic_entropy = "Estallido" -- Interrupt before it casts
+	L.spike_eruption = "Púas"
+	L.unleashed_swarm = "Enjambre"
+	L.void_degeneration = "Orbe azul"
+	L.burning_rage = "Orbe rojo"
 end
 
 L = BigWigs:NewBossLocale("Queen Ansurek", "esES")
 if L then
-	--L.stacks_onboss = "%dx %s on BOSS"
+	L.stacks_onboss = "%dx %s on BOSS"
 
-	--L.reactive_toxin = "Toxins"
-	--L.reactive_toxin_say = "Toxin"
-	--L.venom_nova = "Nova"
-	--L.web_blades = "Blades"
-	--L.silken_tomb = "Roots" -- Raid being rooted in place
-	--L.wrest = "Pull In"
-	--L.slow = "Slow"
-	--L.royal_condemnation = "Shackles"
-	--L.frothing_gluttony = "Ring"
+	L.reactive_toxin = "Toxinas"
+	L.reactive_toxin_say = "Toxina"
+	L.venom_nova = "Nova"
+	L.web_blades = "Hojas"
+	L.silken_tomb = "Enraizar" -- Raid being rooted in place
+	L.wrest = "Atraer"
+	L.slow = "Ralentización"
+	L.royal_condemnation = "Cadenas"
+	L.frothing_gluttony = "Anillo"
 
-	--L.stage_two_end_message_storymode = "Run into the portal"
+	L.stage_two_end_message_storymode = "Corre hacia el portal"
 end
