48e359475c979720eeb38e0f4b63f6e9a872d193
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2025-01-16 00:02:26 +0100

    LiberationOfUndermine/Stix: Heroic Updates

--- LiberationOfUndermine/Stix.lua
+++ LiberationOfUndermine/Stix.lua
@@ -5,21 +5,29 @@ if not BigWigsLoader.isTestBuild then return end
 
 local mod, CL = BigWigs:NewBoss("Stix Bunkjunker", 2769, 2642)
 if not mod then return end
-mod:RegisterEnableMob(230322) -- Stix Bunkjunker XXX Confirm on PTR
+mod:RegisterEnableMob(230322) -- Stix Bunkjunker
 mod:SetEncounterID(3012)
 mod:SetRespawnTime(30)
+mod:SetStage(1)
 
 --------------------------------------------------------------------------------
 -- Locals
 --
 
+local electromagneticSortingCount = 1
+local incineratorCount = 1
+local demolishCount = 1
+local meltdownCount = 1
+
 --------------------------------------------------------------------------------
 -- Localization
 --
 
--- local L = mod:GetLocale()
--- if L then
--- end
+local L = mod:GetLocale()
+if L then
+	L.rolled_over_by = "Rolled over by %s" -- Rolled over by PlayerX
+	L.landing = "Landing"
+end
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -28,57 +36,49 @@ mod:SetRespawnTime(30)
 function mod:GetOptions()
 	return {
 		464399, -- Electromagnetic Sorting
-			461536, -- Rolling Rubbish
-				-- 465741, -- Garbage Dump XXX Can we warn how much health someone removed? for fun.
-				-- 1217685, -- Messed Up
-				-- 465611, -- Rolled! XXX Warn who rolled over you?
+			461536, -- Rolling Rubbish -- XXX Widget messages for size of ball?
+				465611, -- Rolled!
 			464854, -- Garbage Pile
-			-- 464865, -- Discarded Doomsplosive XXX Warn when spawned, and when absorbed?
 				1217975, -- Doomsploded
-				-- 465747, -- Muffled Doomsplosion
-			-- Territorial Bombshell XXX Warn how many are up and left to destroy/kill?
-		466849, -- Cleanup Crew XXX Fix tooltip if used?
+			-- Territorial Bombshell XXX Marking them? can use CLEU order.
 			-- Scrapmaster
 			1219384, -- Scrap Rockets
-			-- 466742, -- Dumpster Dive
 			-- Junkyard Hyena
 			466748, -- Infected Bite
 		464149, -- Incinerator
-			{472893, "SAY"}, -- Incineration
+			472893, -- Incineration
 			464248, -- Hot Garbage
-				1218343, -- Toxic Fumes
-		464112, -- Demolish
-		-- 1217954, -- Meltdown XXX is this an cooldown ability or it's auto attack?
+		{464112, "TANK"}, -- Demolish
+		{1217954, "TANK"}, -- Meltdown
 		467117, -- Overdrive
-			-- 467149, -- Overcharged Bolt
-			467135, -- Trash Compactor
-			-- 473227, -- Maximum Output
+			467109, -- Trash Compactor
 	},{ -- Sections
 
 	},{ -- Renames
-
+		[467109] = L.landing, -- Trash Compactor (Landing)
 	}
 end
 
 function mod:OnRegister()
-	--self:SetSpellRename(999999, CL.renameMe) -- Spell (Rename)
+	self:SetSpellRename(467109, L.landing) -- Trash Compactor (Landing)
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_START", "ElectromagneticSorting", 464399)
-	self:Log("SPELL_AURA_APPLIED", "RollingRubbish", 461536)
+	self:Log("SPELL_AURA_APPLIED", "SortedApplied", 465346) -- These players will become Rolling Rubbish -- XXX Mark
+	-- self:Log("SPELL_AURA_APPLIED", "RollingRubbishRemoved", 461536) -- XXX Unmark
+	self:Log("SPELL_AURA_APPLIED", "RolledApplied", 465611)
 	self:Log("SPELL_AURA_APPLIED", "Doomsploded", 1217975)
-	self:Log("SPELL_CAST_SUCCESS", "CleanupCrew", 466849)
 	self:Log("SPELL_CAST_SUCCESS", "ScrapRockets", 1219384)
 	self:Log("SPELL_AURA_APPLIED", "InfectedBiteApplied", 466748)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "InfectedBiteApplied", 466748)
 	self:Log("SPELL_CAST_SUCCESS", "Incinerator", 464149)
 	self:Log("SPELL_AURA_APPLIED", "IncinerationApplied", 472893)
-	self:Log("SPELL_AURA_APPLIED_DOSE", "ToxicFumesApplied", 1218343)
 	self:Log("SPELL_CAST_START", "Demolish", 464112)
 	self:Log("SPELL_AURA_APPLIED", "DemolishApplied", 464112)
+	self:Log("SPELL_CAST_START", "Meltdown", 1217954)
 	self:Log("SPELL_CAST_START", "Overdrive", 467117)
-	self:Log("SPELL_CAST_SUCCESS", "TrashCompactor", 467135)
+	self:Log("SPELL_CAST_START", "TrashCompactor", 467109)
 
 	self:Log("SPELL_AURA_APPLIED", "GroundDamage", 464854, 464248) -- Garbage Pile, Hot Garbage
 	self:Log("SPELL_PERIODIC_DAMAGE", "GroundDamage", 464854, 464248)
@@ -86,10 +86,17 @@ function mod:OnBossEnable()
 end
 
 function mod:OnEngage()
-	-- self:Bar(464399, 10) -- Electromagnetic Sorting
-	-- self:Bar(466849, 20) -- Cleanup Crew
-	-- self:Bar(464149, 30) -- Incinerator
-	-- self:Bar(464112, 40) -- Demolish
+	self:SetStage(1)
+
+	electromagneticSortingCount = 1
+	incineratorCount = 1
+	demolishCount = 1
+	meltdownCount = 1
+
+	self:Bar(464149, 11.1, CL.count:format(self:SpellName(464149), incineratorCount)) -- Incinerator
+	self:Bar(464112, 17.7, CL.count:format(self:SpellName(464112), demolishCount)) -- Demolish
+	self:Bar(464399, 22.2, CL.count:format(self:SpellName(464399), electromagneticSortingCount)) -- Electromagnetic Sorting
+	self:Bar(1217954, 44.5, CL.count:format(self:SpellName(1217954), meltdownCount)) -- Meltdown
 end
 
 --------------------------------------------------------------------------------
@@ -97,15 +104,23 @@ end
 --
 
 function mod:ElectromagneticSorting(args)
-	self:Message(args.spellId, "orange")
+	self:Message(args.spellId, "orange", CL.count:format(args.spellName, electromagneticSortingCount))
 	self:PlaySound(args.spellId, "long") -- damage and garbage over 5 seconds
-	-- self:Bar(args.spellId, 10)
+	electromagneticSortingCount = electromagneticSortingCount + 1
+	self:Bar(args.spellId, electromagneticSortingCount == 3 and 72.2 or 51.1, CL.count:format(args.spellName, electromagneticSortingCount))
 end
 
-function mod:RollingRubbish(args)
+function mod:SortedApplied(args)
 	if self:Me(args.destGUID) then
-		self:PersonalMessage(args.spellId)
-		self:PlaySound(args.spellId, "info") -- you're rubbish
+		self:PersonalMessage(461536)
+		self:PlaySound(461536, "warning") -- you're becoming rubbish
+	end
+end
+
+function mod:RolledApplied(args)
+	if self:Me(args.destGUID) then
+		self:Message(args.spellId, "cyan", L.rolled_over_by:format(self:ColorName(args.sourceName)))
+		self:PlaySound(args.spellId, "alarm") -- stunned
 	end
 end
 
@@ -118,12 +133,6 @@ do
 			self:PlaySound(args.spellId, "warning")
 		end
 	end
-end
-
-function mod:CleanupCrew(args)
-	self:Message(args.spellId, "cyan")
-	self:PlaySound(args.spellId, "info") -- adds inc
-	-- self:Bar(args.spellId, 10)
 end
 
 function mod:ScrapRockets(args)
@@ -145,35 +154,24 @@ function mod:InfectedBiteApplied(args)
 end
 
 function mod:Incinerator(args)
-	self:Message(args.spellId, "yellow")
-	self:PlaySound(args.spellId, "alert") -- debuffs inc
-	-- self:Bar(args.spellId, 10)
+	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, incineratorCount))
+	self:PlaySound(args.spellId, "alert") -- debuffs
+	incineratorCount = incineratorCount + 1
+	self:Bar(args.spellId, incineratorCount == 5 and 46.7 or 25.6, CL.count:format(args.spellName, incineratorCount))
 end
 
 function mod:IncinerationApplied(args)
 	if self:Me(args.destGUID) then
 		self:PersonalMessage(args.spellId)
 		self:PlaySound(args.spellId, "alarm") -- watch surrouding
-		self:Say(args.spellId, nil, nil, "Incineration")
-	end
-end
-
-function mod:ToxicFumesApplied(args)
-	if self:Me(args.destGUID) then
-		local amount = args.amount or 1
-		if amount % 2 == 1 then
-			self:StackMessage(args.spellId, "blue", args.destName, amount, 6)
-			if amount > 5 then
-				self:PlaySound(args.spellId, "alarm")
-			end
-		end
 	end
 end
 
 function mod:Demolish(args)
-	self:Message(args.spellId, "purple")
+	self:Message(args.spellId, "purple", CL.count:format(args.spellName, demolishCount))
 	self:PlaySound(args.spellId, "info")
-	-- self:Bar(args.spellId, 25)
+	demolishCount = demolishCount + 1
+	self:Bar(args.spellId, demolishCount == 3 and 72.2 or 51.5, CL.count:format(args.spellName, demolishCount)) -- Delayed once due to overdrive?
 end
 
 function mod:DemolishApplied(args)
@@ -183,15 +181,24 @@ function mod:DemolishApplied(args)
 	end
 end
 
+function mod:Meltdown(args)
+	self:Message(args.spellId, "purple", CL.count:format(args.spellName, meltdownCount))
+	self:PlaySound(args.spellId, "info") -- XXX change to play for current target?
+	meltdownCount = meltdownCount + 1
+	self:Bar(args.spellId, meltdownCount == 3 and 72.2 or 51.5, CL.count:format(args.spellName, meltdownCount)) -- Delayed once due to overdrive?
+end
+
 function mod:Overdrive(args)
+	self:SetStage(2)
 	self:Message(args.spellId, "cyan")
-	self:PlaySound(args.spellId, "long") -- final stage inc
-	-- self:Bar(args.spellId, 10)
+	self:PlaySound(args.spellId, "long") -- flying away
+	self:Bar(467109, 13.25, L.landing) -- Trash Compactor // 12.5~14s
 end
 
 function mod:TrashCompactor(args)
 	self:Message(args.spellId, "yellow")
 	self:PlaySound(args.spellId, "warning") -- watch drop location
+	self:Bar(467109, {3.75, 13.25}, L.landing) -- Specify landing time by cast time
 end
 
 do
