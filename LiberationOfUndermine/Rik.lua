52df652d14597e304ade9e1ff928a7cfbb2764c4
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2025-01-23 15:20:27 +0100

    LiberationOfUndermine/Rik: Heroic updates

--- LiberationOfUndermine/Rik.lua
+++ LiberationOfUndermine/Rik.lua
@@ -5,7 +5,7 @@ if not BigWigsLoader.isTestBuild then return end
 
 local mod, CL = BigWigs:NewBoss("Rik Reverb", 2769, 2641)
 if not mod then return end
-mod:RegisterEnableMob(228652) -- Rik Reverb XXX Confirm on ptr
+mod:RegisterEnableMob(228648) -- Rik Reverb
 mod:SetEncounterID(3011)
 mod:SetPrivateAuraSounds({
 	469380, -- Sound Cannon
@@ -16,6 +16,13 @@ mod:SetStage(1)
 --------------------------------------------------------------------------------
 -- Locals
 --
+
+local amplificationCount = 1
+local echoingChantCount = 1
+local soundCannonCount = 1
+local faultyZapCount = 1
+local sparkblastIgnitionCount = 1
+local blaringDropCount = 1
 
 --------------------------------------------------------------------------------
 -- Localization
@@ -31,26 +38,22 @@ mod:SetStage(1)
 
 function mod:GetOptions()
 	return {
+		"stages",
 		-- Stage One: Party Starter
 		473748, -- Amplification!
 			1217122, -- Lingering Voltage
 			468119, -- Resonant Echoes
-				472298, -- Entranced!
+				1214598, -- Entranced!
 			-- 465795, -- Noise Pollution
 			466093, -- Haywire -- XXX Check if this warning is needed
 		466866, -- Echoing Chant
 		467606, -- Sound Cannon
-		{467044, "SAY"}, -- Faulty Zap
-		472293, -- Sparkblast Ignition
+		466979, -- Faulty Zap
+		472306, -- Sparkblast Ignition
 			1214164, -- Excitement
-		-- 464488, -- Sonic Blast
-			464518, -- Tinnitus
+		464518, -- Tinnitus
 		-- Stage Two: Hype Hustle
-		{464584, "CASTBAR"}, -- Hype Hustle!
-		-- 466722, -- Blowout! -- Can we detect when you got hit and are immune?
-		-- 1213817, -- Sound Cloud
-		467991, -- Blaring Drop
-		473655, -- Hype Fever!
+		{473260, "CASTBAR"}, -- Blaring Drop
 	},{ -- Sections
 
 	},{ -- Renames
@@ -63,60 +66,83 @@ function mod:OnRegister()
 end
 
 function mod:OnBossEnable()
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", nil, "boss1")
+
 	self:Log("SPELL_CAST_START", "Amplification", 473748)
 	self:Log("SPELL_AURA_APPLIED", "LingeringVoltageApplied", 1217122)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "LingeringVoltageApplied", 1217122)
 	self:Log("SPELL_AURA_APPLIED", "ResonantEchoesApplied", 468119)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "ResonantEchoesApplied", 468119)
-	self:Log("SPELL_AURA_APPLIED", "EntrancedApplied", 472298)
+	self:Log("SPELL_AURA_APPLIED", "EntrancedApplied", 1214598)
 	self:Log("SPELL_AURA_APPLIED", "HaywireApplied", 466093)
 	self:Log("SPELL_CAST_START", "EchoingChant", 466866)
 	self:Log("SPELL_CAST_START", "SoundCannon", 467606)
-	-- self:Log("SPELL_CAST_SUCCESS", "FaultyZap", 466961) -- XXX Success cast?
-	self:Log("SPELL_AURA_APPLIED", "FaultyZapApplied", 467044) -- 467108 pre debuff?
-	self:Log("SPELL_CAST_START", "SparkblastIgnition", 472293)
+	self:Log("SPELL_CAST_START", "FaultyZap", 466979)
+	self:Log("SPELL_AURA_APPLIED", "FaultyZapApplied", 467108) -- pre debuffs
+	-- self:Log("SPELL_CAST_START", "SparkblastIgnition", 472306) -- USCS
 	self:Log("SPELL_AURA_APPLIED", "ExcitementApplied", 1214164)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "ExcitementApplied", 1214164)
 	self:Log("SPELL_AURA_APPLIED", "TinnitusApplied", 464518)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "TinnitusApplied", 464518)
 
 	-- Stage Two: Hype Hustle
-	self:Log("SPELL_CAST_START", "HypeHustle", 464584)
-	self:Log("SPELL_AURA_REMOVED", "HypeHustleRemoved", 464584)
+	self:Log("SPELL_AURA_APPLIED", "SoundCloudApplied", 1213817)
+	self:Log("SPELL_AURA_REMOVED", "SoundCloudRemoved", 1213817)
+	self:Log("SPELL_CAST_START", "BlaringDropStart", 473260)
 	self:Log("SPELL_AURA_APPLIED", "BlaringDropApplied", 467991)
 	self:Log("SPELL_AURA_APPLIED_DOSE", "BlaringDropApplied", 467991)
-	self:Log("SPELL_CAST_START", "HypeFever", 473655)
 end
 
 function mod:OnEngage()
 	self:SetStage(1)
-	-- self:Bar(473748, 5) -- Amplification!
-	-- self:Bar(466866, 20) -- Echoing Chant
-	-- self:Bar(464488, 30) -- Sound Cannon
-	-- self:Bar(467044, 40) -- Faulty Zap
-	-- self:Bar(472293, 50) -- Sparkblast Ignition
-	-- self:Bar(464584, 120) -- Hype Hustle!
+	amplificationCount = 1
+	echoingChantCount = 1
+	soundCannonCount = 1
+	faultyZapCount = 1
+	sparkblastIgnitionCount = 1
+	blaringDropCount = 1
+
+	self:Bar(473748, 10.5, CL.count:format(self:SpellName(473748), amplificationCount)) -- Amplification!
+	self:Bar(472306, 15, CL.count:format(self:SpellName(472306), sparkblastIgnitionCount)) -- Sparkblast Ignition
+	self:Bar(466866, 21.5, CL.count:format(self:SpellName(466866), echoingChantCount)) -- Echoing Chant
+	self:Bar(467606, 27.5, CL.count:format(self:SpellName(467606), soundCannonCount)) -- Sound Cannon
+	self:Bar(466979, 39.5, CL.count:format(self:SpellName(466979), faultyZapCount)) -- Faulty Zap
+	self:Bar("stages", 116, CL.stage:format(2), 66911) -- disco ball icon
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, _, _, spellId)
+	if spellId == 472306 then -- Sparkblast Ignition
+		self:Message(spellId, "yellow", CL.count:format(self:SpellName(spellId), sparkblastIgnitionCount))
+		self:PlaySound(spellId, "alert")
+		sparkblastIgnitionCount = sparkblastIgnitionCount + 1
+		-- 15.0, 43.5, 44.6, 64.9, 43.5, 44.7
+		local cdTable = {44.6, 64.9, 43.5}
+		local cdCount = sparkblastIgnitionCount % 3 + 1
+		self:Bar(spellId, cdTable[cdCount], CL.count:format(self:SpellName(spellId), sparkblastIgnitionCount))
+	end
+end
+
 
 -- Stage One: Party Starter
 
 function mod:Amplification(args)
-	self:Message(args.spellId, "yellow")
+	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, amplificationCount))
 	self:PlaySound(args.spellId, "alert") -- spawning amplifier
-	-- self:Bar(args.spellId, 30)
+	amplificationCount = amplificationCount + 1
+	local cd = amplificationCount % 3 == 1 and 73.0 or 40.2
+	self:Bar(args.spellId, cd, CL.count:format(args.spellName, amplificationCount))
 end
 
 function mod:LingeringVoltageApplied(args)
 	if self:Me(args.destGUID) then
 		local amount = args.amount or 1
-		local tooHigh = 10
+		local tooHigh = 10 -- XXX Check what is high enough
 		if amount % 2 == 1 or amount > tooHigh then
 			self:StackMessage(args.spellId, "cyan", args.destName, amount, tooHigh)
-			if amount > tooHigh then -- XXX Check what is high enough
+			if amount > tooHigh then
 				self:PlaySound(args.spellId, "alarm") -- watch stacks
 			end
 		end
@@ -145,49 +171,55 @@ do
 		if args.time - prev > 2 then -- Throttle incase of multiple going haywire at the same time
 			prev = args.time
 			self:Message(args.spellId, "red")
-			self:PlaySound(args.spellId, "alarm")
+			-- self:PlaySound(args.spellId, "alarm")
 		end
 	end
 end
 
 function mod:EchoingChant(args)
-	self:Message(args.spellId, "orange")
+	self:Message(args.spellId, "orange", CL.count:format(args.spellName, echoingChantCount))
 	self:PlaySound(args.spellId, "alert") -- watch amplifiers
-	-- self:Bar(args.spellId, 30)
+	echoingChantCount = echoingChantCount + 1
+	local cdTable = {28.5, 66.5, 58.0}
+	local cdCount = echoingChantCount % 3 + 1
+	self:Bar(args.spellId, cdTable[cdCount], CL.count:format(args.spellName, echoingChantCount))
 end
 
 function mod:SoundCannon(args)
-	self:Message(args.spellId, "red")
+	self:Message(args.spellId, "red", CL.count:format(args.spellName, soundCannonCount))
 	self:PlaySound(args.spellId, "alert") -- watch facing/private aura
-	-- self:Bar(args.spellId, 30)
+	soundCannonCount = soundCannonCount + 1
+	local cd = soundCannonCount % 2 == 1 and 34.5 or 118.5
+	-- 27.5, 34.5, 118.5, 34.5
+	self:Bar(args.spellId, cd, CL.count:format(args.spellName, soundCannonCount))
 end
 
-do
-	local prev = 0
-	function mod:FaultyZapApplied(args)
-		if args.time - prev > 2 then
-			prev = args.time
-			self:Message(args.spellId, "yellow")
-			-- self:Bar(args.spellId, 30)
-		end
-		if self:Me(args.destGUID) then
-			self:PersonalMessage(args.spellId)
-			self:PlaySound(args.spellId, "alarm")
-			self:Say(args.spellId, nil, nil, "Faulty Zap")
-		end
+function mod:FaultyZap(args)
+	self:Message(args.spellId, "yellow", CL.count:format(args.spellName, faultyZapCount))
+	faultyZapCount = faultyZapCount + 1
+	-- 39.5, 35.5, 26.0, 91.6, 35.5, 26.0
+	local cdTable = {26.0, 91.6, 35.5}
+	local cdCount = faultyZapCount % 3 + 1
+	self:Bar(args.spellId, cdTable[cdCount], CL.count:format(args.spellName, faultyZapCount))
+end
+
+function mod:FaultyZapApplied(args)
+	if self:Me(args.destGUID) then
+		self:PersonalMessage(466979)
+		self:PlaySound(466979, "alarm")
 	end
 end
 
-do
-	local prev = 0
-	function mod:SparkblastIgnition(args)
-		if args.time - prev > 10 then -- Will multiple spawn at the same time?
-			prev = args.time
-			self:Message(args.spellId, "orange")
-			self:PlaySound(args.spellId, "long") -- kill pyrotechnics?
-		end
-	end
-end
+-- do
+-- 	local prev = 0
+-- 	function mod:SparkblastIgnition(args)
+-- 		if args.time - prev > 10 then -- Will multiple spawn at the same time?
+-- 			prev = args.time
+-- 			self:Message(args.spellId, "orange")
+-- 			self:PlaySound(args.spellId, "long") -- kill pyrotechnics?
+-- 		end
+-- 	end
+-- end
 
 function mod:ExcitementApplied(args)
 	if self:Me(args.destGUID) then
@@ -213,53 +245,31 @@ function mod:TinnitusApplied(args)
 end
 
 -- Stage Two: Hype Hustle
-
-function mod:HypeHustle(args)
-	self:StopBar(473748) -- Amplification!
-	self:StopBar(466866) -- Echoing Chant
-	self:StopBar(464488) -- Sound Cannon
-	self:StopBar(467044) -- Faulty Zap
-	self:StopBar(472293) -- Sparkblast Ignition
-	self:StopBar(464584) -- Hype Hustle!
-	self:StopBar(473655) -- Hype Fever!
-
+function mod:SoundCloudApplied(args)
 	self:SetStage(2)
-	self:Message(args.spellId, "cyan")
-	self:PlaySound(args.spellId, "long") -- stage 2
-	self:CastBar(args.spellId, 33) -- 5s cast, 28s duration
+	self:Message("stages", "cyan", CL.stage:format(2), false)
+	self:PlaySound("stages", "long") -- stage 2
+	self:Bar("stages", 36, CL.stage:format(1), args.spellId)
+	blaringDropCount = 1
 end
 
-function mod:HypeHustleRemoved(args)
+function mod:SoundCloudRemoved(args)
 	self:SetStage(1)
-	self:Message(args.spellId, "cyan", CL.over:format(args.spellName))
-	self:PlaySound(args.spellId, "long") -- stage 1
+	self:Message("stages", "cyan", CL.stage:format(1), false)
+	self:PlaySound("stages", "long") -- stage 1
+	self:Bar("stages", 116, CL.stage:format(2), 66911) -- disco ball icon
+end
 
-	-- self:Bar(473748, 5) -- Amplification!
-	-- self:Bar(466866, 20) -- Echoing Chant
-	-- self:Bar(464488, 30) -- Sound Cannon
-	-- self:Bar(467044, 40) -- Faulty Zap
-	-- self:Bar(472293, 50) -- Sparkblast Ignition
-	-- self:Bar(464584, 120) -- Hype Hustle! -- XXX 3rd time bar to Hype Fever
+function mod:BlaringDropStart(args)
+	self:Message(args.spellId, "red", CL.count:format(args.spellName, blaringDropCount))
+	self:PlaySound(args.spellId, "warning") -- go amplifier
+	self:CastBar(args.spellId, 5, CL.count_amount:format(args.spellName, blaringDropCount, 4))
+	blaringDropCount = blaringDropCount + 1
 end
 
 function mod:BlaringDropApplied(args)
 	if self:Me(args.destGUID) then
-		self:StackMessage(args.spellId, "blue", args.destName, args.amount, 1)
-		self:PlaySound(args.spellId, "warning") -- failed to avoid
+		self:StackMessage(473260, "blue", args.destName, args.amount, 1)
+		self:PlaySound(473260, "warning") -- failed to avoid
 	end
-end
-
-function mod:HypeFever(args)
-	self:StopBar(473748) -- Amplification!
-	self:StopBar(466866) -- Echoing Chant
-	self:StopBar(464488) -- Sound Cannon
-	self:StopBar(467044) -- Faulty Zap
-	self:StopBar(472293) -- Sparkblast Ignition
-	self:StopBar(464584) -- Hype Hustle!
-	self:StopBar(473655) -- Hype Fever!
-
-	self:SetStage(2)
-	self:Message(args.spellId, "cyan")
-	self:PlaySound(args.spellId, "long") -- stage 2
-	-- self:CastBar(args.spellId, 33) -- is there a soft end?
 end
