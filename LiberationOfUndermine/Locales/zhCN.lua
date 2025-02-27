40b86fd9745ed7eb87d919f7e20b6cdb60ff1b0f
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-02-27 15:08:32 +0800

    Update zhCN (#1918)

--- LiberationOfUndermine/Locales/zhCN.lua
+++ LiberationOfUndermine/Locales/zhCN.lua
@@ -1,65 +1,65 @@
 local L = BigWigs:NewBossLocale("Vexie and the Geargrinders", "zhCN")
 if not L then return end
 if L then
-    --L.plating_removed = "%d Protective Plating left"
-	--L.exhaust_fumes = "Raid Damage"
+	L.plating_removed = "剩余 %d 层防御护板"
+	L.exhaust_fumes = "团队伤害"
 end
 
 L = BigWigs:NewBossLocale("Cauldron of Carnage", "zhCN")
 if L then
-	--L.custom_on_fade_out_bars = "Fade out bars"
-	--L.custom_on_fade_out_bars_desc = "Fade out bars which belong to the boss that is out of range."
+	L.custom_on_fade_out_bars = "淡出计时器"
+	L.custom_on_fade_out_bars_desc = "当首领超出范围时，淡出其相关的技能计时器。"
 
-	--L.bomb_explosion = "Bomb Explosion"
-	--L.bomb_explosion_desc = "Show a timer for the explosion off the bombs."
+	L.bomb_explosion = "炸弹爆炸"
+	L.bomb_explosion_desc = "显示炸弹爆炸的倒计时。"
 end
 
 L = BigWigs:NewBossLocale("Rik Reverb", "zhCN")
 if L then
-	--L.amplification = "Amplifiers"
-	--L.echoing_chant = "Echoes"
-	--L.faulty_zap = "Zaps"
-	--L.sparkblast_ignition = "Barrels"
+	L.amplification = "增幅器"
+	L.echoing_chant = "音波"
+	L.faulty_zap = "电击"
+	L.sparkblast_ignition = "桶"
 end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "zhCN")
 if L then
-	--L.rolled_over_by = "Rolled over by %s" -- Rolled over by PlayerX
-	--L.landing = "Landing" -- Landing down from the sky
+	L.rolled_over_by = "被 %s 碾过" -- Rolled over by PlayerX
+	L.landing = "降落" -- Landing down from the sky
 
-	--L.electromagnetic_sorting = "Sorting" -- Short for Electromagnetic Sorting
-	--L.incinerator = "Fire Circles"
+	L.electromagnetic_sorting = "电磁分拣" --中文技能名称短，就不简写了
+	L.incinerator = "火圈"
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "zhCN")
 if L then
-	--L.foot_blasters = "Mines"
-	--L.screw_up = "Drills"
-	--L.sonic_ba_boom = "Raid Damage"
-	--L.polarization_generator = "Color Swaps"
+	L.foot_blasters = "地雷"
+	L.screw_up = "钻头"
+	L.sonic_ba_boom = "团队伤害"
+	L.polarization_generator = "正负极"
 end
 
 L = BigWigs:NewBossLocale("The One-Armed Bandit", "zhCN")
 if L then
-	--L.rewards = "Prizes" -- Fabulous Prizes
-	--L.rewards_desc = "When two Tokens are locked in, the \"Fabulous Prize\" is dispensed.\nMessages will let you know which one has been rewarded.\nThe infobox will show which prizes are still available."
+	L.rewards = "豪华大奖" -- Fabulous Prizes
+	L.rewards_desc = "当2种礼卷被组合后，将发放\"豪华大奖\"。\n信息会提醒你获得了哪种奖励。\n信息框也会显示哪些奖励任然可用。"
 	--L.rewards_icon = "inv_111_vendingmachine_blackwater"
-	--L.deposit_time = "Deposit Time" -- Timer that indicates how long you have left to deposit the tokens.
+	L.deposit_time = "投卷计时" -- Timer that indicates how long you have left to deposit the tokens.
 
-	--L.pay_line = "Coins"
-	--L.shock = "Shock"
-	--L.flame = "Flame"
-	--L.coin = "Coin"
+	L.pay_line = "凭证"
+	L.shock = "震击"
+	L.flame = "烈焰"
+	L.coin = "硬币"
 end
 
 L = BigWigs:NewBossLocale("Mug'Zee, Heads of Security", "zhCN")
 if L then
-	--L.earthshaker_gaol = "Prisons"
-	--L.frostshatter_boots = "Frost Boots" -- Short for Frostshatter Boots
-	--L.frostshatter_spear = "Frost Spears" -- Short for Frostshatter Spears
-	--L.stormfury_finger_gun = "Finger Gun" -- Short for Stormfury Finger Gun
-	--L.molten_gold_knuckles = "Tank Frontal"
-	--L.unstable_crawler_mines = "Mines"
-	--L.goblin_guided_rocket = "Rocket"
-	--L.double_whammy_shot = "Tank Soak"
+	L.earthshaker_gaol = "牢狱"
+	L.frostshatter_boots = "冰靴" -- “霜裂冰靴”简写
+	L.frostshatter_spear = "长矛" -- “霜裂长矛”简写
+	L.stormfury_finger_gun = "手指枪" -- “风暴手指枪”简写
+	L.molten_gold_knuckles = "坦克正面"
+	L.unstable_crawler_mines = "地雷"
+	L.goblin_guided_rocket = "火箭"
+	L.double_whammy_shot = "坦克分摊"
 end
