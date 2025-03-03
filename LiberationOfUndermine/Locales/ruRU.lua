911f0464d65bd2aef337417677a27ce5842000ee
IKnewOne <41551327+IKnewOne@users.noreply.github.com>
2025-03-04 01:01:28 +0200

    Update ruRU (#1927)

--- LiberationOfUndermine/Locales/ruRU.lua
+++ LiberationOfUndermine/Locales/ruRU.lua
@@ -1,84 +1,84 @@
 local L = BigWigs:NewBossLocale("Vexie and the Geargrinders", "ruRU")
 if not L then return end
 if L then
-    --L.plating_removed = "%d Protective Plating left"
-	--L.exhaust_fumes = "Raid Damage"
+    L.plating_removed = "%d пластин осталось"
+	L.exhaust_fumes = "Рейд урон"
 end
 
 L = BigWigs:NewBossLocale("Cauldron of Carnage", "ruRU")
 if L then
-	--L.custom_on_fade_out_bars = "Fade out bars"
-	--L.custom_on_fade_out_bars_desc = "Fade out bars which belong to the boss that is out of range."
+	L.custom_on_fade_out_bars = "Скрывать полосы"
+	L.custom_on_fade_out_bars_desc = "Скрывать полосы боссов вне ренджа."
 
-	--L.bomb_explosion = "Bomb Explosion"
-	--L.bomb_explosion_desc = "Show a timer for the explosion off the bombs."
+	L.bomb_explosion = "Взрыв Бомбы"
+	L.bomb_explosion_desc = "Показывать таймер взрыва бомбы."
 end
 
 L = BigWigs:NewBossLocale("Rik Reverb", "ruRU")
 if L then
-	--L.amplification = "Amplifiers"
-	--L.echoing_chant = "Echoes"
-	--L.faulty_zap = "Zaps"
-	--L.sparkblast_ignition = "Barrels"
+	L.amplification = "Усилители"
+	L.echoing_chant = "Эхо"
+	L.faulty_zap = "Разряды"
+	L.sparkblast_ignition = "Бомбочки"
 end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "ruRU")
 if L then
-	--L.ball_size_medium = "Medium Ball!"
-	--L.ball_size_large = "Large Ball!"
-	--L.rolled_on_you = "%s rolled over YOU" -- PlayerX rolled over you
-	--L.rolled_from_you = "Rolled over %s" -- (you) Rolled over PlayerX
-	--L.garbage_dump_message = "YOU hit BOSS for %s"
+	L.ball_size_medium = "Средний Шар!"
+	L.ball_size_large = "Большой Шапр!"
+	L.rolled_on_you = "%s прокатился по ТЕБЕ" -- PlayerX rolled over you
+	L.rolled_from_you = "Прокатился по %s" -- (you) Rolled over PlayerX
+	L.garbage_dump_message = "ТЫ ударил босса на %s"
 
-	--L.electromagnetic_sorting = "Sorting" -- Short for Electromagnetic Sorting
-	--L.muffled_doomsplosion = "Bomb Soaked"
-	--L.incinerator = "Fire Circles"
-	--L.landing = "Landing" -- Landing down from the sky
+	L.electromagnetic_sorting = "Сортировка" -- Short for Electromagnetic Sorting
+	L.muffled_doomsplosion = "Бомба поглощена"
+	L.incinerator = "Огонь войды"
+	L.landing = "Приземление" -- Landing down from the sky
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "ruRU")
 if L then
-	--L.foot_blasters = "Mines"
-	--L.screw_up = "Drills"
-	--L.sonic_ba_boom = "Raid Damage"
-	--L.polarization_generator = "Color Swaps"
+	L.foot_blasters = "Мины"
+	L.screw_up = "Буры"
+	L.sonic_ba_boom = "Рейд урон"
+	L.polarization_generator = "Смена цвета"
 
-	--L.polarization_soon = "Color Swap Soon: %s"
+	L.polarization_soon = "Смена цвета скоро: %s"
 end
 
 L = BigWigs:NewBossLocale("The One-Armed Bandit", "ruRU")
 if L then
-	--L.rewards = "Prizes" -- Fabulous Prizes
-	--L.rewards_desc = "When two Tokens are locked in, the \"Fabulous Prize\" is dispensed.\nMessages will let you know which one has been rewarded.\nThe infobox will show which prizes are still available."
-	--L.rewards_icon = "inv_111_vendingmachine_blackwater"
-	--L.deposit_time = "Deposit Time" -- Timer that indicates how long you have left to deposit the tokens.
+	L.rewards = "Призы" -- Fabulous Prizes
+	L.rewards_desc = "После сдачи 2 токенов, \"Потрясающие Призы\" активируется.\nСообщения скажут, что было выдано.\nИнфобокс покажет, какие награды всё ещё доступны."
+	L.rewards_icon = "inv_111_vendingmachine_blackwater"
+	L.deposit_time = "Время для сдачи" -- Timer that indicates how long you have left to deposit the tokens.
 
-	--L.pay_line = "Coins"
-	--L.shock = "Shock"
-	--L.flame = "Flame"
-	--L.coin = "Coin"
+	L.pay_line = "Монетки"
+	L.shock = "Шок"
+	L.flame = "Огонь"
+	L.coin = "Монета"
 end
 
 L = BigWigs:NewBossLocale("Mug'Zee, Heads of Security", "ruRU")
 if L then
-	--L.earthshaker_gaol = "Prisons"
-	--L.frostshatter_boots = "Frost Boots" -- Short for Frostshatter Boots
-	--L.frostshatter_spear = "Frost Spears" -- Short for Frostshatter Spears
-	--L.stormfury_finger_gun = "Finger Gun" -- Short for Stormfury Finger Gun
-	--L.molten_gold_knuckles = "Tank Frontal"
-	--L.unstable_crawler_mines = "Mines"
-	--L.goblin_guided_rocket = "Rocket"
-	--L.double_whammy_shot = "Tank Soak"
+	L.earthshaker_gaol = "Темницы"
+	L.frostshatter_boots = "Сапоги льда" -- Short for Frostshatter Boots
+	L.frostshatter_spear = "Ледяные копья" -- Short for Frostshatter Spears
+	L.stormfury_finger_gun = "Палец-пушка" -- Short for Stormfury Finger Gun
+	L.molten_gold_knuckles = "Танк фронтал"
+	L.unstable_crawler_mines = "Мины"
+	L.goblin_guided_rocket = "Ракета"
+	L.double_whammy_shot = "Танк Соак"
 end
 
 L = BigWigs:NewBossLocale("Chrome King Gallywix", "ruRU")
 if L then
-	--L.scatterblast_canisters = "Cone Soak"
-	--L.fused_canisters = "Group Soaks"
-	--L.tick_tock_canisters = "Soaks"
+	L.scatterblast_canisters = "Конус делёжка"
+	L.fused_canisters = "Групповая делёжка"
+	L.tick_tock_canisters = "Делёжка"
 
-	--L.duds = "Duds" -- Short for 1500-Pound "Dud"
-	--L.all_duds_detontated = "All Duds Detonated!"
-	--L.duds_remaining = "%d |4Dud:Duds; remaining" -- 1 Bomb Remaning, 2 Bombs Remaining.. etc
-	--L.duds_soak = "Soak Duds (%d left)"
+	L.duds = "Ждуны" -- Short for 1500-Pound "Dud"
+	L.all_duds_detontated = "Все Ждуны Детонированы!"
+	L.duds_remaining = "%d |4Ждун:Ждуны; осталось" -- 1 Bomb Remaning, 2 Bombs Remaining.. etc
+	L.duds_soak = "Сокай Ждунов (%d осталось)"
 end
