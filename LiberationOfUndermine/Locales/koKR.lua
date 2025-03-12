ce2346aa7c94dc029e4a770cef5093a5c1a19145
007bbb <86834094+007bbb@users.noreply.github.com>
2025-03-12 11:01:36 +0900

    Update koKR.lua

--- LiberationOfUndermine/Locales/koKR.lua
+++ LiberationOfUndermine/Locales/koKR.lua
@@ -13,9 +13,9 @@ if L then
 	L.bomb_explosion = "폭탄 폭발"
 	L.bomb_explosion_desc = "폭탄이 폭파되기까지의 타이머 표시."
 
-	--L.eruption_stomp = "Stomp" -- Short for Eruption Stomp
-	--L.thunderdrum_salvo = "Salvo" -- Short for Thunderdrum Salvo
-	--L.voltaic_image = "Fixates" -- Multiple of Fixate
+	L.eruption_stomp = "분출" -- Short for Eruption Stomp
+	L.thunderdrum_salvo = "바닥" -- Short for Thunderdrum Salvo
+	L.voltaic_image = "환영" -- Multiple of Fixate
 end
 
 L = BigWigs:NewBossLocale("Rik Reverb", "koKR")
@@ -36,7 +36,7 @@ if L then
 
 	L.electromagnetic_sorting = "분류" -- Short for Electromagnetic Sorting
 	L.muffled_doomsplosion = "폭탄 처리됨"
-	--L.short_fuse = "Bombshell Explosion"
+	L.short_fuse = "폭탄 폭발"
 	L.incinerator = "소각"
 	L.landing = "착륙" -- Landing down from the sky
 end
@@ -88,7 +88,7 @@ if L then
 	L.scatterblast_canisters = "브레스 같이맞기"
 	L.fused_canisters = "용기 같이맞기"
 	L.tick_tock_canisters = "바닥밟기"
-	--L.total_destruction = "DESTRUCTION!"
+	L.total_destruction = "파괴!"
 
 	L.duds = "불발탄" -- Short for 1500-Pound "Dud"
 	L.all_duds_detontated = "모든 불발탄 해체완료!"
