ee28162e8faea5e02cbfc011294b5c578e085bdb
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-03-13 18:19:00 +0800

    Update zhCN.lua

--- LiberationOfUndermine/Locales/zhCN.lua
+++ LiberationOfUndermine/Locales/zhCN.lua
@@ -86,6 +86,8 @@ end
 
 L = BigWigs:NewBossLocale("Chrome King Gallywix", "zhCN")
 if L then
+	L.story_phase_trigger = "怎么，自以为胜利了？" -- 怎么，自以为胜利了？呵，我给你准备了惊喜。
+	
 	L.scatterblast_canisters = "裂破分摊" -- 使用技能名“裂破弹药筒”前2字做提醒
 	L.fused_canisters = "引线分摊" -- 使用技能名“引线弹药筒”前2字做提醒
 	L.tick_tock_canisters = "分摊"
