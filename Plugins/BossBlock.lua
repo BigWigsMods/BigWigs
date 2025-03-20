cafc5bfee924f55b6e12abd988faa1f23efcb440
funkydude <funkydude@users.noreply.github.com>
2025-03-20 18:06:43 +0000

    Wrath compat code cleanup

--- Plugins/BossBlock.lua
+++ Plugins/BossBlock.lua
@@ -1023,9 +1023,8 @@ do
 	-- Cinematic skipping hack to workaround an item (Vision of Time) that creates cinematics in Siege of Orgrimmar.
 	function plugin:SiegeOfOrgrimmarCinematics()
 		local hasItem
-		local GetItemCount = C_Item and C_Item.GetItemCount or GetItemCount -- XXX Wrath compat
 		for i = 105930, 105935 do -- Vision of Time items
-			local count = GetItemCount(i)
+			local count = C_Item.GetItemCount(i)
 			if count > 0 then hasItem = true break end -- Item is found in our inventory
 		end
 		if hasItem and not self.SiegeOfOrgrimmarCinematicsFrame then
