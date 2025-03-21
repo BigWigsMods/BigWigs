53a334015292bf02ca8b70d7718ccc616c8be1b2
007bbb <86834094+007bbb@users.noreply.github.com>
2025-03-21 13:56:54 +0900

    Update koKR (#1960)

--- LiberationOfUndermine/Locales/koKR.lua
+++ LiberationOfUndermine/Locales/koKR.lua
@@ -52,7 +52,7 @@ if L then
 	L.mega_magnetize = "자석"
 	L.jumbo_void_beam = "강력 광선"
 	L.void_barrage = "공허 구슬"
-	L.everything = "모든 것" -- ??
+	L.everything = "발명품 활성화" -- ??
 
 	L.under_you_comment = "바닥" -- Implies this setting is for the damage from the ground effect under you
 end
