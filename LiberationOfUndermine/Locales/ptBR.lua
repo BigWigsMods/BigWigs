ff5f77482334693da15b13759399355ac8044d85
Damer-flinn <natanbyt@gmail.com>
2025-03-29 20:43:11 -0300

    Update ptBR.lua

--- LiberationOfUndermine/Locales/ptBR.lua
+++ LiberationOfUndermine/Locales/ptBR.lua
@@ -94,7 +94,7 @@ end
 
 L = BigWigs:NewBossLocale("Chrome King Gallywix", "ptBR")
 if L then
-	L.story_phase_trigger = "What, you think you won?" -- What, you think you won? Nah, I got somethin' else for ya.
+	L.story_phase_trigger = "Tá achando que ganhou?" -- Tá achando que ganhou? Não. Tenho mais uma coisinha pra você.
 
 	L.scatterblast_canisters = "Soak em Cone"
 	L.fused_canisters = "Soak em Grupo"
