BigWigsMods/BigWigs

commit 4ecd1b38c112a83f496cad1a4b6906b9d3c97d64

LiberationOfUndermine/Stix: Add countdown for ball application, locale cleanups

LiberationOfUndermine/Stix.lua
@@ -31,8 +31,6 @@ local mobMark = 0
 
 local L = mod:GetLocale()
 if L then
-	L.ball_size_medium = "Medium Ball!"
-	L.ball_size_large = "Large Ball!"
 	L.rolled_on_you = "%s rolled over YOU"
 	L.rolled_from_you = "Rolled over %s"
 	L.garbage_dump_message = "YOU hit BOSS for %s"
@@ -41,9 +39,6 @@ if L then
 	L.muffled_doomsplosion = "Bomb Soaked"
 	L.short_fuse = "Bombshell Explosion"
 	L.incinerator = "Fire Circles"
-	L.landing = "Landing" -- Landing down from the sky
-
-	L["467109_desc"] = 467135 -- XXX description fixed in 11.1.5
 end
 
 --------------------------------------------------------------------------------
@@ -58,7 +53,7 @@ function mod:GetOptions()
 	return {
 		"berserk",
 		464399, -- Electromagnetic Sorting
-			{461536, "ME_ONLY_EMPHASIZE", "COUNTDOWN"}, -- Rolling Rubbish
+			{461536, "SAY", "SAY_COUNTDOWN", "ME_ONLY_EMPHASIZE", "COUNTDOWN"}, -- Rolling Rubbish
 				rollingRubbishMarker,
 				465741, -- Garbage Dump
 				465611, -- Rolled!
@@ -99,22 +94,24 @@ function mod:GetOptions()
 		[467117] = "", -- Overdrive
 		[1218704] = CL.mythic,
 	},{ -- Renames
-		[464399] = L.electromagnetic_sorting, -- Electromagnetic Sorting (Balls + Adds)
+		[464399] = L.electromagnetic_sorting, -- Electromagnetic Sorting (Sorting)
+		[461536] = CL.balls, -- Rolling Rubbish (Balls)
 		[465747] = L.muffled_doomsplosion, -- Muffled Doomsplosion (Bomb Soaked)
 		[473119] = L.short_fuse, -- Short Fuse (Bombshell Explosion)
 		[464149] = L.incinerator, -- Incinerator (Fire Circles)
-		[467109] = L.landing, -- Trash Compactor (Landing)
+		[467109] = CL.landing, -- Trash Compactor (Landing)
 	}
 end
 
 function mod:OnRegister()
-	self:SetSpellRename(464399, L.electromagnetic_sorting) -- Electromagnetic Sorting (Balls + Adds)
+	self:SetSpellRename(464399, L.electromagnetic_sorting) -- Electromagnetic Sorting (Sorting)
 	self:SetSpellRename(464149, L.incinerator) -- Incinerator (Fire Circles)
-	self:SetSpellRename(467109, L.landing) -- Trash Compactor (Landing)
+	self:SetSpellRename(467109, CL.landing) -- Trash Compactor (Landing)
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_AURA_APPLIED", "SortedApplied", 465346) -- These players will become Rolling Rubbish
+	self:Log("SPELL_AURA_REMOVED", "SortedRemoved", 465346)
 	self:Log("SPELL_AURA_APPLIED", "RollingRubbishApplied", 461536)
 	self:Log("SPELL_AURA_REMOVED", "RollingRubbishRemoved", 461536)
 	self:Log("SPELL_AURA_APPLIED", "RolledApplied", 465611)
@@ -174,7 +171,7 @@ function mod:OnEngage()
 
 	self:Bar(464149, self:Easy() and 10.0 or 11.1, CL.count:format(L.incinerator, incineratorCount)) -- Incinerator -- Fire
 	self:Bar(464112,  self:Easy() and 16.0 or 17.7, CL.count:format(self:SpellName(464112), demolishCount)) -- Demolish
-	self:Bar(464399, self:Easy() and 20.0 or 22.2, CL.count:format(L.electromagnetic_sorting, electromagneticSortingCount)) -- Electromagnetic Sorting -- Balls + Adds
+	self:Bar(464399, self:Easy() and 20.0 or 22.2, CL.count:format(L.electromagnetic_sorting, electromagneticSortingCount)) -- Electromagnetic Sorting
 	if not self:LFR() then
 		self:Bar(1217954, self:Easy() and 41.0 or 45.5, CL.count:format(self:SpellName(1217954), meltdownCount)) -- Meltdown
 	end
@@ -251,7 +248,10 @@ do
 
 	function mod:SortedApplied(args)
 		if self:Me(args.destGUID) then
-			self:PersonalMessage(461536) -- Rolling Rubbish
+			self:PersonalMessage(461536, nil, CL.ball) -- Rolling Rubbish
+			self:TargetBar(461536, 4.5, args.destName, CL.ball)
+			self:Say(461536, CL.ball, nil, "Ball")
+			self:SayCountdown(461536, 4.5)
 			self:PlaySound(461536, "warning") -- you're becoming rubbish
 		end
 
@@ -263,6 +263,12 @@ do
 		}
 		if not scheduled then
 			scheduled = self:ScheduleTimer("MarkPlayers", 0.1)
+		end
+	end
+
+	function mod:SortedRemoved(args)
+		if self:Me(args.destGUID) then
+			self:CancelSayCountdown(461536)
 		end
 	end
 end
@@ -281,10 +287,10 @@ do
 		if powerType == "ALTERNATE" then
 			local power = UnitPower(unit, 10)
 			if power >= 200 and ballSize < 200 then
-				self:Message(461536, "green", L.ball_size_large) -- Rolling Rubbish
+				self:Message(461536, "green", CL.medium) -- Rolling Rubbish
 				self:PlaySound(461536, "info")
 			elseif power >= 100 and ballSize < 100 then
-				self:Message(461536, "green", L.ball_size_medium) -- Rolling Rubbish
+				self:Message(461536, "green", CL.large) -- Rolling Rubbish
 				self:PlaySound(461536, "alert")
 			end
 			ballSize = power
@@ -293,9 +299,9 @@ do
 
 	function mod:RollingRubbishRemoved(args)
 		if self:Me(args.destGUID) then
-			self:StopBar(args.spellName, args.destName)
-			self:PersonalMessage(461536, "removed")
-			self:PlaySound(461536, "info")
+			self:StopBar(CL.ball, args.destName)
+			self:PersonalMessage(args.spellId, "removed", CL.ball)
+			self:PlaySound(args.spellId, "info")
 			self:UnregisterUnitEvent("UNIT_POWER_UPDATE", "player", "vehicle")
 		end
 		self:CustomIcon(rollingRubbishMarker, args.destName)
@@ -480,9 +486,9 @@ function mod:OverdriveRemoved(args)
 end
 
 function mod:TrashCompactor(args)
-	self:Message(args.spellId, "red", L.landing)
+	self:Message(args.spellId, "red", CL.landing)
 	self:PlaySound(args.spellId, "warning") -- watch drop location
-	self:CastBar(args.spellId, 3.75, L.landing)
+	self:CastBar(args.spellId, 3.75, CL.landing)
 end
 
 function mod:TrashCompactorSuccess(args)

LiberationOfUndermine/Locales/deDE.lua
@@ -23,8 +23,6 @@ end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "deDE")
 if L then
-	L.ball_size_medium = "Mittlerer Ball!"
-	L.ball_size_large = "Großer Ball!"
 	L.rolled_on_you = "%s ist über DICH gerollt" -- PlayerX rolled over you
 	L.rolled_from_you = "Ist über %s gerollt" -- (you) Rolled over PlayerX
 	L.garbage_dump_message = "DU hast den BOSS getroffen für %s"
@@ -33,7 +31,6 @@ if L then
 	L.muffled_doomsplosion = "Bombe absorbiert"
 	L.short_fuse = "Bombenhülse Explosion"
 	L.incinerator = "Feuerkreise"
-	L.landing = "Landung" -- Landing down from the sky
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "deDE")

LiberationOfUndermine/Locales/esES.lua
@@ -23,8 +23,6 @@ end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "esES")
 if L then
-	L.ball_size_medium = "Bola mediana!"
-	L.ball_size_large = "Bola grande!"
 	L.rolled_on_you = "%s rodó sobre ti" -- PlayerX rolled over you
 	L.rolled_from_you = "Rodaste sobre %s" -- (you) Rolled over PlayerX
 	L.garbage_dump_message = "Golpeaste al jefe por %s daño"
@@ -33,7 +31,6 @@ if L then
 	L.muffled_doomsplosion = "Explosión amortiguada"
 	L.short_fuse = "Explosión de cangrejobús"
 	L.incinerator = "Circulos de fuego"
-	L.landing = "Aterrizando" -- Landing down from the sky
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "esES")

LiberationOfUndermine/Locales/esMX.lua
@@ -23,8 +23,6 @@ end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "esMX")
 if L then
-	L.ball_size_medium = "Pelota Mediana!"
-	L.ball_size_large = "Pelota Grande!"
 	L.rolled_on_you = "%s Te atropello A TI" -- PlayerX rolled over you
 	L.rolled_from_you = "Atropellaste a %s" -- (you) Rolled over PlayerX
 	L.garbage_dump_message = "Le pegaste al jefe por %s"
@@ -33,7 +31,6 @@ if L then
 	L.muffled_doomsplosion = "Bomba explotada"
 	L.short_fuse = "Explosión de Caparabomba"
 	L.incinerator = "Circulos de fuego"
-	L.landing = "Aterrizaje" -- Landing down from the sky
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "esMX")

LiberationOfUndermine/Locales/frFR.lua
@@ -23,8 +23,6 @@ end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "frFR")
 if L then
-	L.ball_size_medium = "Boule moyenne !"
-	L.ball_size_large = "Boule large !!"
 	L.rolled_on_you = "%s vous a roulé dessus" -- PlayerX rolled over you
 	L.rolled_from_you = "Vous avez roulé sur %s" -- (you) Rolled over PlayerX
 	L.garbage_dump_message = "Vous avez frappé le boss pour %s"
@@ -33,7 +31,6 @@ if L then
 	L.muffled_doomsplosion = "Explosion fatale étouffée"
 	L.short_fuse = "Explosion de crabombe"
 	L.incinerator = "Cercles de feu"
-	L.landing = "Atterrissage" -- Landing down from the sky
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "frFR")

LiberationOfUndermine/Locales/itIT.lua
@@ -23,8 +23,6 @@ end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "itIT")
 if L then
-	--L.ball_size_medium = "Medium Ball!"
-	--L.ball_size_large = "Large Ball!"
 	--L.rolled_on_you = "%s rolled over YOU" -- PlayerX rolled over you
 	--L.rolled_from_you = "Rolled over %s" -- (you) Rolled over PlayerX
 	--L.garbage_dump_message = "YOU hit BOSS for %s"
@@ -33,7 +31,6 @@ if L then
 	--L.muffled_doomsplosion = "Bomb Soaked"
 	--L.short_fuse = "Bombshell Explosion"
 	--L.incinerator = "Fire Circles"
-	--L.landing = "Landing" -- Landing down from the sky
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "itIT")

LiberationOfUndermine/Locales/koKR.lua
@@ -23,8 +23,6 @@ end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "koKR")
 if L then
-	L.ball_size_medium = "중형 구체!"
-	L.ball_size_large = "대형 구체!"
 	L.rolled_on_you = "%s 에게 치임" -- PlayerX rolled over you
 	L.rolled_from_you = "%s 를 치고감" -- (you) Rolled over PlayerX
 	L.garbage_dump_message = "보스에게 %s 피해를 입힘"
@@ -33,7 +31,6 @@ if L then
 	L.muffled_doomsplosion = "폭탄 처리됨"
 	L.short_fuse = "폭탄 폭발"
 	L.incinerator = "소각"
-	L.landing = "착륙" -- Landing down from the sky
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "koKR")

LiberationOfUndermine/Locales/ptBR.lua
@@ -23,8 +23,6 @@ end
 
 L = BigWigs:NewBossLocale("Stix Bunkjunker", "ptBR")
 if L then
-	L.ball_size_medium = "Bola Média!"
-	L.ball_size_large = "Bola Grande!"
 	L.rolled_on_you = "%s rolou sobre VOCÊ" -- PlayerX rolou sobre você
 	L.rolled_from_you = "Rolou sobre %s" -- (você) rolou sobre PlayerX
 	L.garbage_dump_message = "VOCÊ atingiu o CHEFE por %s"
@@ -33,7 +31,6 @@ if L then
 	L.muffled_doomsplosion = "Bomba abafada"
 	L.short_fuse = "Explosão da Bomba"
 	L.incinerator = "Círculos de Fogo"
-	L.landing = "Pouso" -- Descendo do céu
 end
 
 L = BigWigs:NewBossLocale("Sprocketmonger Lockenstock", "ptBR")

commit 3a729f0f1c497e98647c5dfb4472ae6ab85f371c

LiberationOfUndermine/Stix: Fix ball countdown not stopping and size messages being reversed, regression from 4ecd1b38c112a83f496cad1a4b6906b9d3c97d64

LiberationOfUndermine/Stix.lua
@@ -47,7 +47,7 @@ end
 --
 
 local rollingRubbishMarkerMapTable = {1, 2, 3, 4} -- Easier to adjust which icons are used
-local rollingRubbishMarker = mod:AddMarkerOption(false, "player", rollingRubbishMarkerMapTable[1], 461536, unpack(rollingRubbishMarkerMapTable))
+local rollingRubbishMarker = mod:AddMarkerOption(false, "player", rollingRubbishMarkerMapTable[1], 461536, unpack(rollingRubbishMarkerMapTable)) -- Rolling Rubbish
 local scrapmasterMarkerMapTable = {8, 7, 6, 5}
 local scrapmasterMarker = mod:AddMarkerOption(false, "npc", scrapmasterMarkerMapTable[1], -31645, unpack(scrapmasterMarkerMapTable))
 function mod:GetOptions()
@@ -248,13 +248,13 @@ do
 		iconList = {}
 	end
 
-	function mod:SortedApplied(args)
+	function mod:SortedApplied(args) -- You're about to become a ball
 		if self:Me(args.destGUID) then
 			self:PersonalMessage(461536, nil, CL.ball) -- Rolling Rubbish
 			self:TargetBar(461536, 4.5, args.destName, CL.ball)
 			self:Say(461536, CL.ball, nil, "Ball")
 			self:SayCountdown(461536, 4.5)
-			self:PlaySound(461536, "warning") -- you're becoming rubbish
+			self:PlaySound(461536, "warning")
 		end
 
 		iconList[#iconList+1] = {
@@ -281,7 +281,7 @@ do
 		if self:Me(args.destGUID) then
 			ballSize = 0
 			self:RegisterUnitEvent("UNIT_POWER_UPDATE", nil, "player", "vehicle")
-			self:TargetBar(args.spellId, 24, args.destName)
+			self:TargetBar(args.spellId, 24, args.destName, CL.ball)
 		end
 	end
 
@@ -289,10 +289,10 @@ do
 		if powerType == "ALTERNATE" then
 			local power = UnitPower(unit, 10)
 			if power >= 200 and ballSize < 200 then
-				self:Message(461536, "green", CL.medium) -- Rolling Rubbish
-			elseif power >= 100 and ballSize < 100 then
 				self:Message(461536, "green", CL.large) -- Rolling Rubbish
 				self:PlaySound(461536, "long")
+			elseif power >= 100 and ballSize < 100 then
+				self:Message(461536, "green", CL.medium) -- Rolling Rubbish
 			end
 			ballSize = power
 		end