ea58c5abd1c54c666ba3cc892496f05862e857ce
funkydude <funkydude@users.noreply.github.com>
2025-07-07 22:00:31 +0100

    API: Sort alphabetically

--- API.lua
+++ API.lua
@@ -8,83 +8,6 @@ addonTbl.API = API
 local type, next, error = type, next, error
 
 --------------------------------------------------------------------------------
--- Locale
---
-
-do
-	local tbl = {}
-	local myRegion = GetLocale()
-	function API:NewLocale(locale, region)
-		if region == "enUS" or region == myRegion then
-			if not tbl[locale] then
-				tbl[locale] = {}
-			end
-			return tbl[locale]
-		end
-	end
-	function API:GetLocale(locale)
-		if tbl[locale] then
-			return tbl[locale]
-		end
-	end
-end
-
---------------------------------------------------------------------------------
--- Voice
---
-
-do
-	local addons = {}
-	function API.RegisterVoicePack(pack)
-		if type(pack) ~= "string" then error("Voice pack name must be a string.") return end
-
-		if not addons[pack] then
-			addons[pack] = true
-		else
-			error(("Voice pack %s already registered."):format(pack))
-		end
-	end
-
-	function API.HasVoicePack()
-		if next(addons) then
-			return true
-		end
-	end
-end
-
---------------------------------------------------------------------------------
--- Countdown
---
-
-do
-	local voices = {}
-	function API:RegisterCountdown(id, name, data)
-		if not data then data, name = name, id end
-		if type(id) ~= "string" then error("Countdown name must be a string.") end
-		if type(data) ~= "table" or #data < 5 or #data > 10 then error("Countdown data must be an indexed table with 5-10 entries.") end
-		if voices[id] then error(("Countdown %q already registered."):format(id)) end
-
-		voices[id] = { name = name }
-		for i = 1, #data do
-			voices[id][i] = data[i]
-		end
-	end
-	function API:GetCountdownList()
-		local list = {}
-		for k, v in next, voices do
-			list[k] = v.name
-		end
-		return list
-	end
-	function API:HasCountdown(id)
-		return voices[id] and true
-	end
-	function API:GetCountdownSound(id, index)
-		return voices[id] and voices[id][index]
-	end
-end
-
---------------------------------------------------------------------------------
 -- Bar Styles
 --
 
@@ -128,7 +51,61 @@ do
 end
 
 --------------------------------------------------------------------------------
--- Automated profile imports
+-- Countdown
+--
+
+do
+	local voices = {}
+	function API:RegisterCountdown(id, name, data)
+		if not data then data, name = name, id end
+		if type(id) ~= "string" then error("Countdown name must be a string.") end
+		if type(data) ~= "table" or #data < 5 or #data > 10 then error("Countdown data must be an indexed table with 5-10 entries.") end
+		if voices[id] then error(("Countdown %q already registered."):format(id)) end
+
+		voices[id] = { name = name }
+		for i = 1, #data do
+			voices[id][i] = data[i]
+		end
+	end
+	function API:GetCountdownList()
+		local list = {}
+		for k, v in next, voices do
+			list[k] = v.name
+		end
+		return list
+	end
+	function API:HasCountdown(id)
+		return voices[id] and true
+	end
+	function API:GetCountdownSound(id, index)
+		return voices[id] and voices[id][index]
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Locale
+--
+
+do
+	local tbl = {}
+	local myRegion = GetLocale()
+	function API:NewLocale(locale, region)
+		if region == "enUS" or region == myRegion then
+			if not tbl[locale] then
+				tbl[locale] = {}
+			end
+			return tbl[locale]
+		end
+	end
+	function API:GetLocale(locale)
+		if tbl[locale] then
+			return tbl[locale]
+		end
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Profile imports
 --
 
 do
@@ -199,6 +176,30 @@ do
 	-- Returns the BigWigs version hash from Git as a string
 	function API.GetVersionHash()
 		return addonTbl.versionHash
+	end
+end
+
+
+--------------------------------------------------------------------------------
+-- Voice
+--
+
+do
+	local addons = {}
+	function API.RegisterVoicePack(pack)
+		if type(pack) ~= "string" then error("Voice pack name must be a string.") return end
+
+		if not addons[pack] then
+			addons[pack] = true
+		else
+			error(("Voice pack %s already registered."):format(pack))
+		end
+	end
+
+	function API.HasVoicePack()
+		if next(addons) then
+			return true
+		end
 	end
 end
 
