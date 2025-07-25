ac532d0d5460279ab657d6553af174aa63605d32
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-07-25 09:36:16 +0800

    Update zhCN.lua

--- ManaforgeOmega/Locales/zhCN.lua
+++ ManaforgeOmega/Locales/zhCN.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Loom'ithar", "zhCN")
 if not L then return end
 if L then
-	--L.lair_weaving = "Webs" -- Webs that spawn on the edge of the room
-	--L.infusion_pylons = "Pylons" -- Short for Infusion Pylons
+	L.lair_weaving = "蛛网" -- “巢穴编织”注能的缠结连接成的坍塌之环（类似蛛网）
+	L.infusion_pylons = "注能塔" -- 注能晶塔的简称
 end
