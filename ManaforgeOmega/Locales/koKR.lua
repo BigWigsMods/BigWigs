6194579a2c879ae8df8126fd5cd7f87a421efe65
007bbb <86834094+007bbb@users.noreply.github.com>
2025-07-28 01:58:42 +0900

    Update koKR.lua

--- ManaforgeOmega/Locales/koKR.lua
+++ ManaforgeOmega/Locales/koKR.lua
@@ -1,15 +1,15 @@
 local L = BigWigs:NewBossLocale("Loom'ithar", "koKR")
 if not L then return end
 if L then
-	--L.lair_weaving = "Webs" -- Webs that spawn on the edge of the room
-	--L.infusion_pylons = "Pylons" -- Short for Infusion Pylons
+	L.lair_weaving = "거미줄" -- Webs that spawn on the edge of the room
+	L.infusion_pylons = "수정탑" -- Short for Infusion Pylons
 end
 
 L = BigWigs:NewBossLocale("Soulbinder Naazindhri", "koKR")
 if L then
-	--L.voidblade_ambush = "Ambush" -- Short for Voidblade Ambush
-	--L.soulfray_annihilation = "Lines" -- Lines that shoot out an orb along that path
-	--L.soulfray_annihilation_single = "Line" -- Single from Lines
+	L.voidblade_ambush = "매복" -- Short for Voidblade Ambush
+	L.soulfray_annihilation = "구슬" -- Lines that shoot out an orb along that path
+	L.soulfray_annihilation_single = "라인" -- Single from Lines
 end
 
 L = BigWigs:NewBossLocale("Fractillus", "koKR")
