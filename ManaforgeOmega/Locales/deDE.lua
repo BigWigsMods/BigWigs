ca9156b58d61150c0eb8ac6eb2dcdcca1e6125f0
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2025-08-06 13:50:26 +0200

    Update deDE.lua

--- ManaforgeOmega/Locales/deDE.lua
+++ ManaforgeOmega/Locales/deDE.lua
@@ -20,7 +20,7 @@ end
 L = BigWigs:NewBossLocale("Fractillus", "deDE")
 if L then
 	L.crystalline_eruption = "Wände"
-	L.shattershell = "Breaks"
+	L.shattershell = "Brechen"
 	L.shockwave_slam = "Tank Wand"
 	L.nexus_shrapnel = "Schrapnell landet"
 	L.crystal_lacerations = "Blutung"
@@ -28,23 +28,23 @@ end
 
 L = BigWigs:NewBossLocale("Nexus-King Salhadaar", "deDE")
 if L then
-	L.oath_bound_removed_dose = "1x Schwurgebunden entfernt"
+	L.oath_bound_removed_dose = "1x Eidgebunden entfernt"
 	L.behead = "Klauen" -- Claws of a dragon
 	L.netherbreaker = "Zirkel"
 	L.galaxy_smash = "Schmettern" -- Short for Galactic Smash, and multiple of them.
-	L.starkiller_swing = "Sterntöter" -- Short for Starkiller Swing, and multiple of them.
+	L.starkiller_swing = "Sternentöter" -- Short for Starkiller Swing, and multiple of them.
 	L.vengeful_oath = "Geister"
 end
 
 L = BigWigs:NewBossLocale("Dimensius, the All-Devouring", "deDE")
 if L then
-	--L.shattered_space = "Hands" -- Dimensius reaches down with both hands
-	--L.reverse_gravity = "Gravity" -- Short for Reverse Gravity
-	--L.extinction = "Fragment" -- Dimensius hurls a fragment of a broken world
-	--L.slows = "Slows"
-	--L.slow = "Slow" -- Singular of Slows
-	--L.stardust_nova = "Nova" -- Short for Stardust Nova
-	--L.extinguish_the_stars = "Stars" -- Short for Extinguish the Stars
-	--L.darkened_sky = "Rings"
-	--L.cosmic_collapse = "Collapse" -- Short for Cosmic Collapse
+	L.shattered_space = "Hände" -- Dimensius reaches down with both hands
+	L.reverse_gravity = "Gravitation" -- Short for Reverse Gravity
+	L.extinction = "Fragment" -- Dimensius hurls a fragment of a broken world
+	L.slows = "Verlangsamungen"
+	L.slow = "Verlangsamung" -- Singular of Slows
+	L.stardust_nova = "Nova" -- Short for Stardust Nova
+	L.extinguish_the_stars = "Sterne" -- Short for Extinguish the Stars
+	L.darkened_sky = "Ringe"
+	L.cosmic_collapse = "Kollaps" -- Short for Cosmic Collapse
 end
