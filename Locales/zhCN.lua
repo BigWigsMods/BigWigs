1cde171cc34082d647f3513bd502f858e1e20088
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-08-11 08:20:39 +0800

    Update zhCN (#2150)

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -5,7 +5,7 @@ if not L then return end
 L.tempNew = "新：现在输入 |cFFFFFFFF/key|r 可以查看队伍的史诗钥石信息。"
 
 -- API.lua
---L.showAddonBar = "The addon '%s' created the '%s' bar."
+L.showAddonBar = "'%s' 插件创建了 '%s' 动作条。"
 
 -- Core.lua
 L.berserk = "狂暴"
