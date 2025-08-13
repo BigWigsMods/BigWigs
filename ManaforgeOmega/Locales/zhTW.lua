0d9e82f322f087e641a5a0441ac42a8256d17a10
EK <eke00372@gmail.com>
2025-08-13 22:38:31 +0800

    Update zhTW

--- ManaforgeOmega/Locales/zhTW.lua
+++ ManaforgeOmega/Locales/zhTW.lua
@@ -1,27 +1,27 @@
 local L = BigWigs:NewBossLocale("Loom'ithar", "zhTW")
 if not L then return end
 if L then
-	--L.lair_weaving = "Webs" -- Webs that spawn on the edge of the room
-	--L.infusion_pylons = "Pylons" -- Short for Infusion Pylons
+	L.lair_weaving = "織網" -- Webs that spawn on the edge of the room
+	L.infusion_pylons = "注能" -- Short for Infusion Pylons 注能塔
 end
 
 L = BigWigs:NewBossLocale("Soulbinder Naazindhri", "zhTW")
 if L then
-	--L.voidblade_ambush = "Ambush" -- Short for Voidblade Ambush
-	--L.soulfray_annihilation = "Lines" -- Lines that shoot out an orb along that path
-	--L.soulfray_annihilation_single = "Line" -- Single from Lines
+	L.voidblade_ambush = "伏擊" -- Short for Voidblade Ambush
+	L.soulfray_annihilation = "射線" -- 傷魂滅殺/出球/射線
+	L.soulfray_annihilation_single = "射線" -- 傷魂滅殺/出球/射線
 end
 
 L = BigWigs:NewBossLocale("Forgeweaver Araz", "zhTW")
 if L then
-	--L.invoke_collector = "Collector" -- Short for Arcane Collector
+	L.invoke_collector = "收集器" -- Short for Arcane Collector
 end
 
 L = BigWigs:NewBossLocale("Fractillus", "zhTW")
 if L then
-	--L.crystalline_eruption = "Walls"
+	L.crystalline_eruption = "水晶牆" -- 出牆
 	--L.shattershell = "Breaks"
-	--L.shockwave_slam = "Tank Wall"
+	L.shockwave_slam = "坦克牆"
 	--L.nexus_shrapnel = "Shrapnel Lands"
 	--L.crystal_lacerations = "Bleed"
 end
@@ -29,7 +29,7 @@ end
 L = BigWigs:NewBossLocale("Nexus-King Salhadaar", "zhTW")
 if L then
 	--L.oath_bound_removed_dose = "1x Oath-Bound Removed"
-	--L.behead = "Claws" -- Claws of a dragon
+	L.behead = "斬首" -- Claws of a dragon
 	--L.netherbreaker = "Circles"
 	--L.galaxy_smash = "Smashes" -- Short for Galactic Smash, and multiple of them.
 	--L.starkiller_swing = "Starkillers" -- Short for Starkiller Swing, and multiple of them.
