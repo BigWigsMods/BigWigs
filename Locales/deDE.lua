477304537d7b8760dc41188cc251cc45c0dee287
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2025-08-22 23:21:56 +0200

    Update deDE (#2198)

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -381,11 +381,11 @@ L.instanceKeysTest8 = "|cFF00FF98Mönch:|r +8"
 L.instanceKeysTest10 = "|cFFFF7C0ADruide:|r +10"
 L.instanceKeysDisplay = "|c%s%s:|r +%d" -- "PLAYER_NAME: +DUNGEON_LEVEL"
 L.instanceKeysDisplayWithDungeon = "|c%s%s:|r +%d (%s)" -- "PLAYER_NAME: +DUNGEON_LEVEL (DUNGEON_NAME)"
---L.instanceKeysShowAll = "Always show all players"
---L.instanceKeysShowAllDesc = "Enabling this option will show all players in the list, even if their keystone doesn't belong to the dungeon you are in."
---L.instanceKeysOtherDungeonColor = "Other dungeon color"
---L.instanceKeysOtherDungeonColorDesc = "Choose the font color for players that have keystones that don't belong to the dungeon you are in."
---L.instanceKeysEndOfRunDesc = "By default the list will only show when you enter a mythic dungeon. Enabling this option will also show the list when the Mythic+ is over."
+L.instanceKeysShowAll = "Immer alle Spieler anzeigen"
+L.instanceKeysShowAllDesc = "Durch Aktivierung dieser Option werden alle Spieler in der Liste angezeigt, auch wenn deren Schlüsselstein nicht zum aktuellen Dungeon passt."
+L.instanceKeysOtherDungeonColor = "Farbe anderer Dungeons"
+L.instanceKeysOtherDungeonColorDesc = "Schriftfarbe für Spieler wählen, deren Schlüsselstein nicht zum aktuellen Dungeon passt."
+L.instanceKeysEndOfRunDesc = "Standardmäßig wird die Liste nur beim Betreten eines mythischen Dungeons angezeigt. Durch Aktivierung dieser Option wird die Liste auch nach Abschluss von Mythisch+ Dungeons angezeigt."
 
 -----------------------------------------------------------------------
 -- LFGTimer.lua
