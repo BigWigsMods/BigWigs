03b843e8e3ff0dd3e74de1722823f531ef599c71
EK <eke00372@gmail.com>
2025-08-25 20:55:09 +0800

    Update zhTW.lua

--- KhazAlgar/Locales/zhTW.lua
+++ KhazAlgar/Locales/zhTW.lua
@@ -1,10 +1,10 @@
 local L = BigWigs:NewBossLocale("Aggregation of Horrors", "zhTW")
 if not L then return end
 if L then
-	--L.void_rocks = "Void Rocks" -- Plural of Void Rock (452379)
+	L.void_rocks = "彈幕" -- Plural of Void Rock (452379)
 end
 
 L = BigWigs:NewBossLocale("Reshanor, The Untethered", "zhTW")
 if L then
-	--L.run = "Run to the portal and click the extra action button"
+	L.run = "去傳送門點擊額外技能"
 end
