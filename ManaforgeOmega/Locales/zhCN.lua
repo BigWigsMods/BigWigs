bd7f2e20300210099543ce75b42f932e19511b43
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-08-26 22:06:06 +0800

    Update zhCN (#2214)

--- ManaforgeOmega/Locales/zhCN.lua
+++ ManaforgeOmega/Locales/zhCN.lua
@@ -45,18 +45,18 @@ if L then
 	L.slows = "减速"
 	L.slow = "减速"
 	L.mass_destruction = "射线"
-	--L.mass_destruction_single = "Line"
+	L.mass_destruction_single = "射线"
 	L.stardust_nova = "新星" -- 技能“星尘新星”的简称
 	L.extinguish_the_stars = "众星" -- 技能“熄灭众星”的简称
 	L.darkened_sky = "波"
-	--L.cosmic_collapse = "Tank Pull"
-	--L.cosmic_collapse_easy = "Tank Smash"
+	L.cosmic_collapse = "坦克拉人"
+	L.cosmic_collapse_easy = "坦克大圈"
 	L.soaring_reshii = "坐骑可用" -- On the timer for when flying is available
 
-	--L.left_living_mass = "Living Mass (Left)"
-	--L.right_living_mass = "Living Mass (Right)"
+	L.left_living_mass = "活体物质（左）"  -- NPCID：242587 活体物质
+	L.right_living_mass = "活体物质（右）"
 
-	--L.soaring_reshii_monster_yell = "You've done well so far." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
+	--L.soaring_reshii_monster_yell = "你目前的表现好得出奇，" -- [CHAT_MSG_MONSTER_YELL] 你目前的表现好得出奇，可我们还没结束呢。#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
 
-	L.weakened_soon_monster_yell = "必须出击，就是现在！" -- [CHAT_MSG_MONSTER_YELL] We must strike--now!#Xal'atath###Xal'atath##0#0##0#4873#nil#0#false#false#false#false",
+	L.weakened_soon_monster_yell = "必须出击，就是现在！" -- [CHAT_MSG_MONSTER_YELL] 必须出击，就是现在！#Xal'atath###Xal'atath##0#0##0#4873#nil#0#false#false#false#false",
 end
