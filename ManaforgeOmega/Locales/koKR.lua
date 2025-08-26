4f14745a17ffccf264f060ff38bc0d452e5870c9
007bbb <86834094+007bbb@users.noreply.github.com>
2025-08-26 14:12:28 +0900

    Update koKR.lua

--- ManaforgeOmega/Locales/koKR.lua
+++ ManaforgeOmega/Locales/koKR.lua
@@ -56,7 +56,7 @@ if L then
 	L.left_living_mass = "살아있는 질량체 (왼쪽)"
 	L.right_living_mass = "살아있는 질량체 (오른쪽)"
 
-	--L.soaring_reshii_monster_yell = "You've done well so far." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
+	L.soaring_reshii_monster_yell = "지금까진 잘했어요." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
 
 	L.weakened_soon_monster_yell = "지금 공격해야 해요!" -- [CHAT_MSG_MONSTER_YELL] We must strike--now!#Xal'atath###Xal'atath##0#0##0#4873#nil#0#false#false#false#false",
 end
