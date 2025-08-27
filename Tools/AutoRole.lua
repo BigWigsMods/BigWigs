d17f0d603235c3cf156137de9e335a35444071f7
funkydude <funkydude@users.noreply.github.com>
2025-08-27 19:01:22 +0100

    Tools/AutoRole: Only change role when appropriate, closes #2223

--- Tools/AutoRole.lua
+++ Tools/AutoRole.lua
@@ -2,14 +2,17 @@ RolePollPopup:UnregisterEvent("ROLE_POLL_BEGIN") -- We don't need to participate
 
 local LibSpec = LibStub("LibSpecialization")
 local myName = UnitNameUnmodified("player")
+local canChangeRole = false
 
 local frame = CreateFrame("Frame")
 local IsInGroup, IsPartyLFG = IsInGroup, IsPartyLFG
 local InCombatLockdown, UnitAffectingCombat = InCombatLockdown, UnitAffectingCombat
 local UnitGroupRolesAssigned, UnitSetRole = UnitGroupRolesAssigned, UnitSetRole
 
-LibSpec.RegisterGroup({}, function(_, role, _, player)
-	if myName == player and IsInGroup() and not IsPartyLFG() and UnitGroupRolesAssigned("player") ~= role then
+local LibSpecTable = {}
+LibSpec.RegisterGroup(LibSpecTable, function(_, role, _, player)
+	if canChangeRole and myName == player and IsInGroup() and not IsPartyLFG() and UnitGroupRolesAssigned("player") ~= role then
+		canChangeRole = false
 		if InCombatLockdown() or UnitAffectingCombat("player") then
 			frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 			return
@@ -18,15 +21,24 @@ LibSpec.RegisterGroup({}, function(_, role, _, player)
 	end
 end)
 
+LibSpec.RegisterPlayerSpecChange(LibSpecTable, function()
+	canChangeRole = true
+end)
+
 frame:SetScript("OnEvent", function(self, event)
-	self:UnregisterEvent(event)
-	if IsInGroup() and not IsPartyLFG() then
-		local _, role = LibSpec:MySpecialization()
-		if UnitGroupRolesAssigned("player") ~= role then
-			UnitSetRole("player", role)
+	if event == "GROUP_FORMED" then
+		canChangeRole = true
+	else
+		self:UnregisterEvent(event)
+		if IsInGroup() and not IsPartyLFG() then
+			local _, role = LibSpec:MySpecialization()
+			if UnitGroupRolesAssigned("player") ~= role then
+				UnitSetRole("player", role)
+			end
 		end
 	end
 end)
+frame:RegisterEvent("GROUP_FORMED")
 
 local _, addonTbl = ...
 local L = addonTbl.API:GetLocale("BigWigs")
