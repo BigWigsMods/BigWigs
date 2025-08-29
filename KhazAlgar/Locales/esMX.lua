49d1d4729c5cf70c4ef9a1f8cc2b980acb0bae35
Catriel Goldmann <c.goldmann@hotmail.com>
2025-08-29 18:07:11 -0400

    Update esMX.lua

--- KhazAlgar/Locales/esMX.lua
+++ KhazAlgar/Locales/esMX.lua
@@ -1,10 +1,10 @@
 local L = BigWigs:NewBossLocale("Aggregation of Horrors", "esMX")
 if not L then return end
 if L then
-	--L.void_rocks = "Void Rocks" -- Plural of Void Rock (452379)
+	L.void_rocks = "Piedras del Vacío" -- Plural of Void Rock (452379)
 end
 
 L = BigWigs:NewBossLocale("Reshanor, The Untethered", "esMX")
 if L then
-	--L.run = "Run to the portal and click the extra action button"
+	L.run = "Corre al portal y presiona el botón de acción extra"
 end
