3551cddccb88d82deaab4fc0ccdb9e98517c6dac
Catriel Goldmann <c.goldmann@hotmail.com>
2025-08-29 16:29:43 -0400

    Update esMX.lua

--- ManaforgeOmega/Locales/esMX.lua
+++ ManaforgeOmega/Locales/esMX.lua
@@ -1,60 +1,60 @@
 local L = BigWigs:NewBossLocale("Loom'ithar", "esMX")
 if not L then return end
 if L then
-	--L.lair_weaving = "Webs" -- Webs that spawn on the edge of the room
-	--L.infusion_pylons = "Pylons" -- Short for Infusion Pylons
+	L.lair_weaving = "Telarañas" -- Webs that spawn on the edge of the room
+	L.infusion_pylons = "Pilones" -- Short for Infusion Pylons
 end
 
 L = BigWigs:NewBossLocale("Soulbinder Naazindhri", "esMX")
 if L then
-	--L.voidblade_ambush = "Ambush" -- Short for Voidblade Ambush
-	--L.soulfray_annihilation = "Lines" -- Lines that shoot out an orb along that path
-	--L.soulfray_annihilation_single = "Line" -- Single from Lines
-	--L.remaining_adds = "Remaining Adds" -- All remaining adds from Soul Calling spawn
+	L.voidblade_ambush = "Emboscada" -- Short for Voidblade Ambush
+	L.soulfray_annihilation = "Líneas" -- Lines that shoot out an orb along that path
+	L.soulfray_annihilation_single = "Línea" -- Single from Lines
+	L.remaining_adds = "Almas Faltantes" -- All remaining adds from Soul Calling spawn
 end
 
 L = BigWigs:NewBossLocale("Forgeweaver Araz", "esMX")
 if L then
-	--L.invoke_collector = "Collector" -- Short for Arcane Collector
+	L.invoke_collector = "Recolector" -- Short for Arcane Collector
 end
 
 L = BigWigs:NewBossLocale("Fractillus", "esMX")
 if L then
-	--L.crystalline_shockwave = "Walls"
-	--L.shattershell = "Breaks"
-	--L.shockwave_slam = "Tank Wall"
-	--L.nexus_shrapnel = "Shrapnel Lands"
-	--L.crystal_lacerations = "Bleed"
+	L.crystalline_shockwave = "Muro"
+	L.shattershell = "Destruir"
+	L.shockwave_slam = "Muro de Tanque"
+	L.nexus_shrapnel = "Lluvia de Metralla"
+	L.crystal_lacerations = "Sangrado"
 end
 
 L = BigWigs:NewBossLocale("Nexus-King Salhadaar", "esMX")
 if L then
-	--L.fractal_images = "Dragons"
-	--L.oath_bound_removed_dose = "1x Oath-Bound Removed"
-	--L.behead = "Claws" -- Claws of a dragon
-	--L.netherbreaker = "Circles"
-	--L.galaxy_smash = "Smashes" -- Short for Galactic Smash, and multiple of them.
-	--L.starkiller_swing = "Starkillers" -- Short for Starkiller Swing, and multiple of them.
-	--L.vengeful_oath = "Spirits"
+	L.fractal_images = "Imágenes"
+	L.oath_bound_removed_dose = "1x Atado al juramento removido"
+	L.behead = "Garras" -- Claws of a dragon
+	L.netherbreaker = "Círculos"
+	L.galaxy_smash = "Machaque" -- Short for Galactic Smash, and multiple of them.
+	L.starkiller_swing = "Mataestrellas" -- Short for Starkiller Swing, and multiple of them.
+	L.vengeful_oath = "Espíritus"
 end
 
 L = BigWigs:NewBossLocale("Dimensius, the All-Devouring", "esMX")
 if L then
-	--L.gravity = "Gravity" -- Short for Reverse Gravity
-	--L.extinction = "Fragment" -- Dimensius hurls a fragment of a broken world
-	--L.slows = "Slows"
-	--L.slow = "Slow" -- Singular of Slows
-	--L.mass_destruction = "Lines"
-	--L.mass_destruction_single = "Line"
-	--L.stardust_nova = "Nova" -- Short for Stardust Nova
-	--L.extinguish_the_stars = "Stars" -- Short for Extinguish the Stars
-	--L.darkened_sky = "Rings"
-	--L.cosmic_collapse = "Tank Pull"
-	--L.cosmic_collapse_easy = "Tank Smash"
-	--L.soaring_reshii = "Mount Available" -- On the timer for when flying is available
+	L.gravity = "Gravedad" -- Short for Reverse Gravity
+	L.extinction = "Fragmento" -- Dimensius hurls a fragment of a broken world
+	L.slows = "Ralentizaciones"
+	L.slow = "Ralentización" -- Singular of Slows
+	L.mass_destruction = "Líneas"
+	L.mass_destruction_single = "Línea"
+	L.stardust_nova = "Nova" -- Short for Stardust Nova
+	L.extinguish_the_stars = "Estrellas" -- Short for Extinguish the Stars
+	L.darkened_sky = "Anillos"
+	L.cosmic_collapse = "Agarre de Tanque"
+	L.cosmic_collapse_easy = "Golpe de Tanque"
+	L.soaring_reshii = "Montura Disponible" -- On the timer for when flying is available
 
-	--L.left_living_mass = "Living Mass (Left)"
-	--L.right_living_mass = "Living Mass (Right)"
+	L.left_living_mass = "Masa Viviente (Izquierda)"
+	L.right_living_mass = "Masa Viviente (Derecha)"
 
 	--L.soaring_reshii_monster_yell = "You've done well so far." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
 
