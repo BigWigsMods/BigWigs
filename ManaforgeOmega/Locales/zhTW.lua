9c70a9f852c8f1718570ea2b100e54aa9e4c5ea8
EK <eke00372@gmail.com>
2025-09-13 19:07:32 +0800

    Update zhTW (#2257)

--- ManaforgeOmega/Locales/zhTW.lua
+++ ManaforgeOmega/Locales/zhTW.lua
@@ -29,9 +29,9 @@ end
 
 L = BigWigs:NewBossLocale("Nexus-King Salhadaar", "zhTW")
 if L then
-	--L.fractal_images = "Dragons"
+	L.fractal_images = "飛龍"
 	L.oath_bound_removed_dose = "移除一層誓言"
-	L.behead = "斬首" -- Claws of a dragon
+	L.behead = "利爪" -- Claws of a dragon 斬首
 	L.netherbreaker = "大圈"
 	L.galaxy_smash = "撞擊" -- 星河撞擊
 	L.starkiller_swing = "弒星" -- 弒星揮擊，或者射線
@@ -40,23 +40,23 @@ end
 
 L = BigWigs:NewBossLocale("Dimensius, the All-Devouring", "zhTW")
 if L then
-	L.gravity = "逆轉重力" -- 原名就行
+	L.gravity = "重力"
 	L.extinction = "滅絕" -- Dimensius hurls a fragment of a broken world
 	L.slows = "緩速"
 	L.slow = "緩速" -- Singular of Slows
-	--L.mass_destruction = "Lines"
-	--L.mass_destruction_single = "Line"
+	L.mass_destruction = "射線"
+	L.mass_destruction_single = "射線"
 	L.stardust_nova = "新星" -- Short for Stardust Nova
 	L.extinguish_the_stars = "星晨" -- Short for Extinguish the Stars
 	L.darkened_sky = "星環"
-	--L.cosmic_collapse = "Tank Pull"
-	--L.cosmic_collapse_easy = "Tank Smash"
+	L.cosmic_collapse = "坦克拉人"
+	L.cosmic_collapse_easy = "坦克大圈"
 	L.soaring_reshii = "可飛行" -- On the timer for when flying is available
 
-	--L.left_living_mass = "Living Mass (Left)"
-	--L.right_living_mass = "Living Mass (Right)"
+	L.left_living_mass = "左側過剩物質"
+	L.right_living_mass = "右側過剩物質"
 
-	--L.soaring_reshii_monster_yell = "You've done well so far." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
+	L.soaring_reshii_monster_yell = "你的表現很不錯。" -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
 
 	L.weakened_soon_monster_yell = "必需現在就出擊！" -- [CHAT_MSG_MONSTER_YELL] We must strike--now!#Xal'atath###Xal'atath##0#0##0#4873#nil#0#false#false#false#false",
 end
