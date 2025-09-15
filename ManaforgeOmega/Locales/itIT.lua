c0d466c6dd317324b9a9aa40dbeb2718db592679
Ahdoree <panapunker@gmail.com>
2025-09-15 21:33:54 +0200

    Update itIT.lua

--- ManaforgeOmega/Locales/itIT.lua
+++ ManaforgeOmega/Locales/itIT.lua
@@ -5,56 +5,56 @@ if L then
 	L.infusion_pylons = "Piloni" -- Short for Infusion Pylons
 end
 
-L = BigWigs:NewBossLocale("Soulbinder Naazindhri", "itIT")
+L = BigWigs:NewBossLocale("Vincolatrice d'Anime Naazindhri", "itIT")
 if L then
-	--L.voidblade_ambush = "Ambush" -- Short for Voidblade Ambush
-	--L.soulfray_annihilation = "Lines" -- Lines that shoot out an orb along that path
-	--L.soulfray_annihilation_single = "Line" -- Single from Lines
-	--L.remaining_adds = "Remaining Adds" -- All remaining adds from Soul Calling spawn
+	L.voidblade_ambush = "Imboscata" -- Short for Voidblade Ambush
+	L.soulfray_annihilation = "Linee" -- Lines that shoot out an orb along that path
+	L.soulfray_annihilation_single = "Linea" -- Single from Lines
+	L.remaining_adds = "Addini Rimanenti" -- All remaining adds from Soul Calling spawn
 end
 
-L = BigWigs:NewBossLocale("Forgeweaver Araz", "itIT")
+L = BigWigs:NewBossLocale("Tessiforgia Araz", "itIT")
 if L then
-	--L.invoke_collector = "Collector" -- Short for Arcane Collector
+	L.invoke_collector = "Collectore" -- Short for Arcane Collector
 end
 
 L = BigWigs:NewBossLocale("Fractillus", "itIT")
 if L then
-	--L.crystalline_shockwave = "Walls"
-	--L.shattershell = "Breaks"
-	--L.shockwave_slam = "Tank Wall"
-	--L.nexus_shrapnel = "Shrapnel Lands"
-	--L.crystal_lacerations = "Bleed"
+	L.crystalline_shockwave = "Muri"
+	L.shattershell = "Spacca"
+	L.shockwave_slam = "Muro Tank"
+	L.nexus_shrapnel = "Arriva la Scheggia"
+	L.crystal_lacerations = "Sanguinamento"
 end
 
-L = BigWigs:NewBossLocale("Nexus-King Salhadaar", "itIT")
+L = BigWigs:NewBossLocale("Re del Nexus Salhadaar", "itIT")
 if L then
-	--L.fractal_images = "Dragons"
-	--L.oath_bound_removed_dose = "1x Oath-Bound Removed"
-	--L.behead = "Claws" -- Claws of a dragon
-	--L.netherbreaker = "Circles"
-	--L.galaxy_smash = "Smashes" -- Short for Galactic Smash, and multiple of them.
-	--L.starkiller_swing = "Starkillers" -- Short for Starkiller Swing, and multiple of them.
-	--L.vengeful_oath = "Spirits"
+	L.fractal_images = "Draghi"
+	L.oath_bound_removed_dose = "1x Conquista Rimossa"
+	L.behead = "Artigli" -- Claws of a dragon
+	L.netherbreaker = "Cerchi"
+	L.galaxy_smash = "Frantumazioni" -- Short for Galactic Smash, and multiple of them.
+	L.starkiller_swing = "Annientastelle" -- Short for Starkiller Swing, and multiple of them.
+	L.vengeful_oath = "Spettri"
 end
 
 L = BigWigs:NewBossLocale("Dimensius, the All-Devouring", "itIT")
 if L then
-	--L.gravity = "Gravity" -- Short for Reverse Gravity
-	--L.extinction = "Fragment" -- Dimensius hurls a fragment of a broken world
-	--L.slows = "Slows"
-	--L.slow = "Slow" -- Singular of Slows
-	--L.mass_destruction = "Lines"
-	--L.mass_destruction_single = "Line"
-	--L.stardust_nova = "Nova" -- Short for Stardust Nova
-	--L.extinguish_the_stars = "Stars" -- Short for Extinguish the Stars
-	--L.darkened_sky = "Rings"
-	--L.cosmic_collapse = "Tank Pull"
-	--L.cosmic_collapse_easy = "Tank Smash"
-	--L.soaring_reshii = "Mount Available" -- On the timer for when flying is available
+	L.gravity = "Gravità" -- Short for Reverse Gravity
+	L.extinction = "frammento" -- Dimensius hurls a fragment of a broken world
+	L.slows = "Rallentamenti"
+	L.slow = "Rallentamento" -- Singular of Slows
+	L.mass_destruction = "Linee"
+	L.mass_destruction_single = "Linea"
+	L.stardust_nova = "Nova" -- Short for Stardust Nova
+	L.extinguish_the_stars = "Stelle" -- Short for Extinguish the Stars
+	L.darkened_sky = "Anelli"
+	L.cosmic_collapse = "Tank Pulla"
+	L.cosmic_collapse_easy = "Frantumazione"
+	L.soaring_reshii = "Mount Disponibile" -- On the timer for when flying is available
 
-	--L.left_living_mass = "Living Mass (Left)"
-	--L.right_living_mass = "Living Mass (Right)"
+	L.left_living_mass = "Living Mass (Sinistra)"
+	L.right_living_mass = "Living Mass (Destra)"
 
 	--L.soaring_reshii_monster_yell = "You've done well so far." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
 
