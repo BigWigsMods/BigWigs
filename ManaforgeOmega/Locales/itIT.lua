522902d9ac8820fe623ec42b1e56f502a6d44010
Ahdoree <panapunker@gmail.com>
2025-09-17 00:36:25 +0200

    Update itIT.lua
    last 2 lines of Xal'atath's quotes

--- ManaforgeOmega/Locales/itIT.lua
+++ ManaforgeOmega/Locales/itIT.lua
@@ -56,7 +56,7 @@ if L then
 	L.left_living_mass = "Living Mass (Sinistra)"
 	L.right_living_mass = "Living Mass (Destra)"
 
-	--L.soaring_reshii_monster_yell = "You've done well so far." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
+	L.soaring_reshii_monster_yell = "Finora avete agito bene." -- [CHAT_MSG_MONSTER_YELL] You've done well so far. Surprising. But we're not done yet.#Xal'atath###Meeresflask##0#0##0#256#nil#0#false#false#false#false",
 
-	--L.weakened_soon_monster_yell = "We must strike--now!" -- [CHAT_MSG_MONSTER_YELL] We must strike--now!#Xal'atath###Xal'atath##0#0##0#4873#nil#0#false#false#false#false",
+	L.weakened_soon_monster_yell = "Dobbiamo colpire, ora!" -- [CHAT_MSG_MONSTER_YELL] We must strike--now!#Xal'atath###Xal'atath##0#0##0#4873#nil#0#false#false#false#false",
 end
