efaf812c992133a5cfce3ef8e0f15153ded8a94a
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2026-02-08 16:36:18 +0100

    TheVoidspire/Salhadaar: Add Private Aura Config

--- TheVoidspire/Salhadaar.lua
+++ TheVoidspire/Salhadaar.lua
@@ -8,6 +8,19 @@ local mod, CL = BigWigs:NewBoss("Fallen-King Salhadaar", 2912, 2736)
 if not mod then return end
 mod:RegisterEnableMob(240432) -- Fallen-King Salhadaar
 mod:SetEncounterID(3179)
+mod:SetPrivateAuraSounds({
+	{1250828, sound = "underyou"}, -- Void Exposure
+	{1250991, sound = "alarm"}, -- Dark Radiation
+	1245960, -- Void Infusion
+	{1245592, sound = "underyou"}, -- Torturous Extract
+	{1260030, sound = "underyou"}, -- Umbral Beams
+	{1253024, 1268992}, -- Shattering Twilight (tank, others)
+	{1251213, sound = "underyou"}, -- Twilight Spikes
+	1248697, -- Despotic Command
+	{1248709, sound = "alarm"}, -- Oppressive Darkness
+	{1250686, sound = "info"}, -- Twisting Obscurity (Raid damage/dot)
+	{1271577, sound = "alarm"}, -- Destabilizing Strikes
+})
 
 --------------------------------------------------------------------------------
 -- Locals
