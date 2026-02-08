5419442abac6586961e29804674f3b6ad8fa5129
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2026-02-08 17:03:00 +0100

    TheVoidspire/Vanguard: Add Private Aura Config

--- TheVoidspire/Vanguard.lua
+++ TheVoidspire/Vanguard.lua
@@ -8,6 +8,22 @@ local mod, CL = BigWigs:NewBoss("Lightblinded Vanguard", 2912, 2737)
 if not mod then return end
 mod:RegisterEnableMob(240431, 240437, 240438) -- Bellamy, Lightblood, Senn
 mod:SetEncounterID(3180)
+mod:SetPrivateAuraSounds({
+	{1276982, sound = "underyou"}, -- Divine Consecration
+	{1248985, 1248994}, -- Execution Sentence (Targetted)
+	{1249008, 1249024, sound = "info"}, -- Execution Sentence (Soaked Debuff)
+	-- {1249122, 1249123} -- Execution Sentance (Unknown)
+	{1272324, sound = "underyou"}, -- Divine Tempest
+	{1246736, sound = "alarm"}, -- Judgement (Final Verdict)
+	{1251857, sound = "alarm"}, -- Judgement (Shield of the Righteous)
+	-- {1251840, sound = "alarm"}, -- Judgment of the Righteous, Used?
+	{1248652, sound = "alarm"}, -- Divine Toll
+	1246487, -- Avenger's Shield (Targetted)
+	{1246502, sound = "alarm"}, -- Avenger's Shield (DoT Debuff)
+	{1248721, sound = "alarm"}, -- Tyr's Wrath
+	-- {1249130, sound = "info"}, -- Elekk Charge (Buff on the NPC's, lol)
+	{1258514, sound = "alarm"}, -- Blinding Light
+})
 
 --------------------------------------------------------------------------------
 -- Locals
