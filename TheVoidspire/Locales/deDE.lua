b52b4c4f8afa85c1c242f1feefcd73f2a8a0128a
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2026-02-26 22:02:31 +0100

    Update deDE

--- TheVoidspire/Locales/deDE.lua
+++ TheVoidspire/Locales/deDE.lua
@@ -1,21 +1,21 @@
 local L = BigWigs:NewBossLocale("Vorasius", "deDE")
 if not L then return end
 if L then
-	--L.shadowclaw_slam = "Slams"
+	L.shadowclaw_slam = "Hiebe"
 end
 
 L = BigWigs:NewBossLocale("Fallen-King Salhadaar", "deDE")
 if L then
-	--L.fractured_projection = "Kicks"
+	L.fractured_projection = "Unterbrechungen"
 end
 
 L = BigWigs:NewBossLocale("Lightblinded Vanguard", "deDE")
 if L then
-	--L.aura_of_wrath = "Wrath" -- Short for Aura of Wrath
-	--L.execution_sentence = "Executes" -- Short for Execution Sentence
-	--L.judgement_red = "Judgement [R]" -- R for the Red icon.
-	--L.aura_of_devotion = "Devotion" -- Short for Aura of Devotion
-	--L.judgement_blue = "Judgement [B]" -- B for the Blue icon.
-	--L.aura_of_peace = "Peace" -- Short for Aura of Peace
-	--L.zaelous_spirit = "Spirit" -- Short for Zealous Spirit
+	L.aura_of_wrath = "Zorn" -- Short for Aura of Wrath
+	L.execution_sentence = "Todesurteil" -- Short for Execution Sentence
+	L.judgement_red = "Richturteil [R]" -- R for the Red icon.
+	L.aura_of_devotion = "Hingabe" -- Short for Aura of Devotion
+	L.judgement_blue = "Richturteil [B]" -- B for the Blue icon.
+	L.aura_of_peace = "Frieden" -- Short for Aura of Peace
+	L.zaelous_spirit = "Geist" -- Short for Zealous Spirit
 end
