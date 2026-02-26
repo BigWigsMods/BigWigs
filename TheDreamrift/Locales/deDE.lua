60f0b4b0c54f4b0a92070b57915053637b1b3dae
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2026-02-26 21:56:58 +0100

    Update deDE

--- TheDreamrift/Locales/deDE.lua
+++ TheDreamrift/Locales/deDE.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Chimaerus the Undreamt God", "deDE")
 if not L then return end
 if L then
-	--L.rift_madness = "Madness" -- Short for Rift Madness
-	--L.consuming_miasma = "Dispels" -- Debuffs to dispell
+	L.rift_madness = "Wahn" -- Short for Rift Madness
+	L.consuming_miasma = "Dispels" -- Debuffs to dispell
 end
