f3f09981c1ababba75a6cd46b43ee7754707e469
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-03-04 12:06:38 +0800

    Update zhCN.lua

--- TheVoidspire/Locales/zhCN.lua
+++ TheVoidspire/Locales/zhCN.lua
@@ -1,21 +1,21 @@
 local L = BigWigs:NewBossLocale("Vorasius", "zhCN")
 if not L then return end
 if L then
-	--L.shadowclaw_slam = "Slams"
+	L.shadowclaw_slam = "重击" --“影爪重击”简称
 end
 
 L = BigWigs:NewBossLocale("Fallen-King Salhadaar", "zhCN")
 if L then
-	--L.fractured_projection = "Kicks"
+	L.fractured_projection = "镜像" -- 打断？
 end
 
 L = BigWigs:NewBossLocale("Lightblinded Vanguard", "zhCN")
 if L then
-	--L.aura_of_wrath = "Wrath" -- Short for Aura of Wrath
-	--L.execution_sentence = "Executes" -- Short for Execution Sentence
-	--L.judgement_red = "Judgement [R]" -- R for the Red icon.
-	--L.aura_of_devotion = "Devotion" -- Short for Aura of Devotion
-	--L.judgement_blue = "Judgement [B]" -- B for the Blue icon.
-	--L.aura_of_peace = "Peace" -- Short for Aura of Peace
-	--L.zaelous_spirit = "Spirit" -- Short for Zealous Spirit
+	L.aura_of_wrath = "愤怒" -- “愤怒光环”简称
+	L.execution_sentence = "处决" -- “处决宣判”简称
+	L.judgement_red = "审判 [红]" -- 红色图标
+	L.aura_of_devotion = "虔诚" -- “虔诚光环”简称
+	L.judgement_blue = "审判 [蓝]" -- 蓝色图标
+	L.aura_of_peace = "平心" -- “平心光环”简称
+	L.zaelous_spirit = "狂热之魂" -- “狂热之魂”全称合适
 end
