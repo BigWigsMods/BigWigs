8073d2b407bdf28944a35909b0cea0c98dfb2b2d
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-03-06 00:48:06 +0800

    Update zhCN (#2414)

--- TheDreamrift/Locales/zhCN.lua
+++ TheDreamrift/Locales/zhCN.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Chimaerus the Undreamt God", "zhCN")
 if not L then return end
 if L then
-	--L.rift_madness = "Madness" -- Short for Rift Madness
-	--L.consuming_miasma = "Dispels" -- Debuffs to dispell
+	L.rift_madness = "疯狂" -- “裂隙疯狂”简称
+	L.consuming_miasma = "驱散" -- Debuffs to dispell
 end
