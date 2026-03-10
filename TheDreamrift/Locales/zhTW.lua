2a2dca559719083b1dd255047d29725fdd387402
EK <eke00372@gmail.com>
2026-03-10 19:19:49 +0800

    Update zhTW (#2424)

--- TheDreamrift/Locales/zhTW.lua
+++ TheDreamrift/Locales/zhTW.lua
@@ -1,6 +1,6 @@
 local L = BigWigs:NewBossLocale("Chimaerus the Undreamt God", "zhTW")
 if not L then return end
 if L then
-	--L.rift_madness = "Madness" -- Short for Rift Madness
-	--L.consuming_miasma = "Dispels" -- Debuffs to dispell
+	L.rift_madness = "瘋狂" -- Short for Rift Madness
+	L.consuming_miasma = "驅散" -- Debuffs to dispell 或者瘴氣
 end
--- TheVoidspire/Locales/zhTW.lua
+++ TheVoidspire/Locales/zhTW.lua
@@ -1,21 +1,21 @@
 local L = BigWigs:NewBossLocale("Vorasius", "zhTW")
 if not L then return end
 if L then
-	--L.shadowclaw_slam = "Slams"
+	L.shadowclaw_slam = "猛擊"
 end
 
 L = BigWigs:NewBossLocale("Fallen-King Salhadaar", "zhTW")
 if L then
-	--L.fractured_projection = "Kicks"
+	L.fractured_projection = "投影"
 end
 
 L = BigWigs:NewBossLocale("Lightblinded Vanguard", "zhTW")
 if L then
-	--L.aura_of_wrath = "Wrath" -- Short for Aura of Wrath
-	--L.execution_sentence = "Executes" -- Short for Execution Sentence
-	--L.judgement_red = "Judgement [R]" -- R for the Red icon.
-	--L.aura_of_devotion = "Devotion" -- Short for Aura of Devotion
-	--L.judgement_blue = "Judgement [B]" -- B for the Blue icon.
-	--L.aura_of_peace = "Peace" -- Short for Aura of Peace
-	--L.zaelous_spirit = "Spirit" -- Short for Zealous Spirit
+	L.aura_of_wrath = "憤慨光環" -- Short for Aura of Wrath 不用縮寫
+	L.execution_sentence = "死刑宣判" -- Short for Execution Sentence  不用縮寫，或者處決?
+	L.judgement_red = "紅色審判" -- R for the Red icon. 審判+裁決
+	L.aura_of_devotion = "奉獻光環" -- Short for Aura of Devotion
+	L.judgement_blue = "藍色審判" -- B for the Blue icon. 審判+盾猛
+	L.aura_of_peace = "和平光環" -- Short for Aura of Peace
+	L.zaelous_spirit = "狂熱" -- Short for Zealous Spirit
 end
