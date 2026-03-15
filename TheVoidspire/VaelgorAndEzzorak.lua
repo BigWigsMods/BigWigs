d2beb1f1899bd78d80b44a34c0c5c73efa25a10d
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2026-03-15 21:45:03 +0100

    TheVoidspire/VaelgorAndEzzorak: Update enhanced timers

--- TheVoidspire/VaelgorAndEzzorak.lua
+++ TheVoidspire/VaelgorAndEzzorak.lua
@@ -26,10 +26,18 @@ mod:SetPrivateAuraSounds({
 	{1248865, 1249595, sound = "info"}, -- Radiant Barrier
 	1270497, -- Shadowmark
 })
+mod:UseCustomTimers(true)
 
 --------------------------------------------------------------------------------
 -- Locals
 --
+
+local timelineEventCount = 0
+local activeBars = {}
+local countForDuration = {}
+local stageOneCount = 1
+local stageTwoStartTime = 0
+local nextRadiantBarrier = 0
 
 local midnightFlamesCount = 1
 local nullbeamCount = 1
@@ -67,9 +75,18 @@ function mod:GetOptions()
 	}
 end
 
+function mod:OnBossEnable()
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_ADDED")
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED")
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_REMOVED")
+end
 
 function mod:OnEncounterStart()
 	self:SetStage(1)
+	countForDuration = {}
+	stageOneCount = 1
+	timelineEventCount = 0
+	activeBars = {}
 
 	midnightFlamesCount = 1
 	nullbeamCount = 1
@@ -82,12 +99,183 @@ function mod:OnEncounterStart()
 end
 
 --------------------------------------------------------------------------------
+-- Timeline Event Handlers
+--
+
+function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(_, eventInfo)
+	timelineEventCount = timelineEventCount + 1
+	local duration = eventInfo.duration
+	local rounded = math.floor(duration + 0.5)
+	local barInfo
+	local stage = self:GetStage()
+
+	if stage == 1 and rounded == 8 then -- Stage 2
+		self:SetStage(2)
+		stage = 2
+		stageTwoStartTime = GetTime()
+		if self:ShouldShowBars() then
+			self:Message("stages", "cyan", CL.stage:format(2), false)
+			self:PlaySound("stages", "long")
+			self:Bar("stages", 40, CL.stage:format(1), 1249748)
+		end
+	elseif stage == 2 and GetTime() - stageTwoStartTime > 35 then
+		self:SetStage(1)
+		if self:ShouldShowBars() then
+			self:Message("stages", "cyan", CL.stage:format(1), false)
+			self:PlaySound("stages", "long")
+		end
+		timelineEventCount = 1
+		stage = 1
+		stageOneCount = stageOneCount + 1
+		countForDuration = {}
+
+		nullbeamCount = 1
+		dreadBreathCount = 1
+		vaelwingCount = 1
+		gloomCount = 1
+		voidHowlCount = 1
+		rakfangCount = 1
+	end
+
+	if stage == 1 then
+		if timelineEventCount <= 7 then -- Initial timers
+			if stageOneCount == 1 then -- Initial Pull Timers
+				if rounded == 6 then -- Vaelwing
+					barInfo = self:Vaelwing(eventInfo)
+				elseif rounded == 11 then -- Rakfang
+					barInfo = self:Rakfang(eventInfo)
+				elseif rounded == 12 then -- Dread Breath
+					barInfo = self:DreadBreath(eventInfo)
+				elseif rounded == 15 then -- Nullbeam
+					barInfo = self:Nullbeam(eventInfo)
+				elseif rounded == 25 then -- Void Howl
+					barInfo = self:VoidHowl(eventInfo)
+				elseif rounded == 40 then -- Gloom
+					barInfo = self:Gloom(eventInfo)
+				elseif rounded == 120 then -- Radiant Barrier
+					barInfo = self:RadiantBarrier(eventInfo)
+				end
+			elseif stageOneCount == 2 then -- Round 2
+				if rounded == 19 then -- Vaelwing
+					barInfo = self:Vaelwing(eventInfo)
+				elseif rounded == 23 then -- Nullbeam
+					barInfo = self:Nullbeam(eventInfo)
+				elseif rounded == 34 then -- Dread Breath
+					barInfo = self:DreadBreath(eventInfo)
+				elseif rounded == 25 then -- Rakfang
+					barInfo = self:Rakfang(eventInfo)
+				elseif rounded == 28 then -- Gloom
+					barInfo = self:Gloom(eventInfo)
+				elseif rounded == 48 then -- Void Howl
+					barInfo = self:VoidHowl(eventInfo)
+				elseif rounded == 128 then -- Radiant Barrier
+					barInfo = self:RadiantBarrier(eventInfo)
+				end
+			elseif stageOneCount == 3 then -- Round 3
+				if rounded == 21 then -- Vaelwing
+					barInfo = self:Vaelwing(eventInfo)
+				elseif rounded == 20 then -- Nullbeam
+					barInfo = self:Nullbeam(eventInfo)
+				elseif rounded == 46 then -- Dread Breath
+					barInfo = self:DreadBreath(eventInfo)
+				elseif rounded == 27 then -- Rakfang
+					barInfo = self:Rakfang(eventInfo)
+				elseif rounded == 55 then -- Gloom
+					barInfo = self:Gloom(eventInfo)
+				elseif rounded == 15 then -- Void Howl
+					barInfo = self:VoidHowl(eventInfo)
+				elseif rounded == 125 then -- Radiant Barrier
+					barInfo = self:RadiantBarrier(eventInfo)
+				end
+			end
+		else
+			local nextBarrierBuffer = 3
+			if (GetTime() + duration) > (nextRadiantBarrier + nextBarrierBuffer) then -- Don't show timers which won't happen, small buffer incase of a delay
+				return
+			end
+			if rounded == 45 then -- Dread Breath
+				barInfo = self:DreadBreath(eventInfo)
+			elseif rounded == 25 then -- Vaelwing > Rakfang
+				countForDuration[duration] = (countForDuration[duration] or 0) + 1
+				if countForDuration[duration] % 2 == 0 then
+					barInfo = self:Rakfang(eventInfo)
+				else
+					barInfo = self:Vaelwing(eventInfo)
+				end
+			elseif rounded == 45 then -- Dread Breath or Void Howl
+				countForDuration[duration] = (countForDuration[duration] or 0) + 1
+				local breathMod = stageOneCount == 3 and 0 or 1 -- Alternate rotation in 3rd stage 1
+				if countForDuration[duration] % 2 == breathMod then
+					barInfo = self:DreadBreath(eventInfo)
+				else
+					barInfo = self:VoidHowl(eventInfo)
+				end
+			elseif rounded == 50 then -- Gloom
+				barInfo = self:Gloom(eventInfo)
+			elseif rounded == 60 then -- Nullbeam
+				barInfo = self:Nullbeam(eventInfo)
+			end
+		end
+	elseif stage == 2 then
+		-- 1st Stage 2 / Intermission
+		-- { id = 974, duration = 13}) -- Cosmosis: Void Howl
+		-- { id = 975, duration = 15}) -- Cosmosis: Null Beam
+		-- { id = 976, duration = 17}) -- Cosmosis: Dread Breath
+		-- { id = 977, duration = 19}) -- Cosmosis: Dread Breath
+		-- { id = 978, duration = 8}) -- Midnight Flames
+		-- 2nd Stage 2 / Intermission
+		-- { id = 1007, duration = 13}) -- Cosmosis: Dread Breath
+		-- { id = 1008, duration = 15}) -- Cosmosis: Gloom
+		-- { id = 1009, duration = 17}) -- Cosmosis: Dread Breath
+		-- { id = 1010, duration = 19}) -- Cosmosis: Void Howl
+		-- { id = 1011, duration = 8}) -- Midnight Flames
+
+		-- Blizzard times make no sense. We do it ourselves.
+		if rounded == 8 then -- Midnight Flames
+			barInfo = self:MidnightFlames(eventInfo)
+		end
+	end
+
+	if barInfo then
+		activeBars[eventInfo.id] = barInfo
+	elseif self:ShouldShowBars() and not self:IsWiping() then
+		self:ErrorForTimelineEvent(eventInfo)
+	end
+end
+
+function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(_, eventID)
+	local barInfo = activeBars[eventID]
+	if barInfo then
+		local state = C_EncounterTimeline.GetEventState(eventID)
+		if state == 2 or state == 3 then -- Finished or Canceled
+			self:StopBar(barInfo.msg)
+
+			if state == 2 and self:ShouldShowBars() and barInfo.callback then -- Finished
+				barInfo.callback()
+			end
+
+			activeBars[eventID] = nil
+		end
+	end
+end
+
+function mod:ENCOUNTER_TIMELINE_EVENT_REMOVED(_, eventID)
+	local barInfo = activeBars[eventID]
+	if barInfo then
+		self:StopBar(barInfo.msg)
+		activeBars[eventID] = nil
+	end
+end
+
+--------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:MidnightFlames(duration)
+function mod:MidnightFlames(eventInfo)
 	local barText = CL.count:format(CL.raid_damage, midnightFlamesCount)
-	self:CDBar(1249748, duration, barText)
+	if self:ShouldShowBars() then
+		self:CDBar(1249748, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	midnightFlamesCount = midnightFlamesCount + 1
 	return {
 		msg = barText,
@@ -99,9 +287,11 @@ function mod:MidnightFlames(duration)
 end
 
 -- Vaelgor
-function mod:Nullbeam(duration)
+function mod:Nullbeam(eventInfo)
 	local barText = CL.count:format(CL.beam, nullbeamCount)
-	self:CDBar(1262623, duration, barText)
+	if self:ShouldShowBars() then
+		self:CDBar(1262623, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	nullbeamCount = nullbeamCount + 1
 	return {
 		msg = barText,
@@ -112,9 +302,11 @@ function mod:Nullbeam(duration)
 	}
 end
 
-function mod:DreadBreath(duration)
+function mod:DreadBreath(eventInfo)
 	local barText = CL.count:format(CL.breath, dreadBreathCount)
-	self:CDBar(1244221, duration, barText)
+	if self:ShouldShowBars() then
+		self:CDBar(1244221, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	dreadBreathCount = dreadBreathCount + 1
 	return {
 		msg = barText,
@@ -125,9 +317,11 @@ function mod:DreadBreath(duration)
 	}
 end
 
-function mod:Vaelwing(duration)
+function mod:Vaelwing(eventInfo)
 	local barText = CL.count:format(self:SpellName(1265131), vaelwingCount)
-	self:CDBar(1265131, duration, barText)
+	if self:ShouldShowBars() then
+		self:CDBar(1265131, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	vaelwingCount = vaelwingCount + 1
 	return {
 		msg = barText,
@@ -141,9 +335,11 @@ function mod:Vaelwing(duration)
 end
 
 -- Ezzorak
-function mod:Gloom(duration)
+function mod:Gloom(eventInfo)
 	local barText = CL.count:format(self:SpellName(1245391), gloomCount)
-	self:CDBar(1245391, duration, barText)
+	if self:ShouldShowBars() then
+		self:CDBar(1245391, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	gloomCount = gloomCount + 1
 	return {
 		msg = barText,
@@ -154,9 +350,11 @@ function mod:Gloom(duration)
 	}
 end
 
-function mod:VoidHowl(duration)
+function mod:VoidHowl(eventInfo)
 	local barText = CL.count:format(CL.orbs, voidHowlCount)
-	self:CDBar(1244917, duration, barText)
+	if self:ShouldShowBars() then
+		self:CDBar(1244917, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	voidHowlCount = voidHowlCount + 1
 	return {
 		msg = barText,
@@ -167,9 +365,11 @@ function mod:VoidHowl(duration)
 	}
 end
 
-function mod:Rakfang(duration)
+function mod:Rakfang(eventInfo)
 	local barText = CL.count:format(self:SpellName(1245645), rakfangCount)
-	self:CDBar(1245645, duration, barText)
+	if self:ShouldShowBars() then
+		self:CDBar(1245645, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	rakfangCount = rakfangCount + 1
 	return {
 		msg = barText,
@@ -183,9 +383,12 @@ function mod:Rakfang(duration)
 end
 
 -- Lightbound Vanguard
-function mod:RadiantBarrier(duration)
+function mod:RadiantBarrier(eventInfo)
 	local barText = CL.count:format(CL.stage:format(2), radiantBarrierCount)
-	self:CDBar("stages", duration, barText, 1248847) -- Radiant Barrier icon
+	if self:ShouldShowBars() then
+		self:CDBar("stages", eventInfo.duration, barText, 1248847, eventInfo.id) -- Radiant Barrier icon
+	end
+	nextRadiantBarrier = GetTime() + eventInfo.duration
 	radiantBarrierCount = radiantBarrierCount + 1
 	return {
 		msg = barText,
