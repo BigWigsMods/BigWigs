84cc5006f76bd75916026b526d0b7a2c4fa7de3b
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2026-03-15 20:25:59 +0100

    TheVoidspire/Vorasius: Update enhanced timers

--- TheVoidspire/Vorasius.lua
+++ TheVoidspire/Vorasius.lua
@@ -15,10 +15,14 @@ mod:SetPrivateAuraSounds({
 	{1243220, 1243270, sound = "underyou"}, -- Dark Goo
 	1241844, -- Smashed
 })
+mod:UseCustomTimers(true)
 
 --------------------------------------------------------------------------------
 -- Locals
 --
+
+local activeBars = {}
+local timelineEventCount = 0
 
 local breathCount = 1
 local parasiteCount = 1
@@ -53,8 +57,16 @@ function mod:GetOptions()
 	}
 end
 
+function mod:OnBossEnable()
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_ADDED")
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED")
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_REMOVED")
+end
 
 function mod:OnEncounterStart()
+	activeBars = {}
+	timelineEventCount = 0
+
 	breathCount = 1
 	parasiteCount = 1
 	frenzyCount = 1
@@ -62,12 +74,78 @@ function mod:OnEncounterStart()
 end
 
 --------------------------------------------------------------------------------
+-- Timeline Event Handlers
+--
+
+do
+	local lastSharedCD = 0
+	function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(_, eventInfo)
+		local duration = eventInfo.duration
+		local barInfo
+		timelineEventCount = timelineEventCount + 1
+		if timelineEventCount <= 5 then -- Pull Events
+			lastSharedCD = 0
+			if duration == 6 then -- Primordial Roar
+				barInfo = self:PrimordialRoar(eventInfo)
+			elseif duration == 16 or duration == 136 then -- Smashing Frenzy
+				barInfo = self:SmashingFrenzy(eventInfo)
+			elseif duration == 57 then -- Parasite Expulsion
+				barInfo = self:ParasiteExpulsion(eventInfo)
+			elseif duration == 95 then -- Void Breath
+				barInfo = self:VoidBreath(eventInfo)
+			end
+		else
+			if duration == 240 then -- Smashing Frenzy
+				barInfo = self:SmashingFrenzy(eventInfo)
+			elseif duration == 120 and lastSharedCD == 0 then -- Roar (alternates)
+				barInfo = self:PrimordialRoar(eventInfo)
+				lastSharedCD = 1
+			elseif duration == 120 and lastSharedCD == 1 then -- Breath (alternates)
+				barInfo = self:VoidBreath(eventInfo)
+				lastSharedCD = 0
+			elseif duration == 122.5 then -- Parasite Expulsion
+				barInfo = self:ParasiteExpulsion(eventInfo)
+			end
+		end
+		if barInfo then
+			activeBars[eventInfo.id] = barInfo
+		elseif self:ShouldShowBars() and not self:IsWiping() then
+			self:ErrorForTimelineEvent(eventInfo)
+		end
+	end
+end
+
+function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(_, eventID)
+	local barInfo = activeBars[eventID]
+	if barInfo then
+		local state = C_EncounterTimeline.GetEventState(eventID)
+		self:StopBar(barInfo.msg)
+
+		if state == 2 and self:ShouldShowBars() and barInfo.callback then -- Finished
+			barInfo.callback()
+		end
+
+		activeBars[eventID] = nil
+	end
+end
+
+function mod:ENCOUNTER_TIMELINE_EVENT_REMOVED(_, eventID)
+	local barInfo = activeBars[eventID]
+	if barInfo then
+		self:StopBar(barInfo.msg)
+		activeBars[eventID] = nil
+	end
+end
+
+--------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:VoidBreath(duration)
+function mod:VoidBreath(eventInfo)
 	local barText = CL.count:format(CL.breath, breathCount)
-	self:Bar(1256855, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1256855, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	breathCount = breathCount + 1
 	return {
 		msg = barText,
@@ -79,9 +157,11 @@ function mod:VoidBreath(duration)
 	}
 end
 
-function mod:ParasiteExpulsion(duration)
+function mod:ParasiteExpulsion(eventInfo)
 	local barText = CL.count:format(CL.adds, parasiteCount)
-	self:Bar(1254199, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1254199, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	parasiteCount = parasiteCount + 1
 	return {
 		msg = barText,
@@ -93,9 +173,12 @@ function mod:ParasiteExpulsion(duration)
 	}
 end
 
-function mod:SmashingFrenzy(duration)
+function mod:SmashingFrenzy(eventInfo)
+	local count = eventInfo.duration == 136 and frenzyCount + 1 or frenzyCount -- it starts 2 bars from the start.
 	local barText = CL.count:format(L.shadowclaw_slam, frenzyCount)
-	self:Bar(1241692, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1241692, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	frenzyCount = frenzyCount + 1
 	return {
 		msg = barText,
@@ -107,9 +190,11 @@ function mod:SmashingFrenzy(duration)
 	}
 end
 
-function mod:PrimordialRoar(duration)
+function mod:PrimordialRoar(eventInfo)
 	local barText = CL.count:format(CL.roar, roarCount)
-	self:Bar(1260052, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1260052, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	roarCount = roarCount + 1
 	return {
 		msg = barText,
