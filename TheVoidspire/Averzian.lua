0cfd810be96fa6e71b68efacc5e81697fa23fb07
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2026-03-15 19:32:37 +0100

    TheVoidspire/Averzian: Update enhanced timers

--- TheVoidspire/Averzian.lua
+++ TheVoidspire/Averzian.lua
@@ -7,7 +7,7 @@ local mod, CL = BigWigs:NewBoss("Imperator Averzian", 2912, 2733)
 if not mod then return end
 mod:RegisterEnableMob(240435) -- Imperator Averzian
 mod:SetEncounterID(3176)
-mod:SetRespawnTime(30)
+-- mod:SetRespawnTime(30)
 mod:SetPrivateAuraSounds({
 	{1255680, sound = "alarm"}, -- Gnashing Void
 	{1275059, sound = "alert"}, -- Black Miasma
@@ -20,10 +20,14 @@ mod:SetPrivateAuraSounds({
 	{1265540, sound = "alarm"}, -- Blackening Wounds
 	1283069, -- Weakened
 })
+mod:UseCustomTimers(true)
 
 --------------------------------------------------------------------------------
 -- Locals
 --
+
+local activeBars = {}
+local durationEventCount = {}
 
 local shadowAdvanceCount = 1
 local umbralCollapseCount = 1
@@ -56,7 +60,16 @@ function mod:GetOptions()
 	}
 end
 
+function mod:OnBossEnable()
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_ADDED")
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED")
+	self:RegisterEvent("ENCOUNTER_TIMELINE_EVENT_REMOVED")
+end
+
 function mod:OnEncounterStart()
+	activeBars = {}
+	durationEventCount = {}
+
 	shadowAdvanceCount = 1
 	umbralCollapseCount = 1
 	voidMarkedCount = 1
@@ -66,12 +79,84 @@ function mod:OnEncounterStart()
 end
 
 --------------------------------------------------------------------------------
+-- Timeline Event Handlers
+--
+
+function mod:ENCOUNTER_TIMELINE_EVENT_ADDED(_, eventInfo)
+	local duration = eventInfo.duration
+	local barInfo
+	if duration == 160 then -- Void Fall
+		barInfo = self:VoidFall(eventInfo)
+		-- This starts a new rotation, reset counters
+		durationEventCount = {}
+	elseif duration == 94 or duration == 14 then -- Shadow's Advance
+		barInfo = self:ShadowsAdvance(eventInfo)
+	elseif duration == 20 then -- Void Marked
+		barInfo = self:VoidMarked(eventInfo)
+	elseif duration == 4 or duration == 48 or duration == 36 then -- Dark Upheaval
+		barInfo = self:DarkUpheaval(eventInfo)
+	elseif duration == 32 then -- Umbral Collapse
+		barInfo = self:UmbralCollapse(eventInfo)
+	elseif duration == 60 or duration == 18 then -- Oblivion's Wrath
+		barInfo = self:OblivionsWrath(eventInfo)
+	elseif duration == 80 then -- Void Marked, Umbral Collapse, Shadow's Advance (after first stage)
+		durationEventCount[duration] = (durationEventCount[duration] or 0) + 1
+		if (voidFallCount == 2 and durationEventCount[duration] == 1)
+		or (voidFallCount >= 3 and durationEventCount[duration] == 2) then -- Void Marked
+			barInfo = self:VoidMarked(eventInfo)
+		elseif (voidFallCount == 2 and durationEventCount[duration] == 2)
+		or (voidFallCount >= 3 and durationEventCount[duration] == 3) then -- Umbral Collapse
+			barInfo = self:UmbralCollapse(eventInfo)
+		elseif voidFallCount >= 3 and durationEventCount[duration] == 1 then -- Shadow's Advance
+			barInfo = self:ShadowsAdvance(eventInfo)
+		end
+	end
+	if barInfo then
+		activeBars[eventInfo.id] = barInfo
+	elseif self:ShouldShowBars() and not self:IsWiping() then
+		self:ErrorForTimelineEvent(eventInfo)
+	end
+end
+
+function mod:ENCOUNTER_TIMELINE_EVENT_STATE_CHANGED(_, eventID)
+	local barInfo = activeBars[eventID]
+	if barInfo then
+		local state = C_EncounterTimeline.GetEventState(eventID)
+		self:StopBar(barInfo.msg)
+
+		if state == 2 and self:ShouldShowBars() and barInfo.callback then -- Finished
+			barInfo.callback()
+		end
+
+		activeBars[eventID] = nil
+	end
+end
+
+function mod:ENCOUNTER_TIMELINE_EVENT_REMOVED(_, eventID)
+	local barInfo = activeBars[eventID]
+	if barInfo then
+		self:StopBar(barInfo.msg)
+		activeBars[eventID] = nil
+	end
+end
+
+--------------------------------------------------------------------------------
 -- Event Handlers
 --
 
-function mod:ShadowsAdvance(duration)
+function mod:ShadowsAdvance(eventInfo)
+	local count = shadowAdvanceCount
+	-- 82 and 10 appear on pull - where 82 should be bar number 2 and 10 should be bar number 1
+	-- 10 also apears every reset
+	if eventInfo.duration == 82 then
+		count = 2
+	elseif eventInfo.duration == 10 and shadowAdvanceCount <= 1 then
+		count = 1
+	end
 	local barText = CL.count:format(CL.adds, shadowAdvanceCount)
-	self:Bar(1251361, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1251361, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	shadowAdvanceCount = shadowAdvanceCount + 1
 	return {
 		msg = barText,
@@ -83,9 +168,11 @@ function mod:ShadowsAdvance(duration)
 	}
 end
 
-function mod:UmbralCollapse(duration)
+function mod:UmbralCollapse(eventInfo)
 	local barText = CL.count:format(CL.soak, umbralCollapseCount)
-	self:Bar(1249262, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1249262, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	umbralCollapseCount = umbralCollapseCount + 1
 	return {
 		msg = barText,
@@ -97,9 +184,11 @@ function mod:UmbralCollapse(duration)
 	}
 end
 
-function mod:VoidMarked(duration)
+function mod:VoidMarked(eventInfo)
 	local barText = CL.count:format(CL.marks, voidMarkedCount)
-	self:Bar(1280015, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1280015, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	voidMarkedCount = voidMarkedCount + 1
 	return {
 		msg = barText,
@@ -111,9 +200,11 @@ function mod:VoidMarked(duration)
 	}
 end
 
-function mod:OblivionsWrath(duration)
+function mod:OblivionsWrath(eventInfo)
 	local barText = CL.count:format(CL.dodge, oblivionsWrathCount)
-	self:Bar(1260712, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1260712, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	oblivionsWrathCount = oblivionsWrathCount + 1
 	return {
 		msg = barText,
@@ -125,9 +216,11 @@ function mod:OblivionsWrath(duration)
 	}
 end
 
-function mod:VoidFall(duration)
+function mod:VoidFall(eventInfo)
 	local barText = CL.count:format(CL.knockback, voidFallCount)
-	self:Bar(1258883, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1258883, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	voidFallCount = voidFallCount + 1
 	return {
 		msg = barText,
@@ -139,9 +232,11 @@ function mod:VoidFall(duration)
 	}
 end
 
-function mod:DarkUpheaval(duration)
+function mod:DarkUpheaval(eventInfo)
 	local barText = CL.count:format(CL.raid_damage, darkUpheavalCount)
-	self:Bar(1249251, duration, barText)
+	if self:ShouldShowBars() then
+		self:Bar(1249251, eventInfo.duration, barText, nil, eventInfo.id)
+	end
 	darkUpheavalCount = darkUpheavalCount + 1
 	return {
 		msg = barText,
