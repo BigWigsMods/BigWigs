ca4141cceb84344a4678df6b12b6ee88f9bbf6b6
funkydude <funkydude@users.noreply.github.com>
2026-03-25 14:30:15 +0000

    Bump version

--- Loader.lua
+++ Loader.lua
@@ -14,7 +14,7 @@ local strfind = string.find
 
 local BIGWIGS_VERSION = 411
 local CONTENT_PACK_VERSIONS = {
-	["LittleWigs"] = {12, 0, 27},
+	["LittleWigs"] = {12, 0, 28},
 	["BigWigs_Classic"] = {12, 0, 11},
 	["BigWigs_BurningCrusade"] = {12, 0, 9},
 	["BigWigs_WrathOfTheLichKing"] = {12, 0, 3},
@@ -1616,9 +1616,9 @@ end
 --
 
 do
-	local DBMdotRevision = "20260316213103" -- The changing version of the local client, changes with every new zip using the project-date-integer packager replacement.
-	local DBMdotDisplayVersion = "12.0.31" -- "N.N.N" for a release and "N.N.N alpha" for the alpha duration.
-	local DBMdotReleaseRevision = "20260316000000" -- Hardcoded time, manually changed every release, they use it to track the highest release version, a new DBM release is the only time it will change.
+	local DBMdotRevision = "20260325092947" -- The changing version of the local client, changes with every new zip using the project-date-integer packager replacement.
+	local DBMdotDisplayVersion = "12.0.33" -- "N.N.N" for a release and "N.N.N alpha" for the alpha duration.
+	local DBMdotReleaseRevision = "20260324000000" -- Hardcoded time, manually changed every release, they use it to track the highest release version, a new DBM release is the only time it will change.
 	local protocol = 3
 	local versionPrefix = "V"
 	local PForceDisable = 23
