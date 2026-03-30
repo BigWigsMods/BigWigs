5b0e3d755254bbfb4024fb5a4fa7c9fe7906b827
007bbb <86834094+007bbb@users.noreply.github.com>
2026-03-30 20:51:01 +0900

    Update koKR (#2460)

--- TheVoidspire/Locales/koKR.lua
+++ TheVoidspire/Locales/koKR.lua
@@ -17,27 +17,27 @@ end
 L = BigWigs:NewBossLocale("Lightblinded Vanguard", "koKR")
 if L then
 	L.aura_of_wrath = "진노" -- Short for Aura of Wrath
-	--L.executes_mythic = "Executes + Dodge"
+	L.executes_mythic = "선고 + 피하기"
 	L.execution_sentence = "선고" -- Short for Execution Sentence
 	L.judgement_red = "심판 [빨]" -- R for the Red icon.
 	L.aura_of_devotion = "헌오" -- Short for Aura of Devotion
 	L.judgement_blue = "심판 [파]" -- B for the Blue icon.
 	L.aura_of_peace = "바닥" -- Short for Aura of Peace
-	--L.tyrs_wrath_mythic = "Absorbs + Executes"
-	--L.divine_toll_mythic = "Dodge + Absorbs"
+	L.tyrs_wrath_mythic = "보호막 + 선고"
+	L.divine_toll_mythic = "피하기 + 보호막"
 	L.zealous_spirit = "영혼" -- Short for Zealous Spirit
 
-	--L.empowered_searing_radiance = "Empowered Searing Radiance"
-	--L.empowered_searing_radiance_desc = "Show the timer for the empowered Searing Radiance"
+	L.empowered_searing_radiance = "강화된 이글거리는 광휘"
+	L.empowered_searing_radiance_desc = "강화된 이글거리는 광휘 타이머 표시"
 
-	--L.empowered_avengers_shield = "Empowered Avenger's Shield"
-	--L.empowered_avengers_shield_desc = "Show the timer for the empowered Avenger's Shield"
+	L.empowered_avengers_shield = "강화된 응징의 방패"
+	L.empowered_avengers_shield_desc = "강화된 응징의 방패 타이머 표시"
 
-	--L.empowered_divine_storm = "Empowered Divine Storm"
-	--L.empowered_divine_storm_desc = "Show the timer for the empowered Divine Storm"
-	--L.tornadoes = "Tornadoes" -- The renamed empowered Divine Storm
+	L.empowered_divine_storm = "강화된 천상의 폭풍"
+	L.empowered_divine_storm_desc = "강화된 천상의 폭풍 타이머 표시"
+	L.tornadoes = "토네이도" -- The renamed empowered Divine Storm
 
-	--L.empowered = "[E] %s" -- Empowered version of an ability, [E] Avengers Shield
+	L.empowered = "[강화] %s" -- Empowered version of an ability, [E] Avengers Shield
 end
 
 L = BigWigs:NewBossLocale("Crown of the Cosmos", "koKR")
