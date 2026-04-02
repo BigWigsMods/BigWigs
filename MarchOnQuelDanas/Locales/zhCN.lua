3c6d04cecd2f8ee7a83ef5626662073e808c4aaa
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-04-02 18:19:25 +0800

    Update zhCN.lua

--- MarchOnQuelDanas/Locales/zhCN.lua
+++ MarchOnQuelDanas/Locales/zhCN.lua
@@ -1,16 +1,16 @@
 local L = BigWigs:NewBossLocale("Belo'ren, Child of Al'ar", "zhCN")
 if not L then return end
 if L then
-	--L.infused_quills = "Quills"
-	--L.voidlight_convergence = "Color Swaps"
-	--L.light_void_dive = "Light/Void Dive"
+	L.infused_quills = "注能飞羽"
+	L.voidlight_convergence = "颜色转换"
+	L.light_void_dive = "圣光/虚空俯冲"
 end
 
 L = BigWigs:NewBossLocale("Midnight Falls", "zhCN")
 if L then
-	--L.deaths_dirge = "Memory Game"
-	--L.heavens_glaives = "Glaives"
-	--L.heavens_lance = "Lance"
-	--L.dark_constellation = "Lines"
-	--L.the_dark_archangel = "Archangel"
+	L.deaths_dirge = "记忆游戏"
+	L.heavens_glaives = "战刃"
+	L.heavens_lance = "天穹枪"
+	L.dark_constellation = "线"
+	L.the_dark_archangel = "天使长"
 end
