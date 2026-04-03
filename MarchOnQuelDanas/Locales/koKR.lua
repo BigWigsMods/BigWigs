109ca058bc3a0b1b38e7909545be36433e44d110
007bbb <86834094+007bbb@users.noreply.github.com>
2026-04-03 09:01:51 +0900

    Update koKR (#2471)

--- MarchOnQuelDanas/Locales/koKR.lua
+++ MarchOnQuelDanas/Locales/koKR.lua
@@ -1,16 +1,16 @@
 local L = BigWigs:NewBossLocale("Belo'ren, Child of Al'ar", "koKR")
 if not L then return end
 if L then
-	--L.infused_quills = "Quills"
-	--L.voidlight_convergence = "Color Swaps"
-	--L.light_void_dive = "Light/Void Dive"
+	L.infused_quills = "레이저"
+	L.voidlight_convergence = "색상 변경"
+	L.light_void_dive = "빛/공허 바닥"
 end
 
 L = BigWigs:NewBossLocale("Midnight Falls", "koKR")
 if L then
-	--L.deaths_dirge = "Memory Game"
-	--L.heavens_glaives = "Glaives"
-	--L.heavens_lance = "Lance"
-	--L.dark_constellation = "Lines"
-	--L.the_dark_archangel = "Archangel"
+	L.deaths_dirge = "음표"
+	L.heavens_glaives = "칼날"
+	L.heavens_lance = "찌르기"
+	L.dark_constellation = "라인"
+	L.the_dark_archangel = "바닥"
 end
