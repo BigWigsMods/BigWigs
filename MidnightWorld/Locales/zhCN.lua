739f033eced7a13707b83df185e583324329507f
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-04-05 00:54:18 +0800

    Update zhCN (#2474)

--- MidnightWorld/Locales/zhCN.lua
+++ MidnightWorld/Locales/zhCN.lua
@@ -1,9 +1,9 @@
 local L = BigWigs:NewBossLocale("Thorm'belan", "zhCN")
 if not L then return end
 if L then
-	--L.ball = "Ball"
-	--L.ball_incoming = "Ball Incoming - Don't let it touch the ground"
-	--L.ball_fail = "FAIL - Ball touched the ground"
-	--L.tendrils = "Tendrils"
-	--L.tendrils_incoming = "RUN AWAY to snap tendrils"
+	L.ball = "球"
+	L.ball_incoming = "球来了 - 不要让他落地"
+	L.ball_fail = "失败 - 球落地了"
+	L.tendrils = "藤蔓"
+	L.tendrils_incoming = "快跑开，拉断藤蔓"
 end
