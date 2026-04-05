57aace0912c280cb475993ae12ee5cd5e905e644
007bbb <86834094+007bbb@users.noreply.github.com>
2026-04-05 23:49:39 +0900

    Update koKR.lua

--- MidnightWorld/Locales/koKR.lua
+++ MidnightWorld/Locales/koKR.lua
@@ -1,9 +1,9 @@
 local L = BigWigs:NewBossLocale("Thorm'belan", "koKR")
 if not L then return end
 if L then
-	--L.ball = "Ball"
-	--L.ball_incoming = "Ball Incoming - Don't let it touch the ground"
-	--L.ball_fail = "FAIL - Ball touched the ground"
-	--L.tendrils = "Tendrils"
-	--L.tendrils_incoming = "RUN AWAY to snap tendrils"
+	L.ball = "공"
+	L.ball_incoming = "공이 다가옵니다 - 땅에 닿지 않게 하세요"
+	L.ball_fail = "실패 - 공이 땅에 닿았습니다"
+	L.tendrils = "덩굴손"
+	L.tendrils_incoming = "덩굴손을 끊으려면 빨리 도망가세요"
 end
