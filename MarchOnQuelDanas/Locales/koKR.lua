7d7d3fb25361eae0b6e29abfe3dd769709ad48b2
007bbb <86834094+007bbb@users.noreply.github.com>
2026-04-08 18:25:52 +0900

    Update koKR.lua

--- MarchOnQuelDanas/Locales/koKR.lua
+++ MarchOnQuelDanas/Locales/koKR.lua
@@ -8,23 +8,23 @@ end
 
 L = BigWigs:NewBossLocale("Midnight Falls", "koKR")
 if L then
-	L.deaths_dirge = "음표"
+	L.deaths_dirge = "음표 게임"
 	L.heavens_glaives = "칼날"
 	L.heavens_lance = "찌르기"
-	--L.the_dark_archangel = "Big Boom"
-	--L.prism_kicks = "Kicks"
-	--L.dark_constellation = "Stars"
-	--L.dark_rune = "Memory Mark"
-	--L.dark_rune_bar = "Solve the Game"
+	L.the_dark_archangel = "대천사"
+	L.prism_kicks = "차단"
+	L.dark_constellation = "별자리"
+	L.dark_rune = "음표"
+	L.dark_rune_bar = "음표 게임"
 
-	--L.starsplinter = "Blazes" -- Mythic intermission and P4 bar text
-	--L.starsplinter_you = "Blaze"
+	L.starsplinter = "파열" -- Mythic intermission and P4 bar text
+	L.starsplinter_you = "파열"
 
-	--L.left = "[L] %s" -- left/west group bars in p3
-	--L.right = "[R] %s" -- right/east group bars in p3
+	L.left = "[L] %s" -- left/west group bars in p3
+	L.right = "[R] %s" -- right/east group bars in p3
 
-	--L.custom_off_select_limit_warnings = "Mythic Stage Three Group"
-	--L.custom_off_select_limit_warnings_desc = "When set, only warnings for abilities on your side of the room will be shown."
-	--L.custom_off_select_limit_warnings_value1 = "West/Left"
-	--L.custom_off_select_limit_warnings_value2 = "East/Right"
+	L.custom_off_select_limit_warnings = "신화 3단계 그룹"
+	L.custom_off_select_limit_warnings_desc = "이 설정을 적용하면, 자신이 속한 스킬에 대한 경고만 표시됩니다."
+	L.custom_off_select_limit_warnings_value1 = "서쪽/왼쪽"
+	L.custom_off_select_limit_warnings_value2 = "동쪽/오른쪽"
 end
