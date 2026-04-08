1025b0f8f20b54cd901aa05d7761b4d829933e03
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-04-09 07:55:33 +0800

    Update zhCN (#2484)

--- MarchOnQuelDanas/Locales/zhCN.lua
+++ MarchOnQuelDanas/Locales/zhCN.lua
@@ -11,20 +11,20 @@ if L then
 	L.deaths_dirge = "记忆游戏"
 	L.heavens_glaives = "战刃"
 	L.heavens_lance = "天穹枪"
-	--L.the_dark_archangel = "Big Boom"
-	--L.prism_kicks = "Kicks"
-	--L.dark_constellation = "Stars"
-	--L.dark_rune = "Memory Mark"
-	--L.dark_rune_bar = "Solve the Game"
+	L.the_dark_archangel = "大爆炸"
+	L.prism_kicks = "打断"
+	L.dark_constellation = "星座"
+	L.dark_rune = "记忆符号"
+	L.dark_rune_bar = "解密"
 
-	--L.starsplinter = "Blazes" -- Mythic intermission and P4 bar text
-	--L.starsplinter_you = "Blaze"
+	L.starsplinter = "星辰裂片" -- Mythic intermission and P4 bar text
+	L.starsplinter_you = "星辰裂片"
 
-	--L.left = "[L] %s" -- left/west group bars in p3
-	--L.right = "[R] %s" -- right/east group bars in p3
+	L.left = "[左] %s" -- left/west group bars in p3
+	L.right = "[右] %s" -- right/east group bars in p3
 
-	--L.custom_off_select_limit_warnings = "Mythic Stage Three Group"
-	--L.custom_off_select_limit_warnings_desc = "When set, only warnings for abilities on your side of the room will be shown."
-	--L.custom_off_select_limit_warnings_value1 = "West/Left"
-	--L.custom_off_select_limit_warnings_value2 = "East/Right"
+	L.custom_off_select_limit_warnings = "三阶段（史诗）分组"
+	L.custom_off_select_limit_warnings_desc = "开启后，仅显示你所在半场技能的警告。"
+	L.custom_off_select_limit_warnings_value1 = "西/左"
+	L.custom_off_select_limit_warnings_value2 = "东/右"
 end
