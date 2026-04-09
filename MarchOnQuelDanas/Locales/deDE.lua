2ebe9fcef531583fd995be519cae65a9b24309e4
Dynaletik <43783074+Dynaletik@users.noreply.github.com>
2026-04-09 23:05:03 +0200

    Update deDE

--- MarchOnQuelDanas/Locales/deDE.lua
+++ MarchOnQuelDanas/Locales/deDE.lua
@@ -11,20 +11,20 @@ if L then
 	L.deaths_dirge = "Memory Spiel"
 	L.heavens_glaives = "Gleven"
 	L.heavens_lance = "Lanze"
-	--L.the_dark_archangel = "Big Boom"
-	--L.prism_kicks = "Kicks"
-	--L.dark_constellation = "Stars"
-	--L.dark_rune = "Memory Mark"
-	--L.dark_rune_bar = "Solve the Game"
+	L.the_dark_archangel = "Großer Knall"
+	L.prism_kicks = "Unterbrechungen"
+	L.dark_constellation = "Sterne"
+	L.dark_rune = "Memory Markierung"
+	L.dark_rune_bar = "Löse das Spiel"
 
-	--L.starsplinter = "Blazes" -- Mythic intermission and P4 bar text
-	--L.starsplinter_you = "Blaze"
+	L.starsplinter = "Brände" -- Mythic intermission and P4 bar text
+	L.starsplinter_you = "Brand"
 
-	--L.left = "[L] %s" -- left/west group bars in p3
-	--L.right = "[R] %s" -- right/east group bars in p3
+	L.left = "[L] %s" -- left/west group bars in p3
+	L.right = "[R] %s" -- right/east group bars in p3
 
-	--L.custom_off_select_limit_warnings = "Mythic Stage Three Group"
-	--L.custom_off_select_limit_warnings_desc = "When set, only warnings for abilities on your side of the room will be shown."
-	--L.custom_off_select_limit_warnings_value1 = "West/Left"
-	--L.custom_off_select_limit_warnings_value2 = "East/Right"
+	L.custom_off_select_limit_warnings = "Mythisch Phase Drei Gruppe"
+	L.custom_off_select_limit_warnings_desc = "Bei Aktivierung werden nur Warnungen für Fähigkeiten auf Deiner Seite des Raums angezeigt."
+	L.custom_off_select_limit_warnings_value1 = "Westen/Links"
+	L.custom_off_select_limit_warnings_value2 = "Osten/Rechts"
 end
