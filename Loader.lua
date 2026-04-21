010a7bb1471f60f645ae26ef0c373a603d1f3bd0
funkydude <funkydude@users.noreply.github.com>
2026-04-21 21:54:51 +0100

    Bump version

--- Loader.lua
+++ Loader.lua
@@ -12,9 +12,9 @@ local strfind = string.find
 -- Generate our version variables
 --
 
-local BIGWIGS_VERSION = 413
+local BIGWIGS_VERSION = 414
 local CONTENT_PACK_VERSIONS = {
-	["LittleWigs"] = {12, 0, 40},
+	["LittleWigs"] = {12, 0, 41},
 	["BigWigs_Classic"] = {12, 0, 15},
 	["BigWigs_BurningCrusade"] = {12, 0, 11},
 	["BigWigs_WrathOfTheLichKing"] = {12, 0, 6},
@@ -709,6 +709,7 @@ do
 		frame:SetBorder("HeldBagLayout")
 		frame:SetPortraitTextureSizeAndOffset(38, -5, 0)
 		frame:SetPortraitTextureRaw("Interface\\AddOns\\BigWigs\\Media\\Icons\\minimap_raid.tga")
+		frame.GetTitleText = nil
 
 		local text = frame:CreateFontString(nil, nil, "GameFontGreenLarge")
 		text:SetSize(380, 0)
@@ -1337,6 +1338,7 @@ do
 		BigWigs_SiegeOfZuldazar = "BigWigs",
 		FS_Core = "Abandoned", -- abandoned addon breaking the load order
 		Fake_Keystones = "Abandoned", -- abandoned addon breaking LibKeystone
+		["!Fake_Keystones"] = "Abandoned", -- abandoned addon breaking LibKeystone
 		BigWigs_Azeroth = "BigWigs_BattleForAzeroth",
 		BigWigs_BattleOfDazaralor = "BigWigs_BattleForAzeroth",
 		BigWigs_CrucibleOfStorms = "BigWigs_BattleForAzeroth",
