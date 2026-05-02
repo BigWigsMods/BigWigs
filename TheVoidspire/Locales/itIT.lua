a43d55a8617bf13053b01ae1e29ab2f6c4d2f2b3
skyofgems <lorenzopirovani@icloud.com>
2026-05-02 19:51:50 +0200

    Update itIT.lua

--- TheVoidspire/Locales/itIT.lua
+++ TheVoidspire/Locales/itIT.lua
@@ -1,55 +1,55 @@
 local L = BigWigs:NewBossLocale("Vorasius", "itIT")
 if not L then return end
 if L then
-	--L.shadowclaw_slam = "Slams"
+	L.shadowclaw_slam = "Schianto"
 end
 
 L = BigWigs:NewBossLocale("Fallen-King Salhadaar", "itIT")
 if L then
-	--L.fractured_projection = "Kicks"
+	L.fractured_projection = "Interruzioni"
 end
 
 L = BigWigs:NewBossLocale("Vaelgor & Ezzorak", "itIT")
 if L then
-	--L.grappling_maw = "Tank Grip"
+	L.grappling_maw = "Presa Tank"
 end
 
 L = BigWigs:NewBossLocale("Lightblinded Vanguard", "itIT")
 if L then
-	--L.aura_of_wrath = "Wrath" -- Short for Aura of Wrath
-	--L.execution_sentence = "Executes" -- Short for Execution Sentence
-	--L.executes_mythic = "Executes + Dodge"
-	--L.judgement_red = "Judgement [R]" -- R for the Red icon.
-	--L.aura_of_devotion = "Devotion" -- Short for Aura of Devotion
-	--L.judgement_blue = "Judgement [B]" -- B for the Blue icon.
-	--L.aura_of_peace = "Peace" -- Short for Aura of Peace
-	--L.tyrs_wrath_mythic = "Absorbs + Executes"
-	--L.divine_toll_mythic = "Dodge + Absorbs"
-	--L.zealous_spirit = "Spirit" -- Short for Zealous Spirit
+	L.aura_of_wrath = "Ira" -- Short for Aura of Wrath
+	L.execution_sentence = "Esecuzione" -- Short for Execution Sentence
+	L.executes_mythic = "Esecuzioni + Schiva"
+	L.judgement_red = "Giudizio [R]" -- R for the Red icon.
+	L.aura_of_devotion = "Devozione" -- Short for Aura of Devotion
+	L.judgement_blue = "Giudizio [B]" -- B for the Blue icon.
+	L.aura_of_peace = "Pace" -- Short for Aura of Peace
+	L.tyrs_wrath_mythic = "Assorbimenti + Esecuzioni"
+	L.divine_toll_mythic = "Schiva + Assorbimenti"
+	L.zealous_spirit = "Spirito" -- Short for Zealous Spirit
 
-	--L.empowered_searing_radiance = "Empowered Searing Radiance"
-	--L.empowered_searing_radiance_desc = "Show the timer for the empowered Searing Radiance"
+	L.empowered_searing_radiance = "Radiosità Rovente potenziata"
+	L.empowered_searing_radiance_desc = "Mostra il timer per la Radiosità Rovente Potenziata"
 
-	--L.empowered_avengers_shield = "Empowered Avenger's Shield"
-	--L.empowered_avengers_shield_desc = "Show the timer for the empowered Avenger's Shield"
+	L.empowered_avengers_shield = "Scudo del Vendicatore potenziato"
+	L.empowered_avengers_shield_desc = "Mostra il timer per lo Scudo del Vendicatore potenziato"
 
-	--L.empowered_divine_storm = "Empowered Divine Storm"
-	--L.empowered_divine_storm_desc = "Show the timer for the empowered Divine Storm"
-	--L.tornadoes = "Tornadoes" -- The renamed empowered Divine Storm
+	L.empowered_divine_storm = "Tempesta Divina potenziata"
+	L.empowered_divine_storm_desc = "Mostra il timer per la Tempesta Divina potenziata"
+	L.tornadoes = "Tornadi" -- The renamed empowered Divine Storm
 
-	--L.empowered = "[E] %s" -- Empowered version of an ability, [E] Avengers Shield
+	L.empowered = "[P] %s" -- Empowered version of an ability, [E] Avengers Shield
 end
 
 L = BigWigs:NewBossLocale("Crown of the Cosmos", "itIT")
 if L then
-	--L.silverstrike_arrow = "Arrows"
-	--L.grasp_of_emptiness = "Obelisks"
-	--L.interrupting_tremor = "Interrupt"
-	--L.ravenous_abyss = "Move Out"
-	--L.silverstrike_barrage = "Lines"
-	--L.cosmic_barrier = "Barrier"
-	--L.rangers_captains_mark = "Arrows"
-	--L.voidstalker_sting = "Stings"
-	--L.aspect_of_the_end = "Tethers"
-	--L.devouring_cosmos = "Next Platform"
+	L.silverstrike_arrow = "Frecce"
+	L.grasp_of_emptiness = "Obelischi"
+	L.interrupting_tremor = "Interruzione"
+	L.ravenous_abyss = "Uscire"
+	L.silverstrike_barrage = "Linee"
+	L.cosmic_barrier = "Barriera"
+	L.rangers_captains_mark = "Frecce"
+	L.voidstalker_sting = "Punture"
+	L.aspect_of_the_end = "Catene"
+	L.devouring_cosmos = "Prossima Piattaforma"
 end
