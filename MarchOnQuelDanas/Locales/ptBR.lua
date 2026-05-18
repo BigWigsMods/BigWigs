5c41c83fd02985fc16a1168015a4c9e851f45bac
Renan Destri Borges <15971938+hyfronx@users.noreply.github.com>
2026-05-18 18:48:45 -0300

    Update ptBR (#2561)

--- MarchOnQuelDanas/Locales/ptBR.lua
+++ MarchOnQuelDanas/Locales/ptBR.lua
@@ -1,27 +1,27 @@
 if not BigWigsAPI.IsLocale("ptBR") then return end
 BigWigsAPI.SetBossModuleLocale("Belo'ren, Child of Al'ar", {
-	--infused_quills = "Quills",
-	--voidlight_convergence = "Color Swaps",
-	--light_void_dive = "Light/Void Dive",
+	infused_quills = "Cálamos",
+	voidlight_convergence = "Troca de cores",
+	light_void_dive = "Mergulho Luz/Caos",
 })
 
 BigWigsAPI.SetBossModuleLocale("Midnight Falls", {
-	--deaths_dirge = "Memory Game",
-	--heavens_glaives = "Glaives",
-	--heavens_lance = "Lance",
-	--the_dark_archangel = "Big Boom",
-	--prism_kicks = "Kicks",
-	--dark_constellation = "Stars",
-	--dark_rune_bar = "Solve the Game",
+	deaths_dirge = "Jogo da Memória",
+	heavens_glaives = "Glaives",
+	heavens_lance = "Lança",
+	the_dark_archangel = "Grande Explosão",
+	prism_kicks = "Cortes",
+	dark_constellation = "Estrelas",
+	dark_rune_bar = "Resolva o Enigma",
 
-	--left = "[L] %s", -- left/west group bars in p3
-	--right = "[R] %s", -- right/east group bars in p3
+	left = "[E] %s", -- left/west group bars in p3
+	right = "[D] %s", -- right/east group bars in p3
 
-	--custom_select_limit_warnings = "[Mythic] Restrict Stage 3 Warnings",
-	--custom_select_limit_warnings_desc = "Only show warnings for abilities on your side.",
-	--custom_select_limit_warnings_value1 = "Groups 1 & 2 go left, groups 3 & 4 go right.",
-	--custom_select_limit_warnings_value2 = "Odd groups left, even groups right.",
-	--custom_select_limit_warnings_value3 = "Show warnings for both sides.",
-	--custom_select_limit_warnings_value4 = "Show warnings for left side only.",
-	--custom_select_limit_warnings_value5 = "Show warnings for right side only.",
+	custom_select_limit_warnings = "[Mítico] Restringir Avisos de Estágio 3",
+	custom_select_limit_warnings_desc = "Exibir avisos apenas para habilidades do seu lado.",
+	custom_select_limit_warnings_value1 = "Os grupos 1 e 2 vão para a esquerda, os grupos 3 e 4 vão para a direita..",
+	custom_select_limit_warnings_value2 = "Grupos ímpares à esquerda, grupos pares à direita..",
+	custom_select_limit_warnings_value3 = "Exibir avisos para ambos os lados.",
+	custom_select_limit_warnings_value4 = "Exibir avisos apenas para o lado esquerdo.",
+	custom_select_limit_warnings_value5 = "Exibir avisos apenas para o lado direito.",
 })
--- TheVoidspire/Locales/ptBR.lua
+++ TheVoidspire/Locales/ptBR.lua
@@ -1,55 +1,55 @@
 local L = BigWigs:NewBossLocale("Vorasius", "ptBR")
 if not L then return end
 if L then
-	--L.shadowclaw_slam = "Slams"
+	L.shadowclaw_slam = "Golpes"
 end
 
 L = BigWigs:NewBossLocale("Fallen-King Salhadaar", "ptBR")
 if L then
-	--L.fractured_projection = "Kicks"
+	L.fractured_projection = "Cortes"
 end
 
 L = BigWigs:NewBossLocale("Vaelgor & Ezzorak", "ptBR")
 if L then
-	--L.grappling_maw = "Tank Grip"
+	L.grappling_maw = "Agarrão no Tanque"
 end
 
 L = BigWigs:NewBossLocale("Lightblinded Vanguard", "ptBR")
 if L then
-	--L.aura_of_wrath = "Wrath" -- Short for Aura of Wrath
-	--L.execution_sentence = "Executes" -- Short for Execution Sentence
-	--L.executes_mythic = "Executes + Dodge"
-	--L.judgement_red = "Judgement [R]" -- R for the Red icon.
-	--L.aura_of_devotion = "Devotion" -- Short for Aura of Devotion
-	--L.judgement_blue = "Judgement [B]" -- B for the Blue icon.
-	--L.aura_of_peace = "Peace" -- Short for Aura of Peace
-	--L.tyrs_wrath_mythic = "Absorbs + Executes"
-	--L.divine_toll_mythic = "Dodge + Absorbs"
-	--L.zealous_spirit = "Spirit" -- Short for Zealous Spirit
+	L.aura_of_wrath = "Fúria" -- Short for Aura of Wrath
+	L.execution_sentence = "Execuções" -- Short for Execution Sentence
+	L.executes_mythic = "Execuções + Esquiva"
+	L.judgement_red = "Julgamento [V]" -- R for the Red icon.
+	L.aura_of_devotion = "Devoção" -- Short for Aura of Devotion
+	L.judgement_blue = "Julgamento [A]" -- B for the Blue icon.
+	L.aura_of_peace = "Paz" -- Short for Aura of Peace
+	L.tyrs_wrath_mythic = "Absorções + Execuções"
+	L.divine_toll_mythic = "Esquiva + Absorções"
+	L.zealous_spirit = "Espírito" -- Short for Zealous Spirit
 
-	--L.empowered_searing_radiance = "Empowered Searing Radiance"
-	--L.empowered_searing_radiance_desc = "Show the timer for the empowered Searing Radiance"
+	L.empowered_searing_radiance = "Resplendor Calcinante Potencializado"
+	L.empowered_searing_radiance_desc = "Mostra o temporizador para o Resplendor Calcinante potencializado"
 
-	--L.empowered_avengers_shield = "Empowered Avenger's Shield"
-	--L.empowered_avengers_shield_desc = "Show the timer for the empowered Avenger's Shield"
+	L.empowered_avengers_shield = "Escudo do Vingador Potencializado"
+	L.empowered_avengers_shield_desc = "Mostra o temporizador para o Escudo do Vingador potencializado"
 
-	--L.empowered_divine_storm = "Empowered Divine Storm"
-	--L.empowered_divine_storm_desc = "Show the timer for the empowered Divine Storm"
-	--L.tornadoes = "Tornadoes" -- The renamed empowered Divine Storm
+	L.empowered_divine_storm = "Tempestade Divina Potencializada"
+	L.empowered_divine_storm_desc = "Mostra o temporizador para a Tempestade Divina potencializada"
+	L.tornadoes = "Tornados" -- The renamed empowered Divine Storm
 
-	--L.empowered = "[E] %s" -- Empowered version of an ability, [E] Avengers Shield
+	L.empowered = "[P] %s" -- Empowered version of an ability, [E] Avengers Shield
 end
 
 L = BigWigs:NewBossLocale("Crown of the Cosmos", "ptBR")
 if L then
-	--L.silverstrike_arrow = "Arrows"
-	--L.grasp_of_emptiness = "Obelisks"
-	--L.interrupting_tremor = "Interrupt"
-	--L.ravenous_abyss = "Move Out"
-	--L.silverstrike_barrage = "Lines"
-	--L.cosmic_barrier = "Barrier"
-	--L.rangers_captains_mark = "Arrows"
-	--L.voidstalker_sting = "Stings"
-	--L.aspect_of_the_end = "Tethers"
-	--L.devouring_cosmos = "Next Platform"
+	L.silverstrike_arrow = "Flechas"
+	L.grasp_of_emptiness = "Obeliscos"
+	L.interrupting_tremor = "Corte"
+	L.ravenous_abyss = "Afaste-se"
+	L.silverstrike_barrage = "Linhas"
+	L.cosmic_barrier = "Barreira"
+	L.rangers_captains_mark = "Flechas"
+	L.voidstalker_sting = "Ferroadas"
+	L.aspect_of_the_end = "Correntes"
+	L.devouring_cosmos = "Próxima Plataforma"
 end
