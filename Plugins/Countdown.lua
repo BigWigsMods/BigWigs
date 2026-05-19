621135be3e2bb424f5a98d04f872a845d4b1cadb
Matthijs Groot // Justwait <Justw8@users.noreply.github.com>
2026-05-19 21:11:27 +0200

    Plugins/Countdown: Add rounding validation

--- Plugins/Countdown.lua
+++ Plugins/Countdown.lua
@@ -144,7 +144,9 @@ local function updateProfile()
 				end
 			end
 			if db.bossCountdowns[boss][ability].countdownTime and
-			(type(db.bossCountdowns[boss][ability].countdownTime) ~= "number" or db.bossCountdowns[boss][ability].countdownTime > 10 or db.bossCountdowns[boss][ability].countdownTime < 3) then
+			(type(db.bossCountdowns[boss][ability].countdownTime) ~= "number"
+			or db.bossCountdowns[boss][ability].countdownTime > 10 or db.bossCountdowns[boss][ability].countdownTime < 3)
+			or math.floor(db.bossCountdowns[boss][ability].countdownTime+0.5) ~= db.bossCountdowns[boss][ability].countdownTime then
 				db.bossCountdowns[boss][ability].countdownTime = nil
 				if not db.bossCountdowns[boss][ability].voice then
 					db.bossCountdowns[boss][ability] = nil
