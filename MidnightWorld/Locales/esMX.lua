5e4520c2e1e8249671cfceb46b987546d0a5f5fd
asophiel2129 <burtonejairo@gmail.com>
2026-05-22 22:30:56 -0300

    Update esMX.lua

--- MidnightWorld/Locales/esMX.lua
+++ MidnightWorld/Locales/esMX.lua
@@ -5,4 +5,9 @@ BigWigsAPI.SetBossModuleLocale("Thorm'belan", {
 	--ball_fail = "FAIL - Ball touched the ground",
 	--tendrils = "Tendrils",
 	--tendrils_incoming = "RUN AWAY to snap tendrils",
+	ball = "Pelota",
+	ball_incoming = "Se acerca Pelota - Que no toque el piso",
+	ball_fail = "FALLASTE - La Pelota toco el piso",
+	tendrils = "Enredaderas",
+	-endrils_incoming = "CORRE para romper las enredaderas",
 })
