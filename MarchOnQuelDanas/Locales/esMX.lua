07391b298df5f2d3d8fc71f2612dddde407c2bee
Funkeh <funkydude@users.noreply.github.com>
2026-05-23 04:40:50 +0100

    Update esMX.lua

--- MarchOnQuelDanas/Locales/esMX.lua
+++ MarchOnQuelDanas/Locales/esMX.lua
@@ -20,7 +20,7 @@ BigWigsAPI.SetBossModuleLocale("Midnight Falls", {
 	custom_select_limit_warnings = "[Mítico] Restricciones de Alertas de la Fase 3.",
 	custom_select_limit_warnings_desc = "Solo muestra Alertas de habilidades en tu lado.",
 	custom_select_limit_warnings_value1 = "Grupos 1 & 2 izquierda, grupos 3 & 4 derecha.",
-	custom_select_limit_warnings_value2 = "Grupos impares izquierda, grupos pares derecha",
+	custom_select_limit_warnings_value2 = "Grupos impares izquierda, grupos pares derecha.",
 	custom_select_limit_warnings_value3 = "Muestra alertas para ambos lados.",
 	custom_select_limit_warnings_value4 = "Muestra alertas solo para el lado izquierdo.",
 	custom_select_limit_warnings_value5 = "Muestra alertas solo pra el lado derecho.",
