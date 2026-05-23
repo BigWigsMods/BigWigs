18bae140f5ef72a3942df4dc85e8edb7699c69f3
Funkeh <funkydude@users.noreply.github.com>
2026-05-23 04:39:26 +0100

    Update esMX.lua

--- TheVoidspire/Locales/esMX.lua
+++ TheVoidspire/Locales/esMX.lua
@@ -1,19 +1,9 @@
 if not BigWigsAPI.IsLocale("esMX") then return end
 BigWigsAPI.SetBossModuleLocale("Vorasius", {
-	--shadowclaw_slam = "Slams",
 	shadowclaw_slam = "Embates",
 })
 
 BigWigsAPI.SetBossModuleLocale("Lightblinded Vanguard", {
-	--aura_of_wrath = "Wrath", -- Short for Aura of Wrath
-	--execution_sentence = "Executes", -- Short for Execution Sentence
-	--executes_mythic = "Executes + Dodge",
-	--judgement_red = "Judgement [R]", -- R for the Red icon.
-	--aura_of_devotion = "Devotion", -- Short for Aura of Devotion
-	--judgement_blue = "Judgement [B]", -- B for the Blue icon.
-	--aura_of_peace = "Peace", -- Short for Aura of Peace
-	--tyrs_wrath_mythic = "Absorbs + Executes",
-	--divine_toll_mythic = "Dodge + Absorbs",
 	aura_of_wrath = "Cólera", -- Short for Aura of Wrath
 	execution_sentence = "Ejecución", -- Short for Execution Sentence
 	executes_mythic = "Ejecuciones + Esquivar",
@@ -24,36 +14,20 @@ BigWigsAPI.SetBossModuleLocale("Lightblinded Vanguard", {
 	tyrs_wrath_mythic = "Absorciones + Ejecuciones",
 	divine_toll_mythic = "Esquivar + Absorciones",
 
-	--empowered_searing_radiance = "Empowered Searing Radiance",
-	--empowered_searing_radiance_desc = "Show the timer for the empowered Searing Radiance.",
 	empowered_searing_radiance = "Escudo de Vengador Potenciado",
 	empowered_searing_radiance_desc = "Muestra el temporizador para el Escudo de Vengador Potenciado.",
 
-	--empowered_avengers_shield = "Empowered Avenger's Shield",
-	--empowered_avengers_shield_desc = "Show the timer for the empowered Avenger's Shield.",
 	empowered_avengers_shield = "Escudo de Vengador Potenciado",
 	empowered_avengers_shield_desc = "Muestra el temporizador para el Escudo de Vengador Potenciado.",
 
-	--empowered_divine_storm = "Empowered Divine Storm",
-	--empowered_divine_storm_desc = "Show the timer for the empowered Divine Storm.",
-	--tornadoes = "Tornadoes", -- The renamed empowered Divine Storm
 	empowered_divine_storm = "Tormenta Divina Potenciada",
 	empowered_divine_storm_desc = "Muestra el temporizador de la Tormenta Divina Potenciada.",
 	tornadoes = "Tornados", -- The renamed empowered Divine Storm
 
-	--empowered = "[E] %s", -- Empowered version of an ability, [E] Avengers Shield
 	empowered = "[P] %s", -- Empowered version of an ability, [E] Avengers Shield
 })
 
 BigWigsAPI.SetBossModuleLocale("Crown of the Cosmos", {
-	--grasp_of_emptiness = "Obelisks",
-	--interrupting_tremor = "Interrupt",
-	--ravenous_abyss = "Move Out",
-	--silverstrike_barrage = "Lines",
-	--cosmic_barrier = "Barrier",
-	--voidstalker_sting = "Stings",
-	--aspect_of_the_end = "Tethers",
-	--devouring_cosmos = "Next Platform",
 	grasp_of_emptiness = "Obeliscos",
 	interrupting_tremor = "Cortes",
 	ravenous_abyss = "Muevanse",
