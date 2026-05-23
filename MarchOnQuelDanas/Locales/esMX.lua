a7e169e7d76eb5b7525c5e57073621fc897d7b63
Funkeh <funkydude@users.noreply.github.com>
2026-05-23 04:36:59 +0100

    Update esMX.lua

--- MarchOnQuelDanas/Locales/esMX.lua
+++ MarchOnQuelDanas/Locales/esMX.lua
@@ -1,44 +1,24 @@
 if not BigWigsAPI.IsLocale("esMX") then return end
 BigWigsAPI.SetBossModuleLocale("Belo'ren, Child of Al'ar", {
-	--quills = "Quills",
-	--color_swaps = "Color Swaps",
-	--["1241292"] = "Light/Void Dive",
 	quills = "Péndolas",
 	color_swaps = "Cambio de Color",
 	["1241292"] = "Llamas de Luz/Vacío",
 })
 
 BigWigsAPI.SetBossModuleLocale("Midnight Falls", {
-	--deaths_dirge = "Memory Game",
-	--heavens_glaives = "Glaives",
-	--heavens_lance = "Lance",
-	--the_dark_archangel = "Big Boom",
 	deaths_dirge = "Juego de Memoria",
 	heavens_glaives = "Gujas",
 	heavens_lance = "Lanzas",
 	the_dark_archangel = "Explosión",
-	
-	--prism_kicks = "Kicks",
+
 	prism_kicks = "Cortes",
-	
-	--dark_constellation = "Stars",
-	--dark_rune_bar = "Solve the Game",
+
 	dark_constellation = "Estrellas",
 	dark_rune_bar = "Resuelve el Juego",
-	
 
-	--left = "[L] %s", -- left/west group bars in p3
-	--right = "[R] %s", -- right/east group bars in p3
 	left = "[I] %s", -- left/west group bars in p3
 	right = "[D] %s", -- right/east group bars in p3
 
-	--custom_select_limit_warnings = "[Mythic] Restrict Stage 3 Warnings",
-	--custom_select_limit_warnings_desc = "Only show warnings for abilities on your side.",
-	--custom_select_limit_warnings_value1 = "Groups 1 & 2 go left, groups 3 & 4 go right.",
-	--custom_select_limit_warnings_value2 = "Odd groups left, even groups right.",
-	--custom_select_limit_warnings_value3 = "Show warnings for both sides.",
-	--custom_select_limit_warnings_value4 = "Show warnings for left side only.",
-	--custom_select_limit_warnings_value5 = "Show warnings for right side only.",
 	custom_select_limit_warnings = "[Mítico] Restricciones de Alertas de la Fase 3.",
 	custom_select_limit_warnings_desc = "Solo muestra Alertas de habilidades en tu lado.",
 	custom_select_limit_warnings_value1 = "Grupos 1 & 2 izquierda, grupos 3 & 4 derecha.",
