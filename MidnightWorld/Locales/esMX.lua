85923f538a08c26627341abfb3c2e0b67be504e5
Funkeh <funkydude@users.noreply.github.com>
2026-05-23 04:38:52 +0100

    Update esMX.lua

--- MidnightWorld/Locales/esMX.lua
+++ MidnightWorld/Locales/esMX.lua
@@ -1,13 +1,8 @@
 if not BigWigsAPI.IsLocale("esMX") then return end
 BigWigsAPI.SetBossModuleLocale("Thorm'belan", {
-	--ball = "Ball",
-	--ball_incoming = "Ball Incoming - Don't let it touch the ground",
-	--ball_fail = "FAIL - Ball touched the ground",
-	--tendrils = "Tendrils",
-	--tendrils_incoming = "RUN AWAY to snap tendrils",
 	ball = "Pelota",
 	ball_incoming = "Se acerca Pelota - Que no toque el piso",
 	ball_fail = "FALLASTE - La Pelota toco el piso",
 	tendrils = "Enredaderas",
-	-endrils_incoming = "CORRE para romper las enredaderas",
+	tendrils_incoming = "CORRE para romper las enredaderas",
 })
