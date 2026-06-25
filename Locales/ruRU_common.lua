e234bd3eef94c66eff08b0c230005fdbbf9697e6
Funkeh <funkydude@users.noreply.github.com>
2026-06-25 17:22:06 +0100

    Update ruRU_common.lua

--- Locales/ruRU_common.lua
+++ Locales/ruRU_common.lua
@@ -378,4 +378,4 @@ L.tank_frontal = "Танк Фронтал" -- Similar to L.frontal_cone but only
 L.tank_hit = "Танк Удар" -- An attack that will only target the tank, usually a spell that does a lot of heavy damage to the tank
 L.tank_knockback = "Танк Отталкивание" -- Similar to L.knockback but only applies to tanks"
 L.tank_soak = "Танк Соак" -- Similar to L.soak but only applies to tanks
---L.tank_grip = "Танк Притяжка" -- When a boss grapples the tank towards them. We use "Grip" because of the Death Knight ability "Death Grip" (49576) but you can use "Grapple" if it makes more sense
+L.tank_grip = "Танк Притяжка" -- When a boss grapples the tank towards them. We use "Grip" because of the Death Knight ability "Death Grip" (49576) but you can use "Grapple" if it makes more sense
