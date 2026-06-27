19da28c432ababcb3eb635fc3b4f7a55a2ff23c1
funkydude <funkydude@users.noreply.github.com>
2026-06-27 22:58:01 +0100

    Bump version

--- Loader.lua
+++ Loader.lua
@@ -14,7 +14,7 @@ local strfind = string.find
 
 local BIGWIGS_VERSION = 417
 local CONTENT_PACK_VERSIONS = {
-	["LittleWigs"] = {12, 0, 57},
+	["LittleWigs"] = {12, 0, 58},
 	["BigWigs_Classic"] = {12, 0, 19},
 	["BigWigs_BurningCrusade"] = {12, 0, 15},
 	["BigWigs_WrathOfTheLichKing"] = {12, 0, 10},
