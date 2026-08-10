21a179b57ed2870355a0fc851785189cfc762d36
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2026-08-11 03:35:48 +0800

    Update zhCN (#2669)

--- Locales/zhCN_bosses.lua
+++ Locales/zhCN_bosses.lua
@@ -81,10 +81,10 @@ BigWigsAPI.SetBossModuleLocale("Crown of the Cosmos", {
 })
 
 BigWigsAPI.SetBossModuleLocale("Vashnik the Malignant", {
-	--malignant_catalyst = "Catalyst", -- Short for Malignant Catalyst
+	malignant_catalyst = "催化剂", -- “恶性催化剂”简称
 })
 
 BigWigsAPI.SetBossModuleLocale("The Twin Fangs", {
-	--coiling_toxin = "Toxin", -- Short for Coiling Toxin
-	--corrosive_spit = "Spit", -- Short for Corrosive Spit
+	coiling_toxin = "脓液", -- “盘卷脓液”简称
+	corrosive_spit = "唾液", -- “腐蚀唾液”简称
 })
