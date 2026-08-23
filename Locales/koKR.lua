499e00fbb8bfce0557d396272825bcfebd5da741
007bbb <86834094+007bbb@users.noreply.github.com>
2026-08-23 16:48:05 +0900

    Update koKR.lua

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -745,7 +745,7 @@ L.privateAuras = "비공개 오라"
 L.privateAurasDesc1 = "'비공개 오라'는 애드온이 감지하거나 자동화할 수 없는 특별한 유형의 디버프입니다. 이러한 디버프는 현재 모든 최신 보스 전투에서 사용됩니다.\n\n"
 L.privateAurasDesc2 = "BigWigs은 이를 아이콘으로 표시하여 적용 시점을 추적하는 데 도움을 줄 수 있습니다. |cFF33FF99이는 일반 디버프와 구분하여 중요한 디버프를 별도로 표시함으로써 플레이어에게 유용합니다.|r\n\n"
 L.aurasDesc = "BigWigs는 보스 디버프가 적용되었을 때 이를 아이콘으로 표시하여 확인하는 데 도움을 줍니다.\n|cFF33FF99이를 통해 치명적인 디버프를 일반 디버프와 별도로 표시하여 유용하게 활용할 수 있습니다.|r\n\n"
---L.disabledDuringTrash = "Disabled During Trash"
+L.disabledDuringTrash = "휴지통 이동 중 비활성화됨"
 
 L.createTestAura = "테스트 오라 생성"
 L.showDispelType = "해제 유형 표시기 표시"
