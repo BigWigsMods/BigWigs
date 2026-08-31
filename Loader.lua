c485aec120b5e8df3f6623f1bfea2a31b07d4bfc
Nick Towle <ntowle@gmail.com>
2026-08-30 20:52:45 -0400

    better comment

--- Loader.lua
+++ Loader.lua
@@ -1034,7 +1034,8 @@ do
 			else -- Packaged installs of LittleWigs
 				public.littlewigsVersion = GetAddOnMetadata(i, "Version") -- e.g. "v12.1.0" or "v12.1.0-1"
 				if addonEnabled then
-					-- Packaged releases always load the main LittleWigs addon in current season zones
+					-- Packaged releases always load the main LittleWigs addon in current season zones.
+					-- This ensures shared modules will be loaded in seasonal dungeons.
 					for zone in next, public.currentExpansion.currentSeason do
 						enableZones[zone] = true
 						if not loadOnZone[zone] then loadOnZone[zone] = {} end
