e7bec26c85507b5e0fa6b1384c044b9eca643a11
Nick Towle <ntowle@gmail.com>
2026-09-07 21:16:14 -0400

    TheVenomousAbyss: Update aura data

--- TheVenomousAbyss/Sentinels.lua
+++ TheVenomousAbyss/Sentinels.lua
@@ -62,7 +62,7 @@ mod:SetAuraData({
 	{1284590, soundOnApplied = "warning", soundOnAppliedDose = "none", header = CL.important}, -- Helical Toxins
 	{1296880, soundOnApplied = "warning", difficulty = "mythic"}, -- Shifting Protovenom
 	{1288260, soundOnApplied = "warning"}, -- Unstable Miasma
-	{1284471, soundOnApplied = "alarm", header = CL.general}, -- Blighted Blood -- DoT; Drops pool when removed
+	{1284471, soundOnApplied = "alarm", dispel = "magic", header = CL.general}, -- Blighted Blood -- DoT; Drops pool when removed
 	{1284210, soundOnApplied = "underyou"}, -- Blood Venom -- Standing in bad
 	{1284491, soundOnApplied = "none", soundOnAppliedDose = "none", note = CL.tank_debuff}, -- Bloodvenom Injection -- Tank stacks; Drops pool when expires
 	{1288297, soundOnApplied = "none", soundOnAppliedDose = "none"}, -- Clinging Murk -- Soaked Miasma; Drops pool when expires
